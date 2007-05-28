c5b830698857af47ccde2e2d4f92516cf313b606
Mattias Persson <mattias@neotechnology.com>
2007-05-28 15:03:29 +0000

    Moved a simple shell test from neo-utils
    git-svn-id: https://svn.neo4j.org/projects/neo/trunk@24 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ community/src/test/unit/neo/neoshell/NeoShellTest.java
@@ -0,0 +1,50 @@
+package unit.neo.neoshell;
+
+import junit.framework.TestCase;
+import org.neo4j.impl.shell.NeoShellServer;
+import org.neo4j.util.shell.CommandParser;
+
+public class NeoShellTest extends TestCase
+{
+	private CommandParser parse( String line ) throws Exception
+	{
+		return new CommandParser( new NeoShellServer( null, null ), line );
+	}
+	
+	public void testParserEasy() throws Exception
+	{
+		CommandParser parser = this.parse( "ls -la" );
+		assertEquals( "ls", parser.getAppName() );
+		assertEquals( 2, parser.options().size() );
+		assertTrue( parser.options().containsKey( "l" ) );
+		assertTrue( parser.options().containsKey( "a" ) );
+		assertTrue( parser.arguments().isEmpty() );
+	}
+	
+	public void testParserArguments() throws Exception
+	{
+		CommandParser parser =
+			this.parse( "set -t java.lang.Integer key value" );
+		assertEquals( "set", parser.getAppName() );
+		assertTrue( parser.options().containsKey( "t" ) );
+		assertEquals( "java.lang.Integer", parser.options().get( "t" ) );
+		assertEquals( 2, parser.arguments().size() );
+		assertEquals( "key", parser.arguments().get( 0 ) );
+		assertEquals( "value", parser.arguments().get( 1 ) );
+		
+		assertException( "set -tsd" );
+	}
+	
+	private void assertException( String command )
+	{
+		try
+		{
+			this.parse( command );
+			fail( "Should fail" );
+		}
+		catch ( Exception e )
+		{
+			// Good
+		}
+	}
+}
