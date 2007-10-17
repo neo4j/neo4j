8c2e2ae3a3ba60260ce73e86d3ebd5282626aa70
Johan Svensson <johan@neotechnology.com>
2007-10-17 19:29:57 +0000

    o fixed some exception handeling so runtime exceptions aren't wrapped   in a persistence exception when thrown from nioneo package o modified getRelationships methods to use an iterator that checks if   next relationship exist. If relationship can't be found iterator   silenty goes on to the next relationship.
    git-svn-id: https://svn.neo4j.org/projects/neo/trunk@385 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/src/java/org/neo4j/impl/core/RelationshipIterator.java
@@ -0,0 +1,86 @@
+package org.neo4j.impl.core;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.neo4j.api.core.Direction;
+import org.neo4j.api.core.Node;
+import org.neo4j.api.core.Relationship;
+
+class RelationshipIterator implements Iterable<Relationship>, 
+	Iterator<Relationship>
+{
+	private static final NodeManager nm = NodeManager.getManager();
+	
+	private int[] relIds;
+	private Node fromNode;
+	private Direction direction = null;
+	private Relationship nextElement = null;
+	private int nextPosition = 0;
+	
+	RelationshipIterator( int[] relIds, Node fromNode, 
+		Direction direction )
+	{
+		this.relIds = relIds;
+		this.fromNode = fromNode;
+		this.direction = direction;
+	}
+
+	public Iterator<Relationship> iterator()
+	{
+		return new RelationshipIterator( relIds, fromNode, direction );
+	}
+
+	public boolean hasNext()
+	{
+		if ( nextElement != null )
+		{
+			return true;
+		}
+		do
+		{
+			if ( nextPosition < relIds.length )
+			{
+				try
+				{
+					Relationship possibleElement = nm.getRelationshipById( 
+						relIds[nextPosition++] );
+					if ( direction == Direction.INCOMING && 
+						possibleElement.getEndNode().equals( fromNode ) )
+					{
+						nextElement = possibleElement;
+					}
+					else if ( direction == Direction.OUTGOING && 
+						possibleElement.getStartNode().equals( fromNode ) )
+					{
+						nextElement = possibleElement;
+					}
+					else if ( direction == Direction.BOTH )
+					{
+						nextElement = possibleElement;
+					}
+				}
+				catch ( Throwable t )
+				{ // ok unable to get that relationship
+				}
+			}
+		} while ( nextElement == null && nextPosition < relIds.length );
+		return nextElement != null;
+	}
+
+	public Relationship next()
+	{
+		hasNext();
+		if ( nextElement != null )
+		{
+			Relationship elementToReturn = nextElement;
+			nextElement = null;
+			return elementToReturn;
+		}
+		throw new NoSuchElementException();
+	}
+
+	public void remove()
+	{
+		throw new UnsupportedOperationException();
+	}
+}
