2c261382e83c51f7cc595b9225fe52cf240793c1
Johan Svensson <johan@neotechnology.com>
2007-11-19 20:55:07 +0000

    Injected license header in all source files.
    git-svn-id: https://svn.neo4j.org/projects/neo/trunk@510 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/java/org/neo4j/api/core/NeoService.java
+++ enterprise/src/java/org/neo4j/api/core/NeoService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2007 Network Engine for Objects in Lund AB (neotechnology.com)
+ * Copyright 2002-2007 Network Engine for Objects in Lund AB [neotechnology.com]
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
