6269569eb694364ea1e84664edd68d60e9fce0e1
Mattias Persson <mattias@neotechnology.com>
2008-03-19 16:08:36 +0000

    Uses java.lang.reflect.Array class for dealing with neo property arrays instead of the tedious if, else if, else if.... of instanceof statements

--- community/src/main/java/org/neo4j/util/matching/NeoArrayPropertyUtil.java
+++ community/src/main/java/org/neo4j/util/matching/NeoArrayPropertyUtil.java
@@ -1,6 +1,6 @@
 package org.neo4j.util.matching;
 
-import java.util.Arrays;
+import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -24,67 +24,15 @@ public class NeoArrayPropertyUtil
 		Object valueFromNeo )
 	{
 		Set<Object> values = new HashSet<Object>();
-		if ( valueFromNeo instanceof boolean[] )
+		try
 		{
-			values.addAll( Arrays.asList( ( boolean[] ) valueFromNeo ) );
+			int length = Array.getLength( valueFromNeo );
+			for ( int i = 0; i < length; i++ )
+			{
+				values.add( Array.get( valueFromNeo, i ) );
+			}
 		}
-		else if ( valueFromNeo instanceof Boolean[] )
-		{
-			values.addAll( Arrays.asList( ( Boolean[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof byte[] )
-		{
-			values.addAll( Arrays.asList( ( byte[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof Byte[] )
-		{
-			values.addAll( Arrays.asList( ( Byte[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof short[] )
-		{
-			values.addAll( Arrays.asList( ( short[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof Short[] )
-		{
-			values.addAll( Arrays.asList( ( Short[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof int[] )
-		{
-			values.addAll( Arrays.asList( ( int[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof Integer[] )
-		{
-			values.addAll( Arrays.asList( ( Integer[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof long[] )
-		{
-			values.addAll( Arrays.asList( ( long[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof Long[] )
-		{
-			values.addAll( Arrays.asList( ( Long[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof float[] )
-		{
-			values.addAll( Arrays.asList( ( float[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof Float[] )
-		{
-			values.addAll( Arrays.asList( ( Float[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof double[] )
-		{
-			values.addAll( Arrays.asList( ( double[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof Double[] )
-		{
-			values.addAll( Arrays.asList( ( Double[] ) valueFromNeo ) );
-		}
-		else if ( valueFromNeo instanceof String[] )
-		{
-			values.addAll( Arrays.asList( ( String[] ) valueFromNeo ) );
-		}
-		else
+		catch ( IllegalArgumentException e )
 		{
 			values.add( valueFromNeo );
 		}
