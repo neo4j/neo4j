66a26fb02d7806f4e66946e926746954385e6f1f
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2008-09-15 15:18:58 +0000

    Updated the Copyright notices and modified the parent pom so that copyright notice files are always included in the META-INF directory of the produced jar files.
    git-svn-id: https://svn.neo4j.org/components/neo/trunk@2159 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/java/org/neo4j/impl/StandaloneWithShell.java
+++ enterprise/src/java/org/neo4j/impl/StandaloneWithShell.java
@@ -1,7 +1,10 @@
 /*
- * Copyright 2002-2007 Network Engine for Objects in Lund AB [neotechnology.com]
+ * Copyright (c) 2002-2008 "Neo Technology,"
+ *     Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
  * 
- * This program is free software: you can redistribute it and/or modify
+ * Neo4j is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
@@ -12,7 +15,7 @@
  * GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.neo4j.impl;
 
