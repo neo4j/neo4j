9e77d4f398c91bea2db339e541ece54c38824463
Patrik Larsson <patrik.larsson@neotechnology.com>
2008-09-22 15:14:20 +0000

    Corrected a package declaration.
    git-svn-id: https://svn.neo4j.org/components/graph-algo/trunk@2209 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/benchmark/graphGeneration/SubGraphTest.java
+++ community/src/benchmark/graphGeneration/SubGraphTest.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.graphalgo;
+package org.neo4j.graphalgo.benchmark.graphGeneration;
 
 import org.neo4j.api.core.Direction;
 import org.neo4j.graphalgo.benchmark.graphGeneration.SubGraph;
