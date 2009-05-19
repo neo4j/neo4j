31771495d63cce726a1dae6dee78cb24fa9c5286
Mattias Persson <mattias@neotechnology.com>
2009-05-19 08:00:00 +0000

    Fixed some javadoc regarding the shell RMI name for the enableRemoteShell( Map ) method. Fixes #172
    git-svn-id: https://svn.neo4j.org/components/neo/trunk@2859 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/src/java/org/neo4j/api/core/NeoService.java
+++ advanced/src/java/org/neo4j/api/core/NeoService.java
@@ -120,17 +120,17 @@ public interface NeoService
      * configurable port and RMI binding name). It can be accessed by a
      * client that implements <code>org.neo4j.util.shell.ShellClient</code>
      * from the Neo4J <code>shell</code> project. Typically, the
-     * <code>neoshell</code> binary package is used (see
+     * <code>shell</code> binary package is used (see
      * <a href="http://neo4j.org/download">neo4j.org/download</a>).
      * <p>
      * The shell is parameterized by a map of properties passed in to this
      * method. Currently, two properties are used:
      * <ul>
      *	<li><code>port</code>, an {@link Integer} describing the port of the RMI
-     * registry where the Neo shell will be bound, defaults to <code>1337</code>
-     *	<li><code>name</code>, the {@link String} under which the Neo shell will
-     * be bound in the RMI registry, defaults to <code>neoshell</code>
-     * </ul>
+     * registry where the Neo4j shell will be bound, defaults to
+     * <code>1337</code> <li><code>name</code>, the {@link String} under which
+     * the Neo4j shell will be bound in the RMI registry, defaults to
+     * <code>shell</code> </ul>
      * @param initialProperties a set of properties that will be used to
      * configure the remote shell, or <code>null</code> if the default
      * properties should be used
