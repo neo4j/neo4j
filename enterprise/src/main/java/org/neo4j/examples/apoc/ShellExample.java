5a29d764d101ae80c8b9ce6821f1ebdf7dc394bb
Mattias Persson <mattias@neotechnology.com>
2009-11-22 14:35:48 +0000

    Fixed some problems with this example
    git-svn-id: https://svn.neo4j.org/examples/apoc-examples/trunk@3249 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/main/java/org/neo4j/examples/apoc/ShellExample.java
+++ enterprise/src/main/java/org/neo4j/examples/apoc/ShellExample.java
@@ -2,6 +2,11 @@ package org.neo4j.examples.apoc;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import org.neo4j.api.core.Direction;
 import org.neo4j.api.core.EmbeddedNeo;
@@ -25,13 +30,14 @@ public class ShellExample
     {
         USERS_REFERENCE,
         USER,
+        KNOWS,
     }
     
     public static void main( String[] args ) throws Exception
     {
-        neo = new EmbeddedNeo( NEO_DB_PATH );
         registerShutdownHookForNeo();
         
+        startNeo();
         createExampleNodeSpace();
         boolean trueForLocal = waitForUserInput( "Would you like to start a " +
             "local shell instance or enable neo to accept remote " +
@@ -44,26 +50,34 @@ public class ShellExample
         {
             startRemoteShellAndWait();
         }
-        deleteExampleNodeSpace();
         
         System.out.println( "Shutting down..." );
         shutdown();
     }
     
+    private static void startNeo()
+    {
+        neo = new EmbeddedNeo( NEO_DB_PATH );
+    }
+    
     private static void startLocalShell() throws Exception
     {
-        ShellServer shellServer = new LocalNeoShellServer( neo );
+        shutdownNeo();
+        
+        ShellServer shellServer = new LocalNeoShellServer( NEO_DB_PATH, false );
         new SameJvmClient( shellServer ).grabPrompt();
         shellServer.shutdown();
+        
+        startNeo();
     }
 
     private static void startRemoteShellAndWait() throws Exception
     {
         neo.enableRemoteShell();
         waitForUserInput( "Remote shell enabled, connect to it by running:\n" +
-            "java -jar shell.jar\n" +
+            "java -jar lib/shell-<version>.jar\n" +
             "\nWhen you're done playing around, just press any key " +
-            "in this terminal" );
+            "in this terminal " );
     }
     
     private static String waitForUserInput( String textToSystemOut )
@@ -82,16 +96,34 @@ public class ShellExample
             // Create users sub reference node (see design guide lines on
             // http://wiki.neo4j.org)
             System.out.println( "Creating example node space..." );
+            Random random = new Random();
             Node usersReferenceNode = neo.createNode();
             neo.getReferenceNode().createRelationshipTo( usersReferenceNode,
                 RelTypes.USERS_REFERENCE );
             
             // Create some users and index their names with the IndexService
+            List<Node> users = new ArrayList<Node>();
             for ( int id = 0; id < 100; id++ )
             {
                 Node userNode = createUser( formUserName( id ) ); 
                 usersReferenceNode.createRelationshipTo( userNode,
                     RelTypes.USER );
+                if ( id > 10 )
+                {
+                    int numberOfFriends = random.nextInt( 5 );
+                    Set<Node> knows = new HashSet<Node>();
+                    for ( int i = 0; i < numberOfFriends; i++ )
+                    {
+                        Node friend = users.get( random.nextInt(
+                            users.size() ) );
+                        if ( knows.add( friend ) )
+                        {
+                            userNode.createRelationshipTo( friend,
+                                RelTypes.KNOWS );
+                        }
+                    }
+                }
+                users.add( userNode );
             }
             tx.success();
         }
@@ -116,6 +148,11 @@ public class ShellExample
                     Direction.OUTGOING ) )
             {
                 Node user = relationship.getEndNode();
+                for ( Relationship knowsRelationship : user.getRelationships(
+                    RelTypes.KNOWS ) )
+                {
+                    knowsRelationship.delete();
+                }
                 user.delete();
                 relationship.delete();
             }
@@ -130,9 +167,19 @@ public class ShellExample
         }
     }
     
-    private static void shutdown()
+    private static void shutdownNeo()
     {
         neo.shutdown();
+        neo = null;
+    }
+    
+    private static void shutdown()
+    {
+        if ( neo != null )
+        {
+            deleteExampleNodeSpace();
+            shutdownNeo();
+        }
     }
     
     private static void registerShutdownHookForNeo()
