a9c76746faba08d8129611d9aabfa44fe79562c7
Mattias Persson <mattias@neotechnology.com>
2009-12-09 13:38:05 +0000

    Removed shell as compile-time dependency from neo and a NeoService can now instead start a ShellServer purely via reflections.
    The shell got a dependency on neo and doesn't need to to everything
    via reflections anymore. It can, however, still be used as a remote client
    w/o the neo/jta jars on the classpath (java -jar shell.jar) for
    connecting to an already running neo service.
    
    This dependency hierarchy feels better and more right than the
    other way around.
    
    
    git-svn-id: https://svn.neo4j.org/components/neo/trunk@3304 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ advanced/src/java/org/neo4j/api/core/ShellService.java
@@ -0,0 +1,105 @@
+package org.neo4j.api.core;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.util.Map;
+
+/**
+ * Can start and stop a shell server via reflection if the shell should happen
+ * to be on the classpath, else it will gracefully say that it isn't there.
+ */
+class ShellService
+{
+    private final NeoService neo;
+    private final Object shellServer;
+    
+    ShellService( NeoService neo, Map<String, Serializable> config )
+        throws ShellNotAvailableException, RemoteException
+    {
+        this.neo = neo;
+        if ( !shellDependencyAvailable() )
+        {
+            throw new ShellNotAvailableException();
+        }
+        this.shellServer = startShellServer( config );
+    }
+    
+    private boolean shellDependencyAvailable()
+    {
+        try
+        {
+            Class.forName( "org.neo4j.shell.ShellServer" );
+            return true;
+        }
+        catch ( Throwable t )
+        {
+            return false;
+        }
+    }
+    
+    private Object startShellServer( Map<String, Serializable> config )
+        throws RemoteException
+    {
+        Integer port = ( Integer )
+            getConfig( config, "port", "DEFAULT_PORT" );
+        String name = ( String )
+            getConfig( config, "port", "DEFAULT_NAME" );
+        try
+        {
+            Class<?> shellServerClass =
+                Class.forName( "org.neo4j.shell.neo.NeoShellServer" );
+            Object shellServer = shellServerClass.getConstructor(
+                NeoService.class ).newInstance( neo );
+            shellServer.getClass().getMethod( "makeRemotelyAvailable",
+                Integer.TYPE, String.class ).invoke( shellServer, port, name );
+            return shellServer;
+        }
+        catch ( Exception e )
+        {
+            throw new RemoteException( "Couldn't start shell '" + name +
+                "' at port " + port, e );
+        }
+    }
+    
+    private Serializable getConfig( Map<String, Serializable> config,
+        String key, String defaultVariableName ) throws RemoteException
+    {
+        Serializable result = config.get( key );
+        if ( result == null )
+        {
+            try
+            {
+                result = ( Serializable ) Class.forName(
+                    "org.neo4j.shell.AbstractServer" ).
+                        getDeclaredField( defaultVariableName ).get( null );
+            }
+            catch ( Exception e )
+            {
+                throw new RemoteException( "Default variable not found", e );
+            }
+        }
+        return result;
+    }
+
+    public boolean shutdown() throws ShellNotAvailableException
+    {
+        try
+        {
+            shellServer.getClass().getMethod( "shutdown" ).invoke( shellServer );
+            return true;
+        }
+        catch ( Exception e )
+        {
+            // TODO Really swallow this? Why not, who cares?
+            return false;
+        }
+    }
+    
+    static class ShellNotAvailableException extends Exception
+    {
+        public ShellNotAvailableException()
+        {
+            super();
+        }
+    }
+}
