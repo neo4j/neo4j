37f4711d48cf427a1ab2e8131bc3ec6193bb1b7d
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2009-12-18 12:05:40 +0000

    Implementing the new IndexService interface (with IndexHits) in remote neo. Also updating copyright information.
    git-svn-id: https://svn.neo4j.org/components/remote-neo/trunk@3383 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/main/java/org/neo4j/remote/RemoteIndexService.java
+++ enterprise/src/main/java/org/neo4j/remote/RemoteIndexService.java
@@ -1,15 +1,18 @@
 /*
- * Copyright 2008-2009 Network Engine for Objects in Lund AB [neotechnology.com]
+ * Copyright (c) 2008-2009 "Neo Technology,"
+ *     Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
  * 
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
@@ -20,6 +23,7 @@ import java.util.Iterator;
 
 import org.neo4j.api.core.NeoService;
 import org.neo4j.api.core.Node;
+import org.neo4j.util.index.IndexHits;
 import org.neo4j.util.index.IndexService;
 import org.neo4j.util.index.Isolation;
 
@@ -69,7 +73,7 @@ public final class RemoteIndexService implements IndexService
         engine = null;
     }
 
-    public Iterable<Node> getNodes( String key, Object value )
+    public IndexHits<Node> getNodes( String key, Object value )
     {
         return engine().current().getIndexNodes( id, key, value );
     }
