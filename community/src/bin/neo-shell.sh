2e31f4d02f7c36659b141b653eb75da464b196fb
Peter Neubauer <peter.neubauer@neotechnology.com>
2009-12-27 16:00:55 +0000

    adjusting shell assembly to changed JTA spec jar
    git-svn-id: https://svn.neo4j.org/components/shell/trunk@3410 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/bin/neo-shell.sh
+++ community/src/bin/neo-shell.sh
@@ -6,4 +6,4 @@
 
 SCRIPTDIR=`dirname "$0"`
 
-java $JAVA_OPTS -cp $SCRIPTDIR:$SCRIPTDIR/neo-${neo.version}.jar:$SCRIPTDIR/jta-${jta.version}.jar:$SCRIPTDIR/shell-${project.version}.jar:$SCRIPTDIR/jline-${jline.version}.jar org.neo4j.shell.StartLocalClient "$@"
+java $JAVA_OPTS -cp $SCRIPTDIR:$SCRIPTDIR/neo-${neo.version}.jar:$SCRIPTDIR/geronimo-jta_${jta.version}_spec-1.1.1.jar:$SCRIPTDIR/shell-${project.version}.jar:$SCRIPTDIR/jline-${jline.version}.jar org.neo4j.shell.StartLocalClient "$@"
