99ac95c4c035e313dcb4790a39cb2797814cff39
Mattias Persson <mattias@neotechnology.com>
2010-02-01 08:28:33 +0000

    Updated to point to the correct jar files
    git-svn-id: https://svn.neo4j.org/components/shell/trunk@3665 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/bin/neo4j-shell.bat
+++ community/src/bin/neo4j-shell.bat
@@ -2,4 +2,4 @@
 SETLOCAL
 set _SCRIPTS_=%~dp0
 
-java %JAVA_OPTS% -cp ".;%_SCRIPTS_%\neo-${project.version}.jar:$SCRIPTDIR\geronimo-jta_${jta.version}_spec-1.1.1.jar;%_SCRIPTS_%\shell-${project.version}.jar;%_SCRIPTS_%\jline-${jline.version}.jar org.neo4j.shell.StartLocalClient %1 %2 %3 %4 %5 %6 %7 %8 %9
+java %JAVA_OPTS% -cp ".;%_SCRIPTS_%\neo4j-kernel-${project.version}.jar:$SCRIPTDIR\geronimo-jta_${jta.version}_spec-1.1.1.jar;%_SCRIPTS_%\neo4j-shell-${project.version}.jar;%_SCRIPTS_%\jline-${jline.version}.jar org.neo4j.shell.StartClient %1 %2 %3 %4 %5 %6 %7 %8 %9
