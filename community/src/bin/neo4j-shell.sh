03cbe438ccaed404c7a0662b810be8ddd68a9781
Mattias Persson <mattias@neotechnology.com>
2010-02-04 15:56:32 +0000

    Fixed some (maven) properties typos
    git-svn-id: https://svn.neo4j.org/components/shell/trunk@3742 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/bin/neo4j-shell.sh
+++ community/src/bin/neo4j-shell.sh
@@ -6,4 +6,4 @@
 
 SCRIPTDIR=`dirname "$0"`
 
-java $JAVA_OPTS -cp $SCRIPTDIR:$SCRIPTDIR/neo4j-kernel-${neo.version}.jar:$SCRIPTDIR/geronimo-jta_${jta.version}_spec-1.1.1.jar:$SCRIPTDIR/neo4j-shell-${project.version}.jar:$SCRIPTDIR/jline-${jline.version}.jar org.neo4j.shell.StartClient "$@"
+java $JAVA_OPTS -cp $SCRIPTDIR:$SCRIPTDIR/neo4j-kernel-${neo4j.version}.jar:$SCRIPTDIR/geronimo-jta_${jta.version}_spec-1.1.1.jar:$SCRIPTDIR/neo4j-shell-${project.version}.jar:$SCRIPTDIR/jline-${jline.version}.jar org.neo4j.shell.StartClient "$@"
