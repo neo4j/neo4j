b4c633752ae51a778c818844f430be9947b17b1d
Anders Nawroth <anders@neotechnology.com>
2010-02-05 10:50:32 +0000

    Improved on packaging, fixed kernel version number issue in the manifest.
    git-svn-id: https://svn.neo4j.org/components/shell/trunk@3758 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/bin/neo4j-shell.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Script to start the Neo4j shell.
-#
-#
-
-SCRIPTDIR=`dirname "$0"`
-
-java $JAVA_OPTS -cp $SCRIPTDIR:$SCRIPTDIR/neo4j-kernel-${neo4j.version}.jar:$SCRIPTDIR/geronimo-jta_${jta.version}_spec-1.1.1.jar:$SCRIPTDIR/neo4j-shell-${project.version}.jar:$SCRIPTDIR/jline-${jline.version}.jar org.neo4j.shell.StartClient "$@"
