472dd0ab34a5daab03d169dc276f549cf948e307
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2010-04-28 15:55:59 +0000

    Added JMX information about known XA Resources.
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@4355 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/src/main/java/org/neo4j/kernel/manage/XaMonitor.java
+++ advanced/src/main/java/org/neo4j/kernel/manage/XaMonitor.java
@@ -1,32 +1,45 @@
 package org.neo4j.kernel.manage;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.neo4j.kernel.impl.transaction.XaDataSourceManager;
+import org.neo4j.kernel.impl.transaction.xaframework.XaDataSource;
+
 abstract class XaMonitor extends Neo4jJmx
 {
     static class XaManager extends XaMonitor implements XaManagerMBean
     {
-        XaManager( int instanceId )
+        XaManager( int instanceId, XaDataSourceManager datasourceMananger )
         {
-            super( instanceId );
+            super( instanceId, datasourceMananger );
         }
     }
 
     static class MXBeanImplementation extends XaMonitor implements
             XaManagerMXBean
     {
-        MXBeanImplementation( int instanceId )
+        MXBeanImplementation( int instanceId, XaDataSourceManager datasourceMananger )
         {
-            super( instanceId );
+            super( instanceId, datasourceMananger );
         }
     }
 
-    private XaMonitor( int instanceId )
+    private final XaDataSourceManager datasourceMananger;
+
+    private XaMonitor( int instanceId, XaDataSourceManager datasourceMananger )
     {
         super( instanceId );
+        this.datasourceMananger = datasourceMananger;
     }
 
     public XaResourceInfo[] getXaResources()
     {
-        // TODO Auto-generated method stub
-        return new XaResourceInfo[0];
+        List<XaResourceInfo> result = new ArrayList<XaResourceInfo>();
+        for ( XaDataSource datasource : datasourceMananger.getAllRegisteredDataSources() )
+        {
+            result.add( new XaResourceInfo( datasource ) );
+        }
+        return result.toArray( new XaResourceInfo[result.size()] );
     }
 }
--- advanced/src/main/java/org/neo4j/kernel/manage/XaResourceInfo.java
+++ advanced/src/main/java/org/neo4j/kernel/manage/XaResourceInfo.java
@@ -3,18 +3,47 @@ package org.neo4j.kernel.manage;
 import java.beans.ConstructorProperties;
 import java.io.Serializable;
 
+import org.neo4j.kernel.impl.transaction.xaframework.XaDataSource;
+
 public final class XaResourceInfo implements Serializable
 {
     private final String name;
+    private final String branchId;
 
-    @ConstructorProperties( { "Name" } )
-    public XaResourceInfo( String name )
+    @ConstructorProperties( { "name", "branchId" } )
+    public XaResourceInfo( String name, String branchId )
     {
         this.name = name;
+        this.branchId = branchId;
+    }
+
+    XaResourceInfo( XaDataSource datasource )
+    {
+        this( datasource.getName(), toHexString( datasource.getBranchId() ) );
     }
 
     public String getName()
     {
         return name;
+    }
+
+    public String getBranchId()
+    {
+        return branchId;
+    }
+
+    private static String toHexString( byte[] branchId )
+    {
+        StringBuilder result = new StringBuilder();
+        for ( byte part : branchId )
+        {
+            String chunk = Integer.toHexString( part );
+            if ( chunk.length() < 2 ) result.append( "0" );
+            if ( chunk.length() > 2 )
+                result.append( chunk.substring( chunk.length() - 2 ) );
+            else
+                result.append( chunk );
+        }
+        return result.toString();
     }
 }
