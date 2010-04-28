e0b425387b27375e95b59a1063db4698180f59f0
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2010-04-28 08:40:08 +0000

    Started working on using ProtoBuffers as Transport for remote-graphdb.
    git-svn-id: https://svn.neo4j.org/components/remote-graphdb/trunk@4337 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/main/java/org/neo4j/remote/RemoteGraphDatabase.java
+++ community/src/main/java/org/neo4j/remote/RemoteGraphDatabase.java
@@ -3,17 +3,17 @@
  *     Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
- * 
+ *
  * Neo4j is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -29,6 +29,8 @@ import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.event.KernelEventHandler;
+import org.neo4j.graphdb.event.TransactionEventHandler;
 
 /**
  * A remote connection to a running {@link GraphDatabaseService Graph Database}
@@ -172,7 +174,7 @@ public final class RemoteGraphDatabase implements GraphDatabaseService
 
     private RemoteGraphDatabase( RemoteConnection connection, ConfigurationModule config )
     {
-        this.engine = new RemoteGraphDbEngine( connection, config );
+        this.engine = new RemoteGraphDbEngine( this, connection, config );
     }
 
     /**
@@ -229,6 +231,34 @@ public final class RemoteGraphDatabase implements GraphDatabaseService
         engine.shutdown();
     }
 
+    public KernelEventHandler registerKernelEventHandler(
+            KernelEventHandler handler )
+    {
+        throw new UnsupportedOperationException(
+                "Event handlers not suppoerted by RemoteGraphDatabase." );
+    }
+
+    public KernelEventHandler unregisterKernelEventHandler(
+            KernelEventHandler handler )
+    {
+        throw new UnsupportedOperationException(
+                "Event handlers not suppoerted by RemoteGraphDatabase." );
+    }
+
+    public <T> TransactionEventHandler<T> registerTransactionEventHandler(
+            TransactionEventHandler<T> handler )
+    {
+        throw new UnsupportedOperationException(
+                "Event handlers not suppoerted by RemoteGraphDatabase." );
+    }
+
+    public <T> TransactionEventHandler<T> unregisterTransactionEventHandler(
+            TransactionEventHandler<T> handler )
+    {
+        throw new UnsupportedOperationException(
+                "Event handlers not suppoerted by RemoteGraphDatabase." );
+    }
+
     RemoteGraphDbEngine getEngine()
     {
         return engine;
@@ -241,8 +271,7 @@ public final class RemoteGraphDatabase implements GraphDatabaseService
         return false;
     }
 
-    public boolean enableRemoteShell(
-        Map<String, Serializable> initialProperties )
+    public boolean enableRemoteShell( Map<String, Serializable> initialProperties )
     {
         // NOTE This might not be something that we wish to support in RemoteGraphDatabase
         return false;
--- community/src/main/java/org/neo4j/remote/RemotePropertyContainer.java
+++ community/src/main/java/org/neo4j/remote/RemotePropertyContainer.java
@@ -3,17 +3,17 @@
  *     Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
- * 
+ *
  * Neo4j is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,6 +21,7 @@ package org.neo4j.remote;
 
 import java.util.Iterator;
 
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.NotFoundException;
 import org.neo4j.graphdb.PropertyContainer;
 
@@ -33,6 +34,11 @@ abstract class RemotePropertyContainer implements PropertyContainer
     {
         this.engine = engine;
         this.id = id;
+    }
+
+    public GraphDatabaseService getGraphDatabase()
+    {
+        return engine.graphDb;
     }
 
     public final Object getProperty( String key )
