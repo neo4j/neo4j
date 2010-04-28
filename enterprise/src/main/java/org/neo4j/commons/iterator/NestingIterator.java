9096fd9f975474e51984e936df4ccbedbf092f72
Mattias Persson <mattias@neotechnology.com>
2010-04-28 14:36:39 +0000

    Moved commons component into kernel instead of having it as a separate component
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@4351 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/src/main/java/org/neo4j/commons/iterator/NestingIterator.java
@@ -0,0 +1,57 @@
+package org.neo4j.commons.iterator;
+
+import java.util.Iterator;
+
+/**
+ * For each item in the supplied iterator (called "surface item") there's
+ * instantiated an iterator from that item which is iterated before moving
+ * on to the next surface item.
+ *
+ * @param <T> the type of items to return
+ * @param <U> the type of items in the surface item iterator
+ */
+public abstract class NestingIterator<T, U> extends PrefetchingIterator<T>
+{
+	private Iterator<U> source;
+	private Iterator<T> currentNestedIterator;
+	private U currentSurfaceItem;
+	
+	public NestingIterator( Iterator<U> source )
+	{
+		this.source = source;
+	}
+	
+	protected abstract Iterator<T> createNestedIterator( U item );
+	
+	public U getCurrentSurfaceItem()
+	{
+		if ( this.currentSurfaceItem == null )
+		{
+			throw new IllegalStateException( "Has no surface item right now," +
+				" you must do at least one next() first" );
+		}
+		return this.currentSurfaceItem;
+	}
+	
+	@Override
+	protected T fetchNextOrNull()
+	{
+		if ( currentNestedIterator == null ||
+			!currentNestedIterator.hasNext() )
+		{
+			while ( source.hasNext() )
+			{
+				currentSurfaceItem = source.next();
+				currentNestedIterator =
+					createNestedIterator( currentSurfaceItem );
+				if ( currentNestedIterator.hasNext() )
+				{
+					break;
+				}
+			}
+		}
+		return currentNestedIterator != null &&
+			currentNestedIterator.hasNext() ?
+			currentNestedIterator.next() : null;
+	}
+}
