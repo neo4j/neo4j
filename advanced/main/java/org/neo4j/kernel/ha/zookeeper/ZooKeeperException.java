ede1b749768a8379d39fcac9beefd97d817a5565
Mattias Persson <mattias@neotechnology.com>
2010-05-07 12:57:55 +0000

    Added a component to have the HA dependencies and implementation, extracted from online-backup HA branch
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@4400 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ advanced/main/java/org/neo4j/kernel/ha/zookeeper/ZooKeeperException.java
@@ -0,0 +1,9 @@
+package org.neo4j.kernel.ha.zookeeper;
+
+public class ZooKeeperException extends RuntimeException
+{
+    public ZooKeeperException( String message, Throwable cause )
+    {
+        super( message, cause );
+    }
+}
