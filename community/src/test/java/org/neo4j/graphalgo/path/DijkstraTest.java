b28fa5057a4d4a45c27d054653275e99b6debf7d
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2010-06-15 10:45:32 +0000

    Fixed some changes that didn't make it into r4549 due to some bug in subversion.
    git-svn-id: https://svn.neo4j.org/components/graph-algo/trunk@4550 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/test/java/org/neo4j/graphalgo/path/DijkstraTest.java
+++ community/src/test/java/org/neo4j/graphalgo/path/DijkstraTest.java
@@ -11,7 +11,9 @@ import java.util.Set;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import org.neo4j.graphalgo.util.DoubleEvaluator;
+import org.neo4j.graphalgo.WeightedPath;
+import org.neo4j.graphalgo.impl.path.Dijkstra;
+import org.neo4j.graphalgo.impl.util.DoubleEvaluator;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
 import org.neo4j.graphdb.Relationship;
