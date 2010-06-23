532b4995b15a32c6971dc736c340471f4866fb3a
Mattias Persson <mattias@neotechnology.com>
2010-06-23 19:24:54 +0000

    A filtering iterator which catches exceptions and skips such entries
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@4611 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ advanced/src/main/java/org/neo4j/commons/iterator/CatchingIteratorWrapper.java
@@ -0,0 +1,50 @@
+package org.neo4j.commons.iterator;
+
+import java.util.Iterator;
+
+public abstract class CatchingIteratorWrapper<T, U> extends PrefetchingIterator<T>
+{
+    private final Iterator<U> source;
+
+    public CatchingIteratorWrapper( Iterator<U> source )
+    {
+        this.source = source;
+    }
+
+    @Override
+    protected T fetchNextOrNull()
+    {
+        while ( source.hasNext() )
+        {
+            try
+            {
+                U nextItem = source.next();
+                return underlyingObjectToObject( nextItem );
+            }
+            catch ( Throwable t )
+            {
+                if ( exceptionOk( t ) )
+                {
+                    continue;
+                }
+                if ( t instanceof RuntimeException )
+                {
+                    throw (RuntimeException) t;
+                }
+                else if ( t instanceof Error )
+                {
+                    throw (Error) t;
+                }
+                throw new RuntimeException( t );
+            }
+        }
+        return null;
+    }
+    
+    protected boolean exceptionOk( Throwable t )
+    {
+        return true;
+    }
+
+    protected abstract T underlyingObjectToObject( U object );
+}
