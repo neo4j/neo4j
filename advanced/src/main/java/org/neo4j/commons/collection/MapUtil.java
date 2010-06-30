7bcdc214ebce87e03090715b04f69c9e57707a10
Mattias Persson <mattias@neotechnology.com>
2010-06-30 08:47:14 +0000

    Removed a strange comment and renamed the MapUtil methods to better names
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@4640 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/src/main/java/org/neo4j/commons/collection/MapUtil.java
+++ advanced/src/main/java/org/neo4j/commons/collection/MapUtil.java
@@ -6,13 +6,13 @@ import java.util.Map;
 public abstract class MapUtil
 {
     /**
-     * A short-hand method for creating a Map <bold>of</bold> pairs of key/value.
+     * A short-hand method for creating a {@link Map} pairs of key/value.
      * 
      * @param objects alternating key and value.
      * @return a Map with the entries supplied by {@code objects}.
      */
     @SuppressWarnings("unchecked")
-    public static <K, V> Map<K, V> genericOf( Object... objects )
+    public static <K, V> Map<K, V> genericMap( Object... objects )
     {
         Map<K, V> map = new HashMap<K, V>();
         int i = 0;
@@ -24,13 +24,27 @@ public abstract class MapUtil
     }
     
     /**
-     * A short-hand method for creating a Map <bold>of</bold> pairs of key/value.
+     * A short-hand method for creating a {@link Map} pairs of key/value
+     * where both keys and values are {@link String}s.
      * 
      * @param objects alternating key and value.
      * @return a Map with the entries supplied by {@code objects}.
      */
-    public static Map<String, Object> of( Object... objects )
+    public static Map<String, String> stringMap( String... strings )
     {
-        return genericOf( objects );
+        return genericMap( (Object[]) strings );
+    }
+    
+    /**
+     * A short-hand method for creating a {@link Map} of pairs of
+     * key/value where keys are {@link String}s and values are
+     * {@link Object}s.
+     * 
+     * @param objects alternating key and value.
+     * @return a Map with the entries supplied by {@code objects}.
+     */
+    public static Map<String, Object> map( Object... objects )
+    {
+        return genericMap( objects );
     }
 }
