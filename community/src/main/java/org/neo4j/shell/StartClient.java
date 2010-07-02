afd2946ce3e46521af7e44b6a2b40379a49aa20d
Mattias Persson <mattias@neotechnology.com>
2010-07-02 16:18:38 +0000

    Merged all those classes into one. Why have 4 classes to start the shell?
    git-svn-id: https://svn.neo4j.org/components/shell/trunk@4666 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/main/java/org/neo4j/shell/StartClient.java
+++ community/src/main/java/org/neo4j/shell/StartClient.java
@@ -19,18 +19,41 @@
  */
 package org.neo4j.shell;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.rmi.ConnectException;
+import java.rmi.RemoteException;
 import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.neo4j.commons.commandline.Args;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.shell.impl.AbstractStarter;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+import org.neo4j.kernel.EmbeddedReadOnlyGraphDatabase;
+import org.neo4j.shell.impl.AbstractServer;
+import org.neo4j.shell.impl.SameJvmClient;
+import org.neo4j.shell.impl.StandardConsole;
 import org.neo4j.shell.kernel.GraphDatabaseShellServer;
 
 /**
  * Can start clients, either via {@link StartRemoteClient} or
  * {@link StartLocalClient}.
  */
-public class StartClient extends AbstractStarter
+public class StartClient
 {
+    private AtomicBoolean hasBeenShutdown = new AtomicBoolean();
+    public static final String ARG_PATH = "path";
+    public static final String ARG_READONLY = "readonly";
+    public static final String ARG_PORT = "port";
+    public static final String ARG_NAME = "name";
+    
+    private StartClient()
+    {
+    }
+    
     /**
      * Starts a shell client. Remote or local depending on the arguments.
      * @param args the arguments from the command line. Can contain
@@ -38,29 +61,30 @@ public class StartClient extends AbstractStarter
      * {@link GraphDatabaseShellServer} or connect to an already running
      * {@link GraphDatabaseService}.
      */
-    public static void main( String[] args )
+    public static void main( String[] arguments )
     {
-        Map<String, String> argMap = parseArgs( args );
-        if ( argMap.containsKey( "?" ) || argMap.containsKey( "h" ) ||
-                argMap.containsKey( "help" ) || argMap.containsKey( "usage" ) )
+        new StartClient().start( arguments );
+    }
+    
+    private void start( String[] arguments )
+    {
+        Args args = new Args( arguments );
+        if ( args.has( "?" ) || args.has( "h" ) || args.has( "help" ) || args.has( "usage" ) )
         {
             printUsage();
             return;
         }
         
-        String path = argMap.get( StartLocalClient.ARG_PATH );
-        String port = argMap.get( StartRemoteClient.ARG_PORT );
-        String name = argMap.get( StartRemoteClient.ARG_NAME );
+        String path = args.get( ARG_PATH, null );
+        String port = args.get( ARG_PORT, null );
+        String name = args.get( ARG_NAME, null );
         
         if ( path != null && ( port != null || name != null ) )
         {
             System.err.println( "You have supplied both " +
-                StartLocalClient.ARG_PATH + " as well as " +
-                StartRemoteClient.ARG_PORT + "/" +
-                StartRemoteClient.ARG_NAME + ". " +
-                "You should either supply only " + StartLocalClient.ARG_PATH +
-                " or " + StartRemoteClient.ARG_PORT + "/" +
-                StartRemoteClient.ARG_NAME + " so that either a local or " +
+                ARG_PATH + " as well as " + ARG_PORT + "/" + ARG_NAME + ". " +
+                "You should either supply only " + ARG_PATH +
+                " or " + ARG_PORT + "/" + ARG_NAME + " so that either a local or " +
                 "remote shell client can be started" );
             return;
         }
@@ -73,14 +97,14 @@ public class StartClient extends AbstractStarter
             }
             catch ( ShellException e )
             {
-                handleException( e, argMap );
+                handleException( e, args );
             }
-            StartLocalClient.main( args );
+            startLocal( args );
         }
         // Remote
         else
         {
-            StartRemoteClient.main( args );
+            startRemote( args );
         }
     }
 
@@ -94,5 +118,233 @@ public class StartClient extends AbstractStarter
         {
             throw new ShellException( "Neo4j not found on the classpath", e );
         }
+    }
+
+    private void startLocal( Args args )
+    {
+        String dbPath = args.get( ARG_PATH, null );
+        if ( dbPath == null )
+        {
+            System.err.println( "ERROR: To start a local Neo4j service and a " +
+                "shell client on top of that you need to supply a path to a " +
+                "Neo4j store or just a new path where a new store will " +
+                "be created if it doesn't exist. -" + ARG_PATH +
+                " /my/path/here" );
+            return;
+        }
+        
+        try
+        {
+            boolean readOnly = args.getBoolean( ARG_READONLY, false );
+            tryStartLocalServerAndClient( dbPath, readOnly, args );
+        }
+        catch ( Exception e )
+        {
+            if ( storeWasLocked( e ) )
+            {
+                if ( wantToConnectReadOnlyInstead() )
+                {
+                    try
+                    {
+                        tryStartLocalServerAndClient( dbPath, true, args );
+                    }
+                    catch ( Exception innerException )
+                    {
+                        handleException( innerException, args );
+                    }
+                }
+                else
+                {
+                    handleException( e, args );
+                }
+            }
+            else
+            {
+                handleException( e, args );
+            }
+        }
+        System.exit( 0 );
+    }
+
+    private static boolean wantToConnectReadOnlyInstead()
+    {
+        Console console = new StandardConsole();
+        console.format( "\nThe store seem locked. Start a read-only client " +
+            "instead (y/n) [y]? " );
+        String input = console.readLine( "" );
+        return input.length() == 0 || input.equals( "y" );
+    }
+
+    private static boolean storeWasLocked( Exception e )
+    {
+        // TODO Fix this when a specific exception is thrown
+        return mineException( e, IllegalStateException.class,
+            "Unable to lock store" );
+    }
+
+    private static boolean mineException( Throwable e,
+        Class<IllegalStateException> eClass, String startOfMessage )
+    {
+        if ( eClass.isInstance( e ) &&
+            e.getMessage().startsWith( startOfMessage ) )
+        {
+            return true;
+        }
+        
+        Throwable cause = e.getCause();
+        if ( cause != null )
+        {
+            return mineException( cause, eClass, startOfMessage );
+        }
+        return false;
+    }
+
+    private void tryStartLocalServerAndClient( String dbPath,
+        boolean readOnly, Args args ) throws Exception
+    {
+        final GraphDatabaseService graph = readOnly ?
+            new EmbeddedReadOnlyGraphDatabase( dbPath ) :
+            new EmbeddedGraphDatabase( dbPath );
+        
+        final GraphDatabaseShellServer server =
+            new GraphDatabaseShellServer( graph );
+        Runtime.getRuntime().addShutdownHook( new Thread()
+        {
+            @Override
+            public void run()
+            {
+                shutdownIfNecessary( server, graph );
+            }
+        } );
+        
+        System.out.println( "NOTE: Local Neo4j graph database service at '" +
+            dbPath + "'" );
+        ShellClient client = new SameJvmClient( server );
+        setSessionVariablesFromArgs( client, args );
+        client.grabPrompt();
+        shutdownIfNecessary( server, graph );
+    }
+
+    private void shutdownIfNecessary( ShellServer server,
+            GraphDatabaseService graphDb )
+    {
+        try
+        {
+            if ( !hasBeenShutdown.compareAndSet( false, true ) )
+            {
+                server.shutdown();
+                graphDb.shutdown();
+            }
+        }
+        catch ( RemoteException e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+
+    private static void startRemote( Args args )
+    {
+        try
+        {
+            int port = args.getNumber( ARG_PORT, AbstractServer.DEFAULT_PORT ).intValue();
+            String name = args.get( ARG_NAME, AbstractServer.DEFAULT_NAME );
+            ShellClient client = ShellLobby.newClient( port, name );
+            System.out.println( "NOTE: Remote Neo4j graph database service '" + name +
+                    "' at port " + port );
+            setSessionVariablesFromArgs( client, args );
+            client.grabPrompt();
+        }
+        catch ( Exception e )
+        {
+            handleException( e, args );
+        }
+    }
+
+    protected static void setSessionVariablesFromArgs(
+        ShellClient client, Args args ) throws RemoteException
+    {
+        String profile = args.get( "profile", null );
+        if ( profile != null )
+        {
+            applyProfileFile( new File( profile ), client );
+        }
+        
+        for ( Map.Entry<String, String> entry : args.asMap().entrySet() )
+        {
+            String key = entry.getKey();
+            if ( key.startsWith( "D" ) )
+            {
+                key = key.substring( 1 );
+                client.session().set( key, entry.getValue() );
+            }
+        }
+    }
+    
+    private static void applyProfileFile( File file, ShellClient client )
+    {
+        InputStream in = null;
+        try
+        {
+            Properties properties = new Properties();
+            properties.load( new FileInputStream( file ) );
+            for ( Object key : properties.keySet() )
+            {
+                String stringKey = ( String ) key;
+                String value = properties.getProperty( stringKey );
+                client.session().set( stringKey, value );
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new IllegalArgumentException( "Couldn't find profile '" +
+                file.getAbsolutePath() + "'" );
+        }
+        finally
+        {
+            if ( in != null )
+            {
+                try
+                {
+                    in.close();
+                }
+                catch ( IOException e )
+                {
+                    // OK
+                }
+            }
+        }
+    }
+    
+    private static void handleException( Exception e, Args args )
+    {
+        String message = e.getCause() instanceof ConnectException ?
+                "Connection refused" : e.getMessage();
+        System.err.println( "ERROR (-v for expanded information):\n\t" + message );
+        if ( args.has( "v" ) )
+        {
+            e.printStackTrace( System.err );
+        }
+        System.err.println();
+        printUsage();
+        System.exit( 1 );
+    }
+
+    private static void printUsage()
+    {
+        int port = AbstractServer.DEFAULT_PORT;
+        String name = AbstractServer.DEFAULT_NAME;
+        String pathArg = StartClient.ARG_PATH;
+        String portArg = StartClient.ARG_PORT;
+        String nameArg = StartClient.ARG_NAME;
+        System.out.println(
+            "Example arguments for remote:\n" +
+                "\t-" + portArg + " " + port + "\n" +
+                "\t-" + portArg + " " + port +
+                    " -" + nameArg + " " + name + "\n" +
+                "\t...or no arguments\n" +
+            "Example arguments for local:\n" +
+                "\t-" + pathArg + " /path/to/db" + "\n" +
+                "\t-" + pathArg + " /path/to/db -readonly"
+        );
     }
 }
