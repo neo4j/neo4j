ca90f6501ddcf819b6e6779b9847377798124a97
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2010-07-06 08:59:33 +0000

    Refactored the HA branch to facilitate using different implementations of LockManagers and IdGenerators.
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@4674 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ community/main/java/org/neo4j/kernel/ha/zookeeper/ZooKeeperBroker.java
@@ -0,0 +1,15 @@
+package org.neo4j.kernel.ha.zookeeper;
+
+import org.neo4j.kernel.impl.ha.Broker;
+import org.neo4j.kernel.impl.ha.Master;
+
+public class ZooKeeperBroker implements Broker
+{
+
+    public Master getMaster()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
