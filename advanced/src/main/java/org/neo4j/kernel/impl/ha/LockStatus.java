9cfb5dc522eaf59a22ef481abfec5e8e5a25660a
Mattias Persson <mattias@neotechnology.com>
2010-08-09 12:26:57 +0000

    Added a hasMessage to LockStatus
    git-svn-id: https://svn.neo4j.org/components/kernel/branches/ha@5081 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/src/main/java/org/neo4j/kernel/impl/ha/LockStatus.java
+++ advanced/src/main/java/org/neo4j/kernel/impl/ha/LockStatus.java
@@ -4,5 +4,17 @@ public enum LockStatus
 {
     OK_LOCKED,
     NOT_LOCKED,
-    DEAD_LOCKED,
+    DEAD_LOCKED
+    {
+        @Override
+        public boolean hasMessage()
+        {
+            return true;
+        }
+    };
+    
+    public boolean hasMessage()
+    {
+        return false;
+    }
 }
