8ecbb8712724bdc51938f46a960ab255c0ab60f3
Johan Svensson <johan@neotechnology.com>
2010-08-09 07:35:17 +0000

    Shell enabled.
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@5077 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ community/test/java/slavetest/Demo.java
@@ -0,0 +1,26 @@
+package slavetest;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+
+public class Demo extends BasicHaTesting
+{
+    public static void main( String args[] )
+    {
+        Demo demo = new Demo();
+        demo.initializeDbs( 2 );
+        GraphDatabaseService slave1 = demo.getSlave( 0 );
+        GraphDatabaseService slave2 = demo.getSlave( 1 );
+        GraphDatabaseService master = demo.getMaster().getGraphDb();
+
+        master.enableRemoteShell();
+        Map<String,Serializable> shellConfig = new HashMap<String, Serializable>();
+        shellConfig.put( "port", Integer.valueOf( 1338 ) );
+        slave1.enableRemoteShell( shellConfig );
+        shellConfig.put( "port", Integer.valueOf( 1339 ) );
+        slave2.enableRemoteShell( shellConfig );
+    }
+}
