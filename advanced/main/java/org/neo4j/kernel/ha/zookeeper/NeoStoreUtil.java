3cf0cf907426f2184320b0d2a42d86158dc3a585
Johan Svensson <johan@neotechnology.com>
2010-08-11 09:34:02 +0000

    Started Zookeeper integration.
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@5103 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ advanced/main/java/org/neo4j/kernel/ha/zookeeper/NeoStoreUtil.java
@@ -0,0 +1,54 @@
+package org.neo4j.kernel.ha.zookeeper;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+class NeoStoreUtil
+{
+    private static final int RECORD_SIZE = 9;
+
+    private final long creationTime;
+    private final long storeId;
+    private final long txId;
+    
+    
+    NeoStoreUtil( String storeDir )
+    {
+        try
+        {
+            FileChannel fileChannel = new RandomAccessFile( storeDir + "/neostore.db", "r" ).getChannel();
+            ByteBuffer buf = ByteBuffer.allocate( 32 );
+            if ( fileChannel.read( buf ) != 32 )
+            {
+                throw new RuntimeException( "Unable to read neo store header information" );
+            }
+            buf.flip();
+            creationTime = buf.getLong();
+            storeId = buf.getLong();
+            buf.getLong(); // skip log version
+            txId = buf.getLong();
+            fileChannel.close();
+        }
+        catch ( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+    
+    public long getCreationTime()
+    {
+        return creationTime;
+    }
+    
+    public long getStoreId()
+    {
+        return storeId;
+    }
+    
+    public long getLastCommittedTx()
+    {
+        return txId;
+    }
+}
