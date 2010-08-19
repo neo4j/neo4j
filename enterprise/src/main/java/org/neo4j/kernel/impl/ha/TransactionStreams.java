e3790465778a97f32e426859730dcb5d4d377b09
Mattias Persson <mattias@neotechnology.com>
2010-08-19 10:30:23 +0000

    Id allocation ignores state of slave (master sync). It isn't necessary from a logical point of view and it speeds things up
    git-svn-id: https://svn.neo4j.org/components/kernel/branches/ha@5266 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/main/java/org/neo4j/kernel/impl/ha/TransactionStreams.java
+++ enterprise/src/main/java/org/neo4j/kernel/impl/ha/TransactionStreams.java
@@ -4,29 +4,51 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import org.neo4j.helpers.Pair;
 
 public class TransactionStreams
 {
-    private final Collection<Pair<String, TransactionStream>> streams =
-            new ArrayList<Pair<String,TransactionStream>>();
+    public static final TransactionStreams EMPTY = new TransactionStreams()
+    {
+        @Override
+        public void add( String resource, TransactionStream stream )
+        {
+            throw new UnsupportedOperationException( "read-only instance" );
+        }
+    };
+    
+    private List<Pair<String, TransactionStream>> streams;
     
     public void add( String resource, TransactionStream stream )
     {
+        ensureListInstantiated();
         this.streams.add( new Pair<String, TransactionStream>( resource, stream ) );
     }
     
     public Collection<Pair<String, TransactionStream>> getStreams()
     {
-        return Collections.unmodifiableCollection( this.streams );
+        return streams == null ? Collections.<Pair<String, TransactionStream>>emptyList() :
+                Collections.unmodifiableList( streams );
     }
     
+    private void ensureListInstantiated()
+    {
+        if ( streams == null )
+        {
+            streams = new ArrayList<Pair<String,TransactionStream>>();
+        }
+    }
+
     public void close() throws IOException
     {
-        for ( Pair<String, TransactionStream> stream : streams )
+        if ( streams != null )
         {
-            stream.other().close();
+            for ( Pair<String, TransactionStream> stream : streams )
+            {
+                stream.other().close();
+            }
         }
     }
 }
