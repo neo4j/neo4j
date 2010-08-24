c9d68ae08ab7a9737b529e2ef8a61b1dbf1efcac
Mattias Persson <mattias@neotechnology.com>
2010-08-24 08:50:00 +0000

    Added a "hainfo" shell app which just displays i master or slave and (if master) which connected slaves and possibly ongoing transactions.
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@5368 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/main/java/org/neo4j/kernel/Hainfo.java
@@ -0,0 +1,35 @@
+package org.neo4j.kernel;
+
+import java.util.Collection;
+
+import org.neo4j.helpers.Pair;
+import org.neo4j.kernel.ha.MasterServer;
+import org.neo4j.shell.AppCommandParser;
+import org.neo4j.shell.Output;
+import org.neo4j.shell.Session;
+import org.neo4j.shell.kernel.apps.GraphDatabaseApp;
+
+public class Hainfo extends GraphDatabaseApp
+{
+    @Override
+    protected String exec( AppCommandParser parser, Session session, Output out ) throws Exception
+    {
+        HighlyAvailableGraphDatabase db = (HighlyAvailableGraphDatabase) getServer().getDb();
+        MasterServer master = db.getMasterServerIfMaster();
+        out.println( "I'm currently " + (master != null ? "master" : "slave") );
+        
+        if ( master != null )
+        {
+            out.println( "Connected slaves:" );
+            for ( Pair<Integer, Collection<Integer>> entry : master.getConnectedClients() )
+            {
+                out.println( "\tMachine ID: " + entry.first() );
+                if ( !entry.other().isEmpty() )
+                {
+                    out.println( "\tOngoing transactions: " + entry.other() );
+                }
+            }
+        }
+        return null;
+    }
+}
