82b8039bc7ab549b10cb7180c3b73ada7922fe26
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2010-08-25 21:46:20 +0000

    Added hashCode() and equals() to Pair.
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@5422 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/main/java/org/neo4j/helpers/Pair.java
+++ enterprise/src/main/java/org/neo4j/helpers/Pair.java
@@ -29,4 +29,33 @@ public final class Pair<T1, T2>
     {
         return "(" + first + ", " + other + ")";
     }
+
+    @Override
+    public int hashCode()
+    {
+        return ( 31 * hashCode( first ) ) | hashCode( other );
+    }
+
+    @SuppressWarnings( "unchecked" )
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( this == obj ) return true;
+        if ( obj instanceof Pair )
+        {
+            Pair that = (Pair) obj;
+            return equals( this.first, that.first ) && equals( this.other, that.other );
+        }
+        return false;
+    }
+
+    private static int hashCode( Object obj )
+    {
+        return obj == null ? 0 : obj.hashCode();
+    }
+
+    private static boolean equals( Object obj1, Object obj2 )
+    {
+        return ( obj1 == obj2 ) || ( obj1 != null && obj1.equals( obj2 ) );
+    }
 }
