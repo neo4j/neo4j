da787fd06f13ca00fba9d01ce4b9c4dae7ffa420
Mattias Persson <mattias@neotechnology.com>
2010-08-25 12:21:34 +0000

    Renamed a config key
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@5408 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/test/java/slavetest/DemoWithZooBroker.java
+++ enterprise/test/java/slavetest/DemoWithZooBroker.java
@@ -15,7 +15,7 @@ public class DemoWithZooBroker
         Map<String,String> config = MapUtil.stringMap( 
             HighlyAvailableGraphDatabase.CONFIG_KEY_HA_MACHINE_ID, "1",
             HighlyAvailableGraphDatabase.CONFIG_KEY_HA_ZOO_KEEPER_SERVERS, "localhost:2181,localhost:2182,localhost:2183", 
-            HighlyAvailableGraphDatabase.CONFIG_KEY_HA_SERVERS, "1=localhost:5559,2=localhost:5560" );
+            HighlyAvailableGraphDatabase.CONFIG_KEY_HA_SERVER, "1=localhost:5559,2=localhost:5560" );
         GraphDatabaseService graphDb = new HighlyAvailableGraphDatabase( "target/ha1", config );
         graphDb.shutdown();
     }
