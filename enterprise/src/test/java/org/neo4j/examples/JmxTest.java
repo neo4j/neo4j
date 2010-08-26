8f7e038ee55582f694848fd6f4cd00b782a7d3ad
Anders Nawroth <anders@neotechnology.com>
2010-08-26 12:20:11 +0000

    Added a simple JMX test.
    git-svn-id: https://svn.neo4j.org/examples/apoc-examples/trunk@5450 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/src/test/java/org/neo4j/examples/JmxTest.java
@@ -0,0 +1,33 @@
+package org.neo4j.examples;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+import org.neo4j.kernel.management.Kernel;
+
+public class JmxTest
+{
+    @Test
+    public void readJmxProperties()
+    {
+        GraphDatabaseService graphDbService = new EmbeddedGraphDatabase(
+                "target/jmx-db" );
+        Date startTime = getStartTimeFromManagementBean( graphDbService );
+        Date now = new Date();
+        System.out.println( startTime + " " + now );
+        assertTrue( startTime.before( now ) || startTime.equals( now ) );
+    }
+
+    private static Date getStartTimeFromManagementBean(
+            GraphDatabaseService graphDbService )
+    {
+        EmbeddedGraphDatabase graphDb = (EmbeddedGraphDatabase) graphDbService;
+        Kernel kernel = graphDb.getManagementBean( Kernel.class );
+        Date startTime = kernel.getKernelStartTime();
+        return startTime;
+    }
+}
