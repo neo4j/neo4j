caa7bd1892260d2dcd23e2b2d667d0006e62dca9
Mattias Persson <mattias@neotechnology.com>
2010-08-27 08:19:21 +0000

    Added a stacktrace
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@5462 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/main/java/org/neo4j/kernel/Noobinit.java
+++ community/main/java/org/neo4j/kernel/Noobinit.java
@@ -15,25 +15,33 @@ public class Noobinit extends GraphDatabaseApp
     @Override
     protected String exec( AppCommandParser parser, Session session, Output out ) throws Exception
     {
-        RelationshipType type = DynamicRelationshipType.withName( "REL_TYPE" );
-        out.println( "getOrCreateSingleOtherNode" );
-        Node fromNode = getServer().getDb().getReferenceNode();
-        fromNode.removeProperty( "___locking_property___" );
-        out.println( "Grabbed lock" );
-        Relationship singleRelationship =
-                fromNode.getSingleRelationship( type, Direction.OUTGOING );
-        out.println( "Single relationship is " + singleRelationship );
-        Node otherNode = null;
-        if ( singleRelationship != null )
+        try
         {
-            otherNode = singleRelationship.getOtherNode( fromNode );
+            RelationshipType type = DynamicRelationshipType.withName( "REL_TYPE" );
+            out.println( "getOrCreateSingleOtherNode" );
+            Node fromNode = getServer().getDb().getReferenceNode();
+            fromNode.removeProperty( "___locking_property___" );
+            out.println( "Grabbed lock" );
+            Relationship singleRelationship =
+                    fromNode.getSingleRelationship( type, Direction.OUTGOING );
+            out.println( "Single relationship is " + singleRelationship );
+            Node otherNode = null;
+            if ( singleRelationship != null )
+            {
+                otherNode = singleRelationship.getOtherNode( fromNode );
+            }
+            else
+            {
+                otherNode = fromNode.getGraphDatabase().createNode();
+                fromNode.createRelationshipTo( otherNode, type );
+                out.println( "Created new relationship" );
+            }
+            return null;
         }
-        else
+        catch ( Exception e )
         {
-            otherNode = fromNode.getGraphDatabase().createNode();
-            fromNode.createRelationshipTo( otherNode, type );
-            out.println( "Created new relationship" );
+            e.printStackTrace();
+            throw e;
         }
-        return null;
     }
 }
