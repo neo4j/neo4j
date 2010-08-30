58b8c053b884b63db8b859169ddbba8a523803fb
Mattias Persson <mattias@neotechnology.com>
2010-08-30 13:58:39 +0000

    Merged r5490 from HA branch
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@5491 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ advanced/src/main/java/org/neo4j/kernel/AbstractGraphDatabase.java
@@ -0,0 +1,13 @@
+package org.neo4j.kernel;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+
+/**
+ * Exposes the methods getConfig() and getManagementBean() a.s.o.
+ */
+public abstract class AbstractGraphDatabase implements GraphDatabaseService
+{
+    public abstract Config getConfig();
+    
+    public abstract <T> T getManagementBean( Class<T> type );
+}
