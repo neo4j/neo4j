74230dde2641c4b0d2abcbd3a39e2281b247fadb
Mattias Persson <mattias@neotechnology.com>
2010-08-31 13:47:59 +0000

    Added eventIdentifier to SlaveContext
    git-svn-id: https://svn.neo4j.org/components/kernel/branches/ha@5509 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/main/java/org/neo4j/kernel/impl/ha/SlaveContext.java
+++ enterprise/src/main/java/org/neo4j/kernel/impl/ha/SlaveContext.java
@@ -1,16 +1,23 @@
 package org.neo4j.kernel.impl.ha;
 
+import java.util.Arrays;
+
 import org.neo4j.helpers.Pair;
 
 public final class SlaveContext
 {
     private final int machineId;
     private final Pair<String, Long>[] lastAppliedTransactions;
+    private final int eventIdentifier;
+    private final int hashCode;
 
-    public SlaveContext( int machineId, Pair<String, Long>[] lastAppliedTransactions )
+    public SlaveContext( int machineId, int eventIdentifier,
+            Pair<String, Long>[] lastAppliedTransactions )
     {
         this.machineId = machineId;
+        this.eventIdentifier = eventIdentifier;
         this.lastAppliedTransactions = lastAppliedTransactions;
+        this.hashCode = 3*eventIdentifier*machineId;
     }
 
     public int machineId()
@@ -21,5 +28,34 @@ public final class SlaveContext
     public Pair<String, Long>[] lastAppliedTransactions()
     {
         return lastAppliedTransactions;
+    }
+    
+    public int getEventIdentifier()
+    {
+        return eventIdentifier;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "SlaveContext[ID:" + machineId + ", eventIdentifier:" + eventIdentifier + ", " +
+                Arrays.asList( lastAppliedTransactions ) + "]";
+    }
+    
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( !( obj instanceof SlaveContext ) )
+        {
+            return false;
+        }
+        SlaveContext o = (SlaveContext) obj;
+        return o.eventIdentifier == eventIdentifier && o.machineId == machineId;
+    }
+    
+    @Override
+    public int hashCode()
+    {
+        return this.hashCode;
     }
 }
