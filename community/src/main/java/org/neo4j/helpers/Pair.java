9129686aa03e45a64424129466c873ba73718c18
Mattias Persson <mattias@neotechnology.com>
2010-09-01 08:56:21 +0000

    Added some more useful methods in MapUtil and some basic javadocs
    git-svn-id: https://svn.neo4j.org/components/kernel/branches/ha@5515 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/main/java/org/neo4j/helpers/Pair.java
+++ community/src/main/java/org/neo4j/helpers/Pair.java
@@ -8,17 +8,29 @@ public final class Pair<T1, T2>
     private final T1 first;
     private final T2 other;
 
+    /**
+     * Create a new pair of objects.
+     * 
+     * @param first the first object in the pair.
+     * @param other the other object in the pair.
+     */
     public Pair( T1 first, T2 other )
     {
         this.first = first;
         this.other = other;
     }
 
+    /**
+     * @return the first object in the pair.
+     */
     public T1 first()
     {
         return first;
     }
 
+    /**
+     * @return the other object in the pair.
+     */
     public T2 other()
     {
         return other;
--- community/src/main/java/org/neo4j/helpers/collection/MapUtil.java
+++ community/src/main/java/org/neo4j/helpers/collection/MapUtil.java
@@ -1,7 +1,13 @@
 package org.neo4j.helpers.collection;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * Utility to create {@link Map}s.
@@ -16,16 +22,29 @@ public abstract class MapUtil
      * @param <V> type of values
      * @return a Map with the entries supplied by {@code objects}.
      */
-    @SuppressWarnings("unchecked")
     public static <K, V> Map<K, V> genericMap( Object... objects )
     {
-        Map<K, V> map = new HashMap<K, V>();
+        return genericMap( new HashMap<K, V>(), objects );
+    }
+    
+    /**
+     * A short-hand method for adding key/value pairs into a {@link Map}.
+     *
+     * @param targetMap the {@link Map} to put the objects into.
+     * @param objects alternating key and value.
+     * @param <K> type of keys
+     * @param <V> type of values
+     * @return a Map with the entries supplied by {@code objects}.
+     */
+    @SuppressWarnings("unchecked")
+    public static <K, V> Map<K, V> genericMap( Map<K, V> targetMap, Object... objects )
+    {
         int i = 0;
         while ( i < objects.length )
         {
-            map.put( (K) objects[i++], (V) objects[i++] );
+            targetMap.put( (K) objects[i++], (V) objects[i++] );
         }
-        return map;
+        return targetMap;
     }
 
     /**
@@ -42,6 +61,20 @@ public abstract class MapUtil
 
     /**
      * A short-hand method for creating a {@link Map} of key/value pairs where
+     * both keys and values are {@link String}s.
+     * 
+     * @param targetMap the {@link Map} to put the objects into.
+     * @param strings alternating key and value.
+     * @return a Map with the entries supplied by {@code strings}.
+     */
+    public static Map<String, String> stringMap( Map<String, String> targetMap,
+            String... strings )
+    {
+        return genericMap( targetMap, (Object[]) strings );
+    }
+    
+    /**
+     * A short-hand method for creating a {@link Map} of key/value pairs where
      * keys are {@link String}s and values are {@link Object}s.
      * 
      * @param objects alternating key and value.
@@ -50,5 +83,140 @@ public abstract class MapUtil
     public static Map<String, Object> map( Object... objects )
     {
         return genericMap( objects );
+    }
+
+    /**
+     * A short-hand method for creating a {@link Map} of key/value pairs where
+     * keys are {@link String}s and values are {@link Object}s.
+     * 
+     * @param targetMap the {@link Map} to put the objects into.
+     * @param objects alternating key and value.
+     * @return a Map with the entries supplied by {@code objects}.
+     */
+    public static Map<String, Object> map( Map<String, Object> targetMap,
+            Object... objects )
+    {
+        return genericMap( targetMap, objects );
+    }
+    
+    /**
+     * Loads a {@link Map} from a {@link Reader} assuming strings as keys
+     * and values.
+     * 
+     * @param reader the {@link Reader} containing a {@link Properties}-like
+     * layout of keys and values.
+     * @return the read data as a {@link Map}.
+     * @throws IOException if the {@code reader} throws {@link IOException}.
+     */
+    public static Map<String, String> load( Reader reader ) throws IOException
+    {
+        Properties props = new Properties();
+        props.load( reader );
+        return new HashMap<String, String>( (Map) props );
+    }
+    
+    /**
+     * Loads a {@link Map} from a {@link Reader} assuming strings as keys
+     * and values. Any {@link IOException} is wrapped and thrown as a
+     * {@link RuntimeException} instead.
+     * 
+     * @param reader the {@link Reader} containing a {@link Properties}-like
+     * layout of keys and values.
+     * @return the read data as a {@link Map}.
+     */
+    public static Map<String, String> loadStrictly( Reader reader )
+    {
+        try
+        {
+            return load( reader );
+        }
+        catch ( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+    
+    /**
+     * Loads a {@link Map} from an {@link InputStream} assuming strings as keys
+     * and values.
+     * 
+     * @param stream the {@link InputStream} containing a
+     * {@link Properties}-like layout of keys and values.
+     * @return the read data as a {@link Map}.
+     * @throws IOException if the {@code stream} throws {@link IOException}.
+     */
+    public static Map<String, String> load( InputStream stream ) throws IOException
+    {
+        Properties props = new Properties();
+        props.load( stream );
+        return new HashMap<String, String>( (Map) props );
+    }
+    
+    /**
+     * Loads a {@link Map} from an {@link InputStream} assuming strings as keys
+     * and values. Any {@link IOException} is wrapped and thrown as a
+     * {@link RuntimeException} instead.
+     * 
+     * @param stream the {@link InputStream} containing a
+     * {@link Properties}-like layout of keys and values.
+     * @return the read data as a {@link Map}.
+     */
+    public static Map<String, String> loadStrictly( InputStream stream )
+    {
+        try
+        {
+            return load( stream );
+        }
+        catch ( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+    
+    /**
+     * Loads a {@link Map} from a {@link File} assuming strings as keys
+     * and values.
+     * 
+     * @param file the {@link File} containing a {@link Properties}-like
+     * layout of keys and values.
+     * @return the read data as a {@link Map}.
+     * @throws IOException if the file reader throws {@link IOException}.
+     */
+    public static Map<String, String> load( File file ) throws IOException
+    {
+        FileInputStream stream = null;
+        try
+        {
+            stream = new FileInputStream( file );
+            return load( stream );
+        }
+        finally
+        {
+            if ( stream != null )
+            {
+                stream.close();
+            }
+        }
+    }
+    
+    /**
+     * Loads a {@link Map} from a {@link File} assuming strings as keys
+     * and values. Any {@link IOException} is wrapped and thrown as a
+     * {@link RuntimeException} instead.
+     * 
+     * @param file the {@link File} containing a {@link Properties}-like
+     * layout of keys and values.
+     * @return the read data as a {@link Map}.
+     */
+    public static Map<String, String> loadStrictly( File file )
+    {
+        try
+        {
+            return load( file );
+        }
+        catch ( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
     }
 }
