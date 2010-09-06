f64075b6bfa96edd486d77a0af1cb04d2cde9a88
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2010-09-06 11:51:26 +0000

    Added hashCode and equals to Pair and Triplet, plus convenient static factory methods.
    git-svn-id: https://svn.neo4j.org/components/kernel/branches/ha@5574 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/main/java/org/neo4j/helpers/Pair.java
+++ enterprise/src/main/java/org/neo4j/helpers/Pair.java
@@ -1,5 +1,6 @@
 package org.neo4j.helpers;
 
+
 /**
  * Utility to handle pairs of objects.
  */
@@ -10,7 +11,7 @@ public class Pair<T1, T2>
 
     /**
      * Create a new pair of objects.
-     * 
+     *
      * @param first the first object in the pair.
      * @param other the other object in the pair.
      */
@@ -18,6 +19,11 @@ public class Pair<T1, T2>
     {
         this.first = first;
         this.other = other;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of( T1 first, T2 other )
+    {
+        return new Pair<T1, T2>( first, other );
     }
 
     /**
@@ -35,10 +41,44 @@ public class Pair<T1, T2>
     {
         return other;
     }
-    
+
     @Override
     public String toString()
     {
         return "(" + first + ", " + other + ")";
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return ( 31 * hashCode( first ) ) | hashCode( other );
+    }
+
+    @SuppressWarnings( "unchecked" )
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( this == obj ) return true;
+        if ( obj instanceof Pair )
+        {
+            if ( obj.getClass() != this.getClass() ) return false;
+            return pairEquals( (Pair) obj );
+        }
+        return false;
+    }
+
+    boolean pairEquals( Pair that )
+    {
+        return equals( this.first, that.first ) && equals( this.other, that.other );
+    }
+
+    static int hashCode( Object obj )
+    {
+        return obj == null ? 0 : obj.hashCode();
+    }
+
+    static boolean equals( Object obj1, Object obj2 )
+    {
+        return ( obj1 == obj2 ) || ( obj1 != null && obj1.equals( obj2 ) );
     }
 }
