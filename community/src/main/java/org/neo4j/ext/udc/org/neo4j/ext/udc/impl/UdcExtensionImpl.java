2c8b6ef00839bf51ba393655a2e16a7d9025d097
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-09-06 15:20:14 +0000

    initial checkin. project structure and OSGi-friendly
    git-svn-id: https://svn.neo4j.org/laboratory/components/udc@5580 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ community/src/main/java/org/neo4j/ext/udc/org/neo4j/ext/udc/impl/UdcExtensionImpl.java
@@ -0,0 +1,20 @@
+package org.neo4j.ext.udc.org.neo4j.ext.udc.impl;
+
+import org.neo4j.helpers.Service;
+import org.neo4j.kernel.KernelExtension;
+
+/**
+ * Kernel extension for UDC.
+ */
+@Service.Implementation( KernelExtension.class )
+public class UdcExtensionImpl extends KernelExtension {
+
+  public UdcExtensionImpl() {
+    super("kernel udc");
+  }
+
+  @Override
+  protected void load(KernelData kernel) {
+    System.out.println("UDC extension loaded!");
+  }
+}
