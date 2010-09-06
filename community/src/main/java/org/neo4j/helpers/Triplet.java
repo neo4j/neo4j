d9196cebf3d7355d9ea9568ea58edcd5a215ccfc
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2010-09-06 11:51:26 +0000

    Added hashCode and equals to Pair and Triplet, plus convenient static factory methods.
    git-svn-id: https://svn.neo4j.org/components/kernel/branches/ha@5574 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/main/java/org/neo4j/helpers/Triplet.java
+++ community/src/main/java/org/neo4j/helpers/Triplet.java
@@ -9,15 +9,40 @@ public class Triplet<T1, T2, T3> extends Pair<T1, T2>
         super( first, other );
         this.third = third;
     }
-    
+
+    public static <T1, T2, T3> Triplet<T1, T2, T3> of( T1 first, T2 other, T3 third )
+    {
+        return new Triplet<T1, T2, T3>( first, other, third );
+    }
+
     public T3 third()
     {
         return this.third;
     }
-    
+
     @Override
     public String toString()
     {
         return "(" + first() + ", " + other() + ", " + third + ")";
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return ( 31 * super.hashCode() ) | hashCode( third );
+    }
+
+    @SuppressWarnings( "unchecked" )
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( this == obj ) return true;
+        if ( obj instanceof Triplet )
+        {
+            if ( obj.getClass() != this.getClass() ) return false;
+            Triplet that = (Triplet) obj;
+            return this.pairEquals( that ) && equals( this.third, that.third );
+        }
+        return false;
     }
 }
