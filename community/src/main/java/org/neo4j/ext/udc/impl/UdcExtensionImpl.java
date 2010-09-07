025580e589f8bd4663b976a0f9c2b576958402a7
Andrés Taylor <andres.taylor@neotechnology.com>
2010-09-07 15:11:37 +0000

    Added timer
    git-svn-id: https://svn.neo4j.org/laboratory/components/udc@5584 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/main/java/org/neo4j/ext/udc/impl/UdcExtensionImpl.java
+++ community/src/main/java/org/neo4j/ext/udc/impl/UdcExtensionImpl.java
@@ -2,19 +2,28 @@ package org.neo4j.ext.udc.impl;
 
 import org.neo4j.helpers.Service;
 import org.neo4j.kernel.KernelExtension;
+import org.neo4j.kernel.impl.nioneo.xa.NeoStoreXaDataSource;
+
+import java.util.Timer;
 
 /**
  * Kernel extension for UDC.
  */
 @Service.Implementation( KernelExtension.class )
 public class UdcExtensionImpl extends KernelExtension {
+    private static final int FIRST_DELAY = 10;   //minutes
+    private static final int INTERVAL = 10;      //minutes
 
-  public UdcExtensionImpl() {
+    public UdcExtensionImpl() {
     super("kernel udc");
   }
 
   @Override
   protected void load(KernelData kernel) {
-    System.err.println("UDC extension loaded!");
+      Timer timer = new Timer();
+      NeoStoreXaDataSource ds = (NeoStoreXaDataSource) kernel.getConfig().getTxModule().getXaDataSourceManager().getXaDataSource( "nioneodb" );
+      String storeId = Long.toHexString( ds.getRandomIdentifier() );
+      UdcTimerTask task = new UdcTimerTask("127.0.0.1", kernel.version(), storeId);
+      timer.scheduleAtFixedRate(task, FIRST_DELAY * 1000 * 60, INTERVAL * 1000 * 60);
   }
 }
