7be0755524a1aec8ad8c1300d93efb06e2cba89b
Andrés Taylor <andres.taylor@neotechnology.com>
2010-09-07 15:11:37 +0000

    Added timer
    git-svn-id: https://svn.neo4j.org/laboratory/components/udc@5584 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/src/main/java/org/neo4j/ext/udc/impl/UdcTimerTask.java
@@ -0,0 +1,30 @@
+package org.neo4j.ext.udc.impl;
+
+import org.neo4j.ext.udc.org.neo4j.ext.udc.impl.Pinger;
+
+import java.io.IOException;
+import java.util.TimerTask;
+
+public class UdcTimerTask extends TimerTask {
+    private String host;
+    private String version;
+    private String storeId;
+    private Pinger pinger;
+
+    public UdcTimerTask(String host, String version, String storeId)
+    {
+
+        this.host = host;
+        this.version = version;
+        this.storeId = storeId;
+        pinger = new Pinger();
+    }
+
+    @Override
+    public void run()
+    {
+        try {
+            pinger.ping(host, storeId, version);
+        } catch (IOException e) {}
+    }
+}
