0b3a780ec5515aa6657b13644ff11d6f18a00d01
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-09-08 12:26:28 +0000

    added OSGi activator to inject OSGiExtensionLoader, which will find kernel extensions that have been published as OSGi services
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@5593 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/src/main/java/org/neo4j/kernel/impl/osgi/OSGiActivator.java
@@ -0,0 +1,39 @@
+package org.neo4j.kernel.impl.osgi;
+
+import java.util.*;
+
+import org.neo4j.helpers.Service;
+import org.osgi.framework.*;
+
+
+/**
+ * OSGi bundle activator to start an OSGi servicewatcher
+ * for kernel extensions.
+ * 
+ */
+public final class OSGiActivator
+    implements BundleActivator {
+
+  /**
+   * Called whenever the OSGi framework starts our bundle
+   */
+  public void start(BundleContext bc)
+      throws Exception {
+    System.out.println("STARTING neo4j-kernel");
+
+    // start the extension listener
+    Service.osgiExtensionLoader = new OSGiExtensionLoader(bc);
+ }
+
+  /**
+   * Called whenever the OSGi framework stops our bundle
+   */
+  public void stop(BundleContext bc)
+      throws Exception {
+    System.out.println("STOPPING neo4j-kernel");
+
+    // no need to unregister our service - the OSGi framework handles it for us
+  }
+
+}
+
