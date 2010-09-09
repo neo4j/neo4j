e737810fdd2eb8e5fee5d16cee6d4c215d3b407c
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2010-09-09 12:37:48 +0000

    Made JMX work with HA. Added scripts for starting a HA development cluster on a single machine.
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@5602 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ advanced/test/java/org/neo4j/ha/StartLocalHaDb.java
@@ -0,0 +1,18 @@
+package org.neo4j.ha;
+
+import java.util.Map;
+
+import org.junit.Ignore;
+import org.neo4j.kernel.HighlyAvailableGraphDatabase;
+
+@Ignore
+public class StartLocalHaDb
+{
+    public static void main( String[] args )
+    {
+        String path = args[0];
+        String configFile = args[1];
+        Map<String, String> config = HighlyAvailableGraphDatabase.loadConfigurations( configFile );
+        new HighlyAvailableGraphDatabase( path, config );
+    }
+}
