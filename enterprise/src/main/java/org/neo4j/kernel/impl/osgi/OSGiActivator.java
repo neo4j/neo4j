d7f1d6cf3e4f319f77abe1ae598656c8c48d3794
Anders Nawroth <anders@neotechnology.com>
2010-09-10 14:13:43 +0000

    Imports cleanup, code formatting.
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@5621 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/main/java/org/neo4j/kernel/impl/osgi/OSGiActivator.java
+++ enterprise/src/main/java/org/neo4j/kernel/impl/osgi/OSGiActivator.java
@@ -1,39 +1,36 @@
 package org.neo4j.kernel.impl.osgi;
 
-import java.util.*;
-
 import org.neo4j.helpers.Service;
-import org.osgi.framework.*;
-
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
 
 /**
- * OSGi bundle activator to start an OSGi servicewatcher
- * for kernel extensions.
+ * OSGi bundle activator to start an OSGi servicewatcher for kernel extensions.
  * 
  */
-public final class OSGiActivator
-    implements BundleActivator {
+public final class OSGiActivator implements BundleActivator
+{
 
-  /**
-   * Called whenever the OSGi framework starts our bundle
-   */
-  public void start(BundleContext bc)
-      throws Exception {
-    System.out.println("STARTING neo4j-kernel");
+    /**
+     * Called whenever the OSGi framework starts our bundle
+     */
+    public void start( BundleContext bc ) throws Exception
+    {
+        System.out.println( "STARTING neo4j-kernel" );
 
-    // start the extension listener
-    Service.osgiExtensionLoader = new OSGiExtensionLoader(bc);
- }
+        // start the extension listener
+        Service.osgiExtensionLoader = new OSGiExtensionLoader( bc );
+    }
 
-  /**
-   * Called whenever the OSGi framework stops our bundle
-   */
-  public void stop(BundleContext bc)
-      throws Exception {
-    System.out.println("STOPPING neo4j-kernel");
+    /**
+     * Called whenever the OSGi framework stops our bundle
+     */
+    public void stop( BundleContext bc ) throws Exception
+    {
+        System.out.println( "STOPPING neo4j-kernel" );
 
-    // no need to unregister our service - the OSGi framework handles it for us
-  }
+        // no need to unregister our service - the OSGi framework handles it for
+        // us
+    }
 
 }
-
