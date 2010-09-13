c322e0416d919ce17943293e5771466d25abfbfc
Anders Nawroth <anders@neotechnology.com>
2010-09-13 15:48:42 +0000

    Moved out relationship types to enum. Code formatting and cleanup.
    git-svn-id: https://svn.neo4j.org/examples/apoc-examples/trunk@5627 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/src/main/java/org/neo4j/examples/socnet/FriendsStatusUpdateIterator.java
+++ advanced/src/main/java/org/neo4j/examples/socnet/FriendsStatusUpdateIterator.java
@@ -1,63 +1,79 @@
 package org.neo4j.examples.socnet;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
-* Created by IntelliJ IDEA.
-* User: ata
-* Date: Sep 13, 2010
-* Time: 10:21:52 AM
-* To change this template use File | Settings | File Templates.
-*/
-class FriendsStatusUpdateIterator implements Iterator<StatusUpdate> {
+ * Created by IntelliJ IDEA. User: ata Date: Sep 13, 2010 Time: 10:21:52 AM To
+ * change this template use File | Settings | File Templates.
+ */
+class FriendsStatusUpdateIterator implements Iterator<StatusUpdate>
+{
     private ArrayList<SingleStatusUpdateIterator> statuses = new ArrayList<SingleStatusUpdateIterator>();
     private StatusUpdateComparator comparator = new StatusUpdateComparator();
 
-    public FriendsStatusUpdateIterator(Person person) {
-        for (Person friend : person.getFriends()) {
+    public FriendsStatusUpdateIterator( Person person )
+    {
+        for ( Person friend : person.getFriends() )
+        {
             Iterator<StatusUpdate> iterator = friend.getStatus().iterator();
-            if (iterator.hasNext()) {
-                statuses.add(new SingleStatusUpdateIterator(iterator));
+            if ( iterator.hasNext() )
+            {
+                statuses.add( new SingleStatusUpdateIterator( iterator ) );
             }
         }
 
         sort();
     }
 
-    public boolean hasNext() {
+    public boolean hasNext()
+    {
         return statuses.size() > 0;
     }
 
-    public StatusUpdate next() {
-        if(statuses.size() == 0)
+    public StatusUpdate next()
+    {
+        if ( statuses.size() == 0 )
+        {
             throw new NoSuchElementException();
+        }
 
-        SingleStatusUpdateIterator first = statuses.get(0);
+        SingleStatusUpdateIterator first = statuses.get( 0 );
         StatusUpdate returnVal = first.current();
 
-        if(!first.hasNext()) {
-            statuses.remove(0);
-        } else {
+        if ( !first.hasNext() )
+        {
+            statuses.remove( 0 );
+        }
+        else
+        {
             first.next();
             sort();
         }
 
-
         return returnVal;
     }
 
-    private void sort() {
-        Collections.sort(statuses, comparator);
+    private void sort()
+    {
+        Collections.sort( statuses, comparator );
     }
 
-    public void remove() {
-        throw new UnsupportedOperationException("Don't know how to do that...");
+    public void remove()
+    {
+        throw new UnsupportedOperationException( "Don't know how to do that..." );
     }
 
-
-    private class StatusUpdateComparator implements Comparator<SingleStatusUpdateIterator> {
-        public int compare(SingleStatusUpdateIterator a, SingleStatusUpdateIterator b) {
-            return a.current().getDate().compareTo(b.current().getDate());
+    private class StatusUpdateComparator implements
+    Comparator<SingleStatusUpdateIterator>
+    {
+        public int compare( SingleStatusUpdateIterator a,
+                SingleStatusUpdateIterator b )
+        {
+            return a.current().getDate().compareTo( b.current().getDate() );
         }
     }
 }
--- advanced/src/test/java/org/neo4j/examples/socnet/SocnetTest.java
+++ advanced/src/test/java/org/neo4j/examples/socnet/SocnetTest.java
@@ -1,5 +1,13 @@
 package org.neo4j.examples.socnet;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.collection.IsCollectionContaining.hasItems;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+
 import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
@@ -11,186 +19,217 @@ import org.neo4j.index.IndexService;
 import org.neo4j.index.lucene.LuceneIndexService;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Random;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.collection.IsCollectionContaining.hasItems;
-
-
-public class SocnetTest {
-    private static final Random r = new Random(System.currentTimeMillis());
+public class SocnetTest
+{
+    private static final Random r = new Random( System.currentTimeMillis() );
     private GraphDatabaseService graphDb;
     private IndexService index;
     private PersonRepository personRepository;
     private int nrOfPersons;
 
     @Before
-    public void setup() {
-        graphDb = new EmbeddedGraphDatabase("target/socnetdb");
-        index = new LuceneIndexService(graphDb);
-        personRepository = new PersonRepository(graphDb, index);
+    public void setup()
+    {
+        graphDb = new EmbeddedGraphDatabase( "target/socnetdb" );
+        index = new LuceneIndexService( graphDb );
+        personRepository = new PersonRepository( graphDb, index );
 
         nrOfPersons = 20;
         createPersons();
-        setupFriendsBetweenPeople(10);
+        setupFriendsBetweenPeople( 10 );
     }
 
     @After
-    public void teardown() {
-        try {
-            deleteSocialGraph(graphDb, personRepository);
+    public void teardown()
+    {
+        try
+        {
+            deleteSocialGraph( graphDb, personRepository );
         }
-        finally {
+        finally
+        {
             index.shutdown();
             graphDb.shutdown();
         }
     }
 
     @Test
-    public void addStatusAndRetrieveIt() throws Exception {
+    public void addStatusAndRetrieveIt() throws Exception
+    {
         Person person = getRandomPerson();
-        personRepository.addStatusToPerson(person, "Testing!");
+        personRepository.addStatusToPerson( person, "Testing!" );
 
         StatusUpdate update = person.getStatus().iterator().next();
 
-        assertThat(update, CoreMatchers.<Object>notNullValue());
-        assertThat(update.getStatusText(), equalTo("Testing!"));
-        assertThat(update.getPerson(), equalTo(person));
+        assertThat( update, CoreMatchers.<Object>notNullValue() );
+        assertThat( update.getStatusText(), equalTo( "Testing!" ) );
+        assertThat( update.getPerson(), equalTo( person ) );
     }
 
     @Test
-    public void multipleStatusesComeOutInTheRightOrder() throws Exception {
+    public void multipleStatusesComeOutInTheRightOrder() throws Exception
+    {
         ArrayList<String> statuses = new ArrayList<String>();
-        statuses.add("Test1");
-        statuses.add("Test2");
-        statuses.add("Test3");
+        statuses.add( "Test1" );
+        statuses.add( "Test2" );
+        statuses.add( "Test3" );
 
         Person person = getRandomPerson();
-        for(String status : statuses) {
-            personRepository.addStatusToPerson(person, status);
+        for ( String status : statuses )
+        {
+            personRepository.addStatusToPerson( person, status );
         }
 
         int i = statuses.size();
-        for(StatusUpdate update : person.getStatus()){
+        for ( StatusUpdate update : person.getStatus() )
+        {
             i--;
-            assertThat(update.getStatusText(), equalTo(statuses.get(i)));
+            assertThat( update.getStatusText(), equalTo( statuses.get( i ) ) );
         }
     }
 
     @Test
     public void removingOneFriendIsHandledCleanly()
     {
-        Person person1 = personRepository.getPersonByName("person#1");
-        Person person2 = personRepository.getPersonByName("person#2");
-        person1.addFriend(person2);
+        Person person1 = personRepository.getPersonByName( "person#1" );
+        Person person2 = personRepository.getPersonByName( "person#2" );
+        person1.addFriend( person2 );
 
         int noOfFriends = person1.getNrOfFriends();
 
-        person1.removeFriend(person2);
+        person1.removeFriend( person2 );
 
         int noOfFriendsAfterChange = person1.getNrOfFriends();
 
-        assertThat(noOfFriends, equalTo(noOfFriendsAfterChange+1));
+        assertThat( noOfFriends, equalTo( noOfFriendsAfterChange + 1 ) );
     }
 
     @Test
-    public void retrieveStatusUpdatesInDateOrder() throws Exception {
+    public void retrieveStatusUpdatesInDateOrder() throws Exception
+    {
         Person person = getRandomPersonWithFriends();
         int numberOfStatuses = 20;
 
-        for(int i = 0; i<numberOfStatuses; i++) {
-            Person friend = getRandomFriendOf(person);
-            personRepository.addStatusToPerson(friend, "Dum-deli-dum...");
+        for ( int i = 0; i < numberOfStatuses; i++ )
+        {
+            Person friend = getRandomFriendOf( person );
+            personRepository.addStatusToPerson( friend, "Dum-deli-dum..." );
         }
 
         ArrayList<StatusUpdate> updates = new ArrayList<StatusUpdate>();
-        IteratorUtil.addToCollection(person.friendStatuses(), updates);
-        assertThat(updates.size(), equalTo(numberOfStatuses));
-        assertUpdatesAreSortedByDate(updates);
+        IteratorUtil.addToCollection( person.friendStatuses(), updates );
+        assertThat( updates.size(), equalTo( numberOfStatuses ) );
+        assertUpdatesAreSortedByDate( updates );
     }
 
     @Test
-    public void friendsOfFriendsWorks() throws Exception {
+    public void friendsOfFriendsWorks() throws Exception
+    {
         Person person = getRandomPerson();
-        Person friend = getRandomFriendOf(person);
+        Person friend = getRandomFriendOf( person );
 
-        for(Person friendOfFriend : friend.getFriends()) {
-            if(!friendOfFriend.equals(person)) { // You can't be friends with yourself.
-                assertThat(person.getFriendsOfFriends(), hasItems(friendOfFriend));
+        for ( Person friendOfFriend : friend.getFriends() )
+        {
+            if ( !friendOfFriend.equals( person ) )
+            { // You can't be friends with yourself.
+                assertThat( person.getFriendsOfFriends(),
+                        hasItems( friendOfFriend ) );
             }
         }
     }
 
-    private void setupFriendsBetweenPeople(int maxNrOfFriendsEach) {
+    private void setupFriendsBetweenPeople( int maxNrOfFriendsEach )
+    {
         Transaction tx = graphDb.beginTx();
 
-        try {
-            for (Person person : personRepository.getAllPersons()) {
-                int nrOfFriends = r.nextInt(maxNrOfFriendsEach) + 1;
-                for (int j = 0; j < nrOfFriends; j++) {
-                    person.addFriend(getRandomPerson());
+        try
+        {
+            for ( Person person : personRepository.getAllPersons() )
+            {
+                int nrOfFriends = r.nextInt( maxNrOfFriendsEach ) + 1;
+                for ( int j = 0; j < nrOfFriends; j++ )
+                {
+                    person.addFriend( getRandomPerson() );
                 }
             }
             tx.success();
         }
-        finally {
+        finally
+        {
             tx.finish();
         }
     }
 
-    private Person getRandomPerson() {
-        return personRepository.getPersonByName("person#" +
-                r.nextInt(nrOfPersons));
+    private Person getRandomPerson()
+    {
+        return personRepository.getPersonByName( "person#"
+                                                 + r.nextInt( nrOfPersons ) );
     }
 
-    private static void deleteSocialGraph(GraphDatabaseService graphDb,
-                                          PersonRepository personRepository) {
+    private static void deleteSocialGraph( GraphDatabaseService graphDb,
+            PersonRepository personRepository )
+    {
         Transaction tx = graphDb.beginTx();
-        try {
-            for (Person person : personRepository.getAllPersons()) {
-                personRepository.deletePerson(person);
+        try
+        {
+            for ( Person person : personRepository.getAllPersons() )
+            {
+                personRepository.deletePerson( person );
             }
             tx.success();
         }
-        finally {
+        finally
+        {
             tx.finish();
         }
     }
 
-    private Person getRandomFriendOf(Person p) {
+    private Person getRandomFriendOf( Person p )
+    {
         ArrayList<Person> friends = new ArrayList<Person>();
-        IteratorUtil.addToCollection(p.getFriends().iterator(), friends);
-        return friends.get(r.nextInt(friends.size()));
+        IteratorUtil.addToCollection( p.getFriends().iterator(), friends );
+        return friends.get( r.nextInt( friends.size() ) );
     }
 
-    private Person getRandomPersonWithFriends() {
+    private Person getRandomPersonWithFriends()
+    {
         Person p;
-        do {
+        do
+        {
             p = getRandomPerson();
-        } while (p.getNrOfFriends() == 0);
+        }
+        while ( p.getNrOfFriends() == 0 );
         return p;
     }
 
-    private void createPersons() {
+    private void createPersons()
+    {
         Transaction tx = graphDb.beginTx();
-        try {
-            for (int i = 0; i < nrOfPersons; i++) {
-                personRepository.createPerson("person#" + i);
+        try
+        {
+            for ( int i = 0; i < nrOfPersons; i++ )
+            {
+                personRepository.createPerson( "person#" + i );
             }
             tx.success();
         }
-        finally {
+        finally
+        {
             tx.finish();
         }
     }
 
-    private void assertUpdatesAreSortedByDate(ArrayList<StatusUpdate> statusUpdates) {
-        Date date = new Date(0);
-        for(StatusUpdate update : statusUpdates) {
-            org.junit.Assert.assertTrue(date.getTime() < update.getDate().getTime()); //TODO: Should be assertThat(date, lessThan(update.getDate));
+    private void assertUpdatesAreSortedByDate(
+            ArrayList<StatusUpdate> statusUpdates )
+    {
+        Date date = new Date( 0 );
+        for ( StatusUpdate update : statusUpdates )
+        {
+            org.junit.Assert.assertTrue( date.getTime() < update.getDate().getTime() ); // TODO:
+                                                                                        // Should
+                                                                                        // be
+                                                                                        // assertThat(date,
+                                                                                        // lessThan(update.getDate));
         }
     }
 }
