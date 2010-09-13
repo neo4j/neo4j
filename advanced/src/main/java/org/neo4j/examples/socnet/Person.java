b1a77da08b8c4770dd89b88f01400f24a45e75c2
Andrés Taylor <andres.taylor@neotechnology.com>
2010-09-13 11:31:29 +0000

    Added tests for the social graph example
    git-svn-id: https://svn.neo4j.org/examples/apoc-examples/trunk@5622 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/src/main/java/org/neo4j/examples/socnet/Person.java
+++ advanced/src/main/java/org/neo4j/examples/socnet/Person.java
@@ -9,181 +9,150 @@ import org.neo4j.helpers.collection.IteratorUtil;
 import org.neo4j.kernel.Traversal;
 import org.neo4j.kernel.Uniqueness;
 
-import java.util.Collections;
+import java.util.*;
 
-public class Person
-{
+public class Person {
     static final String NAME = "person_name";
-    static final RelationshipType FRIEND = 
-        DynamicRelationshipType.withName( "FRIEND" );
+    static final RelationshipType FRIEND =
+            DynamicRelationshipType.withName("FRIEND");
 
     // START SNIPPET: the-node
     private final Node underlyingNode;
 
-    Person( Node personNode )
-    {
+    Person(Node personNode) {
         this.underlyingNode = personNode;
     }
 
-    protected Node getUnderlyingNode()
-    {
+    protected Node getUnderlyingNode() {
         return underlyingNode;
     }
     // END SNIPPET: the-node
 
     // START SNIPPET: delegate-to-the-node
-    public String getName()
-    {
-        return (String) underlyingNode.getProperty( NAME );
+    public String getName() {
+        return (String) underlyingNode.getProperty(NAME);
     }
     // END SNIPPET: delegate-to-the-node
 
     // START SNIPPET: override
     @Override
-    public int hashCode()
-    {
+    public int hashCode() {
         return underlyingNode.hashCode();
     }
 
     @Override
-    public boolean equals( Object o )
-    {
-        if ( o instanceof Person )
-        {
-            return underlyingNode.equals( ( (Person) o ).getUnderlyingNode() );
+    public boolean equals(Object o) {
+        if (o instanceof Person) {
+            return underlyingNode.equals(((Person) o).getUnderlyingNode());
         }
         return false;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "Person[" + getName() + "]";
     }
 
     // END SNIPPET: override
 
-    public void addFriend( Person otherPerson )
-    {
+    public void addFriend(Person otherPerson) {
         Transaction tx = underlyingNode.getGraphDatabase().beginTx();
-        try
-        {
-            if ( this.equals( otherPerson ) )
-            {
+        try {
+            if (this.equals(otherPerson)) {
                 // ignore
                 return;
             }
-            Relationship friendRel = getFriendRelationshipTo( otherPerson );
-            if ( friendRel == null )
-            {
-                underlyingNode.createRelationshipTo(
-                        otherPerson.getUnderlyingNode(), FRIEND );
+            Relationship friendRel = getFriendRelationshipTo(otherPerson);
+            if (friendRel == null) {
+                underlyingNode.createRelationshipTo(otherPerson.getUnderlyingNode(), FRIEND);
             }
             tx.success();
         }
-        finally
-        {
+        finally {
             tx.finish();
         }
     }
 
-    public int getNrOfFriends(){
+    public int getNrOfFriends() {
         return IteratorUtil.count(getFriends());
     }
 
-    public Iterable<Person> getFriends()
-    {
+    public Iterable<Person> getFriends() {
         return new IterableWrapper<Person, Relationship>(
-                underlyingNode.getRelationships( FRIEND ) )
-        {
+                underlyingNode.getRelationships(FRIEND)) {
             @Override
-            protected Person underlyingObjectToObject( Relationship friendRel )
-            {
-                return new Person( friendRel.getOtherNode( underlyingNode ) );
+            protected Person underlyingObjectToObject(Relationship friendRel) {
+                return new Person(friendRel.getOtherNode(underlyingNode));
             }
         };
     }
 
-    public void removeFriend( Person otherPerson )
-    {
+    public void removeFriend(Person otherPerson) {
         Transaction tx = underlyingNode.getGraphDatabase().beginTx();
-        try
-        {
-            if ( this.equals( otherPerson ) )
-            {
+        try {
+            if (this.equals(otherPerson)) {
                 // ignore
                 return;
             }
-            Relationship friendRel = getFriendRelationshipTo( otherPerson );
-            if ( friendRel != null )
-            {
+            Relationship friendRel = getFriendRelationshipTo(otherPerson);
+            if (friendRel != null) {
                 friendRel.delete();
             }
             tx.success();
         }
-        finally
-        {
+        finally {
             tx.finish();
         }
     }
 
-    private Relationship getFriendRelationshipTo( Person otherPerson )
-    {
+    private Relationship getFriendRelationshipTo(Person otherPerson) {
         Node otherNode = otherPerson.getUnderlyingNode();
-        for ( Relationship rel : underlyingNode.getRelationships( FRIEND ) )
-        {
-            if ( rel.getOtherNode( underlyingNode ).equals( otherNode ) )
-            {
+        for (Relationship rel : underlyingNode.getRelationships(FRIEND)) {
+            if (rel.getOtherNode(underlyingNode).equals(otherNode)) {
                 return rel;
             }
         }
         return null;
     }
 
-    public Iterable<Person> getFriendsOfFriends()
-    {
+    public Iterable<Person> getFriendsOfFriends() {
         // return all my friends and their friends using new traversal API
-        TraversalDescription travDesc = 
+        TraversalDescription travDesc =
                 Traversal.description().
                         depthFirst().
-                        relationships(FRIEND ).
-                        uniqueness( Uniqueness.NODE_GLOBAL ).
-                        prune(Traversal.pruneAfterDepth( 2 ) )
-                        .filter(Traversal.returnAllButStartNode() );
+                        relationships(FRIEND).
+                        uniqueness(Uniqueness.NODE_GLOBAL).
+                        prune(Traversal.pruneAfterDepth(2))
+                        .filter(Traversal.returnAllButStartNode());
 
         return new IterableWrapper<Person, Path>(
-                travDesc.traverse( underlyingNode ) )
-        {
+                travDesc.traverse(underlyingNode)) {
             @Override
-            protected Person underlyingObjectToObject( Path path )
-            {
-                return new Person( path.endNode() );
+            protected Person underlyingObjectToObject(Path path) {
+                return new Person(path.endNode());
             }
         };
     }
 
-    public Iterable<Person> getPersonsFromMeTo( Person otherPerson, int maxDepth )
-    {
+    public Iterable<Person> getPersonsFromMeTo(Person otherPerson, int maxDepth) {
         // use graph algo to calculate a shortest path
         PathFinder<Path> finder = GraphAlgoFactory.
-                shortestPath(Traversal.expanderForTypes( FRIEND, Direction.BOTH ), maxDepth );
+                shortestPath(Traversal.expanderForTypes(FRIEND, Direction.BOTH), maxDepth);
 
-        Path path = finder.findSinglePath( underlyingNode, 
-                otherPerson.getUnderlyingNode() );
-        return new IterableWrapper<Person,Node>(
-                path.nodes() )
-        {
+        Path path = finder.findSinglePath(underlyingNode,
+                otherPerson.getUnderlyingNode());
+        return new IterableWrapper<Person, Node>(
+                path.nodes()) {
             @Override
-            protected Person underlyingObjectToObject( Node node )
-            {
-                return new Person( node );
+            protected Person underlyingObjectToObject(Node node) {
+                return new Person(node);
             }
         };
     }
 
     public Iterable<StatusUpdate> getStatus() {
         Relationship firstStatus = underlyingNode.getSingleRelationship(PersonRepository.STATUS, Direction.OUTGOING);
-        if(firstStatus == null)
+        if (firstStatus == null)
             return Collections.emptyList();
 
 
@@ -194,13 +163,16 @@ public class Person
                 filter(Traversal.returnAll());
 
         return new IterableWrapper<StatusUpdate, Path>(
-                traversal.traverse( firstStatus.getEndNode() ) )
-        {
+                traversal.traverse(firstStatus.getEndNode())) {
             @Override
-            protected StatusUpdate underlyingObjectToObject( Path path )
-            {
-                return new StatusUpdate( path.endNode() );
+            protected StatusUpdate underlyingObjectToObject(Path path) {
+                return new StatusUpdate(path.endNode());
             }
         };
+    }
+
+    public Iterator<StatusUpdate> friendStatuses() {
+        return new FriendsStatusUpdateIterator(this);
+
     }
 }
