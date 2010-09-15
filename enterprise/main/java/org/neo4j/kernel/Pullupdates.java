9116b394fcef4d4e8926dc0701156ba756d55baa
Mattias Persson <mattias@neotechnology.com>
2010-09-15 11:43:06 +0000

    These apps doesn't have to run in a transaction
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@5668 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/main/java/org/neo4j/kernel/Pullupdates.java
+++ enterprise/main/java/org/neo4j/kernel/Pullupdates.java
@@ -6,9 +6,9 @@ import org.neo4j.shell.AppCommandParser;
 import org.neo4j.shell.Output;
 import org.neo4j.shell.Session;
 import org.neo4j.shell.ShellException;
-import org.neo4j.shell.kernel.apps.GraphDatabaseApp;
+import org.neo4j.shell.kernel.apps.ReadOnlyGraphDatabaseApp;
 
-public class Pullupdates extends GraphDatabaseApp
+public class Pullupdates extends ReadOnlyGraphDatabaseApp
 {
     @Override
     protected String exec( AppCommandParser parser, Session session, Output out )
