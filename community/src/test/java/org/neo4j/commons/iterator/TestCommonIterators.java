1c62506cd8ef05c6fb9040a920f59b1ebca48b3e
Mattias Persson <mattias@neotechnology.com>
2010-09-20 22:57:21 +0000

    Added two useful iterators, CachingIterator (which implements ListIterator) and PagingIterator which can divide an iterator into pages, and (since it extends CachingIterator) can go back and forth through the results.
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@5722 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/test/java/org/neo4j/commons/iterator/TestCommonIterators.java
+++ community/src/test/java/org/neo4j/commons/iterator/TestCommonIterators.java
@@ -1,13 +1,20 @@
 package org.neo4j.commons.iterator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.junit.Test;
+import org.neo4j.helpers.collection.CachingIterator;
 import org.neo4j.helpers.collection.FilteringIterator;
+import org.neo4j.helpers.collection.PagingIterator;
+import org.neo4j.helpers.collection.RangeIterator;
 
 public class TestCommonIterators
 {
@@ -22,5 +29,137 @@ public class TestCommonIterators
         assertEquals( (Integer) 100, iterator.next() );
         assertEquals( (Integer) 101, iterator.next() );
         assertEquals( (Integer) 3, iterator.next() );
+    }
+    
+    @Test
+    public void testCachingIterator()
+    {
+        Iterator<Integer> source = new RangeIterator( 8 );
+        CachingIterator<Integer> caching = new CachingIterator<Integer>( source );
+        
+        try
+        {
+            caching.previous();
+            fail( "Should throw exception" );
+        }
+        catch ( NoSuchElementException e ) { /* Good */ }
+        
+        try
+        {
+            caching.current();
+            fail( "Should throw exception" );
+        }
+        catch ( NoSuchElementException e ) { /* Good */ }
+        
+        // Next and previous
+        assertEquals( 0, caching.position() );
+        assertTrue( caching.hasNext() );
+        assertEquals( 0, caching.position() );
+        assertFalse( caching.hasPrevious() );
+        assertEquals( (Integer) 0, caching.next() );
+        assertTrue( caching.hasNext() );
+        assertTrue( caching.hasPrevious() );
+        assertEquals( (Integer) 1, caching.next() );
+        assertTrue( caching.hasPrevious() );
+        assertEquals( (Integer) 1, caching.current() );
+        assertEquals( (Integer) 2, caching.next() );
+        assertEquals( (Integer) 2, caching.current() );
+        assertEquals( (Integer) 3, (Integer) caching.position() );
+        assertEquals( (Integer) 2, caching.current() );
+        assertTrue( caching.hasPrevious() );
+        assertEquals( (Integer) 2, caching.previous() );
+        assertEquals( (Integer) 2, caching.current() );
+        assertEquals( (Integer) 2, (Integer) caching.position() );
+        assertEquals( (Integer) 1, caching.previous() );
+        assertEquals( (Integer) 1, caching.current() );
+        assertEquals( (Integer) 1, (Integer) caching.position() );
+        assertEquals( (Integer) 0, caching.previous() );
+        assertEquals( (Integer) 0, (Integer) caching.position() );
+        assertFalse( caching.hasPrevious() );
+        
+        // Positioning
+        try
+        {
+            caching.position( -1 );
+            fail( "Shouldn't be able to set a lower value than 0" );
+        }
+        catch ( IllegalArgumentException e ) { /* Good */ }
+        assertEquals( (Integer) 0, caching.current() );
+        assertEquals( 0, caching.position( 3 ) );
+        try
+        {
+            caching.current();
+            fail( "Shouldn't be able to call current() after a call to position(int)" );
+        }
+        catch ( NoSuchElementException e ) { /* Good */ }
+        assertTrue( caching.hasNext() );
+        assertEquals( (Integer) 3, caching.next() );
+        assertEquals( (Integer) 3, caching.current() );
+        assertTrue( caching.hasPrevious() );
+        assertEquals( (Integer) 4, caching.next() );
+        assertEquals( 5, caching.position() );
+        assertEquals( (Integer) 4, caching.previous() );
+        assertEquals( (Integer) 4, caching.current() );
+        assertEquals( (Integer) 4, caching.current() );
+        assertEquals( 4, caching.position() );
+        assertEquals( (Integer) 3, caching.previous() );
+        assertEquals( 3, caching.position() );
+        try
+        {
+            caching.position( 9 );
+            fail( "Shouldn't be able to set a position which is too big" );
+        }
+        catch ( NoSuchElementException e ) { /* Good */ }
+        assertEquals( 3, caching.position( 8 ) );
+        assertTrue( caching.hasPrevious() );
+        assertFalse( caching.hasNext() );
+        try
+        {
+            caching.next();
+            fail( "Shouldn't be able to go beyond last item" );
+        }
+        catch ( NoSuchElementException e ) { /* Good */ }
+        assertEquals( 8, caching.position() );
+        assertEquals( (Integer) 7, caching.previous() );
+        assertEquals( (Integer) 6, caching.previous() );
+        assertEquals( 6, caching.position( 0 ) );
+        assertEquals( (Integer) 0, caching.next() );
+    }
+    
+    @Test
+    public void testPagingIterator()
+    {
+        Iterator<Integer> source = new RangeIterator( 24 );
+        PagingIterator<Integer> pager = new PagingIterator<Integer>( source, 10 );
+        assertEquals( 0, pager.page() );
+        assertTrue( pager.hasNext() );
+        assertPage( pager.nextPage(), 10, 0 );
+        assertTrue( pager.hasNext() );
+        
+        assertEquals( 1, pager.page() );
+        assertTrue( pager.hasNext() );
+        assertPage( pager.nextPage(), 10, 10 );
+        assertTrue( pager.hasNext() );
+
+        assertEquals( 2, pager.page() );
+        assertTrue( pager.hasNext() );
+        assertPage( pager.nextPage(), 4, 20 );
+        assertFalse( pager.hasNext() );
+        
+        pager.page( 1 );
+        assertEquals( 1, pager.page() );
+        assertTrue( pager.hasNext() );
+        assertPage( pager.nextPage(), 10, 10 );
+        assertTrue( pager.hasNext() );
+    }
+
+    private void assertPage( Iterator<Integer> page, int size, int plus )
+    {
+        for ( int i = 0; i < size; i++ )
+        {
+            assertTrue( page.hasNext() );
+            assertEquals( (Integer) (i+plus), page.next() );
+        }
+        assertFalse( page.hasNext() );
     }
 }
