c0a7535e988f1579a1b8c98b259d3e8a3f097039
Andrés Taylor <andres.taylor@neotechnology.com>
2010-10-04 08:01:23 +0000

    Expanded the soc net example
    git-svn-id: https://svn.neo4j.org/examples/apoc-examples/trunk@5819 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/test/java/org/neo4j/examples/socnet/SocnetTest.java
+++ community/src/test/java/org/neo4j/examples/socnet/SocnetTest.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.neo4j.examples.socnet;
 
 import org.hamcrest.CoreMatchers;
@@ -153,7 +173,9 @@ public class SocnetTest
         middleMan2.addFriend( endMan );
 
         Iterable<Person> path = start.getShortestPathTo( endMan, 4 );
+
         assertPathIs( path, start, middleMan1, middleMan2, endMan );
+        //assertThat( path, matchesPathByProperty(Person.NAME, "start", "middle1", "middle2", "endMan"));
     }
 
     @Test
