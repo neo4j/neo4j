e9de122f783235ca1cfebe9763219cd8d8bea1c9
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-10-04 12:02:48 +0000

    anticipating extra UDC info to encode, Pinger now takes a Map of params to attach to URL
    git-svn-id: https://svn.neo4j.org/laboratory/components/udc@5822 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/src/main/java/org/neo4j/ext/udc/impl/Pinger.java
+++ advanced/src/main/java/org/neo4j/ext/udc/impl/Pinger.java
@@ -7,12 +7,20 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 public class Pinger {
+    
+    private String address;
+    private Map<String, String> usageDataMap;
+
+    public Pinger(String address, Map<String, String> usageDataMap) {
+        this.address = address;
+        this.usageDataMap = usageDataMap;
+    }
 
 
-    public void ping(String address, String storeId, String kernelVersion) throws IOException {
-        String uri = "http://" + address + "/" + "?id=" + storeId + "+v=" + kernelVersion;
+    public void ping() throws IOException {
+        StringBuffer uri = new StringBuffer("http://" + address + "/" + "?");
 
-        URL url = new URL(uri);
+        URL url = new URL(uri.toString());
         URLConnection con = url.openConnection();
 
         con.setDoInput(true);
