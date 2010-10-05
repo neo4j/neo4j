81da326ea72311912d3e94800827c507ed25f46d
Andrés Taylor <andres.taylor@neotechnology.com>
2010-10-05 10:16:27 +0000

    Made license check mandatory, and updated all files to have the correct license header
    git-svn-id: https://svn.neo4j.org/examples/apoc-examples/trunk@5846 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/main/java/org/neo4j/examples/apoc/Neo4jShell.java
+++ enterprise/src/main/java/org/neo4j/examples/apoc/Neo4jShell.java
@@ -1,25 +1,37 @@
+/**
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
 package org.neo4j.examples.apoc;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-
-import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
 import org.neo4j.helpers.collection.MapUtil;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.neo4j.shell.ShellServer;
 import org.neo4j.shell.impl.SameJvmClient;
 import org.neo4j.shell.kernel.GraphDatabaseShellServer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Neo4jShell
 {
--- enterprise/src/test/java/org/neo4j/examples/MatrixTest.java
+++ enterprise/src/test/java/org/neo4j/examples/MatrixTest.java
@@ -1,23 +1,37 @@
+/**
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.neo4j.examples;
-
-import static org.junit.Assert.assertEquals;
-
-import java.io.File;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Path;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.graphdb.traversal.Traverser;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.neo4j.kernel.Traversal;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
 
 public class MatrixTest
 {
@@ -98,7 +112,7 @@ public class MatrixTest
 
     /**
      * Get the Neo node. (a.k.a. Thomas Anderson node)
-     * 
+     *
      * @return the Neo node
      */
     private static Node getNeoNode()
@@ -118,7 +132,7 @@ public class MatrixTest
         for ( Path friendPath : friendsTraverser )
         {
             System.out.println( "At depth " + friendPath.length() + " => "
-                                + friendPath.endNode().getProperty( "name" ) );
+                    + friendPath.endNode().getProperty( "name" ) );
             numberOfFriends++;
         }
         // END SNIPPET: get-friends-usage
@@ -126,6 +140,7 @@ public class MatrixTest
     }
 
     // START SNIPPET: get-friends
+
     private static Traverser getFriends( final Node person )
     {
         TraversalDescription td = Traversal.description().breadthFirst().relationships(
@@ -145,7 +160,7 @@ public class MatrixTest
         for ( Path hackerPath : traverser )
         {
             System.out.println( "At depth " + hackerPath.length() + " => "
-                                + hackerPath.endNode().getProperty( "name" ) );
+                    + hackerPath.endNode().getProperty( "name" ) );
             numberOfHackers++;
         }
         // END SNIPPET: find-hackers-usage
@@ -153,6 +168,7 @@ public class MatrixTest
     }
 
     // START SNIPPET: find-hackers
+
     private static Traverser findHackers( final Node startNode )
     {
         TraversalDescription td = Traversal.description().breadthFirst().relationships(
@@ -191,8 +207,7 @@ public class MatrixTest
             {
                 deleteFileOrDirectory( child );
             }
-        }
-        else
+        } else
         {
             file.delete();
         }
