963c4f0371c8988cf35830533d9d36d539ebaf68
Andrés Taylor <andres.taylor@neotechnology.com>
2010-10-05 10:16:27 +0000

    Made license check mandatory, and updated all files to have the correct license header
    git-svn-id: https://svn.neo4j.org/examples/apoc-examples/trunk@5846 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- packaging/src/main/java/org/neo4j/examples/apoc/CalculateShortestPath.java
+++ packaging/src/main/java/org/neo4j/examples/apoc/CalculateShortestPath.java
@@ -1,20 +1,34 @@
-package org.neo4j.examples.apoc;
+/**
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import java.io.File;
+package org.neo4j.examples.apoc;
 
 import org.neo4j.graphalgo.GraphAlgoFactory;
 import org.neo4j.graphalgo.PathFinder;
-import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.DynamicRelationshipType;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Path;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
 import org.neo4j.index.IndexService;
 import org.neo4j.index.lucene.LuceneIndexService;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
 import org.neo4j.kernel.Traversal;
+
+import java.io.File;
 
 public class CalculateShortestPath
 {
