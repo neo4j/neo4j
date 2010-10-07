4b7d1abe237b226de858c5375ee86b844480b3ce
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-10-07 09:59:46 +0000

    created top-level multi-module release project
    git-svn-id: https://svn.neo4j.org/release/trunk@5903 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/integration/osgi/src/main/java/org/neo4j/release/tests/osgi/ExampleService.java
@@ -0,0 +1,12 @@
+package org.neo4j.release.tests.osgi;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
