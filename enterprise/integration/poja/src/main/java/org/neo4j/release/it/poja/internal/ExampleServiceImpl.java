88779996c04d654024496461ad62fc0b3db3799f
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-10-18 08:41:08 +0000

    added missing poja
    git-svn-id: https://svn.neo4j.org/release/trunk@6192 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/integration/poja/src/main/java/org/neo4j/release/it/poja/internal/ExampleServiceImpl.java
@@ -0,0 +1,38 @@
+package org.neo4j.release.it.poja.internal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.neo4j.release.it.poja.ExampleService;
+
+/**
+ * Internal implementation of our example OSGi service
+ */
+public final class ExampleServiceImpl
+    implements ExampleService
+{
+    // implementation methods go here...
+
+    public String scramble( String text )
+    {
+        List charList = new ArrayList();
+
+        char[] textChars = text.toCharArray();
+        for( int i = 0; i < textChars.length; i++ )
+        {
+            charList.add( new Character( textChars[i] ) );
+        }
+
+        Collections.shuffle( charList );
+
+        char[] mixedChars = new char[text.length()];
+        for( int i = 0; i < mixedChars.length; i++ )
+        {
+            mixedChars[i] = ( (Character) charList.get( i ) ).charValue();
+        }
+
+        return new String( mixedChars );
+    }
+}
+
