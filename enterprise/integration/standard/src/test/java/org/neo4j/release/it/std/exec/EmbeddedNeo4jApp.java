4cc9f7cf7346e746b62721f43648e1b3cbee0a08
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-10-18 11:16:52 +0000

    added integration test for EmbeddedNeo4jApp
    git-svn-id: https://svn.neo4j.org/release/trunk@6197 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/integration/standard/src/test/java/org/neo4j/release/it/std/exec/EmbeddedNeo4jApp.java
@@ -0,0 +1,83 @@
+package org.neo4j.release.it.std.exec;
+
+import org.neo4j.graphdb.*;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+
+/**
+ * A simple app that embeds Neo4j.
+ */
+public class EmbeddedNeo4jApp
+{
+    private static final String DB_PATH = "neo4j-store";
+    private static final String NAME_KEY = "name";
+
+    // START SNIPPET: createReltype
+    private static enum ExampleRelationshipTypes implements RelationshipType
+    {
+        EXAMPLE
+    }
+    // END SNIPPET: createReltype
+
+    public static void main( final String[] args )
+    {
+        // START SNIPPET: startDb
+        GraphDatabaseService graphDb = new EmbeddedGraphDatabase( DB_PATH );
+        registerShutdownHook( graphDb );
+        // END SNIPPET: startDb
+
+        // START SNIPPET: operationsInATransaction
+        // Encapsulate operations in a transaction
+        Transaction tx = graphDb.beginTx();
+        try
+        {
+            Node firstNode = graphDb.createNode();
+            firstNode.setProperty( NAME_KEY, "Hello" );
+            Node secondNode = graphDb.createNode();
+            secondNode.setProperty( NAME_KEY, "World" );
+
+            firstNode.createRelationshipTo( secondNode,
+                ExampleRelationshipTypes.EXAMPLE );
+
+            String greeting = firstNode.getProperty( NAME_KEY ) + " "
+                + secondNode.getProperty( NAME_KEY );
+            System.out.println( greeting );
+            // END SNIPPET: operationsInATransaction
+
+            // START SNIPPET: removingData
+            // let's remove the data before committing
+            firstNode.getSingleRelationship( ExampleRelationshipTypes.EXAMPLE,
+                    Direction.OUTGOING ).delete();
+            firstNode.delete();
+            secondNode.delete();
+
+            tx.success();
+        }
+        finally
+        {
+            tx.finish();
+        }
+        // END SNIPPET: removingData
+
+        System.out.println( "Shutting down database ..." );
+        // START SNIPPET: shutdownServer
+        graphDb.shutdown();
+        // END SNIPPET: shutdownServer
+    }
+
+    private static void registerShutdownHook( final GraphDatabaseService graphDb )
+    {
+        // Registers a shutdown hook for the Neo4j instance so that it
+        // shuts down nicely when the VM exits (even if you "Ctrl-C" the
+        // running example before it's completed)
+        Runtime.getRuntime().addShutdownHook( new Thread()
+        {
+            @Override
+            public void run()
+            {
+                graphDb.shutdown();
+            }
+        } );
+    }
+
+    
+}
