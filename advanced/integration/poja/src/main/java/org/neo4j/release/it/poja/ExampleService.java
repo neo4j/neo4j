b594073d2c4597525062318cd44209341f0b31d8
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-10-18 08:41:08 +0000

    added missing poja
    git-svn-id: https://svn.neo4j.org/release/trunk@6192 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ advanced/integration/poja/src/main/java/org/neo4j/release/it/poja/ExampleService.java
@@ -0,0 +1,12 @@
+package org.neo4j.release.it.poja;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
