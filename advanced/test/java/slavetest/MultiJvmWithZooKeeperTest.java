62d775086cb4bd25f413370d241ab736d20da242
Andrés Taylor <andres.taylor@neotechnology.com>
2010-10-18 11:26:28 +0000

    Made tests runnable by maven
    git-svn-id: https://svn.neo4j.org/laboratory/components/ha/src@6198 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/test/java/slavetest/MultiJvmWithZooKeeperTest.java
+++ advanced/test/java/slavetest/MultiJvmWithZooKeeperTest.java
@@ -1,4 +1,31 @@
+/*
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package slavetest;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.neo4j.kernel.HighlyAvailableGraphDatabase;
+import org.neo4j.kernel.ha.zookeeper.ClusterManager;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -7,13 +34,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.Before;
-import org.neo4j.kernel.HighlyAvailableGraphDatabase;
-import org.neo4j.kernel.ha.zookeeper.ClusterManager;
-
-public class MultiJvmWithZooKeeperTesting extends MultiJvmTesting
+@Ignore
+public class MultiJvmWithZooKeeperTest extends MultiJvmTest
 {
     private static final File BASE_ZOO_KEEPER_DATA_DIR =
             new File( new File( "target" ), "zookeeper-data" );
