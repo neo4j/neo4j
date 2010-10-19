a36a546b9a119382a2cb3ebfd5eade967c4b15b3
Mattias Persson <mattias@neotechnology.com>
2010-10-19 08:37:47 +0000

    Follows changes in GraphDatabaseService/indexing
    git-svn-id: https://svn.neo4j.org/components/shell/trunk@6208 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/src/main/java/org/neo4j/shell/kernel/ReadOnlyGraphDatabaseProxy.java
+++ community/src/main/java/org/neo4j/shell/kernel/ReadOnlyGraphDatabaseProxy.java
@@ -21,11 +21,13 @@
 package org.neo4j.shell.kernel;
 
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.PropertyContainer;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.ReturnableEvaluator;
@@ -35,6 +37,9 @@ import org.neo4j.graphdb.Traverser;
 import org.neo4j.graphdb.Traverser.Order;
 import org.neo4j.graphdb.event.KernelEventHandler;
 import org.neo4j.graphdb.event.TransactionEventHandler;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
+import org.neo4j.graphdb.index.RelationshipIndex;
 import org.neo4j.helpers.collection.IterableWrapper;
 import org.neo4j.kernel.impl.traversal.OldTraverserWrapper;
 
@@ -436,5 +441,157 @@ public class ReadOnlyGraphDatabaseProxy implements GraphDatabaseService
                 return new ReadOnlyRelationshipProxy( relationship );
             }
         };
+    }
+
+    public Index<Node> nodeIndex( String indexName, Map<String, String> configForCreation )
+    {
+        return new ReadOnlyNodeIndexProxy( actual.nodeIndex( indexName, configForCreation ) );
+    }
+
+    public RelationshipIndex relationshipIndex( String indexName,
+            Map<String, String> configForCreation )
+    {
+        return new ReadOnlyRelationshipIndexProxy( actual.relationshipIndex( indexName, configForCreation ) );
+    }
+
+    abstract class ReadOnlyIndexProxy<T extends PropertyContainer, I extends Index<T>> implements
+            Index<T>
+    {
+        final I actual;
+
+        ReadOnlyIndexProxy( I actual )
+        {
+            this.actual = actual;
+        }
+
+        abstract T wrap( T actual );
+
+        public void delete()
+        {
+            readOnly();
+        }
+
+        public void add( T entity, String key, Object value )
+        {
+            readOnly();
+        }
+
+        public IndexHits<T> get( String key, Object value )
+        {
+            return new ReadOnlyIndexHitsProxy<T>( this, actual.get( key, value ) );
+        }
+
+        public IndexHits<T> query( String key, Object queryOrQueryObject )
+        {
+            return new ReadOnlyIndexHitsProxy<T>( this, actual.query( key, queryOrQueryObject ) );
+        }
+
+        public IndexHits<T> query( Object queryOrQueryObject )
+        {
+            return new ReadOnlyIndexHitsProxy<T>( this, actual.query( queryOrQueryObject ) );
+        }
+
+        public void remove( T entity, String key, Object value )
+        {
+            readOnly();
+        }
+    }
+
+    class ReadOnlyNodeIndexProxy extends ReadOnlyIndexProxy<Node, Index<Node>>
+    {
+        ReadOnlyNodeIndexProxy( Index<Node> actual )
+        {
+            super( actual );
+        }
+
+        @Override
+        Node wrap( Node actual )
+        {
+            return readOnly( actual );
+        }
+    };
+
+    class ReadOnlyRelationshipIndexProxy extends
+            ReadOnlyIndexProxy<Relationship, RelationshipIndex> implements RelationshipIndex
+    {
+        ReadOnlyRelationshipIndexProxy( RelationshipIndex actual )
+        {
+            super( actual );
+        }
+
+        @Override
+        Relationship wrap( Relationship actual )
+        {
+            return readOnly( actual );
+        }
+
+        public IndexHits<Relationship> get( String key, Object valueOrNull, Node startNodeOrNull,
+                Node endNodeOrNull )
+        {
+            return new ReadOnlyIndexHitsProxy<Relationship>( this, actual.get( key, valueOrNull,
+                    startNodeOrNull, endNodeOrNull ) );
+        }
+
+        public IndexHits<Relationship> query( String key, Object queryOrQueryObjectOrNull,
+                Node startNodeOrNull, Node endNodeOrNull )
+        {
+            return new ReadOnlyIndexHitsProxy<Relationship>( this, actual.query( key,
+                    queryOrQueryObjectOrNull, startNodeOrNull, endNodeOrNull ) );
+        }
+
+        public IndexHits<Relationship> query( Object queryOrQueryObjectOrNull,
+                Node startNodeOrNull, Node endNodeOrNull )
+        {
+            return new ReadOnlyIndexHitsProxy<Relationship>( this, actual.query(
+                    queryOrQueryObjectOrNull, startNodeOrNull, endNodeOrNull ) );
+        }
+    }
+
+    private static class ReadOnlyIndexHitsProxy<T extends PropertyContainer> implements
+            IndexHits<T>
+    {
+        private final ReadOnlyIndexProxy<T, ?> index;
+        private final IndexHits<T> actual;
+
+        ReadOnlyIndexHitsProxy( ReadOnlyIndexProxy<T, ?> index, IndexHits<T> actual )
+        {
+            this.index = index;
+            this.actual = actual;
+        }
+
+        public void close()
+        {
+            actual.close();
+        }
+
+        public T getSingle()
+        {
+            return index.wrap( actual.getSingle() );
+        }
+
+        public int size()
+        {
+            return actual.size();
+        }
+
+        public boolean hasNext()
+        {
+            return actual.hasNext();
+        }
+
+        public T next()
+        {
+            return index.wrap( actual.next() );
+        }
+
+        public void remove()
+        {
+            readOnly();
+        }
+
+        public Iterator<T> iterator()
+        {
+            return this;
+        }
     }
 }
