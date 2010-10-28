b01f6506b0ee44db8540207e242bdefbd2287547
Jim Webber <jim@neotechnology.com>
2010-10-28 12:02:08 +0000

    The config now looks in the specified (or default) directories and creates one mega-config. Users ask for their config through namespaced keys, eg. org.example.myapp.myparameter
    git-svn-id: https://svn.neo4j.org/components/server/trunk@6462 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- packaging/src/test/java/org/neo4j/server/configuration/ConfiguratorTest.java
+++ packaging/src/test/java/org/neo4j/server/configuration/ConfiguratorTest.java
@@ -22,33 +22,26 @@ package org.neo4j.server.configuration;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import org.apache.commons.configuration.Configuration;
-
-import org.junit.Before;
-import org.junit.Test;
 
 import java.io.File;
 
+import org.apache.commons.configuration.Configuration;
+import org.junit.Test;
+
 public class ConfiguratorTest {
-
-    private static final String TEST_COMPONENT = "test-component";
-
+    
     @Test
     public void shouldProvideAConfiguration() {
-       Configuration config = new Configurator().getConfigurationFor(TEST_COMPONENT);
-       assertNotNull(config);
-    }    
-   
+        Configuration config = new Configurator().configuration();
+        assertNotNull(config);
+    }
+
     @Test
     public void shouldUseSpecifiedConfigDir() {
-        Configuration testConf = new Configurator().getConfigurationFor( TEST_COMPONENT );
+        Configuration testConf = new Configurator(new File("src/test/resources/etc/neo-server")).configuration();
 
-        final String EXPECTED_VALUE_OF_FOO = "a useful value";
-        assertEquals(EXPECTED_VALUE_OF_FOO, testConf.getString( "foo" ));
-        
+        final String EXPECTED_VALUE = "bar";
+        assertEquals(EXPECTED_VALUE, testConf.getString("org.neo4j.foo"));
+
     }
 }
