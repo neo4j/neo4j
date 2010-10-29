ba94864eeac231ff0dff26eb1feb87c923a4f944
Jim Webber <jim@neotechnology.com>
2010-10-29 08:59:13 +0000

    Moved a functional test back to the unit tests since we have a nice way of isolating test runs with *very* temporary files.
    git-svn-id: https://svn.neo4j.org/components/server/trunk@6488 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/src/functionaltest/java/org/neo4j/server/configuration/ConfiguratorFunctionalTest.java
+++ advanced/src/functionaltest/java/org/neo4j/server/configuration/ConfiguratorFunctionalTest.java
@@ -9,11 +9,5 @@ import org.junit.Test;
 
 
 public class ConfiguratorFunctionalTest {
-    @Test
-    public void shouldUseSpecifiedConfigDir() {
-        Configuration testConf = new Configurator(new File("src/test/resources/etc/neo-server")).configuration();
 
-        final String EXPECTED_VALUE = "bar";
-        assertEquals(EXPECTED_VALUE, testConf.getString("org.neo4j.foo"));
-    }
 }
