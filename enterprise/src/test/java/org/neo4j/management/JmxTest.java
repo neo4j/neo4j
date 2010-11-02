4ec1a248369e95afb2b67574c473edae384c00a8
Peter Neubauer <peter.neubauer@neotechnology.com>
2010-11-02 12:32:53 +0000

    adding license headers, site documentation and fixed pom.xml
    git-svn-id: https://svn.neo4j.org/components/management/trunk@6569 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/src/test/java/org/neo4j/management/JmxTest.java
+++ enterprise/src/test/java/org/neo4j/management/JmxTest.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.neo4j.management;
 
 import static org.junit.Assert.assertNotNull;
@@ -33,7 +53,9 @@ public class JmxTest
     @Test
     public void canAccessKernelBean() throws Exception
     {
+        // START SNIPPET: getKernel
         Kernel kernel = graphDb.getManagementBean( Kernel.class );
+        // END SNIPPET: getKernel
         assertNotNull( "kernel bean is null", kernel );
         assertNotNull( "MBeanQuery of kernel bean is null", kernel.getMBeanQuery() );
     }
