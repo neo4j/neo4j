60f83f6b7f5a754c273fc5697ea8bad564c7faf9
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-11-05 10:26:15 +0000

    cleaned up windows bat scripts; removed extra conf files
    git-svn-id: https://svn.neo4j.org/assemblies/neo4j-standalone/trunk@6765 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- packaging/src/main/distribution/shell-scripts/bin/neo4j-shell.bat
+++ packaging/src/main/distribution/shell-scripts/bin/neo4j-shell.bat
@@ -67,9 +67,25 @@ set BASEDIR=%~dp0\..
 
 if "%JAVACMD%"=="" set JAVACMD=java
 
-if "%REPO%"=="" set REPO=%BASEDIR%\/lib
+if "%REPO%"=="" set REPO=%BASEDIR%\lib
 
-set CLASSPATH="%BASEDIR%"\etc;"%REPO%"\neo4j-kernel-1.2-20101102.005253-124.jar;"%REPO%"\geronimo-jta_1.1_spec-1.1.1.jar;"%REPO%"\neo4j-index-1.2-20101102.071526-163.jar;"%REPO%"\org.apache.servicemix.bundles.lucene-3.0.1_2.jar;"%REPO%"\neo4j-lucene-index-0.2-20101102.124246-66.jar;"%REPO%"\neo4j-shell-1.2-20101102.123804-162.jar;"%REPO%"\org.apache.servicemix.bundles.jline-0.9.94_1.jar;"%REPO%"\neo4j-remote-graphdb-0.8-20101102.124424-143.jar;"%REPO%"\protobuf-java-2.3.0.jar;"%REPO%"\neo4j-graph-algo-0.7-20101102.005429-135.jar;"%REPO%"\neo4j-online-backup-0.7-20101102.124324-142.jar;"%REPO%"\neo4j-udc-0.1-20101102.005450-51.jar;"%REPO%"\neo4j-management-1.2-SNAPSHOT.jar;"%REPO%"\neo4j-rest-0.8-20101102.143839-73.jar;"%REPO%"\jersey-server-1.3.jar;"%REPO%"\jersey-core-1.3.jar;"%REPO%"\jsr311-api-1.1.1.jar;"%REPO%"\asm-3.1.jar;"%REPO%"\oauth-server-1.3.jar;"%REPO%"\oauth-signature-1.3.jar;"%REPO%"\jackson-jaxrs-1.4.1.jar;"%REPO%"\jackson-core-asl-1.4.1.jar;"%REPO%"\jackson-mapper-asl-1.4.1.jar;"%REPO%"\grizzly-servlet-webserver-1.9.18.jar;"%REPO%"\grizzly-http-1.9.18.jar;"%REPO%"\grizzly-framework-1.9.18.jar;"%REPO%"\grizzly-utils-1.9.18.jar;"%REPO%"\grizzly-rcm-1.9.18.jar;"%REPO%"\grizzly-portunif-1.9.18.jar;"%REPO%"\grizzly-http-servlet-1.9.18.jar;"%REPO%"\servlet-api-2.5.jar;"%REPO%"\jetty-6.1.25.jar;"%REPO%"\jetty-util-6.1.25.jar;"%REPO%"\servlet-api-2.5-20081211.jar;"%REPO%"\neo4j-server-0.1-SNAPSHOT.jar;"%REPO%"\log4j-1.2.16.jar;"%REPO%"\commons-configuration-1.6.jar;"%REPO%"\commons-collections-3.2.1.jar;"%REPO%"\commons-lang-2.4.jar;"%REPO%"\commons-logging-1.1.1.jar;"%REPO%"\commons-digester-1.8.jar;"%REPO%"\commons-beanutils-1.7.0.jar;"%REPO%"\commons-beanutils-core-1.8.0.jar;"%REPO%"\commons-io-1.4.jar;"%REPO%"\neo4j-examples-1.2-SNAPSHOT.jar;"%REPO%"\neo4j-udc-0.1-20101102.005450-51-neo4j.jar;"%REPO%"\neo4j-examples-1.2-SNAPSHOT-sources.jar;"%REPO%"\neo4j-examples-1.2-SNAPSHOT-test-sources.jar;"%REPO%"\neo4j-standalone-0.1-SNAPSHOT.pom
+rem Setup the classpath
+set LIBPATH=""
+pushd "%REPO%"
+for %%G in (*.jar) do call:APPEND_TO_LIBPATH %%G
+popd
+goto LIBPATH_END
+
+: APPEND_TO_LIBPATH
+set filename=%~1
+set suffix=%filename:~-4%
+if %suffix% equ .jar set LIBPATH=%LIBPATH%;%REPO%\%filename%
+goto :EOF
+
+:LIBPATH_END
+
+set CLASSPATH=%LIBPATH%
+
 set EXTRA_JVM_ARGUMENTS=
 goto endInit
 
