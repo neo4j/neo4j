cf949f1dbd11558281fe124e66495578be93867b
Mattias Persson <mattias@neotechnology.com>
2010-11-11 19:07:45 +0000

    Some cleaning up and gathered classes for local manual testing under slavetest.manual
    git-svn-id: https://svn.neo4j.org/components/ha/trunk/src@6885 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/test/java/slavetest/DumpLogs.java
+++ enterprise/test/java/slavetest/DumpLogs.java
@@ -28,23 +28,16 @@ public class DumpLogs
 {
     public static void main( String[] args ) throws Exception
     {
-        for ( File file : new File( "var/hadb1" ).listFiles() )
+        for ( File file : new File( "var/hadb" ).listFiles() )
         {
-            if ( !file.getName().contains( ".1" ) ) continue;
+//            if ( !file.getName().contains( ".1" ) ) continue;
             
             try
             {
                 if ( file.getName().contains( "logical" ) && !file.getName().endsWith( ".active" ) )
                 {
                     System.out.println( "\n=== " + file.getPath() + " ===" );
-                    if ( file.getName().contains( ".ptx_" ) || file.getName().contains( ".tx_" ) )
-                    {
-                        DumpLogicalLog.main( new String[] { "-single", file.getPath() } );
-                    }
-                    else
-                    {
-                        DumpLogicalLog.main( new String[] { file.getPath() } );
-                    }
+                    DumpLogicalLog.main( new String[] { file.getPath() } );
                 }
             }
             catch ( Exception e )
