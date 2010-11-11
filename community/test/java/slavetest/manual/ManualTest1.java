17f285dedd6dc8cbc65d57fad5f8306b3443b6a6
Mattias Persson <mattias@neotechnology.com>
2010-11-11 19:07:45 +0000

    Some cleaning up and gathered classes for local manual testing under slavetest.manual
    git-svn-id: https://svn.neo4j.org/components/ha/trunk/src@6885 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/test/java/slavetest/manual/ManualTest1.java
+++ community/test/java/slavetest/manual/ManualTest1.java
@@ -18,9 +18,10 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package slavetest;
+package slavetest.manual;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -29,28 +30,29 @@ import org.neo4j.kernel.Config;
 import org.neo4j.kernel.HighlyAvailableGraphDatabase;
 import org.neo4j.kernel.ha.zookeeper.NeoStoreUtil;
 
-public class BringUpOther
+public class ManualTest1
 {
+    public static final File PATH = new File( "var/man/db1" );
+    static final String ME = "127.0.0.1:5559";
+        
     public static void main( String[] args ) throws Exception
     {
-        File path = new File( "test/dbs/1" );
-        NeoStoreUtil store = new NeoStoreUtil( path.getPath() );
+        NeoStoreUtil store = new NeoStoreUtil( PATH.getPath() );
         System.out.println( "Starting store: createTime=" + new Date( store.getCreationTime() ) +
                 " identifier=" + store.getStoreId() + " last committed tx=" + store.getLastCommittedTx() );
-        final GraphDatabaseService db = new HighlyAvailableGraphDatabase( path.getPath(), MapUtil.stringMap(
-                HighlyAvailableGraphDatabase.CONFIG_KEY_HA_MACHINE_ID, "2",
-                HighlyAvailableGraphDatabase.CONFIG_KEY_HA_ZOO_KEEPER_SERVERS, "localhost:2181,localhost:2182,localhost:2183",
-                HighlyAvailableGraphDatabase.CONFIG_KEY_HA_SERVER, "localhost:5560",
-                Config.ENABLE_REMOTE_SHELL, "port=1338",
+        GraphDatabaseService db = startDb();
+        System.out.println( "Waiting for ENTER (for clean shutdown)" );
+        System.in.read();
+        db.shutdown();
+    }
+
+    private static GraphDatabaseService startDb() throws IOException
+    {
+        return new HighlyAvailableGraphDatabase( PATH.getPath(), MapUtil.stringMap(
+                HighlyAvailableGraphDatabase.CONFIG_KEY_HA_MACHINE_ID, "1",
+                HighlyAvailableGraphDatabase.CONFIG_KEY_HA_ZOO_KEEPER_SERVERS, "127.0.0.1:2181,127.0.0.1:2182",
+                HighlyAvailableGraphDatabase.CONFIG_KEY_HA_SERVER, ME,
+                Config.ENABLE_REMOTE_SHELL, "true",
                 Config.KEEP_LOGICAL_LOGS, "true" ) );
-        Runtime.getRuntime().addShutdownHook( new Thread()
-        {
-            public void run()
-            {
-                db.shutdown();
-            }
-        } );
-        System.out.println( "up" );
-        while ( true ) Thread.sleep( 1000 );
     }
 }
