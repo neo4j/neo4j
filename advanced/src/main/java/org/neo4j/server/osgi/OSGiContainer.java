97cd9e43402c57d9f079d505fc1e95d8c39c7b91
Andrés Taylor <andres.taylor@neotechnology.com>
2010-11-12 16:41:29 +0000

    first draft of an embedded OSGi container
    git-svn-id: https://svn.neo4j.org/components/server/trunk@6897 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ advanced/src/main/java/org/neo4j/server/osgi/OSGiContainer.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.neo4j.server.osgi;
+
+import org.apache.felix.framework.Felix;
+import org.apache.felix.framework.util.FelixConstants;
+import org.apache.felix.main.AutoProcessor;
+import org.neo4j.server.logging.Logger;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.launch.Framework;
+import org.osgi.framework.launch.FrameworkFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+
+/**
+ */
+public class OSGiContainer
+{
+    private Framework osgiFramework;
+    private String bundledir;
+    private HostBridge bridge;
+    private Logger log = Logger.getLogger( OSGiContainer.class );
+
+    public OSGiContainer( String bundleDirectory ) throws Exception
+    {
+        bundledir = bundleDirectory;
+
+        Map configMap = new HashMap();
+        bridge = new HostBridge();
+        List list = new ArrayList();
+        list.add( bridge );
+
+        configMap.put( FelixConstants.SYSTEMBUNDLE_ACTIVATORS_PROP, list );
+
+        osgiFramework = new Felix( configMap );
+
+        // set expected system properties
+    }
+
+    public void startContainer() throws BundleException
+    {
+        log.info("Starting OSGi container...");
+        osgiFramework.init();
+        Map<String, String> autoConfig = new HashMap<String, String>();
+        log.info( "Loading bundles from: " + bundledir );
+        autoConfig.put( AutoProcessor.AUTO_DEPLOY_DIR_PROPERY, bundledir );
+        autoConfig.put( AutoProcessor.AUTO_DEPLOY_ACTION_PROPERY,
+                AutoProcessor.AUTO_DEPLOY_INSTALL_VALUE + "," +
+                        AutoProcessor.AUTO_DEPLOY_START_VALUE
+        );
+        AutoProcessor.process( autoConfig, osgiFramework.getBundleContext() );
+        osgiFramework.start();
+        log.info("OSGi is ready.");
+    }
+
+    public Bundle[] getInstalledBundles()
+    {
+        return bridge.getBundles();
+    }
+
+    public void shutdownContainer() throws BundleException, InterruptedException
+    {
+        osgiFramework.stop();
+        osgiFramework.waitForStop( 0 );
+    }
+
+    private static FrameworkFactory getFrameworkFactory() throws Exception
+    {
+        ServiceLoader<FrameworkFactory> frameworkFactoryLoader = ServiceLoader.load( FrameworkFactory.class );
+        for ( FrameworkFactory factory : frameworkFactoryLoader )
+        {
+            return factory; // return the first one we find
+        }
+
+        throw new Exception( "Could not find framework factory." );
+    }
+
+}
