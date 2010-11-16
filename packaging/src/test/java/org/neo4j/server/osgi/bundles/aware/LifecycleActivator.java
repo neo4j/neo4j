d04f050a98d763b6cb558fef857ef68768d282ca
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-11-16 15:45:31 +0000

    added some tiny bundles for OSGiContainer testing; caused NeoServer.main to throw an exception if start returns an error code so that the server startup fails
    git-svn-id: https://svn.neo4j.org/components/server/trunk@6953 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ packaging/src/test/java/org/neo4j/server/osgi/bundles/aware/LifecycleActivator.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.neo4j.server.osgi.bundles.aware;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * For a bundle to be OSGi "aware" means that it
+ * participates in the OSGi lifecycle. Usually that
+ * involves either publishing or looking for services.
+ */
+public class LifecycleActivator implements BundleActivator
+{
+
+    public void start( BundleContext bundleContext ) throws Exception
+    {
+        System.out.println("OSGi aware bundle started");
+    }
+
+    public void stop( BundleContext bundleContext ) throws Exception
+    {
+        System.out.println("OSGi aware bundle stopped");
+    }
+
+}
