db1b411db6b161bc48db587e811eb68c3b114288
Peter Neubauer <peter.neubauer@neotechnology.com>
2010-11-16 01:11:17 +0000

    adding tests for the MonitorService, for now doing a dummy representation without RRD. Should be better stubbed out tomorrow
    git-svn-id: https://svn.neo4j.org/components/server/trunk@6929 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ advanced/src/test/java/org/neo4j/server/webadmin/rest/MonitorServiceTest.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.neo4j.server.webadmin.rest;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.kernel.ImpermanentGraphDatabase;
+import org.neo4j.server.database.Database;
+import org.neo4j.server.webadmin.console.ConsoleSession;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MonitorServiceTest
+{
+    public MonitorService monitorService;
+
+    @Test
+    public void correctRepresentation() throws URISyntaxException
+    {
+        UriInfo mockUri = mock(UriInfo.class);
+        URI uri = new URI("http://peteriscool.com:6666/");
+        when(mockUri.getBaseUri()).thenReturn(uri);
+        Response resp = monitorService.getServiceDefinition( mockUri );
+
+        assertEquals(200, resp.getStatus());
+        assertThat((String)resp.getEntity(), containsString("resources"));
+        assertThat((String)resp.getEntity(), containsString(uri.toString()));
+        assertThat((String)resp.getEntity(), containsString("monitor/fetch/{start}/{stop}"));
+    }
+    
+    @Test
+    public void canFetchData() throws URISyntaxException
+    {
+        UriInfo mockUri = mock(UriInfo.class);
+        URI uri = new URI("http://peteriscool.com:6666/");
+        when(mockUri.getBaseUri()).thenReturn(uri);
+        Response resp = monitorService.getData();
+
+        assertEquals(200, resp.getStatus());
+        assertThat((String)resp.getEntity(), containsString("timestamps"));
+        assertThat((String)resp.getEntity(), containsString("end_time"));
+        assertThat((String)resp.getEntity(), containsString("property_count"));
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        this.monitorService = new MonitorService(  );
+    }
+
+}
