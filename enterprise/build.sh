6a6361f1c9aafa0ed98e88642ee9dd9cdd3964ee
Anders Nawroth <anders@neotechnology.com>
2010-11-26 18:17:50 +0000

    Got all the backends and output formats working.
    git-svn-id: https://svn.neo4j.org/documentation/manual/trunk@7190 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- enterprise/build.sh
+++ enterprise/build.sh
@@ -1,15 +1,23 @@
-mkdir -p target
+rm -rf target
+mkdir target
 
 # PDF toolchain using FOP
-a2x -a docinfo -k -v -d book -f pdf --fop -D target --xsltproc-opts "--stringparam toc.section.depth 1 --stringparam admon.graphics 1" main/index.txt
+mkdir -p target/fop
+a2x -v -k -f pdf --fop -D target/fop --conf-file=conf/fop.conf --xsltproc-opts "--stringparam toc.section.depth 1 --stringparam admon.graphics 1" index.txt
 
 # output as chunked html
-#a2x -v -a docinfo --attribute icons -d book -f chunked -D target --xsltproc-opts "--stringparam admon.graphics 1" main/index.txt
+#a2x -v -k -f chunked -D target --conf-file=conf/chunked.conf --xsltproc-opts "--stringparam admon.graphics 1" index.txt
+
+# output as single html file, using the xhtml11 backend
+#mkdir -p target/html/images
+#cp -R images/* target/html/images
+#asciidoc -v -a docinfo -a icons -d book -o target/html/index.html --conf-file=conf/asciidoc.conf index.txt
 
 # output in text format
-#a2x -a docinfo -d book -f text -D target main/index.txt
+#mkdir target/text
+#a2x -v -k -f text -D target/text --xsl-file=conf/text.xsl --conf-file=conf/text.conf index.txt
 
-# another pdf toolchain - images won't show up in the pdf
-# at the moment, may need some configuration
-#a2x -v -a docinfo -d book -k -f pdf -D target --dblatex-opts "--param toc.section.depth=1" main/index.txt
+# PDF toolchain using latex
+#mkdir -p target/dblatex
+#a2x -v -k -f pdf -D target/dblatex --conf-file=conf/dblatex.conf index.txt
 
