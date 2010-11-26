8a167e67700958594f866f39549c23ab783b6722
Anders Nawroth <anders@neotechnology.com>
2010-11-26 15:19:59 +0000

    Added basic configuration.
    git-svn-id: https://svn.neo4j.org/documentation/manual/trunk@7186 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- community/build.sh
+++ community/build.sh
@@ -1,13 +1,15 @@
+mkdir -p target
+
 # PDF toolchain using FOP
-a2x -a docinfo -k -v -d book -f pdf --fop -D target --xsltproc-opts "--stringparam toc.section.depth 1" index.txt
+a2x -a docinfo -k -v -d book -f pdf --fop -D target --xsltproc-opts "--stringparam toc.section.depth 1 --stringparam admon.graphics 1" main/index.txt
 
 # output as chunked html
-#a2x -a docinfo -d book -f chunked -D target index.txt
+#a2x -v -a docinfo --attribute icons -d book -f chunked -D target --xsltproc-opts "--stringparam admon.graphics 1" main/index.txt
 
 # output in text format
-#a2x -a docinfo -d book -f text -D target index.txt
+#a2x -a docinfo -d book -f text -D target main/index.txt
 
 # another pdf toolchain - images won't show up in the pdf
 # at the moment, may need some configuration
-#a2x -v -a docinfo -d book -k -f pdf -D target --dblatex-opts "--param toc.section.depth=1" index.txt
+#a2x -v -a docinfo -d book -k -f pdf -D target --dblatex-opts "--param toc.section.depth=1" main/index.txt
 
