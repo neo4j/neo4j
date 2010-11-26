33049024b0416748e5337e4ebe8c509098d184ea
Anders Nawroth <anders@neotechnology.com>
2010-11-26 00:03:44 +0000

    Fixing trunk/tags structure.
    git-svn-id: https://svn.neo4j.org/documentation/manual/trunk@7181 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ community/build.sh
@@ -0,0 +1,13 @@
+# PDF toolchain using FOP
+a2x -a docinfo -k -v -d book -f pdf --fop -D target index.txt
+
+# output as chunked html
+#a2x -a docinfo -d book -f chunked -D target index.txt
+
+# output in text format
+#a2x -a docinfo -d book -f text -D target index.txt
+
+# another pdf toolchain - images won't show up in the pdf
+# at the moment, may need some configuration
+#a2x -v -a docinfo -d book -k -f pdf -D target index.txt
+
