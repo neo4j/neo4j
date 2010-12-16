69783ed7727242351c852c0d57bd524f52fb9eb1
Andrés Taylor <andres.taylor@neotechnology.com>
2010-12-16 12:24:47 +0000

    Example test for AK
    git-svn-id: https://svn.neo4j.org/components/server/trunk@7788 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- packaging/src/main/java/org/neo4j/server/rest/repr/NodeRepresentation.java
+++ packaging/src/main/java/org/neo4j/server/rest/repr/NodeRepresentation.java
@@ -23,8 +23,6 @@ package org.neo4j.server.rest.repr;
 import org.neo4j.graphdb.Node;
 import org.neo4j.helpers.collection.IterableWrapper;
 
-import java.net.URI;
-
 public final class NodeRepresentation extends ObjectRepresentation implements
         ExtensibleRepresentation, EntityRepresentation
 {
@@ -43,7 +41,7 @@ public final class NodeRepresentation extends ObjectRepresentation implements
     }
 
     @Mapping( "self" )
-    public URI selfUri()
+    public ValueRepresentation selfUri()
     {
         return ValueRepresentation.uri( path( "" ) );
     }
