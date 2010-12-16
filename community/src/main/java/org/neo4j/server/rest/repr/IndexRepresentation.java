c60bdf3c3b1b1ada42a5937c592710dc815ab208
Andreas Kollegger <andreas.kollegger@neotechnology.com>
2010-12-16 10:32:28 +0000

    more collab editing
    git-svn-id: https://svn.neo4j.org/components/server/trunk@7784 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ community/src/main/java/org/neo4j/server/rest/repr/IndexRepresentation.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.neo4j.server.rest.repr;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class IndexRepresentation implements ExtensibleRepresentation, EntityRepresentation
+{
+    private final URI baseUri;
+    private final String name;
+    private final Map<String, String> type;
+
+    public IndexRepresentation( URI baseUri, String name, Map<String, String> type )
+    {
+        this.baseUri = baseUri;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Map<String, Object> serialize()
+    {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put( "template", baseUri.toString() + "index/" + propertyContainerType() + "/" + name + "/{key}/{value}" );
+        map.putAll( type );
+        return map;
+    }
+
+    public abstract String propertyContainerType();
+
+    public URI selfUri()
+    {
+        return null; // new URI( baseUri.toString() + name );
+    }
+}
---
+++ community/src/main/java/org/neo4j/server/rest/repr/NodeIndexRootRepresentation.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2002-2010 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.neo4j.server.rest.repr;
+
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexManager;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NodeIndexRootRepresentation implements ExtensibleRepresentation, EntityRepresentation
+{
+    private final URI baseUri;
+    private IndexManager indexManager;
+
+    public NodeIndexRootRepresentation( URI baseUri, IndexManager indexManager )
+    {
+        this.baseUri = baseUri;
+        this.indexManager = indexManager;
+    }
+
+    public ValueRepresentation selfUri()
+    {
+        return null; // new URI( baseUri.toString() + "index" );
+    }
+
+    public Map<String, Object> serialize()
+    {
+        Map<String, Object> map = new HashMap<String, Object>();
+        indexManager.nodeIndexNames();
+
+        for ( String indexName : indexManager.nodeIndexNames() )
+        {
+            Index<Node> index = indexManager.forNodes( indexName );
+            map.put( indexName, new NodeIndexRepresentation( baseUri, indexName, indexManager.getConfiguration( index ) ).serialize() );
+        }
+
+        return map;
+    }
+
+    @Override
+    public String getIdentity()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
