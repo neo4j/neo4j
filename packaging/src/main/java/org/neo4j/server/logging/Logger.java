abe5aca1176744bfa1f283a0701c01ecb9295400
Jim Webber <jim@neotechnology.com>
2011-01-20 14:39:48 +0000

    Added the ability to create loggers by string name as well as class.
    git-svn-id: https://svn.neo4j.org/components/server/trunk@8334 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- packaging/src/main/java/org/neo4j/server/logging/Logger.java
+++ packaging/src/main/java/org/neo4j/server/logging/Logger.java
@@ -54,9 +54,17 @@ public class Logger {
     public static Logger getLogger(Class<?> clazz) {
         return new Logger(clazz);
     }
+    
+    public static Logger getLogger(String logger) {
+        return new Logger(logger);
+    }
 
     public Logger(Class<?> clazz) {
         logger = org.apache.log4j.Logger.getLogger(clazz);
+    }
+
+    public Logger(String str) {
+        logger = org.apache.log4j.Logger.getLogger(str);
     }
 
     public void log(Priority priority, String message, Throwable throwable) {
