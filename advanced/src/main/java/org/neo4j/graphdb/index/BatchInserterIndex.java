bbac71f23e4f8375b669304983c4b654570516b5
Anders Nawroth <anders@neotechnology.com>
2011-01-25 15:05:46 +0000

    Reduce the amount of javadoc warnings.
    git-svn-id: https://svn.neo4j.org/components/kernel/trunk@8422 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- advanced/src/main/java/org/neo4j/graphdb/index/BatchInserterIndex.java
+++ advanced/src/main/java/org/neo4j/graphdb/index/BatchInserterIndex.java
@@ -27,9 +27,9 @@ import org.neo4j.kernel.impl.batchinsert.BatchInserter;
 
 /**
  * The {@link BatchInserter} version of {@link Index}. Additions/updates to a
- * {@link BatchInserterIndex} doesn't necessarily gets added to the actual
- * index immediately, but are instead forced to be writted when the index is
- * shut down, {@link BatchInserterIndexProvider#shutdown()}.
+ * {@link BatchInserterIndex} doesn't necessarily gets added to the actual index
+ * immediately, but are instead forced to be written when the index is shut
+ * down, {@link BatchInserterIndexProvider#shutdown()}.
  * 
  * To guarantee additions/updates are seen by {@link #updateOrAdd(long, Map)},
  * {@link #get(String, Object)}, {@link #query(String, Object)} and
@@ -42,65 +42,64 @@ public interface BatchInserterIndex
     /**
      * Adds key/value pairs for {@code entity} to the index. If there's a
      * previous index for {@code entity} it will co-exist with this new one.
-     * This behaviour is because of performance reasons, to not being forced
-     * to check if indexing for {@code entity} already exists or not. If you
-     * need to update indexing for {@code entity} and it's ok with a slower
-     * indexing process use {@link #addOrUpdate(long, Map)} instead.
+     * This behaviour is because of performance reasons, to not being forced to
+     * check if indexing for {@code entity} already exists or not. If you need
+     * to update indexing for {@code entity} and it's ok with a slower indexing
+     * process use {@link #updateOrAdd(long, Map)} instead.
      * 
-     * Entries added to the index aren't necessarily written to the index and
-     * to disk until {@link BatchInserterIndexProvider#shutdown()} has been
-     * called. Entries added to the index isn't necessarily seen by other
-     * methods: {@link #updateOrAdd(long, Map)}, {@link #get(String, Object)},
-     * {@link #query(String, Object)} and {@link #query(Object)} until a call
-     * to {@link #flush()} has been made.
+     * Entries added to the index aren't necessarily written to the index and to
+     * disk until {@link BatchInserterIndexProvider#shutdown()} has been called.
+     * Entries added to the index isn't necessarily seen by other methods:
+     * {@link #updateOrAdd(long, Map)}, {@link #get(String, Object)},
+     * {@link #query(String, Object)} and {@link #query(Object)} until a call to
+     * {@link #flush()} has been made.
      * 
-     * @param entity the entity (i.e id of {@link Node} or
-     * {@link Relationship}) to associate the key/value pairs with.
+     * @param entityId the entity (i.e id of {@link Node} or
+     *            {@link Relationship}) to associate the key/value pairs with.
      * @param properties key/value pairs to index for {@code entity}.
      */
     void add( long entityId, Map<String, Object> properties );
-    
+
     /**
      * Adds key/value pairs for {@code entity} to the index. If there's any
-     * previous index for {@code entity} all such indexed key/value pairs
-     * will be deleted first. This method can be considerably slower than
+     * previous index for {@code entity} all such indexed key/value pairs will
+     * be deleted first. This method can be considerably slower than
      * {@link #add(long, Map)} because it must check if there are properties
      * already indexed for {@code entity}. So if you know that there's no
-     * previous indexing for {@code entity} use {@link #add(long, Map)}
-     * instead.
+     * previous indexing for {@code entity} use {@link #add(long, Map)} instead.
      * 
-     * Entries added to the index aren't necessarily written to the index and
-     * to disk until {@link BatchInserterIndexProvider#shutdown()} has been
-     * called. Entries added to the index isn't necessarily seen by other
-     * methods: {@link #updateOrAdd(long, Map)}, {@link #get(String, Object)},
-     * {@link #query(String, Object)} and {@link #query(Object)} until a call
-     * to {@link #flush()} has been made. So only entries added before the most
+     * Entries added to the index aren't necessarily written to the index and to
+     * disk until {@link BatchInserterIndexProvider#shutdown()} has been called.
+     * Entries added to the index isn't necessarily seen by other methods:
+     * {@link #updateOrAdd(long, Map)}, {@link #get(String, Object)},
+     * {@link #query(String, Object)} and {@link #query(Object)} until a call to
+     * {@link #flush()} has been made. So only entries added before the most
      * recent {@link #flush()} are guaranteed to be found by this method.
      * 
-     * @param entity the entity (i.e id of {@link Node} or
-     * {@link Relationship}) to associate the key/value pairs with.
+     * @param entityId the entity (i.e id of {@link Node} or
+     *            {@link Relationship}) to associate the key/value pairs with.
      * @param properties key/value pairs to index for {@code entity}.
      */
     void updateOrAdd( long entityId, Map<String, Object> properties );
-    
+
     /**
-     * Returns exact matches from this index, given the key/value pair.
-     * Matches will be for key/value pairs just as they were added by the
-     * {@link #add(long, Map)} or {@link #addOrUpdate(long, Map)} method. 
+     * Returns exact matches from this index, given the key/value pair. Matches
+     * will be for key/value pairs just as they were added by the
+     * {@link #add(long, Map)} or {@link #updateOrAdd(long, Map)} method.
      * 
-     * Entries added to the index aren't necessarily written to the index and
-     * to disk until {@link BatchInserterIndexProvider#shutdown()} has been
-     * called. Entries added to the index isn't necessarily seen by other
-     * methods: {@link #updateOrAdd(long, Map)}, {@link #get(String, Object)},
-     * {@link #query(String, Object)} and {@link #query(Object)} until a call
-     * to {@link #flush()} has been made. So only entries added before the most
+     * Entries added to the index aren't necessarily written to the index and to
+     * disk until {@link BatchInserterIndexProvider#shutdown()} has been called.
+     * Entries added to the index isn't necessarily seen by other methods:
+     * {@link #updateOrAdd(long, Map)}, {@link #get(String, Object)},
+     * {@link #query(String, Object)} and {@link #query(Object)} until a call to
+     * {@link #flush()} has been made. So only entries added before the most
      * recent {@link #flush()} are guaranteed to be found by this method.
      * 
      * @param key the key in the key/value pair to match.
      * @param value the value in the key/value pair to match.
      * @return the result wrapped in an {@link IndexHits} object. If the entire
-     * result set isn't looped through, {@link IndexHits#close()} must be
-     * called before disposing of the result.
+     *         result set isn't looped through, {@link IndexHits#close()} must
+     *         be called before disposing of the result.
      */
     IndexHits<Long> get( String key, Object value );
     
--- advanced/src/main/java/org/neo4j/graphdb/index/RelationshipIndex.java
+++ advanced/src/main/java/org/neo4j/graphdb/index/RelationshipIndex.java
@@ -20,6 +20,7 @@
 package org.neo4j.graphdb.index;
 
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.PropertyContainer;
 import org.neo4j.graphdb.Relationship;
 
 /**
@@ -33,53 +34,53 @@ import org.neo4j.graphdb.Relationship;
 public interface RelationshipIndex extends Index<Relationship>
 {
     /**
-     * Returns exact matches from this index, given the key/value pair.
-     * Matches will be for key/value pairs just as they were added by the
-     * {@link #add(PropertyContainer, String, Object)} method. 
+     * Returns exact matches from this index, given the key/value pair. Matches
+     * will be for key/value pairs just as they were added by the
+     * {@link #add(PropertyContainer, String, Object)} method.
      * 
      * @param key the key in the key/value pair to match.
-     * @param value the value in the key/value pair to match.
+     * @param valueOrNull the value in the key/value pair to match.
      * @param startNodeOrNull filter so that only {@link Relationship}s with
-     * that given start node will be returned.
-     * @param endNodeOrNull filter so that only {@link Relationship}s with
-     * that given end node will be returned.
+     *            that given start node will be returned.
+     * @param endNodeOrNull filter so that only {@link Relationship}s with that
+     *            given end node will be returned.
      * @return the result wrapped in an {@link IndexHits} object. If the entire
-     * result set isn't looped through, {@link IndexHits#close()} must be
-     * called before disposing of the result.
+     *         result set isn't looped through, {@link IndexHits#close()} must
+     *         be called before disposing of the result.
      */
     IndexHits<Relationship> get( String key, Object valueOrNull, Node startNodeOrNull,
             Node endNodeOrNull );
-    
+
     /**
      * Returns matches from this index based on the supplied {@code key} and
      * query object, which can be a query string or an implementation-specific
      * query object.
      * 
      * @param key the key in this query.
-     * @param queryOrQueryObject the query for the {@code key} to match.
+     * @param queryOrQueryObjectOrNull the query for the {@code key} to match.
      * @param startNodeOrNull filter so that only {@link Relationship}s with
-     * that given start node will be returned.
-     * @param endNodeOrNull filter so that only {@link Relationship}s with
-     * that given end node will be returned.
+     *            that given start node will be returned.
+     * @param endNodeOrNull filter so that only {@link Relationship}s with that
+     *            given end node will be returned.
      * @return the result wrapped in an {@link IndexHits} object. If the entire
-     * result set isn't looped through, {@link IndexHits#close()} must be
-     * called before disposing of the result.
+     *         result set isn't looped through, {@link IndexHits#close()} must
+     *         be called before disposing of the result.
      */
     IndexHits<Relationship> query( String key, Object queryOrQueryObjectOrNull,
             Node startNodeOrNull, Node endNodeOrNull );
-    
+
     /**
-     * Returns matches from this index based on the supplied query object,
-     * which can be a query string or an implementation-specific query object.
+     * Returns matches from this index based on the supplied query object, which
+     * can be a query string or an implementation-specific query object.
      * 
-     * @param queryOrQueryObject the query to match.
+     * @param queryOrQueryObjectOrNull the query to match.
      * @param startNodeOrNull filter so that only {@link Relationship}s with
-     * that given start node will be returned.
-     * @param endNodeOrNull filter so that only {@link Relationship}s with
-     * that given end node will be returned.
+     *            that given start node will be returned.
+     * @param endNodeOrNull filter so that only {@link Relationship}s with that
+     *            given end node will be returned.
      * @return the result wrapped in an {@link IndexHits} object. If the entire
-     * result set isn't looped through, {@link IndexHits#close()} must be
-     * called before disposing of the result.
+     *         result set isn't looped through, {@link IndexHits#close()} must
+     *         be called before disposing of the result.
      */
     IndexHits<Relationship> query( Object queryOrQueryObjectOrNull, Node startNodeOrNull,
             Node endNodeOrNull );
