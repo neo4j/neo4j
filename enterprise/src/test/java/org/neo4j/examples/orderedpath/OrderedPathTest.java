42f0cba3fc34f97fc79b3758e064e4754840e661
Peter Neubauer <peter.neubauer@neotechnology.com>
2011-01-26 02:12:37 +0000

    providing an example of an ordered traversal
    git-svn-id: https://svn.neo4j.org/examples/neo4j-examples/trunk@8435 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ enterprise/src/test/java/org/neo4j/examples/orderedpath/OrderedPathTest.java
@@ -0,0 +1,98 @@
+package org.neo4j.examples.orderedpath;
+
+import static org.neo4j.graphdb.DynamicRelationshipType.withName;
+
+import java.util.ArrayList;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Path;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.traversal.Evaluation;
+import org.neo4j.graphdb.traversal.Evaluator;
+import org.neo4j.graphdb.traversal.TraversalDescription;
+import org.neo4j.graphdb.traversal.Traverser;
+import org.neo4j.kernel.EmbeddedGraphDatabase;
+import org.neo4j.kernel.Traversal;
+
+public class OrderedPathTest
+{
+    private static EmbeddedGraphDatabase db;
+    private static RelationshipType REL1 = withName( "REL1" ),
+            REL2 = withName( "REL2" ), REL3 = withName( "REL3" );
+
+    @BeforeClass
+    public static void createTheGraph()
+    {
+        db = new EmbeddedGraphDatabase( "target/db" );
+        Transaction tx = db.beginTx();
+        // START SNIPPET: createGraph
+        Node A = db.createNode();
+        Node B = db.createNode();
+        Node C = db.createNode();
+        Node D = db.createNode();
+        A.createRelationshipTo( B, REL1 );
+        B.createRelationshipTo( C, REL2 );
+        C.createRelationshipTo( D, REL3 );
+        A.createRelationshipTo( C, REL2 );
+
+        // END SNIPPET: createGraph
+        tx.success();
+        tx.finish();
+    }
+    // START SNIPPET: walkOrderedPath
+    @Test
+    public void testPath()
+    {
+        final ArrayList<RelationshipType> orderedPathContext = new ArrayList<RelationshipType>();
+        orderedPathContext.add( REL1 );
+        orderedPathContext.add( withName( "REL2" ) );
+        orderedPathContext.add( withName( "REL3" ) );
+        TraversalDescription td = Traversal.description().evaluator(
+                new Evaluator()
+                {
+
+                    public Evaluation evaluate( Path path )
+                    {
+                        if ( path.length() == 0 )
+                        {
+                            return Evaluation.EXCLUDE_AND_CONTINUE;
+                        }
+                        String currentName = path.lastRelationship().getType().name();
+                        String relationshipType = orderedPathContext.get(
+                                path.length() - 1 ).name();
+                        if ( path.length() == orderedPathContext.size() )
+                        {
+                            if ( currentName.equals( relationshipType ) )
+                            {
+                                return Evaluation.INCLUDE_AND_PRUNE;
+                            }
+                            else
+                            {
+                                return Evaluation.EXCLUDE_AND_PRUNE;
+                            }
+                        }
+                        else
+                        {
+                            if ( currentName.equals( relationshipType ) )
+                            {
+                                return Evaluation.EXCLUDE_AND_CONTINUE;
+                            }
+                            else
+                            {
+                                return Evaluation.EXCLUDE_AND_PRUNE;
+                            }
+                        }
+                    }
+                } );
+        Traverser t = td.traverse( db.getNodeById( 1 ) );
+        for ( Path path : t )
+        {
+            System.out.println( path );
+        }
+        // END SNIPPET: walkOrderedPath
+
+    }
+}
