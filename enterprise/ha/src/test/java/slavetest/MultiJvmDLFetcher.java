e21aaf5c9e1a68ff253849b7b787dc4ccaf7c981
Tobias Ivarsson <thobes@gmail.com>
2011-01-31 18:18:25 +0100

    Changed the dependencies so that shell depends on the graphdb components, not the other way around.

--- enterprise/ha/src/test/java/slavetest/MultiJvmDLFetcher.java
+++ enterprise/ha/src/test/java/slavetest/MultiJvmDLFetcher.java
@@ -21,34 +21,20 @@ package slavetest;
 
 import java.io.Serializable;
 import java.rmi.RemoteException;
-
-import org.neo4j.shell.impl.RmiLocation;
+import java.rmi.server.RemoteObject;
 
 public class MultiJvmDLFetcher implements Fetcher<DoubleLatch>, Serializable
 {
-    public MultiJvmDLFetcher() throws RemoteException
+    private final DoubleLatch latch;
+
+	public MultiJvmDLFetcher() throws RemoteException
     {
-        MultiJvmDoubleLatch latch = new MultiJvmDoubleLatch();
-        RmiLocation location = location();
-        location.ensureRegistryCreated();
-        location.bind( latch );
-    }
-    
-    private RmiLocation location()
-    {
-        return RmiLocation.location( "localhost", 8054, "latch" );
+    	this.latch = (DoubleLatch) RemoteObject.toStub( new MultiJvmDoubleLatch() );
     }
     
     public DoubleLatch fetch()
     {
-        try
-        {
-            return (DoubleLatch) location().getBoundObject();
-        }
-        catch ( RemoteException e )
-        {
-            throw new RuntimeException( e );
-        }
+    	return latch;
     }
 
     public void close()
