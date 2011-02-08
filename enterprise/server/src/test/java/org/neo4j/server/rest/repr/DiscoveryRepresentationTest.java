4b26dd932a9aa455112d3b4ac1f8ed10e5f19556
Jim Webber <jim@neotechnology.com>
2011-02-08 09:45:41 +0100

    Adding missing files, *sigh*

---
+++ enterprise/server/src/test/java/org/neo4j/server/rest/repr/DiscoveryRepresentationTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.server.rest.repr;
+
+import static org.junit.Assert.*;
+
+import java.net.URI;
+import java.util.Map;
+
+import org.junit.Test;
+
+
+public class DiscoveryRepresentationTest {
+    @Test
+    public void shouldCreateAMapContainingDataAndManagementURIs() throws Exception {
+        URI managementUri = new URI("http://localhost:9999/management");
+        URI dataUri = new URI("http://localhost:8888/data");
+        DiscoveryRepresentation dr = new DiscoveryRepresentation(managementUri, dataUri);
+        
+        Map<String, Object> mapOfUris = RepresentationTestBase.serialize(dr);
+        
+        Object mappedManagementUri = mapOfUris.get("management");
+        Object mappedDataUri = mapOfUris.get("data");
+
+        assertNotNull(mappedManagementUri);
+        assertNotNull(mappedDataUri);
+        
+        assertEquals(managementUri.toString(), mappedManagementUri);
+        assertEquals(dataUri.toString(), mappedDataUri);
+    }
+}
