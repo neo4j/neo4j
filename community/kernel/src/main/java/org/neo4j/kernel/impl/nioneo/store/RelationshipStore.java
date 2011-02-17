4be590841f4ee46b7e7cf09edc48b8d7cc27b2b5
Mattias Persson <mattias@neotechnology.com>
2011-02-17 12:56:54 +0100

    Patched relationship store to support higher order bits for ids

--- community/kernel/src/main/java/org/neo4j/kernel/impl/nioneo/store/RelationshipStore.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/nioneo/store/RelationshipStore.java
@@ -157,16 +157,46 @@ public class RelationshipStore extends AbstractStore implements Store
     private void updateRecord( RelationshipRecord record, 
         PersistenceWindow window )
     {
-        int id = (int) record.getId();
+        long id = record.getId();
         Buffer buffer = window.getOffsettedBuffer( id );
         if ( record.inUse() )
         {
-            byte inUse = Record.IN_USE.byteValue();
-            buffer.put( inUse ).putInt( (int) record.getFirstNode() ).putInt(
-                (int) record.getSecondNode() ).putInt( record.getType() ).putInt(
-                (int) record.getFirstPrevRel() ).putInt( (int) record.getFirstNextRel() )
-                .putInt( (int) record.getSecondPrevRel() ).putInt(
-                    (int) record.getSecondNextRel() ).putInt( (int) record.getNextProp() );
+            long firstNode = record.getFirstNode();
+            short firstNodeMod = (short)((firstNode&0x700000000L) >> 31);
+            
+            // [ xxx,    ][    ,    ][    ,    ][    ,    ] second node high order bits,     0x70000000
+            // [    ,xxx ][    ,    ][    ,    ][    ,    ] first prev rel high order bits,  0xE000000
+            // [    ,   x][xx  ,    ][    ,    ][    ,    ] first next rel high order bits,  0x1C00000
+            // [    ,    ][  xx,x   ][    ,    ][    ,    ] second prev rel high order bits, 0x380000
+            // [    ,    ][    , xxx][    ,    ][    ,    ] second next rel high order bits, 0x70000
+            // [    ,    ][    ,    ][xxxx,xxxx][xxxx,xxxx] type
+            long secondNode = record.getSecondNode();
+            long secondNodeMod = (secondNode&0x700000000L) >> 4;
+            
+            long firstPrevRel = record.getFirstPrevRel();
+            long firstPrevRelMod = firstPrevRel == Record.NO_NEXT_RELATIONSHIP.intValue() ? 0 : (firstPrevRel&0xF00000000L) >> 7;
+            
+            long firstNextRel = record.getFirstNextRel();
+            long firstNextRelMod = firstNextRel == Record.NO_NEXT_RELATIONSHIP.intValue() ? 0 : (firstNextRel&0xF00000000L) >> 10;
+            
+            long secondPrevRel = record.getSecondPrevRel();
+            long secondPrevRelMod = secondPrevRel == Record.NO_NEXT_RELATIONSHIP.intValue() ? 0 : (secondPrevRel&0xF00000000L) >> 13;
+            
+            long secondNextRel = record.getSecondNextRel();
+            long secondNextRelMod = secondNextRel == Record.NO_NEXT_RELATIONSHIP.intValue() ? 0 : (secondNextRel&0xF00000000L) >> 16;
+            
+            long nextProp = record.getNextProp();
+            long nextPropMod = nextProp == Record.NO_NEXT_PROPERTY.intValue() ? 0 : (nextProp&0xF00000000L) >> 28;
+            
+            // [    ,   x] in use flag
+            // [    ,xxx ] first node high order bits
+            // [xxxx,    ] next prop high order bits
+            short inUseUnsignedByte = (short)(Record.IN_USE.byteValue()|firstNodeMod|nextPropMod);
+            int typeInt = (int)(record.getType()|secondNodeMod|firstPrevRelMod|firstNextRelMod|secondPrevRelMod|secondNextRelMod);
+            
+            buffer.put( (byte)inUseUnsignedByte ).putInt( (int) firstNode ).putInt( (int) secondNode )
+                .putInt( typeInt ).putInt( (int) firstPrevRel ).putInt( (int) firstNextRel )
+                .putInt( (int) secondPrevRel ).putInt( (int) secondNextRel ).putInt( (int) nextProp );
         }
         else
         {
@@ -178,29 +208,65 @@ public class RelationshipStore extends AbstractStore implements Store
         }
     }
 
-    private RelationshipRecord getRecord( int id, PersistenceWindow window, 
-        boolean check )
+    private RelationshipRecord getRecord( long id, PersistenceWindow window, 
+        boolean checkInUse )
     {
         Buffer buffer = window.getOffsettedBuffer( id );
-        byte inUse = buffer.get();
-        boolean inUseFlag = ((inUse & Record.IN_USE.byteValue()) == 
-            Record.IN_USE.byteValue());
-        if ( !inUseFlag )
+        
+        // [    ,   x] in use flag
+        // [    ,xxx ] first node high order bits
+        // [xxxx,    ] next prop high order bits
+        long inUseByte = buffer.get();
+        
+        boolean inUse = (inUseByte&0x1) == Record.IN_USE.intValue();
+        if ( !inUse )
         {
-            if ( check )
+            if ( checkInUse )
             {
                 return null;
             }
             throw new InvalidRecordException( "Record[" + id + "] not in use" );
         }
+        
+        long firstNode = buffer.getInt();
+        long firstNodeMod = (inUseByte&0xE) << 31;
+            
+        long secondNode = buffer.getInt();
+        
+        // [ xxx,    ][    ,    ][    ,    ][    ,    ] second node high order bits,     0x70000000
+        // [    ,xxx ][    ,    ][    ,    ][    ,    ] first prev rel high order bits,  0xE000000
+        // [    ,   x][xx  ,    ][    ,    ][    ,    ] first next rel high order bits,  0x1C00000
+        // [    ,    ][  xx,x   ][    ,    ][    ,    ] second prev rel high order bits, 0x380000
+        // [    ,    ][    , xxx][    ,    ][    ,    ] second next rel high order bits, 0x70000
+        // [    ,    ][    ,    ][xxxx,xxxx][xxxx,xxxx] type
+        long typeInt = buffer.getInt();
+        long secondNodeMod = (typeInt&0x70000000L) << 4;
+        int type = (int)(typeInt&0xFFFF);
+        
         RelationshipRecord record = new RelationshipRecord( id,
-            buffer.getInt(), buffer.getInt(), buffer.getInt() );
-        record.setInUse( inUseFlag );
-        record.setFirstPrevRel( buffer.getInt() );
-        record.setFirstNextRel( buffer.getInt() );
-        record.setSecondPrevRel( buffer.getInt() );
-        record.setSecondNextRel( buffer.getInt() );
-        record.setNextProp( buffer.getInt() );
+            firstNode|firstNodeMod, secondNode|secondNodeMod, type );
+        record.setInUse( inUse );
+        
+        long firstPrevRel = buffer.getInt();
+        long firstPrevRelMod = firstPrevRel == Record.NO_NEXT_RELATIONSHIP.intValue() && (typeInt&0xE000000) == 0 ? 0 : (typeInt&0xE000000) << 7;
+        record.setFirstPrevRel( firstPrevRel|firstPrevRelMod );
+        
+        long firstNextRel = buffer.getInt();
+        long firstNextRelMod = firstNextRel == Record.NO_NEXT_RELATIONSHIP.intValue() && (typeInt&0x1C00000) == 0 ? 0 : (typeInt&0x1C00000) << 10;
+        record.setFirstNextRel( firstNextRel|firstNextRelMod );
+        
+        long secondPrevRel = buffer.getInt();
+        long secondPrevRelMod = secondPrevRel == Record.NO_NEXT_RELATIONSHIP.intValue() && (typeInt&0x380000) == 0 ? 0 : (typeInt&0x380000) << 13;
+        record.setSecondPrevRel( secondPrevRel|secondPrevRelMod );
+        
+        long secondNextRel = buffer.getInt();
+        long secondNextRelMod = secondNextRel == Record.NO_NEXT_RELATIONSHIP.intValue() && (typeInt&0x70000) == 0 ? 0 : (typeInt&0x70000) << 16;
+        record.setSecondNextRel( secondNextRel|secondNextRelMod );
+        
+        long nextProp = buffer.getInt();
+        long nextPropMod = nextProp == Record.NO_NEXT_PROPERTY.intValue() && (inUseByte&0xF0) == 0 ? 0 : (inUseByte&0xF0) << 28;
+        
+        record.setNextProp( nextProp|nextPropMod );
         return record;
     }
 
