92eb37dc5774717763bfad66436ac1bfa96136e0
unknown <Developer@.(none)>
2011-02-22 16:54:43 +0100

    first draft of windows friendly recipe. installs a neo4j solo server, not a cluster

--- enterprise/tools/ha-ops/with-chef/chef/solo.rb
+++ enterprise/tools/ha-ops/with-chef/chef/solo.rb
@@ -1,4 +1,5 @@
-# chef-solo -c $NEO_CHEF/solo.rb -j $NEO_CHEF/dna.json
+# 1. edit NEO_CHEF to be the full path to this directory
+# 2. run `chef-solo -c solo.rb -j neo4j.json`
 file_cache_path  "$NEO_CHEF"
 cookbook_path    "$NEO_CHEF/cookbooks"
 log_level        :debug
