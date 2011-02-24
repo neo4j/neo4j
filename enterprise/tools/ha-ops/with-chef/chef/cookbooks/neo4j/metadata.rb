af7709f89956f84841c9ee613e8102f5317d6018
Andreas Kollegger <akollegger@tembopublic.org>
2011-02-24 08:11:05 +0100

    used trick to pre-install then use archive zip; propagated changed attribute names to other recipes; starting to re-organize recipes

--- enterprise/tools/ha-ops/with-chef/chef/cookbooks/neo4j/metadata.rb
+++ enterprise/tools/ha-ops/with-chef/chef/cookbooks/neo4j/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "Neo Technology"
+maintainer_email "andreas.kollegger@neotechnology.com"
 license          "Apache 2.0"
-description      "Installs/Configures neo4j"
+description      "Installs/Configures Neo4j"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
