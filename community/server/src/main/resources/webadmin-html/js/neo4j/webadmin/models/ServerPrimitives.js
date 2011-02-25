92e7d64c277e64650d179b36dc48fa3e213828e7
Jacob Hansson <jakewins@voltvoodoo.com>
2011-02-25 17:00:05 +0100

    Added initial JMX tracking ports.

---
+++ community/server/src/main/resources/webadmin-html/js/neo4j/webadmin/models/ServerPrimitives.js
@@ -0,0 +1,26 @@
+(function() {
+  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  };
+  define(['./JmxBackedModel', 'lib/backbone'], function() {
+    var ServerPrimitives;
+    return ServerPrimitives = (function() {
+      function ServerPrimitives() {
+        ServerPrimitives.__super__.constructor.apply(this, arguments);
+      }
+      __extends(ServerPrimitives, JmxBackedModel);
+      ServerPrimitives.prototype.beans = {
+        primitives: {
+          domain: 'neo4j',
+          name: 'Primitive count'
+        }
+      };
+      return ServerPrimitives;
+    })();
+  });
+}).call(this);
