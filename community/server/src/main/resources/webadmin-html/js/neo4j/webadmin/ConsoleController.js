1a36d3209a3a609250c86796df98a45a382ea24d
Jacob Hansson <jakewins@voltvoodoo.com>
2011-03-01 16:50:52 +0100

    Added console implementation.

--- community/server/src/main/resources/webadmin-html/js/neo4j/webadmin/ConsoleController.js
+++ community/server/src/main/resources/webadmin-html/js/neo4j/webadmin/ConsoleController.js
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2002-2011 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
- *
- * This file is part of Neo4j.
- *
- * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 (function() {
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
@@ -26,7 +7,7 @@
     child.__super__ = parent.prototype;
     return child;
   };
-  define(['./views/ConsoleView', 'lib/backbone'], function(ConsoleView) {
+  define(['./models/Console', './views/ConsoleView', 'lib/backbone'], function(Console, ConsoleView) {
     var ConsoleController;
     return ConsoleController = (function() {
       function ConsoleController() {
@@ -39,7 +20,10 @@
         "/console/": "console"
       };
       ConsoleController.prototype.initialize = function(appState) {
-        return this.appState = appState;
+        this.appState = appState;
+        return this.consoleState = new Console({
+          server: this.appState.get("server")
+        });
       };
       ConsoleController.prototype.console = function() {
         return this.appState.set({
@@ -48,7 +32,10 @@
       };
       ConsoleController.prototype.getConsoleView = function() {
         var _ref;
-        return (_ref = this.consoleView) != null ? _ref : this.consoleView = new ConsoleView(this.appState);
+        return (_ref = this.consoleView) != null ? _ref : this.consoleView = new ConsoleView({
+          appState: this.appState,
+          consoleState: this.consoleState
+        });
       };
       return ConsoleController;
     })();
