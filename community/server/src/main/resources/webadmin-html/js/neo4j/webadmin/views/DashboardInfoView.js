f2ad667e65a32db050c978bb61141f332d8a0bc8
Jacob Hansson <jakewins@voltvoodoo.com>
2011-03-01 12:15:22 +0100

    Removed console.log calls.

--- community/server/src/main/resources/webadmin-html/js/neo4j/webadmin/views/DashboardInfoView.js
+++ community/server/src/main/resources/webadmin-html/js/neo4j/webadmin/views/DashboardInfoView.js
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2002-2011 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
- *
- * This file is part of Neo4j.
- *
- * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 (function() {
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
@@ -36,7 +17,6 @@
       __extends(DashboardInfoView, Backbone.View);
       DashboardInfoView.prototype.template = template;
       DashboardInfoView.prototype.initialize = function(opts) {
-        console.log(opts);
         this.primitives = opts.primitives;
         this.diskUsage = opts.diskUsage;
         this.cacheUsage = opts.cacheUsage;
