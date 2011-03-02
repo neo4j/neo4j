265362c83027e8c79ad9694f957421a05e9f6dfb
Marlon Richert <Marlon Richert@Petter-Laptop.guestlan.itmill.com>
2011-03-02 16:15:59 +0200

    Fetch from upstream.

--- community/web-visualization/src/main/java/org/neo4j/server/ext/visualization/WidgetTestApplication.java
+++ community/web-visualization/src/main/java/org/neo4j/server/ext/visualization/WidgetTestApplication.java
@@ -2,6 +2,7 @@ package org.neo4j.server.ext.visualization;
 
 import com.vaadin.Application;
 import com.vaadin.ui.Window;
+import org.neo4j.graphdb.GraphDatabaseService;
 
 /**
  * The Application's "main" class
@@ -10,13 +11,19 @@ import com.vaadin.ui.Window;
 public class WidgetTestApplication extends Application
 {
     private Window window;
+    private GraphDatabaseService graphDb;
+
+    public WidgetTestApplication(GraphDatabaseService graphDb)
+    {
+        this.graphDb = graphDb;
+    }
 
     @Override
     public void init()
     {
         window = new Window("Widget Test");
         setMainWindow(window);
-        window.addComponent(new GraphComponent());
+        window.addComponent(new GraphComponent(graphDb));
     }
     
 }
