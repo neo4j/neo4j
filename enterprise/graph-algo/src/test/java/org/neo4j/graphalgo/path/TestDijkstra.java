41f7191b7ac562ad3193c6c08f20f52e3b842e45
Mattias Persson <mattias@neotechnology.com>
2011-03-03 09:42:02 +0100

    Added test for DoubleCostEvaluatorWithDefault

--- enterprise/graph-algo/src/test/java/org/neo4j/graphalgo/path/TestDijkstra.java
+++ enterprise/graph-algo/src/test/java/org/neo4j/graphalgo/path/TestDijkstra.java
@@ -20,6 +20,9 @@
 package org.neo4j.graphalgo.path;
 
 import static org.junit.Assert.fail;
+import static org.neo4j.helpers.collection.MapUtil.map;
+
+import java.util.Map;
 
 import org.junit.Test;
 import org.neo4j.graphalgo.CommonEvaluators;
@@ -35,8 +38,7 @@ import common.Neo4jAlgoTestCase;
 
 public class TestDijkstra extends Neo4jAlgoTestCase
 {
-    @Test
-    public void testSmallGraph()
+    private Relationship createGraph( boolean includeOnes )
     {
         /* Layout:
          *                       (y)    
@@ -49,19 +51,29 @@ public class TestDijkstra extends Neo4jAlgoTestCase
          *                v  /       | /      \  /
          *               (b)--[1]-->(c)--[1]->(d)
          */
+        
+        Map<String, Object> propertiesForOnes = includeOnes ? map( "cost", (double) 1 ) : map();
+        
         graph.makeEdge( "start", "a", "cost", (double) 1 );
         graph.makeEdge( "a", "x", "cost", (double) 9 );
-        graph.makeEdge( "a", "b", "cost", (double) 1 );
+        graph.makeEdge( "a", "b", propertiesForOnes );
         graph.makeEdge( "b", "x", "cost", (double) 7 );
-        graph.makeEdge( "b", "c", "cost", (double) 1 );
+        graph.makeEdge( "b", "c", propertiesForOnes );
         graph.makeEdge( "c", "x", "cost", (double) 5 );
         Relationship shortCTOXRelationship = graph.makeEdge( "c", "x", "cost", (double) 3 );
-        graph.makeEdge( "c", "d", "cost", (double) 1 );
+        graph.makeEdge( "c", "d", propertiesForOnes );
         graph.makeEdge( "d", "x", "cost", (double) 3 );
-        graph.makeEdge( "d", "e", "cost", (double) 1 );
-        graph.makeEdge( "e", "x", "cost", (double) 1 );
+        graph.makeEdge( "d", "e", propertiesForOnes );
+        graph.makeEdge( "e", "x", propertiesForOnes );
         graph.makeEdge( "e", "f", "cost", (double) 2 );
         graph.makeEdge( "x", "y", "cost", (double) 2 );
+        return shortCTOXRelationship;
+    }
+    
+    @Test
+    public void testSmallGraph()
+    {
+        Relationship shortCTOXRelationship = createGraph( true );
         
         PathFinder<WeightedPath> finder = GraphAlgoFactory.dijkstra(
                 Traversal.expanderForTypes( MyRelTypes.R1, Direction.OUTGOING ), "cost" );
@@ -88,6 +100,32 @@ public class TestDijkstra extends Neo4jAlgoTestCase
         }
     }
 
+    @Test
+    public void testSmallGraphWithDefaults()
+    {
+        Relationship shortCTOXRelationship = createGraph( true );
+        
+        PathFinder<WeightedPath> finder = GraphAlgoFactory.dijkstra(
+                Traversal.expanderForTypes( MyRelTypes.R1, Direction.OUTGOING ),
+                CommonEvaluators.doubleCostEvaluator( "cost", 1.0d ) );
+        
+        // Assert that there are two matching paths
+        Node startNode = graph.getNode( "start" );
+        Node endNode = graph.getNode( "x" );
+        assertPaths( finder.findAllPaths( startNode, endNode ),
+                "start,a,b,c,x", "start,a,b,c,d,e,x" );
+        
+        // Assert that for the shorter one it picked the correct relationship
+        // of the two from (c) --> (x)
+        for ( WeightedPath path : finder.findAllPaths( startNode, endNode ) )
+        {
+            if ( getPathDef( path ).equals( "start,a,b,c,x" ) )
+            {
+                assertContainsRelationship( path, shortCTOXRelationship );
+            }
+        }
+    }
+    
     private void assertContainsRelationship( WeightedPath path,
             Relationship relationship )
     {
