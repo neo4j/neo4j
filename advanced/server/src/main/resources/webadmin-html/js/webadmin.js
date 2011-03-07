ca4a22d2fa75c5aefa7953749554de764a65c2b6
Jacob Hansson <jakewins@voltvoodoo.com>
2011-03-07 23:42:47 +0100

    Added initial keyboard shortcut for searching.

--- advanced/server/src/main/resources/webadmin-html/js/webadmin.js
+++ advanced/server/src/main/resources/webadmin-html/js/webadmin.js
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2002-2011 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
- *
- * This file is part of Neo4j.
- *
- * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 (function() {
   /*
   Copyright (c) 2002-2011 "Neo Technology,"
@@ -36,8 +17,8 @@
 
   You should have received a copy of the GNU Affero General Public License
   along with this program. If not, see <http://www.gnu.org/licenses/>.
-  */  require(["neo4j/webadmin/DashboardController", "neo4j/webadmin/DataBrowserController", "neo4j/webadmin/ConsoleController", "neo4j/webadmin/ServerInfoController", "neo4j/webadmin/models/ApplicationState", "neo4j/webadmin/views/BaseView", "neo4j/webadmin/ui/FoldoutWatcher", "lib/neo4js", "lib/jquery", "lib/underscore", "lib/backbone"], function(DashboardController, DataBrowserController, ConsoleController, ServerInfoController, ApplicationState, BaseView, FoldoutWatcher) {
-    var appState, foldoutWatcher;
+  */  require(["neo4j/webadmin/DashboardController", "neo4j/webadmin/DataBrowserController", "neo4j/webadmin/ConsoleController", "neo4j/webadmin/ServerInfoController", "neo4j/webadmin/models/ApplicationState", "neo4j/webadmin/views/BaseView", "neo4j/webadmin/ui/FoldoutWatcher", "neo4j/webadmin/KeyboardShortcuts", "lib/neo4js", "lib/jquery", "lib/underscore", "lib/backbone"], function(DashboardController, DataBrowserController, ConsoleController, ServerInfoController, ApplicationState, BaseView, FoldoutWatcher, KeyboardShortcuts) {
+    var appState, consoleController, dashboardController, databrowserController, foldoutWatcher, serverInfoController, shortcuts;
     appState = new ApplicationState;
     appState.set({
       server: new neo4j.GraphDatabase(location.protocol + "//" + location.host)
@@ -46,12 +27,14 @@
       el: $("body"),
       appState: appState
     });
-    new DashboardController(appState);
-    new DataBrowserController(appState);
-    new ConsoleController(appState);
-    new ServerInfoController(appState);
+    dashboardController = new DashboardController(appState);
+    databrowserController = new DataBrowserController(appState);
+    consoleController = new ConsoleController(appState);
+    serverInfoController = new ServerInfoController(appState);
     foldoutWatcher = new FoldoutWatcher;
     foldoutWatcher.init();
+    shortcuts = new KeyboardShortcuts(dashboardController, databrowserController, consoleController, serverInfoController);
+    shortcuts.init();
     return Backbone.history.start();
   });
 }).call(this);
