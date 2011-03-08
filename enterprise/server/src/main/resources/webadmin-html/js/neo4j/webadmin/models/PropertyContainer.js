4d7a4bf009d80dbd50dd91fbf9540a2812133e78
Jacob Hansson <jakewins@voltvoodoo.com>
2011-03-08 10:38:26 +0100

    Working on cucumber testing of the data browser.

--- enterprise/server/src/main/resources/webadmin-html/js/neo4j/webadmin/models/PropertyContainer.js
+++ enterprise/server/src/main/resources/webadmin-html/js/neo4j/webadmin/models/PropertyContainer.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 (function() {
   /*
   Copyright (c) 2002-2011 "Neo Technology,"
@@ -113,8 +132,11 @@
         }
       };
       PropertyContainer.prototype.deleteProperty = function(id) {
-        if (!isCantSave()) {
-          return this.setNotSaved();
+        var property;
+        if (!this.isCantSave()) {
+          this.setNotSaved();
+          property = this.getProperty(id);
+          return this.getItem().removeProperty(oldKey);
         }
       };
       PropertyContainer.prototype.addProperty = function(key, value, updatePropertyList, propertyMeta, opts) {
@@ -220,7 +242,7 @@
         return this.getSaveState() === "cantSave";
       };
       PropertyContainer.prototype.isNotSaved = function() {
-        return this.getSaveState() === "notSaved" || isCantSave();
+        return this.getSaveState() === "notSaved" || this.isCantSave();
       };
       PropertyContainer.prototype.getSaveState = function() {
         return this.get("saveState");
