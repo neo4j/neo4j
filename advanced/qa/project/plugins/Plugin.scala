83479be125921f6e4c1a3ffc517bfa62334df36a
Andreas Kollegger <akollegger@tembopublic.org>
2011-03-09 18:47:26 +0100

    removed SBT project remnants from build. all maven, all the time

--- advanced/qa/project/plugins/Plugin.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-
-class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val templemoreRepo = "templemore repo" at "http://templemore.co.uk/repo"
-  val cucumberPlugin = "templemore" % "cucumber-sbt-plugin" % "0.4.1"
-}
