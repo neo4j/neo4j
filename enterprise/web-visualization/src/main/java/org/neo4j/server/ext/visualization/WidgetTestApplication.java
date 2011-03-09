30a83eccbe75f4c80de9ae12f9148a111c55ead6
Marlon Richert <marlon@vaadin.com>
2011-03-09 10:05:16 +0200

    Made GWT compilation work in Maven.

--- enterprise/web-visualization/src/main/java/org/neo4j/server/ext/visualization/WidgetTestApplication.java
+++ enterprise/web-visualization/src/main/java/org/neo4j/server/ext/visualization/WidgetTestApplication.java
@@ -8,22 +8,18 @@ import org.neo4j.graphdb.GraphDatabaseService;
  * The Application's "main" class
  */
 @SuppressWarnings("serial")
-public class WidgetTestApplication extends Application
-{
+public class WidgetTestApplication extends Application {
     private Window window;
     private GraphDatabaseService graphDb;
 
-    public WidgetTestApplication(GraphDatabaseService graphDb)
-    {
-        this.graphDb = graphDb;
+    public WidgetTestApplication() {
     }
 
     @Override
-    public void init()
-    {
+    public void init() {
         window = new Window("Widget Test");
         setMainWindow(window);
         window.addComponent(new GraphComponent(graphDb));
     }
-    
+
 }
