43c5d525694e5e1d92b6d002b64dc7801784fbf3
Tobias Ivarsson <tobias@thobe.org>
2011-03-11 15:34:14 +0100

    Clarified the documentation of NestingIterable/NestingIterator.

--- community/kernel/src/main/java/org/neo4j/helpers/collection/NestingIterable.java
+++ community/kernel/src/main/java/org/neo4j/helpers/collection/NestingIterable.java
@@ -22,22 +22,22 @@ package org.neo4j.helpers.collection;
 import java.util.Iterator;
 
 /**
- * For each item in the supplied iterator (called "surface item") there's
- * instantiated an iterator from that item which is iterated before moving
- * on to the next surface item.
+ * Concatenates sub-iterables of an iterable.
  *
+ * @see NestingIterator
+ * 
  * @param <T> the type of items.
  * @param <U> the type of items in the surface item iterator
  */
 public abstract class NestingIterable<T, U> implements Iterable<T>
 {
-	private Iterable<U> source;
-	
+	private final Iterable<U> source;
+
 	public NestingIterable( Iterable<U> source )
 	{
 		this.source = source;
 	}
-	
+
 	public Iterator<T> iterator()
 	{
 		return new NestingIterator<T, U>( source.iterator() )
@@ -49,6 +49,6 @@ public abstract class NestingIterable<T, U> implements Iterable<T>
 			}
 		};
 	}
-	
+
 	protected abstract Iterator<T> createNestedIterator( U item );
 }
