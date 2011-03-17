92ae35a019604e7c24b67e9d81dba1e55ca2d257
Jacob Hansson <jakewins@voltvoodoo.com>
2011-03-17 12:52:46 +0100

    Improved UI for KPIs on dashboard page.

--- community/server/src/main/resources/webadmin-html/js/neo4j/webadmin/templates/dashboard/info.js
+++ community/server/src/main/resources/webadmin-html/js/neo4j/webadmin/templates/dashboard/info.js
@@ -1,4 +1,4 @@
-define(function(){return function(vars){ with(vars||{}) { return "<div class=\"span-third\"><div class=\"pad\"><h2>Primitives</h2><table class=\"fancy\"><tbody><tr><th>Nodes</th><td>" + 
+define(function(){return function(vars){ with(vars||{}) { return "<div class=\"pad\"><div class=\"span-third\"><table cellspacing=\"0\" class=\"info-table\"><tbody><tr><th>Nodes</th><td>" + 
 primitives.get("NumberOfNodeIdsInUse") + 
 "</td></tr><tr><th>Properties</th><td>" + 
 primitives.get("NumberOfPropertyIdsInUse") + 
@@ -6,16 +6,16 @@ primitives.get("NumberOfPropertyIdsInUse") +
 primitives.get("NumberOfRelationshipIdsInUse") + 
 "</td></tr><tr><th>Relationship types</th><td>" + 
 primitives.get("NumberOfRelationshipTypeIdsInUse") + 
-"</td></tr></tbody></table></div></div><div class=\"span-third\"><div class=\"pad\"><h2>Disk usage</h2><table class=\"fancy\"><tbody><tr><th>Total size</th><td>" + 
+"</td></tr></tbody></table></div><div class=\"span-third\"><table cellspacing=\"0\" class=\"info-table\"><tbody><tr><th>Total disk usage</th><td>" + 
 Math.round(diskUsage.get("TotalStoreSize") / 1024) + " kB " + 
 "</td></tr><tr><th>Database size</th><td>" + 
 Math.round( diskUsage.getDatabaseSize() / 1024) + " kB " + "(" + diskUsage.getDatabasePercentage() + "%)" + 
 "</td></tr><tr><th>Logical log size</th><td>" + 
 Math.round( diskUsage.getLogicalLogSize() / 1024) + " kB " + "(" + diskUsage.getLogicalLogPercentage() + "%)" + 
-"</td></tr></tbody></table></div></div><div class=\"span-third last\"><div class=\"pad\"><h2>Cache</h2><table class=\"fancy\"><tbody><tr><th>Cached nodes</th><td>" + 
+"</td></tr></tbody></table></div><div class=\"span-third last\"><table cellspacing=\"0\" class=\"info-table\"><tbody><tr><th>Cached nodes</th><td>" + 
 cacheUsage.get("NodeCacheSize") + 
 "</td></tr><tr><th>Cached relationships</th><td>" + 
 cacheUsage.get("RelationshipCacheSize") + 
 "</td></tr><tr><th>Cache type</th><td>" + 
 cacheUsage.get("CacheType") + 
-"</td></tr></tbody></table></div></div>";}}; });
+"</td></tr></tbody></table></div><div class=\"break\"></div></div>";}}; });
