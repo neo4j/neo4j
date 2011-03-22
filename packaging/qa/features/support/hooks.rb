8e37ed6d6d373b7f44c56d83219f6512ceb2314d
Thomas Baum <thomas.baum@atns.de>
2011-03-22 12:11:23 +0100

    cucumber-tests shutdown-hooks for windows

--- packaging/qa/features/support/hooks.rb
+++ packaging/qa/features/support/hooks.rb
@@ -4,13 +4,19 @@ After do |scenario|
   # if you use it, you can inspect status with
   # the #failed?, #passed? and #exception methods.
 
-  if neo4j.home
+  if neo4j.home && currrent_platform.unix?
     puts "*** cleanup: get status: " + `#{neo4j.home}/bin/neo4j status`
     if ($? == 0) ## is running
       print "*** cleanup: stop ..."
       `#{neo4j.home}/bin/neo4j stop`
       puts ($? == 0 ? "OK" :"FAIL")
     end
+  end
+
+  if neo4j.home && currrent_platform.windows?
+    print "*** cleanup: stop ..."
+    puts `net stop neo4j`
+    puts ($? == 0 ? "... OK" :"... FAIL")
   end
 end
 
