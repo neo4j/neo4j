ff4c0eee585a5057bea07032bfce45cc908d6aa2
Thomas Baum <thomas.baum@atns.de>
2011-03-22 13:51:29 +0100

    cucumber-tests less verbose cleanup

--- advanced/qa/features/support/hooks.rb
+++ advanced/qa/features/support/hooks.rb
@@ -5,18 +5,14 @@ After do |scenario|
   # the #failed?, #passed? and #exception methods.
 
   if neo4j.home && current_platform.unix?
-    puts "*** cleanup: get status: " + `#{neo4j.home}/bin/neo4j status`
+    `#{neo4j.home}/bin/neo4j status`
     if ($? == 0) ## is running
-      print "*** cleanup: stop ..."
       `#{neo4j.home}/bin/neo4j stop`
-      puts ($? == 0 ? "OK" :"FAIL")
     end
   end
 
   if neo4j.home && current_platform.windows?
-    print "*** cleanup: stop and remove ..."
-    puts `#{neo4j.home}\\bin\\wrapper-windows-x86-32.exe -r ..\\conf\\neo4j-wrapper.conf`
-    puts ($? == 0 ? "... OK" :"... FAIL")
+    `#{neo4j.home}\\bin\\wrapper-windows-x86-32.exe -r ..\\conf\\neo4j-wrapper.conf`
   end
 end
 
