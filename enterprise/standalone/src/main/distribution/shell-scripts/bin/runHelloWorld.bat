94722b64c610a1a8d69f7abf5194f14451afe9e0
Andreas Kollegger <akollegger@tembopublic.org>
2011-03-23 13:59:57 +0100

    also removed helloworld bat script

--- enterprise/standalone/src/main/distribution/shell-scripts/bin/runHelloWorld.bat
+++
@@ -1,7 +0,0 @@
-rem starts a test java program. this can be used to test YAJSW
-rem start the program, note its pid (of the java app), call genConfig <pid>, call runConsole, ...
-
-cd %~dp0
-call setenv.bat
-%java_exe% -cp %wrapper_jar% test.HelloWorld
-pause
