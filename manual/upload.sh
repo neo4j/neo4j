7be5eca796ce0de3ff722b45b4bcd42b90e9fdc2
Anders Nawroth <anders@neotechnology.com>
2011-03-24 18:51:01 +0100

    Changed how the upload script for the manual gets called.

--- manual/upload.sh
+++ manual/upload.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-# Deploys build artifacts to relevant servers
+DIR=$(cd `dirname $0` && pwd)
 
-if [[ $CD ]]
-then
-	cd manual
-fi
+# Deploys build artifacts to relevant servers
 
 
 # Which version the documentation is now.
-VERSION=$(cat target/classes/version)
+VERSION=$(cat $DIR/target/classes/version)
 
 # Name of the symlink created on the docs server, pointing to this version.
 if [[ $VERSION == *SNAPSHOT* ]]
@@ -35,10 +32,10 @@ echo "SYMLINKVERSION = $SYMLINKVERSION"
 ssh docs-server mkdir -p $ROOTPATHDOCS/{text,chunked,annotated}/$VERSION
 
 # Copy artifacts
-rsync -r target/text/ docs-server:$ROOTPATHDOCS/text/$VERSION/
-rsync -r --delete target/annotated/ docs-server:$ROOTPATHDOCS/annotated/
-rsync -r --delete target/chunked/ docs-server:$ROOTPATHDOCS/chunked/$VERSION/
-scp target/pdf/neo4j-manual.pdf dist-server:$ROOTPATHDIST/neo4j-manual-$VERSION.pdf
+rsync -r $DIR/target/text/ docs-server:$ROOTPATHDOCS/text/$VERSION/
+rsync -r --delete $DIR/target/annotated/ docs-server:$ROOTPATHDOCS/annotated/
+rsync -r --delete $DIR/target/chunked/ docs-server:$ROOTPATHDOCS/chunked/$VERSION/
+scp $DIR/target/pdf/neo4j-manual.pdf dist-server:$ROOTPATHDIST/neo4j-manual-$VERSION.pdf
 
 # Symlink this version to a generic url
 ssh docs-server "cd $ROOTPATHDOCS/text/ && (rm $SYMLINKVERSION || true); ln -s $VERSION $SYMLINKVERSION"
@@ -46,9 +43,4 @@ ssh docs-server "cd $ROOTPATHDOCS/chunked/ && (rm $SYMLINKVERSION || true); ln -
 ssh dist-server "cd $ROOTPATHDIST && (rm neo4j-manual-$SYMLINKVERSION.pdf || true); ln -s neo4j-manual-$VERSION.pdf neo4j-manual-$SYMLINKVERSION.pdf"
 
 echo Apparently, successfully copied artifacts to docs-server and dist-server.
-
-if [[ $CD ]]
-then
-	cd ..
-fi
 
