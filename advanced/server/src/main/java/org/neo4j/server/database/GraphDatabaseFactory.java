87f4c86760c241b88241c4d6c7ea14a114613caa
Tobias Ivarsson <tobias@thobe.org>
2011-04-01 14:41:01 +0200

    Initial changes for being able to have different editions of the server.

--- advanced/server/src/main/java/org/neo4j/server/database/GraphDatabaseFactory.java
+++ advanced/server/src/main/java/org/neo4j/server/database/GraphDatabaseFactory.java
@@ -22,31 +22,8 @@ package org.neo4j.server.database;
 import java.util.Map;
 
 import org.neo4j.kernel.AbstractGraphDatabase;
-import org.neo4j.kernel.EmbeddedGraphDatabase;
-import org.neo4j.kernel.HighlyAvailableGraphDatabase;
 
-public enum DatabaseMode
+public interface GraphDatabaseFactory
 {
-    STANDALONE
-    {
-        @Override
-        AbstractGraphDatabase createDatabase( String databaseStoreDirectory,
-                Map<String, String> databaseTuningProperties )
-        {
-            return new EmbeddedGraphDatabase( databaseStoreDirectory, databaseTuningProperties );
-        }
-    },
-    HA
-    {
-        @Override
-        AbstractGraphDatabase createDatabase( String databaseStoreDirectory,
-                Map<String, String> databaseTuningProperties )
-        {
-            return new HighlyAvailableGraphDatabase( databaseStoreDirectory,
-                    databaseTuningProperties );
-        }
-    };
-
-    abstract AbstractGraphDatabase createDatabase( String databaseStoreDirectory,
-            Map<String, String> databaseTuningProperties );
+    AbstractGraphDatabase createDatabase( String databaseStoreDirectory, Map<String, String> databaseProperties );
 }
