7b1f9edccec9af383d2b9e44b687d34f35fdc2cb
Jacob Hansson <jakewins@voltvoodoo.com>
2011-04-01 10:42:54 +0200

    Added a few scripts to aid rapid webadmin development.

--- advanced/server/src/main/resources/webadmin-html/js/neo4j/webadmin/visualization/Renderer.js
+++ advanced/server/src/main/resources/webadmin-html/js/neo4j/webadmin/visualization/Renderer.js
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2002-2011 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
- *
- * This file is part of Neo4j.
- *
- * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 (function() {
   /*
   Renderer, forked from the halfviz library.
@@ -132,7 +113,6 @@
         this.selected = null;
         this.nearest = null;
         this.dragged = null;
-        this.oldmass = 1;
         return $(this.canvas).mousedown(this.clicked);
       };
       Renderer.prototype.clicked = function(e) {
