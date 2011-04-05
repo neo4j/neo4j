f3cf3bea432996a1d2c51d5dcb9e60020f5fc421
Tobias Ivarsson <tobias@thobe.org>
2011-04-05 11:01:07 +0200

    Split server into Advanced and Enterprise.

--- enterprise/server-advanced/src/main/java/org/neo4j/server/advanced/AdvancedNeoServerBootstrapper.java
+++ enterprise/server-advanced/src/main/java/org/neo4j/server/advanced/AdvancedNeoServerBootstrapper.java
@@ -17,7 +17,9 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.server;
+package org.neo4j.server.advanced;
+
+import org.neo4j.server.NeoServerBootstrapper;
 
 public class AdvancedNeoServerBootstrapper extends NeoServerBootstrapper
 {
