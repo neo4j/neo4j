488bc9c60b06d31dc94909db4e4c6e78b9b757eb
Mattias Persson <mattias@neotechnology.com>
2011-04-05 09:45:21 +0200

    Fixed a compilation error due to management/JXM moving around

--- community/src/test/java/org/neo4j/examples/JmxTest.java
+++ community/src/test/java/org/neo4j/examples/JmxTest.java
@@ -25,8 +25,8 @@ import java.util.Date;
 
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.jmx.Kernel;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
-import org.neo4j.management.Kernel;
 
 public class JmxTest
 {
