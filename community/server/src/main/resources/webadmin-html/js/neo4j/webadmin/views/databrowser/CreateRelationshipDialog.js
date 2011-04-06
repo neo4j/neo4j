33a358e77d5b71935a562ef58d3115c54649d44d
Jacob Hansson <jakewins@voltvoodoo.com>
2011-04-06 12:21:10 +0200

    Added better help messages to the create relationship dialog.

--- community/server/src/main/resources/webadmin-html/js/neo4j/webadmin/views/databrowser/CreateRelationshipDialog.js
+++ community/server/src/main/resources/webadmin-html/js/neo4j/webadmin/views/databrowser/CreateRelationshipDialog.js
@@ -25,12 +25,14 @@
     child.__super__ = parent.prototype;
     return child;
   };
-  define(['neo4j/webadmin/data/ItemUrlResolver', 'neo4j/webadmin/templates/databrowser/createRelationship', 'neo4j/webadmin/views/View', 'lib/backbone'], function(ItemUrlResolver, template, View) {
+  define(['neo4j/webadmin/data/ItemUrlResolver', 'neo4j/webadmin/templates/databrowser/createRelationship', 'neo4j/webadmin/views/View', 'neo4j/webadmin/ui/FormHelper', 'lib/backbone'], function(ItemUrlResolver, template, View, FormHelper) {
     var CreateRelationshipDialog;
     return CreateRelationshipDialog = (function() {
       function CreateRelationshipDialog() {
         this.render = __bind(this.render, this);;
         this.position = __bind(this.position, this);;
+        this.saveFailed = __bind(this.saveFailed, this);;
+        this.saveSuccessful = __bind(this.saveSuccessful, this);;
         this.save = __bind(this.save, this);;
         this.pickedFromAvailableTypes = __bind(this.pickedFromAvailableTypes, this);;
         this.initialize = __bind(this.initialize, this);;        CreateRelationshipDialog.__super__.constructor.apply(this, arguments);
@@ -43,6 +45,7 @@
       };
       CreateRelationshipDialog.prototype.initialize = function(opts) {
         $("body").append(this.el);
+        this.formHelper = new FormHelper(this.el);
         this.baseElement = opts.baseElement;
         this.server = opts.server;
         this.dataModel = opts.dataModel;
@@ -70,23 +73,36 @@
         return $("#create-relationship-types").val("Types in use");
       };
       CreateRelationshipDialog.prototype.save = function() {
-        var failCallback, fromId, fromUrl, successCallback, toId, toUrl, type;
+        var type;
+        this.formHelper.removeAllErrors();
         type = $("#create-relationship-type").val();
-        fromId = this.urlResolver.extractNodeId($("#create-relationship-from").val());
-        toId = this.urlResolver.extractNodeId($("#create-relationship-to").val());
-        fromUrl = this.urlResolver.getNodeUrl(fromId);
-        toUrl = this.urlResolver.getNodeUrl(toId);
-        successCallback = __bind(function(relationship) {
-          var id;
-          id = this.urlResolver.extractRelationshipId(relationship.getSelf());
-          this.dataModel.setData(relationship, true, {
-            silent: true
-          });
-          this.dataModel.setQuery("rel:" + id, true);
-          return this.closeCallback();
-        }, this);
-        failCallback = __bind(function(error) {}, this);
-        return this.server.rel(fromUrl, type, toUrl).then(successCallback, failCallback);
+        return this.server.rel(this.getFromUrl(), type, this.getToUrl()).then(this.saveSuccessful, this.saveFailed);
+      };
+      CreateRelationshipDialog.prototype.saveSuccessful = function(relationship) {
+        var id;
+        id = this.urlResolver.extractRelationshipId(relationship.getSelf());
+        this.dataModel.setData(relationship, true, {
+          silent: true
+        });
+        this.dataModel.setQuery("rel:" + id, true);
+        return this.closeCallback();
+      };
+      CreateRelationshipDialog.prototype.saveFailed = function(error) {
+        if (error instanceof neo4j.exceptions.NotFoundException) {
+          if (error.url === this.getFromUrl()) {
+            return this.formHelper.addErrorTo("#create-relationship-from", "This node cannot be found.");
+          } else {
+            return this.formHelper.addErrorTo("#create-relationship-to", "This node cannot be found.");
+          }
+        } else {
+          return this.formHelper.addErrorTo("#create-relationship-from", "Unable to create relationship.");
+        }
+      };
+      CreateRelationshipDialog.prototype.getFromUrl = function() {
+        return this.urlResolver.getNodeUrl(this.urlResolver.extractNodeId($("#create-relationship-from").val()));
+      };
+      CreateRelationshipDialog.prototype.getToUrl = function() {
+        return this.urlResolver.getNodeUrl(this.urlResolver.extractNodeId($("#create-relationship-to").val()));
       };
       CreateRelationshipDialog.prototype.position = function() {
         var basePos, left, top;
