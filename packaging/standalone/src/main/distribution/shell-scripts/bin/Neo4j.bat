04954f8959f88712f1d17e2bef640c1bc43fcd5f
Chris Gioran <chris.gioran@neotechnology.com>
2011-04-07 22:50:33 +0300

    Re-added scripts that should be there. Fixed license headers

--- packaging/standalone/src/main/distribution/shell-scripts/bin/Neo4j.bat
+++ packaging/standalone/src/main/distribution/shell-scripts/bin/Neo4j.bat
@@ -1,6 +1,24 @@
+rem Copyright (c) 2002-2011 "Neo Technology,"
+rem Network Engine for Objects in Lund AB [http://neotechnology.com]
+rem
+rem This file is part of Neo4j.
+rem
+rem Neo4j is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License
+rem along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ 
 @echo off
 
-rem
 rem This script is the main controller for the server
 rem There are commands to install, uninstall, start, stop 
 rem and restart the service on windows and also run it as
@@ -20,7 +38,6 @@ rem
 rem bin\Neo4j.bat install
 rem
 rem to install Neo4j as a Windows Service.
-rem
 
 setlocal
 call "%~dp0"setenv.bat
--- packaging/standalone/src/main/distribution/shell-scripts/bin/Neo4jCoord.bat
+++ packaging/standalone/src/main/distribution/shell-scripts/bin/Neo4jCoord.bat
@@ -1,3 +1,21 @@
+rem Copyright (c) 2002-2011 "Neo Technology,"
+rem Network Engine for Objects in Lund AB [http://neotechnology.com]
+rem
+rem This file is part of Neo4j.
+rem
+rem Neo4j is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU Affero General Public License as
+rem published by the Free Software Foundation, either version 3 of the
+rem License, or (at your option) any later version.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU Affero General Public License for more details.
+rem
+rem You should have received a copy of the GNU Affero General Public License
+rem along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 @echo off
 
 rem
