d7a94ca3bfd104d6b46bb82832ee759b75f4f07a
Marko Rodriguez <okrammarko@gmail.com>
2011-04-12 03:59:30 -0600

    worked with Peter to make first round of generic execution of arbitrary Gremlin script. Currently only supports the returning of nodes/vertices.

--- gremlin-plugin/src/main/java/org/neo4j/server/plugin/gremlin/GremlinPlugin.java
+++ gremlin-plugin/src/main/java/org/neo4j/server/plugin/gremlin/GremlinPlugin.java
@@ -1,29 +1,32 @@
 package org.neo4j.server.plugin.gremlin;
 
-import java.util.ArrayList;
-
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-
+import com.tinkerpop.blueprints.pgm.Edge;
+import com.tinkerpop.blueprints.pgm.Graph;
+import com.tinkerpop.blueprints.pgm.Vertex;
+import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jEdge;
+import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jGraph;
+import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jVertex;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
 import org.neo4j.server.plugins.Description;
 import org.neo4j.server.plugins.Name;
 import org.neo4j.server.plugins.Parameter;
 import org.neo4j.server.plugins.PluginTarget;
 import org.neo4j.server.plugins.ServerPlugin;
 import org.neo4j.server.plugins.Source;
-import org.neo4j.server.rest.repr.EdgeRepresentation;
+import org.neo4j.server.rest.repr.DatabaseRepresentation;
 import org.neo4j.server.rest.repr.ListRepresentation;
-import org.neo4j.server.rest.repr.PipeRepresentation;
+import org.neo4j.server.rest.repr.NodeRepresentation;
+import org.neo4j.server.rest.repr.RelationshipRepresentation;
 import org.neo4j.server.rest.repr.Representation;
 import org.neo4j.server.rest.repr.RepresentationType;
-import org.neo4j.server.rest.repr.VertexRepresentation;
+import org.neo4j.server.rest.repr.ValueRepresentation;
 
-import com.tinkerpop.blueprints.pgm.Vertex;
-import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jGraph;
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.ArrayList;
+import java.util.List;
 
 /* This is a class that will represent a server side
  * gremlin plugin and will run through and return json
@@ -37,118 +40,47 @@ import com.tinkerpop.blueprints.pgm.impls.neo4j.Neo4jGraph;
  * and much, much more.
  */
 
-@Description( "A server side gremlin plugin for the neo4j REST server that will perform various node traversals and searches" )
-public class GremlinPlugin extends ServerPlugin
-{
+@Description("A server side gremlin plugin for the neo4j REST server that will perform various node traversals and searches")
+public class GremlinPlugin extends ServerPlugin {
     /**
      * This will give a plugin of type
-     * curl -d 'script=results.add(start)' http://localhost:7474/db/data/ext/GremlinPlugin/node/0/execute_from_node
-     * 
-     * @param startNode
+     * curl -d 'script=g.v(0).outE' http://localhost:7474/db/data/ext/GremlinPlugin/node/0/execute_from_node
+     *
      * @param script
      * @return
      */
-    @Name( "execute_from_node" )
-    @Description( "execute a Gremlin script with variables 'start' set to the start node 'g' set to the Neo4jGraph and 'results' containing a resulting vertex" )
-    @PluginTarget( Node.class )
-    public VertexRepresentation getVertex(
-            @Source Node startNode,
-            @Description( "The Gremlin script" ) @Parameter( name = "script", optional=true ) String script )
-    {
+    @Name("execute_script")
+    @Description("execute a Gremlin script with 'g' set to the Neo4jGraph and 'results' containing a resulting vertex")
+    @PluginTarget(GraphDatabaseService.class)
+    public Representation executeScript(@Source final GraphDatabaseService neo4j, @Description("The Gremlin script") @Parameter(name = "script", optional = true) final String script) {
         ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName( "gremlin" );
-        ArrayList results = new ArrayList();
-        Neo4jGraph graph = new Neo4jGraph( startNode.getGraphDatabase() );
-        engine.getBindings( ScriptContext.ENGINE_SCOPE ).put( "g",
-                graph );
-        engine.getBindings( ScriptContext.ENGINE_SCOPE ).put( "start",
-                graph.getVertex( startNode.getId() ));
-        engine.getBindings( ScriptContext.GLOBAL_SCOPE ).put( "results",
-                results );
-        try
-        {
-            engine.eval( script );
+        ScriptEngine engine = manager.getEngineByName("gremlin");
+        Neo4jGraph graph = new Neo4jGraph(neo4j);
+        engine.getBindings(ScriptContext.ENGINE_SCOPE).put("g", graph);
+
+        try {
+            Object result = engine.eval(script);
+            if (result instanceof Iterable) {
+                List<Representation> results = new ArrayList<Representation>();
+                for (Object r : (Iterable) result) {
+                    if (r instanceof Vertex) {
+                        results.add(new NodeRepresentation(((Neo4jVertex) r).getRawVertex()));
+                    } else if (r instanceof Edge) {
+                        results.add(new RelationshipRepresentation(((Neo4jEdge) r).getRawEdge()));
+                    } else if (r instanceof Graph) {
+                        results.add(new DatabaseRepresentation(graph.getRawGraph()));
+                    } else {
+                        results.add(ValueRepresentation.string(r.toString()));
+                    }
+                }
+                return new ListRepresentation(RepresentationType.NODE, results);
+            } else {
+                return ValueRepresentation.string(result.toString());
+            }
+        } catch (ScriptException e) {
+            return ValueRepresentation.string(e.getMessage());
         }
-        catch ( ScriptException e )
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return new VertexRepresentation((Vertex) results.get( 0 ));
     }
-    
-    
-    
-    /**
-     * This will give a plugin of type
-     * curl -d 'script=g.E' http://localhost:7474/db/data/ext/GremlinPlugin/graphDb/get_all_edges
-     * which should return an arrayList consisting of all the edges of the current graph
-     * @param startNode
-     * @param script
-     * @return
-     */
-    @Name( "get_all_edges" )
-    @Description( "execute a Gremlin script with variables 'start' set to the start node 'g' set to the Neo4jGraph and 'results' containing a resulting vertex" )
-    @PluginTarget( GraphDatabaseService.class )
-    public Representation getEdges(
-            @Description( "The Gremlin script" ) @Parameter( name = "script", optional=true ) String script ,@Source GraphDatabaseService graphDb)
-    {
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName( "gremlin" );
-        ArrayList<EdgeRepresentation> results = new ArrayList<EdgeRepresentation>();
-        Neo4jGraph graph = new Neo4jGraph( graphDb );
-        engine.getBindings( ScriptContext.ENGINE_SCOPE ).put( "g",
-                graph );
-        engine.getBindings( ScriptContext.GLOBAL_SCOPE ).put( "results",
-                results );
-        try
-        {
-            engine.eval( script );
-        }
-        catch ( ScriptException e )
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return new ListRepresentation( RepresentationType.RELATIONSHIP, results );
-    }
-    
-    
-    
-    
-    /**
-     * This will give a plugin of type
-     * curl -d 'script=g.V' http://localhost:7474/db/data/ext/GremlinPlugin/graphDb/get_all_vertexes
-     * which should return an arrayList consisting of all the vertexes of the current graph
-     * @param startNode
-     * @param script
-     * @return
-     */
-    @Name( "get_all_vertexes" )
-    @Description( "execute a Gremlin script with variables 'start' set to the start node 'g' set to the Neo4jGraph and 'results' containing the set of resulting vertices" )
-    @PluginTarget( GraphDatabaseService.class )
-    public Representation getVertices(
-            @Description( "The Gremlin script" ) @Parameter( name = "script", optional=true ) String script ,@Source GraphDatabaseService graphDb)
-    {
-        ScriptEngineManager manager = new ScriptEngineManager();
-        ScriptEngine engine = manager.getEngineByName( "gremlin" );
-        ArrayList<Vertex> results = new ArrayList<Vertex>();
-        Neo4jGraph graph = new Neo4jGraph( graphDb );
-        engine.getBindings( ScriptContext.ENGINE_SCOPE ).put( "g",
-                graph );
-        engine.getBindings( ScriptContext.GLOBAL_SCOPE ).put( "results",
-                results );
-        try
-        {
-            engine.eval( script );
-        }
-        catch ( ScriptException e )
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        Representation resultRep = new PipeRepresentation( results );
-        return resultRep;
-    }
-    
+
+
 }
--- gremlin-plugin/src/test/java/org/neo4j/server/plugin/gremlin/GremlinPluginTest.java
+++ gremlin-plugin/src/test/java/org/neo4j/server/plugin/gremlin/GremlinPluginTest.java
@@ -1,151 +1,99 @@
 package org.neo4j.server.plugin.gremlin;
 
 
-import static org.junit.Assert.*;
-
-import java.net.URI;
-
 import org.junit.After;
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.EmbeddedGraphDatabase;
-import org.neo4j.server.plugin.gremlin.GremlinPlugin;
-import org.neo4j.server.rest.repr.ListRepresentation;
+import org.neo4j.kernel.ImpermanentGraphDatabase;
 import org.neo4j.server.rest.repr.OutputFormat;
 import org.neo4j.server.rest.repr.Representation;
-import org.neo4j.server.rest.repr.RepresentationFormat;
 import org.neo4j.server.rest.repr.formats.JsonFormat;
+
+import java.net.URI;
+
+import static org.junit.Assert.assertNotNull;
 
 
 public class GremlinPluginTest {
 
-	private static EmbeddedGraphDatabase curGraphDBServiceObj = null;
-	private static GremlinPlugin curGremlinPluginObj = null;
-	private static String curGraphDBEmbeddedPath="target/db";
-	private static Node firstNode = null;
-	private static Node secondNode = null;
-	private static Node thirdNode = null;
-	private static OutputFormat json = null;	
+    private static ImpermanentGraphDatabase neo4j = null;
+    private static GremlinPlugin plugin = null;
+    private static String directory = "target/db";
+    private static Node firstNode = null;
+    private static Node secondNode = null;
+    private static Node thirdNode = null;
+    private static OutputFormat json = null;
 
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	    json = new OutputFormat(new JsonFormat(), new URI( "http://localhost/" ), null );
-		Transaction tx= null;
-		try
-		{
-			curGremlinPluginObj = new GremlinPlugin();
-			curGraphDBServiceObj=new EmbeddedGraphDatabase(curGraphDBEmbeddedPath);
-			tx=curGraphDBServiceObj.beginTx();
-			firstNode=curGraphDBServiceObj.createNode();
-			if (firstNode!=null)
-			{
-				firstNode.setProperty("name","firstNode");
-				firstNode.setProperty("x", 10.23245);
-				firstNode.setProperty("y", -112.346);
-				firstNode.setProperty("altitude",12.34456);
-			}
-		
-			secondNode=curGraphDBServiceObj.createNode();
-			if (secondNode!=null)
-			{
-				secondNode.setProperty("name","secondNode");
-				secondNode.setProperty("x", 1.23245);
-				secondNode.setProperty("y", -12.346);
-				secondNode.setProperty("altitude",100.34456);
-			}
-		
-		
-			thirdNode=curGraphDBServiceObj.createNode();
-			if (thirdNode!=null)
-			{
-				thirdNode.setProperty("name","thirdNode");
-				thirdNode.setProperty("x", 11.23245);
-				thirdNode.setProperty("y", -112.346);
-				thirdNode.setProperty("altitude",40.34456);
-			}
-			tx.success();
-		}
-		catch (Exception graphEx)
-		{
-			System.err.println("Caught a graph exception with message "+graphEx.getMessage());
-		}
-		finally
-		{
-			tx.finish();
-		}
-		
-	}
-	
-	
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        json = new OutputFormat(new JsonFormat(), new URI("http://localhost/"), null);
+        Transaction tx = null;
+        try {
+            plugin = new GremlinPlugin();
+            neo4j = new ImpermanentGraphDatabase(directory);
+            tx = neo4j.beginTx();
+            firstNode = neo4j.createNode();
+            if (firstNode != null) {
+                firstNode.setProperty("name", "firstNode");
+                firstNode.setProperty("x", 10.23245);
+                firstNode.setProperty("y", -112.346);
+                firstNode.setProperty("altitude", 12.34456);
+            }
 
-	@After
-	public void tearDown() throws Exception {
-	}
-	
-	
-	
-	/* Pass in a reference node and the current
-	 * embedded graph database that we're testing
-	 * and get back the list of vertices
-	 * 
-	*/
-	@Test
-	public void testGetVertices() {
-		String script="results.add(g.V)";
-		Transaction tx= null;
-		Representation curRepresentationObj = null;
-		try
-		{
-			tx=curGraphDBServiceObj.beginTx();
-			curRepresentationObj=curGremlinPluginObj.getVertices(script, curGraphDBServiceObj);
-			assertNotNull(curRepresentationObj);
-			tx.success();
-		}
-		catch (Throwable t) {
-			t.printStackTrace ();
-		}
-		finally
-		{
-			tx.finish();
-		}
-		System.out.println(json.format( curRepresentationObj ));
-	}
-	
-	
-	
-	
-	/* Pass in a reference node and the current
-	 * embedded graph database that we're testing
-	 * and get back the list of edges
-	 * 
-	*/
-	@Test
-	public void testGetEdges() {
-		String script="results.add(g.E)";
-		Transaction tx= null;
-		Representation curRepresentationObj = null;
-		try
-		{
-			tx=curGraphDBServiceObj.beginTx();
-			curRepresentationObj=(Representation)curGremlinPluginObj.getEdges(script, curGraphDBServiceObj);
-			assertNotNull(curRepresentationObj);
-			tx.success();
-			
-			System.err.println("GremlinPluginTest::testGetEdges the contents of the representation object="+curRepresentationObj.toString());
-		}
-		catch (Throwable t) {
-			t.printStackTrace ();
-		}
-		finally
-		{
-			tx.finish();
-		}
-		
-	}
+            secondNode = neo4j.createNode();
+            if (secondNode != null) {
+                secondNode.setProperty("name", "secondNode");
+                secondNode.setProperty("x", 1.23245);
+                secondNode.setProperty("y", -12.346);
+                secondNode.setProperty("altitude", 100.34456);
+            }
+
+
+            thirdNode = neo4j.createNode();
+            if (thirdNode != null) {
+                thirdNode.setProperty("name", "thirdNode");
+                thirdNode.setProperty("x", 11.23245);
+                thirdNode.setProperty("y", -112.346);
+                thirdNode.setProperty("altitude", 40.34456);
+            }
+            tx.success();
+        } catch (Exception graphEx) {
+            System.err.println("Caught a graph exception with message " + graphEx.getMessage());
+        } finally {
+            tx.finish();
+        }
+
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+    @Test
+    public void testExecuteScript() {
+        String script = "g.V";
+        Transaction tx = null;
+        Representation representation = null;
+        try {
+            tx = neo4j.beginTx();
+            representation = (Representation) plugin.executeScript(neo4j, script);
+            System.out.println(json.format(representation));
+            assertNotNull(representation);
+            tx.success();
+
+            System.err.println("GremlinPluginTest::executeScript the contents of the representation object=" + representation.toString());
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            tx.finish();
+        }
+
+    }
 
 }
