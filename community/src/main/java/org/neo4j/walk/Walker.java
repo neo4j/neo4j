24c8d9d3860795fdfb5f61d0cd703adbc709bc91
Tobias Ivarsson <tobias@thobe.org>
2011-04-14 10:33:32 +0200

    Updated Neo4j dependency to 1.3

--- community/src/main/java/org/neo4j/walk/Walker.java
+++ community/src/main/java/org/neo4j/walk/Walker.java
@@ -1,7 +1,10 @@
-/*
- * Copyright 2008 Network Engine for Objects in Lund AB [neotechnology.com]
+/**
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
@@ -12,7 +15,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.neo4j.walk;
 
