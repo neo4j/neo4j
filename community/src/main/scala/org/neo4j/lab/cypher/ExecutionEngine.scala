7fc179138b818901792736ffb1358a5ad42c6fd9
Andres Taylor <andres@neotechnology.com>
2011-04-26 18:07:16 +0200

    Added currentRow, and everything works except for relatedTo-queries.

--- community/src/main/scala/org/neo4j/lab/cypher/ExecutionEngine.scala
+++ community/src/main/scala/org/neo4j/lab/cypher/ExecutionEngine.scala
@@ -4,7 +4,6 @@ import commands._
 import pipes.{FilteringPipe, Pipe, FromPump}
 import org.neo4j.graphdb.{NotFoundException, Node, GraphDatabaseService}
 
-//type MapTransformer = (Map[String, Any]) => Map[String, Any]
 
 /**
  * Created by Andres Taylor
@@ -12,49 +11,39 @@ import org.neo4j.graphdb.{NotFoundException, Node, GraphDatabaseService}
  * Time: 09:44
  */
 class ExecutionEngine(val graph: GraphDatabaseService) {
+  type MapTransformer = (Map[String, Any]) => Map[String, Any]
+
   def makeMutable(immutableSources: Map[String, Pipe]): collection.mutable.Map[String, Pipe] = scala.collection.mutable.Map(immutableSources.toSeq: _*)
 
   def execute(query: Query): Projection = query match {
     case Query(select, from, where) => {
 
-      val sourcePumps: List[Pipe] = createSourcePumps(from)
-      val currentRow = new CurrentRow(sourcePumps)
-
-
+      var sourcePumps: List[Pipe] = createSourcePumps(from)
       val filters: Option[Pipe] = createFilters(where)
-      val transformers: Seq[(Map[String, Any]) => Map[String, Any]] = createProjectionTransformers(select)
 
-      val source = sourcePumps.reduceLeft(_ ++ _)
-
-      new Projection(source, transformers)
-    }
-  }
-
-  class CurrentRow(pipes: List[Pipe]) {
-    val immutableSources: Map[String, Pipe] = pipes.flatMap((pipe) => pipe.columnNames.map((columnName) => Map(columnName -> pipe))).reduceLeft(_ ++ _)
-    val sources: collection.mutable.Map[String, Pipe] = makeMutable(immutableSources)
-
-    def getPipeForColumns(neededColumns: List[String]): Option[Pipe] = {
-      var pipes = List[Pipe]()
-      var leftToDo = neededColumns
-
-      while (leftToDo.nonEmpty) {
-        val pipe = sources.get(leftToDo.head) match {
-          case None => return None
-          case Some(x) => {
-            x.columnNames.foreach(sources.remove(_))
-            leftToDo = leftToDo.filterNot(x.columnNames.contains(_))
-            x
-          }
-        }
-        pipes = pipes ++ List(pipe)
+      val currentRow = new CurrentRow()
+      while (sourcePumps.nonEmpty) {
+        sourcePumps = sourcePumps.flatten(currentRow.addPipe)
       }
 
-      Some(pipes.reduceLeft(_ ++ _))
+
+
+      if (filters.nonEmpty) {
+        currentRow.addPipe(filters.get)
+      }
+
+      //      sourcePumps.foreach(currentRow.addPipe)
+
+      val transformers: Seq[MapTransformer] = createProjectionTransformers(select)
+
+      //      val source = sourcePumps.reduceLeft(_ ++ _)
+
+      new Projection(currentRow.constructPipe(), transformers)
     }
   }
 
-  def createProjectionTransformers(select: Select): Seq[(Map[String, Any]) => Map[String, Any]] = select.selectItems.map((selectItem) => {
+
+  def createProjectionTransformers(select: Select): Seq[MapTransformer] = select.selectItems.map((selectItem) => {
     selectItem match {
       case NodePropertyOutput(nodeName, propName) => nodePropertyOutput(nodeName, propName) _
       case NodeOutput(nodeName) => nodeOutput(nodeName) _
@@ -82,21 +71,19 @@ class ExecutionEngine(val graph: GraphDatabaseService) {
   def nodeOutput(column: String)(m: Map[String, Any]): Map[String, Any] = Map(column -> m.getOrElse(column, throw new NotFoundException))
 
   def nodePropertyOutput(column: String, propName: String)(m: Map[String, Any]): Map[String, Any] = {
-    var node = m.getOrElse(column, throw new NotFoundException).asInstanceOf[Node]
+    val node = m.getOrElse(column, throw new NotFoundException).asInstanceOf[Node]
     Map(column + "." + propName -> node.getProperty(propName))
   }
 
 
   private def createSourcePumps(from: List[VariableAssignment]): List[Pipe] = {
-    val roots = from.map((va) => {
-      val f: () => Seq[Node] = va.fromitem match {
-        case NodeById(ids) => () => ids.map(graph.getNodeById)
-        //        case RelatedTo(column, relType, direction) =
-        //        case NodeByIndex(idx, value) => graph.index.forNodes("idx").get()
-      }
+    val roots = from.map((va) => va.fromitem match {
+      case NodeById(ids) => new FromPump(va.variable, ids.map(graph.getNodeById))
+//      case RelatedTo(column, relType, direction) =
+      //        case NodeByIndex(idx, value) => graph.index.forNodes("idx").get()
+    }
 
-      new FromPump(va.variable, f.apply()).asInstanceOf[Pipe]
-    })
+    )
 
     roots
   }
--- community/src/main/scala/org/neo4j/lab/cypher/pipes/RelatedToPipe.scala
+++ community/src/main/scala/org/neo4j/lab/cypher/pipes/RelatedToPipe.scala
@@ -1,10 +1,10 @@
 //package org.neo4j.lab.cypher.pipes
 //
 ///**
-// * Created by Andres Taylor
-// * Date: 4/20/11
-// * Time: 08:37
-// */
+//* Created by Andres Taylor
+//* Date: 4/20/11
+//* Time: 08:37
+//*/
 //
 //class RelatedToPipe(from: Pipe, filter: (Map[String, Any]) => Map[String, Any]) extends Pipe {
 //  def foreach[U](f: (Map[String, Any]) => U) {
