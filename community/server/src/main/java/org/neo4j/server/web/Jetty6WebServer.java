110c4a740c3b8a28e492f2969958fca46b00bede
jimwebber <jim@neotechnology.com>
2011-04-27 09:39:17 +0100

    Reinstated the Jersey shutdown immediately hook.

--- community/server/src/main/java/org/neo4j/server/web/Jetty6WebServer.java
+++ community/server/src/main/java/org/neo4j/server/web/Jetty6WebServer.java
@@ -85,6 +85,7 @@ public class Jetty6WebServer implements WebServer
     {
         try
         {
+            jetty.setStopAtShutdown( false );
             jetty.stop();
             jetty.join();
         }
