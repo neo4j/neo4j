c5c43a715f0e2706772195f5c3e28d53038dad7a
Anders Nawroth <anders@neotechnology.com>
2011-05-06 10:39:39 +0200

    Set correct build id.

--- enterprise/build/deploy.sh
+++ enterprise/build/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 # depends libxml-simple-perl
 
-tcrepo=http://builder.neo4j.org/guestAuth/repository/download/bt58/lastSuccessful
+tcrepo=http://builder.neo4j.org/guestAuth/repository/download/bt61/lastSuccessful
 mvnrepo=https://repo.neo4j.org/content/repositories/snapshots
 
 function work {
