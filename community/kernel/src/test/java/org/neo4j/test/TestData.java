c41670d57cf0a5793a079c6ffefa90584f880708
Tobias Ivarsson <tobias@thobe.org>
2011-05-28 02:36:15 +0200

    Fix for problem with trimming javadocs.

--- community/kernel/src/test/java/org/neo4j/test/TestData.java
+++ community/kernel/src/test/java/org/neo4j/test/TestData.java
@@ -146,13 +146,13 @@ public class TestData<T> implements MethodRule
             }
             // If there is no title, and the first line looks like a title,
             // take the first line as title
-            if ( title == null && lines[start + 1] == EMPTY )
+            if ( title == null && start < end && lines[start + 1] == EMPTY )
             {
                 title = lines[start].trim();
                 start += 2;
             }
             StringBuilder documentation = new StringBuilder();
-            for ( int i = start; i < end; i++ )
+            for ( int i = start; i <= end; i++ )
             {
                 documentation.append( lines[i] == EMPTY ? lines[i] : lines[i].substring( indent ) ).append( "\n" );
             }
