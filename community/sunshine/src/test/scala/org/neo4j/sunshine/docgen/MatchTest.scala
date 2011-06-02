f7d9d309c038ca11b4eb7f0b291db98537e72ed0
Andres Taylor <andres@neotechnology.com>
2011-06-02 20:09:30 +0200

    More documentation testing

--- community/sunshine/src/test/scala/org/neo4j/sunshine/docgen/MatchTest.scala
+++ community/sunshine/src/test/scala/org/neo4j/sunshine/docgen/MatchTest.scala
@@ -22,30 +22,67 @@ package org.neo4j.sunshine.docgen
 import org.junit.Test
 import org.junit.Assert._
 import org.neo4j.graphdb.Node
-
-
-class MatchTest extends DocumentingTestBase {
+class MatchTest extends DocumentingTestBase
+{
   def indexProps: List[String] = List("name")
 
-  def graphDescription: List[String] = List("A KNOWS B", "A KNOWS C", "D KNOWS A")
+  def graphDescription: List[String] = List("A KNOWS B", "A BLOCKS C", "D KNOWS A", "B KNOWS E", "C KNOWS E")
 
   def section: String = "MATCH"
 
-  @Test def allRelationships() {
+  @Test def allRelationships()
+  {
     testQuery(
-      title = "This is the simplest way of finding all related nodes",
-      query = """start n=(nodes,name,"A") match (n)--(x) return x""",
+      title = "Related nodes",
+      text = "The symbol -- means related to, without regard to type or direction.",
+      queryText = """start n=(%A%) match (n)--(x) return x""",
       returns = """All nodes related to A are returned""",
-      (p) => assertEquals(List(node("B"),node("C"), node("D")), p.columnAs[Node]("x") .toList)
+      (p) => assertEquals(List(node("B"), node("D"), node("C")), p.columnAs[Node]("x").toList)
     )
   }
 
-  @Test def allOutgoingRelationships() {
+  @Test def allOutgoingRelationships()
+  {
     testQuery(
-      title = "This is the simplest way of finding related nodes",
-      query = """start n=(nodes,name,"A") match (n)-->(x) return x""",
-      returns = """All nodes that A has outgoing relationships to""",
-      (p) => assertEquals(List(node("B"),node("C")), p.columnAs[Node]("x") .toList)
+      title = "Outgoing relationships",
+      text = "When the direction of a relationship is interesting, it is shown by using --> or <-- ",
+      queryText = """start n=(%A%) match (n)-->(x) return x""",
+      returns = """All nodes that A has outgoing relationships to.""",
+      (p) => assertEquals(List(node("B"), node("C")), p.columnAs[Node]("x").toList)
+    )
+  }
+
+  @Test def allOutgoingRelationships2()
+  {
+    testQuery(
+      title = "This is the simplest way of finding outgoing relationships.",
+      text = "",
+      queryText = """start n=(%A%) match (n)-[r]->() return r""",
+      returns = """All outgoing relationships from node A.""",
+      (p) => assertEquals(2, p.size)
+    )
+  }
+
+  @Test def relatedNodesByRelationshipType()
+  {
+    testQuery(
+      title = "Match by relationship type.",
+      text = "When you know the relationship type you want to match on, you can specify it by using a colon.",
+      queryText = """start n=(%A%) match (n)-[:BLOCKS]->(x) return x""",
+      returns = """All nodes that are BLOCKed by A.""",
+      (p) => assertEquals(List(node("C")), p.columnAs[Node]("x").toList)
+    )
+  }
+
+  @Test def relationshipsByType()
+  {
+    testQuery(
+      title = "Match by relationship type and use a variable.",
+      text = "If you both want to introduce a variable to hold the relationship, and specify the relationship type you want, " +
+        "just separate them with a coma.",
+      queryText = """start n=(%A%) match (n)-[r, :BLOCKS]->() return r""",
+      returns = """All BLOCK relationship going out from A.""",
+      (p) => assertEquals(1, p.size)
     )
   }
 
