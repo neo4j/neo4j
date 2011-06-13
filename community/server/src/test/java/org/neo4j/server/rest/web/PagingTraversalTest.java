8309a2fb2d9c4f5387f249ea2deb35e3911ebb6a
Jim Webber <jim@neotechnology.com>
2011-06-13 14:39:40 +0100

    Outermost in-proc tests for paged traversers

---
+++ community/server/src/test/java/org/neo4j/server/rest/web/PagingTraversalTest.java
@@ -0,0 +1,34 @@
+package org.neo4j.server.rest.web;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PagingTraversalTest
+{
+    @Test
+    public void shouldTraverseGivenRangeAndRespondWith200() {
+        fail("not implemented");
+    }
+    
+    @Test
+    public void shouldTraverseAsFarAsPossibleForAGivenRangeWhenAtEndOfTraversalAndReturn200() {
+        fail("not implemented");
+    }
+    
+    @Test
+    public void shouldRespondWith204IfTraversalResultsInZeroNodes() {
+        
+    }
+    
+    @Test
+    public void shouldFailWith400WhenTraversingRangeThatHasAlreadyBeenTraversed() {
+        fail("not implemented");
+    }
+    
+    @Test
+    public void shouldFailWith404WhenNoTraversalIsRegistered() {
+        fail("not implemented");
+    }
+    
+}
