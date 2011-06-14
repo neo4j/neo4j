614d8be6e6f7453c53431e1d3579202e3fa50262
Peter Neubauer <peter@neubauer.se>
2011-06-14 22:36:02 +0200

    refactored the first half of the graph algo functional tests to make up docs, and pulled it into the manual.

---
+++ community/server/src/functionaltest/java/org/neo4j/server/rest/AbstractRestFunctionalTestBase.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.server.rest;
+
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.metatest.TargetDirectoryTest;
+import org.neo4j.server.WrappingNeoServerBootstrapper;
+import org.neo4j.test.GraphDescription;
+import org.neo4j.test.GraphHolder;
+import org.neo4j.test.ImpermanentGraphDatabase;
+import org.neo4j.test.TargetDirectory;
+import org.neo4j.test.TestData;
+
+public class AbstractRestFunctionalTestBase implements GraphHolder
+{
+
+    private static ImpermanentGraphDatabase graphdb;
+    
+    public @Rule
+    TestData<Map<String, Node>> data = TestData.producedThrough( GraphDescription.createGraphFor(
+            this, true ) );
+    
+    public @Rule
+    TestData<DocsGenerator> gen = TestData.producedThrough( DocsGenerator.PRODUCER );
+    protected static WrappingNeoServerBootstrapper server;
+    @BeforeClass
+    public static void startDatabase()
+    {
+        graphdb = new ImpermanentGraphDatabase("target/db");
+        
+    }
+
+    @AfterClass
+    public static void stopDatabase()
+    {
+    }
+
+    @Override
+    public GraphDatabaseService graphdb()
+    {
+        return graphdb;
+    }
+    
+    @Before
+    public void startServer() {
+        server = new WrappingNeoServerBootstrapper(
+                graphdb );
+        server.start();
+    }
+    
+    @After
+    public void shutdownServer() {
+        server.stop();
+    }
+}
