7075e536e198f764746016583b6ecb4f50d28b17
Peter Neubauer <peter@neubauer.se>
2011-06-17 22:22:11 +0200

    more comments

--- community/shell/src/test/java/org/neo4j/shell/ShellTest.java
+++ community/shell/src/test/java/org/neo4j/shell/ShellTest.java
@@ -119,7 +119,7 @@ public class ShellTest
         Documenter doc = new Documenter("sample session", client);
         doc.add("pwd", "", "where are we?");
         doc.add("start n=(0) return n", "0", "send a cypher query");
-        doc.add("mkrel -c -d i -t LIKES --np \"{'app':'foobar'}\"", "", "make a relationship and create the end node");
+        doc.add("mkrel -c -d i -t LIKES --np \"{'app':'foobar'}\"", "", "make an incoming relationship of type LIKES, create the end node with the node properties specified.");
         doc.add("ls", "1", "where are we?");
         doc.add("cd 1", "", "change to the newly created node");
         doc.add("ls -avr", "LIKES", "list relationships, including relationshship id");
