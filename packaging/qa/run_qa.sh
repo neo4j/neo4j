bf59b1db04b0c1277a39f18a56efc140edbfd239
tomsulston <tom@sulston.net>
2011-07-05 22:58:59 +0200

    TS: Run QA tests through maven

--- packaging/qa/run_qa.sh
+++ packaging/qa/run_qa.sh
@@ -3,4 +3,4 @@ export NEO4J_PRODUCT=community
 export DOWNLOAD_PATH=http://builder.neo4j.org/guestAuth/repository/download/bt85/.lastSuccessful
 export NEO4J_VERSION=`curl ${DOWNLOAD_PATH}/reports/buildNumber.properties | cut -f2 -d=`
 export DOWNLOAD_LOCATION=${DOWNLOAD_PATH}/packages/neo4j-community-${NEO4J_VERSION}-unix.tar.gz
-rake
+mvn clean integration-test
