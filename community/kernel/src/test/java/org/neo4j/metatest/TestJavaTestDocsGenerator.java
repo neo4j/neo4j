3036b1618937bbbaa152a6dd853419d8754428ef
Peter Neubauer <peter@neubauer.se>
2011-09-08 15:08:30 +0200

    adding java docs generator

---
+++ community/kernel/src/test/java/org/neo4j/metatest/TestJavaTestDocsGenerator.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2002-2011 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.metatest;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.kernel.impl.annotations.Documented;
+import org.neo4j.test.GraphDescription;
+import org.neo4j.test.GraphDescription.Graph;
+import org.neo4j.test.GraphHolder;
+import org.neo4j.test.ImpermanentGraphDatabase;
+import org.neo4j.test.JavaTestDocsGenerator;
+import org.neo4j.test.TestData;
+
+public class TestJavaTestDocsGenerator implements GraphHolder
+{
+    private static GraphDatabaseService graphdb;
+    public @Rule
+    TestData<Map<String, Node>> data = TestData.producedThrough( GraphDescription.createGraphFor(
+            this, true ) );
+
+    public @Rule
+    TestData<JavaTestDocsGenerator> gen = TestData.producedThrough( JavaTestDocsGenerator.PRODUCER );
+
+    
+    /**
+     * Docs in some
+     * forms
+     */
+    @Documented
+    @Test
+    @Graph( "I know you" )
+    public void can_create_docs_from_method_name() throws Exception
+    {
+        data.get();
+        gen.get().setGraph( graphdb );
+        assertNotNull(data.get().get( "I" ));
+        gen.get().document();
+    }
+
+    @Override
+    public GraphDatabaseService graphdb()
+    {
+        return graphdb;
+    }
+    
+    @Before
+    public void setUp()
+    {
+        try
+        {
+            graphdb = new ImpermanentGraphDatabase();
+        }
+        catch ( IOException e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
