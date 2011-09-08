16b1f123179c290732806f2eb92837c02fac50b2
Peter Neubauer <peter@neubauer.se>
2011-09-08 15:08:54 +0200

    adding a first documented Java Junit test to the manual

--- community/embedded-examples/src/test/java/org/neo4j/examples/ShortDocumentationExamplesTest.java
+++ community/embedded-examples/src/test/java/org/neo4j/examples/ShortDocumentationExamplesTest.java
@@ -20,8 +20,13 @@ package org.neo4j.examples;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -33,25 +38,41 @@ import org.neo4j.graphdb.traversal.Evaluator;
 import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.kernel.Traversal;
 import org.neo4j.kernel.Uniqueness;
+import org.neo4j.kernel.impl.annotations.Documented;
 import org.neo4j.test.GraphDescription;
 import org.neo4j.test.GraphDescription.Graph;
 import org.neo4j.test.GraphHolder;
 import org.neo4j.test.ImpermanentGraphDatabase;
+import org.neo4j.test.JavaTestDocsGenerator;
 import org.neo4j.test.TestData;
+import org.neo4j.visualization.graphviz.AsciiDocStyle;
+import org.neo4j.visualization.graphviz.GraphvizWriter;
+import org.neo4j.walk.Walker;
 
-public class DescendantsTest implements GraphHolder
+public class ShortDocumentationExamplesTest implements GraphHolder
 {
+    public @Rule
+    TestData<JavaTestDocsGenerator> gen = TestData.producedThrough( JavaTestDocsGenerator.PRODUCER );
     public @Rule
     TestData<Map<String, Node>> data = TestData.producedThrough( GraphDescription.createGraphFor(
             this, true ) );
  
     /**
+     * Uniqueness of Paths in traversals.
      * 
      * This test is demonstrating the use of node uniqueness.
      * In order to return which all descendants 
      * of +Pet0+ which have the relation +owns+ to Principal1 (+Pet1+ and +Pet3+),
      * the Uniqueness of the traversal needs to be set to 
      * +NODE_PATH+ rather than the default +NODE_GLOBAL+.
+     * 
+     * [snippet,java]
+     * ----
+     * component=neo4j-examples
+     * source=org/neo4j/examples/DescendantsTest.java
+     * classifier=test-sources
+     * tag=traverser
+     * ----
      */
     @Graph({"Pet0 descendant Pet1",
         "Pet0 descendant Pet2",
@@ -60,9 +81,12 @@ public class DescendantsTest implements GraphHolder
         "Principal2 owns Pet2",
         "Principal1 owns Pet3"})
     @Test
+    @Documented
     public void testTraversal()
     {
         Node start = data.get().get( "Pet0" );
+        // START SNIPPET: traverser
+        gen.get().description( createGraphViz("descendants1") );
         final Node target = data.get().get( "Principal1" );
         TraversalDescription td = Traversal.description().uniqueness(Uniqueness.NODE_PATH ).evaluator( new Evaluator()
         {
@@ -76,20 +100,50 @@ public class DescendantsTest implements GraphHolder
             }
         } );
         
+        // END SNIPPET: traverser
         org.neo4j.graphdb.traversal.Traverser results = td.traverse( start );
         int count = 0;
         for(Path path : results)
         {
             count ++;
-            System.out.println(path.toString());
         }
         assertEquals(2, count);
     }
-    private static GraphDatabaseService db;
+    
+    public String createGraphViz( String name )
+    {
+        OutputStream out = new ByteArrayOutputStream();
+        GraphvizWriter writer = new GraphvizWriter(new AsciiDocStyle());
+        try
+        {
+            writer.emit( out, Walker.fullGraph( graphdb() ) );
+        }
+        catch ( IOException e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return  "_The Graph_\n\n[\"dot\", \""+name.replace( " ", "-" )+".svg\", \"neoviz\"]\n"+
+                "----\n" +
+                out.toString() +
+                "----\n";
+    }
+    
+    private static ImpermanentGraphDatabase db;
     @BeforeClass
     public static void init()
     {
         db = new ImpermanentGraphDatabase("target/descendants");
+    }
+    
+    @Before
+    public void setUp() {
+        db.cleanContent();
+        gen.get().setGraph( db );
+    }
+    @After
+    public void doc() {
+        gen.get().document("target/docs","examples");
     }
     @Override
     public GraphDatabaseService graphdb()
