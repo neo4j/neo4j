691851ba4e5b40e707356bd256eb6113169fff90
Jacob Hansson <jacob@voltvoodoo.com>
2011-09-10 05:29:47 +0200

    Ignored test for large requests to batch operation API.

--- community/server/src/functionaltest/java/org/neo4j/server/rest/BatchOperationFunctionalTest.java
+++ community/server/src/functionaltest/java/org/neo4j/server/rest/BatchOperationFunctionalTest.java
@@ -398,6 +398,7 @@ public class BatchOperationFunctionalTest
 
     }
     
+    @Ignore
     @Test
     public void testlargerequest() {    
         testlargerequest(1000);
@@ -416,6 +417,8 @@ public class BatchOperationFunctionalTest
             largereq.append("{\"method\":\"post\",\"to\":\"/node\", \"body\":{ \"age\":1 } },");
         }
         largereq.append("{\"method\":\"post\",\"to\":\"/node\", \"body\":{ \"age\":1 } }]");
+        
+        System.out.println(Runtime.getRuntime().totalMemory());
         
         Date d = new Date();
         JaxRsResponse response = RestRequest.req().post(functionalTestHelper.dataUri() + "batch", largereq.toString());
