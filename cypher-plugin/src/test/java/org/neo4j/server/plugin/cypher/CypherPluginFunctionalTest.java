60cfdd29fbf2aa6669fe4a73163968ee47007f18
Anders Nawroth <anders@neotechnology.com>
2011-09-13 12:42:33 +0200

    Removed unused imports.

--- cypher-plugin/src/test/java/org/neo4j/server/plugin/cypher/CypherPluginFunctionalTest.java
+++ cypher-plugin/src/test/java/org/neo4j/server/plugin/cypher/CypherPluginFunctionalTest.java
@@ -23,33 +23,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Collection;
 import java.util.Map;
 
 import javax.ws.rs.core.Response.Status;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
 import org.neo4j.kernel.impl.annotations.Documented;
-import org.neo4j.server.WrappingNeoServerBootstrapper;
 import org.neo4j.server.rest.AbstractRestFunctionalTestBase;
-import org.neo4j.server.rest.RESTDocsGenerator;
 import org.neo4j.server.rest.domain.JsonHelper;
-import org.neo4j.server.rest.web.PropertyValueException;
 import org.neo4j.test.GraphDescription;
 import org.neo4j.test.GraphDescription.Graph;
 import org.neo4j.test.GraphDescription.NODE;
 import org.neo4j.test.GraphDescription.PROP;
 import org.neo4j.test.GraphDescription.REL;
-import org.neo4j.test.GraphHolder;
-import org.neo4j.test.ImpermanentGraphDatabase;
-import org.neo4j.test.TestData;
 import org.neo4j.test.TestData.Title;
 
 public class CypherPluginFunctionalTest extends AbstractRestFunctionalTestBase
