efe6d04c313c21bd78d14e88d97f2c199590d87e
Anders Nawroth <anders@neotechnology.com>
2011-09-29 23:51:50 +0200

    Moved the path uniqueness example to its own class and expanded it.

--- community/embedded-examples/src/test/java/org/neo4j/examples/ShortDocumentationExamplesTest.java
+++ community/embedded-examples/src/test/java/org/neo4j/examples/ShortDocumentationExamplesTest.java
@@ -18,94 +18,17 @@
  */
 package org.neo4j.examples;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.neo4j.test.AsciiDocGenerator.createSourceSnippet;
 import static org.neo4j.visualization.asciidoc.AsciidocHelper.createCypherSnippet;
 import static org.neo4j.visualization.asciidoc.AsciidocHelper.createGraphViz;
 import static org.neo4j.visualization.asciidoc.AsciidocHelper.createOutputSnippet;
 
 import org.junit.Test;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Path;
-import org.neo4j.graphdb.traversal.Evaluation;
-import org.neo4j.graphdb.traversal.Evaluator;
-import org.neo4j.graphdb.traversal.TraversalDescription;
-import org.neo4j.graphdb.traversal.Traverser;
-import org.neo4j.kernel.Traversal;
-import org.neo4j.kernel.Uniqueness;
 import org.neo4j.kernel.impl.annotations.Documented;
 import org.neo4j.test.GraphDescription.Graph;
 
 public class ShortDocumentationExamplesTest extends AbstractJavaDocTestbase
 {
-    
-    /**
-     * Uniqueness of Paths in traversals.
-     * 
-     * This example is demonstrating the use of node uniqueness.
-     * Below an imaginary domain graph with Principals
-     * that own pets that are descendant to other pets.
-     * 
-     * @@graph
-     * 
-     * In order to return which all descendants 
-     * of +Pet0+ which have the relation +owns+ to Principal1 (+Pet1+ and +Pet3+),
-     * the Uniqueness of the traversal needs to be set to 
-     * +NODE_PATH+ rather than the default +NODE_GLOBAL+ so that nodes
-     * can be traversed more that once, and paths that have
-     * different nodes but can have some nodes in common (like the
-     * start and end node) can be returned.
-     * 
-     * @@traverser
-     * 
-     * This will return the following paths:
-     * 
-     * @@output
-     */
-    @Graph({"Pet0 descendant Pet1",
-        "Pet0 descendant Pet2",
-        "Pet0 descendant Pet3",
-        "Principal1 owns Pet1",
-        "Principal2 owns Pet2",
-        "Principal1 owns Pet3"})
-    @Test
-    @Documented
-    public void pathUniquenesExample()
-    {
-        Node start = data.get().get( "Pet0" );
-        gen.get().addSnippet( "graph", createGraphViz("descendants1", graphdb(), gen.get().getTitle()) );
-        String tagName = "traverser";
-        gen.get();
-        gen.get().addSnippet( tagName, createSourceSnippet(tagName, this.getClass()) );
-        // START SNIPPET: traverser
-        final Node target = data.get().get( "Principal1" );
-        TraversalDescription td = Traversal.description().uniqueness(Uniqueness.NODE_PATH ).evaluator( new Evaluator()
-        {
-            @Override
-            public Evaluation evaluate( Path path )
-            {
-                if(path.endNode().equals( target )) {
-                    return Evaluation.INCLUDE_AND_PRUNE;
-                }
-                return Evaluation.EXCLUDE_AND_CONTINUE;
-            }
-        } );
-        
-        Traverser results = td.traverse( start );
-        // END SNIPPET: traverser
-        String output = "";
-        int count = 0;
-        //we should get two paths back, through Pet1 and Pet3
-        for(Path path : results)
-        {       
-            count++;
-            output += path.toString()+"\n";
-        }
-        gen.get().addSnippet( "output", createOutputSnippet(output) );
-        assertEquals(2, count);
-    }
-    
     /**
      * This example shows a multi-relational
      * network between persons and things they like.
@@ -169,6 +92,4 @@ public class ShortDocumentationExamplesTest extends AbstractJavaDocTestbase
         assertTrue(result.contains( "Sara" ));
         assertTrue(result.contains( "Maria" ));
     }
-
-
 }
