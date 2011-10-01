0808edeafb4b00feedf9c7d748df532651c29422
Anders Nawroth <anders@neotechnology.com>
2011-10-01 18:19:30 +0200

    First working version of query result formatted as a table.

--- manual/src/bin/resources/queryresult/queryresult.py
+++ manual/src/bin/resources/queryresult/queryresult.py
@@ -2,34 +2,74 @@
 # -*- mode: Python; coding: utf-8 -*-
 
 import sys
+from xml.sax.saxutils import escape
+from xml.sax.saxutils import unescape
+
+def out_entries(entries,simpara):
+  buff = []
+  buff.append('<row>')
+  for entry in entries:
+    buff.append('<entry align="left" valign="top">')
+    if simpara:
+      buff.append('<simpara><literal>')
+    buff.append(entry.strip()) 
+    if simpara:
+      buff.append('</literal></simpara>')
+    buff.append('</entry>')
+  buff.append('</row>')
+  return buff
+
+def split_line(line):
+  line = unescape(line)
+  if not hasattr(split_line, 'positions'):
+    split_line.positions = []
+    left = 2
+    right = -1
+    for part in line[1:-2].split('|'):
+      right += len(part) + 1
+      split_line.positions.append( {'left':left, 'right':right} )
+      left = right + 3
+  strings = []
+  for position in split_line.positions:
+    strings.append(escape(line[position['left']:position['right']]))
+  return strings
+
+def table_header(headings, info_line):
+  column_count = len(headings)
+  if len(headings[0]) == 0:
+    sys.exit("The first table heading is empty.")
+  buff = []
+  buff.append('<informaltable tabstyle="queryresult table" frame="all" rowsep="1" colsep="1">')
+  buff.append('<tgroup cols="')
+  buff.append(str(column_count))
+  buff.append('">')
+  for i in range(1, column_count + 1):
+    buff.append('<colspec colname="col')
+    buff.append(str(i))
+    buff.append('"/>')
+  buff.append('<thead>')
+  buff.extend(out_entries(headings, 0))
+  buff.append('</thead><tfoot><row><entry align="left" valign="top" namest="col1" nameend="col')
+  buff.append(str(column_count))
+  buff.append('">')
+  buff.append(info_line)
+  buff.append('</entry></row></tfoot>')
+  return buff
+
 
 data = sys.stdin.readlines()
-lastline = data[:-1]
 data.pop(0)
-firstline = data.pop(0)[1:-2].split('|')
-column_count = len(firstline)
+first_line = split_line(data.pop(0))
 data.pop(0)
 
-sys.stdout.write( '<informaltable role="queryresult"><tgroup cols="')
-sys.stdout.write(str(column_count))
-sys.stdout.write('">')
-for i in range(1, column_count):
-  print '<colspec colname="col',i ,'"/>'
-print '<thead><row>'
-for entry in firstline:
-  print '<entry>', entry.strip(), '</entry>'
-print '</row></thead><tbody>'
-
+body = []
+body.append('<tbody>')
 for line in data:
   if line[0] == '|':
-    print '<row>'
-    entries = line[1:-2].split('|')
-    for entry in entries:
-      print '<entry>', entry.strip(), '</entry>'
-    print '</row>'
-print '</tbody>'
-print '<tfoot><row>'
-print '<entry namest="col1" nameend="col',column_count, '">', lastline, '</entry>'
-print '</row></tfoot>'
-print '</tgroup></informaltable>'
+    body.extend(out_entries(split_line(line), 1))
+body.append('</tbody>')
+
+sys.stdout.write(''.join(table_header(first_line, line)))
+sys.stdout.write(''.join(body))
+sys.stdout.write('</tgroup></informaltable>')
 
