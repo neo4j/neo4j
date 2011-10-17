58f4e8ec6dcc2b0d995dcdfeac4fe96d748c5764
Chris Gioran <chris.gioran@neotechnology.com>
2011-10-17 21:10:02 +0300

    Changed the TestUpgradeOneDotFourToFive to check for the log version exception as it was intended to do - the whole stack exceptions are checked elsewhere. Moved the old, unclean db to a new resource directory to match the new package of the test

--- community/kernel/src/test/java/org/neo4j/kernel/impl/transaction/xaframework/TestUpgradeOneDotFourToFive.java
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/transaction/xaframework/TestUpgradeOneDotFourToFive.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.kernel.impl.nioneo.store;
+package org.neo4j.kernel.impl.transaction.xaframework;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -25,35 +25,46 @@ import static org.neo4j.kernel.impl.util.FileUtils.copyRecursively;
 import static org.neo4j.kernel.impl.util.FileUtils.deleteRecursively;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.neo4j.graphdb.TransactionFailureException;
-import org.neo4j.kernel.EmbeddedGraphDatabase;
-import org.neo4j.kernel.impl.transaction.xaframework.IllegalLogFormatException;
+import org.neo4j.kernel.CommonFactories;
 
 public class TestUpgradeOneDotFourToFive
 {
     private static final File PATH = new File( "target/test-data/upgrade-1.4-5" );
-    
+
     @BeforeClass
     public static void doBefore() throws Exception
     {
         deleteRecursively( PATH );
     }
-    
+
     @Test
     public void cannotRecoverNoncleanShutdownDbWithOlderLogFormat() throws Exception
     {
         copyRecursively( new File( TestUpgradeOneDotFourToFive.class.getResource( "non-clean-1.4.2-db/neostore" ).getFile() ).getParentFile(), PATH );
         try
         {
-            new EmbeddedGraphDatabase( PATH.getAbsolutePath() );
+            Map<Object, Object> config = new HashMap<Object, Object>();
+            config.put( "store_dir", PATH.getAbsolutePath() );
+            config.put( LogBufferFactory.class,
+                    CommonFactories.defaultLogBufferFactory() );
+            XaLogicalLog log = new XaLogicalLog( resourceFile(), null, null,
+                    null, config );
+            log.open();
             fail( "Shouldn't be able to start" );
         }
-        catch ( TransactionFailureException e )
+        catch ( Exception e )
         {
-            assertTrue( IllegalLogFormatException.class.isAssignableFrom( e.getCause().getCause().getClass() ) );
+            assertTrue( IllegalLogFormatException.class.isAssignableFrom( e.getClass() ) );
         }
+    }
+
+    protected String resourceFile()
+    {
+        return new File( PATH, "nioneo_logical.log" ).getAbsolutePath();
     }
 }
