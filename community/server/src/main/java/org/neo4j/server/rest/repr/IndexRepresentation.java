7a91e3dcc15d87caf5b7fe1835cc31938e6bb481
Jacob Hansson <jacob@voltvoodoo.com>
2011-10-19 11:36:58 +0200

    Fix for server not supporting indexes with spaces in the names.

--- community/server/src/main/java/org/neo4j/server/rest/repr/IndexRepresentation.java
+++ community/server/src/main/java/org/neo4j/server/rest/repr/IndexRepresentation.java
@@ -58,7 +58,7 @@ public abstract class IndexRepresentation extends MappingRepresentation implemen
 
     protected String path()
     {
-        return "index/" + propertyContainerType() + "/" + name + "/";
+        return "index/" + propertyContainerType() + "/" + URIHelper.encode(name) + "/";
     }
 
     protected abstract String propertyContainerType();
