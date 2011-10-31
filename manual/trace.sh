20131d7b2227658c4f4d31a569d217d847ba3493
Peter Neubauer <peter@neubauer.se>
2011-10-31 11:00:03 -0700

    some exampel in the script

--- manual/trace.sh
+++ manual/trace.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 
+#to search special characters, escape them.
+#./trace.sh -t '\[\[cypher'
+
+#output:
+#target/docs/neo4j-cypher-docs-jar/dev/index.txt:1:[[cypher-query-lang]]
+#target/docs/neo4j-cypher-docs-jar/dev/index.txt:52:[[cypher-parameters]]
+#target/docs/neo4j-cypher-docs-jar/dev/index.txt:114:[[cypher-identifiers]]
+#target/docs/neo4j-cypher-docs-jar/dev/ql/cookbook/index.txt:1:[[cypher-cookbook]]
+#target/docs/neo4j-cypher-plugin-docs-jar/dev/plugins/cypher/index.txt:1:[[cypher-plugin]]
+
 if [[ ! -d "target/docs" ]] ; then
     mvn generate-resources
 fi
