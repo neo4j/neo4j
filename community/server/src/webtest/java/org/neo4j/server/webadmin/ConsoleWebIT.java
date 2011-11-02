c8667608db8a3de3b501c5fca48b5a029581fb7d
Jacob Hansson <jacob@voltvoodoo.com>
2011-11-02 10:28:03 +0100

    Renamed console test to adhere to other webdriver integration tests.

--- community/server/src/webtest/java/org/neo4j/server/webadmin/ConsoleWebIT.java
+++ community/server/src/webtest/java/org/neo4j/server/webadmin/ConsoleWebIT.java
@@ -25,7 +25,7 @@ import org.neo4j.kernel.impl.annotations.Documented;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
-public class ConsoleWebITestIgnored extends AbstractWebadminTest {
+public class ConsoleWebIT extends AbstractWebadminTest {
 
     //
     // CYPHER
