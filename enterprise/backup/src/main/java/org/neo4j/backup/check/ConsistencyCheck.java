cd6dbb2f60e8638505d6d8934c23023b740cfe25
Chris Gioran <chris.gioran@neotechnology.com>
2011-11-02 17:26:25 +0200

    Fixed bug in length of dynamic record 0

--- enterprise/backup/src/main/java/org/neo4j/backup/check/ConsistencyCheck.java
+++ enterprise/backup/src/main/java/org/neo4j/backup/check/ConsistencyCheck.java
@@ -331,10 +331,15 @@ public class ConsistencyCheck extends RecordStore.Processor implements Runnable
             fail |= inconsistent( record,
                     "next record set but length less than maximum" );
         }
-        if ( record.getLength() > store.getRecordSize()
+        if ( record.getId() != 0
+             && record.getLength() > store.getRecordSize()
                                   - store.getRecordHeaderSize() )
         {
-            // The length must always be less than or equal to max
+            /*
+             *  The length must always be less than or equal to max,
+             *  except for the first dynamic record in a store, which
+             *  does not conform to the usual format
+             */
             fail |= inconsistent( record,
                     "length larger than maximum for store" );
         }
