4b73100e178ad6e1c584e49fb9367bba7a82066d
Anders Nawroth <anders@neotechnology.com>
2011-12-14 17:37:35 +0100

    Refactoring for cleaner file structure.

--- manual/src/bin/resources/dot/dot.sh
+++ manual/src/bin/resources/dot/dot.sh
@@ -3,7 +3,11 @@
 nodefontsize=10
 edgefontsize=$nodefontsize
 
-if [[ "$2" == "meta" ]]
+fontpath=$1
+targetimage=$2
+colorset=$3
+
+if [[ "$colorset" == "meta" ]]
 then
   nodefillcolor=slategray1
   nodehighlight=darkseagreen1
@@ -11,7 +15,7 @@ then
   edgehighlight=mediumblue
   boxcolor=black
   nodeshape=box
-elif [[ "$2" == "neoviz" ]]
+elif [[ "$colorset" == "neoviz" ]]
 then
   nodefillcolor=ivory1
   nodehighlight=khaki1
@@ -30,7 +34,7 @@ else
   nodeshape=box
 fi
 
-maxsize="graph [size=\"7.0,9.0\"]"
+graphsettings="graph [size=\"7.0,9.0\" fontpath=\"${fontpath}\"]"
 
 nodestyle=filled,rounded
 #nodeheight=0.37
@@ -40,11 +44,6 @@ textnode=shape=plaintext,style=diagonals,height=0.2,margin=0.0,0.0
 arrowhead=vee
 arrowsize=0.75
 
-graphfont=Sans
-nodefont=$graphfont
-edgefont=$graphfont
-
-
 indata=$(cat);
 indata=${indata//NODEHIGHLIGHT/$nodehighlight}
 indata=${indata//NODE2HIGHLIGHT/$nodehighlight2}
@@ -52,10 +51,15 @@ indata=${indata//EDGEHIGHLIGHT/$edgehighlight}
 indata=${indata//BOXCOLOR/$boxcolor}
 indata=${indata//TEXTNODE/$textnode}
 
-svgfile=$1
+svgfile=$targetimage
 pngfile="${svgfile}.png"
 
-prepend="digraph g{ $maxsize\
+graphfont="FreeSans"
+nodefont=$graphfont
+edgefont=$graphfont
+
+
+prepend="digraph g{ $graphsettings\
   node [shape=\"$nodeshape\" fillcolor=\"$nodefillcolor\" style=\"$nodestyle\" \
     fontsize=$nodefontsize fontname=\"$nodefont\"]
   edge [arrowhead=\"$arrowhead\" arrowtail=\"$arrowhead\" arrowsize=$arrowsize fontsize=$edgefontsize fontname=\"$edgefont\"] \
