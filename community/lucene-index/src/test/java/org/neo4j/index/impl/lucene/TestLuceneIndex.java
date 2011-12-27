d3c93a78ca124946c810159af80aca55483f5f13
Mattias Persson <mattias@neotechnology.com>
2011-12-27 13:50:49 +0100

    Prevents reserved keys to be added or removed

--- community/lucene-index/src/test/java/org/neo4j/index/impl/lucene/TestLuceneIndex.java
+++ community/lucene-index/src/test/java/org/neo4j/index/impl/lucene/TestLuceneIndex.java
@@ -1276,13 +1276,22 @@ public class TestLuceneIndex extends AbstractLuceneIndexTest
     }
 
     @Test
-    public void notAbleToIndexWithNullKey() throws Exception
+    public void notAbleToIndexWithForbiddenKey() throws Exception
     {
         Index<Node> index = graphDb.index().forNodes( "check-for-null" );
         Node node = graphDb.createNode();
         try
         {
             index.add( node, null, "not allowed" );
+            fail( "Shouldn't be able to index something with null key" );
+        }
+        catch ( IllegalArgumentException e )
+        { // OK
+        }
+
+        try
+        {
+            index.add( node, "_id_", "not allowed" );
             fail( "Shouldn't be able to index something with null key" );
         }
         catch ( IllegalArgumentException e )
@@ -1373,5 +1382,30 @@ public class TestLuceneIndex extends AbstractLuceneIndexTest
         assertContains( index.query( "name", "\"maTTias perSson\"" ), node );
         restartTx();
         assertContains( index.query( "name", "\"maTTias perSson\"" ), node );
+    }
+    
+    @Test
+    public void notAbleToRemoveWithForbiddenKey() throws Exception
+    {
+        Index<Node> index = nodeIndex( "broken", LuceneIndexImplementation.EXACT_CONFIG );
+        Node node = graphDb.createNode();
+        index.add( node, "name", "Mattias" );
+        restartTx();
+        try
+        {
+            index.remove( node, null );
+            fail( "Shouldn't be able to" );
+        }
+        catch ( IllegalArgumentException e )
+        {   // OK
+        }
+        try
+        {
+            index.remove( node, "_id_" );
+            fail( "Shouldn't be able to" );
+        }
+        catch ( IllegalArgumentException e )
+        {   // OK
+        }
     }
 }
