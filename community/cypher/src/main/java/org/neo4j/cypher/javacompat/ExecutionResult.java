866081567a0a656ce1b420caab7bc490d6e244da
Andreas Kollegger <akollegger@gmail.com>
2011-12-29 12:19:27 -0500

    added basic javadoc to explain use of ExecutionResult for java devs

--- community/cypher/src/main/java/org/neo4j/cypher/javacompat/ExecutionResult.java
+++ community/cypher/src/main/java/org/neo4j/cypher/javacompat/ExecutionResult.java
@@ -25,22 +25,53 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Holds Cypher query result sets.
+ * Holds Cypher query result sets, in tabular form. Each row of the result is a map
+ * of column name to result object. Each column name correlates directly
+ * with the terms used in the "return" clause of the Cypher query.
+ * The result objects could be {@link org.neo4j.graphdb.Node Nodes},
+ * {@link org.neo4j.graphdb.Relationship Relationships} or java primitives.
+ *
+ *
+ * Either iterate directly over the ExecutionResult to retrieve each row of the result
+ * set, or use <code>columnAs()</code> to access a single column with result objects
+ * cast to a type.
+ *
  */
 public class ExecutionResult implements Iterable<Map<String,Object>>
 {
     private org.neo4j.cypher.ExecutionResult inner;
 
+    /**
+     * Constructor used by the Cypher framework. End-users should not
+     * create an ExecutionResult directly, but instead use the result
+     * returned from calling {@link ExecutionEngine#execute(org.neo4j.cypher.commands.Query)}.
+     *
+     * @param projection
+     */
     public ExecutionResult( org.neo4j.cypher.ExecutionResult projection )
     {
         inner = projection;
     }
 
+    /**
+     * Provides result objects from a single column of the result set.
+     *
+     * @param n exact name of the column, as it appeared in the original query
+     * @param <T> desired type cast for the result objects
+     * @return an iterator of the result objects, possibly empty
+     * @throws ClassCastException when the result object can not be cast to the requested type
+     * @throws org.neo4j.graphdb.NotFoundException when the column name does not appear in the original query
+     */
     public <T> Iterator<T> columnAs( String n )
     {
         return inner.javaColumnAs( n );
     }
 
+    /**
+     * The exact names used to represent each column in the result set.
+     *
+     * @return List of the column names.
+     */
     public List<String> columns()
     {
         return inner.javaColumns();
