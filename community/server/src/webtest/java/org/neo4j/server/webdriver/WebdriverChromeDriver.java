8c210c436ad17691f8d28729a5641a754f15b0bc
Mattias Persson <mattias@neotechnology.com>
2012-01-04 13:55:25 +0100

    License header and imports

--- community/server/src/webtest/java/org/neo4j/server/webdriver/WebdriverChromeDriver.java
+++ community/server/src/webtest/java/org/neo4j/server/webdriver/WebdriverChromeDriver.java
@@ -25,7 +25,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
