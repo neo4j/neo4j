dabafd2ce6789a0769eb2b685634e0e510ea31a1
Anders Nawroth <anders@neotechnology.com>
2012-01-19 13:52:59 +0100

    The check script now prefers the mvn3 command if it exists.

--- manual/check.sh
+++ manual/check.sh
@@ -1,2 +1,10 @@
-mvn clean compile -Dcheck "$@"
+mvncommand="mvn"
+
+# prefer the mvn3 command if it exists
+hash mvn3 &> /dev/null
+if [ $? -eq 0 ]; then
+  mvncommand="mvn3"
+fi
+
+$mvncommand clean compile -Dcheck "$@"
 
