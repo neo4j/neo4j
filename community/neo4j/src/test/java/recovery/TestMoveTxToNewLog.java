43d26ac86ee4a077d3f97814d930c7748a0235fe
Johan Svensson <johan@p150hmx.(none)>
2012-01-26 16:27:18 +0100

    Fixed problem with logical log rotation creating new log that had transactions in wrong order. (cherry picked from commit c346970d921a167cadf97d4ea1e976fd2672adcc)

--- community/neo4j/src/test/java/recovery/TestMoveTxToNewLog.java
+++ community/neo4j/src/test/java/recovery/TestMoveTxToNewLog.java
@@ -28,7 +28,6 @@ import java.util.concurrent.CountDownLatch;
 
 import javax.transaction.xa.Xid;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.AbstractGraphDatabase;
@@ -59,7 +58,6 @@ import org.neo4j.test.subprocess.KillSubProcess;
  * 
  * @author Mattias Persson
  */
-@Ignore( "Until there's a proper fix for it" )
 public class TestMoveTxToNewLog extends AbstractSubProcessTestBase
 {
     private static final String NAME = "name";
