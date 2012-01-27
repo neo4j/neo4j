58aee8a7130ed058ad3a4f063d1aef22f3a5a23c
Mattias Persson <mattias@neotechnology.com>
2012-01-27 17:22:46 +0100

    Exposed kernel panic generator (cherry picked from commit 692d80e348b7a18f0408f201ba9a75757c4d1092)

--- community/kernel/src/main/java/org/neo4j/kernel/Config.java
+++ community/kernel/src/main/java/org/neo4j/kernel/Config.java
@@ -249,6 +249,7 @@ public class Config implements DiagnosticsProvider
     private final IdGeneratorFactory idGeneratorFactory;
     private final TxIdGenerator txIdGenerator;
     private final DiagnosticsManager diagnostics;
+    private final KernelPanicEventGenerator kpe;
 
     Config( AbstractGraphDatabase graphDb, StoreId storeId,
             Map<String, String> inputParams, KernelPanicEventGenerator kpe,
@@ -259,6 +260,7 @@ public class Config implements DiagnosticsProvider
             LastCommittedTxIdSetter lastCommittedTxIdSetter,
             FileSystemAbstraction fileSystem )
     {
+        this.kpe = kpe;
         this.storeDir = graphDb.getStoreDir();
         this.inputParams = inputParams;
         this.ephemeral = graphDb.isEphemeral();
@@ -515,5 +517,10 @@ public class Config implements DiagnosticsProvider
                 }
             }, true );
         }
+    }
+
+    public KernelPanicEventGenerator getKernelPanicGenerator()
+    {
+        return kpe;
     }
 }
--- community/kernel/src/main/java/org/neo4j/kernel/impl/util/DebugUtil.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/util/DebugUtil.java
@@ -19,6 +19,9 @@
  */
 package org.neo4j.kernel.impl.util;
 
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.getAllStackTraces;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -58,5 +61,27 @@ public class DebugUtil
         {
             throw new RuntimeException( "Can't happen", e );
         }
+    }
+
+    public static boolean currentStackTraceContains( String className, String method )
+    {
+        try
+        {
+            return currentStackTraceContains( Class.forName( className ), method );
+        }
+        catch ( ClassNotFoundException e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+    
+    public static boolean currentStackTraceContains( Class<?> cls, String method )
+    {
+        for ( StackTraceElement stack : getAllStackTraces().get( currentThread() ) )
+        {
+            if ( stack.getClassName().equals( cls.getName() ) && stack.getMethodName().equals( method ) )
+                return true;
+        }
+        return false;
     }
 }
