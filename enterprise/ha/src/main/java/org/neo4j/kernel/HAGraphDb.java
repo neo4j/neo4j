e466372649d7b04094d748a1e09aa643bc8a9a29
Chris Gioran <chris.gioran@neotechnology.com>
2012-02-01 19:14:08 +0200

    Added a restart() method on the Broker interface, refactored implementations a bit to make it work with ZooKeeperBroker Use Broker#restart() to rebuild internal state when zoo session becomes expired - solves a bug where stale references to a NO_MASTER master client instance would remain around indefinitely.
    Conflicts:
    
    	ha/src/main/java/org/neo4j/kernel/ha/zookeeper/ZooKeeperBroker.java
    	ha/src/test/java/org/neo4j/kernel/ha/FakeMasterBroker.java

--- enterprise/ha/src/main/java/org/neo4j/kernel/HAGraphDb.java
+++ enterprise/ha/src/main/java/org/neo4j/kernel/HAGraphDb.java
@@ -99,7 +99,7 @@ public class HAGraphDb extends AbstractGraphDatabase
 
     private final Map<String, String> config;
     private final BrokerFactory brokerFactory;
-    private volatile Broker broker;
+    private final Broker broker;
     private ClusterClient clusterClient;
     private volatile EmbeddedGraphDbImpl localGraph;
     private final int machineId;
@@ -630,9 +630,8 @@ public class HAGraphDb extends AbstractGraphDatabase
     {
         if ( broker != null )
         {
-            broker.shutdown();
+            broker.restart();
         }
-        this.broker = brokerFactory.create( this, config );
         newMaster( e );
     }
 
