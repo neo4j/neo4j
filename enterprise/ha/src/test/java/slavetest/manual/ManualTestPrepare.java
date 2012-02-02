710d67865234bd2bdcf11d48e4e42940f1f5c75c
Mattias Persson <mattias@neotechnology.com>
2012-02-02 08:58:25 +0100

    Some cleanup and better exception message (cherry picked from commit b826c2df346e086398168f29196b6e3144659661)
    Conflicts:
    
    	ha/src/main/java/org/neo4j/kernel/ha/MasterImpl.java

--- enterprise/ha/src/test/java/slavetest/manual/ManualTestPrepare.java
+++ enterprise/ha/src/test/java/slavetest/manual/ManualTestPrepare.java
@@ -29,6 +29,5 @@ public class ManualTestPrepare
         FileUtils.deleteDirectory( ManualTest1.PATH.getParentFile() );
         new EmbeddedGraphDatabase( ManualTest1.PATH.getAbsolutePath() ).shutdown();
         FileUtils.copyDirectory( ManualTest1.PATH, ManualTest2.PATH );
-        FileUtils.deleteDirectory( ManualZooKeepers.PATH );
     }
 }
