d739a33bc66073590c33473e7db6ae190b4658d7
simpsonjulian <simpsonjulian@gmail.com>
2012-02-22 12:12:21 +0000

    Build the repo from the correct place

--- packaging/installer-debian/repo.sh
+++ packaging/installer-debian/repo.sh
@@ -2,6 +2,6 @@
 script_dir=$(dirname $0)
 dest="${script_dir}/target/repo"
 mkdir -p $dest/binary
-cp ${script_dir}/target/classes/*.deb $dest
+cp ${script_dir}/target/classes/*.deb $dest/binary
 ( cd $dest && dpkg-scanpackages binary /dev/null | gzip -9c > binary/Packages.gz )
 s3cmd put --recursive $dest s3://debian.us-east.neo4j.org
