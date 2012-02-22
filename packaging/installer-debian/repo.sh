52249a87d899ce63d8c4041a529582cd1e111caf
simpsonjulian <simpsonjulian@gmail.com>
2012-02-22 11:10:28 +0000

    Fetch debs from the classes dir

--- packaging/installer-debian/repo.sh
+++ packaging/installer-debian/repo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 dest='target/repo'
 mkdir -p $dest/binary
-cp target/*.deb $dest
+cp target/classes/*.deb $dest
 ( cd $dest && dpkg-scanpackages binary /dev/null | gzip -9c > binary/Packages.gz )
 # rsync to s3 bucket
 s3sync $dst s3://http://debian.us-east.neo4j.org.s3-website-us-east-1.amazonaws.com/
