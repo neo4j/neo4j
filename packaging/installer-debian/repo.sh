78ae04a6929454986b0d43d12b6d8429aeeb38de
simpsonjulian <simpsonjulian@gmail.com>
2012-02-22 11:52:47 +0000

    Now with accurate bucket name

--- packaging/installer-debian/repo.sh
+++ packaging/installer-debian/repo.sh
@@ -4,4 +4,4 @@ dest="${script_dir}/target/repo"
 mkdir -p $dest/binary
 cp ${script_dir}/target/classes/*.deb $dest
 ( cd $dest && dpkg-scanpackages binary /dev/null | gzip -9c > binary/Packages.gz )
-s3sync $dest s3://http://debian.us-east.neo4j.org.s3-website-us-east-1.amazonaws.com/
+s3cmd put --recursive $dest s3://debian.us-east.neo4j.org
