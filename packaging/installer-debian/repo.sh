a8b147078a673cd30e0f1a99b4f198b0ab7dc3d2
simpsonjulian <simpsonjulian@gmail.com>
2012-02-22 11:35:02 +0000

    Now the script is path-safe

--- packaging/installer-debian/repo.sh
+++ packaging/installer-debian/repo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-dest='target/repo'
+script_dir=$(dirname $0)
+dest="${script_dir}/target/repo"
 mkdir -p $dest/binary
-cp target/classes/*.deb $dest
+cp ${script_dir}/target/classes/*.deb $dest
 ( cd $dest && dpkg-scanpackages binary /dev/null | gzip -9c > binary/Packages.gz )
-# rsync to s3 bucket
-s3sync $dst s3://http://debian.us-east.neo4j.org.s3-website-us-east-1.amazonaws.com/
+s3sync $dest s3://http://debian.us-east.neo4j.org.s3-website-us-east-1.amazonaws.com/
