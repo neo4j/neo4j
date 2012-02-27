8bd8aa8816dcae749c8873a1e813f6f6fb327a92
Peter Neubauer <peter@neubauer.se>
2012-02-27 22:05:25 +0100

    fixing manual build

--- community/server/src/functionaltest/java/org/neo4j/server/rest/security/SecurityRulesFunctionalTest.java
+++ community/server/src/functionaltest/java/org/neo4j/server/rest/security/SecurityRulesFunctionalTest.java
@@ -180,12 +180,10 @@ public class SecurityRulesFunctionalTest extends ExclusiveServerTestBase
                                       PermanentlyFailingSecurityRuleWithComplexWildcardPath.class.getCanonicalName())
                               .build();
         server.start();
+        gen.get().setSection("ops");
         functionalTestHelper = new FunctionalTestHelper(server);
-
-        ClientResponse clientResponse = Client.create().resource(
-                trimTrailingSlash(functionalTestHelper.baseUri()) + mountPoint + "/more/stuff").accept(
-                MediaType.TEXT_PLAIN).get(ClientResponse.class);
-
+        JaxRsResponse clientResponse = gen.get().expectedStatus(401).expectedType( MediaType.TEXT_PLAIN_TYPE ).expectedHeader(
+                "WWW-Authenticate").get(trimTrailingSlash(functionalTestHelper.baseUri()) + mountPoint + "/more/stuff").response();
         assertEquals(401, clientResponse.getStatus());
     }
 
