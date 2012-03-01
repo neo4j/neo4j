5da94a3be90801ff80ff38f73fb682f6c87d3c3d
Peter Neubauer <peter@neubauer.se>
2012-03-01 14:51:10 +0100

    adjusting clear graph

--- gremlin-plugin/src/test/java/org/neo4j/server/plugin/gremlin/GremlinPluginTest.java
+++ gremlin-plugin/src/test/java/org/neo4j/server/plugin/gremlin/GremlinPluginTest.java
@@ -61,6 +61,7 @@ public class GremlinPluginTest
         neo4j = new ImpermanentGraphDatabase();
         plugin = new GremlinPlugin();
         Graph graph = new Neo4jGraph( neo4j );
+        graph.clear();
         Vertex marko = graph.addVertex( "1" );
         marko.setProperty( "name", "marko" );
         marko.setProperty( "age", 29 );
@@ -128,7 +129,7 @@ public class GremlinPluginTest
         JSONArray array = (JSONArray) parser.parse( json.format( GremlinPluginTest.executeTestScript( "g.V", null) ) );
         List<String> ids = new ArrayList<String>( Arrays.asList( "1", "2", "3",
                 "4", "5", "6" ) );
-        Assert.assertEquals( array.size(), 6 );
+        Assert.assertEquals( 6, array.size() );
         for ( Object object : array )
         {
             String self = (String) ( (JSONObject) object ).get( "self" );
