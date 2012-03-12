28f5a55098620da9b571110d9d9d6d8ca44e31ab
Jacob Hansson <jacob@voltvoodoo.com>
2012-03-12 14:01:19 +0100

    Updated vagrant files to adhere to changes in vagrant 0.9.7

--- packaging/installer-qa/src/test/resources/vagrant/all/share/neo4j/cygwin.rb
+++ packaging/installer-qa/src/test/resources/vagrant/all/share/neo4j/cygwin.rb
@@ -1,18 +1,17 @@
-require 'vagrant/systems/linux'
+require 'vagrant/guest/linux'
 
 module Neo4j
-  class Cygwin < Vagrant::Systems::Linux
-    def prepare_host_only_network(net_options=nil)
-      # No-op
-    end
+  class Cygwin < Vagrant::Guest::Linux
 
-    def enable_host_only_network(net_options)
-      vm.ssh.execute do |ssh|
-        # Windows interfaces start counting from 1,
-        # so we need to increase vagrants interface 
-        # number by one.
-        iface_no = net_options[:adapter] + 1
-        ssh.exec!("netsh interface ip set address 'Local Area Connection #{iface_no}' static address=#{net_options[:ip]} mask=#{net_options[:netmask]} store=persistent")
+    def configure_networks(networks)
+      networks.each do |network|
+        vm.ssh.execute do |ssh|
+          # Windows interfaces start counting from 1,
+          # so we need to increase vagrants interface 
+          # number by one.
+          iface_no = network[:interface] + 1
+          ssh.exec!("netsh interface ip set address 'Local Area Connection #{iface_no}' static address=#{network[:ip]} mask=#{network[:netmask]} store=persistent")
+        end
       end
     end
   end
