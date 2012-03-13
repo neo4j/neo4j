477f205a5b3a4b7342a94efecfc1b80c543d8ef8
Mattias Persson <mattias@neotechnology.com>
2012-03-13 12:48:46 +0100

    Added logging to mark which active txlog has been set

--- community/kernel/src/main/java/org/neo4j/kernel/impl/transaction/TxManager.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/transaction/TxManager.java
@@ -266,6 +266,7 @@ public class TxManager extends AbstractTransactionManager
         fc.write( buf );
         fc.force( true );
         fc.close();
+        msgLog.logMessage( "Active txlog set to " + newFileName, true );
     }
 
     void setTmNotOk( Throwable cause )
