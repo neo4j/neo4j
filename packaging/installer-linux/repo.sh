a1c9390b7f129f42810a092627ea526c2b6f091d
simpsonjulian <simpsonjulian@gmail.com>
2012-03-21 13:42:27 +0000

    Push packages to the new bucket

--- packaging/installer-linux/repo.sh
+++ packaging/installer-linux/repo.sh
@@ -4,4 +4,4 @@ dest="${script_dir}/target/repo"
 mkdir -p $dest/binary
 cp ${script_dir}/target/classes/*.deb $dest/binary
 ( cd $dest && dpkg-scanpackages binary /dev/null | gzip -9c > binary/Packages.gz )
-s3cmd put --acl-public --recursive $dest s3://debian.us-east.neo4j.org
+s3cmd put --acl-public --recursive $dest s3://debian.neo4j.org
