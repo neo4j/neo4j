7bd4db0df26d17c57d276ca48847c665ac24dbb6
Rickard Öberg <rickardoberg@gmail.com>
2012-04-02 17:40:44 +0800

    Fixed license

--- community/server/src/main/java/org/neo4j/server/storemigration/PreStartupStoreUpgrader.java
+++ community/server/src/main/java/org/neo4j/server/storemigration/PreStartupStoreUpgrader.java
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.neo4j.server.storemigration;
 
 import java.io.File;
