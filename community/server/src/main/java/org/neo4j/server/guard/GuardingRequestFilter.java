9b569acb4632aa80a761f9dd5d121079a010f8a9
Thomas Baum <thomas.baum@atns.de>
2012-04-02 14:39:20 +0200

    correct shutdown timer

--- community/server/src/main/java/org/neo4j/server/guard/GuardingRequestFilter.java
+++ community/server/src/main/java/org/neo4j/server/guard/GuardingRequestFilter.java
@@ -108,6 +108,7 @@ public class GuardingRequestFilter implements Filter
 
     public void destroy()
     {
+        timer.cancel();
     }
 
     private int getTimeLimit( HttpServletRequest request )
