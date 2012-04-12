1f70ec0569300f3338f34875c7a4e468000128dd
Jacob Hansson <jacob@voltvoodoo.com>
2012-04-12 11:48:18 +0200

    Merged AutoIndexFunctionalTest and AutoIndexerFunctionalTest

--- community/server/src/functionaltest/java/org/neo4j/server/rest/AutoIndexFunctionalTest.java
+++ community/server/src/functionaltest/java/org/neo4j/server/rest/AutoIndexFunctionalTest.java
@@ -19,11 +19,18 @@
  */
 package org.neo4j.server.rest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.neo4j.kernel.impl.annotations.Documented;
 import org.neo4j.server.database.DatabaseBlockedException;
+import org.neo4j.server.rest.domain.JsonHelper;
 import org.neo4j.server.rest.domain.JsonParseException;
 import org.neo4j.server.rest.web.PropertyValueException;
+import org.neo4j.server.rest.web.RestfulGraphDatabase;
 import org.neo4j.test.GraphDescription.Graph;
 import org.neo4j.test.GraphDescription.NODE;
 import org.neo4j.test.GraphDescription.PROP;
@@ -250,9 +257,142 @@ public class AutoIndexFunctionalTest extends AbstractRestFunctionalTestBase
                 .get( relationshipAutoIndexUri() + "since/today/" )
                 .entity() );
     }
+    
+    /**
+     * Get current status for autoindexing on nodes.
+     */
+    @Test
+    @Documented
+    public void getCurrentStatusForNodes() {
+        checkAndAssertAutoIndexerIsEnabled("node", false);
+    }
+
+    /**
+     * Enable node autoindexing.
+     */
+    @Test
+    @Documented
+    public void enableNodeAutoIndexing() {
+        setEnabledAutoIndexingForType("node", true);
+    }
+
+    /**
+     * Add a property for autoindexing on nodes.
+     */
+    @Test
+    @Documented
+    public void addAutoIndexingPropertyForNodes() {
+        gen.get().expectedStatus(204).payload("myProperty1").post(autoIndexURI("node") + "/properties");
+    }
+
+    /**
+     * Lookup list of properties being autoindexed.
+     */
+    @Test
+    @Documented
+    public void listAutoIndexingPropertiesForNodes() throws JsonParseException {
+        String propName = "some-property";
+        server().getDatabase().graph.index().getNodeAutoIndexer().startAutoIndexingProperty(propName);
+        
+        List<String> properties = getAutoIndexedPropertiesForType("node");
+        
+        assertEquals(1, properties.size());
+        assertEquals(propName, properties.get(0));
+    }
+
+    /**
+     * Remove a property for autoindexing on nodes.
+     */
+    @Test
+    @Documented
+    public void removeAutoIndexingPropertyForNodes() {
+        gen.get().expectedStatus(204).delete(autoIndexURI("node") + "/properties/myProperty1");
+    }
+
+    @Test
+    public void switchOnOffAutoIndexingForNodes() {
+        switchOnOffAutoIndexingForType("node");
+    }
+
+    @Test
+    public void switchOnOffAutoIndexingForRelationships() {
+        switchOnOffAutoIndexingForType("relationship");
+    }
+
+    @Test
+    public void addRemoveAutoIndexedPropertyForNodes() throws JsonParseException {
+        addRemoveAutoIndexedPropertyForType("node");
+    }
+
+    @Test
+    public void addRemoveAutoIndexedPropertyForRelationships() throws JsonParseException {
+        addRemoveAutoIndexedPropertyForType("relationship");
+    }
 
     private String relationshipAutoIndexUri()
     {
         return getDataUri() + "index/auto/relationship/";
+    }
+    
+    private void addRemoveAutoIndexedPropertyForType(String uriPartForType) throws JsonParseException {
+//      List<String> properties = getAutoIndexedPropertiesForType(uriPartForType);
+//      assertTrue(properties.isEmpty());
+
+        gen.get().expectedStatus(204).payload("myProperty1")
+                .post(autoIndexURI(uriPartForType) + "/properties");
+        gen.get().expectedStatus(204).payload("myProperty2")
+                .post(autoIndexURI(uriPartForType) + "/properties");
+
+        List<String> properties = getAutoIndexedPropertiesForType(uriPartForType);
+        assertEquals(2, properties.size());
+        assertTrue(properties.contains("myProperty1"));
+        assertTrue(properties.contains("myProperty2"));
+
+        gen.get()
+                .expectedStatus(204)
+                .payload(null)
+                .delete(autoIndexURI(uriPartForType)
+                        + "/properties/myProperty2");
+
+        properties = getAutoIndexedPropertiesForType(uriPartForType);
+        assertEquals(1, properties.size());
+        assertTrue(properties.contains("myProperty1"));
+    }
+
+    private List<String> getAutoIndexedPropertiesForType(String uriPartForType)
+            throws JsonParseException
+    {
+        String result = gen.get().expectedStatus(200)
+                .get(autoIndexURI(uriPartForType) + "/properties").entity();
+        return (List<String>) JsonHelper.readJson(result);
+    }
+
+    private void switchOnOffAutoIndexingForType(String uriPartForType)
+    {
+        setEnabledAutoIndexingForType(uriPartForType, true);
+        checkAndAssertAutoIndexerIsEnabled(uriPartForType, true);
+        setEnabledAutoIndexingForType(uriPartForType, false);
+        checkAndAssertAutoIndexerIsEnabled(uriPartForType, false);
+    }
+
+    private void setEnabledAutoIndexingForType(String uriPartForType,
+            boolean enabled)
+    {
+        gen.get().expectedStatus(204).payload(Boolean.toString(enabled))
+                .put(autoIndexURI(uriPartForType) + "/status");
+    }
+
+    private void checkAndAssertAutoIndexerIsEnabled(String uriPartForType,
+            boolean enabled)
+    {
+        String result = gen.get().expectedStatus(200)
+                .get(autoIndexURI(uriPartForType) + "/status").entity();
+        assertEquals(enabled, Boolean.parseBoolean(result));
+    }
+
+    private String autoIndexURI(String type)
+    {
+        return getDataUri()
+                + RestfulGraphDatabase.PATH_AUTO_INDEX.replace("{type}", type);
     }
 }
