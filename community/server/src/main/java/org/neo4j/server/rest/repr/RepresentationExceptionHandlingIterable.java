42710758b2a178e3554bfb2605237986bd4e050e
Michael Hunger <github@jexp.de>
2012-04-21 01:25:00 +0200

    Fix for cypher-plugin test
    The change to a streaming approach caused the exceptions from cypher (due to the lazy nature) only
    to occur during the iteration of the result which as too late, so the Exception was not converted
    into a BadInputException but Internal Server Error
    
    Change contains a ExceptionHandlingIterable which allows to intercept and convert exceptions thrown
    during iterator(), next(), hasNext() and remove().
    
    A custom implementation for REST Representation then converts Exceptions into BadInputExceptions.

---
+++ community/server/src/main/java/org/neo4j/server/rest/repr/RepresentationExceptionHandlingIterable.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2002-2012 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.neo4j.server.rest.repr;
+
+import org.neo4j.helpers.collection.ExceptionHandlingIterable;
+
+import java.util.Iterator;
+
+public class RepresentationExceptionHandlingIterable<T> extends ExceptionHandlingIterable<T> {
+    public RepresentationExceptionHandlingIterable(Iterable<T> source) {
+        super(source);
+    }
+
+    @Override
+    protected Iterator<T> exceptionOnIterator(Throwable t) {
+        if (t instanceof Exception) rethrow(new BadInputException(t));
+        return super.exceptionOnIterator(t);
+    }
+
+    @Override
+    protected T exceptionOnNext(Throwable t) {
+        if (t instanceof Exception) rethrow(new BadInputException(t));
+        return super.exceptionOnNext(t);
+    }
+
+    @Override
+    protected void exceptionOnRemove(Throwable t) {
+        super.exceptionOnRemove(t);
+    }
+
+    @Override
+    protected boolean exceptionOnHasNext(Throwable t) {
+        if (t instanceof Exception) rethrow(new BadInputException(t));
+        return super.exceptionOnHasNext(t);
+    }
+}
