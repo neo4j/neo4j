ae219a93ddd7c1c88aa1730822522f25b8be3a86
Peter Neubauer <peter@neubauer.se>
2012-04-26 14:11:21 +0200

    switching to native OSGi test container

--- community/embedded-examples/src/test/java/org/neo4j/examples/osgi/OSGiTest.java
+++ community/embedded-examples/src/test/java/org/neo4j/examples/osgi/OSGiTest.java
@@ -18,9 +18,9 @@
  */
 package org.neo4j.examples.osgi;
 
-import static org.ops4j.pax.exam.CoreOptions.autoWrap;
 import static org.ops4j.pax.exam.CoreOptions.cleanCaches;
-import static org.ops4j.pax.exam.CoreOptions.felix;
+import static org.ops4j.pax.exam.CoreOptions.autoWrap;
+import static org.ops4j.pax.exam.CoreOptions.frameworkStartLevel;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.provision;
@@ -28,11 +28,14 @@ import static org.ops4j.pax.exam.CoreOptions.repository;
 import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
 import static org.ops4j.pax.tinybundles.core.TinyBundles.withBnd;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.index.Index;
+import org.ops4j.pax.exam.ExamSystem;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.TestContainer;
 import org.ops4j.pax.exam.player.Player;
+import org.ops4j.pax.exam.spi.PaxExamRuntime;
 import org.ops4j.pax.exam.testforge.BundlesInState;
 import org.ops4j.pax.exam.testforge.CountBundles;
 import org.ops4j.pax.exam.testforge.WaitForService;
@@ -44,28 +47,43 @@ public class OSGiTest {
     public static final String NEO4J_VERSION = "1.8-SNAPSHOT";
     public static final String GERONIMO_JTA_VERSION = "1.1.1";
 
-    @Test @Ignore
+    @Test
     public void neo4jStartupTestFelix()
         throws Exception
     {
+
+        Option[] options = testOptions( );
         Player player = new Player().with(
-            options(
-                autoWrap(),    
-                felix(),
-//                equinox(),
-                repository("https://oss.sonatype.org/content/groups/ops4j/"),
-                cleanCaches(),
-                mavenBundle().groupId( "org.apache.geronimo.specs" ).artifactId( "geronimo-jta_1.1_spec" ).version( GERONIMO_JTA_VERSION ),
-                mavenBundle().groupId( "org.apache.lucene" ).artifactId( "lucene-core" ).version( "3.5.0" ),
-                mavenBundle().groupId( "org.neo4j" ).artifactId( "neo4j-kernel" ).version( NEO4J_VERSION ),
-                mavenBundle().groupId( "org.neo4j" ).artifactId( "neo4j-lucene-index" ).version( NEO4J_VERSION ),
-                provision( bundle()
-                        .add (Neo4jActivator.class )
-                        .set( Constants.BUNDLE_ACTIVATOR, Neo4jActivator.class.getName() )
-                        .build( withBnd() ) )
-            )
+            options
         );
         test(player, 13);
+    }
+
+    public Option[] testOptions( )
+    {
+        String gogoVersion = "0.8.0";
+        Option[] options = options(
+            repository("https://oss.sonatype.org/content/groups/ops4j/"),
+            cleanCaches(),
+            autoWrap(),
+            frameworkStartLevel( 10),
+            mavenBundle().groupId( "org.apache.geronimo.specs" ).artifactId( "geronimo-jta_1.1_spec" ).version( GERONIMO_JTA_VERSION ),
+            mavenBundle().groupId( "org.apache.lucene" ).artifactId( "lucene-core" ).version( "3.5.0" ),
+            mavenBundle().groupId( "org.neo4j" ).artifactId( "neo4j-kernel" ).version( NEO4J_VERSION ),
+            mavenBundle().groupId( "org.neo4j" ).artifactId( "neo4j-lucene-index" ).version( NEO4J_VERSION ),
+            mavenBundle().groupId("org.ops4j.pax.logging").artifactId("pax-logging-api").version("1.6.1"),
+            mavenBundle().groupId("org.osgi").artifactId("org.osgi.compendium").version("4.2.0"),
+            
+            mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.gogo.runtime").version(gogoVersion),
+            mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.gogo.shell").version(gogoVersion),
+            mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.gogo.command").version(gogoVersion),
+//            workingDirectory( "target/server" )
+            provision( bundle()
+                    .add (Neo4jActivator.class )
+                    .set( Constants.BUNDLE_ACTIVATOR, Neo4jActivator.class.getName() )
+                    .build( withBnd() ) )
+        );
+        return options;
     }
 
     private void test(Player player, int expectedBundles) throws Exception
@@ -76,5 +94,16 @@ public class OSGiTest {
         .test( CountBundles.class,  expectedBundles)
         .test( BundlesInState.class, Bundle.ACTIVE, Bundle.ACTIVE )
         .play();
+    }
+    
+    public static void main( String[] args )
+            throws Exception
+    {
+        // create a proper ExamSystem with your options. Focus on "createServerSystem"
+        ExamSystem system = PaxExamRuntime.createServerSystem( new OSGiTest().testOptions() );
+        // create Container (you should have exactly one configured!) and start.
+        TestContainer container = PaxExamRuntime.createContainer( system );
+        container.start();
+        
     }
 }
