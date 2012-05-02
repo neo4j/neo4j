51fa35e903a741e2889f78f667d501f0b82b7289
Chris Gioran <chris.gioran@neotechnology.com>
2012-05-02 14:38:08 +0300

    Moved all common code to a common superclass, removes a lot of duplication keeping the same API

--- community/kernel/src/main/java/org/neo4j/kernel/impl/transaction/SpringTransactionManager.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/transaction/SpringTransactionManager.java
@@ -19,87 +19,14 @@
  */
 package org.neo4j.kernel.impl.transaction;
 
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.InvalidTransactionException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
-import javax.transaction.Transaction;
 import javax.transaction.TransactionManager;
 
 import org.neo4j.kernel.GraphDatabaseAPI;
 
-public class SpringTransactionManager implements TransactionManager
+public class SpringTransactionManager extends BaseSpringTransactionImpl implements TransactionManager
 {
-    /*
-     * The GD API reference below is used exclusively for accessing
-     * the TransactionManager. It is on purpose _not_ replaced with a
-     * reference to that however. In HA settings the reference passed is
-     * to a HAGD which when restarted has its TM changed. If we kept a
-     * reference to the TM it would be valid until the next internal
-     * restart. In contrast, this way always looks up the "real"
-     * reference and keeps the Spring integration working even when
-     * HA master switches happen.
-     */
-    private final GraphDatabaseAPI neo4j;
-
-    public SpringTransactionManager( GraphDatabaseAPI neo4j )
+    public SpringTransactionManager( GraphDatabaseAPI neo4j)
     {
-        this.neo4j = neo4j;
-    }
-
-    private TransactionManager getTxManager()
-    {
-        return neo4j.getTxManager();
-    }
-
-    public void begin() throws NotSupportedException, SystemException
-    {
-        getTxManager().begin();
-    }
-
-    public void commit() throws RollbackException, HeuristicMixedException,
-        HeuristicRollbackException, SecurityException, IllegalStateException,
-        SystemException
-    {
-        getTxManager().commit();
-    }
-
-    public int getStatus() throws SystemException
-    {
-        return getTxManager().getStatus();
-    }
-
-    public Transaction getTransaction() throws SystemException
-    {
-        return getTxManager().getTransaction();
-    }
-
-    public void resume( Transaction tx ) throws InvalidTransactionException,
-        IllegalStateException, SystemException
-    {
-        getTxManager().resume(tx);
-    }
-
-    public void rollback() throws IllegalStateException, SecurityException,
-        SystemException
-    {
-        getTxManager().rollback();
-    }
-
-    public void setRollbackOnly() throws IllegalStateException, SystemException
-    {
-        getTxManager().setRollbackOnly();
-    }
-
-    public void setTransactionTimeout( int sec ) throws SystemException
-    {
-        getTxManager().setTransactionTimeout(sec);
-    }
-
-    public Transaction suspend() throws SystemException
-    {
-        return getTxManager().suspend();
+        super( neo4j );
     }
 }
