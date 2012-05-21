eb5bbcf3ab8a68804f74ed4ec8dc29e17e4dc015
Mattias Persson <mattias@neotechnology.com>
2012-05-21 16:05:59 +0200

    This reverts commits: f56ae512d867a200507910404106d5a137fd66ba. 9c7968d12dbb34c844e1a47066c63138e49a974d. ebaf55265dd12294b21cc5cb5510234c2088103a. c0e11bddfd385074fa9e050f64304d17fde59ed2. b72cfb83b6b0771550d3e620c3645d0174c06841. 5f04495902d564b3c2b2fb3ac411753755b130ac. 9f8e931c90ecb01eaefb8482d556f7fc6ed86834. 45c6298b047d422a28232be86ccdf5a2fa4b7be8. dc44de04d88d1aa8b090f563c0f3d5b9c3c052b9. d6e50b090a474d089453c7abd6d0885565a1d2ae. 2168c1ac44e1fabe9b46744161e621da9e975dd7.
    Due to community (jmx tests) breaking after merge of
    https://github.com/neo4j/community/pull/510

--- community/kernel/src/test/java/org/neo4j/helpers/TestFormat.java
+++ community/kernel/src/test/java/org/neo4j/helpers/TestFormat.java
@@ -37,76 +37,59 @@ public class TestFormat
         // Date
         long timeWithDate = System.currentTimeMillis();
         String dateAsString = Format.date( timeWithDate );
-        assertEquals(
-                timeWithDate,
-                new SimpleDateFormat( Format.DATE_FORMAT ).parse( dateAsString ).getTime() );
-
+        assertEquals( timeWithDate, new SimpleDateFormat( Format.DATE_FORMAT ).parse( dateAsString ).getTime() );
+        
         // Time
         String timeAsString = Format.time( timeWithDate );
-        assertEquals(
-                timeWithDate,
-                translateToDate( timeWithDate, new SimpleDateFormat(
-                        Format.TIME_FORMAT ).parse( timeAsString ).getTime(),
-                        Format.DEFAULT_TIME_ZONE ) );
+        assertEquals( timeWithDate, translateToDate( timeWithDate,
+                new SimpleDateFormat( Format.TIME_FORMAT ).parse( timeAsString ).getTime(), Format.DEFAULT_TIME_ZONE ) );
     }
-
+    
     @Test
     public void dateTimeWithTimeZone() throws Exception
     {
         String zoneOffset = "+03:00";
         TimeZone zone = TimeZone.getTimeZone( "GMT" + zoneOffset );
-
+        
         // Date
         String asString = Format.date( zone );
         assertTrue( asString.endsWith( withoutColon( zoneOffset ) ) );
-        long timeWithDate = new SimpleDateFormat( Format.DATE_FORMAT ).parse(
-                asString ).getTime();
-
+        long timeWithDate = new SimpleDateFormat( Format.DATE_FORMAT ).parse( asString ).getTime();
+        
         asString = Format.date( timeWithDate, zone );
         assertTrue( asString.endsWith( withoutColon( zoneOffset ) ) );
-        assertEquals(
-                timeWithDate,
-                new SimpleDateFormat( Format.DATE_FORMAT ).parse( asString ).getTime() );
+        assertEquals( timeWithDate, new SimpleDateFormat( Format.DATE_FORMAT ).parse( asString ).getTime() );
 
         asString = Format.date( new Date( timeWithDate ), zone );
         assertTrue( asString.endsWith( withoutColon( zoneOffset ) ) );
-        assertEquals(
-                timeWithDate,
-                new SimpleDateFormat( Format.DATE_FORMAT ).parse( asString ).getTime() );
-
+        assertEquals( timeWithDate, new SimpleDateFormat( Format.DATE_FORMAT ).parse( asString ).getTime() );
+        
         // Time
         asString = Format.time( timeWithDate, zone );
-        assertEquals(
-                timeWithDate,
-                translateToDate( timeWithDate, new SimpleDateFormat(
-                        Format.TIME_FORMAT ).parse( asString ).getTime(), zone ) );
+        assertEquals( timeWithDate, translateToDate( timeWithDate,
+                new SimpleDateFormat( Format.TIME_FORMAT ).parse( asString ).getTime(), zone ) );
 
         asString = Format.time( new Date( timeWithDate ), zone );
-        assertEquals(
-                timeWithDate,
-                translateToDate( timeWithDate, new SimpleDateFormat(
-                        Format.TIME_FORMAT ).parse( asString ).getTime(), zone ) );
+        assertEquals( timeWithDate, translateToDate( timeWithDate,
+                new SimpleDateFormat( Format.TIME_FORMAT ).parse( asString ).getTime(), zone ) );
     }
 
-    private long translateToDate( long timeWithDate, long time,
-            TimeZone timeIsGivenInThisTimeZone )
+    private long translateToDate( long timeWithDate, long time, TimeZone timeIsGivenInThisTimeZone )
     {
-        Calendar calendar = Calendar.getInstance( timeIsGivenInThisTimeZone );
+        Calendar calendar = Calendar.getInstance(timeIsGivenInThisTimeZone);
         calendar.setTimeInMillis( timeWithDate );
-
+        
         Calendar timeCalendar = Calendar.getInstance();
         timeCalendar.setTimeInMillis( time );
         timeCalendar.setTimeZone( timeIsGivenInThisTimeZone );
-        timeCalendar.set( Calendar.YEAR, calendar.get( Calendar.YEAR ) );
-        timeCalendar.set( Calendar.MONTH, calendar.get( Calendar.MONTH ) );
-        boolean crossedDayBoundary = !timeIsGivenInThisTimeZone.equals( Format.DEFAULT_TIME_ZONE )
-                                     && timeCalendar.get( Calendar.HOUR_OF_DAY ) < calendar.get( Calendar.HOUR_OF_DAY );
-        timeCalendar.set( Calendar.DAY_OF_MONTH,
-                calendar.get( Calendar.DAY_OF_MONTH )
-                        + ( crossedDayBoundary ? 1 : 0 ) );
+        timeCalendar.set( Calendar.YEAR, calendar.get( Calendar.YEAR ) ); 
+        timeCalendar.set( Calendar.MONTH, calendar.get( Calendar.MONTH ) ); 
+        boolean crossedDayBoundary = !timeIsGivenInThisTimeZone.equals( Format.DEFAULT_TIME_ZONE ) &&
+                timeCalendar.get( Calendar.HOUR_OF_DAY ) < calendar.get( Calendar.HOUR_OF_DAY );
+        timeCalendar.set( Calendar.DAY_OF_MONTH, calendar.get( Calendar.DAY_OF_MONTH ) + (crossedDayBoundary ? 1 : 0) );
         return timeCalendar.getTimeInMillis();
     }
-
+    
     private String withoutColon( String zoneOffset )
     {
         return zoneOffset.replaceAll( ":", "" );
