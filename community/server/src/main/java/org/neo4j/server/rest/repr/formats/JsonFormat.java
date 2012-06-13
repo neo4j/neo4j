cb8dab0b31beb0f2a9480e5cb77dc11be41d9bdb
Mattias Persson <mattias@neotechnology.com>
2012-06-13 09:19:48 +0200

    Imports

--- community/server/src/main/java/org/neo4j/server/rest/repr/formats/JsonFormat.java
+++ community/server/src/main/java/org/neo4j/server/rest/repr/formats/JsonFormat.java
@@ -23,7 +23,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
