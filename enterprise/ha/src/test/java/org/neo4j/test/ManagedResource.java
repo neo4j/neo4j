ea5972cc07b1d74dbf7aaefa5bc7f8b9400d6a64
Tobias Lindaaker <tobias@thobe.org>
2012-07-27 13:43:12 +0200

    Test case for tying the KernelData object to the outer HA db instead of the internal local db.
    This would ensure that a ha db maintains the same instanceId thoughout its entire life cycle,
    even if the internal local db restarts. This will still largely be the case with the aggressive
    reuse of instanceId implemented in kernel, but satisfying this test case would be one step better,
    ensuring the same id even with multiple dbs in the same JVM without requiring configuring the id.

---
+++ enterprise/ha/src/test/java/org/neo4j/test/ManagedResource.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2002-2012 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.test;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public abstract class ManagedResource<R> implements TestRule
+{
+    private R resource;
+
+    public final R getResource()
+    {
+        R result = this.resource;
+        if ( result == null )
+        {
+            throw new IllegalStateException( "Resource is not started." );
+        }
+        return result;
+    }
+
+    protected abstract R createResource( TargetDirectory.TestDirectory dir ) throws Exception;
+
+    protected abstract void disposeResource( R resource );
+
+    @Override
+    public final Statement apply( final Statement base, Description description )
+    {
+        final TargetDirectory.TestDirectory dir = TargetDirectory.forTest( description.getTestClass() ).testDirectory();
+        return dir.apply( new Statement()
+        {
+            @Override
+            public void evaluate() throws Throwable
+            {
+                resource = createResource( dir );
+                try
+                {
+                    base.evaluate();
+                }
+                finally
+                {
+                    R waste = resource;
+                    resource = null;
+                    disposeResource( waste );
+                }
+            }
+        }, description );
+    }
+}
