8cb6719f9ea98125f8dd40f662b670adcbe6b612
Mattias Persson <mattias@neotechnology.com>
2012-08-02 16:10:49 +0200

    Also tests the other possible scenario for logging stopping, which is broker reconnect due to ZK expiration events

--- enterprise/ha/src/test/java/org/neo4j/ha/TestLogging.java
+++ enterprise/ha/src/test/java/org/neo4j/ha/TestLogging.java
@@ -23,10 +23,13 @@ import static junit.framework.Assert.assertTrue;
 
 import java.io.File;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.helpers.collection.IteratorUtil;
 import org.neo4j.kernel.EnterpriseGraphDatabaseFactory;
+import org.neo4j.kernel.GraphDatabaseAPI;
+import org.neo4j.kernel.HighlyAvailableGraphDatabase;
 import org.neo4j.kernel.ha.HaSettings;
 import org.neo4j.kernel.impl.util.StringLogger;
 import org.neo4j.test.TargetDirectory;
@@ -34,32 +37,60 @@ import org.neo4j.test.ha.LocalhostZooKeeperCluster;
 
 public class TestLogging
 {
-    @Test
-    public void makeSureMessageAreLoggedAfterInitialStoreCopy() throws Exception
+    String masterDir, slaveDir;
+    GraphDatabaseAPI master, slave;
+    
+    @Before
+    public void before() throws Exception
     {
         LocalhostZooKeeperCluster zoo = LocalhostZooKeeperCluster.singleton().clearDataAndVerifyConnection();
         File dir = TargetDirectory.forTest( getClass() ).directory( "dbs", true );
         
-        String masterDir = new File( dir, "0" ).getAbsolutePath();
-        GraphDatabaseService master = new EnterpriseGraphDatabaseFactory().newHighlyAvailableDatabaseBuilder( masterDir )
+        masterDir = new File( dir, "0" ).getAbsolutePath();
+        master = (GraphDatabaseAPI) new EnterpriseGraphDatabaseFactory().newHighlyAvailableDatabaseBuilder( masterDir )
                 .setConfig( HaSettings.server, "localhost:6361" )
                 .setConfig( HaSettings.server_id, "0" )
                 .setConfig( HaSettings.coordinators, zoo.getConnectionString() )
                 .newGraphDatabase();
 
-        String slaveDir = new File( dir, "1" ).getAbsolutePath();
-        GraphDatabaseService slave = new EnterpriseGraphDatabaseFactory().newHighlyAvailableDatabaseBuilder( slaveDir )
+        slaveDir = new File( dir, "1" ).getAbsolutePath();
+        slave = (GraphDatabaseAPI) new EnterpriseGraphDatabaseFactory().newHighlyAvailableDatabaseBuilder( slaveDir )
                 .setConfig( HaSettings.server, "localhost:6362" )
                 .setConfig( HaSettings.server_id, "1" )
                 .setConfig( HaSettings.coordinators, zoo.getConnectionString() )
                 .newGraphDatabase();
-        
+    }
+
+    @After
+    public void after() throws Exception
+    {
+        if ( slave != null )
+            slave.shutdown();
+        if ( master != null )
+            master.shutdown();
+    }
+    
+    @Test
+    public void makeSureMessagesAreLoggedAfterInitialStoreCopy() throws Exception
+    {
         slave.shutdown();
+        slave = null;
         master.shutdown();
+        master = null;
         
         assertMessagesLogContains( slaveDir, "STARTUP diagnostics" );
     }
-
+    
+    @Test
+    public void makeSureMessagesAreLoggedAfterBrokerReconnect() throws Exception
+    {
+        ((HighlyAvailableGraphDatabase)slave).getBroker().restart();
+        // Just do something that we know will write something to the log
+        slave.getXaDataSourceManager().getNeoStoreDataSource().rotateLogicalLog();
+        
+        assertMessagesLogContains( slaveDir, "Rotating [" );
+    }
+    
     private void assertMessagesLogContains( String slaveDir, String expectingToFind )
     {
         boolean found = false;
