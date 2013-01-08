abc3d27dca3cb46716c774e9c9c85e085a5e2cd0
Mattias Persson <mattias@neotechnology.com>
2013-01-08 21:52:34 +0100

    Imports

--- advanced/management/src/main/java/org/neo4j/management/impl/HotspotManagementSupport.java
+++ advanced/management/src/main/java/org/neo4j/management/impl/HotspotManagementSupport.java
@@ -29,8 +29,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.management.MBeanServer;
 import javax.management.remote.JMXConnectorServer;
