87ccf3350d466954fada9b135d1dec22c0901f97
Rickard Öberg <rickardoberg@gmail.com>
2013-01-30 16:00:52 +0800

    Ensure that acceptor store doesn't grow unbounded, this time by reacting to what has been delivered

--- enterprise/cluster/src/main/java/org/neo4j/cluster/protocol/atomicbroadcast/multipaxos/InMemoryAcceptorInstanceStore.java
+++ enterprise/cluster/src/main/java/org/neo4j/cluster/protocol/atomicbroadcast/multipaxos/InMemoryAcceptorInstanceStore.java
@@ -32,7 +32,12 @@ public class InMemoryAcceptorInstanceStore
         implements AcceptorInstanceStore
 {
     Map<InstanceId, AcceptorInstance> instances = new HashMap<InstanceId, AcceptorInstance>(  );
-    Deque<InstanceId> currentInstances = new LinkedList<InstanceId>();
+
+    long lastDeliveredInstanceId = -1;
+
+    public InMemoryAcceptorInstanceStore()
+    {
+    }
 
     @Override
     public AcceptorInstance getAcceptorInstance( InstanceId instanceId )
@@ -42,13 +47,6 @@ public class InMemoryAcceptorInstanceStore
         {
             instance = new AcceptorInstance();
             instances.put( instanceId, instance );
-
-            // Ensure that it doesn't grow unbounded
-            currentInstances.offer( instanceId );
-            if (instances.size() > 500)
-            {
-                instances.remove( currentInstances.poll());
-            }
         }
 
         return instance;
@@ -67,9 +65,19 @@ public class InMemoryAcceptorInstanceStore
     }
 
     @Override
+    public void lastDelivered( InstanceId instanceId )
+    {
+        for ( long i = lastDeliveredInstanceId; i <= instanceId.getId(); i++ )
+        {
+            instances.remove( new InstanceId(i) );
+        }
+
+        lastDeliveredInstanceId = instanceId.getId();
+    }
+
+    @Override
     public void clear()
     {
         instances.clear();
-        currentInstances.clear();
     }
 }
