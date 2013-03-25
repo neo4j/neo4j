78ebdc1c7181ac93c7bf0538196d63fa51de1513
Chris Gioran <chris.gioran@neotechnology.com>
2013-03-25 18:30:06 +0200

    Adds wait conditions that were missing in TransactionConstraintsIT and caused failures

--- enterprise/ha/src/test/java/org/neo4j/ha/TransactionConstraintsIT.java
+++ enterprise/ha/src/test/java/org/neo4j/ha/TransactionConstraintsIT.java
@@ -44,6 +44,7 @@ import org.neo4j.kernel.impl.MyRelTypes;
 import org.neo4j.test.AbstractClusterTest;
 import org.neo4j.test.OtherThreadExecutor;
 import org.neo4j.test.OtherThreadExecutor.WorkerCommand;
+import org.neo4j.test.ha.ClusterManager;
 
 public class TransactionConstraintsIT extends AbstractClusterTest
 {
@@ -66,6 +67,8 @@ public class TransactionConstraintsIT extends AbstractClusterTest
             cluster.shutdown( cluster.getMaster() );
             assertFinishGetsTransactionFailure( tx );
         }
+
+        cluster.await( ClusterManager.masterAvailable() );
 
         // THEN
         assertEquals( db, cluster.getMaster() );
@@ -91,6 +94,8 @@ public class TransactionConstraintsIT extends AbstractClusterTest
             cluster.shutdown( cluster.getMaster() );
             assertFinishGetsTransactionFailure( tx );
         }
+
+        cluster.await( ClusterManager.masterAvailable() );
 
         // THEN
         assertFalse( db.isMaster() );
