39e81c6df2ac7b3afb254e5c36446c7f42150bb0
Tobias Lindaaker <tobias@thobe.org>
2013-05-16 14:24:07 +0200

    Bug Fix: infinite loop in port conflict diagnostics dumper.

--- enterprise/server-enterprise/src/test/java/org/neo4j/server/enterprise/functional/DumpPortListenerOnNettyBindFailure.java
+++ enterprise/server-enterprise/src/test/java/org/neo4j/server/enterprise/functional/DumpPortListenerOnNettyBindFailure.java
@@ -71,7 +71,7 @@ public class DumpPortListenerOnNettyBindFailure implements TestRule
                 }
                 catch ( Throwable failure )
                 {
-                    for ( Throwable cause = failure; cause != null; cause = failure.getCause() )
+                    for ( Throwable cause = failure; cause != null; cause = cause.getCause() )
                     {
                         if ( cause instanceof ChannelException )
                         {
