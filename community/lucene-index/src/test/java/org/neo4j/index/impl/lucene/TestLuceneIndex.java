a64d267da02cb97fcab2a88fa4e1196ed15e34ca
Mattias Persson <mattias@neotechnology.com>
2013-05-24 11:05:18 +0200

    Makes sure no traces of the index config is left behind if acquiring an index resulted in its configuration to be created, but with an invalid configuration.

--- community/lucene-index/src/test/java/org/neo4j/index/impl/lucene/TestLuceneIndex.java
+++ community/lucene-index/src/test/java/org/neo4j/index/impl/lucene/TestLuceneIndex.java
@@ -19,24 +19,6 @@
  */
 package org.neo4j.index.impl.lucene;
 
-import static org.apache.lucene.search.NumericRangeQuery.newIntRange;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.nullValue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static org.neo4j.helpers.collection.MapUtil.stringMap;
-import static org.neo4j.index.Neo4jTestCase.assertContains;
-import static org.neo4j.index.Neo4jTestCase.assertContainsInOrder;
-import static org.neo4j.index.impl.lucene.Contains.contains;
-import static org.neo4j.index.impl.lucene.IsEmpty.isEmpty;
-import static org.neo4j.index.lucene.QueryContext.numericRange;
-import static org.neo4j.index.lucene.ValueContext.numeric;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -44,6 +26,7 @@ import java.util.Map;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryParser.QueryParser.Operator;
 import org.apache.lucene.search.BooleanClause.Occur;
@@ -52,6 +35,7 @@ import org.apache.lucene.search.DefaultSimilarity;
 import org.apache.lucene.search.NumericRangeQuery;
 import org.apache.lucene.search.Sort;
 import org.apache.lucene.search.TermQuery;
+import org.hamcrest.CoreMatchers;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.neo4j.graphdb.Direction;
@@ -70,8 +54,28 @@ import org.neo4j.helpers.collection.IteratorUtil;
 import org.neo4j.helpers.collection.MapUtil;
 import org.neo4j.index.lucene.QueryContext;
 import org.neo4j.index.lucene.ValueContext;
+import org.neo4j.kernel.GraphDatabaseAPI;
 import org.neo4j.kernel.InternalAbstractGraphDatabase;
+import org.neo4j.kernel.impl.index.IndexStore;
 import org.neo4j.test.ImpermanentGraphDatabase;
+
+import static org.apache.lucene.search.NumericRangeQuery.newIntRange;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.neo4j.helpers.collection.MapUtil.stringMap;
+import static org.neo4j.index.Neo4jTestCase.assertContains;
+import static org.neo4j.index.Neo4jTestCase.assertContainsInOrder;
+import static org.neo4j.index.impl.lucene.Contains.contains;
+import static org.neo4j.index.impl.lucene.IsEmpty.isEmpty;
+import static org.neo4j.index.lucene.QueryContext.numericRange;
+import static org.neo4j.index.lucene.ValueContext.numeric;
 
 public class TestLuceneIndex extends AbstractLuceneIndexTest
 {
@@ -1729,5 +1733,26 @@ public class TestLuceneIndex extends AbstractLuceneIndexTest
         q.add( LuceneUtil.rangeQuery( "start", 9, null, true, true ), Occur.MUST );
         q.add( LuceneUtil.rangeQuery( "end", null, 30, true, true ), Occur.MUST );
         assertContains( index.query( q ), node );
+    }
+    
+    @Test
+    public void failureToCreateAnIndexShouldNotLeaveConfiguratiobBehind() throws Exception
+    {
+        // WHEN
+        try
+        {
+            // StandardAnalyzer is invalid since it has no publi no-arg constructor
+            nodeIndex( stringMap( "analyzer", StandardAnalyzer.class.getName() ) );
+            fail( "Should have failed" );
+        }
+        catch ( RuntimeException e )
+        {
+            assertThat( e.getMessage(), CoreMatchers.containsString( StandardAnalyzer.class.getName() ) );
+        }
+        
+        // THEN - assert that there's no index config about this index left behind
+        assertFalse( "There should be no index config for index '" + currentIndexName() + "' left behind",
+                ((GraphDatabaseAPI)graphDb).getDependencyResolver().resolveDependency( IndexStore.class ).has(
+                        Node.class, currentIndexName() ) );
     }
 }
