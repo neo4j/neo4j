9bc1e54bf9fb9163b761ee4175051ffe9aa4f92b
Stefan Plantikow <stefan.plantikow@googlemail.com>
2013-08-14 18:33:51 +0200

    Update license and fix button positions

--- packaging/neo4j-desktop/src/main/java/org/neo4j/desktop/ui/SettingsDialog.java
+++ packaging/neo4j-desktop/src/main/java/org/neo4j/desktop/ui/SettingsDialog.java
@@ -1,14 +1,37 @@
+/**
+ * Copyright (c) 2002-2013 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.neo4j.desktop.ui;
 
 import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -34,6 +57,7 @@ import static org.neo4j.desktop.ui.Components.ellipsis;
 import static org.neo4j.desktop.ui.Components.withBorder;
 import static org.neo4j.desktop.ui.Components.withBoxLayout;
 import static org.neo4j.desktop.ui.Components.withFlowLayout;
+import static org.neo4j.desktop.ui.Components.withLayout;
 import static org.neo4j.desktop.ui.Components.withSpacingBorder;
 import static org.neo4j.desktop.ui.Components.withTitledBorder;
 
@@ -91,52 +115,99 @@ class SettingsDialog extends JDialog
 
     private JPanel createExtensionsPanel()
     {
-        // Extensions packages config
+        DefaultListModel<String> packageListModel = createPackageListModel();
+        JList<String> packageList = new JList<>( packageListModel );
+        Component listPane = new JScrollPane( packageList );
+        Component buttonsPane = createPackageListButtons( packageListModel, packageList );
+        return withBorder(
+            BorderFactory.createCompoundBorder(
+                BorderFactory.createTitledBorder(
+                    format( "Server Extensions (at %s)", environment.getExtensionsDirectory().getAbsolutePath() ) ),
+                    createSpacingBorder( 1 ) ),
+            createContentPanel( listPane, buttonsPane ) );
+    }
+
+    private DefaultListModel<String> createPackageListModel()
+    {
         final DefaultListModel<String> packageListModel = new DefaultListModel<>();
         for ( String packageName : model.getExtensionPackagesConfig() )
         {
             packageListModel.addElement( packageName );
         }
-
-        final JList<String> packageList = new JList<>( packageListModel );
-        JPanel packageListButtons = withBoxLayout( BoxLayout.Y_AXIS, createPanel(
-                createTextButton( "+", new ActionListener()
-                {
-                    @Override
-                    public void actionPerformed( ActionEvent e )
-                    {
-                        String newPackage = JOptionPane.showInputDialog( "Package containing extension(s) to include" );
-                        if ( newPackage != null )
-                        {
-                            packageListModel.addElement( newPackage );
-                            model.setExtensionPackagesConfig( itemsAsList( packageListModel ) );
-                        }
-                    }
-                } ),
-                createTextButton( "-", new ActionListener()
-                {
-                    @Override
-                    public void actionPerformed( ActionEvent e )
-                    {
-                        int selectedIndex = packageList.getSelectedIndex();
-                        if ( selectedIndex != -1 )
-                        {
-                            packageListModel.removeElementAt( selectedIndex );
-                            model.setExtensionPackagesConfig( itemsAsList( packageListModel ) );
-                        }
-                    }
-                } )
-        ) );
-
-        return withBorder(
-            BorderFactory.createCompoundBorder(
-                BorderFactory.createTitledBorder(
-                    format( "Server Extensions (at %s)", environment.getExtensionsDirectory().getAbsolutePath() ) ),
-                    createSpacingBorder( 2 ) ),
-            withBoxLayout( BoxLayout.X_AXIS, createPanel(
-                new JScrollPane( packageList ), createHorizontalSpacing(), packageListButtons ) ) );
+        return packageListModel;
     }
 
+    private JPanel createContentPanel( Component listPanel, Component buttonsPanel )
+    {
+        JPanel content = withLayout( new GridBagLayout(), createPanel() );
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0;
+        content.add( createHorizontalSpacing(), c );
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 100;
+        content.add( listPanel, c );
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0;
+        content.add( createHorizontalSpacing(), c );
+        content.add( buttonsPanel, c );
+        return content;
+    }
+
+    private JPanel createPackageListButtons( DefaultListModel<String> packageListModel, JList<String> packageList )
+    {
+        JButton addButton = createAddButton( packageListModel );
+        JButton removeButton = createRemoveButton( packageListModel, packageList );
+
+        JPanel packageListButtons = withLayout( new GridBagLayout(), createPanel() );
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.NORTH;
+        c.gridx = 0;
+        c.gridy = 0;
+        packageListButtons.add( addButton, c );
+        c.gridy = 1;
+        packageListButtons.add( removeButton, c );
+        c.gridy = 2;
+        packageListButtons.add( Box.createVerticalGlue() );
+        packageListButtons.setMinimumSize( removeButton.getPreferredSize() );
+        return packageListButtons;
+    }
+
+    private JButton createRemoveButton( final DefaultListModel<String> packageListModel,
+                                        final JList<String> packageList )
+    {
+        return createTextButton( "Remove", new ActionListener()
+        {
+            @Override
+            public void actionPerformed( ActionEvent e )
+            {
+                int selectedIndex = packageList.getSelectedIndex();
+                if ( selectedIndex != -1 )
+                {
+                    packageListModel.removeElementAt( selectedIndex );
+                    model.setExtensionPackagesConfig( itemsAsList( packageListModel ) );
+                }
+            }
+        } );
+    }
+
+    private JButton createAddButton( final DefaultListModel<String> packageListModel )
+    {
+        return createTextButton( "Add", new ActionListener()
+        {
+            @Override
+            public void actionPerformed( ActionEvent e )
+            {
+                String newPackage = JOptionPane.showInputDialog( "Package containing extension(s) to include" );
+                if ( newPackage != null )
+                {
+                    packageListModel.addElement( newPackage );
+                    model.setExtensionPackagesConfig( itemsAsList( packageListModel ) );
+                }
+            }
+        } );
+    }
 
 
     private List<String> itemsAsList( ListModel<String> model )
