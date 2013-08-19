68ed599a957f22b7c58fb165cccbe589f04beb48
Jim Webber <jim@jimwebber.org>
2013-08-19 13:12:26 +0100

    Tweaked some wording in the manual.

--- community/cypher/src/test/scala/org/neo4j/cypher/docgen/PatternTest.scala
+++ community/cypher/src/test/scala/org/neo4j/cypher/docgen/PatternTest.scala
@@ -45,30 +45,28 @@ class PatternTest extends ArticleTest {
 Patterns
 ========
 
-Patterns are at the very core of Cypher, and are used in a lot of different places.
-Using patterns, you describe the shape of the data that you are looking for.
-Patterns are used in the `MATCH` clause. Path patterns are expressions.
-Since these expressions are collections, they can also be used as predicates (a non-empty collection signifies true).
-They are also used to `CREATE`/`CREATE UNIQUE` the graph.
+Patterns and pattern-matching are at the very heart of Cypher, and so it's very important to understand patterns,
+to be able to be effective with Cypher.
 
-So, understanding patterns is important, to be able to be effective with Cypher.
-
-You describe the pattern, and Cypher will figure out how to get that data for you. The idea is for you to draw your
+For instance, using patterns, you describe the shape of the data you're looking for in the `MATCH` clause. You describe
+the pattern, and Cypher will figure out how to get that data for you. The idea is for you to draw your
 query on a whiteboard, naming the interesting parts of the pattern, so you can then use values from these parts
 to create the result set you are looking for.
+
+
 
 Patterns have bound points, or starting points. They are the parts of the pattern that are already ``bound'' to a set of
 graph nodes or relationships. All parts of the pattern must be directly or indirectly connected to a starting point -- a pattern
 where parts of the pattern are not reachable from any starting point will be rejected.
 
 [options="header", cols=">s,^,^,^,^,^", width="100%"]
-|===================
-|Clause|Optional|Multiple rel. types|Varlength|Paths|Maps
-|Match|Yes|Yes|Yes|Yes|-
-|Create|-|-|-|Yes|Yes
-|Create Unique|-|-|-|Yes|Yes
-|Expressions|-|Yes|Yes|-|-
-|===================
+      |===================
+      |Clause|Optional|Multiple rel. types|Varlength|Paths|Maps
+      |Match|Yes|Yes|Yes|Yes|-
+      |Create|-|-|-|Yes|Yes
+      |Create Unique|-|-|-|Yes|Yes
+      |Expressions|-|Yes|Yes|-|-
+      |===================
 
 == Patterns for related nodes ==
 
@@ -79,7 +77,9 @@ relationships that always start and end in nodes. An example path would be:
 
 This is a path starting from the pattern node `a`, with an outgoing relationship from it to pattern node `b`.
 
-Paths can be of arbitrary length, and the same node may appear in multiple places in the path.
+Paths can be of arbitrary length, and the same node may appear in multiple places in the path. Path patterns are
+expressions, and since these expressions are collections, they can also be used as predicates (where a non-empty
+collection signifies true).
 
 Node identifiers can be used with or without surrounding parenthesis. The following match is semantically identical to
 the one we saw above -- the difference is purely aesthetic.
@@ -115,9 +115,8 @@ Multiple relationship types can not be used with `CREATE` or `CREATE UNIQUE`.
 
 An optional relationship is matched when it is found, but replaced by a `null` otherwise.
 Normally, if no matching relationship is found, that sub-graph is not matched.
-Optional relationships could be called the Cypher equivalent of the outer join in SQL.
-
-They can only be used in `MATCH`.
+Optional relationships could be considered the Cypher equivalent of the outer join in SQL
+and can only be used in a `MATCH` clause.
 
 Optional relationships are marked with a question mark.
 They allow you to write queries like this one:
@@ -206,20 +205,19 @@ RETURN p1,p2,p3###
 
 == Setting properties ==
 
-Nodes and relationships are important, but Neo4j uses properties on both of these to allow for far denser graphs models.
+Nodes and relationships are important, but Neo4j uses properties on both of these to allow for far richer graph models.
 
-Properties are expressed in patterns using the map-construct, which is simply curly brackets surrounding a number of
-key-expression pairs, separated by commas, e.g. `{ name: "Andres", sport: "BJJ" }`. If the map is supplied through a
+Properties are expressed in patterns using a map-construct, simply curly brackets surrounding a number of
+key-expression pairs, separated by commas, e.g. `{ name: "Andres", sport: "Brazilian Ju-Jitsu" }`. If the map is supplied through a
 parameter, the normal parameter expression is used: `{ paramName }`.
 
-Maps are only used by `CREATE` and `CREATE UNIQUE`. In `CREATE` they are used to set the properties on the newly created
-nodes and relationships.
-
-When used with `CREATE UNIQUE`, they are used to try to match a pattern element with the corresponding graph element.
+Patterns are also used to mutate the graph with `CREATE` and `CREATE UNIQUE`. Maps are only used by `CREATE` and
+`CREATE UNIQUE`. In `CREATE` they are used to set the properties on the newly created nodes and relationships. When
+used with `CREATE UNIQUE`, they are used to try to match a pattern element with the corresponding graph element.
 The match is successful if the properties on the pattern element can be matched exactly against properties on the graph
 elements. The graph element can have additional properties, and they do not affect the match. If Neo4j fails to find
 matching graph elements, the maps is used to set the properties on the newly created elements.
-"""
+    """
 }
 
 
