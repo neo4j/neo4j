65d19ca72d7472fce67bdd9a015ece9b78a564c2
Anders Nawroth <anders@neotechnology.com>
2013-09-06 18:29:16 +0200

    Keyword 'end' moved to the correct list in syntax highlighter.

--- manual/js/shBrushCypher.js
+++ manual/js/shBrushCypher.js
@@ -23,9 +23,9 @@
 
   function Brush()
   {
-        var funcs   =   'abs acos all allShortestPaths any asin atan atan2 avg ceil coalesce collect cos cot count degrees e end endnode exp extract filter floor has head id labels last left length log log10 lower ltrim max min node nodes none percentile_cont percentile_disc pi radians rand range reduce rel relationship relationships replace right round rtrim shortestPath sign sin single sqrt startnode stdev stdevp str substring sum tail tan timestamp trim type upper';
+        var funcs   =   'abs acos all allShortestPaths any asin atan atan2 avg ceil coalesce collect cos cot count degrees e endnode exp extract filter floor has head id labels last left length log log10 lower ltrim max min node nodes none percentile_cont percentile_disc pi radians rand range reduce rel relationship relationships replace right round rtrim shortestPath sign sin single sqrt startnode stdev stdevp str substring sum tail tan timestamp trim type upper';
 
-        var keywords =  'as asc ascending assert by case constraint create cypher delete desc descending distinct drop else false foreach in index is limit match merge null on order remove return set skip start then true union unique using when where with';
+        var keywords =  'as asc ascending assert by case constraint create cypher delete desc descending distinct drop else end false foreach in index is limit match merge null on order remove return set skip start then true union unique using when where with';
 
         var operators = 'or and not xor';
 
@@ -47,3 +47,4 @@
   // CommonJS
   typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
 })();
+
