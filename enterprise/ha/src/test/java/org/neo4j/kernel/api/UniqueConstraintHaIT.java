e01603507e20c1d272b031f83180bedd3e3f563c
Alistair Jones <alistair.jones@gmail.com>
2013-09-12 11:24:57 +0100

    License headers.

--- enterprise/ha/src/test/java/org/neo4j/kernel/api/UniqueConstraintHaIT.java
+++ enterprise/ha/src/test/java/org/neo4j/kernel/api/UniqueConstraintHaIT.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2002-2013 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.neo4j.kernel.api;
 
 import org.junit.Rule;
@@ -31,13 +50,19 @@ public class UniqueConstraintHaIT
             tx.success();
         }
 
+        cluster.sync();
+        Thread.sleep( 5000 );
+
         // then
-        try ( Transaction tx = master.beginTx() )
+        for ( HighlyAvailableGraphDatabase clusterMember : cluster.getAllMembers() )
         {
-            UniquenessConstraintDefinition constraint = single( master.schema().getConstraints( label( "Label1" ) ) )
-                    .asUniquenessConstraint();
-            assertEquals( "key1", single( constraint.getPropertyKeys() ) );
-            tx.success();
+            try ( Transaction tx = clusterMember.beginTx() )
+            {
+                UniquenessConstraintDefinition constraint = single( clusterMember.schema().getConstraints( label( "Label1" ) ) )
+                        .asUniquenessConstraint();
+                assertEquals( "key1", single( constraint.getPropertyKeys() ) );
+                tx.success();
+            }
         }
     }
 
