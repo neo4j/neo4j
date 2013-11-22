295f0b57c1581ed1af1f152654c6fa64c1d3abc3
Tatham Oddie <tatham@oddie.com.au>
2013-11-21 17:18:59 -0800

    Handle Java paths with spaces in them (#1534)

--- packaging/standalone/src/main/distribution/shell-scripts/bin/Neo4jInstaller.bat
+++ packaging/standalone/src/main/distribution/shell-scripts/bin/Neo4jInstaller.bat
@@ -50,7 +50,7 @@ goto :main %1
   call %~dps0functions.bat :findJavaHome
   set javaPath=%javaPath:"="""%
 
-  set binPath="%javaPath%\bin\java.exe %loggingProperties% -DworkingDir="%~dps0.." -DconfigFile=%configFile% %classpath% %mainclass% -Dorg.neo4j.cluster.logdirectory="%~dps0..\data\log" -jar %~dps0%wrapperJarFilename%  %serviceName%"
+  set binPath="\"%javaPath%\bin\java.exe\" %loggingProperties% -DworkingDir="%~dps0.." -DconfigFile=%configFile% %classpath% %mainclass% -Dorg.neo4j.cluster.logdirectory="%~dps0..\data\log" -jar %~dps0%wrapperJarFilename%  %serviceName%"
 
   sc create "%serviceName%" binPath= %binpath% DisplayName= "%serviceDisplayName:"=%" start= %serviceStartType%
   sc start %serviceName%
