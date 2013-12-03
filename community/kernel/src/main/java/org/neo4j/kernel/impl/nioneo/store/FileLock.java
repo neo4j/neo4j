0d4213e93567ee41db650eacda3a4cab666c4661
Mattias Persson <mattias@neotechnology.com>
2013-12-03 10:54:50 +0100

    Completes the change where FileLock throws IOException or returns an instance, i.e. stops using null for failure signal
    (cherry picked from commit 0a5eb2aba58c9caf5061d88d20818157011a9add)
    
    Conflicts:
    	community/kernel/src/main/java/org/neo4j/kernel/impl/nioneo/store/CommonAbstractStore.java
    	community/kernel/src/test/java/org/neo4j/kernel/StoreLockerTest.java
    	community/kernel/src/test/java/org/neo4j/kernel/impl/nioneo/store/TestOsSpecificLocks.java
    	community/kernel/src/test/java/org/neo4j/unsafe/batchinsert/BatchInserterImplTest.java
    	enterprise/cluster/src/test/java/org/neo4j/cluster/protocol/heartbeat/HeartbeatContextTest.java
    	enterprise/cluster/src/test/java/org/neo4j/cluster/protocol/heartbeat/HeartbeatStateTest.java
    	enterprise/ha/src/test/java/org/neo4j/kernel/ha/cluster/HighAvailabilityModeSwitcherTest.java

--- community/kernel/src/main/java/org/neo4j/kernel/impl/nioneo/store/FileLock.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/nioneo/store/FileLock.java
@@ -34,7 +34,7 @@ public abstract class FileLock
     {
         if ( lock == null )
         {
-            return null;
+            throw new IllegalArgumentException( "Null lock" );
         }
 
         return new FileLock()
@@ -154,7 +154,6 @@ public abstract class FileLock
         private final java.nio.channels.FileLock fileChannelLock;
 
         public WindowsFileLock( File lockFile, FileChannel fileChannel, java.nio.channels.FileLock lock )
-                throws IOException
         {
             this.lockFile = lockFile;
             this.fileChannel = fileChannel;
