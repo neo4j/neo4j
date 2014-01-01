ad4252c22e3eea6b4e51fb88b8e00495e6ddea16
Devteam <devteam@neotechnology.com>
2014-01-01 14:17:42 -0800

    Update copyright for 2014 (happy new year!)

--- community/cypher/src/test/scala/org/neo4j/cypher/internal/executionplan/builders/ExtractBuilderTest.scala
+++ community/cypher/src/test/scala/org/neo4j/cypher/internal/executionplan/builders/ExtractBuilderTest.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2002-2013 "Neo Technology,"
+ * Copyright (c) 2002-2014 "Neo Technology,"
  * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
@@ -20,7 +20,7 @@
 package org.neo4j.cypher.internal.executionplan.builders
 
 /**
- * Copyright (c) 2002-2013 "Neo Technology,"
+ * Copyright (c) 2002-2014 "Neo Technology,"
  * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
