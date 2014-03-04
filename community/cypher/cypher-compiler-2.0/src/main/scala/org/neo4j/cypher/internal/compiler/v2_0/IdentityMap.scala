302442472b6fe804571c20e8f641d8f8c56198ea
Chris Leishman <chris@leishman.org>
2014-03-04 11:47:21 -0800

    Add MutableIdentityMap

--- community/cypher/cypher-compiler-2.0/src/main/scala/org/neo4j/cypher/internal/compiler/v2_0/IdentityMap.scala
+++ community/cypher/cypher-compiler-2.0/src/main/scala/org/neo4j/cypher/internal/compiler/v2_0/IdentityMap.scala
@@ -19,7 +19,6 @@
  */
 package org.neo4j.cypher.internal.compiler.v2_0
 
-import scala.collection.immutable.Map
 import scala.collection.JavaConverters._
 import java.util
 
@@ -35,30 +34,111 @@ object IdentityMap {
 }
 
 case class IdentityMap[K, V] private (idMap: util.IdentityHashMap[K, V] = new util.IdentityHashMap[K, V]()) extends Map[K, V] {
-  override def get(key: K): Option[V] =
-    idMap.get(key) match {
-      case null => None
-      case value => Some(value)
-    }
+  def get(key: K): Option[V] = {
+    val value = idMap.get(key)
+    if (value == null)
+      None
+    else
+      Some(value)
+  }
 
-  override def +[V1 >: V](kv: (K, V1)): IdentityMap[K, V1] =
+  def +[V1 >: V](kv: (K, V1)): IdentityMap[K, V1] =
     IdentityMap({
       val clone = idMap.clone().asInstanceOf[util.IdentityHashMap[K, V1]]
       clone.put(kv._1, kv._2)
       clone
     })
 
-  override def -(key: K): IdentityMap[K, V] =
+  def -(key: K): IdentityMap[K, V] =
     IdentityMap({
       val clone = idMap.clone().asInstanceOf[util.IdentityHashMap[K, V]]
       clone.remove(key)
       clone
     })
 
-  override def updated[V1 >: V](key: K, value: V1): IdentityMap[K, V1] = this + ((key, value))
+  override def updated[V1 >: V](key: K, value: V1): IdentityMap[K, V1] =
+    IdentityMap({
+      val clone = idMap.clone().asInstanceOf[util.IdentityHashMap[K, V1]]
+      clone.put(key, value)
+      clone
+    })
 
-  override def iterator: Iterator[(K, V)] =
+  def iterator: Iterator[(K, V)] =
     idMap.clone().asInstanceOf[util.IdentityHashMap[K, V]].entrySet().iterator().asScala.map(e => (e.getKey, e.getValue))
 
   override def stringPrefix: String = "IdentityMap"
+}
+
+
+object MutableIdentityMap {
+  def empty[K, V]: MutableIdentityMap[K, V] = MutableIdentityMap()
+  def apply[K, V](elems: (K, V)*): MutableIdentityMap[K, V] = {
+    val idMap = new util.IdentityHashMap[K, V]()
+    elems.foreach {
+      elem => idMap.put(elem._1, elem._2)
+    }
+    MutableIdentityMap(idMap)
+  }
+}
+
+case class MutableIdentityMap[K, V] private (idMap: util.IdentityHashMap[K, V] = new util.IdentityHashMap[K, V]()) extends Map[K, V] {
+  def get(key: K): Option[V] = {
+    val value = idMap.get(key)
+    if (value == null)
+      None
+    else
+      Some(value)
+  }
+
+  def put(key: K, value: V): Option[V] = {
+    val r = get(key)
+    update(key, value)
+    r
+  }
+
+  def update(key: K, value: V) {
+    idMap.put(key, value)
+  }
+
+  def remove(key: K): Option[V] = {
+    val r = get(key)
+    this -= key
+    r
+  }
+
+  override def +[V1 >: V](kv: (K, V1)): MutableIdentityMap[K, V1] =
+    MutableIdentityMap({
+      val clone = idMap.clone().asInstanceOf[util.IdentityHashMap[K, V1]]
+      clone.put(kv._1, kv._2)
+      clone
+    })
+
+  def +=(kv: (K, V)): MutableIdentityMap[K, V] = {
+    update(kv._1, kv._2)
+    this
+  }
+
+  override def -(key: K): MutableIdentityMap[K, V] =
+    MutableIdentityMap({
+      val clone = idMap.clone().asInstanceOf[util.IdentityHashMap[K, V]]
+      clone.remove(key)
+      clone
+    })
+
+  def -=(key: K): MutableIdentityMap[K, V] = {
+    idMap.remove(key)
+    this
+  }
+
+  override def updated[V1 >: V](key: K, value: V1): MutableIdentityMap[K, V1] =
+    MutableIdentityMap({
+      val clone = idMap.clone().asInstanceOf[util.IdentityHashMap[K, V1]]
+      clone.put(key, value)
+      clone
+    })
+
+  override def iterator: Iterator[(K, V)] =
+    idMap.entrySet().iterator().asScala.map(e => (e.getKey, e.getValue))
+
+  override def stringPrefix: String = "MutableIdentityMap"
 }
