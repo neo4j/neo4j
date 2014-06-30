222497864a3c8b5e27d0d269eecaf46e1e36fe4f
Rickard Öberg <rickardoberg@gmail.com>
2014-06-30 14:42:03 +0800

    Fix so that instances can crash and rejoin with a restarted election version

--- enterprise/cluster/src/main/java/org/neo4j/cluster/protocol/atomicbroadcast/multipaxos/context/ClusterContextImpl.java
+++ enterprise/cluster/src/main/java/org/neo4j/cluster/protocol/atomicbroadcast/multipaxos/context/ClusterContextImpl.java
@@ -249,7 +249,7 @@ class ClusterContextImpl
                     return;
                 }
             }
-            else if ( electorId.equals( lastElector ) && (version < electorVersion && version > 0) )
+            else if ( electorId.equals( lastElector ) && (version < electorVersion && version > 1) )
             {
                 getLogger( getClass() ).warn( "Election result for role " + roleName +
                         " received from elector instance " + electorId + " with version " + version +
