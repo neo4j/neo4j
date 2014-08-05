11b391f80688870814314d3c3579058560b7d3e3
Chris Vest <mr.chrisvest@gmail.com>
2014-08-05 11:27:44 +0200

    Move the MuninnPageCache to enterprise.

--- enterprise/enterprise-io/src/main/java/org/neo4j/io/enterprise/pagecache/impl/muninn/MuninnPageEvictionCallback.java
+++ enterprise/enterprise-io/src/main/java/org/neo4j/io/enterprise/pagecache/impl/muninn/MuninnPageEvictionCallback.java
@@ -5,23 +5,23 @@
  * This file is part of Neo4j.
  *
  * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.io.pagecache.impl.muninn;
+package org.neo4j.io.enterprise.pagecache.impl.muninn;
 
 import org.neo4j.collection.primitive.PrimitiveLongObjectMap;
 import org.neo4j.io.pagecache.PageEvictionCallback;
-import org.neo4j.io.pagecache.impl.muninn.jsr166e.StampedLock;
+import org.neo4j.io.enterprise.pagecache.impl.muninn.jsr166e.StampedLock;
 
 public class MuninnPageEvictionCallback implements PageEvictionCallback
 {
--- enterprise/enterprise-io/src/test/java/org/neo4j/io/enterprise/pagecache/impl/muninn/jsr166e/JSR166TestCase.java
+++ enterprise/enterprise-io/src/test/java/org/neo4j/io/enterprise/pagecache/impl/muninn/jsr166e/JSR166TestCase.java
@@ -5,19 +5,18 @@
  * This file is part of Neo4j.
  *
  * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 // Relicensed from public domain. Original license:
 /*
  * Written by Doug Lea with assistance from members of JCP JSR-166
@@ -29,7 +28,7 @@
 
 // Based on revision 1.7: http://gee.cs.oswego.edu/cgi-bin/viewcvs.cgi/jsr166/src/test/tck-jsr166e/JSR166TestCase.java?revision=1.7&view=markup
 
-package org.neo4j.io.pagecache.impl.muninn.jsr166e;
+package org.neo4j.io.enterprise.pagecache.impl.muninn.jsr166e;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
