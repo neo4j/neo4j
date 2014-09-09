0c00d1ca0dc87e3f1d0336aa6bf24fbb14273765
Stefan Plantikow <stefan.plantikow@googlemail.com>
2014-09-09 14:37:40 +0200

    Add information on the version of the used cypher compiler to execution plan descriptions

--- community/cypher/cypher/src/test/scala/org/neo4j/cypher/RootPlanAcceptanceTest.scala
+++ community/cypher/cypher/src/test/scala/org/neo4j/cypher/RootPlanAcceptanceTest.scala
@@ -23,12 +23,29 @@ class RootPlanAcceptanceTest extends ExecutionEngineFunSuite {
 
   test("should include version information in root plan description for queries of each legacy version") {
 
-    //v2.1 must be handled separately since the resulting compiler is dependent on query
-    val versions = CypherVersion.allVersions.filter(!_.name.startsWith(CypherVersion.v2_1.name))
+    //v2.2 must be handled separately since the resulting compiler is dependent on query
+    val versions = CypherVersion.allVersions.filter(!_.name.startsWith(CypherVersion.v2_2.name))
     versions.foreach { v =>
-      assertVersion(v, "create() return 1", v.name)
+      assertVersion(v, "create() return 1", v.name )
     }
   }
+
+  test("should use rule by default in 2.2") {
+    assertVersion(CypherVersion.v2_2, "match n return n", CypherVersion.v2_2_rule.name)
+  }
+
+  test("should use 2.2-cost when possible in 2.2") {
+    assertVersion(CypherVersion.v2_2_cost, "match n return n", CypherVersion.v2_2_cost.name)
+  }
+
+  test("should fallback to 2.2-rule in 2.2") {
+    assertVersion(CypherVersion.v2_2_cost, "create() return 1", CypherVersion.v2_2_rule.name)
+  }
+
+  test("should use 2.2-rule if we really ask for it in 2.2") {
+    assertVersion(CypherVersion.v2_2_rule, "match n return n", CypherVersion.v2_2_rule.name)
+  }
+
 
   def assertVersion(v: CypherVersion, query: String, expectedCompiler: String) {
     val executionResult = eengine.profile(s"cypher ${v.name} ${query}").executionPlanDescription()
@@ -36,6 +53,5 @@ class RootPlanAcceptanceTest extends ExecutionEngineFunSuite {
 
     planDescription.getArguments.get("version") should equal(s"CYPHER ${expectedCompiler}")
   }
-
 
 }
