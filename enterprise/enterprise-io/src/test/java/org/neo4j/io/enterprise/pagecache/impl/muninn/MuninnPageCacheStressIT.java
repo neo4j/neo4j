382c1e626e8ec9309b414db27438475da5d155e4
Lasse Westh-Nielsen <lassewesth@gmail.com>
2014-09-10 09:55:19 +0200

    added stress test for munin

--- enterprise/enterprise-io/src/test/java/org/neo4j/io/enterprise/pagecache/impl/muninn/MuninnPageCacheStressIT.java
+++ enterprise/enterprise-io/src/test/java/org/neo4j/io/enterprise/pagecache/impl/muninn/MuninnPageCacheStressIT.java
@@ -5,24 +5,24 @@
  * This file is part of Neo4j.
  *
  * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.io.pagecache.impl.standard;
+package org.neo4j.io.enterprise.pagecache.impl.muninn;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
 import static org.hamcrest.number.OrderingComparison.lessThanOrEqualTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.neo4j.io.fs.DefaultFileSystemAbstraction;
@@ -32,7 +32,7 @@ import org.neo4j.io.pagecache.stress.Conditions;
 import org.neo4j.io.pagecache.stress.PageCacheStresser;
 import org.neo4j.io.pagecache.stress.StressMonitor;
 
-public class StandardPageCacheStressIT
+public class MuninnPageCacheStressIT
 {
     @Test
     public void shouldHandleTheStressOf10000000Evictions() throws Exception
@@ -53,12 +53,11 @@ public class StandardPageCacheStressIT
         StressMonitor stressMonitor = new StressMonitor();
         Condition condition = Conditions.numberOfEvictions( stressMonitor, desiredNumberOfEvictions );
 
-        RunnablePageCache pageCache = new StandardPageCache(
+        RunnablePageCache pageCache = new MuninnPageCache(
                 new DefaultFileSystemAbstraction(),
                 cacheMaxPages,
                 cachePageSize,
-                stressMonitor
-        );
+                stressMonitor );
 
         Thread thread = new Thread( pageCache );
         thread.start();
