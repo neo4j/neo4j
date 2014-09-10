74371aebce494f13f94f4932252735ba30460d21
Ben Butler-Cole <ben@bridesmere.com>
2014-09-10 13:14:53 +0100

    Java subprocesses that we launch must be headless.

--- enterprise/ha/src/test/java/org/neo4j/ha/TestPullUpdatesApplied.java
+++ enterprise/ha/src/test/java/org/neo4j/ha/TestPullUpdatesApplied.java
@@ -196,7 +196,7 @@ public class TestPullUpdatesApplied
 
     public static int runInOtherJvmToGetExitCode( String... args ) throws Exception
     {
-        List<String> allArgs = new ArrayList<String>( Arrays.asList( "java", "-cp",
+        List<String> allArgs = new ArrayList<String>( Arrays.asList( "java", "-Djava.awt.headless=true", "-cp",
                 System.getProperty( "java.class.path" ), TestPullUpdatesApplied.class.getName() ) );
         allArgs.addAll( Arrays.asList( args ) );
         Process p = Runtime.getRuntime().exec( allArgs.toArray( new String[allArgs.size()] ) );
