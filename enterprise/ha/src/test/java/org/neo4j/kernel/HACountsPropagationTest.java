3f13bb5a7784f746e5c11dc95c177efed8038a54
Davide Grohmann <davide.grohmann@neotechnology.com>
2014-10-16 10:56:29 +0200

    Add test for counts propagation in HA

---
+++ enterprise/ha/src/test/java/org/neo4j/kernel/HACountsPropagationTest.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) 2002-2014 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel;
+
+import static org.junit.Assert.assertEquals;
+import static org.neo4j.test.ha.ClusterManager.allSeesAllAsAvailable;
+import static org.neo4j.test.ha.ClusterManager.clusterOfSize;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.DynamicRelationshipType;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.helpers.collection.MapUtil;
+import org.neo4j.kernel.ha.HaSettings;
+import org.neo4j.kernel.ha.HighlyAvailableGraphDatabase;
+import org.neo4j.kernel.impl.store.NeoStore;
+import org.neo4j.kernel.impl.store.counts.CountsTracker;
+import org.neo4j.test.TargetDirectory;
+import org.neo4j.test.ha.ClusterManager;
+
+public class HACountsPropagationTest
+{
+    private static final int PULL_INTERVAL = 100;
+
+    @Test
+    public void shouldPropagateNodeCountsInHA() throws Throwable
+    {
+        File root = TargetDirectory.forTest( getClass() ).cleanDirectory( "shouldPropagateNodeCountsInHA" );
+        ClusterManager clusterManager = new ClusterManager( clusterOfSize( 3 ), root,
+                MapUtil.stringMap( HaSettings.pull_interval.name(), PULL_INTERVAL + "ms" ) );
+        clusterManager.start();
+        try
+        {
+            ClusterManager.ManagedCluster cluster = clusterManager.getDefaultCluster();
+            cluster.await( allSeesAllAsAvailable() );
+
+            HighlyAvailableGraphDatabase master = cluster.getMaster();
+            try ( Transaction tx = master.beginTx() )
+            {
+                master.createNode();
+                master.createNode( DynamicLabel.label( "A" ) );
+                tx.success();
+            }
+
+            waitForPullUpdates();
+
+            for ( HighlyAvailableGraphDatabase db : cluster.getAllMembers() )
+            {
+                CountsTracker counts = db.dependencyResolver.resolveDependency( NeoStore.class ).getCounts();
+                assertEquals( 2, counts.countsForNode( -1 ) );
+                assertEquals( 1, counts.countsForNode( 0 /* A */ ) );
+            }
+        }
+        finally
+        {
+
+            clusterManager.shutdown();
+        }
+    }
+
+    @Test
+    public void shouldPropagateRelationshipCountsInHA() throws Throwable
+    {
+        File root = TargetDirectory.forTest( getClass() ).cleanDirectory( "shouldPropagateRelationshipCountsInHA" );
+        ClusterManager clusterManager = new ClusterManager( clusterOfSize( 3 ), root,
+                MapUtil.stringMap( HaSettings.pull_interval.name(), PULL_INTERVAL + "ms" ) );
+        clusterManager.start();
+        try
+        {
+            ClusterManager.ManagedCluster cluster = clusterManager.getDefaultCluster();
+            cluster.await( allSeesAllAsAvailable() );
+
+            HighlyAvailableGraphDatabase master = cluster.getMaster();
+            try ( Transaction tx = master.beginTx() )
+            {
+                Node left = master.createNode();
+                Node right = master.createNode( DynamicLabel.label( "A" ) );
+                left.createRelationshipTo( right, DynamicRelationshipType.withName( "Type" ) );
+                tx.success();
+            }
+
+            waitForPullUpdates();
+
+            for ( HighlyAvailableGraphDatabase db : cluster.getAllMembers() )
+            {
+                CountsTracker counts = db.dependencyResolver.resolveDependency( NeoStore.class ).getCounts();
+                assertEquals( 1, counts.countsForRelationship( -1, -1, -1 ) );
+                assertEquals( 1, counts.countsForRelationship( -1, -1, 0 ) );
+                assertEquals( 1, counts.countsForRelationship( -1, 0, -1 ) );
+                assertEquals( 1, counts.countsForRelationship( -1, 0, 0 ) );
+            }
+        }
+        finally
+        {
+            clusterManager.shutdown();
+        }
+    }
+
+    private void waitForPullUpdates()
+    {
+        try
+        {
+            Thread.sleep( PULL_INTERVAL * 2 );
+        }
+        catch ( InterruptedException ex )
+        {
+            // ignore me
+        }
+    }
+}
