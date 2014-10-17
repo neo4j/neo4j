36c6fadb71d1f998384cd20fb303c63fa849ca9f
Andres Taylor <andres@neotechnology.com>
2014-10-17 08:18:23 +0200

    Plan OPTIONAL MATCH using both Apply/Optional and OuterJoin

--- community/cypher/cypher-compiler-2.2/src/main/scala/org/neo4j/cypher/internal/compiler/v2_2/planner/logical/steps/solveOptionalMatches.scala
+++ community/cypher/cypher-compiler-2.2/src/main/scala/org/neo4j/cypher/internal/compiler/v2_2/planner/logical/steps/solveOptionalMatches.scala
@@ -22,11 +22,12 @@ package org.neo4j.cypher.internal.compiler.v2_2.planner.logical.steps
 import org.neo4j.cypher.internal.compiler.v2_2.planner.QueryGraph
 import org.neo4j.cypher.internal.compiler.v2_2.planner.logical.plans.LogicalPlan
 import org.neo4j.cypher.internal.compiler.v2_2.planner.logical.steps.LogicalPlanProducer._
-import org.neo4j.cypher.internal.compiler.v2_2.planner.logical.{LogicalPlanningContext, LogicalPlanningFunction2, PlanTable}
+import org.neo4j.cypher.internal.compiler.v2_2.planner.logical.steps.solveOptionalMatches.OptionalSolver
+import org.neo4j.cypher.internal.compiler.v2_2.planner.logical.{CandidateList, LogicalPlanningContext, LogicalPlanningFunction2, PlanTable}
 
 import scala.annotation.tailrec
 
-object solveOptionalMatches extends LogicalPlanningFunction2[PlanTable, QueryGraph, PlanTable] {
+case class solveOptionalMatches(solvers: Seq[OptionalSolver]) {
   def apply(planTable: PlanTable, qg: QueryGraph)(implicit context: LogicalPlanningContext): PlanTable = {
 
     val p = if (planTable.isEmpty)
@@ -36,9 +37,11 @@ object solveOptionalMatches extends LogicalPlanningFunction2[PlanTable, QueryGra
 
     p.plans.foldLeft(planTable) {
       case (table: PlanTable, plan: LogicalPlan) =>
-        val optionalQGs = findQGsToSolve(plan, table, qg.optionalMatches)
+        val optionalQGs: Seq[QueryGraph] = findQGsToSolve(plan, table, qg.optionalMatches)
         val newPlan = optionalQGs.foldLeft(plan) {
-          case (lhs: LogicalPlan, qg: QueryGraph) => planApply(lhs, planOptional(context.strategy.plan(qg)))
+          case (lhs: LogicalPlan, optionalQg: QueryGraph) =>
+            val plans = solvers.flatMap(_.apply(optionalQg, lhs))
+            CandidateList(plans).bestPlan(context.cost).get
         }
         table + newPlan
     }
@@ -48,9 +51,9 @@ object solveOptionalMatches extends LogicalPlanningFunction2[PlanTable, QueryGra
 
     @tailrec
     def inner(in: Seq[QueryGraph], out: Seq[QueryGraph]): Seq[QueryGraph] = in match {
-      case hd :: tl if isSolved(table, hd) => inner(tl, out)
-      case hd :: tl if applicable(plan, hd)    => inner(tl, out :+ hd)
-      case _                                   => out
+      case hd :: tl if isSolved(table, hd)  => inner(tl, out)
+      case hd :: tl if applicable(plan, hd) => inner(tl, out :+ hd)
+      case _                                => out
     }
 
     inner(graphs, Seq.empty)
@@ -61,4 +64,39 @@ object solveOptionalMatches extends LogicalPlanningFunction2[PlanTable, QueryGra
 
   private def applicable(outerPlan: LogicalPlan, optionalQG: QueryGraph) =
     optionalQG.argumentIds.subsetOf(outerPlan.availableSymbols)
+}
+
+object solveOptionalMatches extends LogicalPlanningFunction2[PlanTable, QueryGraph, PlanTable] {
+
+  type OptionalSolver = LogicalPlanningFunction2[QueryGraph, LogicalPlan, Option[LogicalPlan]]
+
+  private val optionalStrategies: Seq[OptionalSolver] = Seq(
+    applyOptional,
+    outerHashJoin
+  )
+
+  private val instance = new solveOptionalMatches(optionalStrategies)
+
+  def apply(pt: PlanTable, qg: QueryGraph)(implicit context: LogicalPlanningContext) =
+    instance.apply(pt, qg)
+}
+
+case object applyOptional extends OptionalSolver {
+  def apply(optionalQg: QueryGraph, lhs: LogicalPlan)(implicit context: LogicalPlanningContext) =
+    Some(planApply(lhs, planOptional(context.strategy.plan(optionalQg))))
+}
+
+case object outerHashJoin extends OptionalSolver {
+  def apply(optionalQg: QueryGraph, lhs: LogicalPlan)(implicit context: LogicalPlanningContext) = {
+    val joinNodes = optionalQg.argumentIds
+    val rhs = context.strategy.plan(optionalQg.withoutArguments())
+
+    if (joinNodes.nonEmpty &&
+      joinNodes.forall(lhs.availableSymbols) &&
+      joinNodes.forall(optionalQg.patternNodes)) {
+      Some(planOuterHashJoin(joinNodes, lhs, rhs))
+    } else {
+      None
+    }
+  }
 }
