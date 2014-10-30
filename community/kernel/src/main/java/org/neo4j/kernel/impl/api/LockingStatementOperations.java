0f9a556be683e06580d192dcee6e3f51fa893283
Jacob Hansson <jakewins@gmail.com>
2014-10-30 16:13:24 -0500

    Order the locking of nodes when creating a relationship.

--- community/kernel/src/main/java/org/neo4j/kernel/impl/api/LockingStatementOperations.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/api/LockingStatementOperations.java
@@ -210,8 +210,17 @@ public class LockingStatementOperations implements
     @Override
     public long relationshipCreate( KernelStatement state, int relationshipTypeId, long startNodeId, long endNodeId )
     {
-        state.locks().acquireExclusive( ResourceTypes.NODE, startNodeId );
-        state.locks().acquireExclusive( ResourceTypes.NODE, endNodeId );
+        // Order the locks to lower the risk of deadlocks with other threads adding rels concurrently
+        if(startNodeId < endNodeId)
+        {
+            state.locks().acquireExclusive( ResourceTypes.NODE, startNodeId );
+            state.locks().acquireExclusive( ResourceTypes.NODE, endNodeId );
+        }
+        else
+        {
+            state.locks().acquireExclusive( ResourceTypes.NODE, endNodeId );
+            state.locks().acquireExclusive( ResourceTypes.NODE, startNodeId );
+        }
         return entityWriteDelegate.relationshipCreate( state, relationshipTypeId, startNodeId, endNodeId );
     }
 
