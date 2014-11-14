945e198adfeb5a15f54e0ca3003798eea369af7b
Andres Taylor <andres@neotechnology.com>
2014-11-14 16:26:57 +0100

    Fixing build errors

--- community/cypher/cypher-compiler-2.2/src/test/scala/org/neo4j/cypher/internal/compiler/v2_2/commands/expressions/GenericCaseTest.scala
+++ community/cypher/cypher-compiler-2.2/src/test/scala/org/neo4j/cypher/internal/compiler/v2_2/commands/expressions/GenericCaseTest.scala
@@ -81,8 +81,7 @@ class GenericCaseTest extends CypherFunSuite {
     result should equal("other")
   }
 
-  @Test
-  def case_with_a_single_null_value_uses_the_default() {
+  test("case_with_a_single_null_value_uses_the_default") {
     //GIVEN CASE WHEN null THEN 42 ELSE "defaults"
     val caseExpr = GenericCase(Seq(CoercedPredicate(Null())->Literal(42)), Some(Literal("defaults")))
 
