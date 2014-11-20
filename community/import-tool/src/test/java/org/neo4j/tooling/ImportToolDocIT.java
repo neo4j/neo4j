b2500ac7b2f23e2ee3586bd85dea66ddd2bb0d43
Anders Nawroth <anders@neotechnology.com>
2014-11-20 16:51:21 +0100

    Fix for docs bug in import-tool manpage, and an additional example.

--- community/import-tool/src/test/java/org/neo4j/tooling/ImportToolDocIT.java
+++ community/import-tool/src/test/java/org/neo4j/tooling/ImportToolDocIT.java
@@ -19,14 +19,17 @@
  */
 package org.neo4j.tooling;
 
-import static org.junit.Assert.*;
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Rule;
 import org.junit.Test;
+
 import org.neo4j.graphdb.DynamicLabel;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
@@ -36,8 +39,12 @@ import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.test.TargetDirectory;
 import org.neo4j.test.TargetDirectory.TestDirectory;
-import org.neo4j.tooling.GlobalGraphOperations;
 import org.neo4j.tooling.ImportTool.Options;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import static org.neo4j.tooling.ImportTool.MULTI_FILE_DELIMITER;
 
 public class ImportToolDocIT
 {
@@ -48,13 +55,13 @@ public class ImportToolDocIT
     TestDirectory directory = TargetDirectory.testDirForTest( getClass() );
 
     @Test
-    public void shouldShowOffCsvFormat() throws Exception
+    public void basicCsvImport() throws Exception
     {
         // GIVEN
         File movies = file( "ops", "movies.csv" );
         try (PrintStream out = new PrintStream( movies ))
         {
-            out.println( ":ID,title,year:int,s:LABEL" );
+            out.println( ":ID,title,year:int,:LABEL" );
             out.println( "tt0133093,\"The Matrix\",1999,Movie" );
             out.println( "tt0234215,\"The Matrix Reloaded\",2003,Movie;Sequel" );
             out.println( "tt0242653,\"The Matrix Revolutions\",2003,Movie;Sequel" );
@@ -92,9 +99,84 @@ public class ImportToolDocIT
                 "--relationships", roles.getAbsolutePath() );
         ImportTool.main( arguments );
 
+        // DOCS
+        String realDir = movies.getParentFile().getAbsolutePath();
+        printCommandToFile( arguments, realDir, "example-command.adoc" );
+
         // THEN
+        verifyData();
+    }
+
+    @Test
+    public void separateHeadersCsvImport() throws Exception
+    {
+        // GIVEN
+        File moviesHeader = file( "ops", "movies2-header.csv" );
+        try ( PrintStream out = new PrintStream( moviesHeader ) )
+        {
+            out.println( ":ID;title;year:int;:LABEL" );
+        }
+        File movies = file( "ops", "movies2.csv" );
+        try ( PrintStream out = new PrintStream( movies ) )
+        {
+            out.println( "tt0133093;'The Matrix';1999;Movie" );
+            out.println( "tt0234215;'The Matrix Reloaded';2003;Movie|Sequel" );
+            out.println( "tt0242653;'The Matrix Revolutions';2003;Movie|Sequel" );
+        }
+        File actorsHeader = file( "ops", "actors2-header.csv" );
+        try ( PrintStream out = new PrintStream( actorsHeader ) )
+        {
+            out.println( ":ID;name;:LABEL" );
+        }
+        File actors = file( "ops", "actors2.csv" );
+        try ( PrintStream out = new PrintStream( actors ) )
+        {
+            out.println( "keanu;'Keanu Reeves';Actor" );
+            out.println( "laurence;'Laurence Fishburne';Actor" );
+            out.println( "carrieanne;'Carrie-Anne Moss';Actor" );
+        }
+        File rolesHeader = file( "ops", "roles2-header.csv" );
+        try ( PrintStream out = new PrintStream( rolesHeader ) )
+        {
+            out.println( ":START_ID;role;:END_ID;:TYPE" );
+        }
+        File roles = file( "ops", "roles2.csv" );
+        try ( PrintStream out = new PrintStream( roles ) )
+        {
+            out.println( "keanu;'Neo';tt0133093;ACTS_IN" );
+            out.println( "keanu;'Neo';tt0234215;ACTS_IN" );
+            out.println( "keanu;'Neo';tt0242653;ACTS_IN" );
+            out.println( "laurence;'Morpheus';tt0133093;ACTS_IN" );
+            out.println( "laurence;'Morpheus';tt0234215;ACTS_IN" );
+            out.println( "laurence;'Morpheus';tt0242653;ACTS_IN" );
+            out.println( "carrieanne;'Trinity';tt0133093;ACTS_IN" );
+            out.println( "carrieanne;'Trinity';tt0234215;ACTS_IN" );
+            out.println( "carrieanne;'Trinity';tt0242653;ACTS_IN" );
+        }
+
+        // WHEN
+        String[] arguments = arguments(
+                "--into", directory.absolutePath(),
+                "--nodes", moviesHeader.getAbsolutePath() + MULTI_FILE_DELIMITER + movies.getAbsolutePath(),
+                "--nodes", actorsHeader.getAbsolutePath() + MULTI_FILE_DELIMITER + actors.getAbsolutePath(),
+                "--relationships", rolesHeader.getAbsolutePath() + MULTI_FILE_DELIMITER + roles.getAbsolutePath(),
+                "--delimiter", ";",
+                "--array-delimiter", "|",
+                "--quote", "'");
+        ImportTool.main( arguments );
+
+        // DOCS
+        String realDir = movies.getParentFile().getAbsolutePath();
+        printCommandToFile( arguments, realDir, "separate-header-example-command.adoc" );
+
+        // THEN
+        verifyData();
+    }
+
+    private void verifyData()
+    {
         GraphDatabaseService db = new GraphDatabaseFactory().newEmbeddedDatabase( directory.absolutePath() );
-        try (Transaction tx = db.beginTx())
+        try ( Transaction tx = db.beginTx() )
         {
             int nodeCount = 0, relationshipCount = 0, sequelCount = 0;
             for ( Node node : GlobalGraphOperations.at( db ).getAllNodes() )
@@ -117,20 +199,35 @@ public class ImportToolDocIT
             }
             assertEquals( SEQUEL_COUNT, sequelCount );
             tx.success();
+            Object year = db.findNode( DynamicLabel.label( "Movie" ), "title", "The Matrix" ).getProperty( "year" );
+            assertEquals( year, 1999 );
         }
         finally
         {
             db.shutdown();
         }
+    }
 
-        // DOCS
-        String documentationArgs = StringUtils.join( arguments, " " );
-        String realDir = movies.getParentFile().getAbsolutePath();
-        documentationArgs = documentationArgs
-                .replace( realDir + File.separator, "" )
-                .replace( directory.absolutePath(), "path_to_target_directory" );
+    private void printCommandToFile( String[] arguments, String dir, String fileName ) throws FileNotFoundException
+    {
+        List<String> cleanedArguments = new ArrayList<>();
+        for ( String argument : arguments )
+        {
+            if ( argument.contains( " " ) || Arrays.asList( new String[] { ";", "|", "'" } ).contains( argument ) )
+            {
+                cleanedArguments.add( '"' + argument + '"' );
+            }
+            else
+            {
+                cleanedArguments.add( argument );
+            }
+        }
+        String documentationArgs = StringUtils.join( cleanedArguments, " " );
+        documentationArgs =
+                documentationArgs.replace( dir + File.separator, "" ).replace( directory.absolutePath(),
+                        "path_to_target_directory" );
         String docsCommand = "neo4j-import " + documentationArgs;
-        try (PrintStream out = new PrintStream( file( "ops", "example-command.adoc" ) ))
+        try ( PrintStream out = new PrintStream( file( "ops", fileName ) ) )
         {
             out.println( docsCommand );
         }
