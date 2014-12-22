0bb9ce28f812959c7f6e0e131fb50fda74aa393d
Stefan Plantikow <stefan.plantikow@googlemail.com>
2014-12-22 12:16:20 +0100

    Make VERBOSE = true in pickBestPlan print out EXPLAIN plans
    Conflicts:
    	community/cypher/cypher-compiler-2.2/src/main/scala/org/neo4j/cypher/internal/compiler/v2_2/CypherCompiler.scala

--- community/cypher/cypher-compiler-2.2/src/main/scala/org/neo4j/cypher/internal/compiler/v2_2/planner/logical/steps/pickBestPlan.scala
+++ community/cypher/cypher-compiler-2.2/src/main/scala/org/neo4j/cypher/internal/compiler/v2_2/planner/logical/steps/pickBestPlan.scala
@@ -19,28 +19,44 @@
  */
 package org.neo4j.cypher.internal.compiler.v2_2.planner.logical.steps
 
+import org.neo4j.cypher.internal.compiler.v2_2
 import org.neo4j.cypher.internal.compiler.v2_2.planner.logical.plans.LogicalPlan
 import org.neo4j.cypher.internal.compiler.v2_2.planner.logical.{CandidateSelector, LogicalPlanningContext}
+import org.neo4j.helpers.Clock
+import org.neo4j.kernel.monitoring.Monitors
 
 object pickBestPlan extends CandidateSelector {
-  private final val VERBOSE = false
+  private final val VERBOSE = true
 
   def apply(plans: Seq[LogicalPlan])(implicit context: LogicalPlanningContext): Option[LogicalPlan] = {
+
     val costs = context.cost
     val comparePlans = (c: LogicalPlan) =>
       (-c.solved.numHints, costs(c, context.cardinalityInput), -c.availableSymbols.size)
 
     if (VERBOSE) {
+      import org.neo4j.cypher.internal.compiler.v2_2.planner.execution.PipeExecutionPlanBuilder
+      import org.neo4j.cypher.internal.compiler.v2_2.planner.execution.PipeExecutionBuilderContext
+      val pipeBuilderContext = new PipeExecutionBuilderContext(context.cardinality, context.semanticTable)
+      val pipeBuilder = new PipeExecutionPlanBuilder(Clock.SYSTEM_CLOCK, new v2_2.Monitors(new Monitors()))
+
+      def printPlan(plan: LogicalPlan): Unit = {
+        val pipe = pipeBuilder.build(plan)(pipeBuilderContext, context.planContext).pipe
+        println("* " + plan.toString + s"\n${costs(plan, context.cardinalityInput)}\n")
+        println(pipe.planDescription)
+      }
+
       val sortedPlans = plans.sortBy(comparePlans)
 
       if (sortedPlans.size > 1) {
         println("Get best of:")
         for (plan <- sortedPlans) {
-          println("* " + plan.toString + s"\n${costs(plan, context.cardinalityInput)}\n")
+          printPlan(plan)
         }
 
         println("Best is:")
         println(sortedPlans.head.toString)
+        println("--------\n")
       }
 
       sortedPlans.headOption
