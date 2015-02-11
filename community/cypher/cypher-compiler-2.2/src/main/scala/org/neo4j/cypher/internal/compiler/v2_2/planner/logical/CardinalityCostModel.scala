9cd4373232721a5185028590664e9a37abc51584
Andres Taylor <andres@neotechnology.com>
2015-02-11 21:40:38 +0100

    Updates to the cost model

--- community/cypher/cypher-compiler-2.2/src/main/scala/org/neo4j/cypher/internal/compiler/v2_2/planner/logical/CardinalityCostModel.scala
+++ community/cypher/cypher-compiler-2.2/src/main/scala/org/neo4j/cypher/internal/compiler/v2_2/planner/logical/CardinalityCostModel.scala
@@ -19,47 +19,67 @@
  */
 package org.neo4j.cypher.internal.compiler.v2_2.planner.logical
 
-import org.neo4j.cypher.internal.compiler.v2_2.ast.Collection
+import org.neo4j.cypher.internal.compiler.v2_2.ast.{HasLabels, Property, Collection}
 import org.neo4j.cypher.internal.compiler.v2_2.commands.ManyQueryExpression
 import org.neo4j.cypher.internal.compiler.v2_2.planner.logical.Metrics._
 import org.neo4j.cypher.internal.compiler.v2_2.planner.logical.plans._
 
-/*
-A very simplistic cost model. Each row returned by an operator costs 1. That's it.
- */
 case class CardinalityCostModel(cardinality: CardinalityModel) extends CostModel {
 
-  val CPU_BOUND_PLAN_COST_PER_ROW: CostPerRow = 0.1
-  val DB_ACCESS_BOUND_PLAN_COST_PER_ROW: CostPerRow = 1.0
+  val CPU_BOUND: CostPerRow = 0.1
+  val FAST_STORE: CostPerRow = 1.0
+  val SLOW_STORE: CostPerRow = 10.0
 
   private def costPerRow(plan: LogicalPlan): CostPerRow = plan match {
 
+    case _: AllNodesScan |
+         _: DirectedRelationshipByIdSeek |
+         _: UndirectedRelationshipByIdSeek |
+         _: ProjectEndpoints |
+         _: NodeByLabelScan
+    => FAST_STORE
+
+    // Filtering on labels and properties
+    case Selection(predicates, _) if predicates.exists {
+      case _:Property | _: HasLabels => true
+      case _ => false
+    }
+    => FAST_STORE
+
     case _: NodeHashJoin |
-      _: Aggregation |
-      _: AbstractLetSemiApply |
-      _: Limit |
-      _: Optional |
-      _: SingleRow |
-      _: Argument |
-      _: OuterHashJoin |
-      _: AbstractSemiApply |
-      _: Skip |
-      _: Sort |
-      _: SortedLimit |
-      _: Union |
-      _: UnwindCollection
-    => CPU_BOUND_PLAN_COST_PER_ROW
+         _: Aggregation |
+         _: AbstractLetSemiApply |
+         _: Limit |
+         _: Optional |
+         _: SingleRow |
+         _: Argument |
+         _: OuterHashJoin |
+         _: AbstractSemiApply |
+         _: Skip |
+         _: Sort |
+         _: SortedLimit |
+         _: Union |
+         _: Selection |
+         _: UnwindCollection
+    => CPU_BOUND
 
-    case NodeIndexSeek(_, _, _, ManyQueryExpression(Collection(elements)), _) =>
-      DB_ACCESS_BOUND_PLAN_COST_PER_ROW * Multiplier(elements.size)
+    case _: Expand |
+         _: FindShortestPaths |
+         _: LegacyIndexSeek |
+         _: NodeByIdSeek |
+         _: NodeIndexUniqueSeek |
+         _: NodeIndexSeek |
+         _: VarExpand
+    => SLOW_STORE
 
-    case _ =>
-      DB_ACCESS_BOUND_PLAN_COST_PER_ROW
+    case NodeIndexSeek(_, _, _, ManyQueryExpression(Collection(elements)), _)
+    => SLOW_STORE * Multiplier(elements.size)
+
   }
 
   private def cardinalityForPlan(plan: LogicalPlan, input: QueryGraphCardinalityInput): Cardinality = plan match {
     case Selection(_, left) => cardinality(left, input)
-    case _                  => plan.lhs.map(p => cardinality(p, input)).getOrElse(cardinality(plan, input))
+    case _ => plan.lhs.map(p => cardinality(p, input)).getOrElse(cardinality(plan, input))
   }
 
   def apply(plan: LogicalPlan, input: QueryGraphCardinalityInput): Cost = plan match {
@@ -67,7 +87,7 @@ case class CardinalityCostModel(cardinality: CardinalityModel) extends CostModel
     case CartesianProduct(lhs, rhs) =>
       apply(lhs, input) + cardinality(lhs, input) * apply(rhs, input)
 
-    case Apply(lhs, rhs) =>
+    case ApplyVariants(lhs, rhs) =>
       val newInputCardinality = cardinality(lhs, input)
       val newInput = input.withCardinality(newInputCardinality)
 
@@ -75,10 +95,17 @@ case class CardinalityCostModel(cardinality: CardinalityModel) extends CostModel
       val rCost = apply(rhs, newInput)
       lCost + rCost
 
-    case OuterHashJoin(_, lhs, rhs) =>
+    case HashJoin(lhs, rhs) =>
       val lCost = apply(lhs, input)
       val rCost = apply(rhs, input)
-      lCost + rCost
+      val PROBE_BUILD_COST = CPU_BOUND
+      val PROBE_SEARCH_COST = CPU_BOUND * .5
+      val lhsCardinality = cardinality(lhs, input)
+      val rhsCardinality = cardinality(rhs, input)
+
+      lCost + rCost +
+        lhsCardinality * PROBE_BUILD_COST +
+        rhsCardinality * PROBE_SEARCH_COST
 
     case _ =>
       val lhsCost = plan.lhs.map(p => apply(p, input)).getOrElse(Cost(0))
@@ -86,5 +113,22 @@ case class CardinalityCostModel(cardinality: CardinalityModel) extends CostModel
       val costForThisPlan = cardinalityForPlan(plan, input) * costPerRow(plan)
       val totalCost = costForThisPlan + lhsCost + rhsCost
       totalCost
+  }
+
+  object HashJoin {
+    def unapply(x: Any): Option[(LogicalPlan, LogicalPlan)] = x match {
+      case NodeHashJoin(_, l, r) => Some(l -> r)
+      case OuterHashJoin(_, l, r) => Some(l -> r)
+      case _ => None
+    }
+  }
+
+  object ApplyVariants {
+    def unapply(x: Any): Option[(LogicalPlan, LogicalPlan)] = x match {
+      case Apply(l, r) => Some(l -> r)
+      case p: AbstractLetSelectOrSemiApply => Some(p.lhs.get -> p.rhs.get)
+      case p: AbstractSelectOrSemiApply => Some(p.lhs.get -> p.rhs.get)
+      case _ => None
+    }
   }
 }
