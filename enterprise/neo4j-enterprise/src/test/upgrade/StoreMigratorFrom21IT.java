725a0fc2b4e71abe8a7356e5ee0cd86ecb326427
Chris Leishman <chris@leishman.org>
2015-02-14 09:58:45 -0800

    Move tests requiring HA to enterprise module

--- enterprise/neo4j-enterprise/src/test/upgrade/StoreMigratorFrom21IT.java
+++ enterprise/neo4j-enterprise/src/test/upgrade/StoreMigratorFrom21IT.java
@@ -5,17 +5,17 @@
  * This file is part of Neo4j.
  *
  * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package upgrade;
 
@@ -104,7 +104,7 @@ public class StoreMigratorFrom21IT
 
         ConsistencyCheckService.Result result = service.runFullConsistencyCheck(
                 dir.getAbsolutePath(), new Config(), ProgressMonitorFactory.NONE, StringLogger.SYSTEM );
-        assertTrue(result.isSuccessful());
+        assertTrue( result.isSuccessful() );
 
         database = builder.newGraphDatabase();
         // Upgrade is now completed. Verify the contents:
