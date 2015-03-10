56ad2de5814a82e5b3c96edd39c4d7f5a3fa40e8
Mattias Persson <mattias@neotechnology.com>
2015-03-10 11:13:20 +0100

    Import tool has ability to skip duplicate nodes
    rather nodes that have the same input ids in the same group. This ability
    is controlled with `--skip-duplicate-nodes<=true/false>`, whereas the
    skipping of bad relationships `--skip-bad-relationships<=true/false>`. All
    and any allowed bad entities (both relationships and duplicate nodes) are
    collected and controlled using `--bad` and `--bad-tolerance`.
    
    Duplicate nodes are detected after node stage, when preparing IdMapper
    and deleted as a side-effect in node->relationship stage. For this the
    handling of not-in-use records is controlled using inUse record flag as
    opposed to null record due to a detail in the BatchingPageCache. It's
    generally better to not leave a record to chance, rather write it as inUse
    or not.

--- community/kernel/src/main/java/org/neo4j/unsafe/impl/batchimport/UpdateRecordsStep.java
+++ community/kernel/src/main/java/org/neo4j/unsafe/impl/batchimport/UpdateRecordsStep.java
@@ -21,6 +21,8 @@ package org.neo4j.unsafe.impl.batchimport;
 
 import java.util.Collection;
 
+import org.neo4j.function.Predicate;
+import org.neo4j.function.Predicates;
 import org.neo4j.kernel.impl.store.RecordStore;
 import org.neo4j.kernel.impl.store.record.AbstractBaseRecord;
 import org.neo4j.unsafe.impl.batchimport.staging.BatchSender;
@@ -32,7 +34,8 @@ import org.neo4j.unsafe.impl.batchimport.stats.Stat;
 import org.neo4j.unsafe.impl.batchimport.stats.StatsProvider;
 
 /**
- * Updates a batch of records to a store.
+ * Updates a batch of records to a store. Can be given a {@link Predicate} that can choose to not
+ * {@link Predicate#accept(Object) accept} a record, which will have that record be written as unused instead.
  */
 public class UpdateRecordsStep<RECORD extends AbstractBaseRecord>
         extends ProcessorStep<RECORD[]>
@@ -41,23 +44,36 @@ public class UpdateRecordsStep<RECORD extends AbstractBaseRecord>
     private final RecordStore<RECORD> store;
     private final int recordSize;
     private int recordsUpdated;
+    // Decides which records should stay in use. Records that are not accepted are set as not in use
+    // before telling the store to update them, effectively deleting them.
+    private final Predicate<RECORD> inUsePredicate;
 
     public UpdateRecordsStep( StageControl control, Configuration config, RecordStore<RECORD> store )
+    {
+        this( control, config, store, Predicates.<RECORD>alwaysTrue() );
+    }
+
+    public UpdateRecordsStep( StageControl control, Configuration config,
+            RecordStore<RECORD> store, Predicate<RECORD> updatePredicate )
     {
         super( control, "v", config, false );
         this.store = store;
         this.recordSize = store.getRecordSize();
+        this.inUsePredicate = updatePredicate;
     }
 
+    @SuppressWarnings( "unchecked" )
     @Override
     protected void process( RECORD[] batch, BatchSender sender )
     {
         for ( RECORD record : batch )
         {
-            if ( record != null )
+            if ( record.inUse() && !inUsePredicate.test( record ) )
             {
-                store.updateRecord( record );
+                record = (RECORD) record.clone();
+                record.setInUse( false );
             }
+            store.updateRecord( record );
         }
         recordsUpdated += batch.length;
     }
