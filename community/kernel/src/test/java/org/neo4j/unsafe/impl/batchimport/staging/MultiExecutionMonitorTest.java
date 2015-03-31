3694c04d8108a6295ea44252800d53fb16e63370
Mattias Persson <mattias@neotechnology.com>
2015-03-31 09:57:02 +0200

    Removed multi-threading from MultiExecutionMonitorTest
    since it introduced unnecessary flakyness and didn't add anything to the
    test.

--- community/kernel/src/test/java/org/neo4j/unsafe/impl/batchimport/staging/MultiExecutionMonitorTest.java
+++ community/kernel/src/test/java/org/neo4j/unsafe/impl/batchimport/staging/MultiExecutionMonitorTest.java
@@ -22,24 +22,18 @@ package org.neo4j.unsafe.impl.batchimport.staging;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.concurrent.Future;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import org.neo4j.helpers.Clock;
 import org.neo4j.helpers.FakeClock;
 import org.neo4j.test.CleanupRule;
-import org.neo4j.test.OtherThreadExecutor;
-import org.neo4j.test.OtherThreadExecutor.WorkerCommand;
 
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import static java.lang.System.currentTimeMillis;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class MultiExecutionMonitorTest
 {
@@ -48,81 +42,58 @@ public class MultiExecutionMonitorTest
     {
         // GIVEN
         FakeClock clock = new FakeClock();
-        TestableMonitor first = new TestableMonitor( clock, 100, MILLISECONDS );
-        TestableMonitor other = new TestableMonitor( clock, 250, MILLISECONDS );
+        TestableMonitor first = new TestableMonitor( clock, 100, MILLISECONDS, "first" );
+        TestableMonitor other = new TestableMonitor( clock, 250, MILLISECONDS, "other" );
         MultiExecutionMonitor multiMonitor = new MultiExecutionMonitor( clock, first, other );
-        StageExecution execution = mock( StageExecution.class );
-        when( execution.stillExecuting() ).thenReturn( true );
 
-        // WHEN
-        OtherThreadExecutor<Void> t2 = cleanup.add( new OtherThreadExecutor<Void>( "T2", null ) );
-        Future<Object> future = t2.executeDontWait( monitor( clock, multiMonitor, execution ) );
+        // WHEN/THEN
         clock.forward( 110, MILLISECONDS );
-        awaitChecks( first, 1, other, 0 );
+        expectCallsToCheck( multiMonitor, first, 1, other, 0 );
         clock.forward( 100, MILLISECONDS );
-        awaitChecks( first, 2, other, 0 );
+        expectCallsToCheck( multiMonitor, first, 2, other, 0 );
         clock.forward( 45, MILLISECONDS );
-        awaitChecks( first, 2, other, 1 );
-
-        // THEN
-        when( execution.stillExecuting() ).thenReturn( false );
-        future.get();
+        expectCallsToCheck( multiMonitor, first, 2, other, 1 );
     }
 
-    private void awaitChecks( Object... alternatingMonitorAndCount )
+    private void expectCallsToCheck( ExecutionMonitor multiMonitor, Object... alternatingMonitorAndCount )
     {
-        long endTime = currentTimeMillis()+SECONDS.toMillis( 2 );
-        while ( currentTimeMillis() < endTime )
+        multiMonitor.check( null ); // null, knowing that our monitors in this test doesn't use 'em
+        for ( int i = 0; i < alternatingMonitorAndCount.length; i++ )
         {
-            boolean match = true;
-            for ( int i = 0; i < alternatingMonitorAndCount.length; i++ )
+            TestableMonitor monitor = (TestableMonitor) alternatingMonitorAndCount[i++];
+            int count = (Integer) alternatingMonitorAndCount[i];
+            assertThat( monitor.timesPolled, lessThanOrEqualTo( count ) );
+            if ( monitor.timesPolled < count )
             {
-                TestableMonitor monitor = (TestableMonitor) alternatingMonitorAndCount[i++];
-                int count = (Integer) alternatingMonitorAndCount[i];
-                assertThat( monitor.timesPolled, lessThanOrEqualTo( count ) );
-                if ( monitor.timesPolled < count )
-                {
-                    match = false;
-                    break;
-                }
-            }
-            if ( match )
-            {
-                return;
+                fail( "Polls didn't occur, expected " + Arrays.toString( alternatingMonitorAndCount ) );
             }
         }
-        fail( "Polls didn't occur" );
-    }
-
-    private WorkerCommand<Void,Object> monitor( final Clock clock, final ExecutionMonitor multiMonitor,
-            final StageExecution execution )
-    {
-        return new WorkerCommand<Void,Object>()
-        {
-            @Override
-            public Object doWork( Void state ) throws Exception
-            {
-                new ExecutionSupervisor( clock, multiMonitor ).supervise( execution );
-                return null;
-            }
-        };
     }
 
     public final @Rule CleanupRule cleanup = new CleanupRule();
 
     private static class TestableMonitor extends ExecutionMonitor.Adpter
     {
-        private int timesPolled;
+        private volatile int timesPolled;
+        private final String name;
 
-        public TestableMonitor( Clock clock, long interval, TimeUnit unit )
+        public TestableMonitor( Clock clock, long interval, TimeUnit unit, String name )
         {
             super( clock, interval, unit );
+            this.name = name;
         }
 
         @Override
         public void check( StageExecution[] executions )
         {
+            System.out.println( "checked " + name );
             timesPolled++;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "[" + name + ":" + timesPolled + "]";
         }
     }
 }
