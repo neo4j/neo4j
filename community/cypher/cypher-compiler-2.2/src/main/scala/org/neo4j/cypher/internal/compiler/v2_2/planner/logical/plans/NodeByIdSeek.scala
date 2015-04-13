0729f774efb3e492fd1835c563a3475e7d94b028
Craig Taverner <craig@amanzi.com>
2015-04-13 14:35:45 +0200

    Enabled ronja to handle id expressions based on previously bound identifiers

--- community/cypher/cypher-compiler-2.2/src/main/scala/org/neo4j/cypher/internal/compiler/v2_2/planner/logical/plans/NodeByIdSeek.scala
+++ community/cypher/cypher-compiler-2.2/src/main/scala/org/neo4j/cypher/internal/compiler/v2_2/planner/logical/plans/NodeByIdSeek.scala
@@ -19,8 +19,8 @@
  */
 package org.neo4j.cypher.internal.compiler.v2_2.planner.logical.plans
 
-import org.neo4j.cypher.internal.compiler.v2_2.ast.{Expression, Parameter}
-import org.neo4j.cypher.internal.compiler.v2_2.pipes.{EntityByIdRhs => CommandEntityByIdRhs, EntityByIdParameter => CommandEntityByIdParameter, EntityByIdExprs => CommandEntityByIdExprs}
+import org.neo4j.cypher.internal.compiler.v2_2.ast.{Identifier, Expression, Parameter}
+import org.neo4j.cypher.internal.compiler.v2_2.pipes.{EntityByIdRhs => CommandEntityByIdRhs, EntityByIdExpression => CommandEntityByIdExpression, EntityByIdParameter => CommandEntityByIdParameter, EntityByIdExprs => CommandEntityByIdExprs}
 import org.neo4j.cypher.internal.compiler.v2_2.ast.convert.commands.ExpressionConverters._
 import org.neo4j.cypher.internal.compiler.v2_2.planner.{CardinalityEstimation, PlannerQuery}
 
@@ -28,6 +28,15 @@ sealed trait EntityByIdRhs {
   def mapExpressions(f: Expression => Expression): EntityByIdRhs
 
   def asEntityByIdRhs: CommandEntityByIdRhs
+}
+
+case class EntityByIdIdentifier(identifier: Identifier) extends EntityByIdRhs {
+  self =>
+
+  override def mapExpressions(f: Expression => Expression): EntityByIdIdentifier = self
+
+  def asEntityByIdRhs =
+    CommandEntityByIdExpression(identifier.asCommandExpression)
 }
 
 case class EntityByIdParameter(parameter: Parameter) extends EntityByIdRhs {
