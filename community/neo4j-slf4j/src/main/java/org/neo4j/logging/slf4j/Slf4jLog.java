be0770e9af7030241ae51eb8cb48e78576aa4e0f
Chris Leishman <chris@leishman.org>
2015-04-22 15:39:16 -0700

    Add a SLF4J binding for neo4j-logging

---
+++ community/neo4j-slf4j/src/main/java/org/neo4j/logging/slf4j/Slf4jLog.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright (c) 2002-2015 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.logging.slf4j;
+
+import org.neo4j.function.Consumer;
+import org.neo4j.logging.AbstractLog;
+import org.neo4j.logging.Log;
+import org.neo4j.logging.Logger;
+
+public class Slf4jLog extends AbstractLog
+{
+    private final Object lock;
+    private final org.slf4j.Logger slf4jLogger;
+    private final Logger debugLogger;
+    private final Logger infoLogger;
+    private final Logger warnLogger;
+    private final Logger errorLogger;
+
+    public Slf4jLog( final org.slf4j.Logger slf4jLogger )
+    {
+        this.lock = this;
+        this.slf4jLogger = slf4jLogger;
+
+        this.debugLogger = new Logger()
+        {
+            @Override
+            public void log( String message )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.debug( message );
+                }
+            }
+
+            @Override
+            public void log( String message, Throwable throwable )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.debug( message, throwable );
+                }
+            }
+
+            @Override
+            public void log( String format, Object... arguments )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.debug( format, arguments );
+                }
+            }
+
+            @Override
+            public void bulk( Consumer<Logger> consumer )
+            {
+                synchronized (lock)
+                {
+                    consumer.accept( this );
+                }
+            }
+        };
+
+        this.infoLogger = new Logger()
+        {
+            @Override
+            public void log( String message )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.info( message );
+                }
+            }
+
+            @Override
+            public void log( String message, Throwable throwable )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.info( message, throwable );
+                }
+            }
+
+            @Override
+            public void log( String format, Object... arguments )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.info( format, arguments );
+                }
+            }
+
+            @Override
+            public void bulk( Consumer<Logger> consumer )
+            {
+                synchronized (lock)
+                {
+                    consumer.accept( this );
+                }
+            }
+        };
+
+        this.warnLogger = new Logger()
+        {
+            @Override
+            public void log( String message )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.warn( message );
+                }
+            }
+
+            @Override
+            public void log( String message, Throwable throwable )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.warn( message, throwable );
+                }
+            }
+
+            @Override
+            public void log( String format, Object... arguments )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.warn( format, arguments );
+                }
+            }
+
+            @Override
+            public void bulk( Consumer<Logger> consumer )
+            {
+                synchronized (lock)
+                {
+                    consumer.accept( this );
+                }
+            }
+        };
+
+        this.errorLogger = new Logger()
+        {
+            @Override
+            public void log( String message )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.error( message );
+                }
+            }
+
+            @Override
+            public void log( String message, Throwable throwable )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.error( message, throwable );
+                }
+            }
+
+            @Override
+            public void log( String format, Object... arguments )
+            {
+                synchronized (lock)
+                {
+                    slf4jLogger.error( format, arguments );
+                }
+            }
+
+            @Override
+            public void bulk( Consumer<Logger> consumer )
+            {
+                synchronized (lock)
+                {
+                    consumer.accept( this );
+                }
+            }
+        };
+    }
+
+    @Override
+    public boolean isDebugEnabled()
+    {
+        return slf4jLogger.isDebugEnabled();
+    }
+
+    @Override
+    public Logger debugLogger()
+    {
+        return this.debugLogger;
+    }
+
+    @Override
+    public Logger infoLogger()
+    {
+        return this.infoLogger;
+    }
+
+    @Override
+    public Logger warnLogger()
+    {
+        return this.warnLogger;
+    }
+
+    @Override
+    public Logger errorLogger()
+    {
+        return this.errorLogger;
+    }
+
+    @Override
+    public void bulk( Consumer<Log> consumer )
+    {
+        synchronized (lock)
+        {
+            consumer.accept( this );
+        }
+    }
+}
---
+++ community/neo4j-slf4j/src/test/java/org/neo4j/logging/slf4j/Slf4jLogProviderTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2002-2015 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.logging.slf4j;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.logging.Log;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+
+public class Slf4jLogProviderTest
+{
+    Slf4jLogProvider logProvider = new Slf4jLogProvider();
+
+    @Before
+    public void clearLoggingEventsAccumulator()
+    {
+        getAccumulatingAppender().clearEventsList();
+    }
+
+    @Test
+    public void shouldLogDebug()
+    {
+        Log log = logProvider.getLog( getClass() );
+
+        log.debug( "Holy debug batman!" );
+
+        assertLogOccurred( Level.DEBUG, "Holy debug batman!" );
+    }
+
+    @Test
+    public void shouldLogInfo()
+    {
+        Log log = logProvider.getLog( getClass() );
+
+        log.info( "Holy info batman!" );
+
+        assertLogOccurred( Level.INFO, "Holy info batman!" );
+    }
+
+    @Test
+    public void shouldLogWarning()
+    {
+        Log log = logProvider.getLog( getClass() );
+
+        log.warn( "Holy warning batman!" );
+
+        assertLogOccurred( Level.WARN, "Holy warning batman!" );
+    }
+
+    @Test
+    public void shouldLogError()
+    {
+        Log log = logProvider.getLog( getClass() );
+
+        log.error( "Holy error batman!" );
+
+        assertLogOccurred( Level.ERROR, "Holy error batman!" );
+    }
+
+    private void assertLogOccurred( Level level, String message )
+    {
+        ArrayList<LoggingEvent> events = getLoggingEvents();
+        assertThat( events, hasSize( 1 ) );
+        LoggingEvent event = events.get( 0 );
+        assertThat( event.getLoggerName(), is( getClass().getName() ) );
+        assertThat( event.getLevel(), is( level ) );
+        assertThat( event.getMessage(), is( (Object) message ) );
+    }
+
+    private ArrayList<LoggingEvent> getLoggingEvents()
+    {
+        return getAccumulatingAppender().getEventsList();
+    }
+
+    private AccumulatingAppender getAccumulatingAppender()
+    {
+        return (AccumulatingAppender) Logger.getRootLogger().getAppender( "accumulating" );
+    }
+}
