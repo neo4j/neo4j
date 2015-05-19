a07f067ea2b5c4a2e56d9dd6266f74e48ddfc6e5
lutovich <konstantin.lutovich@neotechnology.com>
2015-05-19 09:43:07 +0200

    Some unit tests in MatchAcceptanceTest migrated to cucumber

--- community/cypher/compatibility-suite/src/test/scala/cypher/GlueSteps.scala
+++ community/cypher/compatibility-suite/src/test/scala/cypher/GlueSteps.scala
@@ -98,16 +98,16 @@ class GlueSteps extends FunSuiteLike with Matchers with  ScalaDsl with EN {
       if (left.isEmpty) {
         right.isEmpty
       } else {
-        compareByKey(left, right, left.keySet)
+        compareByKey(left, right, left.keys.toList.sorted)
       }
     }
 
     @tailrec
-    private def compareByKey(left: collection.Map[String, String], right: collection.Map[String, String], keys: collection.Set[String]): Boolean = {
+    private def compareByKey(left: collection.Map[String, String], right: collection.Map[String, String], keys: collection.Seq[String]): Boolean = {
       val key = keys.head
       val l = left(key)
       val r = right(key)
-      if (l === r)
+      if (l == r)
         compareByKey(left, right, keys.tail)
       else l < r
     }
