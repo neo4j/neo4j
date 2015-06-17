1002dcf23ed737c987011565ba0e294cafa717e3
Mats Rydberg <mats@neotechnology.com>
2015-06-17 14:17:50 +0200

    Fix broken forward merge

--- community/cypher/cypher-compiler-2.3/src/test/scala/org/neo4j/cypher/internal/compiler/v2_3/commands/expressions/PathImplTest.scala
+++ community/cypher/cypher-compiler-2.3/src/test/scala/org/neo4j/cypher/internal/compiler/v2_3/commands/expressions/PathImplTest.scala
@@ -73,7 +73,7 @@ class PathImplTest extends CypherFunSuite {
     badPaths.foreach(p => intercept[IllegalArgumentException](new expressions.PathImpl(p:_*)))
   }
 
-  @Test def retrieveLastRelationshipOnLongPath() {
+  def retrieveLastRelationshipOnLongPath() {
     val nodA = new FakeNode
     val nodB = new FakeNode
     val nodC = new FakeNode
