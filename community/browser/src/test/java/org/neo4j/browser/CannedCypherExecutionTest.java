01b132989506074217b499d464f4ae5b209e4dd8
Alistair Jones <alistair.jones@gmail.com>
2015-08-19 14:12:41 +0100

    EXPLAIN canned queries and check for notifications.

--- community/browser/src/test/java/org/neo4j/browser/CannedCypherExecutionTest.java
+++ community/browser/src/test/java/org/neo4j/browser/CannedCypherExecutionTest.java
@@ -27,6 +27,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -38,6 +40,7 @@ import org.junit.Test;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.QueryExecutionException;
+import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.io.fs.FileUtils;
 import org.neo4j.kernel.impl.util.Charsets;
@@ -45,6 +48,10 @@ import org.neo4j.test.TestGraphDatabaseFactory;
 
 import static java.lang.String.format;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyIterable;
+import static org.jsoup.helper.StringUtil.join;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -65,7 +72,8 @@ public class CannedCypherExecutionTest
         assertNotNull( resourceLoc );
 
         final GraphDatabaseService database = new TestGraphDatabaseFactory().newImpermanentDatabase();
-        final AtomicInteger cypherStatementCount = new AtomicInteger( 0 );
+        final AtomicInteger explainCount = new AtomicInteger( 0 );
+        final AtomicInteger executionCount = new AtomicInteger( 0 );
 
         Files.walkFileTree( Paths.get( resourceLoc.toURI() ), new SimpleFileVisitor<Path>()
         {
@@ -82,9 +90,26 @@ public class CannedCypherExecutionTest
 
                         if ( !statement.startsWith( ":" ) )
                         {
+                            if ( shouldExplain( statement ) )
+                            {
+                                try ( Transaction transaction = database.beginTx() )
+                                {
+                                    Result result = database.execute( prependExplain( statement ) );
+                                    assertThat( format( "Query [%s] should produce no notifications", statement ),
+                                            result.getNotifications(), is( emptyIterable() ) );
+                                    explainCount.incrementAndGet();
+                                    transaction.success();
+                                }
+                                catch ( QueryExecutionException e )
+                                {
+                                    throw new AssertionError( format( "Failed to explain query [%s] in file [%s]",
+                                            statement, file ), e );
+                                }
+                            }
                             try ( Transaction transaction = database.beginTx() )
                             {
                                 database.execute( statement );
+                                executionCount.incrementAndGet();
                                 transaction.success();
                             }
                             catch ( QueryExecutionException e )
@@ -92,7 +117,6 @@ public class CannedCypherExecutionTest
                                 throw new AssertionError( format( "Failed to execute query [%s] in file [%s]",
                                         statement, file ), e );
                             }
-                            cypherStatementCount.incrementAndGet();
                         }
                     }
                 }
@@ -101,18 +125,20 @@ public class CannedCypherExecutionTest
         } );
 
         assertTrue( "Static files should contain at least one valid cypher statement",
-                cypherStatementCount.intValue() > 0 );
-        System.out.printf( "Successfully executed %s cypher statements extracted from HTML files.%n",
-                cypherStatementCount );
+                executionCount.intValue() >= 1 );
+        System.out.printf( "Explained %s cypher statements extracted from HTML files, with no notifications.%n",
+                explainCount );
+        System.out.printf( "Executed %s cypher statements extracted from HTML files, with no errors.%n",
+                executionCount );
     }
 
-    private String replaceAngularExpressions( String statement )
+    private static String replaceAngularExpressions( String statement )
     {
         Pattern angularExpressionPattern = Pattern.compile( "\\{\\{(.*?)}}" );
         Matcher matcher = angularExpressionPattern.matcher( statement );
 
-        StringBuffer buffer = new StringBuffer(  );
-        while( matcher.find() )
+        StringBuffer buffer = new StringBuffer();
+        while ( matcher.find() )
         {
             String expression = matcher.group( 1 );
             matcher.appendReplacement( buffer, chooseSuitableExpressionValue( expression ) );
@@ -121,11 +147,39 @@ public class CannedCypherExecutionTest
         return buffer.toString();
     }
 
-    private String chooseSuitableExpressionValue( String expression )
+    private static String chooseSuitableExpressionValue( String expression )
     {
         // Generally we can safely return any old string, but in rare situations, a number might be
         // required. The rare situation is had-coded below. Unfortunately, if the canned cypher queries use more
         // parameters that need to be integers, this code will have to be updated.
         return "relationshipDepth".equals( expression ) ? "1" : "string";
+    }
+
+    private static boolean shouldExplain( String statement )
+    {
+        return !stripComments( statement ).toUpperCase().startsWith( "PROFILE" );
+    }
+
+    private static String prependExplain( String statement )
+    {
+        if ( !stripComments( statement ).toUpperCase().startsWith( "EXPLAIN" ) )
+        {
+            return "EXPLAIN " + statement;
+        }
+        return statement;
+    }
+
+    private static String stripComments( String statement )
+    {
+        String[] lines = statement.replaceAll( "/\\*.*\\*/", "" ).split( "\n" );
+        List<String> nonCommentLines = new ArrayList<>();
+        for ( String line : lines )
+        {
+            if ( !line.trim().startsWith( "//" ) )
+            {
+                nonCommentLines.add( line );
+            }
+        }
+        return join( nonCommentLines, "\n" );
     }
 }
