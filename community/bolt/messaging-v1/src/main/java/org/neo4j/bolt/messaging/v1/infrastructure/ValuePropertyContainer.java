cc9525cacce45c2f93d4e730dff00e94de6e2a8a
Nigel Small <nigel.small@neotechnology.com>
2015-09-08 02:48:51 +0100

    Fixed package

--- community/bolt/messaging-v1/src/main/java/org/neo4j/bolt/messaging/v1/infrastructure/ValuePropertyContainer.java
+++ community/bolt/messaging-v1/src/main/java/org/neo4j/bolt/messaging/v1/infrastructure/ValuePropertyContainer.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.ndp.messaging.v1.infrastructure;
+package org.neo4j.bolt.messaging.v1.infrastructure;
 
 import java.util.HashMap;
 import java.util.Map;
