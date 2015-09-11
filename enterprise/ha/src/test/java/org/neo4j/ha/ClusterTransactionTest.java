56b1a2509247417886ef9ade365584192b13eb63
Davide Grohmann <davide.grohmann@neotechnology.com>
2015-09-11 14:12:18 +0200

    Fix to test that slave cannot grab lock when master is shutdown
    The old code was green not because we were failing to grab a lock, but
    because the no node with id 0 existed.  Effectively making the test
    useless.

--- enterprise/ha/src/test/java/org/neo4j/ha/ClusterTransactionTest.java
+++ enterprise/ha/src/test/java/org/neo4j/ha/ClusterTransactionTest.java
@@ -19,87 +19,92 @@
  */
 package org.neo4j.ha;
 
+import org.junit.Rule;
+import org.junit.Test;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
-import org.junit.Test;
-
+import org.neo4j.com.ComException;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.kernel.GraphDatabaseAPI;
+import org.neo4j.kernel.api.exceptions.TransactionFailureException;
 import org.neo4j.kernel.ha.HaSettings;
+import org.neo4j.kernel.ha.HighlyAvailableGraphDatabase;
 import org.neo4j.kernel.lifecycle.LifeSupport;
 import org.neo4j.kernel.lifecycle.LifecycleListener;
 import org.neo4j.kernel.lifecycle.LifecycleStatus;
 import org.neo4j.test.ha.ClusterManager;
+import org.neo4j.test.ha.ClusterRule;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
-
-import static org.neo4j.helpers.collection.MapUtil.stringMap;
-import static org.neo4j.test.TargetDirectory.forTest;
 import static org.neo4j.test.ha.ClusterManager.fromXml;
 
 public class ClusterTransactionTest
 {
+    @Rule
+    public final ClusterRule clusterRule = new ClusterRule( getClass() );
+
     @Test
     public void givenClusterWhenShutdownMasterThenCannotStartTransactionOnSlave() throws Throwable
     {
-        // Given
-        ClusterManager clusterManager = new ClusterManager(
-                fromXml( getClass().getResource( "/threeinstances.xml" ).toURI() ),
-                forTest( getClass() ).cleanDirectory( "testCluster" ),
-                stringMap( HaSettings.ha_server.name(), ":6001-6005", HaSettings.tx_push_factor.name(), "2" ) );
-        try
+
+        final ClusterManager.ManagedCluster cluster =
+                clusterRule.provider( fromXml( getClass().getResource( "/threeinstances.xml" ).toURI() ) )
+                        .config( HaSettings.ha_server, ":6001-6005" )
+                        .config( HaSettings.tx_push_factor, "2" ).startCluster();
+
+        cluster.await( ClusterManager.allSeesAllAsAvailable() );
+
+        final HighlyAvailableGraphDatabase master = cluster.getMaster();
+        final HighlyAvailableGraphDatabase slave = cluster.getAnySlave();
+
+        final long nodeId;
+        try ( Transaction tx = master.beginTx() )
         {
-            clusterManager.start();
+            nodeId = master.createNode().getId();
+            tx.success();
+        }
 
-            clusterManager.getDefaultCluster().await( ClusterManager.allSeesAllAsAvailable() );
+        cluster.sync();
 
-            GraphDatabaseAPI master = clusterManager.getDefaultCluster().getMaster();
-            final GraphDatabaseAPI slave = clusterManager.getDefaultCluster().getAnySlave();
-
-            // When
-            final FutureTask<Boolean> result = new FutureTask<>( new Callable<Boolean>()
+        // When
+        final FutureTask<Boolean> result = new FutureTask<>( new Callable<Boolean>()
+        {
+            @Override
+            public Boolean call() throws Exception
             {
-                @Override
-                public Boolean call() throws Exception
+                try ( Transaction tx = slave.beginTx() )
                 {
-                    try ( Transaction tx = slave.beginTx() )
-                    {
-                        tx.acquireWriteLock( slave.getNodeById( 0 ) );
-                        // Fail
-                        return false;
-                    }
-                    catch ( Exception e )
-                    {
-                        // Ok!
-                        return true;
-                    }
+                    tx.acquireWriteLock( slave.getNodeById( nodeId ) );
                 }
-            } );
-            master.getDependencyResolver()
-                    .resolveDependency( LifeSupport.class )
-                    .addLifecycleListener( new LifecycleListener()
+                catch ( ComException e )
+                {
+                    return e.getCause() instanceof TransactionFailureException;
+                }
+                // Fail otherwise
+                return false;
+            }
+        } );
+
+        master.getDependencyResolver()
+                .resolveDependency( LifeSupport.class )
+                .addLifecycleListener( new LifecycleListener()
+                {
+                    @Override
+                    public void notifyStatusChanged( Object instance, LifecycleStatus from, LifecycleStatus to )
                     {
-                        @Override
-                        public void notifyStatusChanged( Object instance, LifecycleStatus from, LifecycleStatus to )
+                        if ( instance.getClass().getName().contains( "DatabaseAvailability" ) &&
+                             to == LifecycleStatus.STOPPED )
                         {
-                            if ( instance.getClass().getName().contains( "DatabaseAvailability" ) &&
-                                    to == LifecycleStatus.STOPPED )
-                            {
-                                result.run();
-                            }
+                            result.run();
                         }
-                    } );
+                    }
+                } );
 
-            master.shutdown();
+        master.shutdown();
 
-            // Then
-            assertThat( result.get(), equalTo( true ) );
-        }
-        finally
-        {
-            clusterManager.stop();
-        }
+        // Then
+        assertThat( result.get(), equalTo( true ) );
     }
 }
