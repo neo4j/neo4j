c8676696be4e99b6bd51e16f79e407a631c53a42
Pontus Melke <pontus.melke@neotechnology.com>
2015-09-17 21:58:39 +0200

    Updated documentation

--- manual/cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/SkipTest.scala
+++ manual/cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/SkipTest.scala
@@ -19,7 +19,7 @@
  */
 package org.neo4j.cypher.docgen
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.{assertEquals, assertTrue}
 import org.junit.Test
 import org.neo4j.graphdb.Node
 import org.neo4j.visualization.graphviz.{AsciiDocSimpleStyle, GraphStyle}
@@ -48,6 +48,15 @@ class SkipTest extends DocumentingTestBase {
       queryText = "match (n) return n order by n.name skip 1 limit 2",
       optionalResultExplanation = "Two nodes from the middle are returned.",
       assertions = (p) => assertEquals(List(node("B"), node("C")), p.columnAs[Node]("n").toList))
+  }
+
+  @Test def returnFromExpression() {
+    testQuery(
+      title = "Skip first from expression",
+      text = "Skip accepts any expression that evaluates to a positive integer as long as it is not referring to any external identifiers:",
+      queryText = "match (n) return n order by n.name skip toInt(3*rand()) + 1",
+      optionalResultExplanation = "The first three nodes are skipped, and only the last two are returned in the result.",
+      assertions = (p) => assertTrue(p.columnAs[Node]("n").nonEmpty))
   }
 }
 
