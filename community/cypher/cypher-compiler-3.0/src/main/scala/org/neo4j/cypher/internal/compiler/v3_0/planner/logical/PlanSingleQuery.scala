5d785ac4a128f1d47f1b1d50e33b30f3ab417325
Craig Taverner <craig@amanzi.com>
2015-09-18 12:13:56 +0200

    Updates to CountByStats refactoring to get counts tests passing

--- community/cypher/cypher-compiler-3.0/src/main/scala/org/neo4j/cypher/internal/compiler/v3_0/planner/logical/PlanSingleQuery.scala
+++ community/cypher/cypher-compiler-3.0/src/main/scala/org/neo4j/cypher/internal/compiler/v3_0/planner/logical/PlanSingleQuery.scala
@@ -21,13 +21,11 @@ package org.neo4j.cypher.internal.compiler.v3_0.planner.logical
 
 import org.neo4j.cypher.internal.compiler.v3_0.pipes.LazyLabel
 import org.neo4j.cypher.internal.compiler.v3_0.planner._
-import org.neo4j.cypher.internal.compiler.v3_0.planner.logical.planPart
 import org.neo4j.cypher.internal.compiler.v3_0.planner.logical.plans.{SimplePatternLength, IdName, PatternRelationship, LogicalPlan}
 import org.neo4j.cypher.internal.compiler.v3_0.planner.logical.steps.verifyBestPlan
-import org.neo4j.cypher.internal.frontend.v3_0.Rewriter
+import org.neo4j.cypher.internal.frontend.v3_0.{SemanticTable, Rewriter}
+import org.neo4j.cypher.internal.frontend.v3_0.SemanticDirection._
 import org.neo4j.cypher.internal.frontend.v3_0.ast._
-
-import scala.collection.Set
 
 /*
 This coordinates PlannerQuery planning and delegates work to the classes that do the actual planning of
@@ -53,49 +51,64 @@ case class PlanSingleQuery(planPart: (PlannerQuery, LogicalPlanningContext, Opti
 
 case object countStorePlanPart extends ((PlannerQuery, LogicalPlanningContext, Option[LogicalPlan]) => Option[LogicalPlan]) {
   def apply(query: PlannerQuery, context: LogicalPlanningContext, leafPlan: Option[LogicalPlan]): Option[LogicalPlan] = {
+    implicit val semanticTable = context.semanticTable
     query.horizon match {
       case AggregatingQueryProjection(groupingKeys, aggregatingExpressions, shuffle)
         if (groupingKeys.isEmpty && aggregatingExpressions.size == 1) => aggregatingExpressions.head match {
+
         case (aggregationIdent, a@FunctionInvocation(FunctionName("count"), false, Vector(Identifier(countName)))) =>
 
-          val queryGraphWithCardinalityEstimation = CardinalityEstimation.lift(query, Cardinality(0))
-
           query.graph match {
+
             case QueryGraph(patternRelationships, patternNodes, argumentIds, selections, Seq(), hints, shortestPathPatterns)
               if hints.isEmpty && shortestPathPatterns.isEmpty =>
                 if (patternNodes.size == 1 && patternRelationships.isEmpty && patternNodes.head.name == countName) {
-                  // MATCH (n) RETURN count(n)
-                  Some(context.logicalPlanProducer.planCountStoreNodeAggregation(queryGraphWithCardinalityEstimation, IdName(aggregationIdent), None, argumentIds))
+                  def planNodeAggr(label: Option[LazyLabel]) =
+                    Some(context.logicalPlanProducer.planCountStoreNodeAggregation(query, IdName(aggregationIdent), label, argumentIds)(context))
+                  convertToLabels(patternNodes.head, selections) match {
+                    // MATCH (n)
+                    case (Nil) => planNodeAggr(None)
+                    // MATCH (n:X)
+                    case (nodeLabel :: Nil) => planNodeAggr(Some(nodeLabel))
+                    // MATCH (n:X:Y) - UNSUPPORTED by stats
+                    case _ => None
+                  }
                 } else if(patternRelationships.size == 1) {
                   // MATCH ()-[r]->(), MATCH ()-[r:X]->(), MATCH ()-[r:X|Y]->()
                   patternRelationships.head match {
-                    case PatternRelationship(relId, (startNodeId, endNodeId), _, types, SimplePatternLength) =>
 
-                      def convertToLabels(nodeId: IdName): Seq[LazyLabel] =
-                        selections.predicates.collectFirst {
-                          case Predicate(nIds,h:HasLabels) if nIds.head.name == nodeId.name =>
-                            h.labels.map(n => new LazyLabel(n.name))
-                        }.getOrElse(Seq.empty[LazyLabel])
+                    case PatternRelationship(relId, (startNodeId, endNodeId), direction, types, SimplePatternLength) =>
 
-                      (convertToLabels(startNodeId), convertToLabels(endNodeId)) match {
-                        case (Nil,  Nil) =>
-                          Some(context.logicalPlanProducer.planCountStoreRelationshipAggregation(queryGraphWithCardinalityEstimation, IdName(aggregationIdent), None, types, None, argumentIds))
-                        case (Nil, endLabel :: Nil) =>
-                          Some(context.logicalPlanProducer.planCountStoreRelationshipAggregation(queryGraphWithCardinalityEstimation, IdName(aggregationIdent), None, types, Some(endLabel), argumentIds))
-                        case (startLabel :: Nil, Nil) =>
-                          Some(context.logicalPlanProducer.planCountStoreRelationshipAggregation(queryGraphWithCardinalityEstimation, IdName(aggregationIdent), Some(startLabel), types, None, argumentIds))
+                      def planRelAggr(fromLabel: Option[LazyLabel], toLabel: Option[LazyLabel]) =
+                        Some(context.logicalPlanProducer.planCountStoreRelationshipAggregation(query, IdName(aggregationIdent), fromLabel, types, toLabel, argumentIds)(context))
+
+                      (convertToLabels(startNodeId, selections), direction, convertToLabels(endNodeId, selections)) match {
+                        case (Nil,               _,        Nil)             => planRelAggr(None, None)
+                        case (Nil,               OUTGOING, endLabel :: Nil) => planRelAggr(None, Some(endLabel))
+                        case (startLabel :: Nil, OUTGOING, Nil)             => planRelAggr(Some(startLabel), None)
+                        case (Nil,               INCOMING, endLabel :: Nil) => planRelAggr(Some(endLabel), None)
+                        case (startLabel :: Nil, INCOMING, Nil)             => planRelAggr(None, Some(startLabel))
                         case _ => None
                       }
+
                     case _ => None
                   }
                 } else None
+
             case _ => None
           }
+
         case _ => None
       }
+
       case _ => None
     }
   }
+  def convertToLabels(nodeId: IdName, selections: Selections)(implicit semanticTable: SemanticTable): Seq[LazyLabel] =
+    selections.predicates.collectFirst {
+      case Predicate(nIds,h:HasLabels) if nIds.head.name == nodeId.name =>
+        h.labels.map(n => LazyLabel(n.name))
+    }.getOrElse(Seq.empty[LazyLabel])
 }
 
 
--- community/cypher/cypher-compiler-3.0/src/main/scala/org/neo4j/cypher/internal/compiler/v3_0/planner/logical/steps/LogicalPlanProducer.scala
+++ community/cypher/cypher-compiler-3.0/src/main/scala/org/neo4j/cypher/internal/compiler/v3_0/planner/logical/steps/LogicalPlanProducer.scala
@@ -336,17 +336,21 @@ case class LogicalPlanProducer(cardinalityModel: CardinalityModel) extends Colle
 
   def planRegularProjection(inner: LogicalPlan, expressions: Map[String, Expression])
                            (implicit context: LogicalPlanningContext) = {
-    val solved = inner.solved.updateTailOrSelf(_.updateQueryProjection(_.withProjections(expressions)))
+    val solved: PlannerQuery = inner.solved.updateTailOrSelf(_.updateQueryProjection(_.withProjections(expressions)))
     Projection(inner, expressions)(solved)
   }
 
-  def planCountStoreNodeAggregation(solved: PlannerQuery with CardinalityEstimation, idName: IdName, label: Option[LazyLabel], argumentIds: Set[IdName]) = {
+  def planCountStoreNodeAggregation(query: PlannerQuery, idName: IdName, label: Option[LazyLabel], argumentIds: Set[IdName])
+                                   (implicit context: LogicalPlanningContext) = {
+    val solved: PlannerQuery = PlannerQuery(query.graph, query.horizon)
     CountStoreNodeAggregation(idName, label, argumentIds)(solved)
   }
 
-  def planCountStoreRelationshipAggregation(solved: PlannerQuery with CardinalityEstimation, idName: IdName, startLabel: Option[LazyLabel],
+  def planCountStoreRelationshipAggregation(query: PlannerQuery, idName: IdName, startLabel: Option[LazyLabel],
                                             typeNames: Seq[RelTypeName], endLabel: Option[LazyLabel],
-                                            argumentIds: Set[IdName]) = {
+                                            argumentIds: Set[IdName])
+                                           (implicit context: LogicalPlanningContext) = {
+    val solved: PlannerQuery = PlannerQuery(query.graph, query.horizon)
     CountStoreRelationshipAggregation(idName, startLabel, typeNames, endLabel, argumentIds)(solved)
   }
 
