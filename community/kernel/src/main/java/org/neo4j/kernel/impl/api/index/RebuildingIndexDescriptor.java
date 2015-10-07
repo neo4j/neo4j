19949f2c1803ba8363ecabafa3cb17d8456c752c
Mattias Persson <mattias@neotechnology.com>
2015-10-07 13:59:25 +0200

    Awaits uniqueness constraint indexes to fully populate during recovery
    before returning from the GDS constructor. Previous there was this
    mismatch where a newly created uniqueness constraint, after a crash, would
    have to be rebuilt from scratch. This is done after the store has
    recovered, so that's fine. However the usualt semantics of creating a
    uniqneness constraint is that the transaction creating it waits for it to
    be fully populated before returning. This added waiting mimics that
    behaviour for recovery. Without it recovery could be considered completed
    while uniquness constraint indexes were still being populated in the
    background.

---
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/api/index/RebuildingIndexDescriptor.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2002-2015 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel.impl.api.index;
+
+import org.neo4j.kernel.api.index.IndexConfiguration;
+import org.neo4j.kernel.api.index.IndexDescriptor;
+import org.neo4j.kernel.api.index.SchemaIndexProvider;
+import org.neo4j.kernel.api.index.SchemaIndexProvider.Descriptor;
+
+/**
+ * Small class for holding a bunch of information about an index that we need to rebuild during recovery.
+ */
+public class RebuildingIndexDescriptor
+{
+    private final IndexDescriptor indexDescriptor;
+    private final Descriptor providerDescriptor;
+    private final IndexConfiguration configuration;
+
+    RebuildingIndexDescriptor( IndexDescriptor indexDescriptor, SchemaIndexProvider.Descriptor providerDescriptor,
+            IndexConfiguration configuration )
+    {
+        this.indexDescriptor = indexDescriptor;
+        this.providerDescriptor = providerDescriptor;
+        this.configuration = configuration;
+    }
+
+    public IndexDescriptor getIndexDescriptor()
+    {
+        return indexDescriptor;
+    }
+
+    public Descriptor getProviderDescriptor()
+    {
+        return providerDescriptor;
+    }
+
+    public IndexConfiguration getConfiguration()
+    {
+        return configuration;
+    }
+}
