c4f350a517e58dcaafcf0935aefd51c4227cdf5d
Andres Taylor <andres@neotechnology.com>
2015-10-15 12:04:57 +0200

    Added the rest of the content for PrettyGraphsTest

--- manual/cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/cookbook/NewPrettyGraphsTest.scala
+++ manual/cypher/cypher-docs/src/test/scala/org/neo4j/cypher/docgen/cookbook/NewPrettyGraphsTest.scala
@@ -36,10 +36,74 @@ class NewPrettyGraphsTest extends DocumentingTest with QueryStatisticsTestSuppor
         graphViz()
       }
     }
+    section("Wheel graph") {
+      p( """This graph is created in a number of steps:
+           |
+           |- Create a center node.
+           |- Once per element in the range, create a leaf and connect it to the center.
+           |- Connect neighboring leafs.
+           |- Find the minimum and maximum leaf and connect these.
+           |- Return the id of the center node.""")
+      query( """CREATE (center)
+               |foreach( x in range(1,6) |
+               |   CREATE (leaf {count:x}), (center)-[:X]->(leaf)
+               |)
+               |WITH center
+               |MATCH (large_leaf)<--(center)-->(small_leaf)
+               |WHERE large_leaf.count = small_leaf.count + 1
+               |CREATE (small_leaf)-[:X]->(large_leaf)
+               |
+               |WITH center, min(small_leaf.count) as min, max(large_leaf.count) as max
+               |MATCH (first_leaf)<--(center)-->(last_leaf)
+               |WHERE first_leaf.count = min AND last_leaf.count = max
+               |CREATE (last_leaf)-[:X]->(first_leaf)
+               |
+               |RETURN id(center) as id""", assertWheelGraph) {
+        p("The query returns the id of the center node.")
+        resultTable()
+        graphViz()
+      }
+    }
+    section("Complete graph") {
+      p(
+        """To create this graph, we first create 6 nodes and label them with the Leaf label.
+          |We then match all the unique pairs of nodes, and create a relationship between them.""".stripMargin)
+      query( """FOREACH (x IN range(1,6)| CREATE (leaf:Leaf { count : x }))
+               |WITH *
+               |MATCH (leaf1:Leaf),(leaf2:Leaf)
+               |WHERE leaf1.count < leaf2.count
+               |CREATE (leaf1)-[:X]->(leaf2)""", assertCompleteGraph) {
+        p("Nothing is returned by this query")
+        resultTable()
+        graphViz()
+      }
+    }
+    section("Friendship graph") {
+      p(
+        """This query first creates a center node, and then once per element in the range, creates a cycle graph and connects it to the center.""".stripMargin)
+      query( """CREATE (center)
+               |FOREACH (x IN range(1,3)| CREATE (leaf1),(leaf2),(center)-[:X]->(leaf1),(center)-[:X]->(leaf2),
+               |  (leaf1)-[:X]->(leaf2))
+               |RETURN ID(center) AS id""", assertFriendshipGraph) {
+        resultTable()
+        graphViz()
+      }
+    }
   }.build()
 
   private def assertAStarIsBorn = ResultAssertions { p =>
     assertStats(p, nodesCreated = 7, relationshipsCreated = 6)
-//    p.toList should equal(List(Map("id" -> 0)))
+  }
+
+  private def assertWheelGraph = ResultAssertions { p =>
+    assertStats(p, nodesCreated = 7, relationshipsCreated = 12, propertiesSet = 6)
+  }
+
+  private def assertCompleteGraph = ResultAssertions { p =>
+    assertStats(p, nodesCreated = 6, relationshipsCreated = 15, propertiesSet = 6, labelsAdded = 6)
+  }
+
+  private def assertFriendshipGraph = ResultAssertions { p =>
+    assertStats(p, nodesCreated = 7, relationshipsCreated = 9)
   }
 }
