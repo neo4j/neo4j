10031079fb78007a4d86b70ad0bfdd3991b2aecc
Mattias Persson <mattias@neotechnology.com>
2015-11-12 16:35:48 +0100

    Adds tool for building db from transaction logs of other db
    As part of this commit also introduces a generic ConsoleInput utility
    which should help building other tools as well.

---
+++ tools/src/main/java/org/neo4j/tools/applytx/ApplyTransactionsCommand.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2002-2015 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.tools.applytx;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.neo4j.graphdb.DependencyResolver;
+import org.neo4j.helpers.Args;
+import org.neo4j.helpers.ArrayUtil;
+import org.neo4j.helpers.Provider;
+import org.neo4j.helpers.progress.ProgressListener;
+import org.neo4j.io.fs.DefaultFileSystemAbstraction;
+import org.neo4j.io.pagecache.PageCache;
+import org.neo4j.kernel.GraphDatabaseAPI;
+import org.neo4j.kernel.KernelHealth;
+import org.neo4j.kernel.api.exceptions.TransactionFailureException;
+import org.neo4j.kernel.api.labelscan.LabelScanStore;
+import org.neo4j.kernel.impl.api.BatchingTransactionRepresentationStoreApplier;
+import org.neo4j.kernel.impl.api.LegacyIndexApplierLookup;
+import org.neo4j.kernel.impl.api.TransactionRepresentationCommitProcess;
+import org.neo4j.kernel.impl.api.index.IndexUpdatesValidator;
+import org.neo4j.kernel.impl.api.index.IndexingService;
+import org.neo4j.kernel.impl.core.CacheAccessBackDoor;
+import org.neo4j.kernel.impl.index.IndexConfigStore;
+import org.neo4j.kernel.impl.locking.LockGroup;
+import org.neo4j.kernel.impl.locking.LockService;
+import org.neo4j.kernel.impl.pagecache.StandalonePageCacheFactory;
+import org.neo4j.kernel.impl.transaction.CommittedTransactionRepresentation;
+import org.neo4j.kernel.impl.transaction.log.IOCursor;
+import org.neo4j.kernel.impl.transaction.log.LogicalTransactionStore;
+import org.neo4j.kernel.impl.transaction.log.ReadOnlyTransactionStore;
+import org.neo4j.kernel.impl.transaction.log.TransactionIdStore;
+import org.neo4j.kernel.impl.transaction.state.NeoStoreProvider;
+import org.neo4j.kernel.impl.transaction.tracing.CommitEvent;
+import org.neo4j.kernel.impl.util.IdOrderingQueue;
+import org.neo4j.kernel.lifecycle.LifeSupport;
+import org.neo4j.kernel.monitoring.Monitors;
+import org.neo4j.tools.console.input.ArgsCommand;
+
+import static java.lang.String.format;
+
+import static org.neo4j.helpers.progress.ProgressMonitorFactory.textual;
+import static org.neo4j.kernel.impl.api.TransactionApplicationMode.RECOVERY;
+
+public class ApplyTransactionsCommand extends ArgsCommand
+{
+    private final File from;
+    private final Provider<GraphDatabaseAPI> to;
+
+    public ApplyTransactionsCommand( File from, Provider<GraphDatabaseAPI> to )
+    {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected void run( Args args, PrintStream out ) throws Exception
+    {
+        TransactionIdStore txIdStore = to.instance().getDependencyResolver().resolveDependency(
+                TransactionIdStore.class );
+        long fromTx = txIdStore.getLastCommittedTransaction().transactionId();
+        long toTx;
+        if ( args.orphans().isEmpty() )
+        {
+            throw new IllegalArgumentException( "No tx specified" );
+        }
+
+        String whereTo = args.orphans().get( 0 );
+        if ( whereTo.equals( "next" ) )
+        {
+            toTx = fromTx + 1;
+        }
+        else if ( whereTo.equals( "last" ) )
+        {
+            toTx = Long.MAX_VALUE;
+        }
+        else
+        {
+            toTx = Long.parseLong( whereTo );
+        }
+
+        long lastApplied = applyTransactions( from, to.instance(), fromTx, toTx, out );
+        out.println( "Applied transactions up to and including " + lastApplied );
+    }
+
+    private long applyTransactions( File fromPath, GraphDatabaseAPI toDb, long fromTxExclusive, long toTxInclusive,
+            PrintStream out )
+            throws IOException, TransactionFailureException
+    {
+        DependencyResolver resolver = toDb.getDependencyResolver();
+        BatchingTransactionRepresentationStoreApplier applier = new BatchingTransactionRepresentationStoreApplier(
+                resolver.resolveDependency( IndexingService.class ),
+                resolver.resolveDependency( LabelScanStore.class ),
+                resolver.resolveDependency( NeoStoreProvider.class ).evaluate(),
+                resolver.resolveDependency( CacheAccessBackDoor.class ),
+                resolver.resolveDependency( LockService.class ),
+                resolver.resolveDependency( LegacyIndexApplierLookup.class ),
+                resolver.resolveDependency( IndexConfigStore.class ),
+                resolver.resolveDependency( KernelHealth.class ),
+                resolver.resolveDependency( IdOrderingQueue.class ) );
+        TransactionRepresentationCommitProcess commitProcess =
+                new TransactionRepresentationCommitProcess(
+                        resolver.resolveDependency( LogicalTransactionStore.class ),
+                        resolver.resolveDependency( NeoStoreProvider.class ).evaluate(),
+                        applier,
+                        resolver.resolveDependency( IndexUpdatesValidator.class ) );
+        LifeSupport life = new LifeSupport();
+        DefaultFileSystemAbstraction fileSystem = new DefaultFileSystemAbstraction();
+        try ( PageCache pageCache = StandalonePageCacheFactory.createPageCache( fileSystem ) )
+        {
+            LogicalTransactionStore source = life.add( new ReadOnlyTransactionStore( fileSystem, fromPath,
+                    new Monitors(), resolver.resolveDependency( KernelHealth.class ) ) );
+            life.start();
+            long lastAppliedTx = fromTxExclusive;
+            // Some progress if there are more than a couple of transactions to apply
+            ProgressListener progress = toTxInclusive - fromTxExclusive >= 100 ?
+                                        textual( out ).singlePart( "Application progress",
+                                                toTxInclusive - fromTxExclusive ) :
+                                        ProgressListener.NONE;
+            try ( IOCursor<CommittedTransactionRepresentation> cursor = source.getTransactions( fromTxExclusive + 1 ) )
+            {
+                while ( cursor.next() )
+                {
+                    CommittedTransactionRepresentation transaction = cursor.get();
+                    try ( LockGroup locks = new LockGroup() )
+                    {
+                        commitProcess.commit( transaction.getTransactionRepresentation(), locks,
+                                CommitEvent.NULL, RECOVERY );
+                        progress.add( 1 );
+                    }
+                    catch ( final Throwable e )
+                    {
+                        System.err.println( "ERROR applying transaction " + transaction.getCommitEntry().getTxId() );
+                        throw e;
+                    }
+                    lastAppliedTx = transaction.getCommitEntry().getTxId();
+                    if ( lastAppliedTx == toTxInclusive )
+                    {
+                        break;
+                    }
+                }
+            }
+            applier.closeBatch();
+            return lastAppliedTx;
+        }
+        finally
+        {
+            life.shutdown();
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return ArrayUtil.join( new String[] {
+                "Applies transaction from the source onto the new db. Example:",
+                "  apply last : applies transactions from the currently last applied and up to the last",
+                "               transaction of source db",
+                "  apply next : applies the next transaction onto the new db",
+                "  apply 234  : applies up to and including tx 234 from the source db onto the new db" },
+                format( "%n" ) );
+    }
+}
---
+++ tools/src/main/java/org/neo4j/tools/console/input/ConsoleInput.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright (c) 2002-2015 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.tools.console.input;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.neo4j.kernel.impl.util.Listener;
+import org.neo4j.kernel.lifecycle.LifecycleAdapter;
+
+import static java.util.Arrays.copyOfRange;
+
+import static org.neo4j.tools.console.input.ConsoleUtil.tokenizeStringWithQuotes;
+
+/**
+ * Useful utility which listens to input from console and reacts to each line, i.e. after each ENTER.
+ * {@link Command} are added with {@link #add(String, Command)} and are then reacted to in a separate thread,
+ * which continuously sits and listens to console input.
+ *
+ * Use of this class can be a shell-like tool which boots up, instantiates a {@link ConsoleInput},
+ * {@link #start() starts it} followed by {@link #waitFor()} which will block until the input stream ends
+ * or an exit command is issued.
+ *
+ * Another use is to instantiate {@link ConsoleInput}, {@link #start() start it} and then move on to do
+ * something else entirely. That way the commands added here will be available user input something in
+ * the console while all other things are happening. In this case {@link #shutdown()} should be called
+ * when the application otherwise shuts down.
+ */
+public class ConsoleInput extends LifecycleAdapter
+{
+    private final Map<String,Command> commands = new HashMap<>();
+    private Reactor reactor;
+    private final BufferedReader inputReader;
+    private final Listener<PrintStream> prompt;
+    private final PrintStream out;
+
+    public ConsoleInput( InputStream input, PrintStream out, Listener<PrintStream> prompt )
+    {
+        this.out = out;
+        this.prompt = prompt;
+        this.inputReader = new BufferedReader( new InputStreamReader( input ) );
+    }
+
+    /**
+     * Add {@link Command} to be available and executed when input uses it.
+     *
+     * @param name command name, i.e the first word of the whole command line to listen for.
+     * @param command {@link Command} to {@link Command#run(String[]) run} as part of command line
+     * starting with {@code name}-
+     */
+    public void add( String name, Command command )
+    {
+        commands.put( name, command );
+    }
+
+    /**
+     * Starts to listen on the input supplied in constructor.
+     */
+    @Override
+    public void start()
+    {
+        reactor = new Reactor();
+        reactor.start();
+    }
+
+    /**
+     * Waits till input stream ends or exit command is given.
+     */
+    public void waitFor() throws InterruptedException
+    {
+        reactor.join();
+    }
+
+    /**
+     * Shuts down and stops listen on the input.
+     */
+    @Override
+    public void stop() throws InterruptedException
+    {
+        reactor.halt();
+        waitFor();
+    }
+
+    /**
+     * Prints usage and help for all available commands.
+     */
+    public void printUsage()
+    {
+        out.println( "Available commands:" );
+        for ( Map.Entry<String, Command> entry : commands.entrySet() )
+        {
+            out.println( entry.getKey() + ": " + entry.getValue() );
+        }
+    }
+
+    private class Reactor extends Thread
+    {
+        private volatile boolean halted;
+
+        public Reactor()
+        {
+            super( ConsoleInput.class.getSimpleName() + " reactor" );
+        }
+
+        void halt()
+        {
+            halted = true;
+
+            // Interrupt this thread since it's probably sitting listening to input.
+            interrupt();
+        }
+
+        @Override
+        public void run()
+        {
+            while ( !halted )
+            {
+                try
+                {
+                    prompt.receive( out );
+                    String commandLine = inputReader.readLine(); // Blocking call
+                    if ( commandLine == null )
+                    {
+                        halted = true;
+                        break;
+                    }
+
+                    String[] args = tokenizeStringWithQuotes( commandLine );
+                    if ( args.length == 0 )
+                    {
+                        continue;
+                    }
+                    String commandName = args[0];
+                    Command action = commands.get( commandName );
+                    if ( action != null )
+                    {
+                        action.run( copyOfRange( args, 1, args.length ), out );
+                    }
+                    else
+                    {
+                        switch ( commandName )
+                        {
+                            case "help":
+                            case "?":
+                            case "man":
+                                printUsage();
+                                break;
+                            case "exit":
+                                halt();
+                                break;
+                            default:
+                                System.err.println( "Unrecognized command '" + commandName + "'" );
+                                break;
+                        }
+                    }
+                }
+                catch ( Exception e )
+                {
+                    e.printStackTrace();
+                    // The show must go on
+                }
+            }
+        }
+    }
+}
