1b019d0f3839319f4b120167d2e6ca501732ff54
Jonas Kalderstam <jonas@kalderstam.se>
2015-11-20 10:46:35 +0100

    Revert ClusterUri changes to fix tyre-kicking
    Revert "Simplify cluster server address logic"
    
    This reverts commit 596b65b3b56c63c340839f53d3a388f2ce9517ff.
    
    Revert "Remove ServerUtil"
    
    This reverts commit c05c691b253aee56a5e97f6207486a936154eaaa.
    
    Revert "Fix cluster uri resolution"
    
    This reverts commit 577e84523b637df035fc11c2bb00771660d777df.

--- enterprise/cluster/src/main/java/org/neo4j/cluster/com/NetworkReceiver.java
+++ enterprise/cluster/src/main/java/org/neo4j/cluster/com/NetworkReceiver.java
@@ -243,14 +243,19 @@ public class NetworkReceiver
 
     private URI getURI( InetSocketAddress address )
     {
-        // Socket.toString() already prepends a '/'
-        String uri = CLUSTER_SCHEME + "://" + address.getHostString() + ":" + address.getPort();
+        String uri;
+
+        if ( address.getAddress().getHostAddress().startsWith( "0" ) )
+            uri = CLUSTER_SCHEME + "://0.0.0.0:" + address.getPort(); // Socket.toString() already prepends a /
+        else
+        {
+            uri = CLUSTER_SCHEME + "://" + address.getAddress().getHostName() + ":" + address.getPort(); // Socket
+        }
+            // .toString() already prepends a /
 
         // Add name if given
-        if ( config.name() != null )
-        {
-            uri += "/?name=" + config.name();
-        }
+        if (config.name() != null)
+            uri += "/?name="+config.name();
 
         return URI.create( uri );
     }
