08d2ce870858166d0a3a5c7d5eb441cad7806b7d
Ben Butler-Cole <ben@bridesmere.com>
2015-12-16 17:08:05 +0000

    Remove deprecated neo4j-install scripts

--- packaging/standalone/src/main/distribution/shell-scripts/bin/base.bat
+++ packaging/standalone/src/main/distribution/shell-scripts/bin/base.bat
@@ -56,7 +56,6 @@ goto:eof
 
   set wrapperJarFilename=#{windows-wrapper.filename}
   set command=""
-  call:parseConfig "%~dps0..\%configFile%"
 
   for /F %%v in ('echo %1^|findstr "^help$ ^console$"') do set command=%%v
 
@@ -97,17 +96,6 @@ goto:eof
   goto:eof
 
 :checkSettings
-  if "%serviceName%" == "" (
-    set settingError=serviceName
-    goto:eof
-  )
-  if "%serviceDisplayName%" == "" (
-    set settingError=serviceDisplayName
-    goto:eof
-  )
-  if %serviceStartType% == "" (
-    serviceStartType=auto
-  )
   if %classpath% == "" (
     set settingError=classpath
     goto:eof
@@ -134,21 +122,6 @@ goto:eof
 
 :help
   echo Proper arguments for this command are: help console
-  goto :eof
-
-:parseConfig
-  set confFileName=%1
-  for /F "tokens=1,2 delims== eol=#" %%a in (%confFileName%) do call:setOption %%a %%b
-  goto :eof
-
-:setOption
-	if "%1"=="wrapper.name" (
-	set serviceName=%2
-	) else if "%1"=="serviceDisplayName" (
-	set serviceDisplayName=%2
-	) else if "%1"=="serviceStartType" (
-	set serviceStartType=%2
-	)
   goto :eof
 
 :instructions
