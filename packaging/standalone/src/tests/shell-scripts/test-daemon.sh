f60df7ca8260f0e4dc8f6d7df235ee8af97c7001
Ben Butler-Cole <ben@bridesmere.com>
2015-12-22 13:42:39 +0000

    Add more tests for scripts running daemon

--- packaging/standalone/src/tests/shell-scripts/test-daemon.sh
+++ packaging/standalone/src/tests/shell-scripts/test-daemon.sh
@@ -6,12 +6,16 @@ test_description="Test happy path operations for the daemon"
 fake_install
 export HARNESS_JAVA_SLEEP=1
 
-test_expect_success "should report that it's not running" "
+test_expect_success "should report that it's not running before started" "
   test_expect_code 3 neo4j-home/bin/neo4j status
 "
 
 test_expect_success "should start" "
-  neo4j-home/bin/neo4j start
+  neo4j-home/bin/neo4j start >neo4j.stdout
+"
+
+test_expect_success "should output server URL" "
+  grep 'http://localhost:7474' neo4j.stdout
 "
 
 test_expect_success "should report that it's running" "
@@ -23,8 +27,16 @@ test_expect_success "should redirect output to console.log" "
   grep 'stderr from java' neo4j-home/data/log/console.log
 "
 
+test_expect_success "should exit 0 if already running" "
+  neo4j-home/bin/neo4j start
+"
+
 test_expect_success "should stop" "
   neo4j-home/bin/neo4j stop
+"
+
+test_expect_success "should report that it's not running once stopped" "
+  test_expect_code 3 neo4j-home/bin/neo4j status
 "
 
 test_done
