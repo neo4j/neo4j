9c09b68f3e593dfe4861d15e22892b77a5bf3784
Henrik Nyman <henrik.nyman@neotechnology.com>
2016-01-19 11:40:37 +0100

    Disable unrelated test

--- community/cypher/acceptance/src/test/scala/org/neo4j/internal/cypher/acceptance/MergeNodeAcceptanceTest.scala
+++ community/cypher/acceptance/src/test/scala/org/neo4j/internal/cypher/acceptance/MergeNodeAcceptanceTest.scala
@@ -524,7 +524,7 @@ class MergeNodeAcceptanceTest extends ExecutionEngineFunSuite with QueryStatisti
     assertStats(result, nodesCreated = 1, labelsAdded = 1, propertiesWritten = 1)
   }
 
-  test("merge should handle arguments properly with only write clauses") {
+  ignore("merge should handle arguments properly with only write clauses") {
     val query = "CREATE (a {p: 1}) MERGE (b {v: a.p})"
 
     val result = updateWithBothPlanners(query)
