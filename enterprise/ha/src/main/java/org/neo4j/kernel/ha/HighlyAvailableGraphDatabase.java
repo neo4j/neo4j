a961f96ab412a97183d207a96522f30e9d09fb7a
Tobias Lindaaker <tobias@thobe.org>
2016-01-22 11:29:37 +0100

    Fix compilation failure in downstream testing

--- enterprise/ha/src/main/java/org/neo4j/kernel/ha/HighlyAvailableGraphDatabase.java
+++ enterprise/ha/src/main/java/org/neo4j/kernel/ha/HighlyAvailableGraphDatabase.java
@@ -38,7 +38,12 @@ public class HighlyAvailableGraphDatabase extends GraphDatabaseFacade
 {
     public HighlyAvailableGraphDatabase( File storeDir, Map<String,String> params, GraphDatabaseFacadeFactory.Dependencies dependencies )
     {
-        new HighlyAvailableFacadeFactory().newFacade( storeDir, params, dependencies, this );
+        newHighlyAvailableFacadeFactory().newFacade( storeDir, params, dependencies, this );
+    }
+
+    protected HighlyAvailableFacadeFactory newHighlyAvailableFacadeFactory()
+    {
+        return new HighlyAvailableFacadeFactory();
     }
 
     public HighAvailabilityMemberState getInstanceState()
