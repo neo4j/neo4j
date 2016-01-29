aa9af00ee4d6ba867215cdff793d44a13bcf0e13
Mats Rydberg <mats@neotechnology.com>
2016-01-29 16:41:01 +0100

    Mark returned deleted entities as deleted

--- community/server/src/test/java/org/neo4j/server/rest/transactional/Neo4jJsonCodecTest.java
+++ community/server/src/test/java/org/neo4j/server/rest/transactional/Neo4jJsonCodecTest.java
@@ -38,7 +38,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 public class Neo4jJsonCodecTest
 {
 
