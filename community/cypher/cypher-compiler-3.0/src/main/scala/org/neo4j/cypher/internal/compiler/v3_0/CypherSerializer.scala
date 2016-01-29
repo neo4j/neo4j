71fc7dc0636a2405f55368adcacd14fc6853bc36
Mats Rydberg <mats@neotechnology.com>
2016-01-29 16:41:01 +0100

    Mark returned deleted entities as deleted

--- community/cypher/cypher-compiler-3.0/src/main/scala/org/neo4j/cypher/internal/compiler/v3_0/CypherSerializer.scala
+++ community/cypher/cypher-compiler-3.0/src/main/scala/org/neo4j/cypher/internal/compiler/v3_0/CypherSerializer.scala
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2002-2016 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.neo4j.cypher.internal.compiler.v3_0
 
 import org.neo4j.cypher.internal.compiler.v3_0.commands.values.KeyToken
@@ -11,12 +30,13 @@ import scala.collection.Map
 trait CypherSerializer {
 
   protected def serializeProperties(x: PropertyContainer, qtx: QueryContext): String = {
-    val (ops, id) = x match {
-      case n: Node => (qtx.nodeOps, n.getId)
-      case r: Relationship => (qtx.relationshipOps, r.getId)
+    val (ops, id, deleted) = x match {
+      case n: Node => (qtx.nodeOps, n.getId, !qtx.nodeOps.exists(n))
+      case r: Relationship => (qtx.relationshipOps, r.getId, !qtx.relationshipOps.exists(r))
     }
 
-    val keyValStrings = ops.propertyKeyIds(id).
+    val keyValStrings = if (deleted) Iterator("deleted")
+    else ops.propertyKeyIds(id).
       map(pkId => qtx.getPropertyKeyName(pkId) + ":" + serialize(ops.getProperty(id, pkId), qtx))
 
     keyValStrings.mkString("{", ",", "}")
