6d7c70f3856810ee8ba082a042bdbea8c9d947a9
Chris Gioran <chris@neotechnology.com>
2016-02-15 17:50:50 +0200

    Fixes compilation errors from last merge

--- enterprise/ha/src/test/java/org/neo4j/kernel/ha/TestFailover.java
+++ enterprise/ha/src/test/java/org/neo4j/kernel/ha/TestFailover.java
@@ -41,7 +41,8 @@ public class TestFailover
 {
     @Rule
     public LoggerRule logger = new LoggerRule();
-    public TargetDirectory dir = TargetDirectory.forTest( getClass() );
+    @Rule
+    public TargetDirectory.TestDirectory dir = TargetDirectory.testDirForTest( getClass() );
 
     // parameters
     private int clusterSize;
@@ -65,10 +66,13 @@ public class TestFailover
     private void testFailOver( int clusterSize ) throws Throwable
     {
         // given
-        ClusterManager clusterManager = new ClusterManager( clusterOfSize( clusterSize ), dir.cleanDirectory( "failover" ), stringMap(
-                ClusterSettings.default_timeout.name(),    "1",
-                ClusterSettings.heartbeat_interval.name(), "1",
-                ClusterSettings.heartbeat_timeout.name(),  "2" ) );
+        ClusterManager clusterManager = new ClusterManager.Builder().withRootDirectory( dir.cleanDirectory( "failover" ) ).
+        withProvider( ClusterManager.clusterOfSize( clusterSize ) )
+                .withSharedConfig( stringMap(
+                    ClusterSettings.default_timeout.name(),    "1",
+                    ClusterSettings.heartbeat_interval.name(), "1",
+                    ClusterSettings.heartbeat_timeout.name(),  "2" ) )
+                .build();
 
         clusterManager.start();
         ClusterManager.ManagedCluster cluster = clusterManager.getDefaultCluster();
@@ -79,13 +83,13 @@ public class TestFailover
         // When
         long start = System.nanoTime();
         ClusterManager.RepairKit repairKit = cluster.fail( oldMaster );
-        logger.getLogger().warn( "Shut down master" );
+        logger.getLogger().warning( "Shut down master" );
 
         // Then
         cluster.await( ClusterManager.masterAvailable( oldMaster ) );
         long end = System.nanoTime();
 
-        logger.getLogger().warn( "Failover took:" + (end - start) / 1000000 + "ms" );
+        logger.getLogger().warning( "Failover took:" + (end - start) / 1000000 + "ms" );
 
         repairKit.repair();
         Thread.sleep( 3000 ); // give repaired instance chance to cleanly rejoin and exit faster
