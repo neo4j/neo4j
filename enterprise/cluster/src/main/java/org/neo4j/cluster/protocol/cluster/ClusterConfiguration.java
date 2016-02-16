815010e3db4873d06f71ec853cafe6ae3a1c79d0
Alistair Jones <alistair.jones@gmail.com>
2016-02-16 11:06:22 +0000

    Revert "Fix failover not working in certain scenarios."
    Reverted because futher work is required in later branches.
    
    This reverts commit c497fcb7594c73c5779471ee553ea6ff1dcf0799.
    
    Conflicts:
    	enterprise/ha/src/test/java/org/neo4j/kernel/ha/TestFailover.java
    	enterprise/ha/src/test/java/org/neo4j/kernel/ha/TestFailoverWithAdditionalSlaveFailures.java

--- enterprise/cluster/src/main/java/org/neo4j/cluster/protocol/cluster/ClusterConfiguration.java
+++ enterprise/cluster/src/main/java/org/neo4j/cluster/protocol/cluster/ClusterConfiguration.java
@@ -47,6 +47,7 @@ public class ClusterConfiguration
     private final List<URI> candidateMembers;
     private Map<InstanceId, URI> members;
     private Map<String, InstanceId> roles = new HashMap<String, InstanceId>();
+    private int allowedFailures = 1;
 
     public ClusterConfiguration( String name, LogProvider logProvider, String... members )
     {
@@ -176,8 +177,7 @@ public class ClusterConfiguration
 
     public int getAllowedFailures()
     {
-        assert members.size() > 0;
-        return (members.size() - 1) / 2;
+        return allowedFailures;
     }
 
     public void left()
@@ -259,6 +259,10 @@ public class ClusterConfiguration
 
         ClusterConfiguration that = (ClusterConfiguration) o;
 
+        if ( allowedFailures != that.allowedFailures )
+        {
+            return false;
+        }
         if ( !candidateMembers.equals( that.candidateMembers ) )
         {
             return false;
@@ -286,6 +290,7 @@ public class ClusterConfiguration
         result = 31 * result + candidateMembers.hashCode();
         result = 31 * result + members.hashCode();
         result = 31 * result + roles.hashCode();
+        result = 31 * result + allowedFailures;
         return result;
     }
 }
