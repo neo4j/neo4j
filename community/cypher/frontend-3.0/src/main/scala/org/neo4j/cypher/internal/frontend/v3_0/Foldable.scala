593208264c28a2d4ddfa0398622d63a81ac2b0b8
Andres Taylor <andres@neotechnology.com>
2016-02-22 11:20:35 +0100

    Refactored method to not be recursive

--- community/cypher/frontend-3.0/src/main/scala/org/neo4j/cypher/internal/frontend/v3_0/Foldable.scala
+++ community/cypher/frontend-3.0/src/main/scala/org/neo4j/cypher/internal/frontend/v3_0/Foldable.scala
@@ -90,8 +90,21 @@ object Foldable {
     def findByClass[A : Manifest]: A =
       findAcc[A](mutable.ArrayStack(that))
 
-    def findByAllClass[A : Manifest]: Seq[A] =
-      findAllAcc[A](mutable.ArrayStack(that))
+    def findByAllClass[A: ClassTag]: Seq[A] = {
+      val remaining = mutable.ArrayStack(that)
+      var result = mutable.ListBuffer[A]()
+
+      while (remaining.nonEmpty) {
+        val that = remaining.pop()
+        that match {
+          case x: A => result += x
+          case _ =>
+        }
+        remaining ++= that.reverseChildren
+      }
+
+      result.toSeq
+    }
   }
 
   @tailrec
@@ -155,19 +168,6 @@ object Foldable {
         case x: A => x
         case _ => findAcc(remaining ++= that.reverseChildren)
       }
-    }
-
-  private def findAllAcc[A: ClassTag](remaining: mutable.ArrayStack[Any]): Seq[A] =
-    if (remaining.isEmpty) {
-      Seq.empty
-    } else {
-      val that = remaining.pop()
-      val thisElement = that match {
-        case x: A => Seq(x)
-        case _ => Seq.empty
-      }
-
-      thisElement ++ findAllAcc(remaining ++= that.reverseChildren)
     }
 }
 
