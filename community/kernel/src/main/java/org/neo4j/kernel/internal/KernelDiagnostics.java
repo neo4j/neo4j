5cc9078bc979e8572821b36ebd6783806523444d
Davide Grohmann <davide.grohmann@neotechnology.com>
2016-02-24 13:07:17 +0100

    Move KernelDiagnostics to internal pkgs

--- community/kernel/src/main/java/org/neo4j/kernel/internal/KernelDiagnostics.java
+++ community/kernel/src/main/java/org/neo4j/kernel/internal/KernelDiagnostics.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.kernel;
+package org.neo4j.kernel.internal;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -33,13 +33,8 @@ import org.neo4j.kernel.impl.factory.DatabaseInfo;
 import org.neo4j.kernel.impl.store.StoreId;
 import org.neo4j.kernel.info.DiagnosticsPhase;
 import org.neo4j.kernel.info.DiagnosticsProvider;
-import org.neo4j.kernel.internal.Version;
 import org.neo4j.logging.Logger;
 
-/**
- * @deprecated This will be moved to internal packages in the next major release.
- */
-@Deprecated
 public abstract class KernelDiagnostics implements DiagnosticsProvider
 {
     public static class Versions extends KernelDiagnostics
