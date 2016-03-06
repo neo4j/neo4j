5e2f5db2650e2bcdd36f94eb8e495e3c4ebd3abc
Steven R. Baker <steven@stevenrbaker.com>
2016-03-06 14:44:41 +0100

    Add log rotation to GC Logging.

--- packaging/standalone/src/tests/shell-scripts/test-java-arguments.sh
+++ packaging/standalone/src/tests/shell-scripts/test-java-arguments.sh
@@ -56,6 +56,18 @@ for run_command in run_console run_daemon; do
     ${run_command} &&
     test_expect_java_arg '-Xloggc:data/log/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintPromotionFailure -XX:+PrintTenuringDistribution'
   "
+
+  test_expect_success "should set gc logging rotation options" "
+    clear_config &&
+    set_config 'dbms.logs.rotation.size' '10m' neo4j.conf &&
+    set_config 'dbms.logs.rotation.keep_number' '8' neo4j.conf &&
+    set_config 'dbms.logs.gc.enabled' 'true' neo4j.conf &&
+    set_config 'dbms.logs.gc.options' '-XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintPromotionFailure -XX:+PrintTenuringDistribution' neo4j.conf &&
+
+    ${run_command} &&
+    test_expect_java_arg '-Xloggc:data/log/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintPromotionFailure -XX:+PrintTenuringDistribution'
+    test_expect_java_arg '-XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=8 -XX:GCLogFileSize=10m'
+  "
 done
 
 test_expect_success "should set heap size constraints when checking version" "
