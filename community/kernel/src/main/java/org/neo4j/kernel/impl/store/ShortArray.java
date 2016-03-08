2ed572a67b33d9372d39a45f0a3ea1850e19dfcd
Mattias Persson <mattias@neotechnology.com>
2016-03-09 00:01:59 +0100

    Cheaper ShortArray#calculateNumberOfBlocksUsed

--- community/kernel/src/main/java/org/neo4j/kernel/impl/store/ShortArray.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/store/ShortArray.java
@@ -629,11 +629,12 @@ public enum ShortArray
         return array.getClass().getComponentType().isPrimitive();
     }
 
-    private static final Map<Class<?>, ShortArray> all = new IdentityHashMap<>( values().length * 2 );
+    private static final ShortArray[] TYPES = ShortArray.values();
+    private static final Map<Class<?>, ShortArray> all = new IdentityHashMap<>( TYPES.length * 2 );
 
     static
     {
-        for ( ShortArray shortArray : values() )
+        for ( ShortArray shortArray : TYPES )
         {
             all.put( shortArray.primitiveClass, shortArray );
             all.put( shortArray.boxedClass, shortArray );
@@ -772,7 +773,7 @@ public enum ShortArray
 
     public static ShortArray typeOf( byte typeId )
     {
-        return ShortArray.values()[typeId-1];
+        return TYPES[typeId-1];
     }
 
     public static ShortArray typeOf( Object array )
@@ -782,17 +783,15 @@ public enum ShortArray
 
     public static int calculateNumberOfBlocksUsed( long firstBlock )
     {
-        Bits bits = Bits.bitsFromLongs( new long[] {firstBlock} );
-        // bbbb][bbll,llll][yyyy,tttt][kkkk,kkkk][kkkk,kkkk][kkkk,kkkk]
-        bits.getInt( 24 ); // Get rid of key
-        bits.getByte( 4 ); // Get rid of short array type
-        bits.getByte( 4 ); // Get rid of the type
-        int arrayLength = bits.getByte( 6 );
-        int requiredBits = bits.getByte( 6 );
+        int highInt = (int) (firstBlock >>> 32);
+        int arrayLength = highInt & 0b11_1111;
+        highInt >>>= 6;
+        int requiredBits = highInt & 0b11_1111;
         if ( requiredBits == 0 )
         {
             requiredBits = 64;
         }
+
         return calculateNumberOfBlocksUsed( arrayLength, requiredBits );
     }
 
