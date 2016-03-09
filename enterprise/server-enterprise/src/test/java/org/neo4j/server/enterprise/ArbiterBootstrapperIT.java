ab22001317bcccc30bfa597030d457fde3a6572e
Steven R. Baker <steven@stevenrbaker.com>
2016-03-09 13:31:30 +0100

    Make StandaloneClusterClient use the configured logs directory.

--- enterprise/server-enterprise/src/test/java/org/neo4j/server/enterprise/ArbiterBootstrapperIT.java
+++ enterprise/server-enterprise/src/test/java/org/neo4j/server/enterprise/ArbiterBootstrapperIT.java
@@ -260,8 +260,7 @@ public class ArbiterBootstrapperIT
 
     private Process startArbiterProcess( File configFile ) throws Exception
     {
-        List<String> args = new ArrayList<>( asList( "java", "-cp", getProperty( "java.class.path" ),
-                "-Dorg.neo4j.cluster.logdirectory=" + directory.getAbsolutePath() ) );
+        List<String> args = new ArrayList<>( asList( "java", "-cp", getProperty( "java.class.path" ) ) );
         if ( configFile != null )
         {
             args.add( "-D" + ServerSettings.SERVER_CONFIG_FILE_KEY + "=" + configFile.getAbsolutePath() );
