5598e888f9a65122fd81fb825be9e4a43d3cab79
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-03-19 22:06:50 -0700

    Update Powershell unit tests for new powershell code
    Updated the unit tests for the new simplified powershell code

--- packaging/standalone/src/tests/Neo4j-Management/Common.ps1
+++ packaging/standalone/src/tests/Neo4j-Management/Common.ps1
@@ -5,14 +5,46 @@ $src = Resolve-Path -Path "$($here)\..\..\main\distribution\shell-scripts\bin\Ne
 
 # Helper functions must be created in the global scope due to the InModuleScope command
 
-Function global:New-MockNeo4jInstall($RootDir, $IncludeFiles = $true, $ServerType = 'Community', $ServerVersion = '0.0', $DatabaseMode = '') {
+Function global:New-MockJavaHome() {
+  $javaHome = "TestDrive:\JavaHome"
+  
+  New-Item $javaHome -ItemType Directory | Out-Null
+  New-Item "$javaHome\bin" -ItemType Directory | Out-Null
+  New-Item "$javaHome\bin\server" -ItemType Directory | Out-Null
+  
+  "This is a mock java.exe" | Out-File "$javaHome\bin\java.exe"
+  "This is a mock java.exe" | Out-File "$javaHome\bin\server\jvm.dll"
+
+  return $javaHome
+}
+
+Function global:New-InvalidNeo4jInstall($ServerType = 'Enterprise', $ServerVersion = '99.99', $DatabaseMode = '') {
+  $serverObject = (New-Object -TypeName PSCustomObject -Property @{
+    'Home' =  'TestDrive:\some-dir-that-doesnt-exist';
+    'ServerVersion' = $ServerVersion;
+    'ServerType' = $ServerType;
+    'DatabaseMode' = $DatabaseMode;
+  })
+  return $serverObject
+}
+
+Function global:New-MockNeo4jInstall($IncludeFiles = $true, $ServerType = 'Community', $ServerVersion = '0.0', $DatabaseMode = '') {
   # Creates a skeleton directory and file structure of a Neo4j Installation
+  $RootDir = "TestDrive:\Neo4j"
   New-Item $RootDir -ItemType Directory | Out-Null
   New-Item "$RootDir\lib" -ItemType Directory | Out-Null
+  New-Item "$RootDir\bin" -ItemType Directory | Out-Null
+  New-Item "$RootDir\bin\tools" -ItemType Directory | Out-Null
   
   if ($IncludeFiles) {
     'TempFile' | Out-File -FilePath "$RootDir\lib\neo4j-server-$($ServerVersion).jar"
     if ($ServerType -eq 'Enterprise') { 'TempFile' | Out-File -FilePath "$RootDir\lib\neo4j-server-enterprise-$($ServerVersion).jar" }
+
+    # Additional Jars
+    'TempFile' | Out-File -FilePath "$RootDir\lib\lib1.jar"
+    'TempFile' | Out-File -FilePath "$RootDir\bin\bin1.jar"
+    'TempFile' | Out-File -FilePath "$RootDir\bin\tools\prunsrv-amd64.exe"
+    'TempFile' | Out-File -FilePath "$RootDir\bin\tools\prunsrv-i386.exe"
   
     # Create fake neo4j.conf
     $neoConf = ''
@@ -21,7 +53,15 @@ Function global:New-MockNeo4jInstall($RootDir, $IncludeFiles = $true, $ServerTyp
       $neoConf += "dbms.mode=$DatabaseMode`n`r"
     }
     
-  New-Item "$RootDir\conf" -ItemType Directory | Out-Null
+    New-Item "$RootDir\conf" -ItemType Directory | Out-Null
     $neoConf | Out-File -FilePath "$RootDir\conf\neo4j.conf"
   }
+  
+  $serverObject = (New-Object -TypeName PSCustomObject -Property @{
+    'Home' = $RootDir;
+    'ServerVersion' = $ServerVersion;
+    'ServerType' = $ServerType;
+    'DatabaseMode' = $DatabaseMode;
+  })
+  return $serverObject
 }
