9bbf038d5ee4af04f55b47359ee0789db073534a
Ben Butler-Cole <ben@bridesmere.com>
2016-03-22 15:45:43 +0000

    Rename auth store setting

--- community/kernel/src/main/java/org/neo4j/graphdb/factory/GraphDatabaseSettings.java
+++ community/kernel/src/main/java/org/neo4j/graphdb/factory/GraphDatabaseSettings.java
@@ -451,7 +451,7 @@ public abstract class GraphDatabaseSettings
     public static final Setting<Boolean> auth_enabled = setting( "dbms.security.auth_enabled", BOOLEAN, "false" );
 
     @Internal
-    public static final Setting<File> auth_store = setting("dbms.security.auth_store.location", PATH, NO_DEFAULT);
+    public static final Setting<File> auth_store = setting("unsupported.dbms.security.auth_store.location", PATH, NO_DEFAULT);
 
     // Bolt Settings
 
