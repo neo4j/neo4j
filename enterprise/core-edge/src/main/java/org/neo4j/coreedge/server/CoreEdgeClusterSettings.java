b2a733fcd380084cb08c082b7c3557d757316794
Chris Gioran <chris@neotechnology.com>
2016-04-05 12:24:58 +0300

    Revert "Revert to naive raft log, while investigating issues with physical"
    This reverts commit 4fc1a689f45e9a33c9476912ee2c3437fc03b0c2.

--- enterprise/core-edge/src/main/java/org/neo4j/coreedge/server/CoreEdgeClusterSettings.java
+++ enterprise/core-edge/src/main/java/org/neo4j/coreedge/server/CoreEdgeClusterSettings.java
@@ -181,7 +181,7 @@ public class CoreEdgeClusterSettings
 
     @Description( "RAFT log implementation" )
     public static final Setting<String> raft_log_implementation =
-            setting( "core_edge.raft_log_implementation", STRING, "NAIVE" );
+            setting( "core_edge.raft_log_implementation", STRING, "PHYSICAL" );
 
     @Description( "RAFT log rotation size" )
     public static final Setting<Long> raft_log_rotation_size =
