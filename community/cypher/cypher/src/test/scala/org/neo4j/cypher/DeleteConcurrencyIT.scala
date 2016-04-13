3b82527073769842212ba7968cbc0da023c81396
Davide Grohmann <davide.grohmann@neotechnology.com>
2016-04-13 10:52:49 +0200

    Deleting a node/relationship should never fail
    The problem was that when trying to delete a node/relationship from a
    transaction, it could fail if another transaction deleted already the
    node/relationship.  This commit will simply ignore the exception and
    continue since the deleting a node/relationship should be an
    idempotent operation.

---
+++ community/cypher/cypher/src/test/scala/org/neo4j/cypher/DeleteConcurrencyIT.scala
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2002-2016 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher
+
+import java.io.{PrintWriter, StringWriter}
+import scala.language.reflectiveCalls
+
+class DeleteConcurrencyIT extends ExecutionEngineFunSuite {
+
+  test("should not fail if a node has been already deleted by another transaction") {
+    graph.inTx {
+      execute("CREATE (n:person)")
+    }
+
+    val threads: List[MyThread] = (0 until 2).map { ignored =>
+      new MyThread(() => {
+        execute(s"MATCH (root) WHERE ID(root) = 0 DELETE root").toList
+      })
+    }.toList
+
+    threads.foreach(_.start())
+    threads.foreach(_.join())
+
+    val errors = threads.collect {
+      case t if t.exception != null => t.exception
+    }
+
+    withClue(prettyPrintErrors(errors)) {
+      errors shouldBe empty
+    }
+  }
+
+  test("should not fail if a relationship has been already deleted by another transaction") {
+    graph.inTx {
+      execute("CREATE (:person)-[:FRIEND]->(:person)")
+    }
+
+    val threads: List[MyThread] = (0 until 2).map { ignored =>
+      new MyThread(() => {
+        execute(s"MATCH ()-[r:FRIEND]->() WHERE ID(r) = 0 DELETE r").toList
+      })
+    }.toList
+
+    threads.foreach(_.start())
+    threads.foreach(_.join())
+
+    val errors = threads.collect {
+      case t if t.exception != null => t.exception
+    }
+
+    withClue(prettyPrintErrors(errors)) {
+      errors shouldBe empty
+    }
+  }
+
+  test("relationship delete deadlock") {
+    graph.inTx {
+      execute("CREATE (p1:person) CREATE (p2:person) CREATE (p1)<-[:T]-(p2)")
+    }
+
+    val threads: List[MyThread] = (0 until 30).map { ignored =>
+      new MyThread(() => {
+        execute(s"MATCH ()-[r]->() WITH r DELETE r").toList
+      })
+    }.toList ++ (0 until 30).map { ignored =>
+      new MyThread(() => {
+        execute(s"MATCH (p1), (p2) WHERE id(p1) < id(p2) CREATE (p2)-[:T]->(p1)").toList
+      })
+    }.toList
+
+    threads.foreach(_.start())
+    threads.foreach(_.join())
+
+    val errors = threads.collect {
+      case t if t.exception != null => t.exception
+    }
+
+    withClue(prettyPrintErrors(errors)) {
+      errors shouldBe empty
+    }
+  }
+
+  private def prettyPrintErrors(errors: Seq[Throwable]): String = {
+    val stringWriter = new StringWriter()
+    val writer = new PrintWriter(stringWriter)
+    errors.foreach { e => e.printStackTrace(writer); writer.println() }
+    stringWriter.toString
+  }
+
+  private class MyThread(f: () => Unit) extends Thread {
+    private var ex: Throwable = null
+
+    def exception: Throwable = ex
+
+    override def run() {
+      try {
+        graph.inTx { f() }
+      } catch {
+        case ex: Throwable => this.ex = ex
+      }
+    }
+  }
+}
