77bc5b7b146ec55eea9444e2e9e5534bbb73df97
Mats Rydberg <mats@neotechnology.com>
2016-05-04 12:00:54 +0200

    Revert "Partially revert 9fa6cd2, and again remove logs"
    This reverts commit 3c8bab9b10d673b0003b468ad4920dc13978070e.

--- community/kernel/src/main/java/org/neo4j/kernel/impl/util/StoreUtil.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/util/StoreUtil.java
@@ -45,7 +45,7 @@ public class StoreUtil
         }
 
         return storeDir.listFiles(file -> {
-            for ( String directory : new String[] {"metrics", "certificates"} )
+            for ( String directory : new String[] {"metrics", "logs", "certificates"} )
             {
                 if ( file.getName().startsWith( directory ) )
                 {
