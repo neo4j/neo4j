e8ec6a27e58556ac2c59309c4787ec32c4d6e7bd
Pontus Melke <pontusmelke@gmail.com>
2016-05-11 16:14:32 +0200

    Added public API for spatial points

---
+++ community/graphdb-api/src/main/java/org/neo4j/graphdb/spatial/CRS.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2002-2016 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphdb.spatial;
+
+
+/**
+ * A coordinate reference system (CRS) determines how a ${@link Coordinate} should be interpreted
+ * <p>
+ * The CRS is defined by three properties a code, a type, and a link to CRS parameters on the web.
+ * Example:
+ * <code>
+ * {
+ * code: 4326,
+ * type: "WGS-84",
+ * href: "http://spatialreference.org/ref/epsg/4326/"
+ * }
+ * </code>
+ */
+public interface CRS
+{
+
+    /**
+     * The numerical code associated with the CRS
+     *
+     * @return a numerical code associated with the CRS
+     */
+    int getCode();
+
+    /**
+     * The type of the CRS is a descriptive name, indicating which CRS is used
+     *
+     * @return the type of the CRS
+     */
+    String getType();
+
+    /**
+     * A link uniquely identifying the CRS.
+     *
+     * @return A link to where the CRS is described.
+     */
+    String getHref();
+}
