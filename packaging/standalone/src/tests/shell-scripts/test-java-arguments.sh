8b7b15b3a9f4fe685e8c5f44c0ff964deddefdc6
Steven R. Baker <steven@stevenrbaker.com>
2016-05-12 15:39:52 +0200

    Resolve paths from config relative to $NEO4J_HOME.

--- packaging/standalone/src/tests/shell-scripts/test-java-arguments.sh
+++ packaging/standalone/src/tests/shell-scripts/test-java-arguments.sh
@@ -41,7 +41,7 @@ for run_command in run_console run_daemon; do
 
   test_expect_success "should construct the classpath and include plugins and conf dirs so that plugins can load config files from the classpath and developers can override plugin classes" "
     ${run_command} &&
-    test_expect_java_arg '-cp plugins:conf:lib/*:plugins/*'
+    test_expect_java_arg '-cp $(neo4j_home)/plugins:$(neo4j_home)/conf:$(neo4j_home)/lib/*:$(neo4j_home)/plugins/*'
   "
 
   test_expect_success "classpath elements should be configurable" "
@@ -49,14 +49,14 @@ for run_command in run_console run_daemon; do
     set_config 'dbms.directories.lib' 'some-other-lib' neo4j.conf &&
     set_config 'dbms.directories.plugins' 'some-other-plugins' neo4j.conf &&
     ${run_command} &&
-    test_expect_java_arg '-cp some-other-plugins:conf:some-other-lib/*:some-other-plugins/*'
+    test_expect_java_arg '-cp $(neo4j_home)/some-other-plugins:$(neo4j_home)/conf:$(neo4j_home)/some-other-lib/*:$(neo4j_home)/some-other-plugins/*'
   "
 
   test_expect_success "should set gc log location when gc log is enabled" "
     clear_config &&
     set_config 'dbms.logs.gc.enabled' 'true' neo4j.conf &&
     ${run_command} &&
-    test_expect_java_arg '-Xloggc:logs/gc.log'
+    test_expect_java_arg '-Xloggc:$(neo4j_home)/logs/gc.log'
   "
 
   test_expect_success "should put gc log into configured logs directory" "
@@ -65,7 +65,7 @@ for run_command in run_console run_daemon; do
     set_config 'dbms.logs.gc.enabled' 'true' neo4j.conf &&
     set_config 'dbms.directories.logs' 'some-other-logs' neo4j.conf &&
     ${run_command} &&
-    test_expect_java_arg '-Xloggc:some-other-logs/gc.log'
+    test_expect_java_arg '-Xloggc:$(neo4j_home)/some-other-logs/gc.log'
   "
 
   test_expect_success "should set gc logging options when gc log is enabled" "
@@ -95,7 +95,7 @@ for run_command in run_console run_daemon; do
 
   test_expect_success "should pass config dir location" "
     ${run_command} &&
-    test_expect_java_arg '--config-dir=conf'
+    test_expect_java_arg '--config-dir=$(neo4j_home)/conf'
   "
 
   test_expect_success "should be able to override config dir location" "
