ad81f6e68192bea7f2ec097612c536a3e091133a
Martin Furmanski <martin.furmanski@neotechnology.com>
2016-05-16 15:58:05 +0200

    backup: test that cluster can be restored from a backup

--- enterprise/core-edge/src/test/java/org/neo4j/coreedge/backup/BackupCoreIT.java
+++ enterprise/core-edge/src/test/java/org/neo4j/coreedge/backup/BackupCoreIT.java
@@ -19,27 +19,30 @@
  */
 package org.neo4j.coreedge.backup;
 
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-
-import org.neo4j.backup.OnlineBackupSettings;
-import org.neo4j.coreedge.discovery.Cluster;
-import org.neo4j.coreedge.server.core.CoreGraphDatabase;
-import org.neo4j.kernel.configuration.Settings;
-import org.neo4j.test.coreedge.ClusterRule;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.factory.GraphDatabaseSettings;
-import org.neo4j.helpers.collection.MapUtil;
-import org.neo4j.kernel.configuration.Config;
-import org.neo4j.kernel.impl.store.format.standard.StandardV3_0;
-import org.neo4j.test.DbRepresentation;
 
 import java.io.File;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Stream;
+
+import org.neo4j.backup.OnlineBackupSettings;
+import org.neo4j.coreedge.convert.ConvertClassicStoreCommand;
+import org.neo4j.coreedge.discovery.Cluster;
+import org.neo4j.coreedge.server.core.CoreGraphDatabase;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
+import org.neo4j.helpers.collection.MapUtil;
+import org.neo4j.io.fs.FileUtils;
+import org.neo4j.kernel.configuration.Config;
+import org.neo4j.kernel.configuration.Settings;
+import org.neo4j.kernel.impl.store.format.standard.StandardV3_0;
+import org.neo4j.test.DbRepresentation;
+import org.neo4j.test.coreedge.ClusterRule;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -48,17 +51,17 @@ import static org.neo4j.graphdb.Label.label;
 
 public class BackupCoreIT
 {
-    @ClassRule
-    public static ClusterRule clusterRule = new ClusterRule( BackupCoreIT.class )
+    @Rule
+    public ClusterRule clusterRule = new ClusterRule( BackupCoreIT.class )
             .withNumberOfCoreServers( 3 )
             .withNumberOfEdgeServers( 0 )
             .withSharedCoreParam( OnlineBackupSettings.online_backup_enabled, Settings.TRUE )
             .withInstanceCoreParam( OnlineBackupSettings.online_backup_server, serverId -> (":" + (8000 + serverId) ) );
-    private static Cluster cluster;
-    private static File backupPath;
+    private Cluster cluster;
+    private File backupPath;
 
-    @BeforeClass
-    public static void setup() throws Exception
+    @Before
+    public void setup() throws Exception
     {
         backupPath = clusterRule.testDirectory().cleanDirectory( "backup-db" );
         cluster = clusterRule.startCluster();
@@ -101,6 +104,33 @@ public class BackupCoreIT
             assertEquals( beforeChange, backupRepresentation );
             assertNotEquals( backupRepresentation, afterChange );
         }
+    }
+
+    @Test
+    public void makeSureCoreClusterCanBeRestoredFromABackup() throws Throwable
+    {
+        // given
+        CoreGraphDatabase db = createSomeData( cluster );
+        DbRepresentation beforeBackup = DbRepresentation.of( db );
+        String[] args = backupArguments(backupAddress(db), backupPath.getPath() );
+        assertEquals( 0, runBackupToolFromOtherJvmToGetExitCode( args ) );
+        cluster.shutdown();
+
+        // when
+        for ( int i = 0; i < cluster.coreServers().size(); i++ )
+        {
+            File coreStoreDir = cluster.coreServerStoreDirectory( i );
+            FileUtils.deleteRecursively( coreStoreDir );
+            FileUtils.copyRecursively( backupPath, coreStoreDir );
+            ConvertClassicStoreCommand convertCommand = new ConvertClassicStoreCommand( coreStoreDir, StandardV3_0.NAME );
+            convertCommand.execute();
+        }
+
+        cluster.start();
+
+        // then
+        Stream<DbRepresentation> dbRepresentations = cluster.coreServers().stream().map( DbRepresentation::of );
+        dbRepresentations.forEach( afterReSeed -> assertEquals( beforeBackup, afterReSeed ) );
     }
 
     static CoreGraphDatabase createSomeData( Cluster cluster ) throws TimeoutException, InterruptedException
