65922faa46771bb1226686974ee2ec9a53c09e63
Martin Furmanski <martin.furmanski@neotechnology.com>
2016-05-16 15:54:37 +0200

    nsds: fix erroneous format printing

--- community/kernel/src/main/java/org/neo4j/kernel/NeoStoreDataSource.java
+++ community/kernel/src/main/java/org/neo4j/kernel/NeoStoreDataSource.java
@@ -484,8 +484,7 @@ public class NeoStoreDataSource implements Lifecycle, IndexProviders
         catch ( Throwable e )
         {
             // Something unexpected happened during startup
-            msgLog.warn( "Exception occurred while setting up store modules. Attempting to close things down.",
-                    e, true );
+            msgLog.warn( "Exception occurred while setting up store modules. Attempting to close things down.", e );
             try
             {
                 // Close the neostore, so that locks are released properly
@@ -508,8 +507,7 @@ public class NeoStoreDataSource implements Lifecycle, IndexProviders
         catch ( Throwable e )
         {
             // Something unexpected happened during startup
-            msgLog.warn( "Exception occurred while starting the datasource. Attempting to close things down.",
-                    e, true );
+            msgLog.warn( "Exception occurred while starting the datasource. Attempting to close things down.", e );
             try
             {
                 life.shutdown();
