757c2045c68c049a646563bece291abb62db2525
Mats Rydberg <mats@neotechnology.com>
2016-06-14 14:07:09 +0200

    Blacklist scenarios that expose bugs

--- community/cypher/compatibility-suite/src/test/scala/cypher/feature/steps/CypherTCKSteps.scala
+++ community/cypher/compatibility-suite/src/test/scala/cypher/feature/steps/CypherTCKSteps.scala
@@ -50,6 +50,8 @@ class CypherTCKSteps extends FunSuiteLike with Matchers with TCKCucumberTemplate
                                  "`toFloat()` failing on invalid arguments",
                                  "`type()` handling `Any` type",
                                  "`type()` failing on invalid arguments",
+                                 "Concatenating lists of different type",
+                                 "Appending to a list of different type",
                                  "Matching relationships into a list and matching variable length using the list", // only broken in rule planner TODO: separate this list between configurations
                                  "Aggregation with `min()`")
 
