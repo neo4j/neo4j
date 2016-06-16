081765d261650373484688e8bb9cf624f37122ce
Mats Rydberg <mats@neotechnology.com>
2016-06-16 10:53:59 +0200

    Make `CREATE UNIQUE` handle a null property map well
    Fixes #7085

--- community/cypher/cypher-compiler-3.0/src/main/scala/org/neo4j/cypher/internal/compiler/v3_0/mutation/GraphElementPropertyFunctions.scala
+++ community/cypher/cypher-compiler-3.0/src/main/scala/org/neo4j/cypher/internal/compiler/v3_0/mutation/GraphElementPropertyFunctions.scala
@@ -23,9 +23,9 @@ import java.util.{Map => JavaMap}
 
 import org.neo4j.cypher.internal.compiler.v3_0._
 import org.neo4j.cypher.internal.compiler.v3_0.commands.expressions.Expression
-import org.neo4j.cypher.internal.compiler.v3_0.helpers.{CastSupport, CollectionSupport}
+import org.neo4j.cypher.internal.compiler.v3_0.helpers.CollectionSupport
 import org.neo4j.cypher.internal.compiler.v3_0.pipes.QueryState
-import org.neo4j.cypher.internal.frontend.v3_0.CypherTypeException
+import org.neo4j.cypher.internal.frontend.v3_0.{CypherTypeException, InvalidArgumentException}
 import org.neo4j.graphdb.{Node, PropertyContainer, Relationship}
 
 import scala.collection.JavaConverters._
@@ -54,6 +54,7 @@ trait GraphElementPropertyFunctions extends CollectionSupport {
 
   def getMapFromExpression(v: Any): Map[String, Any] = {
     v match {
+      case null                    => throw new InvalidArgumentException("Property map expression is null")
       case _: collection.Map[_, _] => v.asInstanceOf[collection.Map[String, Any]].toMap
       case _: JavaMap[_, _]        => v.asInstanceOf[JavaMap[String, Any]].asScala.toMap
       case _                       => throw new CypherTypeException(s"Don't know how to extract parameters from this type: ${v.getClass.getName}")
