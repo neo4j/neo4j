30091f04f31aad965a8e72dc5cda0f526c4fa6a0
lutovich <konstantin.lutovich@neotechnology.com>
2016-07-06 23:29:15 +0200

    Make SlaveLocksClient return correct delegate
    Before this commit it was returning the underlying Locks as it's delegate.
    This was wrong because delegate does not actually mean 'delegate'. Instead it
    means client itself, if deferred locks are not used, and real underlying client,
    if deferred locks are used.

--- enterprise/ha/src/main/java/org/neo4j/kernel/ha/lock/SlaveLocksClient.java
+++ enterprise/ha/src/main/java/org/neo4j/kernel/ha/lock/SlaveLocksClient.java
@@ -325,12 +325,6 @@ class SlaveLocksClient extends Locks.ClientAdapter
         }
     }
 
-    @Override
-    public Locks.Client delegate()
-    {
-        return client;
-    }
-
     private void acquireSharedOnMaster( Locks.ResourceType resourceType, long... resourceIds )
     {
         if ( resourceType == ResourceTypes.NODE
