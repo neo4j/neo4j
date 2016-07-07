372d91000d2afda243d7dfe70b0516b488e226f0
Chris Vest <mr.chrisvest@gmail.com>
2016-07-07 11:38:42 +0200

    Use AssertableLogProvider for testing

--- community/kernel/src/test/java/org/neo4j/kernel/impl/pagecache/ConfiguringPageCacheFactoryTest.java
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/pagecache/ConfiguringPageCacheFactoryTest.java
@@ -27,11 +27,11 @@ import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.io.pagecache.PageCache;
 import org.neo4j.io.pagecache.tracing.PageCacheTracer;
 import org.neo4j.kernel.configuration.Config;
-import org.neo4j.logging.BufferingLog;
+import org.neo4j.logging.AssertableLogProvider;
+import org.neo4j.logging.Log;
 import org.neo4j.logging.NullLog;
 import org.neo4j.test.EphemeralFileSystemRule;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -84,7 +84,8 @@ public class ConfiguringPageCacheFactoryTest
         Config config = new Config( stringMap(
                 pagecache_memory.name(), "8m",
                 pagecache_swapper.name(), TEST_PAGESWAPPER_NAME ) );
-        BufferingLog log = new BufferingLog();
+        AssertableLogProvider logProvider = new AssertableLogProvider();
+        Log log = logProvider.getLog( PageCache.class );
 
         // When
         new ConfiguringPageCacheFactory( fsRule.get(), config, PageCacheTracer.NULL, log );
@@ -92,7 +93,7 @@ public class ConfiguringPageCacheFactoryTest
         // Then
         assertThat( PageSwapperFactoryForTesting.countCreatedPageSwapperFactories(), is( 1 ) );
         assertThat( PageSwapperFactoryForTesting.countConfiguredPageSwapperFactories(), is( 1 ) );
-        assertThat( log.toString(), containsString( TEST_PAGESWAPPER_NAME ) );
+        logProvider.assertContainsMessageContaining( TEST_PAGESWAPPER_NAME );
     }
 
     @Test( expected = IllegalArgumentException.class )
