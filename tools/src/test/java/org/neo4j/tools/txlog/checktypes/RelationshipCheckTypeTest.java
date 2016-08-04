197ba69157fb485cf18c8d473348411173b803bd
lutovich <konstantin.lutovich@neotechnology.com>
2016-08-04 14:46:22 +0200

    Relax relationship command check in CheckTxLogs
    Now it will compare inUse records by all fields and !inUse records only by id.
    This is needed because type is written out to the tx log for deleted
    relationships but when record is reused it's type is initialized to default
    value (-1) as all other pointers. So previous after state and next before state
    for relationship records might not match.

---
+++ tools/src/test/java/org/neo4j/tools/txlog/checktypes/RelationshipCheckTypeTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2002-2016 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.tools.txlog.checktypes;
+
+import org.junit.Test;
+
+import org.neo4j.kernel.impl.store.record.RelationshipRecord;
+
+import static org.junit.Assert.assertTrue;
+
+public class RelationshipCheckTypeTest
+{
+    @Test
+    public void inUseRecordEquality()
+    {
+        RelationshipRecord record1 = new RelationshipRecord( 1 );
+        record1.initialize( true, 1, 2, 3, 4, 5, 6, 7, 8, true, false );
+        record1.setSecondaryUnitId( 42 );
+
+        RelationshipRecord record2 = record1.clone();
+
+        RelationshipCheckType check = new RelationshipCheckType();
+
+        assertTrue( check.equal( record1, record2 ) );
+    }
+
+    @Test
+    public void notInUseRecordEquality()
+    {
+        RelationshipRecord record1 = new RelationshipRecord( 1 );
+        record1.initialize( false, 1, 2, 3, 4, 5, 6, 7, 8, true, false );
+        record1.setSecondaryUnitId( 42 );
+
+        RelationshipRecord record2 = new RelationshipRecord( 1 );
+        record2.initialize( false, 11, 22, 33, 44, 55, 66, 77, 88, false, true );
+        record2.setSecondaryUnitId( 24 );
+
+        RelationshipCheckType check = new RelationshipCheckType();
+
+        assertTrue( check.equal( record1, record2 ) );
+    }
+}
