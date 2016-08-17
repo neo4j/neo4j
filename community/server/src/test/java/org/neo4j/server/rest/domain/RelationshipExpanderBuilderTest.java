0536755077f9e63b7e4f12122e7f63625e125982
Tobias Lindaaker <tobias@thobe.org>
2016-08-17 14:53:41 +0200

    Update package name reference
    Logical merge conflict not spotted during merging.

--- community/server/src/test/java/org/neo4j/server/rest/domain/RelationshipExpanderBuilderTest.java
+++ community/server/src/test/java/org/neo4j/server/rest/domain/RelationshipExpanderBuilderTest.java
@@ -29,8 +29,8 @@ import org.neo4j.graphdb.PathExpander;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.impl.MyRelTypes;
-import org.neo4j.test.DatabaseRule;
-import org.neo4j.test.ImpermanentDatabaseRule;
+import org.neo4j.test.rule.DatabaseRule;
+import org.neo4j.test.rule.ImpermanentDatabaseRule;
 
 import static org.junit.Assert.assertEquals;
 
