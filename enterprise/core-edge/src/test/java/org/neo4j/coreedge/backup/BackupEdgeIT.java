f392e902b40f1bbf842b5fc73cc41849b9bdd293
jimwebber <jim@jimwebber.org>
2016-08-31 14:26:59 +0100

    Enabling Backup from Edge Servers.
    Historically Edge servers could not back up because of cluster state (and their transient nature).
    This commit enables backup from Edge servers giving operators more deployment choices.

--- enterprise/core-edge/src/test/java/org/neo4j/coreedge/backup/BackupEdgeIT.java
+++ enterprise/core-edge/src/test/java/org/neo4j/coreedge/backup/BackupEdgeIT.java
@@ -19,47 +19,103 @@
  */
 package org.neo4j.coreedge.backup;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.io.File;
-
 import org.neo4j.backup.OnlineBackupSettings;
+import org.neo4j.coreedge.discovery.Cluster;
+import org.neo4j.coreedge.edge.EdgeGraphDatabase;
 import org.neo4j.kernel.configuration.Settings;
+import org.neo4j.test.DbRepresentation;
 import org.neo4j.test.coreedge.ClusterRule;
 import org.neo4j.test.rule.SuppressOutput;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import static org.neo4j.backup.BackupEmbeddedIT.runBackupToolFromOtherJvmToGetExitCode;
+import static org.neo4j.coreedge.backup.BackupCoreIT.backupAddress;
 import static org.neo4j.coreedge.backup.BackupCoreIT.backupArguments;
+import static org.neo4j.coreedge.backup.BackupCoreIT.createSomeData;
+import static org.neo4j.coreedge.backup.BackupCoreIT.getConfig;
+import static org.neo4j.test.rule.SuppressOutput.suppress;
 
 public class BackupEdgeIT
 {
     @Rule
-    public SuppressOutput suppressOutput = SuppressOutput.suppress( SuppressOutput.System.out );
+    public SuppressOutput suppressOutput = suppress( SuppressOutput.System.out, SuppressOutput.System.err );
 
     @Rule
-    public ClusterRule clusterRule = new ClusterRule( BackupCoreIT.class )
+    public ClusterRule clusterRule = new ClusterRule( BackupEdgeIT.class )
             .withNumberOfCoreMembers( 3 )
             .withSharedCoreParam( OnlineBackupSettings.online_backup_enabled, Settings.FALSE )
             .withNumberOfEdgeMembers( 1 )
             .withSharedEdgeParam( OnlineBackupSettings.online_backup_enabled, Settings.TRUE )
-            .withInstanceEdgeParam( OnlineBackupSettings.online_backup_server, serverId -> ":8000" );
+            .withInstanceEdgeParam( OnlineBackupSettings.online_backup_server, serverId -> ":" + findFreePort( 8000, 9000 ) );
 
+    private Cluster cluster;
     private File backupPath;
 
     @Before
     public void setup() throws Exception
     {
         backupPath = clusterRule.testDirectory().cleanDirectory( "backup-db" );
-        clusterRule.startCluster();
+        cluster = clusterRule.startCluster();
     }
 
     @Test
-    public void makeSureBackupCannotBePerformed() throws Throwable
+    public void makeSureBackupCanBePerformed() throws Throwable
     {
-        String[] args = backupArguments( "localhost:8000", backupPath.getPath() );
-        assertEquals( 1, runBackupToolFromOtherJvmToGetExitCode( args ) );
+        // Run backup
+        createSomeData( cluster );
+
+        EdgeGraphDatabase db = cluster.findAnEdgeMember().database();
+
+        DbRepresentation beforeChange = DbRepresentation.of( db );
+        String backupAddress = backupAddress( db );
+        System.out.println( backupAddress );
+        String[] args = backupArguments( backupAddress, backupPath.getPath() );
+        assertEquals( 0, runBackupToolFromOtherJvmToGetExitCode( args ) );
+
+        // Add some new data
+        DbRepresentation afterChange = DbRepresentation.of( createSomeData( cluster ) );
+
+        // Verify that backed up database can be started and compare representation
+        DbRepresentation backupRepresentation = DbRepresentation.of( backupPath, getConfig() );
+        assertEquals( beforeChange, backupRepresentation );
+        assertNotEquals( backupRepresentation, afterChange );
+    }
+
+    private static int findFreePort( int startRange, int endRange )
+    {
+        InetSocketAddress address = null;
+        RuntimeException ex = null;
+        for ( int port = startRange; port <= endRange; port++ )
+        {
+            address = new InetSocketAddress( port );
+
+            try
+            {
+                new ServerSocket( address.getPort(), 100, address.getAddress() ).close();
+                ex = null;
+                break;
+            }
+            catch ( IOException e )
+            {
+                ex = new RuntimeException( e );
+            }
+        }
+        if ( ex != null )
+        {
+            throw ex;
+        }
+        assert address != null;
+        return address.getPort();
     }
 }
