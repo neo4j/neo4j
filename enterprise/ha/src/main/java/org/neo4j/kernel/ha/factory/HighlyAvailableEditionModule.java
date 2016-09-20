49c5e8790a389abfb45336b430f9abc943d45dec
Chris Vest <mr.chrisvest@gmail.com>
2016-09-20 12:04:01 +0200

    Allow HA to boot up with custom IO subsystem configurations

--- enterprise/ha/src/main/java/org/neo4j/kernel/ha/factory/HighlyAvailableEditionModule.java
+++ enterprise/ha/src/main/java/org/neo4j/kernel/ha/factory/HighlyAvailableEditionModule.java
@@ -156,7 +156,6 @@ import org.neo4j.kernel.impl.transaction.log.ReadableClosablePositionAwareChanne
 import org.neo4j.kernel.impl.transaction.log.TransactionIdStore;
 import org.neo4j.kernel.impl.transaction.log.checkpoint.CheckPointer;
 import org.neo4j.kernel.impl.transaction.log.entry.LogEntryReader;
-import org.neo4j.kernel.impl.util.CustomIOConfigValidator;
 import org.neo4j.kernel.impl.util.Dependencies;
 import org.neo4j.kernel.impl.util.JobScheduler;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
@@ -181,8 +180,6 @@ import static org.neo4j.kernel.impl.transaction.log.TransactionMetadataCache.Tra
 public class HighlyAvailableEditionModule
         extends EditionModule
 {
-    public static final String CUSTOM_IO_EXCEPTION_MESSAGE = "HA mode not allowed with custom IO integrations";
-
     private HighAvailabilityMemberStateMachine memberStateMachine;
     public ClusterMembers members;
     private SecurityLog securityLog;
@@ -220,9 +217,6 @@ public class HighlyAvailableEditionModule
         final Monitors monitors = platformModule.monitors;
 
         idTypeConfigurationProvider = new EnterpriseIdTypeConfigurationProvider( config );
-
-        //Temporary check for custom IO
-        CustomIOConfigValidator.assertCustomIOConfigNotUsed( config, CUSTOM_IO_EXCEPTION_MESSAGE );
 
         // Set Netty logger
         InternalLoggerFactory.setDefaultFactory( new NettyLoggerFactory( logging.getInternalLogProvider() ) );
