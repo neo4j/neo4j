5ce23e1c6e188808d23d33dd5dd933fbaab0f323
Chris Vest <mr.chrisvest@gmail.com>
2016-09-30 11:03:49 +0200

    Add javadocs for PageSwapperFactory.streamFilesRecursive

--- community/io/src/main/java/org/neo4j/io/pagecache/PageSwapperFactory.java
+++ community/io/src/main/java/org/neo4j/io/pagecache/PageSwapperFactory.java
@@ -22,6 +22,7 @@ package org.neo4j.io.pagecache;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.CopyOption;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
@@ -33,6 +34,9 @@ import org.neo4j.io.fs.FileSystemAbstraction;
  * A PageSwapper is responsible for swapping file pages in and out of memory.
  * <p>
  * The PageSwapperFactory presumably knows about what file system to use.
+ * <p>
+ * Note that this API is <em>only</em> intended to be used by a {@link PageCache} implementation.
+ * It should never be used directly by user code.
  */
 public interface PageSwapperFactory
 {
@@ -118,5 +122,30 @@ public interface PageSwapperFactory
      */
     void renameUnopenedFile( File sourceFile, File targetFile, CopyOption... copyOptions ) throws IOException;
 
+    /**
+     * Return a stream of {@link FileHandle file handles} for every file in the given directory, and its
+     * sub-directories.
+     * <p>
+     * Alternatively, if the {@link File} given as an argument refers to a file instead of a directory, then a stream
+     * will be returned with a file handle for just that file.
+     * <p>
+     * The stream is based on a snapshot of the file tree, so changes made to the tree using the returned file handles
+     * will not be reflected in the stream.
+     * <p>
+     * No directories will be returned. Only files. If a file handle ends up leaving a directory empty through a
+     * rename or a delete, then the empty directory will automatically be deleted as well.
+     * Likewise, if a file is moved to a path where not all of the directories in the path exists, then those missing
+     * directories will be created prior to the file rename.
+     * <p>
+     * This method form the basis of the implementation of the {@link PageCache#streamFilesRecursive(File)} method.
+     * The key difference is that the stream and file handles are oblivious about which files are mapped or not.
+     * Thus, the returned {@link FileHandle file handles} will never throw any
+     * {@link org.neo4j.io.pagecache.impl.FileIsMappedException}s.
+     *
+     * @param directory The base directory to start streaming files from, or the specific individual file to stream.
+     * @return A stream of all files in the tree.
+     * @throws NoSuchFileException If the given base directory or file does not exists.
+     * @throws IOException If an I/O error occurs, possibly with the canonicalisation of the paths.
+     */
     Stream<FileHandle> streamFilesRecursive( File directory ) throws IOException;
 }
