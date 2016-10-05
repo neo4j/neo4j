63f45db45f2baf72b03f535b7edc2ff84b86bd18
Mattias Persson <mattias@neotechnology.com>
2016-10-05 13:08:26 +0200

    Increased read timeout in StoreMigratorFrom20IT
    since apparently store copy in this test goes very slowly in CI

--- enterprise/neo4j-enterprise/src/test/java/org/neo4j/upgrade/StoreMigratorFrom20IT.java
+++ enterprise/neo4j-enterprise/src/test/java/org/neo4j/upgrade/StoreMigratorFrom20IT.java
@@ -47,6 +47,7 @@ import org.neo4j.kernel.api.impl.index.storage.DirectoryFactory;
 import org.neo4j.kernel.api.impl.schema.LuceneSchemaIndexProvider;
 import org.neo4j.kernel.api.index.SchemaIndexProvider;
 import org.neo4j.kernel.configuration.Config;
+import org.neo4j.kernel.ha.HaSettings;
 import org.neo4j.kernel.ha.HighlyAvailableGraphDatabase;
 import org.neo4j.kernel.impl.MyRelTypes;
 import org.neo4j.kernel.impl.api.scan.InMemoryLabelScanStore;
@@ -251,7 +252,9 @@ public class StoreMigratorFrom20IT
 
     private Config getConfig()
     {
-        return new Config( stringMap( GraphDatabaseSettings.record_format.name(), recordFormatName ),
+        return new Config( stringMap(
+                GraphDatabaseSettings.record_format.name(), recordFormatName,
+                HaSettings.read_timeout.name(), "2m" ),
                 GraphDatabaseSettings.class );
     }
 }
