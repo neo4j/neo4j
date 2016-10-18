4e4f0573387e8eade98af52044edd22207e4265a
Steven R. Baker <steven@stevenrbaker.com>
2016-10-18 20:05:47 +0200

    Improve the help output for `neo4j-admin import`.

--- community/dbms/src/main/java/org/neo4j/commandline/dbms/CsvImporter.java
+++ community/dbms/src/main/java/org/neo4j/commandline/dbms/CsvImporter.java
@@ -68,22 +68,24 @@ class CsvImporter implements Importer
 
     public static String description()
     {
-        return "--mode=csv Import a database from a collection of CSV files.\n" + "--report-file <filename>\n" +
+        return "--mode=csv\n" +
+                "        Import a database from a collection of CSV files.\n" +
+                "--report-file=<filename>\n" +
                 "        File name in which to store the report of the import.\n" +
-                "        Defaults to " + ImportCommand.DEFAULT_REPORT_FILE_NAME +
-                " in the current directory.\n" +
+                "        Defaults to " + ImportCommand.DEFAULT_REPORT_FILE_NAME + " in the current directory.\n" +
                 "--nodes[:Label1:Label2]=\"<file1>,<file2>,...\"\n" +
                 "        Node CSV header and data. Multiple files will be logically seen as\n" +
                 "        one big file from the perspective of the importer. The first line\n" +
                 "        must contain the header. Multiple data sources like these can be\n" +
                 "        specified in one import, where each data source has its own header.\n" +
                 "        Note that file groups must be enclosed in quotation marks.\n" +
-                "--relationships[:RELATIONSHIP_TYPE] \"<file1>,<file2>,...\"\n" +
+                "--relationships[:RELATIONSHIP_TYPE]=\"<file1>,<file2>,...\"\n" +
                 "        Relationship CSV header and data. Multiple files will be logically\n" +
                 "        seen as one big file from the perspective of the importer. The first\n" +
                 "        line must contain the header. Multiple data sources like these can be\n" +
                 "        specified in one import, where each data source has its own header.\n" +
-                "        Note that file groups must be enclosed in quotation marks.\n" + "--id-type <id-type>\n" +
+                "        Note that file groups must be enclosed in quotation marks.\n" +
+                "--id-type=<id-type>\n" +
                 "        Each node must provide a unique id. This is used to find the correct\n" +
                 "        nodes when creating relationships. Must be one of:\n" +
                 "            STRING: (default) arbitrary strings for identifying nodes.\n" +
@@ -91,16 +93,20 @@ class CsvImporter implements Importer
                 "            ACTUAL: (advanced) actual node ids. The default option is STRING.\n" +
                 "        For more information on id handling, please see the Neo4j Manual:\n" +
                 "        http://neo4j.com/docs/operations-manual/current/deployment/#import-tool\n" +
-                "--input-encoding <character-set>\n" +
+                "--input-encoding=<character-set>\n" +
                 "        Character set that input data is encoded in. Defaults to UTF-8.\n" +
-                "--page-size <page-size>\n" + "        Page size to use for import in bytes. (e. g. 4M or 8k)\n";
+                "--page-size=<page-size>\n" +
+                "        Page size to use for import in bytes. (e. g. 4M or 8k)\n";
     }
 
     public static String arguments()
     {
-        return "[--report-file=<filename>] [--nodes[:Label1:Label2]=\"<file1>,<file2>,...\"] " +
+        return "[--report-file=<filename>] " +
+                "[--nodes[:Label1:Label2]=\"<file1>,<file2>,...\"] " +
                 "[--relationships[:RELATIONSHIP_TYPE]=\"<file1>,<file2>,...\"] " +
-                "[--input-encoding=<character-set>] " + "[--id-type=<id-type>] ";
+                "[--id-type=<id-type>] " +
+                "[--input-encoding=<character-set>] " +
+                "[--page-size=<page-size>]";
     }
 
     CsvImporter( Args args, Config config, OutsideWorld outsideWorld ) throws IncorrectUsage
