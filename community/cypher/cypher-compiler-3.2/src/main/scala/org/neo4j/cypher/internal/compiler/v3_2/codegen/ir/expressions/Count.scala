pontusmelke/neo4j-old

Compiled runtime support for count with no grouping key

community/cypher/cypher-compiler-3.1/src/main/scala/org/neo4j/cypher/internal/compiler/v3_1/codegen/ir/expressions/Count.scala
@@ -1,0 +1,52 @@
+/*
+ * Copyright (c) 2002-2016 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.compiler.v3_1.codegen.ir.expressions
+
+import org.neo4j.cypher.internal.compiler.v3_1.codegen.{CodeGenContext, MethodStructure}
+import org.neo4j.cypher.internal.frontend.v3_1.symbols._
+
+
+trait AggregateExpression extends CodeGenExpression {
+  def initialValue[E](generator: MethodStructure[E])(implicit context: CodeGenContext): E
+}
+
+case class Count(expression: CodeGenExpression) extends AggregateExpression {
+
+  private var name: String = null
+  def init[E](generator: MethodStructure[E])(implicit context: CodeGenContext) = {
+    expression.init(generator)
+    name = context.namer.newVarName()
+    generator.assign(name, CodeGenType.primitiveInt, generator.constantExpression(Long.box(0L)))
+  }
+
+  def initialValue[E](generator: MethodStructure[E])(implicit context: CodeGenContext): E =
+      generator.constantExpression(Long.box(0L))
+
+  def generateExpression[E](structure: MethodStructure[E])(implicit context: CodeGenContext) = {
+    structure.ifNonNullStatement(expression.generateExpression(structure)) { body =>
+      body.incrementInteger(name)
+    }
+    structure.loadVariable(name)
+  }
+
+  override def nullable(implicit context: CodeGenContext) = false
+
+  override def codeGenType(implicit context: CodeGenContext) = CodeGenType(CTInteger, IntType)
+}

community/cypher/cypher-compiler-3.1/src/main/scala/org/neo4j/cypher/internal/compiler/v3_1/planner/logical/LogicalPlan2PlanDescription.scala
@@ -19,6 +19,7 @@
  */
 package org.neo4j.cypher.internal.compiler.v3_1.planner.logical
 
+import org.neo4j.cypher.internal.compiler.v3_1.planDescription.InternalPlanDescription.Arguments
 import org.neo4j.cypher.internal.compiler.v3_1.planDescription.InternalPlanDescription.Arguments._
 import org.neo4j.cypher.internal.compiler.v3_1.planDescription._
 import org.neo4j.cypher.internal.compiler.v3_1.planner.CantCompileQueryException
@@ -87,6 +88,10 @@ object LogicalPlan2PlanDescription extends ((LogicalPlan, Map[LogicalPlan, Id]) 
       case Skip(lhs, count) =>
         PlanDescriptionImpl(id = idMap(plan), name = "Skip", children = SingleChild(apply(lhs, idMap)),
                             Seq(Expression(count)), symbols)
+
+      case Aggregation(source, grouping, aggregation) =>
+        PlanDescriptionImpl(id = idMap(plan), name = "EagerAggregation", children = SingleChild(apply(source, idMap)),
+                            Seq(Arguments.KeyNames(grouping.keys.toSeq)), symbols)
 
       case row: SingleRow =>
         new SingleRowPlanDescription(id = idMap(plan), Seq.empty, row.argumentIds.map(_.name))

neo4j/neo4j

community/cypher/cypher-compiler-3.2/src/main/scala/org/neo4j/cypher/internal/compiler/v3_2/codegen/ir/expressions/Count.scala
@@ -1,0 +1,51 @@
+/*
+ * Copyright (c) 2002-2016 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.compiler.v3_2.codegen.ir.expressions
+
+import org.neo4j.cypher.internal.compiler.v3_2.codegen.{CodeGenContext, MethodStructure}
+import org.neo4j.cypher.internal.frontend.v3_2.symbols
+
+trait AggregateExpression extends CodeGenExpression {
+  def initialValue[E](generator: MethodStructure[E])(implicit context: CodeGenContext): E
+}
+
+case class Count(expression: CodeGenExpression) extends AggregateExpression {
+
+  private var name: String = null
+  def init[E](generator: MethodStructure[E])(implicit context: CodeGenContext) = {
+    expression.init(generator)
+    name = context.namer.newVarName()
+    generator.assign(name, CodeGenType.primitiveInt, generator.constantExpression(Long.box(0L)))
+  }
+
+  def initialValue[E](generator: MethodStructure[E])(implicit context: CodeGenContext): E =
+      generator.constantExpression(Long.box(0L))
+
+  def generateExpression[E](structure: MethodStructure[E])(implicit context: CodeGenContext) = {
+    structure.ifNonNullStatement(expression.generateExpression(structure)) { body =>
+      body.incrementInteger(name)
+    }
+    structure.loadVariable(name)
+  }
+
+  override def nullable(implicit context: CodeGenContext) = false
+
+  override def codeGenType(implicit context: CodeGenContext) = CodeGenType(symbols.CTInteger, IntType)
+}

community/cypher/cypher-compiler-3.2/src/main/scala/org/neo4j/cypher/internal/compiler/v3_2/planner/logical/LogicalPlan2PlanDescription.scala
@@ -24,6 +24,7 @@ import org.neo4j.cypher.internal.compiler.v3_2.ast.convert.commands.ExpressionCo
 import org.neo4j.cypher.internal.compiler.v3_2.commands.expressions.{InequalitySeekRangeExpression, PrefixSeekRangeExpression}
 import org.neo4j.cypher.internal.compiler.v3_2.commands.{QueryExpression, RangeQueryExpression}
 import org.neo4j.cypher.internal.compiler.v3_2.pipes._
+import org.neo4j.cypher.internal.compiler.v3_2.planDescription.InternalPlanDescription.Arguments
 import org.neo4j.cypher.internal.compiler.v3_2.planDescription.InternalPlanDescription.Arguments._
 import org.neo4j.cypher.internal.compiler.v3_2.planDescription._
 import org.neo4j.cypher.internal.compiler.v3_2.planner.logical.plans._
@@ -288,8 +289,17 @@ case class LogicalPlan2PlanDescription(idMap: Map[LogicalPlan, Id], readOnly: Bo
       case _: ForeachApply =>
         PlanDescriptionImpl(id, "Foreach", children, Seq.empty, variables)
 
+<<<<<<< HEAD
       case LetSelectOrSemiApply(_, _, _, predicate) =>
         PlanDescriptionImpl(id, "LetSelectOrSemiApply", children, Seq(Expression(predicate)), variables)
+=======
+      case Aggregation(source, grouping, aggregation) =>
+        PlanDescriptionImpl(id = idMap(plan), name = "EagerAggregation", children = SingleChild(apply(source, idMap)),
+                            Seq(Arguments.KeyNames(grouping.keys.toSeq)), symbols)
+
+      case row: SingleRow =>
+        new SingleRowPlanDescription(id = idMap(plan), Seq.empty, row.argumentIds.map(_.name))
+>>>>>>> Compiled runtime support for count with no grouping key
 
       case LetSelectOrAntiSemiApply(_, _, _, predicate) =>
         PlanDescriptionImpl(id, "LetSelectOrSemiApply", children, Seq(Expression(predicate)), variables)