7cc344dbd22b901946e53003328e7ae33f674b63
Chris Vest <mr.chrisvest@gmail.com>
2017-01-02 09:12:07 +0100

    License Headers 2017

--- community/cypher/cypher/src/test/java/org/neo4j/cypher/internal/javacompat/CypherEngineProviderTest.java
+++ community/cypher/cypher/src/test/java/org/neo4j/cypher/internal/javacompat/CypherEngineProviderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2016 "Neo Technology,"
+ * Copyright (c) 2002-2017 "Neo Technology,"
  * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.neo4j.cypher.internal.javacompat;
 
 import static org.junit.Assert.*;
