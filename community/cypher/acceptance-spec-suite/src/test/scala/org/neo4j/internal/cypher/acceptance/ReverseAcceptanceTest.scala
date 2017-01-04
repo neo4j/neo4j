ac50a400dcf845bed9320310e4d83bc60b8c8bc6
eebus <simpan.svensson@gmail.com>
2017-01-04 11:55:37 +0100

    Change dates

--- community/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ReverseAcceptanceTest.scala
+++ community/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ReverseAcceptanceTest.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-201 "Neo Technology,"
+ * Copyright (c) 2002-2017 "Neo Technology,"
  * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
