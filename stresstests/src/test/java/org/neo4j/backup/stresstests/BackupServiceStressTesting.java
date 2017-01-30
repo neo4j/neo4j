3af04e8a3171c27e703ed9bdec1545288958786d
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2017-01-30 15:07:40 +0100

    Use standard stringification of exception

--- stresstests/src/test/java/org/neo4j/backup/stresstests/BackupServiceStressTesting.java
+++ stresstests/src/test/java/org/neo4j/backup/stresstests/BackupServiceStressTesting.java
@@ -35,6 +35,7 @@ import java.util.function.BooleanSupplier;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.helpers.Exceptions;
 import org.neo4j.io.fs.FileUtils;
 import org.neo4j.test.ThreadTestUtils;
 
@@ -50,7 +51,6 @@ import static org.neo4j.helper.DatabaseConfiguration.configureBackup;
 import static org.neo4j.helper.DatabaseConfiguration.configureTxLogRotationAndPruning;
 import static org.neo4j.helper.StressTestingHelper.ensureExistsAndEmpty;
 import static org.neo4j.helper.StressTestingHelper.fromEnv;
-import static org.neo4j.helper.StressTestingHelper.prettyPrintStackTrace;
 
 /**
  * Notice the class name: this is _not_ going to be run as part of the main build.
@@ -110,9 +110,9 @@ public class BackupServiceStressTesting
                     new StartStop( keepGoingSupplier, onFailure, graphDatabaseBuilder::newGraphDatabase, dbRef ) );
 
             long timeout = durationInMinutes + 5;
-            assertNull( prettyPrintStackTrace( workload.get() ), workload.get( timeout, MINUTES ) );
-            assertNull( prettyPrintStackTrace( backupWorker.get() ), backupWorker.get( timeout, MINUTES ) );
-            assertNull( prettyPrintStackTrace( startStopWorker.get() ), startStopWorker.get( timeout, MINUTES ) );
+            assertNull( Exceptions.stringify( workload.get() ), workload.get( timeout, MINUTES ) );
+            assertNull( Exceptions.stringify( backupWorker.get() ), backupWorker.get( timeout, MINUTES ) );
+            assertNull( Exceptions.stringify( startStopWorker.get() ), startStopWorker.get( timeout, MINUTES ) );
 
             service.shutdown();
             if ( !service.awaitTermination( 30, TimeUnit.SECONDS ) )
