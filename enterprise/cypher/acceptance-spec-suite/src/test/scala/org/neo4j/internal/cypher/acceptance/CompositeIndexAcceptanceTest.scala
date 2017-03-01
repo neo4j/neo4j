216e986f8f3cb0e5f55172a3ac5fd059bc4c376b
Craig Taverner <craig@amanzi.com>
2017-03-01 13:53:30 +0100

    Cleaned up composite index tests

--- enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/CompositeIndexAcceptanceTest.scala
+++ enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/CompositeIndexAcceptanceTest.scala
@@ -33,7 +33,7 @@ import scala.collection.JavaConverters._
   */
 class CompositeIndexAcceptanceTest extends ExecutionEngineFunSuite with NewPlannerTestSupport {
 
-  test("should succeed in creating composite index") {
+  test("should succeed in creating and deleting composite index") {
     // When
     executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :Person(firstname)")
     executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :Person(firstname,lastname)")
@@ -51,7 +51,6 @@ class CompositeIndexAcceptanceTest extends ExecutionEngineFunSuite with NewPlann
   }
 
   test("should use composite index when all predicates are present") {
-    System.setProperty("pickBestPlan.VERBOSE", "true")
     // Given
     executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :User(firstname,lastname)")
     val n1 = createLabeledNode(Map("firstname" -> "Joe", "lastname" -> "Soap"), "User")
@@ -66,40 +65,104 @@ class CompositeIndexAcceptanceTest extends ExecutionEngineFunSuite with NewPlann
     val result = executeWithCostPlannerAndInterpretedRuntimeOnly("CYPHER runtime=interpreted MATCH (n:User) WHERE n.lastname = 'Soap' AND n.firstname = 'Joe' RETURN n")
 
     // Then
-    println(result.executionPlanDescription())
     result should use("NodeIndexSeek")
     result should evaluateTo(List(Map("n" -> n1)))
   }
 
-  ignore("should be able to update composite index when only one property has changed") {
+  test("should not use composite index when not all predicates are present") {
+    // Given
+    executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :User(firstname,lastname)")
+    val n1 = createLabeledNode(Map("firstname" -> "Joe", "lastname" -> "Soap"), "User")
+    val n2 = createLabeledNode(Map("firstname" -> "Joe", "lastname" -> "Smoke"), "User")
+    val n3 = createLabeledNode(Map("firstname" -> "Jake", "lastname" -> "Soap"), "User")
+    for (i <- 1 to 100) {
+      createLabeledNode(Map("firstname" -> "Joe"), "User")
+      createLabeledNode(Map("lastname" -> "Soap"), "User")
+    }
+
+    // When
+    val result = executeWithCostPlannerAndInterpretedRuntimeOnly("CYPHER runtime=interpreted MATCH (n:User) WHERE n.firstname = 'Jake' RETURN n")
+
+    // Then
+    result should not(use("NodeIndexSeek"))
+    result should evaluateTo(List(Map("n" -> n3)))
+  }
+
+  test("should use composite index when all predicates are present even in competition with other single property indexes with similar cardinality") {
+    // Given
+    executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :User(firstname)")
+    executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :User(lastname)")
+    executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :User(firstname,lastname)")
+    val n1 = createLabeledNode(Map("firstname" -> "Joe", "lastname" -> "Soap"), "User")
+    val n2 = createLabeledNode(Map("firstname" -> "Joe", "lastname" -> "Smoke"), "User")
+    val n3 = createLabeledNode(Map("firstname" -> "Jake", "lastname" -> "Soap"), "User")
+    for (i <- 1 to 100) {
+      createLabeledNode("User")
+      createLabeledNode("User")
+    }
+
+    // When
+    val result = executeWithCostPlannerAndInterpretedRuntimeOnly("CYPHER runtime=interpreted MATCH (n:User) WHERE n.lastname = 'Soap' AND n.firstname = 'Joe' RETURN n")
+
+    // Then
+    result should useIndex(":User(firstname,lastname)")
+    result should not(useIndex(":User(firstname)"))
+    result should not(useIndex(":User(lastname)"))
+    result should evaluateTo(List(Map("n" -> n1)))
+  }
+
+  // TODO: Support existence predicates in indexQuery
+  ignore("should use composite index with combined equality and existence predicates") {
+    System.setProperty("pickBestPlan.VERBOSE", "true")
+    // Given
+    executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :User(firstname)")
+    executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :User(lastname)")
+    executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :User(firstname,lastname)")
+    val n1 = createLabeledNode(Map("firstname" -> "Joe", "lastname" -> "Soap"), "User")
+    val n2 = createLabeledNode(Map("firstname" -> "Joe", "lastname" -> "Smoke"), "User")
+    val n3 = createLabeledNode(Map("firstname" -> "Jake", "lastname" -> "Soap"), "User")
+    for (i <- 1 to 100) {
+      createLabeledNode("User")
+      createLabeledNode("User")
+    }
+
+    // When
+    val result = executeWithCostPlannerAndInterpretedRuntimeOnly("CYPHER runtime=interpreted MATCH (n:User) WHERE exists(n.lastname) AND n.firstname = 'Jake' RETURN n")
+
+    // Then
+    println(result.executionPlanDescription())
+    result should useIndex(":User(firstname,lastname)")
+    result should not(useIndex(":User(firstname)"))
+    result should not(useIndex(":User(lastname)"))
+    result should evaluateTo(List(Map("n" -> n3)))
+  }
+
+  test("should be able to update composite index when only one property has changed") {
     executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :Person(firstname, lastname)")
     val n = executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE (n:Person {firstname:'Joe', lastname:'Soap'}) RETURN n").columnAs("n").toList(0).asInstanceOf[Node]
     executeWithCostPlannerAndInterpretedRuntimeOnly("MATCH (n:Person) SET n.lastname = 'Bloggs'")
     val result = executeWithCostPlannerAndInterpretedRuntimeOnly("CYPHER runtime=interpreted MATCH (n:Person) where n.firstname = 'Joe' and n.lastname = 'Bloggs' RETURN n")
     result should use("NodeIndexSeek")
-    result.columnAs("n").toList should be(List(n))
+    result should evaluateTo(List(Map("n" -> n)))
   }
 
-  ignore("should plan a composite index seek for a multiple property predicate expression") {
+  // TODO: Support composite index specifications in IndexSpecifier.java
+  ignore("should plan a composite index seek for a multiple property predicate expression when index is created after data") {
     executeWithCostPlannerAndInterpretedRuntimeOnly("WITH RANGE(0,10) AS num CREATE (:Person {id:num})") // ensure label cardinality favors index
     executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE (n:Person {firstname:'Joe', lastname:'Soap'})")
     executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :Person(firstname)")
     executeWithCostPlannerAndInterpretedRuntimeOnly("CALL db.awaitIndex(':Person(firstname)')")
-    //    executeWithCostPlannerOnly("CREATE INDEX ON :Person(firstname, lastname)")
-    //    executeWithCostPlannerOnly("CALL db.awaitIndex(':Person(firstname,lastname)')")
+    executeWithCostPlannerAndInterpretedRuntimeOnly("CREATE INDEX ON :Person(firstname, lastname)")
+    executeWithCostPlannerAndInterpretedRuntimeOnly("CALL db.awaitIndex(':Person(firstname,lastname)')")
     val result = executeWithCostPlannerAndInterpretedRuntimeOnly("MATCH (n:Person) WHERE n.firstname = 'Joe' AND n.lastname = 'Soap' RETURN n")
-    result should use("NodeIndexSeek")
+    result should useIndex(":User(firstname,lastname)")
   }
 
   case class haveIndexes(expectedIndexes: String*) extends Matcher[GraphDatabaseQueryService] {
     def apply(graph: GraphDatabaseQueryService): MatchResult = {
-
       graph.inTx {
         val indexNames = graph.schema().getIndexes.asScala.toList.map(i => s":${i.getLabel}(${i.getPropertyKeys.asScala.toList.mkString(",")})")
-        println("Found indexes" + indexNames.mkString(", "))
-
         val result = expectedIndexes.forall(i => indexNames.contains(i.toString))
-
         MatchResult(
           result,
           s"Expected graph to have indexes ${expectedIndexes.mkString(", ")}, but it was ${indexNames.mkString(", ")}",
