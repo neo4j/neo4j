24e997e47fd4a9c41b0aba420269fc82426d085f
fickludd <johan.teleman@neotechnology.com>
2017-03-28 14:14:32 +0200

    Merged SortedLongArrayUtil into PrimitiveSortedArraySet

--- community/primitive-collections/src/main/java/org/neo4j/collection/primitive/PrimitiveSortedArraySet.java
+++ community/primitive-collections/src/main/java/org/neo4j/collection/primitive/PrimitiveSortedArraySet.java
@@ -21,10 +21,19 @@ package org.neo4j.collection.primitive;
 
 import java.util.Arrays;
 
+/**
+ * Specialized methods for operations on sets represented as sorted primitive arrays.
+ *
+ * This class does not contain a complete set of operations for all primitives, but only
+ * the ones that were needed. Feel free to add specializations on demand, but remember to test.
+ */
 public class PrimitiveSortedArraySet
 {
+    private static final long[] NO_LONGS = new long[]{};
+    private static final int INT_BITS = Integer.BYTES * 8;
+
     /**
-     * Merges two sets of integers represented as sorted arrays.
+     * Compute union of two sets of integers represented as sorted arrays.
      *
      * @param lhs
      *         a set of integers, represented as a sorted array.
@@ -34,6 +43,11 @@ public class PrimitiveSortedArraySet
      */
     public static int[] union( int[] lhs, int[] rhs )
     {
+        if ( lhs == null || rhs == null )
+        {
+            return lhs == null ? rhs : lhs;
+        }
+
         if ( lhs.length < rhs.length )
         {
             return union( rhs, lhs );
@@ -95,5 +109,165 @@ public class PrimitiveSortedArraySet
             merged = Arrays.copyOf( merged, m );
         }
         return merged;
+    }
+
+    /**
+     * Compute intersection of two sorted long array sets.
+     * @param left a sorted array set
+     * @param right another sorted array set
+     * @return the intersection, which is also sorted
+     */
+    public static long[] intersect( long[] left, long[] right )
+    {
+        if ( left == null || right == null )
+        {
+            return NO_LONGS;
+        }
+
+        long uniqueCounts = countUnique( left, right );
+        if ( uniqueCounts == 0 ) // complete intersection
+        {
+            return right;
+        }
+        if ( right( uniqueCounts ) == right.length || left( uniqueCounts ) == left.length ) // non-intersecting
+        {
+            return NO_LONGS;
+        }
+
+        long[] intersect = new long[left.length - left( uniqueCounts )];
+
+        int cursor = 0;
+        int l = 0;
+        int r = 0;
+        while ( l < left.length && r < right.length )
+        {
+            if ( left[l] == right[r] )
+            {
+                intersect[cursor++] = left[l];
+                l++;
+                r++;
+            }
+            else if ( left[l] < right[r] )
+            {
+                l++;
+            }
+            else
+            {
+                r++;
+            }
+        }
+
+        assert cursor == intersect.length;
+        return intersect;
+    }
+    /**
+     * Compute the symmetric difference (set XOR basically) of two sorted long array sets.
+     * @param left a sorted array set
+     * @param right another sorted array set
+     * @return the union, which is NOT sorted
+     */
+    public static long[] symmetricDifference( long[] left, long[] right )
+    {
+        if ( left == null || right == null )
+        {
+            return left == null ? right : left;
+        }
+
+        long uniqueCounts = countUnique( left, right );
+        if ( uniqueCounts == 0 ) // complete intersection
+        {
+            return NO_LONGS;
+        }
+
+        long[] difference = new long[left( uniqueCounts ) + right( uniqueCounts )];
+
+        int cursor = 0;
+        int l = 0;
+        int r = 0;
+        while ( l < left.length && r < right.length )
+        {
+            if ( left[l] == right[r] )
+            {
+                l++;
+                r++;
+            }
+            else if ( left[l] < right[r] )
+            {
+                difference[cursor++] = left[l];
+                l++;
+            }
+            else
+            {
+                difference[cursor++] = right[r];
+                r++;
+            }
+        }
+        while ( l < left.length )
+        {
+            difference[cursor++] = left[l];
+            l++;
+        }
+        while ( r < right.length )
+        {
+            difference[cursor++] = right[r];
+            r++;
+        }
+
+        assert cursor == difference.length;
+        return difference;
+    }
+
+    /**
+     * Compute the number of unique values in two sorted long array sets
+     * @param left a sorted array set
+     * @param right another sorted array set
+     * @return int pair packed into long
+     */
+    static long countUnique( long[] left, long[] right )
+    {
+        int l = 0;
+        int r = 0;
+        int uniqueInLeft = 0;
+        int uniqueInRight = 0;
+        while ( l < left.length && r < right.length )
+        {
+            if ( left[l] == right[r] )
+            {
+                l++;
+                r++;
+            }
+            else if ( left[l] < right[r] )
+            {
+                uniqueInLeft++;
+                l++;
+            }
+            else
+            {
+                uniqueInRight++;
+                r++;
+            }
+        }
+        uniqueInLeft += left.length - l;
+        uniqueInRight += right.length - r;
+        return intPair( uniqueInLeft, uniqueInRight );
+    }
+
+    private static long intPair( int left, int right )
+    {
+        return ( ((long)left) << INT_BITS ) | right;
+    }
+
+    private static int left( long pair )
+    {
+        return (int)(pair >> INT_BITS);
+    }
+
+    private static int right( long pair )
+    {
+        return (int)(pair & 0xFFFF_FFFFL);
+    }
+
+    private PrimitiveSortedArraySet()
+    {   // No instances allowed
     }
 }
--- community/primitive-collections/src/test/java/org/neo4j/collection/primitive/PrimitiveSortedArraySetTest.java
+++ community/primitive-collections/src/test/java/org/neo4j/collection/primitive/PrimitiveSortedArraySetTest.java
@@ -19,132 +19,185 @@
  */
 package org.neo4j.collection.primitive;
 
-import java.util.Arrays;
-
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertSame;
-import static org.neo4j.collection.primitive.PrimitiveSortedArraySet.union;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
 
-@RunWith( Parameterized.class )
 public class PrimitiveSortedArraySetTest
 {
-    @Parameterized.Parameters( name = "{0}" )
-    public static Iterable<Object[]> parameters()
+    private static final int[] NO_INTS = new int[0];
+    private static final int[] ONE_INT = new int[]{1};
+    private static final long[] NO_LONGS = new long[0];
+    private static final long[] ONE_LONG = new long[]{1};
+
+    // union() null checks. Actual behaviour is tested in PrimitiveSortedArraySetUnionTest
+
+    @Test
+    public void union_shouldHandleNullInput()
     {
-        return Arrays.asList(
-                lhs( 1, 2, 3 ).rhs( 1, 2, 3 ).expectLhs(),
-                lhs( 1, 2, 3 ).rhs( 1 ).expectLhs(),
-                lhs( 1, 2, 3 ).rhs( 2 ).expectLhs(),
-                lhs( 1, 2, 3 ).rhs( 3 ).expectLhs(),
-                lhs( 1 ).rhs( 1, 2, 3 ).expectRhs(),
-                lhs( 2 ).rhs( 1, 2, 3 ).expectRhs(),
-                lhs( 3 ).rhs( 1, 2, 3 ).expectRhs(),
-                lhs( 1, 2, 3 ).rhs( 4, 5, 6 ).expect( 1, 2, 3, 4, 5, 6 ),
-                lhs( 1, 3, 5 ).rhs( 2, 4, 6 ).expect( 1, 2, 3, 4, 5, 6 ),
-                lhs( 1, 2, 3, 5 ).rhs( 2, 4, 6 ).expect( 1, 2, 3, 4, 5, 6 ),
-                lhs( 2, 3, 4, 7, 8, 9, 12, 16, 19 ).rhs( 4, 6, 9, 11, 12, 15 )
-                        .expect( 2, 3, 4, 6, 7, 8, 9, 11, 12, 15, 16, 19 )
-        );
+        assertThat( PrimitiveSortedArraySet.union( null, null ), nullValue() );
+        assertThat( PrimitiveSortedArraySet.union( null, NO_INTS ), equalTo( NO_INTS ) );
+        assertThat( PrimitiveSortedArraySet.union( NO_INTS, null ), equalTo( NO_INTS ) );
+        assertThat( PrimitiveSortedArraySet.union( null, ONE_INT ), equalTo( ONE_INT ) );
+        assertThat( PrimitiveSortedArraySet.union( ONE_INT, null ), equalTo( ONE_INT ) );
     }
 
-    private final int[] lhs;
-    private final int[] rhs;
-    private final int[] expected;
+    // intersect()
 
-    public PrimitiveSortedArraySetTest( Input input )
+    @Test
+    public void intersect_shouldHandleNullInput()
     {
-        this.lhs = input.lhs;
-        this.rhs = input.rhs;
-        this.expected = input.expected;
+        assertThat( PrimitiveSortedArraySet.intersect( null, null ), equalTo( NO_LONGS ) );
+        assertThat( PrimitiveSortedArraySet.intersect( null, NO_LONGS ), equalTo( NO_LONGS ) );
+        assertThat( PrimitiveSortedArraySet.intersect( NO_LONGS, null ), equalTo( NO_LONGS ) );
+        assertThat( PrimitiveSortedArraySet.intersect( null, ONE_LONG ), equalTo( NO_LONGS ) );
+        assertThat( PrimitiveSortedArraySet.intersect( ONE_LONG, null ), equalTo( NO_LONGS ) );
     }
 
     @Test
-    public void testMerge() throws Exception
+    public void intersect_shouldHandleNonIntersectingArrays()
     {
-        int[] actual = union( lhs, rhs );
-        if ( lhs == expected || rhs == expected )
-        {
-            assertSame( expected, actual );
-        }
-        else
-        {
-            assertArrayEquals( expected, actual );
-        }
+        assertThat( PrimitiveSortedArraySet.intersect( new long[]{1, 2, 3}, new long[]{4, 5, 6} ),
+                equalTo( NO_LONGS ) );
+
+        assertThat( PrimitiveSortedArraySet.intersect( new long[]{14, 15, 16}, new long[]{1, 2, 3} ),
+                equalTo( NO_LONGS ) );
     }
 
-    private static Input.Lhs lhs( int... lhs )
+    @Test
+    public void intersect_shouldHandleIntersectingArrays()
     {
-        return new Input.Lhs( lhs );
+        assertThat( PrimitiveSortedArraySet.intersect( new long[]{1, 2, 3}, new long[]{3, 4, 5} ),
+                isArray( 3 ) );
+
+        assertThat( PrimitiveSortedArraySet.intersect( new long[]{3, 4, 5}, new long[]{1, 2, 3, 4} ),
+                isArray( 3, 4 ) );
     }
 
-    static class Input
+    @Test
+    public void intersect_shouldHandleComplexIntersectingArraysWithGaps()
     {
-        final int[] lhs;
-        final int[] rhs;
-        final int[] expected;
+        assertThat(
+                PrimitiveSortedArraySet.intersect( new long[]{4, 6, 9, 11, 12, 15}, new long[]{2, 3, 4, 7, 8, 9, 12, 16, 19} ),
+                isArray( 4, 9, 12 ) );
+        assertThat(
+                PrimitiveSortedArraySet.intersect( new long[]{2, 3, 4, 7, 8, 9, 12, 16, 19}, new long[]{4, 6, 9, 11, 12, 15} ),
+                isArray( 4, 9, 12 ) );
+    }
 
-        Input( int[] lhs, int[] rhs, int[] expected )
+    // symmetricDifference()
+
+    @Test
+    public void symDiff_shouldHandleNullInput()
+    {
+        assertThat( PrimitiveSortedArraySet.symmetricDifference( null, null ), equalTo( null ) );
+        assertThat( PrimitiveSortedArraySet.symmetricDifference( null, NO_LONGS ), equalTo( NO_LONGS ) );
+        assertThat( PrimitiveSortedArraySet.symmetricDifference( NO_LONGS, null ), equalTo( NO_LONGS ) );
+        assertThat( PrimitiveSortedArraySet.symmetricDifference( null, ONE_LONG ), equalTo( ONE_LONG ) );
+        assertThat( PrimitiveSortedArraySet.symmetricDifference( ONE_LONG, null ), equalTo( ONE_LONG ) );
+    }
+
+    @Test
+    public void symDiff_shouldHandleNonIntersectingArrays()
+    {
+        assertThat( PrimitiveSortedArraySet.symmetricDifference( new long[]{1, 2, 3}, new long[]{4, 5, 6} ),
+                isArray( 1, 2, 3, 4, 5, 6 ) );
+
+        assertThat( PrimitiveSortedArraySet.symmetricDifference( new long[]{14, 15, 16}, new long[]{1, 2, 3} ),
+                isArray( 1, 2, 3, 14, 15, 16 ) );
+    }
+
+    @Test
+    public void symDiff_shouldHandleIntersectingArrays()
+    {
+        assertThat( PrimitiveSortedArraySet.symmetricDifference( new long[]{1, 2, 3}, new long[]{3, 4, 5} ),
+                isArray( 1, 2, 4, 5 ) );
+
+        assertThat( PrimitiveSortedArraySet.symmetricDifference( new long[]{3, 4, 5}, new long[]{1, 2, 3, 4} ),
+                isArray( 1, 2, 5 ) );
+    }
+
+    @Test
+    public void symDiff_shouldHandleComplexIntersectingArraysWithGaps()
+    {
+        assertThat(
+                PrimitiveSortedArraySet.symmetricDifference( new long[]{4, 6, 9, 11, 12, 15}, new long[]{2, 3, 4, 7, 8, 9, 12, 16, 19} ),
+                isArray( 2, 3, 6, 7, 8, 11, 15, 16, 19 ) );
+        assertThat(
+                PrimitiveSortedArraySet.symmetricDifference( new long[]{2, 3, 4, 7, 8, 9, 12, 16, 19}, new long[]{4, 6, 9, 11, 12, 15} ),
+                isArray( 2, 3, 6, 7, 8, 11, 15, 16, 19 ) );
+    }
+
+    // count unique
+
+    @Test
+    public void shouldCountUnique()
+    {
+        assertThat(
+                PrimitiveSortedArraySet.countUnique( new long[]{1, 2, 3}, new long[]{4, 5, 6} ),
+                isIntPair( 3, 3 ) );
+
+        assertThat(
+                PrimitiveSortedArraySet.countUnique( new long[]{1, 2, 3}, new long[]{3, 6} ),
+                isIntPair( 2, 1 ) );
+
+        assertThat(
+                PrimitiveSortedArraySet.countUnique( new long[]{1, 2, 3}, new long[]{3} ),
+                isIntPair( 2, 0 ) );
+
+        assertThat(
+                PrimitiveSortedArraySet.countUnique( new long[]{3}, new long[]{1, 2, 3} ),
+                isIntPair( 0, 2 ) );
+
+        assertThat(
+                PrimitiveSortedArraySet.countUnique( new long[]{3}, new long[]{3} ),
+                isIntPair( 0, 0 ) );
+
+        assertThat(
+                PrimitiveSortedArraySet.countUnique( new long[]{3, 6, 8}, new long[]{} ),
+                isIntPair( 3, 0 ) );
+
+        assertThat(
+                PrimitiveSortedArraySet.countUnique( new long[]{}, new long[]{3, 6, 8} ),
+                isIntPair( 0, 3 ) );
+
+        assertThat(
+                PrimitiveSortedArraySet.countUnique( new long[]{}, new long[]{} ),
+                isIntPair( 0, 0 ) );
+
+        assertThat(
+                PrimitiveSortedArraySet.countUnique( new long[]{4, 6, 9, 11, 12, 15}, new long[]{2, 3, 4, 7, 8, 9, 12, 16, 19} ),
+                isIntPair( 3, 6 ) );
+    }
+
+    // helpers
+
+    private Matcher<Long> isIntPair( int left, int right )
+    {
+        return new BaseMatcher<Long>()
         {
-            this.lhs = lhs;
-            this.rhs = rhs;
-            this.expected = expected;
-        }
-
-        @Override
-        public String toString()
-        {
-            return String.format(
-                    "{lhs=%s, rhs=%s, expected=%s}",
-                    Arrays.toString( lhs ),
-                    Arrays.toString( rhs ),
-                    Arrays.toString( expected ) );
-        }
-
-        static class Lhs
-        {
-            final int[] lhs;
-
-            Lhs( int[] lhs )
+            @Override
+            public void describeTo( Description description )
             {
-                this.lhs = lhs;
+                description.appendValue( left );
+                description.appendValue( right );
             }
 
-            Rhs rhs( int... rhs )
+            @Override
+            public boolean matches( Object o )
             {
-                return new Rhs( lhs, rhs );
+                return o instanceof Long && ((Long) o) == (((long) left << 32) | right);
             }
-        }
+        };
+    }
 
-        static class Rhs
-        {
-            final int[] lhs;
-            final int[] rhs;
-
-            Rhs( int[] lhs, int[] rhs )
-            {
-
-                this.lhs = lhs;
-                this.rhs = rhs;
-            }
-
-            Object[] expect( int... expected )
-            {
-                return new Object[] {new Input( lhs, rhs, expected )};
-            }
-
-            Object[] expectLhs()
-            {
-                return new Object[] {new Input( lhs, rhs, lhs )};
-            }
-
-            Object[] expectRhs()
-            {
-                return new Object[] {new Input( lhs, rhs, rhs )};
-            }
-        }
+    private static Matcher<long[]> isArray( long... values )
+    {
+        return equalTo( values );
     }
 }
