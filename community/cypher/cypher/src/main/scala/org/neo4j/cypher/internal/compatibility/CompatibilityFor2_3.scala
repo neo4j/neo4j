a6030ded6ecf870f052f8d56cf0fc8f76c389f77
fickludd <johan.teleman@neotechnology.com>
2017-04-04 11:44:26 +0200

    Update cypher compatibility compiler to 2.3.10

--- community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/CompatibilityFor2_3.scala
+++ community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/CompatibilityFor2_3.scala
@@ -68,11 +68,6 @@ object exceptionHandlerFor2_3 extends MapToPublicExceptions[CypherException] {
     throw new ProfilerStatisticsNotReadyException(cause)
   }
 
-  override def incomparableValuesException(lhs: String, rhs: String,
-                                           cause: Throwable): CypherException = {
-    incomparableValuesException(None, lhs, rhs, cause)
-  }
-
   def incomparableValuesException(details: Option[String], lhs: String, rhs: String, cause: Throwable): CypherException = new IncomparableValuesException(details, lhs, rhs, cause)
 
   def unknownLabelException(s: String, cause: Throwable) = new UnknownLabelException(s, cause)
