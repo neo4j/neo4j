f3621f6fca7cfdd143abe013e9b040e570c1d44a
Lasse Westh-Nielsen <lassewesth@gmail.com>
2017-04-26 11:39:11 +0200

    fixing ClusterRuleIT

--- enterprise/causal-clustering/src/test/java/org/neo4j/test/causalclustering/ClusterRuleIT.java
+++ enterprise/causal-clustering/src/test/java/org/neo4j/test/causalclustering/ClusterRuleIT.java
@@ -42,7 +42,7 @@ import org.neo4j.kernel.configuration.HttpConnector;
 public class ClusterRuleIT
 {
     private static final int NumberOfPortsUsedByCoreMember = 6;
-    private static final int NumberOfPortsUsedByReadReplica = 3;
+    private static final int NumberOfPortsUsedByReadReplica = 4;
 
     @Rule
     public final ClusterRule clusterRule = new ClusterRule( getClass() );
@@ -50,12 +50,12 @@ public class ClusterRuleIT
     @Test
     public void shouldAssignPortsToMembersAutomatically() throws Exception
     {
-        Cluster cluster = clusterRule.startCluster();
+        Cluster cluster = clusterRule.withNumberOfCoreMembers( 3 ).withNumberOfReadReplicas( 5 ).startCluster();
 
         int numberOfCoreMembers = cluster.coreMembers().size();
         assertThat( numberOfCoreMembers, is( 3 ) );
         int numberOfReadReplicas = cluster.readReplicas().size();
-        assertThat( numberOfReadReplicas, is( 3 ) );
+        assertThat( numberOfReadReplicas, is( 5 ) );
 
         Set<Integer> portsUsed = gatherPortsUsed( cluster );
 
