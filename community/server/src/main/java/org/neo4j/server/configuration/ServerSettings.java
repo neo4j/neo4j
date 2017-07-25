ea2d97dfbd8a6dd9ffff206e539d36278f656a74
Oskar Hane <oh@oskarhane.com>
2017-07-25 22:03:59 +0200

    Fix styling issues

--- community/server/src/main/java/org/neo4j/server/configuration/ServerSettings.java
+++ community/server/src/main/java/org/neo4j/server/configuration/ServerSettings.java
@@ -185,9 +185,9 @@ public class ServerSettings implements LoadableConfig
                   "commands with semi-colon." )
     public static final Setting<String> browser_postConnectCmd = setting( "browser.post_connect_cmd", STRING, "" );
 
-    @SuppressWarnings("unused") // accessed from the browser
+    @SuppressWarnings( "unused" ) // accessed from the browser
     @Description( "Whitelist of hosts for the Neo4j Browser to be allowed to fetch content from." )
-    public static final Setting<String> browser_remoteContentHostnameWhitelist = 
+    public static final Setting<String> browser_remoteContentHostnameWhitelist =
             setting( "browser.remote_content_hostname_whitelist", STRING, "guides.neo4j.com,localhost");
 
     @Description( "SSL policy name." )
