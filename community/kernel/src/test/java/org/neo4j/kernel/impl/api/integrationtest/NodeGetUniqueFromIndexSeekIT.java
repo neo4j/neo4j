954c42cefbe3eed83bc711969093704d2abc22f0
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2017-07-26 14:11:27 +0200

    Bump another set of test timeouts for kernel related tests

--- community/kernel/src/test/java/org/neo4j/kernel/impl/api/integrationtest/NodeGetUniqueFromIndexSeekIT.java
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/api/integrationtest/NodeGetUniqueFromIndexSeekIT.java
@@ -151,7 +151,7 @@ public class NodeGetUniqueFromIndexSeekIT extends KernelIntegrationTest
         assertTrue( "Non-matching created node was found", isNoSuchNode( foundId ) );
     }
 
-    @Test( timeout = 1000 )
+    @Test( timeout = 10_000 )
     public void shouldBlockUniqueIndexSeekFromCompetingTransaction() throws Exception
     {
         // This is the interleaving that we are trying to verify works correctly:
