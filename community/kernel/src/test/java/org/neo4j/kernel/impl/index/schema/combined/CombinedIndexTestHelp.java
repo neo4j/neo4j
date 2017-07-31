a0d47b33a01adc380877c78b869484a0452ce357
Mattias Persson <mattias@neotechnology.com>
2017-07-31 21:48:38 +0200

    Don't test with null string array

--- community/kernel/src/test/java/org/neo4j/kernel/impl/index/schema/combined/CombinedIndexTestHelp.java
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/index/schema/combined/CombinedIndexTestHelp.java
@@ -66,7 +66,7 @@ class CombinedIndexTestHelp
                     Values.floatArray( new float[]{9.10f, 11.12f} ),
                     Values.doubleArray( new double[]{13.14, 15.16} ),
                     Values.charArray( new char[2] ),
-                    Values.stringArray( new String[2] ),
+                    Values.stringArray( new String[]{"a", "b"} ),
                     Values.NO_VALUE
             };
 
