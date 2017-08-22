4f237accd3e5ef6d02221b30f699f16a0fd9422b
Anton Klaren <anton.klaren@neotechnology.com>
2017-08-22 16:04:07 +0200

    ReplicatedTokenRequestSerializer accidentally sent zeroes at the end of each command. It's unnecessary and the LogEntryParser does not accept those request as valid anymore.

--- enterprise/causal-clustering/src/main/java/org/neo4j/causalclustering/core/state/machines/token/ReplicatedTokenRequestSerializer.java
+++ enterprise/causal-clustering/src/main/java/org/neo4j/causalclustering/core/state/machines/token/ReplicatedTokenRequestSerializer.java
@@ -23,6 +23,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -45,6 +46,7 @@ public class ReplicatedTokenRequestSerializer
 {
     private ReplicatedTokenRequestSerializer()
     {
+        throw new AssertionError();
     }
 
     public static void marshal( ReplicatedTokenRequest content, WritableChannel channel ) throws IOException
@@ -103,7 +105,7 @@ public class ReplicatedTokenRequestSerializer
             e.printStackTrace(); // TODO: Handle or throw.
         }
 
-        byte[] commandsBytes = commandBuffer.array().clone();
+        byte[] commandsBytes = Arrays.copyOf( commandBuffer.array(), commandBuffer.writerIndex() );
         commandBuffer.release();
 
         return commandsBytes;
