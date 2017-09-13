f8cebab60611c41a4bceeb6205878a07eec62e32
Chris Vest <mr.chrisvest@gmail.com>
2017-09-13 15:56:15 +0200

    Fix checkstyle in new class

--- enterprise/fulltext-addon/src/main/java/org/neo4j/kernel/api/impl/fulltext/FulltextUpdateApplier.java
+++ enterprise/fulltext-addon/src/main/java/org/neo4j/kernel/api/impl/fulltext/FulltextUpdateApplier.java
@@ -51,7 +51,7 @@ class FulltextUpdateApplier
     private final ApplierThread workerThread;
     private final Log log;
 
-    public FulltextUpdateApplier( Log log )
+    FulltextUpdateApplier( Log log )
     {
         this.log = log;
         workQueue = new LinkedBlockingQueue<>();
@@ -216,7 +216,7 @@ class FulltextUpdateApplier
         private LinkedBlockingQueue<FulltextIndexUpdate> workQueue;
         private final Log log;
 
-        public ApplierThread( LinkedBlockingQueue<FulltextIndexUpdate> workQueue, Log log )
+        ApplierThread( LinkedBlockingQueue<FulltextIndexUpdate> workQueue, Log log )
         {
             super( "Fulltext Index Add-On Applier Thread" );
             this.workQueue = workQueue;
