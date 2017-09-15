eaa12c64c3e54279bd7c3f9150e97cb52102537f
mariascharin <maria.scharin@neotechnology.com>
2017-09-15 14:08:20 +0200

    More wording changes.

--- community/kernel/src/main/java/org/neo4j/graphdb/factory/GraphDatabaseSettings.java
+++ community/kernel/src/main/java/org/neo4j/graphdb/factory/GraphDatabaseSettings.java
@@ -116,9 +116,9 @@ public abstract class GraphDatabaseSettings
     public static final Setting<Boolean> allow_store_upgrade = setting("dbms.allow_format_migration", BOOLEAN, FALSE );
 
     @Description( "Database record format. Valid values: `standard`, `high_limit`. " +
-            "`high-limit` is available for Enterprise Edition only. " +
+            "The `high_limit` format is available for Enterprise Edition only. " +
             "It is required if you have a graph that is larger than 34 billion nodes, 34 billion relationships, or 68 billion properties. " +
-            "A change to `high-limit` is irreversible. " +
+            "A change of the record format is irreversible. " +
             "Certain operations may suffer from a performance penalty of up to 10%, which is why this format is not switched on by default.")
     public static final Setting<String> record_format = setting( "dbms.record_format", Settings.STRING, "" );
 
