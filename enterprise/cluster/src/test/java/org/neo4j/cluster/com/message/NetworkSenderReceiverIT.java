8b5a4342e5b0e29fcfb533bd7c960b251b29cc71
Lasse Westh-Nielsen <lassewesth@gmail.com>
2017-10-03 11:15:15 +0200

    re-classifying test

--- enterprise/cluster/src/test/java/org/neo4j/cluster/com/message/NetworkSenderReceiverIT.java
+++ enterprise/cluster/src/test/java/org/neo4j/cluster/com/message/NetworkSenderReceiverIT.java
@@ -48,7 +48,7 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class NetworkSenderReceiverTest
+public class NetworkSenderReceiverIT
 {
     public enum TestMessage implements MessageType
     {
