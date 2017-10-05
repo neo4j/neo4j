e049072cfbb02efd70f3801438e5e3e0befdf164
Chris Vest <mr.chrisvest@gmail.com>
2017-10-05 11:35:16 +0200

    Bring back unused LuceneKernelExtension constructor
    because removing it breaks the public API, according to clirr.

--- community/lucene-index/src/main/java/org/neo4j/index/lucene/LuceneKernelExtension.java
+++ community/lucene-index/src/main/java/org/neo4j/index/lucene/LuceneKernelExtension.java
@@ -40,6 +40,11 @@ public class LuceneKernelExtension extends LifecycleAdapter
     private final OperationalMode operationalMode;
 
     public LuceneKernelExtension( File storeDir, Config config, Supplier<IndexConfigStore> indexStore,
+            FileSystemAbstraction fileSystemAbstraction, IndexProviders indexProviders )
+    {
+        this( storeDir, config, indexStore, fileSystemAbstraction, indexProviders, OperationalMode.single );
+    }
+    public LuceneKernelExtension( File storeDir, Config config, Supplier<IndexConfigStore> indexStore,
             FileSystemAbstraction fileSystemAbstraction, IndexProviders indexProviders, OperationalMode operationalMode )
     {
         this.storeDir = storeDir;
