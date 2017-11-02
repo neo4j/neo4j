37891a16cce1d542b25c3b40eabf2a3e267da569
Sascha Peukert <sascha.peukert@neotechnology.com>
2017-11-02 14:02:33 +0100

    Made sure that only admins can use the procedure

--- community/cypher/cypher/src/main/java/org/neo4j/cypher/internal/javacompat/QueryEngineProcedures.java
+++ community/cypher/cypher/src/main/java/org/neo4j/cypher/internal/javacompat/QueryEngineProcedures.java
@@ -21,12 +21,15 @@ package org.neo4j.cypher.internal.javacompat;
 
 import java.util.stream.Stream;
 
+import org.neo4j.graphdb.security.AuthorizationViolationException;
+import org.neo4j.kernel.api.security.SecurityContext;
 import org.neo4j.kernel.impl.query.QueryExecutionEngine;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Procedure;
 
+import static org.neo4j.graphdb.security.AuthorizationViolationException.PERMISSION_DENIED;
 import static org.neo4j.procedure.Mode.DBMS;
 
 public class QueryEngineProcedures
@@ -34,12 +37,20 @@ public class QueryEngineProcedures
     @Context
     public GraphDatabaseAPI graph;
 
+    @Context
+    public SecurityContext context;
+
     @Description( "Clears all query caches." )
     @Procedure( name = "dbms.clearQueryCaches", mode = DBMS )
     public Stream<StringResult> clearAllQueryCaches()
     {
+        if ( !context.isAdmin() )
+        {
+            throw new AuthorizationViolationException( PERMISSION_DENIED );
+        }
+
         QueryExecutionEngine queryExecutionEngine = graph.getDependencyResolver().resolveDependency( QueryExecutionEngine.class );
-        Long numberOfClearedQueries = queryExecutionEngine.clearQueryCaches() - 1; // this query itself does not count
+        long numberOfClearedQueries = queryExecutionEngine.clearQueryCaches() - 1; // this query itself does not count
 
         String result = numberOfClearedQueries == 0 ? "Query cache already empty."
                                                     : "Query caches successfully cleared of " + numberOfClearedQueries + " queries.";
