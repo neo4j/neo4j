03e88c07ab9ddabf4f9e3f823693ffe6736b4543
Satia Herfert <satiaherfert@gmx.de>
2017-11-16 13:30:08 +0100

    Test fixes

--- community/kernel/src/test/java/org/neo4j/kernel/impl/storemigration/participant/NativeLabelScanStoreMigratorTest.java
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/storemigration/participant/NativeLabelScanStoreMigratorTest.java
@@ -47,6 +47,7 @@ import org.neo4j.kernel.impl.index.labelscan.NativeLabelScanStore;
 import org.neo4j.kernel.impl.store.MetaDataStore;
 import org.neo4j.kernel.impl.store.format.standard.StandardV2_3;
 import org.neo4j.kernel.impl.store.format.standard.StandardV3_2;
+import org.neo4j.kernel.impl.store.format.standard.StandardV3_4;
 import org.neo4j.kernel.impl.util.monitoring.ProgressReporter;
 import org.neo4j.kernel.lifecycle.Lifespan;
 import org.neo4j.kernel.monitoring.Monitors;
@@ -155,8 +156,8 @@ public class NativeLabelScanStoreMigratorTest
     @Test
     public void populateNativeLabelScanIndexDuringMigration() throws IOException
     {
-        prepare32DatabaseWithNodes();
-        indexMigrator.migrate( storeDir, migrationDir, progressReporter, StandardV3_2.STORE_VERSION, StandardV3_2.STORE_VERSION );
+        prepare34DatabaseWithNodes();
+        indexMigrator.migrate( storeDir, migrationDir, progressReporter, StandardV3_4.STORE_VERSION, StandardV3_4.STORE_VERSION );
         indexMigrator.moveMigratedFiles( migrationDir, storeDir, StandardV2_3.STORE_VERSION, StandardV3_2.STORE_VERSION );
 
         try ( Lifespan lifespan = new Lifespan() )
@@ -178,8 +179,8 @@ public class NativeLabelScanStoreMigratorTest
     @Test
     public void reportProgressOnNativeIndexPopulation() throws IOException
     {
-        prepare32DatabaseWithNodes();
-        indexMigrator.migrate( storeDir, migrationDir, progressReporter, StandardV3_2.STORE_VERSION, StandardV3_2.STORE_VERSION );
+        prepare34DatabaseWithNodes();
+        indexMigrator.migrate( storeDir, migrationDir, progressReporter, StandardV3_4.STORE_VERSION, StandardV3_4.STORE_VERSION );
         indexMigrator.moveMigratedFiles( migrationDir, storeDir, StandardV2_3.STORE_VERSION, StandardV3_2.STORE_VERSION );
 
         verify( progressReporter ).start( 10 );
@@ -228,7 +229,7 @@ public class NativeLabelScanStoreMigratorTest
         return sourceBuffer;
     }
 
-    private void prepare32DatabaseWithNodes()
+    private void prepare34DatabaseWithNodes()
     {
         GraphDatabaseService embeddedDatabase = new TestGraphDatabaseFactory().newEmbeddedDatabase( storeDir );
         try
