014651ab81b202edd7f3e6044fe3008dcfe5879f
Satia Herfert <satiaherfert@gmx.de>
2017-11-24 17:02:06 +0100

    Tests with Reflections library. Coding many special conversion cases.

--- community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_3/LogicalPlanConverter.scala
+++ community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_3/LogicalPlanConverter.scala
@@ -3,17 +3,20 @@ package org.neo4j.cypher.internal.compatibility.v3_3
 import java.lang.reflect.Constructor
 
 import org.neo4j.cypher.internal.frontend.v3_3.ast.{Expression => ExpressionV3_3}
-import org.neo4j.cypher.internal.frontend.v3_3.{ast => astV3_3}
+import org.neo4j.cypher.internal.frontend.v3_3.{SemanticDirection => SemanticDirectionV3_3, ast => astV3_3, symbols => symbolsV3_3}
+import org.neo4j.cypher.internal.frontend.{v3_3 => frontendV3_3}
 import org.neo4j.cypher.internal.ir.v3_3.{IdName => IdNameV3_3}
 import org.neo4j.cypher.internal.ir.v3_4.{PlannerQuery, IdName => IdNameV3_4}
 import org.neo4j.cypher.internal.util.v3_4.Rewritable.{DuplicatableProduct, RewritableAny}
-import org.neo4j.cypher.internal.util.v3_4.{InputPosition, InternalException, RewriterWithArgs, bottomUpWithArgs}
+import org.neo4j.cypher.internal.util.v3_4.{symbols => symbolsV3_4, _}
 import org.neo4j.cypher.internal.v3_3.logical.plans.{LogicalPlan => LogicalPlanV3_3}
 import org.neo4j.cypher.internal.v3_3.logical.{plans => plansV3_3}
 import org.neo4j.cypher.internal.v3_4.expressions.{Expression => ExpressionV3_4}
 import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan => LogicalPlanV3_4}
+import org.neo4j.cypher.internal.v3_4.{expressions => expressionsV3_4}
 
 import scala.collection.mutable.{HashMap => MutableHashMap}
+import scala.util.{Failure, Success, Try}
 
 object LogicalPlanConverter {
   private val rewriter: RewriterWithArgs = bottomUpWithArgs(RewriterWithArgs.lift {
@@ -21,9 +24,44 @@ object LogicalPlanConverter {
       val newPlan = convertVersion("v3_3", "v3_4", plan, children, null, classOf[PlannerQuery])
       newPlan.asInstanceOf[LogicalPlanV3_4].setIdTo(helpers.as3_4(plan.assignedId))
       newPlan
+
+    case (inp: astV3_3.InvalidNodePattern, children: Seq[AnyRef]) =>
+      new expressionsV3_4.InvalidNodePattern(children.head.asInstanceOf[Option[expressionsV3_4.Variable]].get)(helpers.as3_4(inp.position))
+    case (mp: astV3_3.MapProjection, children: Seq[AnyRef]) =>
+      expressionsV3_4.MapProjection(children(0).asInstanceOf[expressionsV3_4.Variable],
+        children(1).asInstanceOf[Seq[expressionsV3_4.MapProjectionElement]])(helpers.as3_4(mp.position))
+
+    case (expressionV3_3: astV3_3.rewriters.DesugaredMapProjection, children: Seq[AnyRef]) =>
+      convertVersion("v3_3", "v3_4", expressionV3_3, children, helpers.as3_4(expressionV3_3.position), classOf[InputPosition])
     case (expressionV3_3: astV3_3.ASTNode, children: Seq[AnyRef]) =>
       convertVersion("frontend.v3_3.ast", "v3_4.expressions", expressionV3_3, children, helpers.as3_4(expressionV3_3.position), classOf[InputPosition])
     case (IdNameV3_3(name), _) => IdNameV3_4(name)
+
+    case (symbolsV3_3.CTAny, _) => symbolsV3_4.CTAny
+    case (symbolsV3_3.CTBoolean, _) => symbolsV3_4.CTBoolean
+    case (symbolsV3_3.CTFloat, _) => symbolsV3_4.CTFloat
+    case (symbolsV3_3.CTGeometry, _) => symbolsV3_4.CTGeometry
+    case (symbolsV3_3.CTGraphRef, _) => symbolsV3_4.CTGraphRef
+    case (symbolsV3_3.CTInteger, _) => symbolsV3_4.CTInteger
+    case (symbolsV3_3.ListType(_), children: Seq[AnyRef]) => symbolsV3_4.CTList(children.head.asInstanceOf[symbolsV3_4.CypherType])
+    case (symbolsV3_3.CTMap, _) => symbolsV3_4.CTMap
+    case (symbolsV3_3.CTNode, _) => symbolsV3_4.CTNode
+    case (symbolsV3_3.CTNumber, _) => symbolsV3_4.CTNumber
+    case (symbolsV3_3.CTPath, _) => symbolsV3_4.CTPath
+    case (symbolsV3_3.CTPoint, _) => symbolsV3_4.CTPoint
+    case (symbolsV3_3.CTRelationship, _) => symbolsV3_4.CTRelationship
+    case (symbolsV3_3.CTString, _) => symbolsV3_4.CTString
+
+    case (SemanticDirectionV3_3.BOTH, _) => expressionsV3_4.SemanticDirection.BOTH
+    case (SemanticDirectionV3_3.INCOMING, _) => expressionsV3_4.SemanticDirection.INCOMING
+    case (SemanticDirectionV3_3.OUTGOING, _) => expressionsV3_4.SemanticDirection.OUTGOING
+
+    case (astV3_3.NilPathStep, _) => expressionsV3_4.NilPathStep
+    case (pathStep: astV3_3.PathStep, children: Seq[AnyRef]) =>
+      convertVersion("frontend.v3_3.ast", "v3_4.expressions", pathStep, children)
+    case (_: frontendV3_3.helpers.NonEmptyList[_], children: Seq[AnyRef]) => NonEmptyList.from(children)
+
+    case (_: List[_], children: Seq[AnyRef]) => children.toList
     case (_: Seq[_], children: Seq[AnyRef]) => children.toIndexedSeq
     case (_: Set[_], children: Seq[AnyRef]) => children.toSet
     case (_: Map[_, _], children: Seq[AnyRef]) => Map(children.map(_.asInstanceOf[(_, _)]): _*)
@@ -47,28 +85,29 @@ object LogicalPlanConverter {
   private def getConstructor(classNameV3_3: String, oldPackage: String, newPackage: String): Constructor[_] = {
     constructors.get.getOrElseUpdate((classNameV3_3, oldPackage, newPackage), {
       val classNameV3_4 = classNameV3_3.replace(oldPackage, newPackage)
-      val clazz = Class.forName(classNameV3_4)
-      try {
-        clazz.getConstructors.head
-      } catch {
-        case _: NoSuchElementException =>
-          throw new InternalException(
-            s"Failed trying to rewrite $clazz - this class does not have a constructor"
-          )
+      Try(Class.forName(classNameV3_4)).map(_.getConstructors.head) match {
+        case Success(c) => c
+        case Failure(e: ClassNotFoundException) => throw new InternalException(
+          s"Failed trying to rewrite $classNameV3_3 - 3.4 class not found ($classNameV3_4)", e)
+        case Failure(e: NoSuchElementException) => throw new InternalException(
+          s"Failed trying to rewrite $classNameV3_3 - this class does not have a constructor", e)
       }
     })
   }
 
-  private def convertVersion(oldPackage: String, newPackage: String, thing: AnyRef, children: Seq[AnyRef], extraArg: AnyRef, assignableClazzForArg: Class[_]): AnyRef = {
-    val classNameV3_3 = thing.getClass.getName
+  private def convertVersion(oldPackage: String, newPackage: String, thing: AnyRef, children: Seq[AnyRef], extraArg: AnyRef = null, assignableClazzForArg: Class[_] = null): AnyRef = {
+    val thingClass = thing.getClass
+    val classNameV3_3 = thingClass.getName
     val constructor = getConstructor(classNameV3_3, oldPackage, newPackage)
 
     val params = constructor.getParameterTypes
     val args = children.toVector
-    val hasExtraParam = params.length == args.length + 1
-    val lastParamIsPos = params.last.isAssignableFrom(assignableClazzForArg)
-    val ctorArgs = if (hasExtraParam && lastParamIsPos) args :+ extraArg else args
+    val ctorArgs = if (params.length == args.length + 1 && params.last.isAssignableFrom(assignableClazzForArg)) args :+ extraArg else args
 
-    constructor.newInstance(ctorArgs: _*).asInstanceOf[AnyRef]
+    Try(constructor.newInstance(ctorArgs: _*).asInstanceOf[AnyRef]) match {
+      case Success(i) => i
+      case Failure(e) =>
+        throw new IllegalArgumentException(s"Could not construct ${thingClass.getSimpleName} with arguments ${ctorArgs.toList}", e)
+    }
   }
 }
--- community/cypher/cypher/src/test/scala/org/neo4j/cypher/internal/compatibility/v3_3/LogicalPlanConverterTest.scala
+++ community/cypher/cypher/src/test/scala/org/neo4j/cypher/internal/compatibility/v3_3/LogicalPlanConverterTest.scala
@@ -1,18 +1,28 @@
 package org.neo4j.cypher.internal.compatibility.v3_3
 
-import org.neo4j.cypher.internal.frontend.v3_3.{InputPosition => InputPositionV3_3, ast => astV3_3}
+import java.lang.reflect.Modifier
+
+import org.neo4j.cypher.internal.frontend.v3_3.{InputPosition => InputPositionV3_3, SemanticDirection => SemanticDirectionV3_3, ast => astV3_3, symbols => symbolsV3_3}
+import org.neo4j.cypher.internal.frontend.{v3_3 => frontendV3_3}
 import org.neo4j.cypher.internal.ir.v3_3.{IdName => IdNameV3_3}
 import org.neo4j.cypher.internal.ir.v3_4.{IdName => IdNameV3_4}
-import org.neo4j.cypher.internal.util.v3_4.InputPosition
-import org.neo4j.cypher.internal.v3_4.{expressions => expressionsV3_4}
+import org.neo4j.cypher.internal.util.v3_4.{InputPosition, NonEmptyList, symbols => symbolsV3_4}
 import org.neo4j.cypher.internal.v3_3.logical.{plans => plansV3_3}
+import org.neo4j.cypher.internal.v3_4.expressions.{PathExpression, SemanticDirection}
 import org.neo4j.cypher.internal.v3_4.logical.{plans => plansV3_4}
+import org.neo4j.cypher.internal.v3_4.{expressions => expressionsV3_4}
+import org.reflections.Reflections
 import org.scalatest.{FunSuite, Matchers}
+
+import scala.collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
 
 class LogicalPlanConverterTest extends FunSuite with Matchers {
 
   val pos3_3 = InputPositionV3_3(0,0,0)
   val pos3_4 = InputPosition(0,0,0)
+  val reflectExpressions = new Reflections("org.neo4j.cypher.internal.frontend.v3_3.ast")
+  val reflectLogicalPlans = new Reflections("org.neo4j.cypher.internal.v3_3.logical.plans")
 
   test("should convert an IntegerLiteral with its position") {
     val i3_3 = astV3_3.SignedDecimalIntegerLiteral("5")(InputPositionV3_3(1, 2, 3))
@@ -107,6 +117,48 @@ class LogicalPlanConverterTest extends FunSuite with Matchers {
     LogicalPlanConverter.convertExpression[expressionsV3_4.CaseExpression](m3_3) should be(m3_4)
   }
 
+  test("should convert PathExpression") {
+    val var3_3 = astV3_3.Variable("n")(pos3_3)
+    val psv3_3a = astV3_3.NilPathStep
+    val psv3_3b = astV3_3.MultiRelationshipPathStep(var3_3, SemanticDirectionV3_3.BOTH, psv3_3a)
+    val psv3_3c = astV3_3.SingleRelationshipPathStep(var3_3, SemanticDirectionV3_3.OUTGOING, psv3_3b)
+    val psv3_3d = astV3_3.NodePathStep(var3_3, psv3_3c)
+    val pexpv3_3 = astV3_3.PathExpression(psv3_3d)(pos3_3)
+
+    val var3_4 = expressionsV3_4.Variable("n")(pos3_4)
+    val psv3_4a = expressionsV3_4.NilPathStep
+    val psv3_4b = expressionsV3_4.MultiRelationshipPathStep(var3_4, SemanticDirection.BOTH, psv3_4a)
+    val psv3_4c = expressionsV3_4.SingleRelationshipPathStep(var3_4, SemanticDirection.OUTGOING, psv3_4b)
+    val psv3_4d = expressionsV3_4.NodePathStep(var3_4, psv3_4c)
+    val pexpv3_4 = expressionsV3_4.PathExpression(psv3_4d)(pos3_4)
+
+    LogicalPlanConverter.convertExpression[PathExpression](pexpv3_3) should be(pexpv3_4)
+  }
+
+  test("should convert AndedPropertyInequalities") {
+    val var3_3 = astV3_3.Variable("n")(pos3_3)
+    val p3_3 = astV3_3.Property(var3_3, astV3_3.PropertyKeyName("n")(pos3_3))(pos3_3)
+    val i3_3 = astV3_3.LessThan(var3_3, var3_3)(pos3_3)
+    val a3_3 = astV3_3.AndedPropertyInequalities(var3_3, p3_3, frontendV3_3.helpers.NonEmptyList(i3_3))
+
+    val var3_4 = expressionsV3_4.Variable("n")(pos3_4)
+    val p3_4 = expressionsV3_4.Property(var3_4, expressionsV3_4.PropertyKeyName("n")(pos3_4))(pos3_4)
+    val i3_4 = expressionsV3_4.LessThan(var3_4, var3_4)(pos3_4)
+    val a3_4 = expressionsV3_4.AndedPropertyInequalities(var3_4, p3_4, NonEmptyList(i3_4))
+
+    LogicalPlanConverter.convertExpression[PathExpression](a3_3) should be(a3_4)
+  }
+
+  test("should convert Parameter and CypherTypes") {
+    val p3_3a = astV3_3.Parameter("a", symbolsV3_3.CTBoolean)(pos3_3)
+    val p3_3b = astV3_3.Parameter("a", symbolsV3_3.CTList(symbolsV3_3.CTAny))(pos3_3)
+    val p3_4a = expressionsV3_4.Parameter("a", symbolsV3_4.CTBoolean)(pos3_4)
+    val p3_4b = expressionsV3_4.Parameter("a", symbolsV3_4.CTList(symbolsV3_4.CTAny))(pos3_4)
+
+    LogicalPlanConverter.convertExpression[PathExpression](p3_3a) should be(p3_4a)
+    LogicalPlanConverter.convertExpression[PathExpression](p3_3b) should be(p3_4b)
+  }
+
   test("should convert AllNodeScan and keep id") {
     val a3_3 = plansV3_3.AllNodesScan(IdNameV3_3("n"), Set.empty)(null)
     a3_3.assignIds()
@@ -138,5 +190,58 @@ class LogicalPlanConverterTest extends FunSuite with Matchers {
     rewrittenPlan.lhs.get.assignedId should be(helpers.as3_4(ans_id))
   }
 
-  // TODO test all Expression and LogicalPlan subclasses with reflections library
+  test("should convert all expressions") {
+    val subTypes = reflectExpressions.getSubTypesOf(classOf[astV3_3.Expression]).asScala
+    subTypes.filter { c => !Modifier.isAbstract(c.getModifiers) }.foreach { subType =>
+      val constructor = subType.getConstructors.head
+      val paramTypes = constructor.getParameterTypes
+      Try {
+        val constructorArgs = paramTypes.asInstanceOf[Array[Class[AnyRef]]].map(x => argumentProvider(x))
+        constructor.newInstance(constructorArgs: _*).asInstanceOf[astV3_3.Expression]
+      } match {
+        case Success(expressionV3_3) =>
+          val rewritten = LogicalPlanConverter.convertExpression[expressionsV3_4.Expression](expressionV3_3)
+          rewritten shouldBe an[expressionsV3_4.Expression]
+        case Failure(e: InstantiationException) => fail(s"could not instantiate 3.3 expression: ${subType.getSimpleName} with arguments ${paramTypes.toList}", e)
+        case Failure(e) => fail(s"Converting ${subType.getName} failed", e)
+      }
+    }
+  }
+
+  private def argumentProvider[T <: AnyRef](clazz: Class[T]): T = {
+    val variable = astV3_3.Variable("n")(pos3_3)
+    val value = clazz.getSimpleName match {
+      case "Variable" => variable
+      case "Property" => astV3_3.Property(variable, argumentProvider(classOf[astV3_3.PropertyKeyName]))(pos3_3)
+      case "PropertyKeyName" => astV3_3.PropertyKeyName("n")(pos3_3)
+      case "PathStep" => astV3_3.NilPathStep
+      case "Expression" => variable
+      case "InputPosition" => pos3_3
+      case "ReduceScope" => astV3_3.ReduceScope(variable, variable, variable)(pos3_3)
+      case "FilterScope" => astV3_3.FilterScope(variable, Some(variable))(pos3_3)
+      case "ExtractScope" => astV3_3.ExtractScope(variable, Some(variable), Some(variable))(pos3_3)
+      case "RelationshipsPattern" => astV3_3.RelationshipsPattern(argumentProvider(classOf[astV3_3.RelationshipChain]))(pos3_3)
+      case "RelationshipChain" => astV3_3.RelationshipChain(argumentProvider(classOf[astV3_3.PatternElement]), argumentProvider(classOf[astV3_3.RelationshipPattern]), argumentProvider(classOf[astV3_3.NodePattern]))(pos3_3)
+      case "RelationshipPattern" => astV3_3.RelationshipPattern(Some(variable), Seq.empty, None, None, frontendV3_3.SemanticDirection.OUTGOING)(pos3_3)
+      case "NodePattern" => new astV3_3.InvalidNodePattern(variable)(pos3_3)
+      case "PatternElement" => new astV3_3.InvalidNodePattern(variable)(pos3_3)
+      case "NonEmptyList" => frontendV3_3.helpers.NonEmptyList(1)
+      case "Namespace" => astV3_3.Namespace()(pos3_3)
+      case "FunctionName" => astV3_3.FunctionName("a")(pos3_3)
+      case "SemanticDirection" => frontendV3_3.SemanticDirection.OUTGOING
+      case "ShortestPaths" => astV3_3.ShortestPaths(argumentProvider(classOf[astV3_3.PatternElement]), true)(pos3_3)
+      case "CypherType" => symbolsV3_3.CTBoolean
+      case "Scope" => frontendV3_3.Scope.empty
+
+      case "IndexedSeq" => IndexedSeq.empty
+      case "boolean" => true
+      case "String" => "test"
+      case "Option" => None
+      case "Set" => Set.empty
+      case "Seq" => Seq.empty
+    }
+    value.asInstanceOf[T]
+  }
+
+  // TODO test all LogicalPlan subclasses with reflections library
 }
