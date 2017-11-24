8a8a19d6e7d3e4b18877740c65a91eddf5c1432b
Satia Herfert <satiaherfert@gmx.de>
2017-11-24 11:30:51 +0100

    Memoization of Constructors.

--- community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_3/LogicalPlanConverter.scala
+++ community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_3/LogicalPlanConverter.scala
@@ -2,17 +2,18 @@ package org.neo4j.cypher.internal.compatibility.v3_3
 
 import java.lang.reflect.Constructor
 
+import org.neo4j.cypher.internal.frontend.v3_3.ast.{Expression => ExpressionV3_3}
+import org.neo4j.cypher.internal.frontend.v3_3.{ast => astV3_3}
+import org.neo4j.cypher.internal.ir.v3_3.{IdName => IdNameV3_3}
+import org.neo4j.cypher.internal.ir.v3_4.{PlannerQuery, IdName => IdNameV3_4}
 import org.neo4j.cypher.internal.util.v3_4.Rewritable.{DuplicatableProduct, RewritableAny}
 import org.neo4j.cypher.internal.util.v3_4.{InputPosition, InternalException, RewriterWithArgs, bottomUpWithArgs}
-import org.neo4j.cypher.internal.frontend.v3_3.{ast => astV3_3}
-import org.neo4j.cypher.internal.ir.v3_4.PlannerQuery
 import org.neo4j.cypher.internal.v3_3.logical.plans.{LogicalPlan => LogicalPlanV3_3}
-import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan => LogicalPlanV3_4}
 import org.neo4j.cypher.internal.v3_3.logical.{plans => plansV3_3}
-import org.neo4j.cypher.internal.frontend.v3_3.ast.{Expression => ExpressionV3_3}
 import org.neo4j.cypher.internal.v3_4.expressions.{Expression => ExpressionV3_4}
-import org.neo4j.cypher.internal.ir.v3_3.{IdName => IdNameV3_3}
-import org.neo4j.cypher.internal.ir.v3_4.{IdName => IdNameV3_4}
+import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan => LogicalPlanV3_4}
+
+import scala.collection.mutable.{HashMap => MutableHashMap}
 
 object LogicalPlanConverter {
   private val rewriter: RewriterWithArgs = bottomUpWithArgs(RewriterWithArgs.lift {
@@ -38,22 +39,29 @@ object LogicalPlanConverter {
     new RewritableAny[ExpressionV3_3](expression).rewrite(rewriter, Seq.empty).asInstanceOf[T]
   }
 
-  // TODO memoization?
-  private def getConstructor(clazz: Class[_]): Constructor[_] = {
-    try {
-      clazz.getConstructors.head
-    } catch {
-      case _: NoSuchElementException =>
-        throw new InternalException(
-          s"Failed trying to rewrite $clazz - this class does not have a constructor"
-        )
-    }
+  private val constructors = new ThreadLocal[MutableHashMap[(String, String, String), Constructor[_]]]() {
+    override def initialValue: MutableHashMap[(String, String, String), Constructor[_]] =
+      new MutableHashMap[(String, String, String), Constructor[_]]
+  }
+
+  private def getConstructor(classNameV3_3: String, oldPackage: String, newPackage: String): Constructor[_] = {
+    constructors.get.getOrElseUpdate((classNameV3_3, oldPackage, newPackage), {
+      val classNameV3_4 = classNameV3_3.replace(oldPackage, newPackage)
+      val clazz = Class.forName(classNameV3_4)
+      try {
+        clazz.getConstructors.head
+      } catch {
+        case _: NoSuchElementException =>
+          throw new InternalException(
+            s"Failed trying to rewrite $clazz - this class does not have a constructor"
+          )
+      }
+    })
   }
 
   private def convertVersion(oldPackage: String, newPackage: String, thing: AnyRef, children: Seq[AnyRef], extraArg: AnyRef, assignableClazzForArg: Class[_]): AnyRef = {
     val classNameV3_3 = thing.getClass.getName
-    val classNameV3_4 = classNameV3_3.replace(oldPackage, newPackage)
-    val constructor = getConstructor(Class.forName(classNameV3_4))
+    val constructor = getConstructor(classNameV3_3, oldPackage, newPackage)
 
     val params = constructor.getParameterTypes
     val args = children.toVector
