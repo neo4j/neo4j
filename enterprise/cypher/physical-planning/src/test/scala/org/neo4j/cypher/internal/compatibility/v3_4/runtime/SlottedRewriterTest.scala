819442fcb4825eea9514e38015e1bdedd18f5503
Henrik Nyman <henrik.nyman@neotechnology.com>
2017-11-24 09:39:15 +0100

    Fix unit test

--- enterprise/cypher/physical-planning/src/test/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/SlottedRewriterTest.scala
+++ enterprise/cypher/physical-planning/src/test/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/SlottedRewriterTest.scala
@@ -355,7 +355,7 @@ class SlottedRewriterTest extends CypherFunSuite with AstConstructionTestSupport
 
     // then
 
-    val pr1BafterRewrite = Projection(pr1A, Map("xx" -> ReferenceFromSlot(0, "xx")))(solved)
+    val pr1BafterRewrite = Projection(pr1A, Map("xx" -> ReferenceFromSlot(0, "x")))(solved)
     val applyAfterRewrite = Apply(pr1BafterRewrite, pr2)(solved)
 
     resultPlan should equal(
