97b642159f600b1c3ab01a644fc46308f35f2834
Martin Furmanski <martin.furmanski@neotechnology.com>
2017-12-05 17:26:10 +0100

    Release the storecopy mutex after the store copy
    The store copy request handler just queues up requests
    on the netty pipeline and the release of the mutex must
    be scheduled as a response to the completion of the actual
    handling. This is done by registering on the future supplied
    by netty.
    
    Also did a general clean up of the code here with a better flow,
    cleaner paragraphs, refactoring of methods and a few comments.

--- enterprise/causal-clustering/src/main/java/org/neo4j/causalclustering/catchup/storecopy/GetStoreRequestHandler.java
+++ enterprise/causal-clustering/src/main/java/org/neo4j/causalclustering/catchup/storecopy/GetStoreRequestHandler.java
@@ -19,12 +19,15 @@
  */
 package org.neo4j.causalclustering.catchup.storecopy;
 
-import java.io.File;
-import java.util.Optional;
-import java.util.function.Supplier;
-
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 import org.neo4j.causalclustering.catchup.CatchupServerProtocol;
 import org.neo4j.causalclustering.catchup.ResponseMessageType;
@@ -43,6 +46,7 @@ import org.neo4j.logging.LogProvider;
 import org.neo4j.storageengine.api.StoreFileMetadata;
 
 import static org.neo4j.causalclustering.catchup.CatchupServerProtocol.State;
+import static org.neo4j.causalclustering.catchup.storecopy.StoreCopyFinishedResponse.Status.E_STORE_ID_MISMATCH;
 import static org.neo4j.causalclustering.catchup.storecopy.StoreCopyFinishedResponse.Status.SUCCESS;
 import static org.neo4j.io.fs.FileUtils.relativePath;
 
@@ -56,9 +60,8 @@ public class GetStoreRequestHandler extends SimpleChannelInboundHandler<GetStore
     private final Log log;
     private final StoreCopyCheckPointMutex mutex;
 
-    public GetStoreRequestHandler( CatchupServerProtocol protocol, Supplier<NeoStoreDataSource> dataSource,
-            Supplier<CheckPointer> checkPointerSupplier, FileSystemAbstraction fs, PageCache pageCache,
-            LogProvider logProvider, StoreCopyCheckPointMutex mutex )
+    public GetStoreRequestHandler( CatchupServerProtocol protocol, Supplier<NeoStoreDataSource> dataSource, Supplier<CheckPointer> checkPointerSupplier,
+            FileSystemAbstraction fs, PageCache pageCache, LogProvider logProvider, StoreCopyCheckPointMutex mutex )
     {
         this.protocol = protocol;
         this.dataSource = dataSource;
@@ -74,48 +77,67 @@ public class GetStoreRequestHandler extends SimpleChannelInboundHandler<GetStore
     {
         if ( !msg.expectedStoreId().equalToKernelStoreId( dataSource.get().getStoreId() ) )
         {
-            endStoreCopy( SUCCESS, ctx, -1 );
+            endStoreCopy( E_STORE_ID_MISMATCH, ctx, -1 );
         }
         else
         {
-            CheckPointer checkPointer = checkPointerSupplier.get();
-            long lastCheckPointedTx;
-            try ( Resource lock = mutex.storeCopy(
-                    () -> checkPointer.tryCheckPoint( new SimpleTriggerInfo( "Store copy" ) ) );
-                    ResourceIterator<StoreFileMetadata> files = dataSource.get().listStoreFiles( false ) )
-            {
-                lastCheckPointedTx = checkPointer.lastCheckPointedTransactionId();
-                while ( files.hasNext() )
-                {
-                    StoreFileMetadata fileMetadata = files.next();
-                    File file = fileMetadata.file();
-                    log.info( "Sending file " + file );
-                    ctx.writeAndFlush( ResponseMessageType.FILE );
-                    ctx.writeAndFlush( new FileHeader( relativePath( dataSource.get().getStoreDir(), file ),
-                            fileMetadata.recordSize() ) );
-                    Optional<PagedFile> existingMapping = pageCache.getExistingMapping( file );
-                    if ( existingMapping.isPresent() )
-                    {
-                        try ( PagedFile pagedFile = existingMapping.get() )
-                        {
-                            ctx.writeAndFlush( new FileSender(
-                                    pagedFile.openReadableByteChannel() ) );
-                        }
-                    }
-                    else
-                    {
-                        ctx.writeAndFlush( new FileSender( fs.open( file, "r" ) ) );
-                    }
-                }
-            }
-            endStoreCopy( SUCCESS, ctx, lastCheckPointedTx );
+            queueStoreCopy( ctx );
         }
         protocol.expect( State.MESSAGE_TYPE );
     }
 
-    private void endStoreCopy( Status status, ChannelHandlerContext ctx, long lastCommittedTxBeforeStoreCopy )
+    /**
+     * This queues all the file sending operations on the outgoing pipeline, including
+     * chunking {@link FileSender} handlers. Note that we do not block here.
+     */
+    private void queueStoreCopy( ChannelHandlerContext ctx ) throws IOException
+    {
+        CheckPointer checkPointer = checkPointerSupplier.get();
+
+        // The checkpoint-lock will be released when the entire store copy has finished.
+        Resource checkPointLock = mutex.storeCopy( () -> checkPointer.tryCheckPoint( new SimpleTriggerInfo( "Store copy" ) ) );
+        long lastCheckPointedTx = checkPointer.lastCheckPointedTransactionId();
+
+        ResourceIterator<StoreFileMetadata> files = dataSource.get().listStoreFiles( false );
+        while ( files.hasNext() )
+        {
+            StoreFileMetadata fileMetadata = files.next();
+            File file = fileMetadata.file();
+            log.debug( "Sending file " + file );
+
+            ctx.writeAndFlush( ResponseMessageType.FILE );
+            ctx.writeAndFlush( new FileHeader( relativePath( dataSource.get().getStoreDir(), file ), fileMetadata.recordSize() ) );
+
+            ReadableByteChannel channel = getReadableChannel( file );
+            ctx.writeAndFlush( new FileSender( channel ) );
+        }
+
+        ChannelFuture completion = endStoreCopy( SUCCESS, ctx, lastCheckPointedTx );
+        completion.addListener( f -> checkPointLock.close() );
+    }
+
+    private ReadableByteChannel getReadableChannel( File file ) throws IOException
+    {
+        ReadableByteChannel channel;
+        Optional<PagedFile> existingMapping = pageCache.getExistingMapping( file );
+        if ( existingMapping.isPresent() )
+        {
+            try ( PagedFile pagedFile = existingMapping.get() )
+            {
+
+                channel = pagedFile.openReadableByteChannel();
+            }
+        }
+        else
+        {
+            channel = fs.open( file, "r" );
+        }
+        return channel;
+    }
+
+    private ChannelFuture endStoreCopy( Status status, ChannelHandlerContext ctx, long lastCommittedTxBeforeStoreCopy )
     {
         ctx.write( ResponseMessageType.STORE_COPY_FINISHED );
-        ctx.writeAndFlush( new StoreCopyFinishedResponse( status, lastCommittedTxBeforeStoreCopy ) );
+        return ctx.writeAndFlush( new StoreCopyFinishedResponse( status, lastCommittedTxBeforeStoreCopy ) );
     }
 }
