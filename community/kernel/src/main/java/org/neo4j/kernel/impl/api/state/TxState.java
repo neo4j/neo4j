c1c4f33f51493e3b82f082e05a38379fe32e32f1
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2017-12-28 11:50:36 +0100

    Lazy propertiesMap in TxState

--- community/kernel/src/main/java/org/neo4j/kernel/impl/api/state/TxState.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/api/state/TxState.java
@@ -152,7 +152,7 @@ public final class TxState implements TransactionState, RelationshipVisitor.Home
     // Tracks added and removed relationships, not modified relationships
     private RelationshipDiffSets<Long> relationships;
 
-    private PrimitiveLongObjectMap<PropertyContainerState> propertiesMap = Primitive.longObjectMap();
+    private PrimitiveLongObjectMap<PropertyContainerState> propertiesMap;
 
     /**
      * These two sets are needed because create-delete in same transaction is a no-op in {@link DiffSets}
@@ -166,11 +166,11 @@ public final class TxState implements TransactionState, RelationshipVisitor.Home
 
     private Map<LabelSchemaDescriptor, Map<ValueTuple, DiffSets<Long>>> indexUpdates;
 
-    private InstanceCache<TxSingleNodeCursor> singleNodeCursor;
-    private InstanceCache<TxIteratorRelationshipCursor> iteratorRelationshipCursor;
-    private InstanceCache<TxSingleRelationshipCursor> singleRelationshipCursor;
-    private InstanceCache<TxAllPropertyCursor> propertyCursor;
-    private InstanceCache<TxSinglePropertyCursor> singlePropertyCursor;
+    private final InstanceCache<TxSingleNodeCursor> singleNodeCursor;
+    private final InstanceCache<TxIteratorRelationshipCursor> iteratorRelationshipCursor;
+    private final InstanceCache<TxSingleRelationshipCursor> singleRelationshipCursor;
+    private final InstanceCache<TxAllPropertyCursor> propertyCursor;
+    private final InstanceCache<TxSinglePropertyCursor> singlePropertyCursor;
 
     private boolean hasChanges;
     private boolean hasDataChanges;
@@ -667,6 +667,10 @@ public final class TxState implements TransactionState, RelationshipVisitor.Home
     @Override
     public void registerProperties( long ref, PropertyContainerState state )
     {
+        if ( propertiesMap == null )
+        {
+            propertiesMap = Primitive.longObjectMap();
+        }
         propertiesMap.put( ref, state );
     }
 
@@ -718,7 +722,7 @@ public final class TxState implements TransactionState, RelationshipVisitor.Home
     @Override
     public PropertyContainerState getPropertiesState( long reference )
     {
-        return propertiesMap.get( reference );
+        return propertiesMap == null ? null : propertiesMap.get( reference );
     }
 
     @Override
