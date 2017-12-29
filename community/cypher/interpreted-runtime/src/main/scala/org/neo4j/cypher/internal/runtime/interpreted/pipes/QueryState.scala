57e7f1151d5275059aedbb23c271bd0e8b69ceb4
Pontus Melke <pontusmelke@gmail.com>
2017-12-29 11:08:58 +0100

    Cast instead of polymorphism
    This extra layer of indirection costs us a lot so even though the code
    is now slightly uglier we are getting the benefit of a monomorphic call
    site with essentially perfect branch prediction.

--- community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/QueryState.scala
+++ community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/QueryState.scala
@@ -22,11 +22,11 @@ package org.neo4j.cypher.internal.runtime.interpreted.pipes
 import java.util.UUID
 
 import org.neo4j.collection.primitive.PrimitiveLongSet
-import org.neo4j.cypher.internal.runtime.{QueryContext, QueryStatistics}
-import org.neo4j.cypher.internal.util.v3_4.ParameterNotFoundException
-import org.neo4j.cypher.internal.runtime.interpreted.{ExecutionContext, MutableMaps}
 import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.PathValueBuilder
 import org.neo4j.cypher.internal.runtime.interpreted.commands.predicates.{InCheckContainer, SingleThreadedLRUCache}
+import org.neo4j.cypher.internal.runtime.interpreted.{ExecutionContext, MapExecutionContext, MutableMaps}
+import org.neo4j.cypher.internal.runtime.{QueryContext, QueryStatistics}
+import org.neo4j.cypher.internal.util.v3_4.ParameterNotFoundException
 import org.neo4j.values.AnyValue
 import org.neo4j.values.virtual.MapValue
 
@@ -42,7 +42,8 @@ class QueryState(val query: QueryContext,
                  val triadicState: mutable.Map[String, PrimitiveLongSet] = mutable.Map.empty,
                  val repeatableReads: mutable.Map[Pipe, Seq[ExecutionContext]] = mutable.Map.empty,
                  val cachedIn: SingleThreadedLRUCache[Any, InCheckContainer] =
-                   new SingleThreadedLRUCache(maxSize = 16)) {
+                 new SingleThreadedLRUCache(maxSize = 16)) {
+
   private var _pathValueBuilder: PathValueBuilder = _
 
   def createOrGetInitialContext(factory: ExecutionContextFactory): ExecutionContext =
@@ -64,7 +65,7 @@ class QueryState(val query: QueryContext,
 
   def readTimeStamp(): Long = timeReader.getTime
 
-  def  getParam(key: String): AnyValue = {
+  def getParam(key: String): AnyValue = {
     if (!params.containsKey(key)) throw new ParameterNotFoundException("Expected a parameter named " + key)
     params.get(key)
   }
@@ -72,38 +73,52 @@ class QueryState(val query: QueryContext,
   def getStatistics: QueryStatistics = query.getOptStatistics.getOrElse(QueryState.defaultStatistics)
 
   def withDecorator(decorator: PipeDecorator) =
-    new QueryState(query, resources, params, decorator, timeReader, initialContext, queryId, triadicState, repeatableReads, cachedIn)
+    new QueryState(query, resources, params, decorator, timeReader, initialContext, queryId, triadicState,
+                   repeatableReads, cachedIn)
 
   def withInitialContext(initialContext: ExecutionContext) =
-    new QueryState(query, resources, params, decorator, timeReader, Some(initialContext), queryId, triadicState, repeatableReads, cachedIn)
+    new QueryState(query, resources, params, decorator, timeReader, Some(initialContext), queryId, triadicState,
+                   repeatableReads, cachedIn)
 
   /**
     * When running on the RHS of an Apply, this method will fill an execution context with argument data
+    *
     * @param ctx ExecutionContext to fill with data
     */
-  def copyArgumentStateTo(ctx: ExecutionContext, nLongs: Int, nRefs: Int): Unit = initialContext.foreach(initData => ctx.copyFrom(initData, nLongs, nRefs))
+  def copyArgumentStateTo(ctx: ExecutionContext, nLongs: Int, nRefs: Int): Unit = initialContext
+    .foreach(initData => ctx.copyFrom(initData, nLongs, nRefs))
 
   def copyArgumentStateTo(ctx: ExecutionContext): Unit = initialContext.foreach(initData => initData.copyTo(ctx))
 
   def withQueryContext(query: QueryContext) =
-    new QueryState(query, resources, params, decorator, timeReader, initialContext, queryId, triadicState, repeatableReads, cachedIn)
+    new QueryState(query, resources, params, decorator, timeReader, initialContext, queryId, triadicState,
+                   repeatableReads, cachedIn)
 }
 
 object QueryState {
+
   val defaultStatistics = QueryStatistics()
 }
 
 class TimeReader {
+
   lazy val getTime: Long = System.currentTimeMillis()
 }
 
 trait ExecutionContextFactory {
+
   def newExecutionContext(m: mutable.Map[String, AnyValue] = MutableMaps.empty): ExecutionContext
+
   def newExecutionContext(): ExecutionContext
+
   def copyWith(init: ExecutionContext): ExecutionContext
+
   def copyWith(row: ExecutionContext, newEntries: Seq[(String, AnyValue)]): ExecutionContext
+
   def copyWith(row: ExecutionContext, key: String, value: AnyValue): ExecutionContext
+
   def copyWith(row: ExecutionContext, key1: String, value1: AnyValue, key2: String, value2: AnyValue): ExecutionContext
+
   def copyWith(row: ExecutionContext,
                key1: String, value1: AnyValue,
                key2: String, value2: AnyValue,
@@ -111,6 +126,7 @@ trait ExecutionContextFactory {
 }
 
 case class CommunityExecutionContextFactory() extends ExecutionContextFactory {
+
   override def newExecutionContext(m: mutable.Map[String, AnyValue] = MutableMaps.empty): ExecutionContext =
     ExecutionContext(m)
 
@@ -119,20 +135,32 @@ case class CommunityExecutionContextFactory() extends ExecutionContextFactory {
   // As community execution ctxs are immutable, we can simply return init here.
   override def copyWith(init: ExecutionContext): ExecutionContext = init
 
-  override def copyWith(row: ExecutionContext, newEntries: Seq[(String, AnyValue)]): ExecutionContext =
-    row.copyWith(newEntries)
+  override def copyWith(row: ExecutionContext, newEntries: Seq[(String, AnyValue)]): ExecutionContext =row match {
+    case context: MapExecutionContext => context.set(newEntries)
+    case _ =>  row.copyWith(newEntries)
+  }
 
-  override def copyWith(row: ExecutionContext, key: String, value: AnyValue): ExecutionContext =
-    row.copyWith(key, value)
+  // Not using polymorphism here, instead cast since the cost of being megamorhpic is too high
+  override def copyWith(row: ExecutionContext, key: String, value: AnyValue): ExecutionContext = row match {
+    case context: MapExecutionContext => context.set(key, value)
+    case _ => row.copyWith(key, value)
+  }
 
+  // Not using polymorphism here, instead cast since the cost of being megamorhpic is too high
   override def copyWith(row: ExecutionContext,
                         key1: String, value1: AnyValue,
-                        key2: String, value2: AnyValue): ExecutionContext =
-    row.copyWith(key1, value1, key2, value2)
+                        key2: String, value2: AnyValue): ExecutionContext = row match {
+    case context: MapExecutionContext => context.set(key1, value1, key2, value2)
+    case _ => row.copyWith(key1, value1, key2, value2)
+  }
 
+  // Not using polymorphism here, instead cast since the cost of being megamorhpic is too high
   override def copyWith(row: ExecutionContext,
                         key1: String, value1: AnyValue,
                         key2: String, value2: AnyValue,
-                        key3: String, value3: AnyValue): ExecutionContext =
-    row.copyWith(key1, value1, key2, value2, key3, value3)
+                        key3: String, value3: AnyValue): ExecutionContext = row match {
+    case context: MapExecutionContext => context.set(key1, value1, key2, value2, key3, value3)
+    case _ => row.copyWith(key1, value1, key2, value2, key3, value3)
+  }
+
 }
