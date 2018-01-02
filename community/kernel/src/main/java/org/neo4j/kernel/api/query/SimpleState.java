40b6a50a8b8be05049db11ba71ef622b7d5a6a09
Paul McIntosh <paul.mcintosh@monash.edu>
2018-01-02 12:59:33 +1100

    copyright updated to 2018 #10704

--- community/kernel/src/main/java/org/neo4j/kernel/api/query/SimpleState.java
+++ community/kernel/src/main/java/org/neo4j/kernel/api/query/SimpleState.java
@@ -1,7 +1,7 @@
 package org.neo4j.kernel.api.query;
 
 /*
- * Copyright (c) 2002-2017 "Neo Technology,"
+ * Copyright (c) 2002-2018 "Neo Technology,"
  * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
