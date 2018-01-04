557549e9e47c49067b48a550af821bab4c6e45a8
lutovich <konstantin.lutovich@neotechnology.com>
2018-01-04 23:58:40 +0100

    Fixed INIT struct size
    INIT struct has two fields - user agent and auth token. Thus size of
    the INIT struct is 2. However it has been packed as 1 by the
    `BoltRequestMessageWriter`, which is only used in tests.
    
    This commit fixes the problem.

--- community/bolt/src/test/java/org/neo4j/bolt/v1/messaging/BoltRequestMessageWriter.java
+++ community/bolt/src/test/java/org/neo4j/bolt/v1/messaging/BoltRequestMessageWriter.java
@@ -56,7 +56,7 @@ public class BoltRequestMessageWriter implements BoltRequestMessageHandler<IOExc
     @Override
     public void onInit( String clientName, Map<String,Object> credentials ) throws IOException
     {
-        packer.packStructHeader( 1, INIT.signature() );
+        packer.packStructHeader( 2, INIT.signature() );
         packer.pack( clientName );
         packer.pack( ValueUtils.asMapValue( credentials ) );
         onMessageComplete.onMessageComplete();
