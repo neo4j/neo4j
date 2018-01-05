d51569abd2748a7149482c6fec9a17ef9d421eb8
pstutz <philip@stutz.tech>
2018-01-05 14:06:43 +0100

    Fix variable name

--- enterprise/cypher/acceptance-spec-suite/src/test/scala/cypher/features/TCKValueToNeo4jValue.scala
+++ enterprise/cypher/acceptance-spec-suite/src/test/scala/cypher/features/TCKValueToNeo4jValue.scala
@@ -35,7 +35,7 @@ object TCKValueToNeo4jValue extends (CypherValue => Object) {
       case CypherPropertyMap(ps) => ps.map { case (k, v) => k -> TCKValueToNeo4jValue(v) }.asJava
       case l: CypherList => l.elements.map(TCKValueToNeo4jValue).asJava
       case CypherNull => null
-      case _ => throw new UnsupportedOperationException(s"Could not convert value $v to a Neo4j representation")
+      case _ => throw new UnsupportedOperationException(s"Could not convert value $value to a Neo4j representation")
     }
   }
 
