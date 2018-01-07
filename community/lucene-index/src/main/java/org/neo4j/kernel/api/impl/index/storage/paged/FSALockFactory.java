2425619c764be7ea9166b596d0ad480e5f021019
Jacob Hansson <jakewins@gmail.com>
2018-01-07 14:18:03 +0100

    Introduce PageCache backend for Lucene
    Note: This is an unsanctioned proposal, if you are an outside
    reader please do not expect this to be merged.
    
    Kernel: As discussed. Benchmarks forthcoming, opening PR to discuss
    feasibility of this. Suggested end-game, if we decide to do this, would
    be to introduce this behind an unsanctioned toggle, pickle in
    soak testing for a few months and then evaluate.
    
    Overview & Safety
    --
    
    This provides an implementation of the Lucene Directory API that uses
    `FileSystemAbstraction` for writes and `PageCache` for reads.
    
    It is modelled after, and is intended to inherit the safety aspects of,
    the `MMapDirectory` that ships with Lucene. In fact, it is precisely the
    same as `MMapDirectory`, except is uses the `PageCache` as its buffer
    backend for index inputs.
    
    Since Lucene writes to immutable files, and since it does not read
    from those files before they are fully written, this implementation
    has the same safety properties as MMapDirectory: Safety comes from
    proper fsyncing of writes, and this implementation shares the exact
    same write behavior as MMapDirectory.
    
    Hence: Review of this should, outside of regular crash fuzzing, verify
    that the above actually holds true: The write-side implementation should
    be equivalent to that of MMapDirectory.
    
    Background
    --
    
    Lucene provides multiple storage backends:
    
    - RAMDirectory
        in-memory, used by kernel in Ephemeral mode
    
    - NIOFSDirectory
        persistent, loads fixed chunks of indexes into a buffer
        as they are read; meaning lots of IO. Used by kernel if
        -Dorg.neo4j.kernel.api.impl.index.storage.DirectoryFactory.default_directory_factory=true
    
    - MMapDirectory
        persistent, memory maps whole indexes as they are read,
        used by kernel by default.
    
    RAMDirectory is not persistent. NIOFSDirectory is slow, so MMapDirectory
    is used by default.
    
    However, there are issues with MMapDirectory as well:
    
    *1. Swapping*
    
    Because MMapDirectory memory maps entire indexes, any non-trivial
    deployment will be forced to swap the Neo4j process. This introduces at
    least two issues. 1, there is no way to operate Neo4j in an environment
    without swap space and, 2, memory management of Neo4j is expanded to
    require operators to consider their virtual memory limits on day one.
    
    Since standard AWS images do not have swap enabled by default, this
    poses a substantial UX pain: Most users will experience this by the
    Neo4j process simply consuming indefinite amounts of memory, with no
    indication that turning on swap would solve the problem.
    
    There may also be problems with this swapping then inducing competition
    between hot indexes and runtime data structures in the database itself,
    although this isn't entirely clear.
    
    *2. Java MappedByteBuffer*
    
    As we are acutely aware, the Java memory mapping APIs have some
    undesireable characteristics. Lucene tries to alleviate this by
    using the Cleaner API.
    While this somewhat reduces GC pain, the BBs objects are still stuck
    on ReferenceQueues by the Cleaner API, increasing GC work during
    safepoints. It also means we're implicitly calling unsafe.freeMemory(address),
    and memory mapping, over and over as index readers are opened and closed.
    
    Rationale
    ---
    
    This implementation thus provides two benefits:
    
    1) It allows Neo4j to run in environments with no or limited Swap
    2) It gives Neo4j predictable and configurable memory use
    3) It removes substantial numbers of `mmap/dealloc` from hot paths

---
+++ community/lucene-index/src/main/java/org/neo4j/kernel/api/impl/index/storage/paged/FSALockFactory.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel.api.impl.index.storage.paged;
+
+import org.apache.lucene.store.AlreadyClosedException;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.Lock;
+import org.apache.lucene.store.LockFactory;
+import org.apache.lucene.store.LockObtainFailedException;
+import org.apache.lucene.store.NativeFSLockFactory;
+import org.apache.lucene.util.IOUtils;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.neo4j.io.fs.FileSystemAbstraction;
+import org.neo4j.io.fs.OpenMode;
+import org.neo4j.io.fs.StoreChannel;
+
+/**
+ * Clone of {@link NativeFSLockFactory} for {@link FileSystemAbstraction}.
+ */
+public class FSALockFactory extends LockFactory
+{
+    // Lucene implements concurrency across multiple Directory instances
+    // by using file locks; this way, concurrency is permitted across processes.
+    // However, Neo4j does not do cross-process concurrent access to store
+    // files. Meaning, we are paying for all these file locks for no reason..
+    //
+    // Lucene comes with an alternate locking implementation,
+    // SingleInstanceLockFactory, for use cases where it can be guaranteed a
+    // single Directory instance will be used at a time; we can give this
+    // guarantee.
+    //
+    // Hence: If we refactored our three (explicit, schema, full-text) usages
+    // of Lucene to use a single global Directory instance, we could get a
+    // performance boost by removing the need for file-system locks for Lucene.
+
+    private static final Set<String> LOCKS_HELD =
+            Collections.synchronizedSet( new HashSet<String>() );
+
+    private final FileSystemAbstraction fs;
+
+    public FSALockFactory( FileSystemAbstraction fs )
+    {
+        this.fs = fs;
+    }
+
+    @Override
+    public Lock obtainLock( Directory dir, String lockName ) throws IOException
+    {
+        if ( !(dir instanceof PagedDirectory) )
+        {
+            throw new IOException( "FSALockFactory can only be used in " +
+                    "combination with PagedDirectory." );
+        }
+        Path lockDir = ((PagedDirectory) dir).getPath();
+
+        // Ensure that lockDir exists and is a directory.
+        fs.mkdirs( lockDir.toFile() );
+
+        Path lockFile = lockDir.resolve( lockName );
+
+        try
+        {
+            fs.create( lockFile.toFile() ).close();
+        }
+        catch ( IOException ignore )
+        {
+            // we must create the file to have a truly canonical path.
+            // if it's already created, we don't care.
+            // if it cant be created, it will fail below.
+        }
+
+        final Path absPath = lockFile.toAbsolutePath();
+
+        // used as a best-effort check, to see if the underlying file has changed
+        final long creationTime = fs.lastModifiedTime( absPath.toFile() );
+
+        if ( LOCKS_HELD.add( absPath.toString() ) )
+        {
+            StoreChannel channel = null;
+            FileLock lock = null;
+            try
+            {
+                channel = fs.open( absPath.toFile(), OpenMode.READ_WRITE );
+                lock = channel.tryLock();
+                if ( lock != null )
+                {
+                    return new FSALock( fs, lock, absPath, creationTime );
+                }
+                else
+                {
+                    throw new LockObtainFailedException(
+                            "Lock held by another program: " + absPath );
+                }
+            }
+            finally
+            {
+                if ( lock == null )
+                {
+                    // not successful - clear up and move out
+                    IOUtils.closeWhileHandlingException( channel );
+                    clearLockHeld( absPath );  // clear LOCKS_HELD last
+                }
+            }
+        }
+        else
+        {
+            throw new LockObtainFailedException(
+                    "Lock held by this virtual machine: " + absPath );
+        }
+    }
+
+    private static void clearLockHeld( Path path ) throws IOException
+    {
+        boolean remove = LOCKS_HELD.remove( path.toString() );
+        if ( !remove )
+        {
+            throw new AlreadyClosedException(
+                    "Lock path was cleared but never marked as held: " + path );
+        }
+    }
+
+    static final class FSALock extends Lock
+    {
+        final FileSystemAbstraction fs;
+        final FileLock lock;
+        final Path path;
+        final long creationTime;
+        volatile boolean closed;
+
+        FSALock( FileSystemAbstraction fs, FileLock lock, Path path,
+                long creationTime )
+        {
+            this.fs = fs;
+            this.lock = lock;
+            this.path = path;
+            this.creationTime = creationTime;
+        }
+
+        @Override
+        public void ensureValid() throws IOException
+        {
+            if ( closed )
+            {
+                throw new AlreadyClosedException(
+                        "Lock instance already released: " + this );
+            }
+            // check we are still in the locks map
+            // (some debugger or something crazy didn't remove us)
+            if ( !LOCKS_HELD.contains( path.toString() ) )
+            {
+                throw new AlreadyClosedException(
+                        "Lock path unexpectedly cleared from map: " + this );
+            }
+            // check our lock wasn't invalidated.
+            if ( !lock.isValid() )
+            {
+                throw new AlreadyClosedException(
+                        "FileLock invalidated by an external force: " + this );
+            }
+            // try to validate the underlying file descriptor.
+            // this will throw IOException if something is wrong.
+            long size = lock.channel().size();
+            if ( size != 0 )
+            {
+                throw new AlreadyClosedException(
+                        "Unexpected lock file size: " + size + ", (lock=" +
+                                this + ")" );
+            }
+            // try to validate the backing file name, that it still exists,
+            // and has the same creation time as when we obtained the lock.
+            // if it differs, someone deleted our lock file
+            long ctime = fs.lastModifiedTime( path.toFile() );
+            if ( creationTime != ctime )
+            {
+                throw new AlreadyClosedException(
+                        "Underlying file changed by an external force at " +
+                                creationTime + ", (lock=" + this + ")" );
+            }
+        }
+
+        @Override
+        public synchronized void close() throws IOException
+        {
+            if ( closed )
+            {
+                return;
+            }
+            try ( FileChannel channel = lock.channel();
+                    FileLock ignored = this.lock )
+            {
+                assert channel != null;
+            }
+            finally
+            {
+                closed = true;
+                clearLockHeld( path );
+            }
+        }
+
+        @Override
+        public String toString()
+        {
+            return "FSALock(path=" + path + ",impl=" + lock + ",ctime=" +
+                    creationTime + ")";
+        }
+    }
+}
