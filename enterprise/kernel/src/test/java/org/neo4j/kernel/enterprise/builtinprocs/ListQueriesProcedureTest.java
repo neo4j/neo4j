67ecbe49cd10b5a73145b4a14dbc62b2cd90706d
Ragnar Mellbin <ragnar.mellbin@neotechnology.com>
2018-01-10 16:18:13 +0100

    Fix broken ListQueriesProceduresTest

--- enterprise/kernel/src/test/java/org/neo4j/kernel/enterprise/builtinprocs/ListQueriesProcedureTest.java
+++ enterprise/kernel/src/test/java/org/neo4j/kernel/enterprise/builtinprocs/ListQueriesProcedureTest.java
@@ -492,7 +492,8 @@ public class ListQueriesProcedureTest
             data = getQueryListing( query );
         }
         // then
-        assertThat( data, hasEntry( equalTo( "allocatedBytes" ), notNullValue() ) );
+        assertThat( data, hasEntry( equalTo( "allocatedBytes" ), anyOf( nullValue(), (Matcher) allOf(
+                instanceOf( Long.class ), greaterThan( 0L ) ) ) ) );
 
         // when
         db.execute( "call dbms.setConfigValue('" + track_query_allocation.name() + "', 'false')" );
@@ -510,7 +511,8 @@ public class ListQueriesProcedureTest
             data = getQueryListing( query );
         }
         // then
-        assertThat( data, hasEntry( equalTo( "allocatedBytes" ), notNullValue() ) );
+        assertThat( data, hasEntry( equalTo( "allocatedBytes" ), anyOf( nullValue(), (Matcher) allOf(
+                instanceOf( Long.class ), greaterThan( 0L ) ) ) ) );
     }
 
     private void shouldListUsedIndexes( String label, String property ) throws Exception
