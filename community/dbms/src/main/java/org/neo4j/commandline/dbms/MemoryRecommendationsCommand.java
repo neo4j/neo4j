cf089ca674a20a81c2f69151a1bd5edf7a29864c
Chris Vest <mr.chrisvest@gmail.com>
2018-01-12 10:48:11 +0100

    Add a `neo4j-admin memrec` command.
    This is an initial stab at having automated recommendations for the
    Neo4j memory settings.

---
+++ community/dbms/src/main/java/org/neo4j/commandline/dbms/MemoryRecommendationsCommand.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.commandline.dbms;
+
+import java.util.Locale;
+import java.util.function.ToDoubleFunction;
+
+import org.neo4j.commandline.admin.AdminCommand;
+import org.neo4j.commandline.admin.IncorrectUsage;
+import org.neo4j.commandline.admin.OutsideWorld;
+import org.neo4j.commandline.arguments.Arguments;
+import org.neo4j.commandline.arguments.OptionalNamedArg;
+import org.neo4j.kernel.impl.util.OsBeanUtil;
+
+import static java.lang.String.format;
+import static org.neo4j.configuration.ExternalSettings.initialHeapSize;
+import static org.neo4j.configuration.ExternalSettings.maxHeapSize;
+import static org.neo4j.graphdb.factory.GraphDatabaseSettings.pagecache_memory;
+import static org.neo4j.io.ByteUnit.ONE_GIBI_BYTE;
+import static org.neo4j.io.ByteUnit.ONE_KIBI_BYTE;
+import static org.neo4j.io.ByteUnit.ONE_MEBI_BYTE;
+import static org.neo4j.io.ByteUnit.gibiBytes;
+import static org.neo4j.io.ByteUnit.mebiBytes;
+import static org.neo4j.io.ByteUnit.tebiBytes;
+import static org.neo4j.kernel.configuration.Settings.BYTES;
+import static org.neo4j.kernel.configuration.Settings.buildSetting;
+
+public class MemoryRecommendationsCommand implements AdminCommand
+{
+    // Fields: {System Memory in GiBs; OS memory reserve in GiBs; JVM Heap memory in GiBs}.
+    // And the page cache gets what's left, though always at least 100 MiB.
+    // Heap never goes beyond 31 GiBs, which is the upper limit for compressed oops.
+    // More details on heap sizing wrt. compressed oops:
+    // https://www.elastic.co/guide/en/elasticsearch/guide/current/heap-sizing.html
+    private static final Bracket[] datapoints = {
+            new Bracket( 0.01, 0.005, 0.005 ),
+            new Bracket( 1.0, 0.5, 0.5 ),
+            new Bracket( 2.0, 1, 0.5 ),
+            new Bracket( 4.0, 1.5, 2 ),
+            new Bracket( 6.0, 2, 3 ),
+            new Bracket( 8.0, 2.5, 3.5 ),
+            new Bracket( 10.0, 3, 4 ),
+            new Bracket( 12.0, 3.5, 4.5 ),
+            new Bracket( 16.0, 4, 5 ),
+            new Bracket( 24.0, 6, 8 ),
+            new Bracket( 32.0, 8, 12 ),
+            new Bracket( 64.0, 12, 24 ),
+            new Bracket( 128.0, 16, 31 ),
+            new Bracket( 256.0, 20, 31 ),
+            new Bracket( 512.0, 24, 31 ),
+            new Bracket( 1024.0, 30, 31 ),
+    };
+    private static final String ARG_MEMORY = "memory";
+    private final OutsideWorld outsideWorld;
+
+    static long recommendOsMemory( long totalMemoryBytes )
+    {
+        Brackets brackets = findMemoryBrackets( totalMemoryBytes );
+        return brackets.recommend( Bracket::osMemory );
+    }
+
+    static long recommendHeapMemory( long totalMemoryBytes )
+    {
+        Brackets brackets = findMemoryBrackets( totalMemoryBytes );
+        return brackets.recommend( Bracket::heapMemory );
+    }
+
+    static long recommendPageCacheMemory( long totalMemoryBytes )
+    {
+        long osMemory = recommendOsMemory( totalMemoryBytes );
+        long heapMemory = recommendHeapMemory( totalMemoryBytes );
+        long recommendation = totalMemoryBytes - osMemory - heapMemory;
+        recommendation = Math.max( mebiBytes( 100 ), recommendation );
+        recommendation = Math.min( tebiBytes( 16 ), recommendation );
+        return recommendation;
+    }
+
+    private static Brackets findMemoryBrackets( long totalMemoryBytes )
+    {
+        double totalMemoryGB = ((double) totalMemoryBytes) / ((double) gibiBytes( 1 ));
+        Bracket lower = null;
+        Bracket upper = null;
+        for ( int i = 1; i < datapoints.length; i++ )
+        {
+            if ( totalMemoryGB < datapoints[i].totalMemory )
+            {
+                lower = datapoints[i - 1];
+                upper = datapoints[i];
+                break;
+            }
+        }
+        if ( lower == null )
+        {
+            lower = datapoints[datapoints.length - 1];
+            upper = datapoints[datapoints.length - 1];
+        }
+        return new Brackets( totalMemoryGB, lower, upper );
+    }
+
+    public static Arguments buildArgs()
+    {
+        long totalPhysicalMemory = OsBeanUtil.getTotalPhysicalMemory();
+        return new Arguments()
+                .withArgument( new OptionalNamedArg( ARG_MEMORY, "8g", bytesToString( totalPhysicalMemory ),
+                        "Recommend memory settings with respect to the given amount of memory, " +
+                        "instead of the total memory of the system running the command." ) );
+    }
+
+    static String bytesToString( long bytes )
+    {
+        if ( bytes > ONE_GIBI_BYTE )
+        {
+            return format( Locale.ROOT, "%.1fg", bytes / (double) ONE_GIBI_BYTE );
+        }
+        else if ( bytes > ONE_MEBI_BYTE )
+        {
+            return format( Locale.ROOT, "%.1fm", bytes / (double) ONE_MEBI_BYTE );
+        }
+        else if ( bytes > ONE_KIBI_BYTE )
+        {
+            return format( Locale.ROOT, "%.0fk", bytes / (double) ONE_KIBI_BYTE );
+        }
+        else
+        {
+            return String.valueOf( bytes );
+        }
+    }
+
+    MemoryRecommendationsCommand( OutsideWorld outsideWorld )
+    {
+        this.outsideWorld = outsideWorld;
+    }
+
+    @Override
+    public void execute( String[] args ) throws IncorrectUsage
+    {
+        Arguments arguments = buildArgs().parse( args );
+
+        String mem = arguments.get( ARG_MEMORY );
+        long memory = buildSetting( ARG_MEMORY, BYTES ).build().apply( arguments::get );
+        String os = bytesToString( recommendOsMemory( memory ) );
+        String heap = bytesToString( recommendHeapMemory( memory ) );
+        String pagecache = bytesToString( recommendPageCacheMemory( memory ) );
+
+        print( "# Memory settings recommendation from neo4j-admin memrec:" );
+        print( "#" );
+        print( "# Assuming the system is dedicated to running Neo4j and has " + mem + " of memory," );
+        print( "# we recommend a heap size of around " + heap + ", and a page cache of around " + pagecache + "," );
+        print( "# and that about " + os + " is left for the operating system, and the native memory" );
+        print( "# needed by Lucene and Netty." );
+        print( "#" );
+        print( "# Tip: If the indexing storage use is high, e.g. there are many indexes or most" );
+        print( "# data indexed, then it might advantageous to leave more memory for the" );
+        print( "# operating system." );
+        print( "#" );
+        print( "# Tip: The more concurrent transactions your workload has and the more updates" );
+        print( "# they do, the more heap memory you will need. However, don't allocate more" );
+        print( "# than 31g of heap, since this will disable pointer compression, also known as" );
+        print( "# \"compressed oops\", in the JVM and make less effective use of the heap." );
+        print( "#" );
+        print( "# Tip: Setting the initial and the max heap size to the same value means the" );
+        print( "# JVM will never need to change the heap size. Changing the heap size otherwise" );
+        print( "# involves a full GC, which is desirable to avoid." );
+        print( "#" );
+        print( "# Based on the above, the following memory settings are recommended:" );
+        print( initialHeapSize.name() + "=" + heap );
+        print( maxHeapSize.name() + "=" + heap );
+        print( pagecache_memory.name() + "=" + pagecache );
+    }
+
+    private void print( String text )
+    {
+        outsideWorld.stdOutLine( text );
+    }
+
+    private static final class Bracket
+    {
+        private final double totalMemory;
+        private final double osMemory;
+        private final double heapMemory;
+
+        private Bracket( double totalMemory, double osMemory, double heapMemory )
+        {
+            this.totalMemory = totalMemory;
+            this.osMemory = osMemory;
+            this.heapMemory = heapMemory;
+        }
+
+        double osMemory()
+        {
+            return osMemory;
+        }
+
+        double heapMemory()
+        {
+            return heapMemory;
+        }
+    }
+
+    private static final class Brackets
+    {
+        private final double totalMemoryGB;
+        private final Bracket lower;
+        private final Bracket upper;
+
+        private Brackets( double totalMemoryGB, Bracket lower, Bracket upper )
+        {
+            this.totalMemoryGB = totalMemoryGB;
+            this.lower = lower;
+            this.upper = upper;
+        }
+
+        private double differenceFactor()
+        {
+            if ( lower == upper )
+            {
+                return 0;
+            }
+            return (totalMemoryGB - lower.totalMemory) / (upper.totalMemory - lower.totalMemory);
+        }
+
+        public long recommend( ToDoubleFunction<Bracket> parameter )
+        {
+            double factor = differenceFactor();
+            double lowerParam = parameter.applyAsDouble( lower );
+            double upperParam = parameter.applyAsDouble( upper );
+            double diff = upperParam - lowerParam;
+            double recommend = lowerParam + (diff * factor);
+            return mebiBytes( (long) (recommend * 1024.0) );
+        }
+    }
+}
---
+++ community/dbms/src/test/java/org/neo4j/commandline/dbms/MemoryRecommendationsCommandTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.commandline.dbms;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import java.io.StringReader;
+import java.util.Map;
+
+import org.neo4j.commandline.admin.OutsideWorld;
+import org.neo4j.commandline.admin.RealOutsideWorld;
+import org.neo4j.helpers.collection.MapUtil;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+import static org.neo4j.commandline.dbms.MemoryRecommendationsCommand.bytesToString;
+import static org.neo4j.commandline.dbms.MemoryRecommendationsCommand.recommendHeapMemory;
+import static org.neo4j.commandline.dbms.MemoryRecommendationsCommand.recommendOsMemory;
+import static org.neo4j.commandline.dbms.MemoryRecommendationsCommand.recommendPageCacheMemory;
+import static org.neo4j.configuration.ExternalSettings.initialHeapSize;
+import static org.neo4j.configuration.ExternalSettings.maxHeapSize;
+import static org.neo4j.graphdb.factory.GraphDatabaseSettings.pagecache_memory;
+import static org.neo4j.io.ByteUnit.exbiBytes;
+import static org.neo4j.io.ByteUnit.gibiBytes;
+import static org.neo4j.io.ByteUnit.mebiBytes;
+import static org.neo4j.io.ByteUnit.tebiBytes;
+
+public class MemoryRecommendationsCommandTest
+{
+    @Test
+    public void mustRecommendOSMemory()
+    {
+        assertThat( recommendOsMemory( mebiBytes( 100 ) ), between( mebiBytes( 45 ), mebiBytes( 55 ) ) );
+        assertThat( recommendOsMemory( gibiBytes( 1 ) ), between( mebiBytes( 450 ), mebiBytes( 550 ) ) );
+        assertThat( recommendOsMemory( gibiBytes( 3 ) ), between( mebiBytes( 1256 ), mebiBytes( 1356 ) ) );
+        assertThat( recommendOsMemory( gibiBytes( 192 ) ), between( gibiBytes( 17 ), gibiBytes( 19 ) ) );
+        assertThat( recommendOsMemory( gibiBytes( 1920 ) ), greaterThan( gibiBytes( 29 ) ) );
+    }
+
+    @Test
+    public void mustRecommendHeapMemory()
+    {
+        assertThat( recommendHeapMemory( mebiBytes( 100 ) ), between( mebiBytes( 45 ), mebiBytes( 55 ) ) );
+        assertThat( recommendHeapMemory( gibiBytes( 1 ) ), between( mebiBytes( 450 ), mebiBytes( 550 ) ) );
+        assertThat( recommendHeapMemory( gibiBytes( 3 ) ), between( mebiBytes( 1256 ), mebiBytes( 1356 ) ) );
+        assertThat( recommendHeapMemory( gibiBytes( 6 ) ), between( mebiBytes( 3000 ), mebiBytes( 3200 ) ) );
+        assertThat( recommendHeapMemory( gibiBytes( 192 ) ), between( gibiBytes( 30 ), gibiBytes( 32 ) ) );
+        assertThat( recommendHeapMemory( gibiBytes( 1920 ) ), between( gibiBytes( 30 ), gibiBytes( 32 ) ) );
+    }
+
+    @Test
+    public void mustRecommendPageCacheMemory()
+    {
+        assertThat( recommendPageCacheMemory( mebiBytes( 100 ) ), between( mebiBytes( 95 ), mebiBytes( 105 ) ) );
+        assertThat( recommendPageCacheMemory( gibiBytes( 1 ) ), between( mebiBytes( 95 ), mebiBytes( 105 ) ) );
+        assertThat( recommendPageCacheMemory( gibiBytes( 3 ) ), between( mebiBytes( 470 ), mebiBytes( 530 ) ) );
+        assertThat( recommendPageCacheMemory( gibiBytes( 6 ) ), between( mebiBytes( 980 ), mebiBytes( 1048 ) ) );
+        assertThat( recommendPageCacheMemory( gibiBytes( 192 ) ), between( gibiBytes( 140 ), gibiBytes( 150 ) ) );
+        assertThat( recommendPageCacheMemory( gibiBytes( 1920 ) ), between( gibiBytes( 1850 ), gibiBytes( 1900 ) ) );
+
+        // Also never recommend more than 16 TiB of page cache memory, regardless of how much is available.
+        assertThat( recommendPageCacheMemory( exbiBytes( 1 ) ), lessThan( tebiBytes( 17 ) ) );
+    }
+
+    private Matcher<Long> between( long lowerBound, long upperBound )
+    {
+        return allOf( greaterThan( lowerBound ), lessThan( upperBound ) );
+    }
+
+    @Test
+    public void mustPrintRecommendationsAsConfigReadableOutput() throws Exception
+    {
+        StringBuilder output = new StringBuilder();
+        OutsideWorld outsideWorld = new RealOutsideWorld()
+        {
+            @Override
+            public void stdOutLine( String text )
+            {
+                output.append( text ).append( System.lineSeparator() );
+            }
+        };
+        MemoryRecommendationsCommand command = new MemoryRecommendationsCommand( outsideWorld );
+        String heap = bytesToString( recommendHeapMemory( gibiBytes( 8 ) ) );
+        String pagecache = bytesToString( recommendPageCacheMemory( gibiBytes( 8 ) ) );
+
+        command.execute( new String[]{"--memory=8g"} );
+
+        Map<String,String> stringMap = MapUtil.load( new StringReader( output.toString() ) );
+        assertThat( stringMap.get( initialHeapSize.name() ), is( heap ) );
+        assertThat( stringMap.get( maxHeapSize.name() ), is( heap ) );
+        assertThat( stringMap.get( pagecache_memory.name() ), is( pagecache ) );
+    }
+}
