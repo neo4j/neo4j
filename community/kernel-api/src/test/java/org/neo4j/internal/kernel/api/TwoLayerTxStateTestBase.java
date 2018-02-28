371764dbc9676c87d30d7e8d340746f03b522515
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-02-28 01:02:13 +0100

    Forbid to import junit.framework package
    Adapt checkstyle to forbid usage of old junit.framework package.
    Please use more analogs from recent junit's instead.

--- community/kernel-api/src/test/java/org/neo4j/internal/kernel/api/TwoLayerTxStateTestBase.java
+++ community/kernel-api/src/test/java/org/neo4j/internal/kernel/api/TwoLayerTxStateTestBase.java
@@ -32,9 +32,9 @@ import java.util.stream.Collectors;
 
 import org.neo4j.values.storable.Value;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.neo4j.values.storable.Values.booleanValue;
 import static org.neo4j.values.storable.Values.intValue;
 import static org.neo4j.values.storable.Values.stringValue;
