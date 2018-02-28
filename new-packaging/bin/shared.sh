253cde9de64646c873dd37d37bea5ce09e083a97
Jonas Kalderstam <jonas@kalderstam.se>
2018-02-28 12:12:36 +0100

    Backport packaging changes from 3.3

--- new-packaging/bin/shared.sh
+++ new-packaging/bin/shared.sh
@@ -1,24 +1,51 @@
+#!/bin/bash
 # Parse the version from a neo4j tarball like 'neo4j-community-3.2.0-SNAPSHOT-unix.tar.gz'
 # and parses out
 # version: 3.2.0-SNAPSHOT
-# deb_version: 3.2.0.SNAPSHOT
+# deb_version: 3.2.0~SNAPSHOT
 # rpm_version: 3.2.0
 # version_label: SNAPSHOT (or empty if no label present)
-parse_version() {
-  local filename=$(basename ${1})
-  local edition_version_extension=${filename#*-}
-  local version_extension=${edition_version_extension#*-}
+parse_version_from_tarball() {
+  local filename edition_version_extension version_extension
+  filename=$(basename "${1}")
+  edition_version_extension=${filename#*-}
+  version_extension=${edition_version_extension#*-}
+
   version=${version_extension%-*}
-  # Need the 1: prefix to make sure that newer debian packages are in
-  # fact considered newer, to overrule bad version numbers in old
-  # versions.
-  deb_version=1:${version/-/"~"}
+  parse_version "${version}"
+}
+
+parse_version() {
+  version="${1}"
+  # Deb-files do not include the epoch in their filename so provide an
+  # epoch-less variant for Make
+  deb_version=${version/-/"~"}
+  # Need the 1: prefix (epoch) to make sure that newer debian packages
+  # are in fact considered newer, to overrule bad version numbers in
+  # old versions. We also add -1 to get a unique filename because
+  # epochs are not included in deb-filenames.
+  deb_version_full="1:${deb_version}"
   rpm_version=${version%-*}
   version_label=${version#*-}
   # if no label
-  if [[ ${version_label} = ${rpm_version} ]]; then
+  if [[ "${version_label}" = "${rpm_version}" ]]; then
     version_label=""
   fi
+  # https://fedoraproject.org/wiki/Packaging:Versioning
+  if [ -z ${version_label} ]; then
+    # Release version
+    rpm_release="1"
+  else
+    # Pre-release version
+    rpm_release="0.${version_label}.1"
+  fi
+
+  export version
+  export version_label
+  export deb_version
+  export deb_version_full
+  export rpm_version
+  export rpm_release
 }
 
 # Parse the version from a neo4j tarball like 'neo4j-community-3.2.0-SNAPSHOT-unix.tar.gz'
@@ -26,11 +53,12 @@ parse_version() {
 # package_name: neo4j-enterprise (if enterprise)
 parse_pkgname() {
   # Figure if community or enterprise
-  if [[ $(basename ${1}) =~ "enterprise" ]]; then
+  if [[ $(basename "${1}") =~ "enterprise" ]]; then
     package_name="neo4j-enterprise"
   else
     package_name="neo4j"
   fi
+  export package_name
 }
 
 # Parse the license from a neo4j tarball like 'neo4j-community-3.2.0-SNAPSHOT-unix.tar.gz'
@@ -43,4 +71,5 @@ parse_license() {
   else
     license="GPLv3"
   fi
+  export license
 }
