e40e0d42f863a9f80953659b4a343d20d8dd344b
pstutz <philip.stutz@gmail.com>
2018-03-05 17:26:40 +0000

    Fix license

--- community/cypher/frontend-3.4/src/main/scala/org/neo4j/cypher/internal/frontend/v3_4/ast/QualifiedGraphName.scala
+++ community/cypher/frontend-3.4/src/main/scala/org/neo4j/cypher/internal/frontend/v3_4/ast/QualifiedGraphName.scala
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2016-2018 "Neo4j, Inc." [https://neo4j.com]
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
