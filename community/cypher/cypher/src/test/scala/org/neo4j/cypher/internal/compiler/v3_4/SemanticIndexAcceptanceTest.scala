2ac09a311d95db93c243edcfe2740d246facbf9f
fickludd <johan.teleman@neotechnology.com>
2018-03-09 15:03:54 +0100

    Refactor semantic index acceptance test for more extensibility

--- community/cypher/cypher/src/test/scala/org/neo4j/cypher/internal/compiler/v3_4/SemanticIndexAcceptanceTest.scala
+++ community/cypher/cypher/src/test/scala/org/neo4j/cypher/internal/compiler/v3_4/SemanticIndexAcceptanceTest.scala
@@ -20,72 +20,77 @@
 package org.neo4j.cypher.internal.compiler.v3_4
 
 import org.neo4j.cypher.ExecutionEngineFunSuite
+import org.neo4j.values.storable._
 import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 
 class SemanticIndexAcceptanceTest extends ExecutionEngineFunSuite with PropertyChecks {
 
   //the actual test
-  List("<", "<=", "=", ">", ">=").foreach(testOperator)
+  for {
+    bound <- List("<", "<=", "=", ">", ">=")
+    valueGen <- List(
+      ValueSetup[LongValue]("longs", longGen, x => x.minus(1L), x => x.plus(1L)),
+      ValueSetup[DoubleValue]("doubles", doubleGen, x => x.minus(0.1), x => x.plus(0.1)),
+      ValueSetup[TextValue]("strings", textGen, changeLastChar(c => (c - 1).toChar), changeLastChar(c => (c + 1).toChar))
+    )
+  } {
+    testOperator(bound, valueGen)
+  }
+
+  case class ValueSetup[T <: Value](name: String, generator:Gen[T], lower: T => T, upper: T => T)
+
+  def longGen: Gen[LongValue] =
+    for (x <- Gen.chooseNum(Long.MinValue+1, Long.MaxValue-1)) yield Values.longValue(x)
+
+  def doubleGen: Gen[DoubleValue] =
+    for (x <- Gen.chooseNum(Double.MinValue, Double.MaxValue)) yield Values.doubleValue(x)
+
+  def textGen: Gen[TextValue] =
+    for (x <- Gen.alphaStr) yield Values.stringValue(x)
 
   override protected def initTest(): Unit = {
     super.initTest()
     for(_ <- 1 to 1000) createLabeledNode("Label")
   }
 
-  private def changeLastChar(f: Char => Char)(in: String) =
-    if (in.isEmpty) ""
-    else
-      in.substring(0, in.length - 1) + (in.last - 1).toChar
-
-  private def testOperator(operator: String): Unit = {
+  private def testOperator[T <: Value](operator: String, setup: ValueSetup[T]): Unit = {
 
     val queryNotUsingIndex = s"match (n:Label) where n.nonIndexed $operator {prop} return n order by id(n)"
     val queryUsingIndex = s"match (n:Label) where n.indexed $operator {prop} return n order by id(n)"
 
-    def testValue(queryNotUsingIndex: String, queryUsingIndex: String, value: Any): Unit = {
-      val indexedResult = execute(queryUsingIndex, "prop" -> value)
-      execute(queryNotUsingIndex, "prop" -> value).toList should equal(indexedResult.toList)
+    def testValue(queryNotUsingIndex: String, queryUsingIndex: String, value: Value): Unit = {
+      val valueObject = value.asObject()
+      val indexedResult = execute(queryUsingIndex, "prop" -> valueObject)
+      execute(queryNotUsingIndex, "prop" -> valueObject).toList should equal(indexedResult.toList)
       indexedResult.executionPlanDescription().toString should include("NodeIndexSeek")
     }
 
-    def tester[T](propertyValue: T, prev: T => T, next: T => T): Unit = {
-      graph.inTx {
-        createLabeledNode(Map("nonIndexed" -> propertyValue, "indexed" -> propertyValue), "Label")
+    test(s"testing ${setup.name} with $operator") {
+      graph.createIndex("Label", "indexed")
+      forAll(setup.generator) { propertyValue: T =>
+        graph.inTx {
+          createLabeledNode(Map("nonIndexed" -> propertyValue.asObject(), "indexed" -> propertyValue.asObject()), "Label")
 
-        withClue("with TxState") {
+          withClue("with TxState") {
+            testValue(queryNotUsingIndex, queryUsingIndex, propertyValue)
+            testValue(queryNotUsingIndex, queryUsingIndex, setup.lower(propertyValue))
+            testValue(queryNotUsingIndex, queryUsingIndex, setup.upper(propertyValue))
+          }
+        }
+        withClue("without TxState") {
           testValue(queryNotUsingIndex, queryUsingIndex, propertyValue)
-          testValue(queryNotUsingIndex, queryUsingIndex, prev(propertyValue))
-          testValue(queryNotUsingIndex, queryUsingIndex, next(propertyValue))
+          testValue(queryNotUsingIndex, queryUsingIndex, setup.lower(propertyValue))
+          testValue(queryNotUsingIndex, queryUsingIndex, setup.upper(propertyValue))
         }
       }
-      withClue("without TxState") {
-        testValue(queryNotUsingIndex, queryUsingIndex, propertyValue)
-        testValue(queryNotUsingIndex, queryUsingIndex, prev(propertyValue))
-        testValue(queryNotUsingIndex, queryUsingIndex, next(propertyValue))
-      }
     }
+  }
 
-    test(s"testing long with $operator") {
-      graph.createIndex("Label", "indexed")
-      forAll { propertyValue: Long =>
-        tester(propertyValue, (l: Long) => l - 1L, (l: Long) => l + 1)
-      }
-    }
-
-    test(s"testing double with $operator") {
-      graph.createIndex("Label", "indexed")
-      forAll { propertyValue: Double =>
-        tester(propertyValue, (d: Double) => d - 0.5, (d: Double) => d + 0.5)
-      }
-    }
-
-    test(s"testing string with $operator") {
-      graph.createIndex("Label", "indexed")
-      forAll (Gen.alphaStr){ propertyValue: String =>
-        tester(propertyValue, changeLastChar(c => (c - 1).toChar), changeLastChar(c => (c + 1).toChar))
-      }
-    }
-
+  private def changeLastChar(f: Char => Char)(in: TextValue): TextValue = {
+    val str = in.stringValue()
+    if (str.isEmpty) in
+    else
+      Values.stringValue(str.substring(0, in.length - 1) + (str.last - 1).toChar)
   }
 }
