e7f3148d9639e80c1a6396b9d832d4b6ac04fbeb
Sascha Peukert <sascha.peukert@neotechnology.com>
2018-03-28 10:49:44 +0200

    Moved test class to actual project and removed duplicate test class

--- enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ReflectiveProcedureCallAcceptanceTest.scala
+++ enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/ReflectiveProcedureCallAcceptanceTest.scala
@@ -5,17 +5,17 @@
  * This file is part of Neo4j.
  *
  * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.neo4j.internal.cypher.acceptance
 
@@ -23,7 +23,7 @@ import java.io.PrintWriter
 
 import org.apache.commons.lang3.exception.ExceptionUtils
 import org.neo4j.cypher._
-import org.neo4j.cypher.internal.compiler.v3_1.test_helpers.CreateTempFileTestSupport
+import org.neo4j.cypher.internal.compiler.v3_2.test_helpers.CreateTempFileTestSupport
 import org.neo4j.graphdb.{QueryExecutionException, TransactionFailureException}
 import org.neo4j.internal.cypher.acceptance.TestResourceProcedure.{SimulateFailureException, SimulateFailureOnCloseException}
 import org.neo4j.kernel.api.KernelTransaction
@@ -53,7 +53,7 @@ class ReflectiveProcedureCallAcceptanceTest extends ExecutionEngineFunSuite with
   private def setUpProcedures(): TestResourceProcedure.Counters = {
     val procedures = graph.getDependencyResolver.resolveDependency(classOf[Procedures])
     val counters = new TestResourceProcedure.Counters
-    procedures.registerComponent(classOf[TestResourceProcedure.Counters], TestResourceProcedure.countersProvider(counters))
+    procedures.registerComponent(classOf[TestResourceProcedure.Counters], TestResourceProcedure.countersProvider(counters), true)
 
     procedures.registerProcedure(classOf[TestResourceProcedure])
     procedures.registerFunction(classOf[TestResourceProcedure])
