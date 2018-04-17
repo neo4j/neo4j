e1eddb5cbc1a03abd2c106d7e767d1b532bd86d7
Pontus Melke <pontusmelke@gmail.com>
2018-04-17 08:56:38 +0200

    Fix infinite loop

--- community/kernel-api/src/main/java/org/neo4j/internal/kernel/api/helpers/RelationshipDenseSelectionIterator.java
+++ community/kernel-api/src/main/java/org/neo4j/internal/kernel/api/helpers/RelationshipDenseSelectionIterator.java
@@ -43,7 +43,7 @@ public final class RelationshipDenseSelectionIterator<R> extends RelationshipDen
     {
         if ( next == RelationshipSelections.UNINITIALIZED )
         {
-            this.next();
+            fetchNext();
             next = relationshipCursor.relationshipReference();
         }
 
