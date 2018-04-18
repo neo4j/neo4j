aeee67c22c447454dfd781ba620e4cf753e8e2e3
Satia Herfert <satiaherfert@gmx.de>
2018-04-18 11:09:30 +0200

    Fix bug when sending null to STARTS WITH on 2.3

--- community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/spi/v3_1/TransactionBoundQueryContext.scala
+++ community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/spi/v3_1/TransactionBoundQueryContext.scala
@@ -147,6 +147,8 @@ final class TransactionBoundQueryContext(txContext: TransactionalContextWrapper)
 
     case PrefixRange(prefix: String) =>
       indexSeekByPrefixRange(index, prefix)
+    case PrefixRange(null) =>
+      Iterator.empty
     case range: InequalitySeekRange[Any] =>
       indexSeekByPrefixRange(index, range)
 
