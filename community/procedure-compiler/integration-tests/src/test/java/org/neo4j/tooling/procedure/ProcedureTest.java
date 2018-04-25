f1e732f9e9d1755e6226900a4a2146375e223f68
Pontus Melke <pontusmelke@gmail.com>
2018-04-25 16:21:48 +0200

    Unflake test

--- community/procedure-compiler/integration-tests/src/test/java/org/neo4j/tooling/procedure/ProcedureTest.java
+++ community/procedure-compiler/integration-tests/src/test/java/org/neo4j/tooling/procedure/ProcedureTest.java
@@ -22,6 +22,8 @@ package org.neo4j.tooling.procedure;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import org.neo4j.driver.v1.Config;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
@@ -46,6 +48,7 @@ public class ProcedureTest
     @Rule
     public Neo4jRule graphDb = new Neo4jRule()
             .withConfig( ServerSettings.script_enabled, Settings.TRUE )
+            .dumpLogsOnFailure( System.out )
             .withProcedure( PROCEDURES_CLASS );
     private String procedureNamespace = PROCEDURES_CLASS.getPackage().getName();
 
@@ -120,7 +123,9 @@ public class ProcedureTest
 
     private Config configuration()
     {
-        return Config.build().withEncryptionLevel( Config.EncryptionLevel.NONE ).toConfig();
+        return Config.build().withEncryptionLevel( Config.EncryptionLevel.NONE )
+                .withConnectionTimeout( 10, TimeUnit.SECONDS )
+                .toConfig();
     }
 
 }
