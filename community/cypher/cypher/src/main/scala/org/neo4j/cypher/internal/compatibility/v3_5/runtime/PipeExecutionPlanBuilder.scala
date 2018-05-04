fickludd/neo4j-legacy

Clean up Fingerprint and Monitors around PipeExecutionPlanBuilder

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/PipeExecutionPlanBuilder.scala
@@ -19,38 +19,25 @@
  */
 package org.neo4j.cypher.internal.compatibility.v3_4.runtime
 
-import java.time.Clock
-
-import org.neo4j.cypher.internal.util.v3_4.InternalException
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime.executionplan._
-import org.neo4j.cypher.internal.frontend.v3_4.phases.Monitors
 import org.neo4j.cypher.internal.ir.v3_4.PeriodicCommit
-import org.neo4j.cypher.internal.planner.v3_4.spi.{InstrumentedGraphStatistics, PlanContext}
+import org.neo4j.cypher.internal.planner.v3_4.spi.PlanContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.Pipe
+import org.neo4j.cypher.internal.util.v3_4.InternalException
 import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
 
 import scala.collection.mutable
 
-class PipeExecutionPlanBuilder(clock: Clock,
-                               monitors: Monitors,
-                               pipeBuilderFactory: PipeBuilderFactory,
+class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
                                expressionConverters: ExpressionConverters) {
   def build(periodicCommit: Option[PeriodicCommit], plan: LogicalPlan)
            (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): PipeInfo = {
 
     val topLevelPipe = buildPipe(plan)
 
-    val fingerprint = planContext.statistics match {
-      case igs: InstrumentedGraphStatistics =>
-        Some(PlanFingerprint(clock.millis(), planContext.txIdProvider(), igs.snapshot.freeze))
-      case _ =>
-        None
-    }
-
     val periodicCommitInfo = periodicCommit.map(x => PeriodicCommitInfo(x.batchSize))
-    PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id),
-             periodicCommitInfo, fingerprint, context.plannerName)
+    PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id), periodicCommitInfo, context.plannerName)
   }
 
   /*
@@ -73,11 +60,9 @@ class PipeExecutionPlanBuilder(clock: Clock,
    build the pipe for 'a'. Thanks for reading this far - I didn't think we would make it!
    */
   private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Pipe = {
-    val pipeBuilder = pipeBuilderFactory(
-      monitors = monitors,
-      recurse = p => buildPipe(p),
-      readOnly = context.readOnlies.get(plan.id),
-      expressionConverters = expressionConverters)
+    val pipeBuilder = pipeBuilderFactory(recurse = p => buildPipe(p),
+                                         readOnly = context.readOnlies.get(plan.id),
+                                         expressionConverters = expressionConverters)
 
     val planStack = new mutable.Stack[LogicalPlan]()
     val pipeStack = new mutable.Stack[Pipe]()
@@ -146,11 +131,11 @@ class PipeExecutionPlanBuilder(clock: Clock,
 }
 
 object CommunityPipeBuilderFactory extends PipeBuilderFactory {
-  def apply(monitors: Monitors, recurse: LogicalPlan => Pipe,
+  def apply(recurse: LogicalPlan => Pipe,
             readOnly: Boolean,
             expressionConverters: ExpressionConverters)
            (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): CommunityPipeBuilder = {
-    CommunityPipeBuilder(monitors, recurse, readOnly, expressionConverters, recursePipes(recurse, planContext))
+    CommunityPipeBuilder(recurse, readOnly, expressionConverters, recursePipes(recurse, planContext))
   }
 
 }

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/executionplan/PlanFingerprint.scala
@@ -21,8 +21,8 @@ package org.neo4j.cypher.internal.compatibility.v3_4.runtime.executionplan
 
 import java.time.Clock
 
-import org.neo4j.cypher.internal.compiler.v3_4.{CacheCheckResult, NeedsReplan, FineToReuse, StatsDivergenceCalculator}
-import org.neo4j.cypher.internal.planner.v3_4.spi.{GraphStatistics, GraphStatisticsSnapshot}
+import org.neo4j.cypher.internal.compiler.v3_4.{CacheCheckResult, FineToReuse, NeedsReplan, StatsDivergenceCalculator}
+import org.neo4j.cypher.internal.planner.v3_4.spi.{GraphStatistics, GraphStatisticsSnapshot, InstrumentedGraphStatistics}
 
 case class PlanFingerprint(creationTimeMillis: Long, lastCheckTimeMillis: Long, txId: Long, snapshot: GraphStatisticsSnapshot) {
   if (snapshot.statsValues.isEmpty) {
@@ -62,4 +62,12 @@ class PlanFingerprintReference(clock: Clock, divergence: StatsDivergenceCalculat
 object PlanFingerprint {
   def apply(creationTimeMillis: Long, txId: Long, snapshot: GraphStatisticsSnapshot): PlanFingerprint =
     PlanFingerprint(creationTimeMillis, creationTimeMillis, txId, snapshot)
+
+  def take(clock: Clock, txIdProvider: () => Long, graphStatistics: GraphStatistics): Option[PlanFingerprint] =
+    graphStatistics match {
+      case igs: InstrumentedGraphStatistics =>
+        Some(PlanFingerprint(clock.millis(), txIdProvider(), igs.snapshot.freeze))
+      case _ =>
+        None
+    }
 }

neo4j/neo4j

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/PipeExecutionPlanBuilder.scala
@@ -19,38 +19,25 @@
  */
 package org.neo4j.cypher.internal.compatibility.v3_5.runtime
 
-import java.time.Clock
-
-import org.neo4j.cypher.internal.util.v3_5.InternalException
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime.executionplan._
-import org.neo4j.cypher.internal.frontend.v3_5.phases.Monitors
 import org.neo4j.cypher.internal.ir.v3_5.PeriodicCommit
-import org.neo4j.cypher.internal.planner.v3_5.spi.{InstrumentedGraphStatistics, PlanContext}
+import org.neo4j.cypher.internal.planner.v3_5.spi.PlanContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.Pipe
+import org.neo4j.cypher.internal.util.v3_5.InternalException
 import org.neo4j.cypher.internal.v3_5.logical.plans.{LogicalPlan, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
 
 import scala.collection.mutable
 
-class PipeExecutionPlanBuilder(clock: Clock,
-                               monitors: Monitors,
-                               pipeBuilderFactory: PipeBuilderFactory,
+class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
                                expressionConverters: ExpressionConverters) {
   def build(periodicCommit: Option[PeriodicCommit], plan: LogicalPlan)
            (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): PipeInfo = {
 
     val topLevelPipe = buildPipe(plan)
 
-    val fingerprint = planContext.statistics match {
-      case igs: InstrumentedGraphStatistics =>
-        Some(PlanFingerprint(clock.millis(), planContext.txIdProvider(), igs.snapshot.freeze))
-      case _ =>
-        None
-    }
-
     val periodicCommitInfo = periodicCommit.map(x => PeriodicCommitInfo(x.batchSize))
-    PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id),
-             periodicCommitInfo, fingerprint, context.plannerName)
+    PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id), periodicCommitInfo, context.plannerName)
   }
 
   /*
@@ -73,11 +60,9 @@ class PipeExecutionPlanBuilder(clock: Clock,
    build the pipe for 'a'. Thanks for reading this far - I didn't think we would make it!
    */
   private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Pipe = {
-    val pipeBuilder = pipeBuilderFactory(
-      monitors = monitors,
-      recurse = p => buildPipe(p),
-      readOnly = context.readOnlies.get(plan.id),
-      expressionConverters = expressionConverters)
+    val pipeBuilder = pipeBuilderFactory(recurse = p => buildPipe(p),
+                                         readOnly = context.readOnlies.get(plan.id),
+                                         expressionConverters = expressionConverters)
 
     val planStack = new mutable.Stack[LogicalPlan]()
     val pipeStack = new mutable.Stack[Pipe]()
@@ -146,11 +131,11 @@ class PipeExecutionPlanBuilder(clock: Clock,
 }
 
 object CommunityPipeBuilderFactory extends PipeBuilderFactory {
-  def apply(monitors: Monitors, recurse: LogicalPlan => Pipe,
+  def apply(recurse: LogicalPlan => Pipe,
             readOnly: Boolean,
             expressionConverters: ExpressionConverters)
            (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): CommunityPipeBuilder = {
-    CommunityPipeBuilder(monitors, recurse, readOnly, expressionConverters, recursePipes(recurse, planContext))
+    CommunityPipeBuilder(recurse, readOnly, expressionConverters, recursePipes(recurse, planContext))
   }
 
 }

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/executionplan/PlanFingerprint.scala
@@ -21,8 +21,8 @@ package org.neo4j.cypher.internal.compatibility.v3_5.runtime.executionplan
 
 import java.time.Clock
 
-import org.neo4j.cypher.internal.compiler.v3_5.{CacheCheckResult, NeedsReplan, FineToReuse, StatsDivergenceCalculator}
-import org.neo4j.cypher.internal.planner.v3_5.spi.{GraphStatistics, GraphStatisticsSnapshot}
+import org.neo4j.cypher.internal.compiler.v3_5.{CacheCheckResult, FineToReuse, NeedsReplan, StatsDivergenceCalculator}
+import org.neo4j.cypher.internal.planner.v3_5.spi.{GraphStatistics, GraphStatisticsSnapshot, InstrumentedGraphStatistics}
 
 case class PlanFingerprint(creationTimeMillis: Long, lastCheckTimeMillis: Long, txId: Long, snapshot: GraphStatisticsSnapshot) {
   if (snapshot.statsValues.isEmpty) {
@@ -62,4 +62,12 @@ class PlanFingerprintReference(clock: Clock, divergence: StatsDivergenceCalculat
 object PlanFingerprint {
   def apply(creationTimeMillis: Long, txId: Long, snapshot: GraphStatisticsSnapshot): PlanFingerprint =
     PlanFingerprint(creationTimeMillis, creationTimeMillis, txId, snapshot)
+
+  def take(clock: Clock, txIdProvider: () => Long, graphStatistics: GraphStatistics): Option[PlanFingerprint] =
+    graphStatistics match {
+      case igs: InstrumentedGraphStatistics =>
+        Some(PlanFingerprint(clock.millis(), txIdProvider(), igs.snapshot.freeze))
+      case _ =>
+        None
+    }
 }