fickludd/neo4j-legacy

Remove PipeInfo

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/PipeExecutionPlanBuilder.scala
@@ -19,23 +19,18 @@
  */
 package org.neo4j.cypher.internal.compatibility.v3_4.runtime
 
-import org.neo4j.cypher.internal.compatibility.v3_4.runtime.executionplan._
-import org.neo4j.cypher.internal.ir.v3_4.PeriodicCommit
 import org.neo4j.cypher.internal.planner.v3_4.spi.TokenContext
 import org.neo4j.cypher.internal.runtime.interpreted.InterpretedPipeBuilder
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.{Pipe, PipeBuilderFactory, PipeExecutionBuilderContext}
-import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan, LogicalPlans, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
+import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan, LogicalPlans}
 
 class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
                                expressionConverters: ExpressionConverters) {
-  def build(periodicCommit: Option[PeriodicCommit], plan: LogicalPlan)
-           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): PipeInfo = {
+  def build(plan: LogicalPlan)
+           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): Pipe = {
 
-    val topLevelPipe = buildPipe(plan)
-
-    val periodicCommitInfo = periodicCommit.map(x => PeriodicCommitInfo(x.batchSize))
-    PipeInfo(topLevelPipe, periodicCommitInfo)
+    buildPipe(plan)
   }
 
   private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): Pipe = {

neo4j/neo4j

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/PipeExecutionPlanBuilder.scala
@@ -19,23 +19,18 @@
  */
 package org.neo4j.cypher.internal.compatibility.v3_5.runtime
 
-import org.neo4j.cypher.internal.compatibility.v3_5.runtime.executionplan._
-import org.neo4j.cypher.internal.ir.v3_5.PeriodicCommit
 import org.neo4j.cypher.internal.planner.v3_5.spi.TokenContext
 import org.neo4j.cypher.internal.runtime.interpreted.InterpretedPipeBuilder
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.{Pipe, PipeBuilderFactory, PipeExecutionBuilderContext}
-import org.neo4j.cypher.internal.v3_5.logical.plans.{LogicalPlan, LogicalPlans, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
+import org.neo4j.cypher.internal.v3_5.logical.plans.{LogicalPlan, LogicalPlans}
 
 class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
                                expressionConverters: ExpressionConverters) {
-  def build(periodicCommit: Option[PeriodicCommit], plan: LogicalPlan)
-           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): PipeInfo = {
+  def build(plan: LogicalPlan)
+           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): Pipe = {
 
-    val topLevelPipe = buildPipe(plan)
-
-    val periodicCommitInfo = periodicCommit.map(x => PeriodicCommitInfo(x.batchSize))
-    PipeInfo(topLevelPipe, periodicCommitInfo)
+    buildPipe(plan)
   }
 
   private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): Pipe = {