fickludd/neo4j-legacy

Move PlannerName out of PipeInfo, and clean PipeExecutionBuilderContext

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/CommunityPipeBuilder.scala
@@ -21,10 +21,9 @@ package org.neo4j.cypher.internal.compatibility.v3_4.runtime
 
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime.executionplan.builders.prepare.KeyTokenResolver
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime.pipes.DropResultPipe
-import org.neo4j.cypher.internal.frontend.v3_4.phases.Monitors
 import org.neo4j.cypher.internal.frontend.v3_4.semantics.SemanticTable
 import org.neo4j.cypher.internal.ir.v3_4.VarPatternLength
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanContext
+import org.neo4j.cypher.internal.planner.v3_4.spi.TokenContext
 import org.neo4j.cypher.internal.runtime.ProcedureCallMode
 import org.neo4j.cypher.internal.runtime.interpreted.ExecutionContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
@@ -43,16 +42,18 @@ import org.neo4j.values.virtual.{NodeValue, RelationshipValue}
  * Responsible for turning a logical plan with argument pipes into a new pipe.
  * When adding new Pipes and LogicalPlans, this is where you should be looking.
  */
-case class CommunityPipeBuilder(recurse: LogicalPlan => Pipe, readOnly: Boolean,
+case class CommunityPipeBuilder(recurse: LogicalPlan => Pipe,
+                                readOnly: Boolean,
                                 expressionConverters: ExpressionConverters,
-                                rewriteAstExpression: (ASTExpression) => ASTExpression)
-                               (implicit context: PipeExecutionBuilderContext, planContext: PlanContext) extends PipeBuilder {
+                                rewriteAstExpression: (ASTExpression) => ASTExpression,
+                                tokenContext: TokenContext)
+                               (implicit context: PipeExecutionBuilderContext) extends PipeBuilder {
 
 
   private val buildExpression =
     rewriteAstExpression andThen
       expressionConverters.toCommandExpression andThen
-      (expression => expression.rewrite(KeyTokenResolver.resolveExpressions(_, planContext)))
+      (expression => expression.rewrite(KeyTokenResolver.resolveExpressions(_, tokenContext)))
 
 
   def onLeaf(plan: LogicalPlan): Pipe = {
@@ -423,10 +424,10 @@ case class CommunityPipeBuilder(recurse: LogicalPlan => Pipe, readOnly: Boolean,
 
   implicit val table: SemanticTable = context.semanticTable
 
-  private def buildPredicate(expr: ASTExpression)(implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Predicate = {
+  private def buildPredicate(expr: ASTExpression)(implicit context: PipeExecutionBuilderContext): Predicate = {
     val rewrittenExpr: ASTExpression = rewriteAstExpression(expr)
 
-    expressionConverters.toCommandPredicate(rewrittenExpr).rewrite(KeyTokenResolver.resolveExpressions(_, planContext)).asInstanceOf[Predicate]
+    expressionConverters.toCommandPredicate(rewrittenExpr).rewrite(KeyTokenResolver.resolveExpressions(_, tokenContext)).asInstanceOf[Predicate]
   }
 
   private def translateColumnOrder(s: ColumnOrder): org.neo4j.cypher.internal.runtime.interpreted.pipes.ColumnOrder = s match {

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/PipeBuilderFactory.scala
@@ -19,8 +19,8 @@
  */
 package org.neo4j.cypher.internal.compatibility.v3_4.runtime
 
+import org.neo4j.cypher.internal.planner.v3_4.spi.TokenContext
 import org.neo4j.cypher.internal.util.v3_4.{Rewriter, bottomUp}
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.{NestedPipeExpression, Pipe}
 import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan, NestedPlanExpression}
@@ -31,9 +31,9 @@ trait PipeBuilderFactory {
   def apply(recurse: LogicalPlan => Pipe,
             readOnly: Boolean,
             expressionConverters: ExpressionConverters)
-           (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): PipeBuilder
+           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): PipeBuilder
 
-  protected def recursePipes(recurse: LogicalPlan => Pipe, planContext: PlanContext)
+  protected def recursePipes(recurse: LogicalPlan => Pipe)
                             (in: frontEndAst.Expression): frontEndAst.Expression = {
 
     val buildPipeExpressions = new Rewriter {

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/PipeExecutionPlanBuilder.scala
@@ -21,7 +21,7 @@ package org.neo4j.cypher.internal.compatibility.v3_4.runtime
 
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime.executionplan._
 import org.neo4j.cypher.internal.ir.v3_4.PeriodicCommit
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanContext
+import org.neo4j.cypher.internal.planner.v3_4.spi.TokenContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.Pipe
 import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan, LogicalPlans, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
@@ -29,15 +29,15 @@ import org.neo4j.cypher.internal.v3_4.logical.plans.{LogicalPlan, LogicalPlans, 
 class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
                                expressionConverters: ExpressionConverters) {
   def build(periodicCommit: Option[PeriodicCommit], plan: LogicalPlan)
-           (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): PipeInfo = {
+           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): PipeInfo = {
 
     val topLevelPipe = buildPipe(plan)
 
     val periodicCommitInfo = periodicCommit.map(x => PeriodicCommitInfo(x.batchSize))
-    PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id), periodicCommitInfo, context.plannerName)
+    PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id), periodicCommitInfo)
   }
 
-  private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Pipe = {
+  private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): Pipe = {
     val pipeBuilder = pipeBuilderFactory(recurse = p => buildPipe(p),
                                          readOnly = context.readOnlies.get(plan.id),
                                          expressionConverters = expressionConverters)
@@ -49,8 +49,8 @@ object CommunityPipeBuilderFactory extends PipeBuilderFactory {
   def apply(recurse: LogicalPlan => Pipe,
             readOnly: Boolean,
             expressionConverters: ExpressionConverters)
-           (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): CommunityPipeBuilder = {
-    CommunityPipeBuilder(recurse, readOnly, expressionConverters, recursePipes(recurse, planContext))
+           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): CommunityPipeBuilder = {
+    CommunityPipeBuilder(recurse, readOnly, expressionConverters, recursePipes(recurse), tokenContext)
   }
 }
 

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/executionplan/DefaultExecutionResultBuilderFactory.scala
@@ -21,6 +21,7 @@ package org.neo4j.cypher.internal.compatibility.v3_4.runtime.executionplan
 
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime._
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime.helpers.InternalWrapping._
+import org.neo4j.cypher.internal.frontend.v3_4.PlannerName
 import org.neo4j.cypher.internal.frontend.v3_4.phases.InternalNotificationLogger
 import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
 import org.neo4j.cypher.internal.runtime.interpreted.pipes._
@@ -67,6 +68,7 @@ abstract class BaseExecutionResultBuilderFactory(pipeInfo: PipeInfo,
     override def build(planType: ExecutionMode,
                        params: MapValue,
                        notificationLogger: InternalNotificationLogger,
+                       plannerName: PlannerName,
                        runtimeName: RuntimeName,
                        readOnlies: ReadOnlies,
                        cardinalities: Cardinalities): InternalExecutionResult = {
@@ -74,7 +76,7 @@ abstract class BaseExecutionResultBuilderFactory(pipeInfo: PipeInfo,
       taskCloser.addTask(queryContext.resources.close)
       val state = createQueryState(params)
       try {
-        createResults(state, planType, notificationLogger, runtimeName, readOnlies, cardinalities)
+        createResults(state, planType, notificationLogger, plannerName, runtimeName, readOnlies, cardinalities)
       }
       catch {
         case e: CypherException =>
@@ -88,12 +90,13 @@ abstract class BaseExecutionResultBuilderFactory(pipeInfo: PipeInfo,
 
     private def createResults(state: QueryState, planType: ExecutionMode,
                               notificationLogger: InternalNotificationLogger,
+                              plannerName: PlannerName,
                               runtimeName: RuntimeName,
                               readOnlies: ReadOnlies,
                               cardinalities: Cardinalities): InternalExecutionResult = {
       val queryType: InternalQueryType = getQueryType
       val planDescription =
-        () => LogicalPlan2PlanDescription(logicalPlan, pipeInfo.plannerUsed, readOnlies, cardinalities)
+        () => LogicalPlan2PlanDescription(logicalPlan, plannerName, readOnlies, cardinalities)
           .addArgument(Runtime(runtimeName.toTextOutput))
           .addArgument(RuntimeImpl(runtimeName.name))
       if (planType == ExplainMode) {

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_4/runtime/executionplan/ExecutionResultBuilder.scala
@@ -21,6 +21,7 @@ package org.neo4j.cypher.internal.compatibility.v3_4.runtime.executionplan
 
 import org.neo4j.cypher.internal.util.v3_4.CypherException
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime.RuntimeName
+import org.neo4j.cypher.internal.frontend.v3_4.PlannerName
 import org.neo4j.cypher.internal.frontend.v3_4.phases.InternalNotificationLogger
 import org.neo4j.cypher.internal.planner.v3_4.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.PipeDecorator
@@ -35,6 +36,7 @@ trait ExecutionResultBuilder {
   def build(planType: ExecutionMode,
             params: MapValue,
             notificationLogger: InternalNotificationLogger,
+            plannerName: PlannerName,
             runtimeName: RuntimeName,
             readOnlies: ReadOnlies,
             cardinalities: Cardinalities): InternalExecutionResult

enterprise/cypher/slotted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/slotted/SlottedPipeBuilder.scala
@@ -23,10 +23,9 @@ import org.neo4j.cypher.internal.compatibility.v3_4.runtime.SlotAllocation.Physi
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime._
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime.executionplan.builders.prepare.KeyTokenResolver
 import org.neo4j.cypher.internal.compatibility.v3_4.runtime.pipes.DropResultPipe
-import org.neo4j.cypher.internal.frontend.v3_4.phases.Monitors
 import org.neo4j.cypher.internal.frontend.v3_4.semantics.SemanticTable
 import org.neo4j.cypher.internal.ir.v3_4.VarPatternLength
-import org.neo4j.cypher.internal.planner.v3_4.spi.PlanContext
+import org.neo4j.cypher.internal.planner.v3_4.spi.TokenContext
 import org.neo4j.cypher.internal.runtime.interpreted.ExecutionContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.{AggregationExpression, Expression}
@@ -49,7 +48,7 @@ class SlottedPipeBuilder(fallback: PipeBuilder,
                          physicalPlan: PhysicalPlan,
                          readOnly: Boolean,
                          rewriteAstExpression: (frontEndAst.Expression) => frontEndAst.Expression)
-                        (implicit context: PipeExecutionBuilderContext, planContext: PlanContext)
+                        (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext)
   extends PipeBuilder {
 
   private val convertExpressions: (frontEndAst.Expression) => commandExpressions.Expression =
@@ -336,10 +335,12 @@ class SlottedPipeBuilder(fallback: PipeBuilder,
   }
 
   private def buildPredicate(expr: frontEndAst.Expression)
-                            (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Predicate = {
+                            (implicit context: PipeExecutionBuilderContext): Predicate = {
     val rewrittenExpr: frontEndAst.Expression = rewriteAstExpression(expr)
 
-    expressionConverters.toCommandPredicate(rewrittenExpr).rewrite(KeyTokenResolver.resolveExpressions(_, planContext))
+    expressionConverters
+      .toCommandPredicate(rewrittenExpr)
+      .rewrite(KeyTokenResolver.resolveExpressions(_, tokenContext))
       .asInstanceOf[Predicate]
   }
 
@@ -522,13 +523,13 @@ object SlottedPipeBuilder {
 
   case class Factory(physicalPlan: PhysicalPlan)
     extends PipeBuilderFactory {
-    def apply(recurse: LogicalPlan => Pipe, readOnly: Boolean,
+    override def apply(recurse: LogicalPlan => Pipe, readOnly: Boolean,
               expressionConverters: ExpressionConverters)
-             (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): PipeBuilder = {
+             (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): PipeBuilder = {
 
-      val expressionToExpression = recursePipes(recurse, planContext) _
+      val expressionToExpression = recursePipes(recurse) _
 
-      val fallback = CommunityPipeBuilder(recurse, readOnly, expressionConverters, expressionToExpression)
+      val fallback = CommunityPipeBuilder(recurse, readOnly, expressionConverters, expressionToExpression, tokenContext)
 
       new SlottedPipeBuilder(fallback, expressionConverters, physicalPlan, readOnly, expressionToExpression)
     }

neo4j/neo4j

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/CommunityPipeBuilder.scala
@@ -21,10 +21,9 @@ package org.neo4j.cypher.internal.compatibility.v3_5.runtime
 
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime.executionplan.builders.prepare.KeyTokenResolver
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime.pipes.DropResultPipe
-import org.neo4j.cypher.internal.frontend.v3_5.phases.Monitors
 import org.neo4j.cypher.internal.frontend.v3_5.semantics.SemanticTable
 import org.neo4j.cypher.internal.ir.v3_5.VarPatternLength
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanContext
+import org.neo4j.cypher.internal.planner.v3_5.spi.TokenContext
 import org.neo4j.cypher.internal.runtime.ProcedureCallMode
 import org.neo4j.cypher.internal.runtime.interpreted.ExecutionContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
@@ -43,16 +42,18 @@ import org.neo4j.values.virtual.{NodeValue, RelationshipValue}
  * Responsible for turning a logical plan with argument pipes into a new pipe.
  * When adding new Pipes and LogicalPlans, this is where you should be looking.
  */
-case class CommunityPipeBuilder(recurse: LogicalPlan => Pipe, readOnly: Boolean,
+case class CommunityPipeBuilder(recurse: LogicalPlan => Pipe,
+                                readOnly: Boolean,
                                 expressionConverters: ExpressionConverters,
-                                rewriteAstExpression: (ASTExpression) => ASTExpression)
-                               (implicit context: PipeExecutionBuilderContext, planContext: PlanContext) extends PipeBuilder {
+                                rewriteAstExpression: (ASTExpression) => ASTExpression,
+                                tokenContext: TokenContext)
+                               (implicit context: PipeExecutionBuilderContext) extends PipeBuilder {
 
 
   private val buildExpression =
     rewriteAstExpression andThen
       expressionConverters.toCommandExpression andThen
-      (expression => expression.rewrite(KeyTokenResolver.resolveExpressions(_, planContext)))
+      (expression => expression.rewrite(KeyTokenResolver.resolveExpressions(_, tokenContext)))
 
 
   def onLeaf(plan: LogicalPlan): Pipe = {
@@ -423,10 +424,10 @@ case class CommunityPipeBuilder(recurse: LogicalPlan => Pipe, readOnly: Boolean,
 
   implicit val table: SemanticTable = context.semanticTable
 
-  private def buildPredicate(expr: ASTExpression)(implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Predicate = {
+  private def buildPredicate(expr: ASTExpression)(implicit context: PipeExecutionBuilderContext): Predicate = {
     val rewrittenExpr: ASTExpression = rewriteAstExpression(expr)
 
-    expressionConverters.toCommandPredicate(rewrittenExpr).rewrite(KeyTokenResolver.resolveExpressions(_, planContext)).asInstanceOf[Predicate]
+    expressionConverters.toCommandPredicate(rewrittenExpr).rewrite(KeyTokenResolver.resolveExpressions(_, tokenContext)).asInstanceOf[Predicate]
   }
 
   private def translateColumnOrder(s: ColumnOrder): org.neo4j.cypher.internal.runtime.interpreted.pipes.ColumnOrder = s match {

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/PipeBuilderFactory.scala
@@ -19,8 +19,8 @@
  */
 package org.neo4j.cypher.internal.compatibility.v3_5.runtime
 
+import org.neo4j.cypher.internal.planner.v3_5.spi.TokenContext
 import org.neo4j.cypher.internal.util.v3_5.{Rewriter, bottomUp}
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.{NestedPipeExpression, Pipe}
 import org.neo4j.cypher.internal.v3_5.logical.plans.{LogicalPlan, NestedPlanExpression}
@@ -31,9 +31,9 @@ trait PipeBuilderFactory {
   def apply(recurse: LogicalPlan => Pipe,
             readOnly: Boolean,
             expressionConverters: ExpressionConverters)
-           (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): PipeBuilder
+           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): PipeBuilder
 
-  protected def recursePipes(recurse: LogicalPlan => Pipe, planContext: PlanContext)
+  protected def recursePipes(recurse: LogicalPlan => Pipe)
                             (in: frontEndAst.Expression): frontEndAst.Expression = {
 
     val buildPipeExpressions = new Rewriter {

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/PipeExecutionPlanBuilder.scala
@@ -21,7 +21,7 @@ package org.neo4j.cypher.internal.compatibility.v3_5.runtime
 
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime.executionplan._
 import org.neo4j.cypher.internal.ir.v3_5.PeriodicCommit
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanContext
+import org.neo4j.cypher.internal.planner.v3_5.spi.TokenContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.Pipe
 import org.neo4j.cypher.internal.v3_5.logical.plans.{LogicalPlan, LogicalPlans, Limit => LimitPlan, LoadCSV => LoadCSVPlan, Skip => SkipPlan}
@@ -29,15 +29,15 @@ import org.neo4j.cypher.internal.v3_5.logical.plans.{LogicalPlan, LogicalPlans, 
 class PipeExecutionPlanBuilder(pipeBuilderFactory: PipeBuilderFactory,
                                expressionConverters: ExpressionConverters) {
   def build(periodicCommit: Option[PeriodicCommit], plan: LogicalPlan)
-           (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): PipeInfo = {
+           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): PipeInfo = {
 
     val topLevelPipe = buildPipe(plan)
 
     val periodicCommitInfo = periodicCommit.map(x => PeriodicCommitInfo(x.batchSize))
-    PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id), periodicCommitInfo, context.plannerName)
+    PipeInfo(topLevelPipe, !context.readOnlies.get(plan.id), periodicCommitInfo)
   }
 
-  private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Pipe = {
+  private def buildPipe(plan: LogicalPlan)(implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): Pipe = {
     val pipeBuilder = pipeBuilderFactory(recurse = p => buildPipe(p),
                                          readOnly = context.readOnlies.get(plan.id),
                                          expressionConverters = expressionConverters)
@@ -49,8 +49,8 @@ object CommunityPipeBuilderFactory extends PipeBuilderFactory {
   def apply(recurse: LogicalPlan => Pipe,
             readOnly: Boolean,
             expressionConverters: ExpressionConverters)
-           (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): CommunityPipeBuilder = {
-    CommunityPipeBuilder(recurse, readOnly, expressionConverters, recursePipes(recurse, planContext))
+           (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): CommunityPipeBuilder = {
+    CommunityPipeBuilder(recurse, readOnly, expressionConverters, recursePipes(recurse), tokenContext)
   }
 }
 

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/executionplan/DefaultExecutionResultBuilderFactory.scala
@@ -21,6 +21,7 @@ package org.neo4j.cypher.internal.compatibility.v3_5.runtime.executionplan
 
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime._
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime.helpers.InternalWrapping._
+import org.neo4j.cypher.internal.frontend.v3_5.PlannerName
 import org.neo4j.cypher.internal.frontend.v3_5.phases.InternalNotificationLogger
 import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
 import org.neo4j.cypher.internal.runtime.interpreted.pipes._
@@ -67,6 +68,7 @@ abstract class BaseExecutionResultBuilderFactory(pipeInfo: PipeInfo,
     override def build(planType: ExecutionMode,
                        params: MapValue,
                        notificationLogger: InternalNotificationLogger,
+                       plannerName: PlannerName,
                        runtimeName: RuntimeName,
                        readOnlies: ReadOnlies,
                        cardinalities: Cardinalities): InternalExecutionResult = {
@@ -74,7 +76,7 @@ abstract class BaseExecutionResultBuilderFactory(pipeInfo: PipeInfo,
       taskCloser.addTask(queryContext.resources.close)
       val state = createQueryState(params)
       try {
-        createResults(state, planType, notificationLogger, runtimeName, readOnlies, cardinalities)
+        createResults(state, planType, notificationLogger, plannerName, runtimeName, readOnlies, cardinalities)
       }
       catch {
         case e: CypherException =>
@@ -88,12 +90,13 @@ abstract class BaseExecutionResultBuilderFactory(pipeInfo: PipeInfo,
 
     private def createResults(state: QueryState, planType: ExecutionMode,
                               notificationLogger: InternalNotificationLogger,
+                              plannerName: PlannerName,
                               runtimeName: RuntimeName,
                               readOnlies: ReadOnlies,
                               cardinalities: Cardinalities): InternalExecutionResult = {
       val queryType: InternalQueryType = getQueryType
       val planDescription =
-        () => LogicalPlan2PlanDescription(logicalPlan, pipeInfo.plannerUsed, readOnlies, cardinalities)
+        () => LogicalPlan2PlanDescription(logicalPlan, plannerName, readOnlies, cardinalities)
           .addArgument(Runtime(runtimeName.toTextOutput))
           .addArgument(RuntimeImpl(runtimeName.name))
       if (planType == ExplainMode) {

community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/compatibility/v3_5/runtime/executionplan/ExecutionResultBuilder.scala
@@ -21,6 +21,7 @@ package org.neo4j.cypher.internal.compatibility.v3_5.runtime.executionplan
 
 import org.neo4j.cypher.internal.util.v3_5.CypherException
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime.RuntimeName
+import org.neo4j.cypher.internal.frontend.v3_5.PlannerName
 import org.neo4j.cypher.internal.frontend.v3_5.phases.InternalNotificationLogger
 import org.neo4j.cypher.internal.planner.v3_5.spi.PlanningAttributes.{Cardinalities, ReadOnlies}
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.PipeDecorator
@@ -35,6 +36,7 @@ trait ExecutionResultBuilder {
   def build(planType: ExecutionMode,
             params: MapValue,
             notificationLogger: InternalNotificationLogger,
+            plannerName: PlannerName,
             runtimeName: RuntimeName,
             readOnlies: ReadOnlies,
             cardinalities: Cardinalities): InternalExecutionResult

enterprise/cypher/slotted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/slotted/SlottedPipeBuilder.scala
@@ -23,10 +23,9 @@ import org.neo4j.cypher.internal.compatibility.v3_5.runtime.SlotAllocation.Physi
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime._
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime.executionplan.builders.prepare.KeyTokenResolver
 import org.neo4j.cypher.internal.compatibility.v3_5.runtime.pipes.DropResultPipe
-import org.neo4j.cypher.internal.frontend.v3_5.phases.Monitors
 import org.neo4j.cypher.internal.frontend.v3_5.semantics.SemanticTable
 import org.neo4j.cypher.internal.ir.v3_5.VarPatternLength
-import org.neo4j.cypher.internal.planner.v3_5.spi.PlanContext
+import org.neo4j.cypher.internal.planner.v3_5.spi.TokenContext
 import org.neo4j.cypher.internal.runtime.interpreted.ExecutionContext
 import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.ExpressionConverters
 import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.{AggregationExpression, Expression}
@@ -49,7 +48,7 @@ class SlottedPipeBuilder(fallback: PipeBuilder,
                          physicalPlan: PhysicalPlan,
                          readOnly: Boolean,
                          rewriteAstExpression: (frontEndAst.Expression) => frontEndAst.Expression)
-                        (implicit context: PipeExecutionBuilderContext, planContext: PlanContext)
+                        (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext)
   extends PipeBuilder {
 
   private val convertExpressions: (frontEndAst.Expression) => commandExpressions.Expression =
@@ -336,10 +335,12 @@ class SlottedPipeBuilder(fallback: PipeBuilder,
   }
 
   private def buildPredicate(expr: frontEndAst.Expression)
-                            (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): Predicate = {
+                            (implicit context: PipeExecutionBuilderContext): Predicate = {
     val rewrittenExpr: frontEndAst.Expression = rewriteAstExpression(expr)
 
-    expressionConverters.toCommandPredicate(rewrittenExpr).rewrite(KeyTokenResolver.resolveExpressions(_, planContext))
+    expressionConverters
+      .toCommandPredicate(rewrittenExpr)
+      .rewrite(KeyTokenResolver.resolveExpressions(_, tokenContext))
       .asInstanceOf[Predicate]
   }
 
@@ -522,13 +523,13 @@ object SlottedPipeBuilder {
 
   case class Factory(physicalPlan: PhysicalPlan)
     extends PipeBuilderFactory {
-    def apply(recurse: LogicalPlan => Pipe, readOnly: Boolean,
+    override def apply(recurse: LogicalPlan => Pipe, readOnly: Boolean,
               expressionConverters: ExpressionConverters)
-             (implicit context: PipeExecutionBuilderContext, planContext: PlanContext): PipeBuilder = {
+             (implicit context: PipeExecutionBuilderContext, tokenContext: TokenContext): PipeBuilder = {
 
-      val expressionToExpression = recursePipes(recurse, planContext) _
+      val expressionToExpression = recursePipes(recurse) _
 
-      val fallback = CommunityPipeBuilder(recurse, readOnly, expressionConverters, expressionToExpression)
+      val fallback = CommunityPipeBuilder(recurse, readOnly, expressionConverters, expressionToExpression, tokenContext)
 
       new SlottedPipeBuilder(fallback, expressionConverters, physicalPlan, readOnly, expressionToExpression)
     }