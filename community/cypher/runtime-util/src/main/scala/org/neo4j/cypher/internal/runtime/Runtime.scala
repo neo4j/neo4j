83a9fe3d76404108f13cfd3d7056f4a7c117b838
fickludd <johan.teleman@neotechnology.com>
2018-05-14 08:36:08 +0200

    fix double merge mixup

--- community/cypher/runtime-util/src/main/scala/org/neo4j/cypher/internal/runtime/Runtime.scala
+++ community/cypher/runtime-util/src/main/scala/org/neo4j/cypher/internal/runtime/Runtime.scala
@@ -19,11 +19,11 @@
  */
 package org.neo4j.cypher.internal.runtime
 
-import org.neo4j.cypher.internal.frontend.v3_4.semantics.SemanticTable
-import org.neo4j.cypher.internal.planner.v3_4.spi.TokenContext
-import org.neo4j.cypher.internal.util.v3_4.symbols.CypherType
-import org.neo4j.cypher.internal.v3_4.expressions.Expression
-import org.neo4j.cypher.internal.v3_4.logical.plans.LogicalPlan
+import org.neo4j.cypher.internal.frontend.v3_5.semantics.SemanticTable
+import org.neo4j.cypher.internal.planner.v3_5.spi.TokenContext
+import org.neo4j.cypher.internal.util.v3_5.symbols.CypherType
+import org.neo4j.cypher.internal.v3_5.expressions.Expression
+import org.neo4j.cypher.internal.v3_5.logical.plans.LogicalPlan
 import org.neo4j.internal.kernel.api.Transaction
 import org.neo4j.kernel.impl.query.{QueryExecution, ResultBuffer}
 import org.neo4j.values.virtual.MapValue
