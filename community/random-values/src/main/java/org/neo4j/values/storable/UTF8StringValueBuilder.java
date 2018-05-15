fab4b27f4a4ba4c0389001331357b0c21cd292d2
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-05-15 19:54:56 +0200

    Small cleanup in random-values module

--- community/random-values/src/main/java/org/neo4j/values/storable/UTF8StringValueBuilder.java
+++ community/random-values/src/main/java/org/neo4j/values/storable/UTF8StringValueBuilder.java
@@ -29,7 +29,7 @@ import java.util.Arrays;
  */
 class UTF8StringValueBuilder
 {
-    private static final int DEFAULT_SIZE = 8;
+    private static final int DEFAULT_SIZE = Byte.SIZE;
     private byte[] bytes;
     private int length;
 
