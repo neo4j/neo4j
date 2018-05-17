72dadfffa021d9f81d81e45428ac33f7ef0546fa
Mattias Finné <mattias@neotechnology.com>
2018-05-17 13:17:28 +0200

    Licensing formatting

--- community/kernel/src/main/java/org/neo4j/kernel/impl/index/schema/fusion/FusionSlotSelector10.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/index/schema/fusion/FusionSlotSelector10.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2002-2018 "Neo4j,"
- * Neo4j Sweden AB [http://neo4j.com]
+ * Copyright (c) 2002-2018 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
  *
