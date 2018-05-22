tinwelint/neo4j-public

Removes old StorageReader#acquireSingleNodeCursor
Replacing it with StorageReader#singleNode

community/kernel-api/src/main/java/org/neo4j/internal/kernel/api/helpers/Nodes.java
@@ -19,7 +19,10 @@
  */
 package org.neo4j.internal.kernel.api.helpers;
 
+import java.util.function.IntConsumer;
+
 import org.neo4j.internal.kernel.api.CursorFactory;
+import org.neo4j.internal.kernel.api.LabelSet;
 import org.neo4j.internal.kernel.api.NodeCursor;
 import org.neo4j.internal.kernel.api.RelationshipGroupCursor;
 import org.neo4j.internal.kernel.api.RelationshipTraversalCursor;
@@ -287,6 +290,20 @@ public final class Nodes
                 }
                 return count;
             }
+        }
+    }
+
+    /**
+     * Loops through all label ids in the {@code labelIds} {@link LabelSet} and lets the {@code visitor} consume them.
+     *
+     * @param labelIds {@link LabelSet} containing the label ids.
+     * @param visitor {@link IntConsumer} to pass the label ids to.
+     */
+    public static void visitLabels( LabelSet labelIds, IntConsumer visitor )
+    {
+        for ( int i = 0; i < labelIds.numberOfLabels(); i++ )
+        {
+            visitor.accept( labelIds.label( i ) );
         }
     }
 }

community/kernel/src/test/java/org/neo4j/kernel/impl/storageengine/impl/recordstorage/RecordStorageReaderLabelTest.java
@@ -26,9 +26,13 @@ import org.junit.Test;
 import org.neo4j.collection.PrimitiveLongCollections;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.internal.kernel.api.LabelSet;
+import org.neo4j.internal.kernel.api.NodeCursor;
+import org.neo4j.internal.kernel.api.helpers.Nodes;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.neo4j.helpers.collection.Iterators.asSet;
 import static org.neo4j.helpers.collection.MapUtil.map;
 import static org.neo4j.test.mockito.matcher.Neo4jMatchers.containsOnly;
@@ -57,8 +61,10 @@ public class RecordStorageReaderLabelTest extends RecordStorageReaderTestBase
         }
 
         // THEN
-        storageReader.acquireSingleNodeCursor( nodeId ).forAll(
-                node -> assertEquals( IntHashSet.newSetWith( labelId1, labelId2 ), node.labels() ) );
+        NodeCursor nodeCursor = storageReader.allocateNodeCursor();
+        storageReader.singleNode( nodeId, nodeCursor );
+        assertTrue( nodeCursor.next() );
+        assertEquals( IntHashSet.newSetWith( labelId1, labelId2 ), asIntSet( nodeCursor.labels() ) );
     }
 
     @Test
@@ -101,5 +107,12 @@ public class RecordStorageReaderLabelTest extends RecordStorageReaderTestBase
         // THEN
         assertEquals( asSet( node1.getId(), node2.getId() ), PrimitiveLongCollections.toSet( nodesForLabel1 ) );
         assertEquals( asSet( node2.getId() ), PrimitiveLongCollections.toSet( nodesForLabel2 ) );
+    }
+
+    private IntHashSet asIntSet( LabelSet labels )
+    {
+        IntHashSet set = IntHashSet.newSetWith();
+        Nodes.visitLabels( labels, set::add );
+        return set;
     }
 }

neo4j/neo4j

community/kernel-api/src/main/java/org/neo4j/internal/kernel/api/helpers/Nodes.java
@@ -19,7 +19,11 @@
  */
 package org.neo4j.internal.kernel.api.helpers;
 
+import java.util.function.IntConsumer;
+import java.util.function.LongConsumer;
+
 import org.neo4j.internal.kernel.api.CursorFactory;
+import org.neo4j.internal.kernel.api.LabelSet;
 import org.neo4j.internal.kernel.api.NodeCursor;
 import org.neo4j.internal.kernel.api.RelationshipGroupCursor;
 import org.neo4j.internal.kernel.api.RelationshipTraversalCursor;
@@ -287,6 +291,20 @@ public final class Nodes
                 }
                 return count;
             }
+        }
+    }
+
+    /**
+     * Loops through all label ids in the {@code labelIds} {@link LabelSet} and lets the {@code visitor} consume them.
+     *
+     * @param labelIds {@link LabelSet} containing the label ids.
+     * @param visitor {@link IntConsumer} to pass the label ids to.
+     */
+    public static void visitLabels( LabelSet labelIds, LongConsumer visitor )
+    {
+        for ( int i = 0; i < labelIds.numberOfLabels(); i++ )
+        {
+            visitor.accept( labelIds.label( i ) );
         }
     }
 }

community/kernel/src/test/java/org/neo4j/kernel/impl/storageengine/impl/recordstorage/RecordStorageReaderLabelTest.java
@@ -20,15 +20,20 @@
 package org.neo4j.kernel.impl.storageengine.impl.recordstorage;
 
 import org.eclipse.collections.api.iterator.LongIterator;
+import org.eclipse.collections.impl.set.mutable.primitive.LongHashSet;
 import org.junit.Test;
 
 import org.neo4j.collection.PrimitiveLongCollections;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.internal.kernel.api.LabelSet;
+import org.neo4j.internal.kernel.api.NodeCursor;
+import org.neo4j.internal.kernel.api.helpers.Nodes;
 
 import static org.eclipse.collections.impl.set.mutable.primitive.LongHashSet.newSetWith;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.neo4j.helpers.collection.Iterators.asSet;
 import static org.neo4j.helpers.collection.MapUtil.map;
 import static org.neo4j.test.mockito.matcher.Neo4jMatchers.containsOnly;
@@ -57,8 +62,10 @@ public class RecordStorageReaderLabelTest extends RecordStorageReaderTestBase
         }
 
         // THEN
-        storageReader.acquireSingleNodeCursor( nodeId ).forAll(
-                node -> assertEquals( newSetWith( labelId1, labelId2 ), node.labels() ) );
+        NodeCursor nodeCursor = storageReader.allocateNodeCursor();
+        storageReader.singleNode( nodeId, nodeCursor );
+        assertTrue( nodeCursor.next() );
+        assertEquals( newSetWith( labelId1, labelId2 ), asLongSet( nodeCursor.labels() ) );
     }
 
     @Test
@@ -101,5 +108,12 @@ public class RecordStorageReaderLabelTest extends RecordStorageReaderTestBase
         // THEN
         assertEquals( asSet( node1.getId(), node2.getId() ), PrimitiveLongCollections.toSet( nodesForLabel1 ) );
         assertEquals( asSet( node2.getId() ), PrimitiveLongCollections.toSet( nodesForLabel2 ) );
+    }
+
+    private LongHashSet asLongSet( LabelSet labels )
+    {
+        LongHashSet set = newSetWith();
+        Nodes.visitLabels( labels, set::add );
+        return set;
     }
 }