af1a2d4338e90d15f85fd9b28f6019e18a3b8b91
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-05-24 17:12:44 +0200

    Adapt tests

--- enterprise/kernel/src/test/java/org/neo4j/kernel/enterprise/builtinprocs/ListQueriesProcedureTest.java
+++ enterprise/kernel/src/test/java/org/neo4j/kernel/enterprise/builtinprocs/ListQueriesProcedureTest.java
@@ -40,7 +40,8 @@ import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.PropertyContainer;
 import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
+import org.neo4j.kernel.configuration.Settings;
 import org.neo4j.kernel.impl.factory.GraphDatabaseFacade;
 import org.neo4j.kernel.impl.locking.ResourceTypes;
 import org.neo4j.test.rule.DatabaseRule;
@@ -79,13 +80,11 @@ import static org.neo4j.test.rule.concurrent.ThreadingRule.waitingWhileIn;
 public class ListQueriesProcedureTest
 {
     private final DatabaseRule db = new ImpermanentEnterpriseDatabaseRule()
-    {
-        @Override
-        protected void configure( GraphDatabaseBuilder builder )
-        {
-            builder.setConfig( cypher_hints_error, "true" );
-        }
-    }.startLazily();
+            .withSetting( cypher_hints_error, Settings.TRUE )
+            .withSetting( GraphDatabaseSettings.track_query_allocation, Settings.TRUE )
+            .withSetting( track_query_cpu_time, Settings.TRUE )
+            .startLazily();
+
     private final ThreadingRule threads = new ThreadingRule();
 
     @Rule
