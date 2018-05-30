933de68add17be87015a9467e503ada36e2f0fae
Anton Persson <anton.persson@neotechnology.com>
2018-05-30 08:46:34 +0200

    Fix license headers

--- community/kernel/src/test/java/org/neo4j/kernel/impl/index/schema/fusion/FusionSlotSelectorTest.java
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/index/schema/fusion/FusionSlotSelectorTest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2002-2018 "Neo Technology,"
- * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
  *
  * This file is part of Neo4j.
  *
