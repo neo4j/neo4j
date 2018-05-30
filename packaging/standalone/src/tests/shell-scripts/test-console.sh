897447f2b043a6a0a7c4f39a2f2f029e950fd4db
Andrew Jefferson <andrew.jefferson@neotechnology.com>
2018-05-30 18:23:24 +0100

    dont try and set env vars that start with numbers. It's not allowed

--- packaging/standalone/src/tests/shell-scripts/test-console.sh
+++ packaging/standalone/src/tests/shell-scripts/test-console.sh
@@ -9,4 +9,9 @@ test_expect_success "should run okay" "
   run_console
 "
 
+test_expect_success "should run okay with env var" "
+  set_config '1a' 'foo' neo4j.conf &&
+  run_console
+"
+
 test_done
