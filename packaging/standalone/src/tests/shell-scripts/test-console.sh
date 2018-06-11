ef614237cd5a1de6ebdfba7bf5e872fa6d1330a7
Andrew Jefferson <andrew.jefferson@neotechnology.com>
2018-06-11 14:34:48 +0100

    Log a warning if settings are not converted to env vars because they cannot be. Improving associated test name

--- packaging/standalone/src/tests/shell-scripts/test-console.sh
+++ packaging/standalone/src/tests/shell-scripts/test-console.sh
@@ -9,7 +9,7 @@ test_expect_success "should run okay" "
   run_console
 "
 
-test_expect_success "should run okay with env var" "
+test_expect_success "should run okay when there is a setting in the config that starts with a number" "
   set_config '1a' 'foo' neo4j.conf &&
   run_console
 "
