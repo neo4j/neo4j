2bdf7a76031e81ac894c55d16fd89bad276bac9c
RagnarW <ragnar.wernersson@neotechnology.com>
2018-07-05 16:03:14 +0200

    Revert "ReplicationError is considered transient in tests"
    This reverts commit 396f0541cbb2fd380055e7127c70417238a3a26e.

--- enterprise/causal-clustering/src/test/java/org/neo4j/causalclustering/discovery/Cluster.java
+++ enterprise/causal-clustering/src/test/java/org/neo4j/causalclustering/discovery/Cluster.java
@@ -60,7 +60,6 @@ import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.TransactionFailureException;
 import org.neo4j.graphdb.security.WriteOperationsNotAllowedException;
 import org.neo4j.helpers.AdvertisedSocketAddress;
-import org.neo4j.kernel.api.exceptions.Status;
 import org.neo4j.kernel.internal.DatabaseHealth;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.kernel.monitoring.Monitors;
@@ -512,34 +511,29 @@ public class Cluster
         return awaitEx( supplier, notNull()::test, timeout, timeUnit );
     }
 
-    private static boolean isTransientFailure( Throwable e )
+    private boolean isTransientFailure( Throwable e )
     {
         // TODO: This should really catch all cases of transient failures. Must be able to express that in a clearer
         // manner...
-        return (e instanceof IdGenerationException) || isLockExpired( e ) || isLockOnFollower( e ) || isWriteNotOnLeader( e ) || isUnableToReplicate( e );
+        return (e instanceof IdGenerationException) || isLockExpired( e ) || isLockOnFollower( e ) ||
+               isWriteNotOnLeader( e );
 
     }
 
-    private static boolean isWriteNotOnLeader( Throwable e )
+    private boolean isWriteNotOnLeader( Throwable e )
     {
         return e instanceof WriteOperationsNotAllowedException &&
                e.getMessage().startsWith( String.format( LeaderCanWrite.NOT_LEADER_ERROR_MSG, "" ) );
     }
 
-    private static boolean isLockOnFollower( Throwable e )
+    private boolean isLockOnFollower( Throwable e )
     {
         return e instanceof AcquireLockTimeoutException &&
                (e.getMessage().equals( LeaderOnlyLockManager.LOCK_NOT_ON_LEADER_ERROR_MESSAGE ) ||
                 e.getCause() instanceof NoLeaderFoundException);
     }
 
-    private static boolean isUnableToReplicate( Throwable e )
-    {
-        return e instanceof org.neo4j.internal.kernel.api.exceptions.TransactionFailureException &&
-                ((org.neo4j.internal.kernel.api.exceptions.TransactionFailureException) e).status().equals( Status.Cluster.ReplicationFailure );
-    }
-
-    private static boolean isLockExpired( Throwable e )
+    private boolean isLockExpired( Throwable e )
     {
         return e instanceof TransactionFailureException &&
                e.getCause() instanceof org.neo4j.internal.kernel.api.exceptions.TransactionFailureException &&
