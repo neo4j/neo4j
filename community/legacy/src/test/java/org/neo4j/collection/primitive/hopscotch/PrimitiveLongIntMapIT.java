b1c8d3d7fa641d99f7c84425cbd63d59b7a6a645
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2018-07-21 00:14:41 +0200

    Rename PrimitiveLongIntMapRIT -> PrimitiveLongIntMapIT

--- community/legacy/src/test/java/org/neo4j/collection/primitive/hopscotch/PrimitiveLongIntMapIT.java
+++ community/legacy/src/test/java/org/neo4j/collection/primitive/hopscotch/PrimitiveLongIntMapIT.java
@@ -45,7 +45,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-class PrimitiveLongIntMapRIT
+class PrimitiveLongIntMapIT
 {
     @Test
     void thoroughlyTestIt()
