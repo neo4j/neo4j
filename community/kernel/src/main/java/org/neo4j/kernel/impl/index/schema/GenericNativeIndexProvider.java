6ef391b5b70d5e9943fd1c26664b2e6034235ae9
Anton Persson <anton.persson@neotechnology.com>
2018-07-23 13:31:13 +0200

    PR comment: Description of granular composite index query

--- community/kernel/src/main/java/org/neo4j/kernel/impl/index/schema/GenericNativeIndexProvider.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/index/schema/GenericNativeIndexProvider.java
@@ -39,6 +39,45 @@ import org.neo4j.values.storable.ValueCategory;
 
 /**
  * Single-value all-in-one native index
+ *
+ * A composite index query have one index query per slot / column.
+ * The different "slot query" variants are exact, range and exists.
+ * Other index providers have support for exact match on all columns and exists match on all columns (full scan).
+ * This index provider have some additional capabilities. It can combine the slot queries under the following rules:
+ * a. Exact can only follow another Exact or be in first slot.
+ * b. Range can only follow Exact or be in first slot.
+ *
+ * We use the following notation for the "slot queries":
+ * x: exact query
+ * -: exists query
+ * >: range query
+ *
+ * With an index on 5 slots as en example we can build several different queries:
+ *     p1 p2 p3 p4 p5 (order is important)
+ * 1:  x  x  x  x  x
+ * 2:  -  -  -  -  -
+ * 3:  x  -  -  -  -
+ * 4:  x  x  x  x  -
+ * 5:  >  -  -  -  -
+ * 6:  x  >  -  -  -
+ * 7:  x  x  x  x  >
+ * 8:  >  x  -  -  - (not allowed!)
+ * 9:  >  >  -  -  - (not allowed!)
+ * 10: -  x  -  -  - (not allowed!)
+ * 11: -  >  -  -  - (not allowed!)
+ *
+ * 1: Exact match on all slots. Supported by all index providers.
+ * 2: Exists scan on all slots. Supported by all index providers.
+ * 3: Exact match on first column and exists on the rest.
+ * 4: Exact match on all columns but the last.
+ * 5: Range on first column and exists on rest.
+ * 6: Exact on first, range on second and exists on rest.
+ * 7: Exact on all but last column. Range on last.
+ * 8: Not allowed because Exact can only follow another Exact.
+ * 9: Not allowed because range can only follow Exact.
+ * 10: Not allowed because Exact can only follow another Exact.
+ * 11: Not allowed because range can only follow Exact.
+ * WHY?
  */
 public class GenericNativeIndexProvider extends NativeIndexProvider<CompositeGenericKey,NativeIndexValue>
 {
