2509fa49d4f9e4ae0eb17711e49e4ad23f6efa7b
Anton Persson <anton.persson@neotechnology.com>
2018-07-27 16:53:24 +0200

    Move test to correct module: index-it

--- community/community-it/index-it/src/test/java/org/neo4j/kernel/api/impl/schema/StringLengthIndexValidationIT.java
+++ community/community-it/index-it/src/test/java/org/neo4j/kernel/api/impl/schema/StringLengthIndexValidationIT.java
@@ -43,7 +43,7 @@ public class StringLengthIndexValidationIT
 {
     @Rule
     public DatabaseRule db = new EmbeddedDatabaseRule()
-            .withSetting( GraphDatabaseSettings.default_schema_provider, GraphDatabaseSettings.SchemaIndex.NATIVE20.providerName() );
+            .withSetting( GraphDatabaseSettings.default_schema_provider, GraphDatabaseSettings.SchemaIndex.NATIVE20.providerIdentifier() );
 
     private static final String propKey = "largeString";
     private static final int keySizeLimit = TreeNodeDynamicSize.keyValueSizeCapFromPageSize( PageCache.PAGE_SIZE ) - Long.BYTES;
