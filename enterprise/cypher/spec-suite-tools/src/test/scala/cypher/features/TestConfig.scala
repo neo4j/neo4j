c6cdd9b25af7a8aea6596a5118e852c40c8dddd3
Alex Averbuch <alex.averbuch@gmail.com>
2018-08-03 08:40:28 +0300

    Revert "only create new (inner) QueryContexts when using threaded scheduler. splits morsel feature test config into threaded & non-threaded, to run tests with both schedulers."
    This reverts commit f85b8b0333f6a4091014f0ccda4961e8f73ee5c4.

--- enterprise/cypher/spec-suite-tools/src/test/scala/cypher/features/TestConfig.scala
+++ enterprise/cypher/spec-suite-tools/src/test/scala/cypher/features/TestConfig.scala
@@ -31,9 +31,7 @@ case object DefaultTestConfig extends TestConfig(Some("default.txt"),"")
 
 case object CostSlottedTestConfig extends TestConfig(Some("cost-slotted.txt"),"CYPHER planner=cost runtime=slotted")
 
-case object CostMorselTestConfigSingleThreaded extends TestConfig(Some("cost-morsel.txt"),"CYPHER planner=cost runtime=morsel debug=singlethreaded")
-
-case object CostMorselTestConfig extends TestConfig(Some("cost-morsel.txt"),"CYPHER planner=cost runtime=morsel")
+case object CostMorselTestConfig extends TestConfig(Some("cost-morsel.txt"),"CYPHER planner=cost runtime=morsel debug=singlethreaded")
 
 case object CostCompiledTestConfig extends TestConfig(Some("cost-compiled.txt"),
   "CYPHER planner=cost runtime=compiled debug=generate_java_source")
