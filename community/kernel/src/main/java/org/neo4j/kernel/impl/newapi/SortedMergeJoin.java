7feecf8d1a62e90024a2cbef0996cc8c19407921
fickludd <johan.teleman@neotechnology.com>
2018-08-29 16:46:29 +0200

    Implement SortedMergeJoin
    This will be used for merging index seeks from txState and index

---
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/newapi/SortedMergeJoin.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel.impl.newapi;
+
+import org.neo4j.internal.kernel.api.IndexOrder;
+import org.neo4j.values.storable.Value;
+import org.neo4j.values.storable.Values;
+
+import static org.neo4j.util.Preconditions.checkArgument;
+
+final class SortedMergeJoin
+{
+    private long nextFromA;
+    private long nextFromB;
+    private Value[] valuesFromA;
+    private Value[] valuesFromB;
+    private int indexOrder;
+
+    void initialize( IndexOrder indexOrder )
+    {
+        this.indexOrder = indexOrder == IndexOrder.DESCENDING ? 1 : -1;
+        this.nextFromA = -1;
+        this.nextFromB = -1;
+        this.valuesFromA = null;
+        this.valuesFromB = null;
+    }
+
+    boolean needsA()
+    {
+        return nextFromA == -1;
+    }
+
+    boolean needsB()
+    {
+        return nextFromB == -1;
+    }
+
+    void setA( long nodeId, Value[] values )
+    {
+        nextFromA = nodeId;
+        valuesFromA = values;
+    }
+
+    void setB( long nodeId, Value[] values )
+    {
+        nextFromB = nodeId;
+        valuesFromB = values;
+    }
+
+    void next( Sink sink )
+    {
+        int c = 0;
+        if ( valuesFromA != null && valuesFromB != null )
+        {
+            checkArgument( valuesFromA.length == valuesFromB.length,
+                           "Expected index and txState values to have same dimensions, but got %d values from index and %d from txState",
+                           valuesFromB.length, valuesFromA.length );
+
+            for ( int i = 0; c == 0 && i < valuesFromA.length; i++ )
+            {
+                c = Values.COMPARATOR.compare( valuesFromA[i], valuesFromB[i] );
+            }
+        }
+
+        if ( nextFromB == -1 || Integer.signum( c ) == indexOrder )
+        {
+            sink.acceptSortedMergeJoin( nextFromA, valuesFromA );
+            nextFromA = -1;
+            valuesFromA = null;
+        }
+        else
+        {
+            sink.acceptSortedMergeJoin( nextFromB, valuesFromB );
+            nextFromB = -1;
+            valuesFromB = null;
+        }
+    }
+
+    interface Sink
+    {
+        void acceptSortedMergeJoin( long nodeId, Value[] values );
+    }
+}
---
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/newapi/SortedMergeJoinTest.java
@@ -0,0 +1,171 @@
+package org.neo4j.kernel.impl.newapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.neo4j.internal.kernel.api.IndexOrder;
+import org.neo4j.values.storable.Value;
+import org.neo4j.values.storable.ValueTuple;
+import org.neo4j.values.storable.Values;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith( Parameterized.class )
+public class SortedMergeJoinTest
+{
+    @Parameterized.Parameters()
+    public static Iterable<Object[]> data()
+    {
+        return Arrays.asList(new Object[][] {
+                { IndexOrder.ASCENDING }, { IndexOrder.DESCENDING }
+        });
+    }
+
+    @Parameterized.Parameter
+    public IndexOrder indexOrder;
+
+    @Test
+    public void shouldWorkWithEmptyLists()
+    {
+        assertThatItWorksOneWay( Collections.emptyList(), Collections.emptyList() );
+    }
+
+    @Test
+    public void shouldWorkWithAList()
+    {
+        assertThatItWorks( Arrays.asList(
+                                   node( 1L, "a" ),
+                                   node( 3L, "aa" ),
+                                   node( 5L, "c" ),
+                                   node( 7L, "g" ) ),
+                           Collections.emptyList() );
+    }
+
+    @Test
+    public void shouldWorkWith2Lists()
+    {
+        assertThatItWorks( Arrays.asList(
+                                   node( 1L, "a" ),
+                                   node( 3L, "aa" ),
+                                   node( 5L, "c" ),
+                                   node( 7L, "g" ) ),
+                           Arrays.asList(
+                                   node( 2L, "b" ),
+                                   node( 4L, "ba" ),
+                                   node( 6L, "ca" ),
+                                   node( 8L, "d" ) ) );
+    }
+
+    @Test
+    public void shouldWorkWithSameElements()
+    {
+        assertThatItWorks( Arrays.asList(
+                node( 1L, "a" ),
+                node( 3L, "b" ),
+                node( 5L, "c" ) ),
+                           Arrays.asList(
+                                   node( 2L, "aa" ),
+                                   node( 3L, "b" ),
+                                   node( 6L, "ca" ) ) );
+    }
+
+    @Test
+    public void shouldWorkWithCompositeValues()
+    {
+        assertThatItWorks( Arrays.asList(
+                                   node( 1L, "a", "a" ),
+                                   node( 3L, "b", "a" ),
+                                   node( 5L, "b", "b" ),
+                                   node( 7L, "c", "d" ) ),
+                           Arrays.asList(
+                                   node( 2L, "a", "b" ),
+                                   node( 5L, "b", "b" ),
+                                   node( 6L, "c", "e" ) ) );
+    }
+
+    private void assertThatItWorks( List<NodeWithPropertyValues> listA, List<NodeWithPropertyValues> listB )
+    {
+        assertThatItWorksOneWay( listA, listB );
+        assertThatItWorksOneWay( listB, listA );
+    }
+
+    private void assertThatItWorksOneWay( List<NodeWithPropertyValues> listA, List<NodeWithPropertyValues> listB )
+    {
+        SortedMergeJoin sortedMergeJoin = new SortedMergeJoin();
+        sortedMergeJoin.initialize( indexOrder );
+
+        Comparator<NodeWithPropertyValues> comparator =
+                indexOrder == IndexOrder.ASCENDING ?
+                (a, b) -> ValueTuple.COMPARATOR.compare( ValueTuple.of( a.getValues() ), ValueTuple.of( b.getValues() ) ) :
+                (a, b) -> ValueTuple.COMPARATOR.compare( ValueTuple.of( b.getValues() ), ValueTuple.of( a.getValues() ) );
+
+        listA.sort( comparator );
+        listB.sort( comparator );
+
+        List<NodeWithPropertyValues> result = process( sortedMergeJoin, listA.iterator(), listB.iterator() );
+
+        List<NodeWithPropertyValues> expected = new ArrayList<>();
+        expected.addAll( listA );
+        expected.addAll( listB );
+        expected.sort( comparator );
+
+        assertThat( result, equalTo( expected ) );
+    }
+
+    private List<NodeWithPropertyValues> process( SortedMergeJoin sortedMergeJoin,
+                                                  Iterator<NodeWithPropertyValues> iteratorA,
+                                                  Iterator<NodeWithPropertyValues> iteratorB )
+    {
+        Collector collector = new Collector();
+        while ( !collector.done )
+        {
+            if ( iteratorA.hasNext() && sortedMergeJoin.needsA() )
+            {
+                NodeWithPropertyValues a = iteratorA.next();
+                sortedMergeJoin.setA( a.getNodeId(), a.getValues() );
+            }
+            if ( iteratorB.hasNext() && sortedMergeJoin.needsB() )
+            {
+                NodeWithPropertyValues b = iteratorB.next();
+                sortedMergeJoin.setB( b.getNodeId(), b.getValues() );
+            }
+
+            sortedMergeJoin.next( collector );
+        }
+        return collector.result;
+    }
+
+    private NodeWithPropertyValues node( long id, Object... values )
+    {
+        return new NodeWithPropertyValues( id, Stream.of( values ).map( Values::of ).toArray( Value[]::new ) );
+    }
+
+    class Collector implements SortedMergeJoin.Sink
+    {
+        final List<NodeWithPropertyValues> result = new ArrayList<>();
+        boolean done = false;
+
+        @Override
+        public void acceptSortedMergeJoin( long nodeId, Value[] values )
+        {
+            if ( nodeId == -1 )
+            {
+                done = true;
+            }
+            else
+            {
+                result.add( new NodeWithPropertyValues( nodeId, values ) );
+            }
+        }
+    }
+}
