33be7f195dfc0f9b12394928bd9e77342647079c
Alex Averbuch <alex.averbuch@gmail.com>
2018-08-29 11:25:00 +0300

    updates test expectations inline with GRAPH-related syntax changes

--- enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/UnsupportedFeaturesAcceptanceTest.scala
+++ enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/UnsupportedFeaturesAcceptanceTest.scala
@@ -41,18 +41,18 @@ class UnsupportedFeaturesAcceptanceTest extends ExecutionEngineFunSuite with Cyp
   }
 
   test("construct graph") {
-    val query = "MATCH (a) CONSTRUCT ON foo.bar CLONE a NEW (a)-[:T {prop: a.prop}]->(:X) RETURN 1 AS a"
+    val query = "MATCH (a) CONSTRUCT ON foo.bar CLONE a CREATE (a)-[:T {prop: a.prop}]->(:X) RETURN 1 AS a"
     failWithError(configs, query, List("The `CONSTRUCT` clause is not available in this implementation of Cypher due to lack of support for multiple graphs."))
   }
 
   test("create graph") {
-    val query = "CREATE GRAPH foo { RETURN GRAPH }"
-    failWithError(configs, query, List("The `CREATE GRAPH` clause is not available in this implementation of Cypher due to lack of support for multiple graphs."))
+    val query = "CATALOG CREATE GRAPH foo { RETURN GRAPH }"
+    failWithError(configs, query, List("The `CATALOG CREATE GRAPH` clause is not available in this implementation of Cypher due to lack of support for multiple graphs."))
   }
 
   test("delete graph") {
-    val query = "DELETE GRAPH foo"
-    failWithError(configs, query, List("The `DELETE GRAPH` clause is not available in this implementation of Cypher due to lack of support for multiple graphs."))
+    val query = "CATALOG DROP GRAPH foo"
+    failWithError(configs, query, List("The `CATALOG DROP GRAPH` clause is not available in this implementation of Cypher due to lack of support for multiple graphs."))
   }
 
   test("equivalence operator") {
