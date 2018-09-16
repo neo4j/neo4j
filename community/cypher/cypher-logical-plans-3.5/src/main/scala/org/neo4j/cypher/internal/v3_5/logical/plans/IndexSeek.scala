f6a65d9fbd215a6750453c652f9b36cb84e9c59b
fickludd <johan.teleman@neotechnology.com>
2018-09-16 22:52:20 +0200

    Initial IndexSeek test support
    This small DSL allows one to express index seek using a string formatted
    with a special syntax. See test for examples.

---
+++ community/cypher/cypher-logical-plans-3.5/src/main/scala/org/neo4j/cypher/internal/v3_5/logical/plans/IndexSeek.scala
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.v3_5.logical.plans
+
+import org.opencypher.v9_0.expressions._
+import org.opencypher.v9_0.util.attribution.IdGen
+import org.opencypher.v9_0.util.{InputPosition, LabelId, NonEmptyList, PropertyKeyId}
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Helper object for constructing node index operators from strings.
+  */
+object IndexSeek {
+
+  // primitives
+  private val ID = "([a-zA-Z][a-zA-Z0-9]*)"
+  private val VALUE = "([0-9'].*)"
+  private val INT = "([0-9])".r
+  private val STRING = s"'(.*)'".r
+
+  // entry point
+  private val INDEX_SEEK_PATTERN = s"$ID: ?$ID ?\\(([^\\)]+)\\)".r
+
+  // predicates
+  private val EXACT = s"$ID ?= ?$VALUE".r
+  private val EXISTS = s"$ID".r
+  private val LESS_THAN = s"$ID ?< ?$VALUE".r
+  private val LESS_THAN_OR_EQ = s"$ID ?<= ?$VALUE".r
+  private val GREATER_THAN = s"$ID ?> ?$VALUE".r
+  private val GREATER_THAN_OR_EQ = s"$ID ?>= ?$VALUE".r
+  private val STARTS_WITH = s"$ID STARTS WITH $STRING".r
+  private val ENDS_WITH = s"$ID ENDS WITH $STRING".r
+  private val CONTAINS = s"$ID CONTAINS $STRING".r
+
+  private val pos = InputPosition.NONE
+
+  /**
+    * Construct a node index seek/scan operator by parsing a string.
+    */
+  def apply(indexSeekString: String)(implicit idGen: IdGen): IndexLeafPlan = {
+
+    val INDEX_SEEK_PATTERN(node, labelStr, predicateStr) = indexSeekString.trim
+    val label = LabelToken(labelStr, LabelId(0))
+    val predicates = predicateStr.split(',').map(_.trim)
+
+    def prop(prop: String) =
+      IndexedProperty(PropertyKeyToken(PropertyKeyName(prop)(pos), PropertyKeyId(0)), DoNotGetValue)
+
+    def value(value: String): Literal =
+      value match {
+        case INT(int) => SignedDecimalIntegerLiteral(int)(pos)
+        case STRING(str) => StringLiteral(str)(pos)
+        case _ => throw new IllegalArgumentException(s"Value `$value` is not supported")
+      }
+
+    if (predicates.length == 1) {
+      predicates.head match {
+        case EXACT(propStr, valueStr) =>
+          val valueExpr = SingleQueryExpression(value(valueStr))
+          NodeIndexSeek(node, label, List(prop(propStr)), valueExpr, Set.empty)
+
+        case LESS_THAN(propStr, valueStr) =>
+          val valueExpr = RangeQueryExpression(InequalitySeekRangeWrapper(RangeLessThan(NonEmptyList(ExclusiveBound(value(valueStr)))))(pos))
+          NodeIndexSeek(node, label, List(prop(propStr)), valueExpr, Set.empty)
+
+        case LESS_THAN_OR_EQ(propStr, valueStr) =>
+          val valueExpr = RangeQueryExpression(InequalitySeekRangeWrapper(RangeLessThan(NonEmptyList(InclusiveBound(value(valueStr)))))(pos))
+          NodeIndexSeek(node, label, List(prop(propStr)), valueExpr, Set.empty)
+
+        case GREATER_THAN(propStr, valueStr) =>
+          val valueExpr = RangeQueryExpression(InequalitySeekRangeWrapper(RangeGreaterThan(NonEmptyList(ExclusiveBound(value(valueStr)))))(pos))
+          NodeIndexSeek(node, label, List(prop(propStr)), valueExpr, Set.empty)
+
+        case GREATER_THAN_OR_EQ(propStr, valueStr) =>
+          val valueExpr = RangeQueryExpression(InequalitySeekRangeWrapper(RangeGreaterThan(NonEmptyList(InclusiveBound(value(valueStr)))))(pos))
+          NodeIndexSeek(node, label, List(prop(propStr)), valueExpr, Set.empty)
+
+        case STARTS_WITH(propStr, string) =>
+          val valueExpr = RangeQueryExpression(PrefixSeekRangeWrapper(PrefixRange(StringLiteral(string)(pos)))(pos))
+          NodeIndexSeek(node, label, List(prop(propStr)), valueExpr, Set.empty)
+
+        case ENDS_WITH(propStr, string) =>
+          NodeIndexEndsWithScan(node, label, prop(propStr), StringLiteral(string)(pos), Set.empty)
+
+        case CONTAINS(propStr, string) =>
+          NodeIndexContainsScan(node, label, prop(propStr), StringLiteral(string)(pos), Set.empty)
+
+        case EXISTS(propStr) =>
+          NodeIndexScan(node, label, prop(propStr), Set.empty)
+      }
+    } else if (predicates.length > 1) {
+
+      val properties = new ArrayBuffer[IndexedProperty]()
+      val valueExprs = new ArrayBuffer[SingleQueryExpression[Expression]]()
+
+      for (predicate <- predicates)
+        predicate match {
+          case EXACT(propStr, valueStr) =>
+            valueExprs += SingleQueryExpression(value(valueStr))
+            properties += prop(propStr)
+          case _ => throw new IllegalArgumentException("Only exact predicates are allowed in composite seeks.")
+        }
+
+      NodeIndexSeek(node, label, properties, CompositeQueryExpression(valueExprs), Set.empty)
+    } else
+      throw new IllegalArgumentException("Cannot parse 'str' and index seek.")
+  }
+}
---
+++ community/cypher/cypher-logical-plans-3.5/src/test/scala/org/neo4j/cypher/internal/v3_5/logical/plans/IndexSeekTest.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2002-2018 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.v3_5.logical.plans
+
+import org.opencypher.v9_0.expressions._
+import org.opencypher.v9_0.util.attribution.{Id, IdGen, SameId}
+import org.opencypher.v9_0.util.{InputPosition, LabelId, NonEmptyList, PropertyKeyId}
+import org.opencypher.v9_0.util.test_helpers.CypherFunSuite
+
+class IndexSeekTest extends CypherFunSuite {
+
+  implicit val idGen: IdGen = SameId(Id(42))
+  private val pos = InputPosition.NONE
+
+  val testCases = List(
+    "a:X(prop = 1)" -> NodeIndexSeek("a", label("X"), Seq(prop("prop")), exactInt(1), Set.empty),
+    "b:X(prop = 1)" -> NodeIndexSeek("b", label("X"), Seq(prop("prop")), exactInt(1), Set.empty),
+    "b:Y(prop = 1)" -> NodeIndexSeek("b", label("Y"), Seq(prop("prop")), exactInt(1), Set.empty),
+    "b:Y(dogs = 1)" -> NodeIndexSeek("b", label("Y"), Seq(prop("dogs")), exactInt(1), Set.empty),
+    "b:Y(dogs = 2)" -> NodeIndexSeek("b", label("Y"), Seq(prop("dogs")), exactInt(2), Set.empty),
+    "b:Y(dogs = 2, cats = 4)" -> NodeIndexSeek("b", label("Y"), Seq(prop("dogs"), prop("cats")), CompositeQueryExpression(Seq(exactInt(2), exactInt(4))), Set.empty),
+    "b:Y(name = 'hi')" -> NodeIndexSeek("b", label("Y"), Seq(prop("name")), exactString("hi"), Set.empty),
+    "b:Y(name < 'hi')" -> NodeIndexSeek("b", label("Y"), Seq(prop("name")), lt(string("hi")), Set.empty),
+    "b:Y(name <= 'hi')" -> NodeIndexSeek("b", label("Y"), Seq(prop("name")), lte(string("hi")), Set.empty),
+    "b:Y(name > 'hi')" -> NodeIndexSeek("b", label("Y"), Seq(prop("name")), gt(string("hi")), Set.empty),
+    "b:Y(name >= 'hi')" -> NodeIndexSeek("b", label("Y"), Seq(prop("name")), gte(string("hi")), Set.empty),
+    "b:Y(name STARTS WITH 'hi')" -> NodeIndexSeek("b", label("Y"), Seq(prop("name")), startsWith("hi"), Set.empty),
+    "b:Y(name ENDS WITH 'hi')" -> NodeIndexEndsWithScan("b", label("Y"), prop("name"), string("hi"), Set.empty),
+    "b:Y(name CONTAINS 'hi')" -> NodeIndexContainsScan("b", label("Y"), prop("name"), string("hi"), Set.empty),
+    "b:Y(name)" -> NodeIndexScan("b", label("Y"), prop("name"), Set.empty)
+  )
+
+  for ((str, expectedPlan) <- testCases) {
+    test(s"should parse `$str`") {
+      IndexSeek(str) should be(expectedPlan)
+    }
+  }
+
+  // HELPERS
+
+  private def label(str:String) = LabelToken(str, LabelId(0))
+
+  private def prop(name: String) = IndexedProperty(PropertyKeyToken(name, PropertyKeyId(0)), DoNotGetValue)
+
+  private def exactInt(int: Int) = SingleQueryExpression(SignedDecimalIntegerLiteral(int.toString)(pos))
+
+  private def exactString(x: String) = SingleQueryExpression(string(x))
+
+  private def string(x: String) = StringLiteral(x)(pos)
+
+  private def lt(x: Literal) = RangeQueryExpression(InequalitySeekRangeWrapper(RangeLessThan(NonEmptyList(ExclusiveBound(x))))(pos))
+  private def lte(x: Literal) = RangeQueryExpression(InequalitySeekRangeWrapper(RangeLessThan(NonEmptyList(InclusiveBound(x))))(pos))
+  private def gt(x: Literal) = RangeQueryExpression(InequalitySeekRangeWrapper(RangeGreaterThan(NonEmptyList(ExclusiveBound(x))))(pos))
+  private def gte(x: Literal) = RangeQueryExpression(InequalitySeekRangeWrapper(RangeGreaterThan(NonEmptyList(InclusiveBound(x))))(pos))
+
+  private def startsWith(x: String) = RangeQueryExpression(PrefixSeekRangeWrapper(PrefixRange(string(x)))(pos))
+
+}
