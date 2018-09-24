0518a528d5e76a71369cfbcd94d435d0a9b2c473
Mattias Finné <mattias@neotechnology.com>
2018-09-24 11:50:39 +0200

    Consistency-checker PropertyReader detects circular property chains
    And reports those, instead of going OOM.
    
    Also made a slight optimization where previously the entire property chain
    would always be loaded when looking for a specific property, but now stops
    loading after finding the property it's looking for.

--- community/consistency-check/src/main/java/org/neo4j/consistency/report/ConsistencyReport.java
+++ community/consistency-check/src/main/java/org/neo4j/consistency/report/ConsistencyReport.java
@@ -207,6 +207,9 @@ public interface ConsistencyReport
 
         @Documented( "The first relationship group record has another node set as owner." )
         void relationshipGroupHasOtherOwner( RelationshipGroupRecord group );
+
+        @Documented( "The property chain contains circular reference." )
+        void propertyChainContainsCircularReference( PropertyRecord propertyRecord );
     }
 
     interface RelationshipConsistencyReport
