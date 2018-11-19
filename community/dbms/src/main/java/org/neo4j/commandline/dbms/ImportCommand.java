52e1f1b418d7099fb5d24914c25bb94d31b8adc2
Mattias Finné <mattias@neotechnology.com>
2018-11-19 11:10:21 +0100

    Removes explicit neo4j-admin import db existence check
    Since it's up to the importer to decide that, e.g.
    enterprise version has resumable import

--- community/dbms/src/main/java/org/neo4j/commandline/dbms/ImportCommand.java
+++ community/dbms/src/main/java/org/neo4j/commandline/dbms/ImportCommand.java
@@ -39,7 +39,6 @@ import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.helpers.Args;
 import org.neo4j.helpers.collection.MapUtil;
 import org.neo4j.kernel.configuration.Config;
-import org.neo4j.kernel.impl.util.Validators;
 
 import static org.neo4j.commandline.arguments.common.Database.ARG_DATABASE;
 import static org.neo4j.csv.reader.Configuration.DEFAULT;
@@ -240,8 +239,6 @@ public class ImportCommand implements AdminCommand
         {
             Config config =
                     loadNeo4jConfig( homeDir, configDir, database, loadAdditionalConfig( additionalConfigFile ) );
-            Validators.CONTAINS_NO_EXISTING_DATABASE
-                    .validate( config.get( GraphDatabaseSettings.database_path ) );
 
             // The "args" parameter may have been reassigned from what came into this method.
             // This can happen if there was a -f argument in it, where arguments inside that file gets loaded into it.
