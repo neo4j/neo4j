32c028a02f9ea69546f9949be8197ae296fa4a06
Olivia Ytterbrink <olivia.ytterbrink@neotechnology.com>
2018-11-29 15:53:55 +0100

    Properly pass cachedNodeProperties through DistinctPipes

--- community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/DistinctPipe.scala
+++ community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/DistinctPipe.scala
@@ -40,7 +40,9 @@ case class DistinctPipe(source: Pipe, expressions: Map[String, Expression])
     // Run the return item expressions, and replace the execution context's with their values
     val result = input.map(ctx => {
       val newMap = Eagerly.mutableMapValues(expressions, (expression: Expression) => expression(ctx, state))
-      executionContextFactory.newExecutionContext(newMap)
+      val newCtx = executionContextFactory.newExecutionContext(newMap)
+      newCtx.copyCachedFrom(ctx)
+      newCtx
     })
 
     /*
