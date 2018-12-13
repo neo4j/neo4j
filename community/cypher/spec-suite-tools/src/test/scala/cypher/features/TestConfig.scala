1ba26ff0d2d30e977015396593eb2b4b4f514e4c
Henrik Nyman <henrik.nyman@neotechnology.com>
2018-12-13 13:27:43 +0100

    Fix failing tests
    - Register a dummy owner pipe on NestedPipeExpressions
    - Update the morsel runtime "not supported" query
    - Add support on SlottedExecutionContext for copyFrom with a
    morsel context
    - Revert removal of temporary measure
    - Run scala acceptance tests also with single-threaded

--- community/cypher/spec-suite-tools/src/test/scala/cypher/features/TestConfig.scala
+++ community/cypher/spec-suite-tools/src/test/scala/cypher/features/TestConfig.scala
@@ -28,7 +28,7 @@ case object DefaultTestConfig extends TestConfig(Some("default.txt"),"")
 
 case object CostSlottedTestConfig extends TestConfig(Some("cost-slotted.txt"),"CYPHER planner=cost runtime=slotted")
 
-case object CostSlottedWithCompiledExpressionsTestConfig extends TestConfig(Some("cost-slotted-compiled-expressions.txt"),"CYPHER planner=cost runtime=slotted expressionEngine=COMPILED")
+case object CostSlottedWithCompiledExpressionsTestConfig extends TestConfig(Some("cost-slotted.txt"),"CYPHER planner=cost runtime=slotted expressionEngine=COMPILED")
 
 case object CostMorselSingleThreadedTestConfig extends TestConfig(Some("cost-morsel-single-threaded.txt"),"CYPHER planner=cost runtime=morsel debug=singlethreaded")
 
