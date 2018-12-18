babf3b96a3d1464a101d707d758fe8f780aad066
Pontus Melke <pontusmelke@gmail.com>
2018-12-18 17:33:38 +0100

    Code review fixes

--- community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/GroupingExpression.scala
+++ community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/GroupingExpression.scala
@@ -22,11 +22,39 @@ package org.neo4j.cypher.internal.runtime.interpreted
 import org.neo4j.cypher.internal.runtime.interpreted.pipes.{Pipe, QueryState}
 import org.neo4j.values.AnyValue
 
+/**
+  * Abstraction for grouping expressions used for aggregation and distinct.
+  */
 trait GroupingExpression{
-  type T <: AnyValue
+  type KeyType <: AnyValue
+
+  /**
+    * Registers the upstream pipe for all expressions, used for interacting with interpreted runtime.
+    * @param pipe the upstream pipe to register
+    */
   def registerOwningPipe(pipe: Pipe): Unit
-  def computeGroupingKey(context: ExecutionContext, state: QueryState): T
-  def getGroupingKey(context: ExecutionContext): T
+
+  /**
+    * Computes the grouping key, it will either be a single AnyValue or a SequenceValue of AnyValues
+    * @param context used for evaluating expressions
+    * @param state used for evaluating expressions
+    * @return the grouping key
+    */
+  def computeGroupingKey(context: ExecutionContext, state: QueryState): KeyType
+
+  /**
+    * Retrieves an already computed and projected key. Can be called after a call to project grouping key.
+    * @param context The context to get the values for the key
+    * @return The grouping key as read from the context
+    */
+  def getGroupingKey(context: ExecutionContext): KeyType
+
+  /**
+    * Projects a computed key to the context
+    * @param context The context where to project.
+    * @param groupingKey The computed grouping key to project
+    */
+  def project(context: ExecutionContext,  groupingKey: KeyType): Unit
+
   def isEmpty: Boolean
-  def project(context: ExecutionContext,  groupingKey: T): Unit
 }
