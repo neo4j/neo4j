06e4cfdbf06f6281bb823a981bc4b2ca20d6ec02
Anton Persson <anton.persson@neotechnology.com>
2019-02-01 17:12:11 +0100

    Backport: Removes accidental limitation of number of reltypes to 64K for high_limit format
    The format itself already supports 3B, i.e. ~16M relationship types, but the format
    was accidentally limited to 2B from outside upon its construction.
    This removes that limit and will not incur any format change.

--- enterprise/kernel/src/main/java/org/neo4j/kernel/impl/store/format/highlimit/HighLimit.java
+++ enterprise/kernel/src/main/java/org/neo4j/kernel/impl/store/format/highlimit/HighLimit.java
@@ -51,6 +51,7 @@ public class HighLimit extends BaseRecordFormats
      * Default maximum number of bits that can be used to represent id
      */
     static final int DEFAULT_MAXIMUM_BITS_PER_ID = 50;
+    static final int RELATIONSHIP_TYPE_TOKEN_MAXIMUM_ID_BITS = Byte.SIZE * 3;
 
     public static final String STORE_VERSION = StoreVersion.HIGH_LIMIT_V3_2_0.versionString();
 
@@ -102,7 +103,7 @@ public class HighLimit extends BaseRecordFormats
     @Override
     public RecordFormat<RelationshipTypeTokenRecord> relationshipTypeToken()
     {
-        return new RelationshipTypeTokenRecordFormat();
+        return new RelationshipTypeTokenRecordFormat( RELATIONSHIP_TYPE_TOKEN_MAXIMUM_ID_BITS );
     }
 
     @Override
