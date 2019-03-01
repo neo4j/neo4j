5bb915246ae01a98a5ca2947d7952042aef5a7cb
Pontus Melke <pontusmelke@gmail.com>
2019-03-01 15:27:55 +0100

    Don't create and delete store files in tests
    AutoIndexAcceptanceTest relied on deleting and creating store files
    which is a brittle operation which lead to flakyness.

---
+++ enterprise/cypher/cypher/src/test/java/org/neo4j/cypher/internal/javacompat/AutoIndexAcceptanceTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j Enterprise Edition. The included source
+ * code can be redistributed and/or modified under the terms of the
+ * GNU AFFERO GENERAL PUBLIC LICENSE Version 3
+ * (http://www.fsf.org/licensing/licenses/agpl-3.0.html) with the
+ * Commons Clause, as found in the associated LICENSE.txt file.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * Neo4j object code can be licensed independently from the source
+ * under separate terms from the AGPL. Inquiries can be directed to:
+ * licensing@neo4j.com
+ *
+ * More information is also available at:
+ * https://neo4j.com/licensing/
+ */
+package org.neo4j.cypher.internal.javacompat;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
+import org.neo4j.test.rule.DatabaseRule;
+import org.neo4j.test.rule.EnterpriseDatabaseRule;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.neo4j.helpers.collection.Iterators.asList;
+
+public class AutoIndexAcceptanceTest
+{
+    @Rule
+    public DatabaseRule db = new EnterpriseDatabaseRule();
+
+    @Test
+    public void shouldAutoIndexOnSetEvenIfValueNotChanged() throws IOException
+    {
+        // Given
+        try ( Transaction tx = db.beginTx() )
+        {
+            db.execute( "CREATE ({name:'test'}), ({name:'test2'})" );
+            tx.success();
+        }
+        db.restartDatabase(
+                GraphDatabaseSettings.node_auto_indexing.name(), "true",
+                GraphDatabaseSettings.node_keys_indexable.name(), "name" );
+
+        // When, set with same value as it had before
+        assertThat( asList( db.execute( "MATCH (p) WITH p, p.name as name SET p.name = name RETURN count(p)" ) ), hasSize(1));
+
+        // Then, we should be able to find it in the index
+        assertThat( asList( db.execute( "START i=node:node_auto_index('name:test') return i limit 1" ) ), hasSize(1));
+
+    }
+}
