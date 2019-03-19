5d042e2e625a2e287a7e324e39f327466c79f364
Pontus Melke <pontusmelke@gmail.com>
2019-03-19 15:10:02 +0100

    Add basic implementation of and expression evaluator

---
+++ community/cypher/expression-evaluator/src/main/scala/org/neo4j/cypher/internal/evaluator/SimpleInternalExpressionEvaluator.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.evaluator
+
+import org.neo4j.cypher.evaluator.EvaluationException
+import org.neo4j.cypher.internal.planner.spi.TokenContext
+import org.neo4j.cypher.internal.runtime.expressionVariableAllocation.Result
+import org.neo4j.cypher.internal.runtime.interpreted.commands.convert.{CommunityExpressionConverter, ExpressionConverters}
+import org.neo4j.cypher.internal.runtime.interpreted.pipes.QueryState
+import org.neo4j.cypher.internal.runtime.{ExecutionContext, expressionVariableAllocation}
+import org.neo4j.cypher.internal.v4_0.ast._
+import org.neo4j.cypher.internal.v4_0.expressions.{Expression, Variable}
+import org.neo4j.cypher.internal.v4_0.parser.CypherParser
+import org.neo4j.cypher.internal.v4_0.util.attribution.Id
+import org.neo4j.internal.kernel.api.IndexReadSession
+import org.neo4j.values.AnyValue
+
+class SimpleInternalExpressionEvaluator extends InternalExpressionEvaluator {
+
+  import SimpleInternalExpressionEvaluator.COLUMN
+
+  private val parser = new CypherParser()
+
+  override def evaluate(expression: String): AnyValue = {
+    val e = parseToExpression(expression)
+    val Result(rewritten, nExpressionSlots, _) = expressionVariableAllocation.allocate(e)
+    val converters = new ExpressionConverters(CommunityExpressionConverter(TokenContext.EMPTY))
+    val commandExpr = converters.toCommandExpression(Id.INVALID_ID, rewritten)
+
+    val emptyQueryState = new QueryState(null,
+                                         null,
+                                         Array.empty,
+                                         null,
+                                         Array.empty[IndexReadSession],
+                                         new Array(nExpressionSlots))
+    try {
+      commandExpr(ExecutionContext.empty, emptyQueryState)
+    }
+    catch {
+      case e: Exception => throw new EvaluationException(s"Failed to evaluate expression $expression", e)
+    }
+  }
+
+  private def parseToExpression(expression: String): Expression = {
+    val statement: Statement = parser.parse(s"RETURN $expression AS $COLUMN", None)
+    statement match {
+      case Query(_,
+                 SingleQuery(
+                 Seq(Return(_, ReturnItems(_, Seq(AliasedReturnItem(e, Variable(COLUMN)))), _, _, _, _)))) =>
+        e
+      case _ => throw new EvaluationException(s"Invalid statement $statement")
+    }
+  }
+}
+
+object SimpleInternalExpressionEvaluator {
+
+  private val COLUMN = "RESULT"
+}
---
+++ community/cypher/expression-evaluator/src/test/scala/org/neo4j/cypher/internal/evaluator/SimpleInternalExpressionEvaluatorTest.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.evaluator
+
+import java.lang.Math.{PI, sin}
+
+import org.neo4j.values.storable.CoordinateReferenceSystem.WGS84_3D
+import org.neo4j.values.storable.Values
+import org.neo4j.values.storable.Values.{intValue, pointValue, stringValue}
+import org.neo4j.values.virtual.VirtualValues.{list, map}
+import org.scalatest._
+
+class SimpleInternalExpressionEvaluatorTest extends FunSuiteLike with Matchers {
+
+  test("parse literals") {
+    val evaluator = new SimpleInternalExpressionEvaluator
+
+    evaluator.evaluate("'hello'") should equal(stringValue("hello"))
+    evaluator.evaluate("42") should equal(intValue(42))
+    evaluator.evaluate("false") should equal(Values.FALSE)
+    evaluator.evaluate("[1,'foo', true]") should equal(list(intValue(1), stringValue("foo"), Values.TRUE))
+    evaluator.evaluate("{prop1: 42}") should equal(map(Array("prop1"), Array(intValue(42))))
+  }
+
+  test("list comprehensions") {
+    val evaluator = new SimpleInternalExpressionEvaluator
+
+    evaluator.evaluate("[x IN range(0,10) WHERE x % 2 = 0 | x^3]") should equal(
+      list(intValue(0), intValue(8), intValue(64), intValue(216), intValue(512), intValue(1000)))
+  }
+
+  test("functions") {
+    val evaluator = new SimpleInternalExpressionEvaluator
+
+    evaluator.evaluate("point({ latitude: 12, longitude: 56, height: 1000 })") should
+      equal(pointValue(WGS84_3D, 56, 12, 1000))
+    evaluator.evaluate("sin(pi())") should equal(Values.doubleValue(sin(PI)))
+  }
+}
