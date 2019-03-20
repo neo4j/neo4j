29e7e635d8129fb303597385ba74cd3bcf503b2e
Andrew Kerr <andrew.kerr@neotechnology.com>
2019-03-20 15:25:08 +0000

    Update a couple of test annotations to Jupiter.
    Fixes one test class.

--- community/bolt/src/test/java/org/neo4j/bolt/transport/DefaultBoltProtocolFactoryTest.java
+++ community/bolt/src/test/java/org/neo4j/bolt/transport/DefaultBoltProtocolFactoryTest.java
@@ -20,7 +20,7 @@
 package org.neo4j.bolt.transport;
 
 import io.netty.channel.embedded.EmbeddedChannel;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
