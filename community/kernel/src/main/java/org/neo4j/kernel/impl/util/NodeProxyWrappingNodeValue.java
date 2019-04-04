ded41770746bbcf0f51121f2702644427074a6a8
Chris Vest <mr.chrisvest@gmail.com>
2019-04-04 18:47:04 +0200

    Revert the exception type change for accessing labels on node proxies that are concurrently deleted by overlapping committed transactions.

--- community/kernel/src/main/java/org/neo4j/kernel/impl/util/NodeProxyWrappingNodeValue.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/util/NodeProxyWrappingNodeValue.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.NotFoundException;
+import org.neo4j.kernel.impl.store.InvalidRecordException;
 import org.neo4j.values.AnyValueWriter;
 import org.neo4j.values.storable.TextArray;
 import org.neo4j.values.storable.Values;
@@ -63,7 +64,7 @@ public class NodeProxyWrappingNodeValue extends NodeValue
             l = Values.stringArray();
             p = VirtualValues.EMPTY_MAP;
         }
-        catch ( IllegalStateException e )
+        catch ( InvalidRecordException e )
         {
             throw new ReadAndDeleteTransactionConflictException( e );
         }
