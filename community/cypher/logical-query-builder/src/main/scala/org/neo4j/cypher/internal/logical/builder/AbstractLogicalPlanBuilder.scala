189db1cb405e1731aad909150d81d85df22fa761
Satia Herfert <satiaherfert@gmx.de>
2019-05-17 15:29:49 +0200

    Unit tests for PipelineBuilder. Matcher for ExecutionGraphDefinition.

--- community/cypher/logical-query-builder/src/main/scala/org/neo4j/cypher/internal/logical/builder/AbstractLogicalPlanBuilder.scala
+++ community/cypher/logical-query-builder/src/main/scala/org/neo4j/cypher/internal/logical/builder/AbstractLogicalPlanBuilder.scala
@@ -24,7 +24,7 @@ import org.neo4j.cypher.internal.logical.builder.AbstractLogicalPlanBuilder.pos
 import org.neo4j.cypher.internal.logical.plans._
 import org.neo4j.cypher.internal.v4_0.expressions.{Expression, LabelName, SignedDecimalIntegerLiteral, Variable}
 import org.neo4j.cypher.internal.v4_0.util.InputPosition
-import org.neo4j.cypher.internal.v4_0.util.attribution.{IdGen, SequentialIdGen}
+import org.neo4j.cypher.internal.v4_0.util.attribution.{Id, IdGen, SameId, SequentialIdGen}
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -48,9 +48,21 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
   self: IMPL =>
 
   private sealed trait OperatorBuilder
-  private case class LeafOperator(plan: LogicalPlan) extends OperatorBuilder
-  private case class UnaryOperator(planConstructor: LogicalPlan => LogicalPlan) extends OperatorBuilder
-  private case class BinaryOperator(planConstructor: (LogicalPlan, LogicalPlan) => LogicalPlan) extends OperatorBuilder
+  private case class LeafOperator(planToIdConstructor: IdGen => LogicalPlan) extends OperatorBuilder{
+    private val id = idGen.id()
+    _idOfLastPlan = id
+    val plan: LogicalPlan = planToIdConstructor(SameId(id))
+  }
+  private case class UnaryOperator(planToIdConstructor: LogicalPlan => IdGen => LogicalPlan) extends OperatorBuilder {
+    private val id = idGen.id()
+    _idOfLastPlan = id
+    val planConstructor: LogicalPlan => LogicalPlan = planToIdConstructor(_)(SameId(id))
+  }
+  private case class BinaryOperator(planToIdConstructor: (LogicalPlan, LogicalPlan) => IdGen => LogicalPlan) extends OperatorBuilder {
+    private val id = idGen.id()
+    _idOfLastPlan = id
+    val planConstructor: (LogicalPlan, LogicalPlan) => LogicalPlan = planToIdConstructor(_, _)(SameId(id))
+  }
 
   private class Tree(operator: OperatorBuilder) {
     var left: Option[Tree] = None
@@ -58,21 +70,25 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
 
     def build(): LogicalPlan = {
       operator match {
-        case LeafOperator(plan) => plan
-        case UnaryOperator(planConstructor) =>
-          planConstructor(left.get.build())
-        case BinaryOperator(planConstructor) =>
-          planConstructor(left.get.build(), right.get.build())
+        case o:LeafOperator => o.plan
+        case o:UnaryOperator =>
+          o.planConstructor(left.get.build())
+        case o:BinaryOperator =>
+          o.planConstructor(left.get.build(), right.get.build())
       }
     }
   }
 
-  implicit val idGen: IdGen = new SequentialIdGen()
+  private val idGen: IdGen = new SequentialIdGen()
 
   private var tree: Tree = _
   private val looseEnds = new ArrayBuffer[Tree]
   private var indent = 0
   protected var resultColumns: Array[String] = _
+
+  private var _idOfLastPlan = Id.INVALID_ID
+
+  protected def idOfLastPlan: Id = _idOfLastPlan
 
   /**
     * Increase indent. The indent determines where the next
@@ -87,18 +103,18 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
 
   def produceResults(vars: String*): IMPL = {
     resultColumns = vars.toArray
-    tree = new Tree(UnaryOperator(lp => ProduceResult(lp, resultColumns)))
+    tree = new Tree(UnaryOperator(lp => ProduceResult(lp, resultColumns)(_)))
     looseEnds += tree
     self
   }
 
   def optional(): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => Optional(lp, Set.empty)))
+    appendAtCurrentIndent(UnaryOperator(lp => Optional(lp, Set.empty)(_)))
     self
   }
 
   def limit(count: Int): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => Limit(lp, literalInt(count), DoNotIncludeTies)))
+    appendAtCurrentIndent(UnaryOperator(lp => Limit(lp, literalInt(count), DoNotIncludeTies)(_)))
     self
   }
 
@@ -106,7 +122,7 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
     val p = PatternParser.parse(pattern)
     p.length match {
       case SimplePatternLength =>
-        appendAtCurrentIndent(UnaryOperator(lp => Expand(lp, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandAll)))
+        appendAtCurrentIndent(UnaryOperator(lp => Expand(lp, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandAll)(_)))
       // TODO VarExpand and PruningVarExpand
     }
     self
@@ -117,7 +133,7 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
     p.length match {
       case SimplePatternLength =>
         val pred = predicate.map(ExpressionParser.parseExpression)
-        appendAtCurrentIndent(UnaryOperator(lp => OptionalExpand(lp, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandAll, pred)))
+        appendAtCurrentIndent(UnaryOperator(lp => OptionalExpand(lp, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandAll, pred)(_)))
       case _ =>
         throw new IllegalArgumentException("Cannot have optional expand with variable length pattern")
     }
@@ -129,7 +145,7 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
     p.length match {
       case SimplePatternLength =>
         val pred = predicate.map(ExpressionParser.parseExpression)
-        appendAtCurrentIndent(UnaryOperator(lp => OptionalExpand(lp, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandInto, pred)))
+        appendAtCurrentIndent(UnaryOperator(lp => OptionalExpand(lp, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandInto, pred)(_)))
       case _ =>
         throw new IllegalArgumentException("Cannot have optional expand with variable length pattern")
     }
@@ -137,53 +153,53 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
   }
 
   def partialSort(alreadySortedPrefix: Seq[ColumnOrder], stillToSortSuffix: Seq[ColumnOrder]): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => PartialSort(lp, alreadySortedPrefix, stillToSortSuffix)))
+    appendAtCurrentIndent(UnaryOperator(lp => PartialSort(lp, alreadySortedPrefix, stillToSortSuffix)(_)))
     self
   }
 
   def sort(sortItems: Seq[ColumnOrder]): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => Sort(lp, sortItems)))
+    appendAtCurrentIndent(UnaryOperator(lp => Sort(lp, sortItems)(_)))
     self
   }
 
   def top(sortItems: Seq[ColumnOrder], limit: Long): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => Top(lp, sortItems, literalInt(limit))))
+    appendAtCurrentIndent(UnaryOperator(lp => Top(lp, sortItems, literalInt(limit))(_)))
     self
   }
 
   def top(sortItems: Seq[ColumnOrder], limitVariable: String): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => Top(lp, sortItems, varFor(limitVariable))))
+    appendAtCurrentIndent(UnaryOperator(lp => Top(lp, sortItems, varFor(limitVariable))(_)))
     self
   }
 
   def partialTop(alreadySortedPrefix: Seq[ColumnOrder], stillToSortSuffix: Seq[ColumnOrder], limit: Long): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => PartialTop(lp, alreadySortedPrefix, stillToSortSuffix, literalInt(limit))))
+    appendAtCurrentIndent(UnaryOperator(lp => PartialTop(lp, alreadySortedPrefix, stillToSortSuffix, literalInt(limit))(_)))
     self
   }
 
   def partialTop(alreadySortedPrefix: Seq[ColumnOrder], stillToSortSuffix: Seq[ColumnOrder], limitVariable: String): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => PartialTop(lp, alreadySortedPrefix, stillToSortSuffix, varFor(limitVariable))))
+    appendAtCurrentIndent(UnaryOperator(lp => PartialTop(lp, alreadySortedPrefix, stillToSortSuffix, varFor(limitVariable))(_)))
     self
   }
 
   def eager(): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => Eager(lp)))
+    appendAtCurrentIndent(UnaryOperator(lp => Eager(lp)(_)))
     self
   }
 
 
   def emptyResult(): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => EmptyResult(lp)))
+    appendAtCurrentIndent(UnaryOperator(lp => EmptyResult(lp)(_)))
     self
   }
 
   def detachDeleteNode(node: String): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => DetachDeleteNode(lp, varFor(node))))
+    appendAtCurrentIndent(UnaryOperator(lp => DetachDeleteNode(lp, varFor(node))(_)))
     self
   }
 
   def deleteRel(rel: String): IMPL = {
-    appendAtCurrentIndent(UnaryOperator(lp => DeleteRelationship(lp, varFor(rel))))
+    appendAtCurrentIndent(UnaryOperator(lp => DeleteRelationship(lp, varFor(rel))(_)))
     self
   }
 
@@ -191,46 +207,46 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
     val p = PatternParser.parse(pattern)
     newNode(varFor(p.from))
     newNode(varFor(p.to))
-    appendAtCurrentIndent(UnaryOperator(lp => Expand(lp, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandInto)))
+    appendAtCurrentIndent(UnaryOperator(lp => Expand(lp, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandInto)(_)))
     self
   }
 
   def unwind(projectionString: String): IMPL = {
     val (name, expression) = ExpressionParser.parseProjections(projectionString).head
-    appendAtCurrentIndent(UnaryOperator(lp => UnwindCollection(lp, name, expression)))
+    appendAtCurrentIndent(UnaryOperator(lp => UnwindCollection(lp, name, expression)(_)))
     self
   }
 
   def projection(projectionStrings: String*): IMPL = {
     val projections = ExpressionParser.parseProjections(projectionStrings: _*)
-    appendAtCurrentIndent(UnaryOperator(lp => Projection(lp, projections)))
+    appendAtCurrentIndent(UnaryOperator(lp => Projection(lp, projections)(_)))
     self
   }
 
   def distinct(projectionStrings: String*): IMPL = {
     val projections = ExpressionParser.parseProjections(projectionStrings: _*)
-    appendAtCurrentIndent(UnaryOperator(lp => Distinct(lp, projections)))
+    appendAtCurrentIndent(UnaryOperator(lp => Distinct(lp, projections)(_)))
     self
   }
 
   def orderedDistinct(orderToLeverage: Seq[Expression], projectionStrings: String*): IMPL = {
     val projections = ExpressionParser.parseProjections(projectionStrings: _*)
-    appendAtCurrentIndent(UnaryOperator(lp => OrderedDistinct(lp, projections, orderToLeverage)))
+    appendAtCurrentIndent(UnaryOperator(lp => OrderedDistinct(lp, projections, orderToLeverage)(_)))
     self
   }
 
   def allNodeScan(node: String, args: String*): IMPL = {
     newNode(varFor(node))
-    appendAtCurrentIndent(LeafOperator(AllNodesScan(node, args.toSet)))
+    appendAtCurrentIndent(LeafOperator(AllNodesScan(node, args.toSet)(_)))
   }
 
   def argument(args: String*): IMPL = {
-    appendAtCurrentIndent(LeafOperator(Argument(args.toSet)))
+    appendAtCurrentIndent(LeafOperator(Argument(args.toSet)(_)))
   }
 
   def nodeByLabelScan(node: String, label: String, args: String*): IMPL = {
     newNode(varFor(node))
-    appendAtCurrentIndent(LeafOperator(NodeByLabelScan(node, labelName(label), args.toSet)))
+    appendAtCurrentIndent(LeafOperator(NodeByLabelScan(node, labelName(label), args.toSet)(_)))
   }
 
   def nodeIndexOperator(indexSeekString: String,
@@ -242,9 +258,12 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
                         customQueryExpression: Option[QueryExpression[Expression]] = None): IMPL = {
     val label = tokenResolver.getLabelId(IndexSeek.labelFromIndexSeekString(indexSeekString))
     val propIds = PartialFunction(tokenResolver.getPropertyKeyId)
-    val plan = IndexSeek(indexSeekString, getValue, indexOrder, paramExpr, argumentIds, Some(propIds), label, unique, customQueryExpression)
-    newNode(varFor(plan.idName))
-    appendAtCurrentIndent(LeafOperator(plan))
+    val planBuilder = (idGen: IdGen) => {
+      val plan = IndexSeek(indexSeekString, getValue, indexOrder, paramExpr, argumentIds, Some(propIds), label, unique, customQueryExpression)(idGen)
+      newNode(varFor(plan.idName))
+      plan
+    }
+    appendAtCurrentIndent(LeafOperator(planBuilder))
   }
 
   def aggregation(groupingExpressions: Seq[String],
@@ -259,28 +278,28 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
     appendAtCurrentIndent(UnaryOperator(lp => OrderedAggregation(lp,
       ExpressionParser.parseProjections(groupingExpressions: _*),
       ExpressionParser.parseProjections(aggregationExpression: _*),
-      orderToLeverage)))
+      orderToLeverage)(_)))
 
   def apply(): IMPL =
-    appendAtCurrentIndent(BinaryOperator((lhs, rhs) => Apply(lhs, rhs)))
+    appendAtCurrentIndent(BinaryOperator((lhs, rhs) => Apply(lhs, rhs)(_)))
 
   def cartesianProduct(): IMPL =
-    appendAtCurrentIndent(BinaryOperator((lhs, rhs) => CartesianProduct(lhs, rhs)))
+    appendAtCurrentIndent(BinaryOperator((lhs, rhs) => CartesianProduct(lhs, rhs)(_)))
 
   def union(): IMPL =
-    appendAtCurrentIndent(BinaryOperator((lhs, rhs) => Union(lhs, rhs)))
+    appendAtCurrentIndent(BinaryOperator((lhs, rhs) => Union(lhs, rhs)(_)))
 
   def expandAll(pattern: String): IMPL = {
     val p = PatternParser.parse(pattern)
-    appendAtCurrentIndent(UnaryOperator(source => Expand(source, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandAll)))
+    appendAtCurrentIndent(UnaryOperator(source => Expand(source, p.from, p.dir, p.relTypes, p.to, p.relName, ExpandAll)(_)))
   }
 
   def nodeHashJoin(nodes: String*): IMPL = {
-    appendAtCurrentIndent(BinaryOperator((left, right) => NodeHashJoin(nodes.toSet, left, right)))
+    appendAtCurrentIndent(BinaryOperator((left, right) => NodeHashJoin(nodes.toSet, left, right)(_)))
   }
 
   def argument(): IMPL =
-    appendAtCurrentIndent(LeafOperator(Argument()))
+    appendAtCurrentIndent(LeafOperator(Argument()(_)))
 
   def input(nodes: Seq[String] = Seq.empty, variables: Seq[String] = Seq.empty, nullable: Boolean = true): IMPL = {
     if (indent != 0) {
@@ -290,12 +309,12 @@ abstract class AbstractLogicalPlanBuilder[T, IMPL <: AbstractLogicalPlanBuilder[
       throw new IllegalArgumentException("Input must create unique variables")
     }
     nodes.foreach(node => newNode(varFor(node)))
-    appendAtCurrentIndent(LeafOperator(Input(nodes.toArray, variables.toArray, nullable)))
+    appendAtCurrentIndent(LeafOperator(Input(nodes.toArray, variables.toArray, nullable)(_)))
   }
 
   def filter(predicateStrings: String*): IMPL = {
     val predicates = predicateStrings.map(ExpressionParser.parseExpression)
-    appendAtCurrentIndent(UnaryOperator(lp => Selection(predicates, lp)))
+    appendAtCurrentIndent(UnaryOperator(lp => Selection(predicates, lp)(_)))
   }
 
   // SHIP IP
