363afb642b82c5acb82e6a874687f9c87143ae50
Mattias Finné <mattias@neotechnology.com>
2019-06-18 13:37:19 +0200

    ArrayQueueOutOfOrderSequence fixes
    It turns out that the meta values returned from the get() method
    would sometimes be values from another number.
    Tests that were supposed to be multi-threaded and stress the unit
    and expose things like these wasn't actually multi-threaded,
    so improved on that too.

--- community/kernel/src/test/java/org/neo4j/kernel/impl/store/ArrayQueueOutOfOrderSequenceTest.java
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/store/ArrayQueueOutOfOrderSequenceTest.java
@@ -21,18 +21,18 @@ package org.neo4j.kernel.impl.store;
 
 import org.junit.Test;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.LongFunction;
 
 import org.neo4j.kernel.impl.util.ArrayQueueOutOfOrderSequence;
 import org.neo4j.kernel.impl.util.OutOfOrderSequence;
+import org.neo4j.test.Race;
 
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.yield;
+import static java.lang.Integer.max;
+import static java.util.Arrays.copyOfRange;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -52,30 +52,29 @@ public class ArrayQueueOutOfOrderSequenceTest
         OutOfOrderSequence sequence = new ArrayQueueOutOfOrderSequence( 0L, 10, new long[1] );
 
         // WHEN/THEN
-        offer( sequence, 1, new long[]{1} );
+        sequence.offer( 1, new long[]{1} );
         assertGet( sequence, 1, new long[]{1} );
 
-        offer( sequence, 2, new long[]{2} );
+        sequence.offer( 2, new long[]{2} );
         assertGet( sequence, 2, new long[]{2} );
 
-        assertFalse( sequence.seen( 4, new long[]{3} ) );
         sequence.offer( 4, new long[]{3} );
         assertGet( sequence, 2, new long[]{2} );
 
-        offer( sequence, 3, new long[]{4} );
+        sequence.offer( 3, new long[]{4} );
         assertGet( sequence, 4, new long[]{3} );
 
-        offer( sequence, 5, new long[]{5} );
+        sequence.offer( 5, new long[]{5} );
         assertGet( sequence, 5, new long[]{5} );
 
         // AND WHEN/THEN
-        offer( sequence, 10, new long[]{6} );
-        offer( sequence, 11, new long[]{7} );
-        offer( sequence, 8, new long[]{8} );
-        offer( sequence, 9, new long[]{9} );
-        offer( sequence, 7, new long[]{10} );
+        sequence.offer( 10, new long[]{6} );
+        sequence.offer( 11, new long[]{7} );
+        sequence.offer( 8, new long[]{8} );
+        sequence.offer( 9, new long[]{9} );
+        sequence.offer( 7, new long[]{10} );
         assertGet( sequence, 5, new long[]{5} );
-        offer( sequence, 6, new long[]{11} );
+        sequence.offer( 6, new long[]{11} );
         assertGet( sequence, 11L, new long[]{7} );
     }
 
@@ -85,15 +84,15 @@ public class ArrayQueueOutOfOrderSequenceTest
         // GIVEN
         OutOfOrderSequence sequence = new ArrayQueueOutOfOrderSequence( 0L, 5, new long[1] );
 
-        offer( sequence, 3L, new long[]{0} );
-        offer( sequence, 2L, new long[]{1} );
-        offer( sequence, 5L, new long[]{2} );
-        offer( sequence, 4L, new long[]{3} );
+        sequence.offer( 3L, new long[]{0} );
+        sequence.offer( 2L, new long[]{1} );
+        sequence.offer( 5L, new long[]{2} );
+        sequence.offer( 4L, new long[]{3} );
 
         // WHEN offering a number that should result in extending the array
-        offer( sequence, 6L, new long[]{4} );
+        sequence.offer( 6L, new long[]{4} );
         // and WHEN offering the missing number to fill the gap
-        offer( sequence, 1L, new long[]{5} );
+        sequence.offer( 1L, new long[]{5} );
 
         // THEN the high number should be visible
         assertGet( sequence, 6L, new long[]{4} );
@@ -104,74 +103,57 @@ public class ArrayQueueOutOfOrderSequenceTest
     {
         // GIVEN
         OutOfOrderSequence sequence = new ArrayQueueOutOfOrderSequence( 0, 5, new long[1] );
-        assertTrue( offer( sequence, 1, new long[]{0} ) );
-        assertFalse( offer( sequence, 3, new long[]{0} ) );
-        assertFalse( offer( sequence, 4, new long[]{0} ) );
-        assertTrue( offer( sequence, 2, new long[]{0} ) );
-        assertFalse( offer( sequence, 6, new long[]{0} ) );
-        assertTrue( offer( sequence, 5, new long[]{0} ) );
+        assertTrue( sequence.offer( 1, new long[]{0} ) );
+        assertFalse( sequence.offer( 3, new long[]{0} ) );
+        assertFalse( sequence.offer( 4, new long[]{0} ) );
+        assertTrue( sequence.offer( 2, new long[]{0} ) );
+        assertFalse( sequence.offer( 6, new long[]{0} ) );
+        assertTrue( sequence.offer( 5, new long[]{0} ) );
         // leave out 7
-        assertFalse( offer( sequence, 8, new long[]{0} ) );
-        assertFalse( offer( sequence, 9, new long[]{0} ) );
-        assertFalse( offer( sequence, 10, new long[]{0} ) );
-        assertFalse( offer( sequence, 11, new long[]{0} ) );
+        assertFalse( sequence.offer( 8, new long[]{0} ) );
+        assertFalse( sequence.offer( 9, new long[]{0} ) );
+        assertFalse( sequence.offer( 10, new long[]{0} ) );
+        assertFalse( sequence.offer( 11, new long[]{0} ) );
         // putting 12 should need extending the backing queue array
-        assertFalse( offer( sequence, 12, new long[]{0} ) );
-        assertFalse( offer( sequence, 13, new long[]{0} ) );
-        assertFalse( offer( sequence, 14, new long[]{0} ) );
+        assertFalse( sequence.offer( 12, new long[]{0} ) );
+        assertFalse( sequence.offer( 13, new long[]{0} ) );
+        assertFalse( sequence.offer( 14, new long[]{0} ) );
 
         // WHEN finally offering nr 7
-        assertTrue( offer( sequence, 7, new long[]{0} ) );
+        assertTrue( sequence.offer( 7, new long[]{0} ) );
 
         // THEN the number should jump to 14
         assertGet( sequence, 14, new long[]{0} );
     }
 
     @Test
-    public void shouldKeepItsCoolWhenMultipleThreadsAreHammeringIt() throws Exception
+    public void shouldKeepItsCoolWhenMultipleThreadsAreHammeringIt() throws Throwable
     {
         // An interesting note is that during tests the call to sequence#offer made no difference
         // in performance, so there seems to be no visible penalty in using ArrayQueueOutOfOrderSequence.
 
         // GIVEN a sequence with intentionally low starting queue size
+        LongFunction<long[]> metaFunction = number -> new long[]{number + 2, number * 2};
         final AtomicLong numberSource = new AtomicLong();
-        final OutOfOrderSequence sequence = new ArrayQueueOutOfOrderSequence( numberSource.get(), 5, new long[1] );
-        final AtomicBoolean end = new AtomicBoolean();
-        // and a bunch of threads that will start offering numbers at the same time
-        final CountDownLatch startSignal = new CountDownLatch( 1 );
-        Thread[] threads = new Thread[1];
-        for ( int i = 0; i < threads.length; i++ )
+        final OutOfOrderSequence sequence = new ArrayQueueOutOfOrderSequence( numberSource.get(), 5, metaFunction.apply( numberSource.get() ) );
+        final Race race = new Race().withEndCondition( () -> numberSource.get() > 10_000_000 );
+        int offerThreads = max( 2, Runtime.getRuntime().availableProcessors() - 1 );
+        race.addContestants( offerThreads, () ->
         {
-            threads[i] = new Thread( () -> {
-                await( startSignal );
-                while ( !end.get() )
-                {
-                    long number = numberSource.incrementAndGet();
-                    offer( sequence, number, new long[]{number + 2} );
-                }
-            } );
-        }
-
-        // WHEN
-        for ( Thread thread : threads )
+            long number = numberSource.incrementAndGet();
+            sequence.offer( number, metaFunction.apply( number ) );
+        } );
+        Runnable verifier = () ->
         {
-            thread.start();
-        }
-        startSignal.countDown();
-        while ( numberSource.get() < 10_000_000 )
-        {
-            sleep( 1 );
-            yield();
-        }
-        end.set( true );
-        for ( Thread thread : threads )
-        {
-            thread.join();
-        }
+            long[] highest = sequence.get();
+            long[] expectedMeta = metaFunction.apply( highest[0] );
+            assertArrayEquals( expectedMeta, copyOfRange( highest, 1, highest.length ) );
+        };
+        race.addContestant( verifier );
+        race.go();
 
         // THEN
-        long lastNumber = numberSource.get();
-        assertGet( sequence, lastNumber, new long[]{lastNumber + 2} );
+        verifier.run();
     }
 
     @Test
@@ -254,14 +236,6 @@ public class ArrayQueueOutOfOrderSequenceTest
         numberWaiter.join();
     }
 
-    private boolean offer( OutOfOrderSequence sequence, long number, long[] meta )
-    {
-        assertFalse( sequence.seen( number, meta ) );
-        boolean result = sequence.offer( number, meta );
-        assertTrue( sequence.seen( number, meta ) );
-        return result;
-    }
-
     private void assertGet( OutOfOrderSequence sequence, long number, long[] meta )
     {
         long[] data = sequence.get();
@@ -269,17 +243,5 @@ public class ArrayQueueOutOfOrderSequenceTest
         expected[0] = number;
         System.arraycopy( meta, 0, expected, 1, meta.length );
         assertArrayEquals( expected, data );
-    }
-
-    private void await( CountDownLatch latch )
-    {
-        try
-        {
-            latch.await();
-        }
-        catch ( InterruptedException e )
-        {
-            throw new RuntimeException( e );
-        }
     }
 }
