3c81e8a6ef31234a3360f5a5cdaca2f2f158393b
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2019-06-25 13:52:05 +0200

    Add `disableNativeAccess` flag to be able to complete disable native access.
    Add possibility to disable native access for cases where some unexpected
    behavior is observed etc.

--- community/native/src/main/java/org/neo4j/internal/nativeimpl/NativeAccessProvider.java
+++ community/native/src/main/java/org/neo4j/internal/nativeimpl/NativeAccessProvider.java
@@ -21,8 +21,11 @@ package org.neo4j.internal.nativeimpl;
 
 import com.sun.jna.Platform;
 
+import static java.lang.Boolean.getBoolean;
+
 public class NativeAccessProvider
 {
+    private static final boolean DISABLE_NATIVE_ACCESS = getBoolean( NativeAccessProvider.class.getName() + ".disableNativeAccess" );
     private static final AccessHolder HOLDER = new AccessHolder();
 
     private NativeAccessProvider()
@@ -34,14 +37,13 @@ public class NativeAccessProvider
     {
         return HOLDER.nativeAccess;
     }
-
     private static class AccessHolder
     {
         private final NativeAccess nativeAccess;
 
         AccessHolder()
         {
-            if ( !Platform.isLinux() )
+            if ( DISABLE_NATIVE_ACCESS || !Platform.isLinux() )
             {
                 nativeAccess = new AbsentNativeAccess();
             }
