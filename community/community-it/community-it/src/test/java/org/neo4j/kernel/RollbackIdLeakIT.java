055e9fc751b97acbd092005a08dfe3480a21eff8
Mattias Finné <mattias@neotechnology.com>
2019-07-04 00:47:23 +0200

    A test to show an ID leak problem

---
+++ community/community-it/community-it/src/test/java/org/neo4j/kernel/RollbackIdLeakIT.java
@@ -0,0 +1,275 @@
+/*
+ * Copyright (c) 2002-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel;
+
+import org.eclipse.collections.api.set.primitive.MutableLongSet;
+import org.eclipse.collections.impl.set.mutable.primitive.LongHashSet;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.function.Supplier;
+
+import org.neo4j.dbms.api.DatabaseManagementService;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.io.fs.EphemeralFileSystemAbstraction;
+import org.neo4j.test.Barrier;
+import org.neo4j.test.Race;
+import org.neo4j.test.TestDatabaseManagementServiceBuilder;
+import org.neo4j.test.extension.EphemeralFileSystemExtension;
+import org.neo4j.test.extension.Inject;
+import org.neo4j.test.extension.TestDirectoryExtension;
+import org.neo4j.test.rule.TestDirectory;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
+import static org.neo4j.kernel.impl.MyRelTypes.TEST;
+import static org.neo4j.test.Race.throwing;
+
+@ExtendWith( {TestDirectoryExtension.class, EphemeralFileSystemExtension.class} )
+class RollbackIdLeakIT
+{
+    @Inject
+    private TestDirectory directory;
+
+    @Inject
+    private EphemeralFileSystemAbstraction fs;
+
+    @Test
+    void shouldNotLeakHighIdsOnRollbackAfterCleanRestart() throws IOException
+    {
+        shouldNotLeakHighIdsOnRollback( this::clean );
+    }
+
+    @Test
+    void shouldNotLeakHighIdsOnRollbackAfterCrashAndRecovery() throws IOException
+    {
+        shouldNotLeakHighIdsOnRollback( this::nonClean );
+    }
+
+    private void shouldNotLeakHighIdsOnRollback( Supplier<DbRestarter> restarterSupplier ) throws IOException
+    {
+        // given some (lower) node/relationship ids rolled back and some (higher) node/relationship ids committed
+        MutableLongSet rolledBackNodeIds = new LongHashSet();
+        MutableLongSet rolledBackRelationshipIds = new LongHashSet();
+        MutableLongSet committedNodeIds = new LongHashSet();
+        MutableLongSet committedRelationshipIds = new LongHashSet();
+        try ( DbRestarter restarter = restarterSupplier.get() )
+        {
+            {
+                GraphDatabaseService db = restarter.start();
+                Barrier.Control flowControl = new Barrier.Control();
+                Race race = new Race();
+                race.addContestant( () ->
+                {
+                    try ( Transaction tx = db.beginTx() )
+                    {
+                        Node node = db.createNode();
+                        Relationship relationship = node.createRelationshipTo( node, TEST );
+                        rolledBackNodeIds.add( node.getId() );
+                        rolledBackRelationshipIds.add( relationship.getId() );
+                        flowControl.reached();
+                        // DO NOT call tx.success()
+                    }
+                }, 1 );
+                race.addContestant( throwing( () ->
+                {
+                    flowControl.await();
+                    try ( Transaction tx = db.beginTx() )
+                    {
+                        Node node = db.createNode();
+                        Relationship relationship = node.createRelationshipTo( node, TEST );
+                        committedNodeIds.add( node.getId() );
+                        committedRelationshipIds.add( relationship.getId() );
+                        tx.success(); // this one we commit
+                    }
+                    flowControl.release();
+                } ), 1 );
+                race.goUnchecked();
+
+                // when deleting the committed nodes/relationships and restarting
+                try ( Transaction tx = db.beginTx() )
+                {
+                    committedNodeIds.forEach( nodeId -> db.getNodeById( nodeId ).delete() );
+                    committedRelationshipIds.forEach( relationshipId -> db.getRelationshipById( relationshipId ).delete() );
+                    tx.success();
+                }
+            }
+
+            // then after a restart both the ids from rolled back and committed transactions should be reused
+            MutableLongSet nodeIds = new LongHashSet();
+            nodeIds.addAll( rolledBackNodeIds );
+            nodeIds.addAll( committedNodeIds );
+            MutableLongSet relationshipIds = new LongHashSet();
+            relationshipIds.addAll( rolledBackRelationshipIds );
+            relationshipIds.addAll( committedRelationshipIds );
+            assertAllocateIds( restarter.restart(), nodeIds, relationshipIds );
+        }
+    }
+
+    @Test
+    void shouldNotLeakHighIdsOnCreateDeleteInSameTxAfterCleanRestart() throws IOException
+    {
+        shouldNotLeakHighIdsOnCreateDeleteInSameTx( this::clean );
+    }
+
+    @Test
+    void shouldNotLeakHighIdsOnCreateDeleteInSameTxAfterCrashAndRestart() throws IOException
+    {
+        shouldNotLeakHighIdsOnCreateDeleteInSameTx( this::nonClean );
+    }
+
+    private void shouldNotLeakHighIdsOnCreateDeleteInSameTx( Supplier<DbRestarter> restarterSupplier ) throws IOException
+    {
+        // given
+        MutableLongSet nodeIds = new LongHashSet();
+        MutableLongSet relationshipIds = new LongHashSet();
+        try ( DbRestarter restarter = restarterSupplier.get() )
+        {
+            GraphDatabaseService db = restarter.start();
+            Node node2;
+            Relationship relationship2;
+            try ( Transaction tx = db.beginTx() )
+            {
+                Node node1 = db.createNode();
+                Relationship relationship1 = node1.createRelationshipTo( node1, TEST );
+
+                node2 = db.createNode();
+                relationship2 = node2.createRelationshipTo( node2, TEST );
+                nodeIds.add( node2.getId() );
+                relationshipIds.add( relationship2.getId() );
+
+                // Delete first node/relationship, but keep second ones
+                node1.delete();
+                nodeIds.add( node1.getId() );
+                relationship1.delete();
+                relationshipIds.add( relationship1.getId() );
+
+                tx.success();
+            }
+
+            try ( Transaction tx = db.beginTx() )
+            {
+                node2.delete();
+                relationship2.delete();
+                tx.success();
+            }
+
+            assertAllocateIds( restarter.restart(), nodeIds, relationshipIds );
+        }
+    }
+
+    private void assertAllocateIds( GraphDatabaseService db, MutableLongSet nodeIds, MutableLongSet relationshipIds )
+    {
+        try ( Transaction tx = db.beginTx() )
+        {
+            int nodes = nodeIds.size();
+            int relationships = relationshipIds.size();
+            for ( int i = 0; i < nodes; i++ )
+            {
+                Node node = db.createNode();
+                assertTrue( nodeIds.remove( node.getId() ) );
+            }
+            for ( int i = 0; i < relationships; i++ )
+            {
+                Relationship relationship = db.createNode().createRelationshipTo( db.createNode(), TEST );
+                assertTrue( relationshipIds.remove( relationship.getId() ) );
+            }
+            tx.success();
+        }
+        assertTrue( nodeIds.isEmpty() );
+        assertTrue( relationshipIds.isEmpty() );
+    }
+
+    private DbRestarter clean()
+    {
+        return new DbRestarter()
+        {
+            private DatabaseManagementService dbms;
+
+            @Override
+            public GraphDatabaseService start()
+            {
+                dbms = new TestDatabaseManagementServiceBuilder( directory.directory() ).build();
+                return dbms.database( DEFAULT_DATABASE_NAME );
+            }
+
+            @Override
+            public GraphDatabaseService restart()
+            {
+                close();
+                return start();
+            }
+
+            @Override
+            public void close()
+            {
+                dbms.shutdown();
+            }
+        };
+    }
+
+    private DbRestarter nonClean()
+    {
+        return new DbRestarter()
+        {
+            private DatabaseManagementService dbms;
+            private EphemeralFileSystemAbstraction fsSnapshot;
+
+            @Override
+            public GraphDatabaseService start()
+            {
+                return start( fs );
+            }
+
+            @Override
+            public GraphDatabaseService restart()
+            {
+                fsSnapshot = fs.snapshot();
+                dbms.shutdown();
+                return start( fsSnapshot );
+            }
+
+            @Override
+            public void close() throws IOException
+            {
+                dbms.shutdown();
+                fsSnapshot.close();
+            }
+
+            private GraphDatabaseService start( EphemeralFileSystemAbstraction fs )
+            {
+                dbms = new TestDatabaseManagementServiceBuilder( directory.directory() ).setFileSystem( fs ).build();
+                return dbms.database( DEFAULT_DATABASE_NAME );
+            }
+        };
+    }
+
+    private interface DbRestarter extends Closeable
+    {
+        GraphDatabaseService start();
+
+        GraphDatabaseService restart() throws IOException;
+    }
+}
