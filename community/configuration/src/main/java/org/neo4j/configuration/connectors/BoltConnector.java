097fccf2ef1681996560a435725897b094d24b88
Zhen Li <zhen.li@neotechnology.com>
2019-08-08 17:15:25 +0200

    Revert bolt encryption_level config setting to use the same name as 3.5.

--- community/configuration/src/main/java/org/neo4j/configuration/connectors/BoltConnector.java
+++ community/configuration/src/main/java/org/neo4j/configuration/connectors/BoltConnector.java
@@ -53,7 +53,7 @@ public final class BoltConnector implements SettingsDeclaration
 
     @Description( "Encryption level to require this connector to use" )
     public static final Setting<EncryptionLevel> encryption_level =
-            newBuilder( "dbms.connector.bolt.encryption_level", ofEnum( EncryptionLevel.class ), DISABLED ).build();
+            newBuilder( "dbms.connector.bolt.tls_level", ofEnum( EncryptionLevel.class ), DISABLED ).build();
 
     @Description( "Address the connector should bind to" )
     public static final Setting<SocketAddress> listen_address =
