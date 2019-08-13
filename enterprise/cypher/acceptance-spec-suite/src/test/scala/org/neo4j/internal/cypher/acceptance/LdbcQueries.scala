bdb8bf95375def06c981e993fcb6a53acb05295c
Louise Söderström <louise.matematik@gmail.com>
2019-08-13 10:55:24 +0200

    Replace deprecated toInt() and rels() functions in Ldbc.

--- enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/LdbcQueries.scala
+++ enterprise/cypher/acceptance-spec-suite/src/test/scala/org/neo4j/internal/cypher/acceptance/LdbcQueries.scala
@@ -1631,7 +1631,7 @@ object LdbcQueries {
     val query = """MATCH path = allShortestPaths((person1:Person {id:{1}})-[:KNOWS*0..]-(person2:Person {id:{2}}))
                   |RETURN
                   |extract(n IN nodes(path) | n.id) AS pathNodeIds,
-                  |reduce(weight=0.0, r IN rels(path) |
+                  |reduce(weight=0.0, r IN relationships(path) |
                   |           weight +
                   |           length(()-[r]->()<-[:COMMENT_HAS_CREATOR]-(:Comment)-[:REPLY_OF_POST]->(:Post)-[:POST_HAS_CREATOR]->()-[r]->())*1.0 +
                   |           length(()<-[r]-()<-[:COMMENT_HAS_CREATOR]-(:Comment)-[:REPLY_OF_POST]->(:Post)-[:POST_HAS_CREATOR]->()<-[r]-())*1.0 +
