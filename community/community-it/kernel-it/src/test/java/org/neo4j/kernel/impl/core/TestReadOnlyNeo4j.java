729db2e0c2aae16f6876e52df736a86675e8dd9b
Anton Persson <anton.persson@neotechnology.com>
2019-09-11 14:16:53 +0200

    Fix TestReadOnlyNeo4j
    Dbms does not throw if database is unable to start, rather it writes in
    log and we verify that the log entry is there.

--- community/community-it/kernel-it/src/test/java/org/neo4j/kernel/impl/core/TestReadOnlyNeo4j.java
+++ community/community-it/kernel-it/src/test/java/org/neo4j/kernel/impl/core/TestReadOnlyNeo4j.java
@@ -36,10 +36,11 @@ import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.WriteOperationsNotAllowedException;
-import org.neo4j.internal.helpers.Exceptions;
 import org.neo4j.io.fs.EphemeralFileSystemAbstraction;
 import org.neo4j.io.fs.UncloseableDelegatingFileSystemAbstraction;
 import org.neo4j.kernel.api.index.IndexDirectoryStructure;
+import org.neo4j.logging.AssertableLogProvider;
+import org.neo4j.logging.LogProvider;
 import org.neo4j.test.DbRepresentation;
 import org.neo4j.test.TestDatabaseManagementServiceBuilder;
 import org.neo4j.test.extension.Inject;
@@ -47,9 +48,10 @@ import org.neo4j.test.extension.testdirectory.EphemeralTestDirectoryExtension;
 import org.neo4j.test.rule.TestDirectory;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.text.StringContainsInOrder.stringContainsInOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
 import static org.neo4j.graphdb.RelationshipType.withName;
 import static org.neo4j.test.mockito.matcher.Neo4jMatchers.hasProperty;
@@ -97,13 +99,15 @@ class TestReadOnlyNeo4j
         createIndex();
         deleteIndexFolder();
 
-        managementService = dbmsReadOnly();
-        final RuntimeException e = assertThrows( RuntimeException.class, () -> managementService.database( DEFAULT_DATABASE_NAME ) );
-        Throwable rootCause = Exceptions.rootCause( e );
-        assertTrue( rootCause instanceof IllegalStateException );
-        assertTrue( rootCause.getMessage().contains(
-                "Some indexes need to be rebuilt. This is not allowed in read only mode. Please start db in writable mode to rebuild indexes. Indexes " +
-                        "needing rebuild:" ) );
+        AssertableLogProvider logProvider = new AssertableLogProvider();
+        managementService = dbmsReadOnly( logProvider );
+        final GraphDatabaseService db = managementService.database( DEFAULT_DATABASE_NAME );
+        assertFalse( db.isAvailable( 1L ), "Did not expect db to start" );
+        logProvider.internalToStringMessageMatcher().assertContains( stringContainsInOrder(
+                "[neo4j] Exception occurred while starting the database. Trying to stop already started components.",
+                "Some indexes need to be rebuilt. This is not allowed in read only mode. Please start db in " +
+                        "writable mode to rebuild indexes. Indexes needing rebuild: "
+        ) );
     }
 
     @Test
@@ -141,7 +145,7 @@ class TestReadOnlyNeo4j
 
     private void createIndex()
     {
-        DatabaseManagementService managementService = new TestDatabaseManagementServiceBuilder( testDirectory.storeDir() )
+        DatabaseManagementService managementService = new TestDatabaseManagementServiceBuilder( testDirectory.homeDir() )
                 .setFileSystem( new UncloseableDelegatingFileSystemAbstraction( fs ) )
                 .impermanent()
                 .build();
@@ -188,6 +192,16 @@ class TestReadOnlyNeo4j
         DbRepresentation result = DbRepresentation.of( db );
         managementService.shutdown();
         return result;
+    }
+
+    private DatabaseManagementService dbmsReadOnly( LogProvider logProvider )
+    {
+        return new TestDatabaseManagementServiceBuilder( testDirectory.homeDir() )
+                .setFileSystem( new UncloseableDelegatingFileSystemAbstraction( fs ) )
+                .impermanent()
+                .setConfig( GraphDatabaseSettings.read_only, true )
+                .setInternalLogProvider( logProvider )
+                .build();
     }
 
     private DatabaseManagementService dbmsReadOnly()
