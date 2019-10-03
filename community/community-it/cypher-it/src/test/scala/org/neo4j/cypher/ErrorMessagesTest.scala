320111a9c93620f25ce24dc8bff8a3f50f7ad8b9
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2019-10-03 12:41:40 +0200

    Add 'USE GRAPH' to expected error message on parse error

--- community/community-it/cypher-it/src/test/scala/org/neo4j/cypher/ErrorMessagesTest.scala
+++ community/community-it/cypher-it/src/test/scala/org/neo4j/cypher/ErrorMessagesTest.scala
@@ -137,7 +137,7 @@ class ErrorMessagesTest extends ExecutionEngineFunSuite {
     expectSyntaxError(
       "match (p) where id(p) = 2 match p[:likes]->dude return dude.name",
       "Invalid input '[': expected an identifier character, whitespace, '='," +
-        " node labels, a property map, a relationship pattern, ',', USING, WHERE, FROM GRAPH," +
+        " node labels, a property map, a relationship pattern, ',', USING, WHERE, FROM GRAPH, USE GRAPH," +
         " CONSTRUCT, LOAD CSV, START, MATCH, UNWIND, MERGE, CREATE UNIQUE, CREATE, SET, DELETE," +
         " REMOVE, FOREACH, WITH, CALL, RETURN, UNION, ';' or end of input (line 1, column 34 (offset: 33))",
       33
