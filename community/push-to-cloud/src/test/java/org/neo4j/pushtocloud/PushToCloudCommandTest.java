9357f5a5b2e5ffa025ea5626e7906527367956bf
Johannes Unterstein <johannes.unterstein@neotechnology.com>
2019-10-07 12:57:57 +0200

    handle imports

--- community/push-to-cloud/src/test/java/org/neo4j/pushtocloud/PushToCloudCommandTest.java
+++ community/push-to-cloud/src/test/java/org/neo4j/pushtocloud/PushToCloudCommandTest.java
@@ -51,7 +51,14 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.neo4j.helpers.collection.Iterators.array;
-import static org.neo4j.pushtocloud.PushToCloudCommand.*;
+import static org.neo4j.pushtocloud.PushToCloudCommand.ARG_BOLT_URI;
+import static org.neo4j.pushtocloud.PushToCloudCommand.ARG_DATABASE;
+import static org.neo4j.pushtocloud.PushToCloudCommand.ARG_CONFIRMED;
+import static org.neo4j.pushtocloud.PushToCloudCommand.ARG_DUMP;
+import static org.neo4j.pushtocloud.PushToCloudCommand.ARG_DUMP_TO;
+import static org.neo4j.pushtocloud.PushToCloudCommand.ARG_PASSWORD;
+import static org.neo4j.pushtocloud.PushToCloudCommand.ARG_USERNAME;
+import static org.neo4j.pushtocloud.PushToCloudCommand.ARG_OVERWRITE;
 
 public class PushToCloudCommandTest
 {
