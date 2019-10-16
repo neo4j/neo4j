95f7de879758317847bdcbb0534e17e4b53b19f1
Pontus Melke <pontusmelke@gmail.com>
2019-10-16 14:36:25 +0200

    Code review: Check exception that is not thrown
    Maybe one day we will throw something

--- community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/planning/ExceptionTranslatingQueryContext.scala
+++ community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/planning/ExceptionTranslatingQueryContext.scala
@@ -196,7 +196,7 @@ class ExceptionTranslatingQueryContext(val inner: QueryContext) extends QueryCon
     translateException(inner.aggregateFunction(id, allowed))
 
   override def isLabelSetOnNode(label: Int, node: Long, nodeCursor: NodeCursor): Boolean =
-    inner.isLabelSetOnNode(label, node, nodeCursor)
+    translateException(inner.isLabelSetOnNode(label, node, nodeCursor))
 
   override def getRelTypeId(relType: String): Int =
     translateException(inner.getRelTypeId(relType))
