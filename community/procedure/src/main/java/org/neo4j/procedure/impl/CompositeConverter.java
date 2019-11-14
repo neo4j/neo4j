dba5f3164bde3e26af5c3ae5823acb3983da93e8
Paul Horn <paul.horn@neotechnology.com>
2019-11-14 14:40:22 +0100

    Clarify exit condition logic for composite parser for default values
    Co-Authored-By: Mats Rydberg <mats@neotechnology.com>

--- community/procedure/src/main/java/org/neo4j/procedure/impl/CompositeConverter.java
+++ community/procedure/src/main/java/org/neo4j/procedure/impl/CompositeConverter.java
@@ -19,44 +19,44 @@
  */
 package org.neo4j.procedure.impl;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.function.Function;
 
+import org.neo4j.internal.helpers.Exceptions;
 import org.neo4j.internal.kernel.api.procs.DefaultParameterValue;
 import org.neo4j.internal.kernel.api.procs.Neo4jTypes;
 
 final class CompositeConverter implements Function<String,DefaultParameterValue>
 {
     private final Neo4jTypes.AnyType upCastTo;
-    private final Iterable<Function<String,DefaultParameterValue>> functions;
+    private final Function<String,DefaultParameterValue>[] functions;
 
     @SafeVarargs
     CompositeConverter( Neo4jTypes.AnyType upCastTo, Function<String,DefaultParameterValue>... functions )
     {
         this.upCastTo = upCastTo;
-        this.functions = List.of( functions );
+        this.functions = functions;
     }
 
     @Override
     public DefaultParameterValue apply( String s )
     {
-        for ( Iterator<Function<String,DefaultParameterValue>> iterator = functions.iterator(); iterator.hasNext(); )
+        IllegalArgumentException lastException = null;
+        for ( Function<String,DefaultParameterValue> function : functions )
         {
-            Function<String,DefaultParameterValue> function = iterator.next();
             try
             {
                 return function.apply( s ).castAs( upCastTo );
             }
             catch ( IllegalArgumentException invalidConversion )
             {
-                if ( !iterator.hasNext() )
-                {
-                    throw invalidConversion;
-                }
+                lastException = Exceptions.chain( lastException, invalidConversion );
             }
         }
 
-        throw new IllegalArgumentException( String.format( "%s is not a valid default value expression", s ) );
+        // lastException will not be null when we arrive here
+        // we either return in the non-exceptional case or we have a valid
+        // non-null exception due to the contract of Exceptions#chain
+        //noinspection ConstantConditions
+        throw lastException;
     }
 }
