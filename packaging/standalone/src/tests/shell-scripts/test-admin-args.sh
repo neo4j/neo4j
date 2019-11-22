e9c742043899d8564250be88235cce62c4c78ce8
Mattias Finné <mattias@neotechnology.com>
2019-11-22 09:47:46 +0100

    Adds script tests for the HEAP_SIZE priority change

--- packaging/standalone/src/tests/shell-scripts/test-admin-args.sh
+++ packaging/standalone/src/tests/shell-scripts/test-admin-args.sh
@@ -25,6 +25,24 @@ test_expect_success "should specify heap size when given" "
   test_expect_java_arg '-Xmx666m'
 "
 
+test_expect_success "should let higher heap size env var override memory setting when both given" "
+  clear_config &&
+  set_config 'dbms.memory.heap.initial_size' '111m' neo4j.conf &&
+  set_config 'dbms.memory.heap.max_size' '222m' neo4j.conf &&
+  HEAP_SIZE=666m neo4j-home/bin/neo4j-admin backup &&
+  test_expect_java_arg '-Xmx666m'
+  test_expect_java_arg '-Xms666m'
+"
+
+test_expect_success "should let lower heap size env var override memory setting when both given" "
+  clear_config &&
+  set_config 'dbms.memory.heap.initial_size' '666m' neo4j.conf &&
+  set_config 'dbms.memory.heap.max_size' '777m' neo4j.conf &&
+  HEAP_SIZE=222m neo4j-home/bin/neo4j-admin backup &&
+  test_expect_java_arg '-Xmx222m'
+  test_expect_java_arg '-Xms222m'
+"
+
 test_expect_success "should pass parallel collector option" "
   neo4j-home/bin/neo4j-admin &&
   test_expect_java_arg '-XX:+UseParallelGC'
