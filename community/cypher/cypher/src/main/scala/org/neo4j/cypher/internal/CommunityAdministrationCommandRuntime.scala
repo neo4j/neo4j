0afb0c215ba4a2c5a9a19b1e0c786a37c87182ed
Therese Magnusson <scout.therese@gmail.com>
2019-11-26 13:44:11 +0100

    Added return back to fix system update count in statistics
    If we don't return anything we call onResultCompleted twice for
    the deleting of nodes when replacing users/roles and then gets the wrong
    statistics for system updates

--- community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/CommunityAdministrationCommandRuntime.scala
+++ community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/CommunityAdministrationCommandRuntime.scala
@@ -142,7 +142,8 @@ case class CommunityAdministrationCommandRuntime(normalExecutionEngine: Executio
     case DropUser(source, userName) => (context, parameterMapping, securityContext) =>
       if (securityContext.subject().hasUsername(userName)) throw new InvalidArgumentsException(s"Failed to delete the specified user '$userName': Deleting yourself is not allowed.")
       UpdatingSystemCommandExecutionPlan("DropUser", normalExecutionEngine,
-        """MATCH (user:User {name: $name}) DETACH DELETE user""".stripMargin,
+        """MATCH (user:User {name: $name}) DETACH DELETE user
+          |RETURN 1 AS ignore""".stripMargin,
         VirtualValues.map(Array("name"), Array(Values.stringValue(userName))),
         QueryHandler
           .handleError {
