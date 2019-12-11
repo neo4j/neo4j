fce7306a58e688f7a8a0db6e7668dbcb6adc447e
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2019-12-11 15:20:16 +0100

    Negative page cache usage counter
    Page cache usage metric is based on a number of reported page faults and evictions.
    There are known problems with the incorrectly reported number of those that can be
    provoked by different kinds of workloads and operations.
    It's not possible o fix incorrect reporting without major changes in a way how we report them.
    To be able to avoid showing negative usage, for now, we will show 0 instead.

--- community/io/src/test/java/org/neo4j/io/pagecache/tracing/DefaultPageCacheTracerTest.java
+++ community/io/src/test/java/org/neo4j/io/pagecache/tracing/DefaultPageCacheTracerTest.java
@@ -125,7 +125,7 @@ public class DefaultPageCacheTracerTest
     @Test
     void usageRatio()
     {
-        assertThat( tracer.usageRatio(), is( Double.NaN ) );
+        assertThat( tracer.usageRatio(), closeTo( 0d, 0.0001 ) );
         tracer.maxPages( 10 );
         assertThat( tracer.usageRatio(), closeTo( 0d, 0.0001 ) );
         tracer.faults( 5 );
@@ -135,6 +135,9 @@ public class DefaultPageCacheTracerTest
         assertThat( tracer.usageRatio(), closeTo( 0.5, 0.0001 ) );
         tracer.faults( 5 );
         assertThat( tracer.usageRatio(), closeTo( 1d, 0.0001 ) );
+
+        tracer.evictions( 500 );
+        assertThat( tracer.usageRatio(), closeTo( 0d, 0.0001 ) );
     }
 
     private void assertCounts( long pins, long unpins, long hits, long faults, long evictions, long evictionExceptions,
