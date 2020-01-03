ed0fbbaa740df9c02b6bb8b29fbea16f5178c760
roxling <valdemar.roxling@neotechnology.com>
2020-01-03 10:51:32 +0100

    New year, new license

--- community/cypher/util-3.4/src/main/scala/org/neo4j/cypher/internal/util/v3_4/Cardinality.scala
+++ community/cypher/util-3.4/src/main/scala/org/neo4j/cypher/internal/util/v3_4/Cardinality.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2019 "Neo4j,"
+ * Copyright (c) 2002-2020 "Neo4j,"
  * Neo4j Sweden AB [http://neo4j.com]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
