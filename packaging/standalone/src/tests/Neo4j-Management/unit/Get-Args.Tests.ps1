cc0f2ab9fccad7d2d360cec35931b6ef5c6fe942
roxling <valdemar.roxling@neotechnology.com>
2020-01-03 13:35:31 +0100

    Manual license update

--- packaging/standalone/src/tests/Neo4j-Management/unit/Get-Args.Tests.ps1
+++ packaging/standalone/src/tests/Neo4j-Management/unit/Get-Args.Tests.ps1
@@ -1,5 +1,5 @@
-# Copyright (c) 2002-2018 "Neo Technology,"
-# Network Engine for Objects in Lund AB [http://neotechnology.com]
+# Copyright (c) 2002-2020 "Neo4j,"
+# Neo4j Sweden AB [http://neo4j.com]
 #
 # This file is part of Neo4j.
 #
