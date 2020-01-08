e6b1f3c8d4521a8593488171c9fb99f4e29b28e0
Pontus Melke <pontusmelke@gmail.com>
2020-01-08 10:54:27 +0100

    Code style

--- community/kernel-api/src/main/java/org/neo4j/internal/kernel/api/helpers/RelationshipSparseSelectionCursor.java
+++ community/kernel-api/src/main/java/org/neo4j/internal/kernel/api/helpers/RelationshipSparseSelectionCursor.java
@@ -93,7 +93,6 @@ public final class RelationshipSparseSelectionCursor extends RelationshipSparseS
         cursor.properties( propertyCursor );
     }
 
-
     @Override
     public void source( NodeCursor nodeCursor )
     {
