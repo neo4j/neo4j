ba57faecd091da006e917f9870cffe218b29d607
Johan Teleman <johan.teleman@neotechnology.com>
2020-01-29 10:15:59 +0100

    Disallow unaliased return items in subquery RETURN ()
    * Disallow unaliased return items in subquery RETURN

--- community/cypher/front-end/rewriting/src/main/scala/org/neo4j/cypher/internal/rewriting/rewriters/normalizeWithAndReturnClauses.scala
+++ community/cypher/front-end/rewriting/src/main/scala/org/neo4j/cypher/internal/rewriting/rewriters/normalizeWithAndReturnClauses.scala
@@ -47,14 +47,14 @@ case class normalizeWithAndReturnClauses(cypherExceptionFactory: CypherException
   private val clauseRewriter: Clause => Clause = {
     // Only alias return items
     case clause@ProjectionClause(_, ri: ReturnItems, None, _, _, None) =>
-      val (unaliasedReturnItems, aliasedReturnItems) = partitionReturnItems(ri.items, clause.isReturn)
+      val (unaliasedReturnItems, aliasedReturnItems) = partitionReturnItems(ri.items, implicitlyAliasVariables)
       val initialReturnItems = unaliasedReturnItems ++ aliasedReturnItems
       clause.copyProjection(returnItems = ri.copy(items = initialReturnItems)(ri.position))
 
     // Alias return items and rewrite ORDER BY and WHERE
     case clause@ProjectionClause(distinct, ri: ReturnItems, orderBy, skip, limit, where) =>
       clause.verifyOrderByAggregationUse((s, i) => throw cypherExceptionFactory.syntaxException(s, i))
-      val (unaliasedReturnItems, aliasedReturnItems) = partitionReturnItems(ri.items, clause.isReturn)
+      val (unaliasedReturnItems, aliasedReturnItems) = partitionReturnItems(ri.items, implicitlyAliasVariables)
       val initialReturnItems = unaliasedReturnItems ++ aliasedReturnItems
 
       val existingAliases = aliasedReturnItems.map(i => i.expression -> i.alias.get.copyId).toMap
@@ -68,31 +68,35 @@ case class normalizeWithAndReturnClauses(cypherExceptionFactory: CypherException
       clause
   }
 
+  private def implicitlyAliasVariables(i: UnaliasedReturnItem): Option[AliasedReturnItem] =
+    if (i.alias.isDefined) {
+      Some(AliasedReturnItem(i.expression, i.alias.get.copyId)(i.position))
+    } else {
+      None
+    }
+
   /**
     * Aliases return items if possible. Return a tuple of unaliased (because impossible) and
     * aliased (because they already were aliases or we just introduced an alias for them)
     * return items.
     *
-    * @param inAReturn if `true` this will create aliases for expressions like `n.prop` (RETURN case).
-    *      Otherwise it will return this in the unaliased set (WITH case).
+    * @param aliasImplicitly UnaliasedReturnItems are rewritten to AliasedReturnItems if this function is defined.
     */
-  private def partitionReturnItems(returnItems: Seq[ReturnItem], inAReturn: Boolean): (Seq[ReturnItem], Seq[AliasedReturnItem]) =
+  private def partitionReturnItems(returnItems: Seq[ReturnItem],
+                                   aliasImplicitly: UnaliasedReturnItem => Option[AliasedReturnItem]): (Seq[ReturnItem], Seq[AliasedReturnItem]) =
     returnItems.foldLeft((Vector.empty[ReturnItem], Vector.empty[AliasedReturnItem])) {
       case ((unaliasedItems, aliasedItems), item) => item match {
         case i: AliasedReturnItem =>
           (unaliasedItems, aliasedItems :+ i)
 
-        case i if i.alias.isDefined =>
-          (unaliasedItems, aliasedItems :+ AliasedReturnItem(item.expression, item.alias.get.copyId)(item.position))
-
-        case _ if inAReturn =>
-          // Unaliased return items in RETURN are OK
-          val newPosition = item.expression.position.bumped()
-          (unaliasedItems, aliasedItems :+ AliasedReturnItem(item.expression, Variable(item.name)(newPosition))(item.position))
-
-        case _ =>
-          // Unaliased return items in WITH will be preserved so that semantic check can report them as an error
-          (unaliasedItems :+ item, aliasedItems)
+        case i: UnaliasedReturnItem =>
+          aliasImplicitly(i) match {
+            case Some(aliasedReturnItem) =>
+              (unaliasedItems, aliasedItems :+ aliasedReturnItem)
+            case None =>
+              // Unaliased return items are preserved so that semantic check can report them as errors
+              (unaliasedItems :+ item, aliasedItems)
+          }
       }
     }
 
@@ -142,6 +146,22 @@ case class normalizeWithAndReturnClauses(cypherExceptionFactory: CypherException
 
   private val instance: Rewriter = bottomUp(Rewriter.lift {
     case query@SingleQuery(clauses) =>
-      query.copy(clauses = clauses.map(clauseRewriter))(query.position)
+      val finalClause =
+        clauses.last match {
+          case r @ Return(_, ri: ReturnItems, _, _, _, _) =>
+
+            // All un-aliased return items in the final RETURN are OK
+            val aliasedReturnItems =
+              ri.items.map {
+                case i: AliasedReturnItem => i
+                case i: UnaliasedReturnItem =>
+                  val newPosition = i.expression.position.bumped()
+                  AliasedReturnItem(i.expression, Variable(i.name)(newPosition))(i.position)
+              }
+            r.copyProjection(returnItems = ri.copy(items = aliasedReturnItems)(ri.position))
+
+          case clause => clause
+      }
+      query.copy(clauses = (clauses.init :+ finalClause).map(clauseRewriter))(query.position)
   })
 }
