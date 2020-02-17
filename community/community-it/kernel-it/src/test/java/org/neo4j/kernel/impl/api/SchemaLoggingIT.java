dcc04e2393a9be9e0d049ef855596761ce0ed902
Anton Klaren <anton.klaren@neotechnology.com>
2020-02-17 15:12:10 +0100

    Try to unflake test using AssertableLogProvider

--- community/community-it/kernel-it/src/test/java/org/neo4j/kernel/impl/api/SchemaLoggingIT.java
+++ community/community-it/kernel-it/src/test/java/org/neo4j/kernel/impl/api/SchemaLoggingIT.java
@@ -19,21 +19,16 @@
  */
 package org.neo4j.kernel.impl.api;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.TimeUnit;
 
 import org.neo4j.graphdb.Transaction;
-import org.neo4j.internal.kernel.api.InternalIndexState;
-import org.neo4j.internal.schema.IndexDescriptor;
 import org.neo4j.internal.schema.IndexProviderDescriptor;
 import org.neo4j.kernel.api.index.IndexProvider;
 import org.neo4j.kernel.impl.api.index.IndexProviderMap;
-import org.neo4j.kernel.impl.api.index.IndexingService;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.logging.AssertableLogProvider;
-import org.neo4j.monitoring.Monitors;
 import org.neo4j.test.TestDatabaseManagementServiceBuilder;
 import org.neo4j.test.extension.ExtensionCallback;
 import org.neo4j.test.extension.ImpermanentDbmsExtension;
@@ -58,9 +53,6 @@ class SchemaLoggingIT
     void configure( TestDatabaseManagementServiceBuilder builder )
     {
         builder.setInternalLogProvider( logProvider );
-        Monitors monitors = new Monitors();
-        monitors.addMonitorListener( new CancelMonitor() );
-        builder.setMonitors( monitors );
     }
 
     @Test
@@ -75,12 +67,25 @@ class SchemaLoggingIT
         // then
         IndexProvider defaultProvider = indexProviderMap.getDefaultProvider();
         IndexProviderDescriptor providerDescriptor = defaultProvider.getProviderDescriptor();
-        assertThat( logProvider ).forLevel( INFO )
-                .containsMessageWithArguments( "Index population started: [%s]",
-            "Index( id=1, name='index_a908f819', type='GENERAL BTREE', schema=(:User {name}), indexProvider='" + providerDescriptor.name() + "' )" )
-                .containsMessageWithArguments( CREATION_FINISHED,
-            "Index( id=1, name='index_a908f819', type='GENERAL BTREE', schema=(:User {name}), indexProvider='" + providerDescriptor.name() + "' )",
-                        "ONLINE" );
+        String before = logProvider.serialize();
+        try
+        {
+            assertThat( logProvider ).forLevel( INFO )
+                    .containsMessageWithArguments( "Index population started: [%s]",
+                            "Index( id=1, name='index_a908f819', type='GENERAL BTREE', schema=(:User {name}), indexProvider='" + providerDescriptor.name() +
+                                    "' )" )
+                    .containsMessageWithArguments( CREATION_FINISHED,
+                            "Index( id=1, name='index_a908f819', type='GENERAL BTREE', schema=(:User {name}), indexProvider='" + providerDescriptor.name() +
+                                    "' )",
+                            "ONLINE" );
+        }
+        catch ( Throwable t )
+        {
+            System.out.println( "Before: \n" + before );
+            System.out.println( "############################\n" );
+            System.out.println( "After: \n" + logProvider.serialize() );
+            throw t;
+        }
     }
 
     private static void createIndex( GraphDatabaseAPI db, String labelName, String property )
@@ -95,15 +100,6 @@ class SchemaLoggingIT
         {
             tx.schema().awaitIndexesOnline( 1, TimeUnit.MINUTES );
             tx.commit();
-        }
-    }
-
-    private static class CancelMonitor extends IndexingService.MonitorAdapter
-    {
-        @Override
-        public void populationCancelled()
-        {
-            System.out.println( "Job was canceled" );
         }
     }
 }
