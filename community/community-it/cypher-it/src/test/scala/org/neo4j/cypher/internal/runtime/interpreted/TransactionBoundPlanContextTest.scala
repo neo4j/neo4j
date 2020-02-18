a0f19143715ac9c1b9d10e212a8973d1416ee1d1
Louise Söderström <louise.matematik@gmail.com>
2020-02-18 14:15:27 +0100

    Re-throw any errors in plan context tests.
    The result of the assertions in the tests were ignored.

--- community/community-it/cypher-it/src/test/scala/org/neo4j/cypher/internal/runtime/interpreted/TransactionBoundPlanContextTest.scala
+++ community/community-it/cypher-it/src/test/scala/org/neo4j/cypher/internal/runtime/interpreted/TransactionBoundPlanContextTest.scala
@@ -23,7 +23,7 @@ import java.util.concurrent.TimeUnit.SECONDS
 
 import org.neo4j.cypher.internal.javacompat.GraphDatabaseCypherService
 import org.neo4j.cypher.internal.planner.v3_5.spi.{IndexDescriptor, IndexLimitation, SlowContains}
-import org.neo4j.graphdb.{GraphDatabaseService, Label, RelationshipType, Transaction}
+import org.neo4j.graphdb.{GraphDatabaseService, Label, RelationshipType}
 import org.neo4j.internal.kernel.api.Transaction.Type._
 import org.neo4j.internal.kernel.api.security.LoginContext.AUTH_DISABLED
 import org.neo4j.kernel.impl.coreapi.{InternalTransaction, PropertyContainerLocker}
@@ -221,7 +221,7 @@ class TransactionBoundPlanContextTest extends CypherFunSuite {
     })
   }
 
-  def inTx(f: TransactionBoundPlanContext => Unit) = {
+  def inTx(f: TransactionBoundPlanContext => Unit): Unit = {
     val tx = graph.beginTransaction(explicit, AUTH_DISABLED)
     val transactionalContext = createTransactionContext(graph, tx)
     val planContext = TransactionBoundPlanContext(TransactionalContextWrapper(transactionalContext), devNullLogger)
@@ -231,8 +231,9 @@ class TransactionBoundPlanContextTest extends CypherFunSuite {
       transactionalContext.close(true)
       tx.success()
     } catch {
-      case _: Throwable =>
+      case t: Throwable =>
         transactionalContext.close(false)
+        throw t
     } finally {
       tx.close()
     }
