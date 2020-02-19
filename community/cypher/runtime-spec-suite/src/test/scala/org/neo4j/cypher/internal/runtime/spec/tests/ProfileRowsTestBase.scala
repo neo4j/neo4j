de299a40cd88a3ef92edeccdeaa1ddbd84f2b578
Georgiy Kargapolov <inmost-light@users.noreply.github.com>
2020-02-19 14:51:35 +0100

    Connect all nodes to always get the same number of rows from expand
    In parallel, order of an `allNodeScan` is not guaranteed, so a single
    connected node could sometimes not make it past `limit`.

--- community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/ProfileRowsTestBase.scala
+++ community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/ProfileRowsTestBase.scala
@@ -147,14 +147,15 @@ abstract class ProfileRowsTestBase[CONTEXT <: RuntimeContext](edition: Edition[C
 
   test("should profile rows with limit + expand") {
     given {
-      val nodes = nodeGraph(sizeHint * 10)
-      connect(nodes, Seq((1, 2, "REL")))
+      circleGraph(sizeHint * 10)
     }
+
+    val limitSize = sizeHint * 2L
 
     val logicalQuery = new LogicalQueryBuilder(this)
       .produceResults("x")
       .expand("(x)-->(y)")
-      .limit(sizeHint * 2)
+      .limit(limitSize)
       .allNodeScan("x")
       .build()
 
@@ -163,10 +164,10 @@ abstract class ProfileRowsTestBase[CONTEXT <: RuntimeContext](edition: Edition[C
 
     // then
     val queryProfile = runtimeResult.runtimeResult.queryProfile()
-    queryProfile.operatorProfile(0).rows() shouldBe 1 // produce results
-    queryProfile.operatorProfile(1).rows() shouldBe 1 // expand
-    queryProfile.operatorProfile(2).rows() shouldBe sizeHint * 2 // limit
-    queryProfile.operatorProfile(3).rows() should be >= (sizeHint * 2L) // all node scan
+    queryProfile.operatorProfile(0).rows() shouldBe limitSize // produce results
+    queryProfile.operatorProfile(1).rows() shouldBe limitSize // expand
+    queryProfile.operatorProfile(2).rows() shouldBe limitSize // limit
+    queryProfile.operatorProfile(3).rows() should be >= limitSize // all node scan
   }
 
   test("should profile rows with optional expand all") {
