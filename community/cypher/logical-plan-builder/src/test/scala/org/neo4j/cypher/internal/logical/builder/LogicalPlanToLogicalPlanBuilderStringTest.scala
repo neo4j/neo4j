bf43b7daa2123d1916e4c3773aafa029d735b4f9
Alex Averbuch <alex.averbuch@gmail.com>
2020-02-28 17:32:08 +0200

    expect more SemiApply tests to pass

--- community/cypher/logical-plan-builder/src/test/scala/org/neo4j/cypher/internal/logical/builder/LogicalPlanToLogicalPlanBuilderStringTest.scala
+++ community/cypher/logical-plan-builder/src/test/scala/org/neo4j/cypher/internal/logical/builder/LogicalPlanToLogicalPlanBuilderStringTest.scala
@@ -203,6 +203,14 @@ class LogicalPlanToLogicalPlanBuilderStringTest extends CypherFunSuite with Test
       .allNodeScan("x")
       .build())
 
+  testPlan("semiApply",
+    new TestPlanBuilder()
+      .produceResults("x", "y")
+      .semiApply()
+      .|.allNodeScan("y", "x")
+      .allNodeScan("x")
+      .build())
+
   testPlan("antiSemiApply",
     new TestPlanBuilder()
       .produceResults("x", "y")
