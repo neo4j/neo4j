8ec769a80d8abdf24d35436c79bd9d51fd4b5791
fickludd <johan.teleman@neotechnology.com>
2020-03-27 21:42:22 +0100

    Add complex RollUpApply tests w. limit, apply & nodeHashJoin

--- community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/RollupApplyTestBase.scala
+++ community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/RollupApplyTestBase.scala
@@ -27,6 +27,7 @@ import org.neo4j.cypher.internal.logical.plans.Ascending
 import org.neo4j.cypher.internal.runtime.spec.Edition
 import org.neo4j.cypher.internal.runtime.spec.LogicalQueryBuilder
 import org.neo4j.cypher.internal.runtime.spec.RuntimeTestSuite
+import org.neo4j.graphdb.Node
 
 import scala.collection.JavaConverters.seqAsJavaListConverter
 
@@ -137,7 +138,91 @@ abstract class RollupApplyTestBase[CONTEXT <: RuntimeContext](edition: Edition[C
     // then
     val expectedRows: Iterable[Array[_]] =
       aNodes.map(a => Array[Any](a, bNodes.asJava)) ++
-      bNodes.map(b => Array[Any](b, Collections.singletonList(null)))
+        bNodes.map(b => Array[Any](b, Collections.singletonList(null)))
+    val runtimeResult = execute(logicalQuery, runtime)
+    runtimeResult should beColumns("x", "list").withRows(expectedRows)
+  }
+
+  test("should RollUpApply under apply") {
+    val size = Math.sqrt(sizeHint).toInt
+    val (aNodes, bNodes) =
+      given {
+        bipartiteGraph(size, "A", "B", "R")
+      }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("i", "x", "list")
+      .apply()
+      .|.rollUpApply("list", "y")
+      .|.|.sort(Seq(Ascending("y"))) // to get consistent order in the produced lists
+      .|.|.expandAll("(x)-->(y)")
+      .|.|.argument("x")
+      .|.filter("id(x) % 4 = i")
+      .|.allNodeScan("x")
+      .unwind("[0,1,2,3] AS i")
+      .argument()
+      .build()
+
+    // then
+    val expectedRows: Iterable[Array[_]] =
+      aNodes.map(a => Array[Any](a.getId % 4, a, bNodes.asJava)) ++
+      bNodes.map(b => Array[Any](b.getId % 4, b, Collections.emptyList))
+    val runtimeResult = execute(logicalQuery, runtime)
+    runtimeResult should beColumns("i", "x", "list").withRows(expectedRows)
+  }
+
+  test("should RollUpApply under limit") {
+    val size = Math.sqrt(sizeHint).toInt
+    val limit = size / 2
+    val (aNodes, bNodes) =
+      given {
+        bipartiteGraph(size, "A", "B", "R")
+      }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("x", "list")
+      .top(Seq(Ascending("x")), limit)
+      .rollUpApply("list", "y")
+      .|.sort(Seq(Ascending("y"))) // to get consistent order in the produced lists
+      .|.expandAll("(x)-->(y)")
+      .|.argument()
+      .allNodeScan("x")
+      .build()
+
+    // then
+    val expectedRows: Seq[Array[_]] =
+      (
+        aNodes.map(a => Array[Any](a, bNodes.asJava)) ++
+        bNodes.map(b => Array[Any](b, Collections.emptyList))
+      ).sortBy(arr => arr(0).asInstanceOf[Node].getId).take(limit)
+
+    val runtimeResult = execute(logicalQuery, runtime)
+    runtimeResult should beColumns("x", "list").withRows(expectedRows)
+  }
+
+  test("should RollUpApply under join") {
+    val size = Math.sqrt(sizeHint).toInt
+    val (aNodes, bNodes) =
+      given {
+        bipartiteGraph(size, "A", "B", "R")
+      }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("x", "list")
+      .nodeHashJoin("x")
+      .|.rollUpApply("list", "y")
+      .|.|.sort(Seq(Ascending("y"))) // to get consistent order in the produced lists
+      .|.|.expandAll("(x)-->(y)")
+      .|.|.argument()
+      .|.allNodeScan("x")
+      .nodeByLabelScan("x", "A")
+      .build()
+
+    // then
+    val expectedRows: Iterable[Array[_]] = aNodes.map(a => Array[Any](a, bNodes.asJava))
     val runtimeResult = execute(logicalQuery, runtime)
     runtimeResult should beColumns("x", "list").withRows(expectedRows)
   }
