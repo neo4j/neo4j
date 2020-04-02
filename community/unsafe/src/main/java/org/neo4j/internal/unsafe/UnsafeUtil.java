dd46a088b1aaf21baafa5fdcd67f3af1cb3b7baf
Chris Vest <mr.chrisvest@gmail.com>
2020-04-02 17:49:00 +0200

    Fix a bug where a large aligned allocation may end up extending beyond the end of the grab area.
    This could sometimes manifest itself as a rare JVM crash with a `sysmalloc: Assertion` or `SIGSEGV` error message.
    Native access bounds checks have also been added to some UnsafeUtil methods that didn't have them, and which would have caught this bug earlier if they did.

--- community/unsafe/src/main/java/org/neo4j/internal/unsafe/UnsafeUtil.java
+++ community/unsafe/src/main/java/org/neo4j/internal/unsafe/UnsafeUtil.java
@@ -467,12 +467,12 @@ public final class UnsafeUtil
             throw new NativeMemoryAllocationRefusedError( bytes, GlobalMemoryTracker.INSTANCE.usedDirectMemory() );
         }
 
+        addAllocatedPointer( pointer, bytes );
+        GlobalMemoryTracker.INSTANCE.allocateDirect( bytes );
         if ( DIRTY_MEMORY )
         {
             setMemory( pointer, bytes, (byte) 0xA5 );
         }
-        addAllocatedPointer( pointer, bytes );
-        GlobalMemoryTracker.INSTANCE.allocateDirect( bytes );
         return pointer;
     }
 
@@ -541,7 +541,7 @@ public final class UnsafeUtil
         freeTraces[idx] = new FreeTrace( pointer, allocation, count );
     }
 
-    private static void checkAccess( long pointer, int size )
+    private static void checkAccess( long pointer, long size )
     {
         if ( CHECK_NATIVE_ACCESS && nativeAccessCheckEnabled )
         {
@@ -549,7 +549,7 @@ public final class UnsafeUtil
         }
     }
 
-    private static void doCheckAccess( long pointer, int size )
+    private static void doCheckAccess( long pointer, long size )
     {
         long boundary = pointer + size;
         Allocation allocation = lastUsedAllocation.get();
@@ -573,7 +573,7 @@ public final class UnsafeUtil
         lastUsedAllocation.set( fentry.getValue() );
     }
 
-    private static void throwBadAccess( long pointer, int size, Map.Entry<Long,Allocation> fentry,
+    private static void throwBadAccess( long pointer, long size, Map.Entry<Long,Allocation> fentry,
                                         Map.Entry<Long,Allocation> centry )
     {
         long now = System.nanoTime();
@@ -991,6 +991,7 @@ public final class UnsafeUtil
      */
     public static void setMemory( long address, long bytes, byte value )
     {
+        checkAccess( address, bytes );
         if ( 0 == (address & 1) && bytes > 64 )
         {
             unsafe.putByte( address, value );
@@ -1007,6 +1008,8 @@ public final class UnsafeUtil
      */
     public static void copyMemory( long srcAddress, long destAddress, long bytes )
     {
+        checkAccess( srcAddress, bytes );
+        checkAccess( destAddress, bytes );
         unsafe.copyMemory( srcAddress, destAddress, bytes );
     }
 
@@ -1026,6 +1029,7 @@ public final class UnsafeUtil
      */
     public static ByteBuffer newDirectByteBuffer( long addr, int cap ) throws Exception
     {
+        checkAccess( addr, cap );
         if ( directByteBufferCtor == null )
         {
             // Simulate the JNI NewDirectByteBuffer(void*, long) invocation.
@@ -1042,6 +1046,7 @@ public final class UnsafeUtil
      */
     public static void initDirectByteBuffer( Object dbb, long addr, int cap )
     {
+        checkAccess( addr, cap );
         unsafe.putInt( dbb, directByteBufferMarkOffset, -1 );
         unsafe.putInt( dbb, directByteBufferPositionOffset, 0 );
         unsafe.putInt( dbb, directByteBufferLimitOffset, cap );
@@ -1227,6 +1232,13 @@ public final class UnsafeUtil
             this.sizeInBytes = sizeInBytes;
             this.freeCounter = freeCounter;
             this.boundary = pointer + sizeInBytes;
+        }
+
+        @Override
+        public String toString()
+        {
+            return format( "Allocation[pointer=%s (%x), size=%s, boundary=%s (%x), free counter=%s]",
+                    pointer, pointer, sizeInBytes, boundary, boundary, freeCounter );
         }
     }
 
