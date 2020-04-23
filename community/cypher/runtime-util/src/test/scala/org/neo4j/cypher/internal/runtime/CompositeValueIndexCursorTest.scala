ddffc5912014a358fa0bcd87ffc1383418a65baa
Pontus Melke <pontusmelke@gmail.com>
2020-04-23 15:41:19 +0200

    code review: randomized test cases for CompositeValueIndexCursor

--- community/cypher/runtime-util/src/test/scala/org/neo4j/cypher/internal/runtime/CompositeValueIndexCursorTest.scala
+++ community/cypher/runtime-util/src/test/scala/org/neo4j/cypher/internal/runtime/CompositeValueIndexCursorTest.scala
@@ -19,6 +19,10 @@
  */
 package org.neo4j.cypher.internal.runtime
 
+import org.neo4j.cypher.internal.logical.plans.IndexOrder
+import org.neo4j.cypher.internal.logical.plans.IndexOrderAscending
+import org.neo4j.cypher.internal.logical.plans.IndexOrderDescending
+import org.neo4j.cypher.internal.logical.plans.IndexOrderNone
 import org.neo4j.cypher.internal.runtime.CompositeValueIndexCursor.ascending
 import org.neo4j.cypher.internal.runtime.CompositeValueIndexCursor.descending
 import org.neo4j.cypher.internal.runtime.CompositeValueIndexCursor.unordered
@@ -28,6 +32,7 @@ import org.neo4j.internal.kernel.api.helpers.StubNodeValueIndexCursor
 import org.neo4j.values.storable.Values
 
 import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
 class CompositeValueIndexCursorTest extends CypherFunSuite {
 
@@ -47,6 +52,18 @@ class CompositeValueIndexCursorTest extends CypherFunSuite {
     list should equal(List(10, 11, 12, 5, 11, 15))
   }
 
+  test("randomized unordered cursor") {
+    //given
+    val (cursors, totalSize) = randomCursors()
+    val cursor = unordered(cursors)
+
+    //when
+    val list = asList(cursor)
+
+    //then
+    list should have size totalSize
+  }
+
   test("should create ascending cursor") {
     //given
     val cursor = ascending(
@@ -61,6 +78,19 @@ class CompositeValueIndexCursorTest extends CypherFunSuite {
 
     //then
     list should equal(List(5, 10, 11, 11, 12, 15))
+  }
+
+  test("randomized ascending cursor") {
+    //given
+    val (cursors, totalSize) = randomCursors(IndexOrderAscending)
+    val cursor = ascending(cursors)
+
+    //when
+    val list = asList(cursor)
+
+    //then
+    list should have size totalSize
+    list shouldBe sorted
   }
 
   test("should create descending cursor") {
@@ -79,19 +109,49 @@ class CompositeValueIndexCursorTest extends CypherFunSuite {
     list should equal(List(15, 12, 11, 11, 10, 5))
   }
 
-  private def cursorFor(values: Any*): NodeValueIndexCursor = {
+  test("randomized descending cursor") {
+    //given
+    val (cursors, totalSize) = randomCursors(IndexOrderDescending)
+    val cursor = descending(cursors)
+
+    //when
+    val list = asList(cursor)
+
+    //then
+    list should have size totalSize
+    list.reverse shouldBe sorted
+  }
+
+  private def cursorFor(values: Int*): NodeValueIndexCursor = {
     val stub = new StubNodeValueIndexCursor()
       values.zipWithIndex.foreach {
-        case (v, i) => stub.withNode(i, Values.of(v))
+        case (v, i) => stub.withNode(i, Values.intValue(v))
       }
      stub
   }
 
-  private def asList(cursor: NodeValueIndexCursor): Seq[AnyRef] = {
-    val values = ArrayBuffer.empty[AnyRef]
+  private def asList(cursor: NodeValueIndexCursor): Seq[Int] = {
+    val values = ArrayBuffer.empty[Int]
     while (cursor.next()) {
-      values.append(cursor.propertyValue(0).asObject())
+      values.append(cursor.propertyValue(0).asObject().asInstanceOf[Int])
     }
     values
+  }
+
+  private def randomCursors(indexOrder: IndexOrder = IndexOrderNone) = {
+    val randomArray = new Array[NodeValueIndexCursor](Random.nextInt(1000))
+    var i = 0
+    var totalSize = 0
+    while (i < randomArray.length) {
+      val randomInts = indexOrder match {
+        case IndexOrderNone => Seq.fill(Random.nextInt(100))(Random.nextInt)
+        case IndexOrderAscending => Seq.fill(Random.nextInt(100))(Random.nextInt).sorted
+        case IndexOrderDescending => Seq.fill(Random.nextInt(100))(Random.nextInt).sorted(Ordering.Int.reverse)
+      }
+      randomArray(i) = cursorFor(randomInts:_*)
+      totalSize += randomInts.size
+      i += 1
+    }
+    (randomArray, totalSize)
   }
 }
