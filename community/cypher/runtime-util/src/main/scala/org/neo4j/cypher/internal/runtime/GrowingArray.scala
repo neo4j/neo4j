9558c471b0f7c239dda65d5b5e8f4be71428c8fd
Georgiy Kargapolov <inmost-light@users.noreply.github.com>
2020-04-29 21:47:06 +0200

    Move GrowingArray into runtime-util module

---
+++ community/cypher/runtime-util/src/main/scala/org/neo4j/cypher/internal/runtime/GrowingArray.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.runtime
+
+/**
+ * Random access data structure which grows dynamically as elements are added.
+ */
+class GrowingArray[T <: AnyRef] {
+
+  private var array: Array[AnyRef] = new Array[AnyRef](4)
+  private var highWaterMark: Int = 0
+
+  /**
+   * Set an element at a given index, and grows the underlying structure if needed.
+   */
+  def set(index: Int, t: T): Unit = {
+    ensureCapacity(index+1)
+    array(index) = t
+  }
+
+  /**
+   * Get the element at a given index.
+   */
+  def get(index: Int): T = {
+    array(index).asInstanceOf[T]
+  }
+
+  /**
+   * Get the element at a given index. If the element at that index is `null`,
+   * instead compute a new element, set it at the index, and return it.
+   *
+   * This is useful for storing resources that can be reused depending on their index.
+   */
+  def computeIfAbsent(index: Int, compute: () => T): T = {
+    ensureCapacity(index+1)
+    var t = array(index)
+    if (t == null) {
+      t = compute()
+      array(index) = t
+    }
+    t.asInstanceOf[T]
+  }
+
+  /**
+   * Apply the given function `f` once for each element.
+   *
+   * If there are gaps, `f` will be called with `null` as argument.
+   */
+  def foreach(f: T => Unit): Unit = {
+    var i = 0
+    while (i < highWaterMark) {
+      f(get(i))
+      i += 1
+    }
+  }
+
+  /**
+   * Return `true` if any element has ever been set.
+   */
+  def hasNeverSeenData: Boolean = highWaterMark == 0
+
+  private def ensureCapacity(size: Int): Unit = {
+    if (this.highWaterMark < size) {
+      this.highWaterMark = size
+    }
+
+    if (array.length < size) {
+      val temp = array
+      val newLength = math.max(array.length * 2, size)
+      array = new Array[AnyRef](newLength)
+      System.arraycopy(temp, 0, array, 0, temp.length)
+    }
+  }
+}
---
+++ community/cypher/runtime-util/src/test/scala/org/neo4j/cypher/internal/runtime/GrowingArrayTest.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.runtime
+
+import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
+
+class GrowingArrayTest extends CypherFunSuite {
+
+  test("empty") {
+    val x = new GrowingArray[String]
+    x.hasNeverSeenData shouldBe true
+    x.foreach(l => fail("There should not be any elements"))
+  }
+
+  test("set and get") {
+    val x = new GrowingArray[String]
+    x.set(0, "a")
+    x.get(0) shouldBe "a"
+
+    x.set(0, "b")
+    x.get(0) shouldBe "b"
+
+    x.set(1, "c")
+    x.get(0) shouldBe "b"
+    x.get(1) shouldBe "c"
+  }
+
+  test("set a lot") {
+    val x = new GrowingArray[String]
+
+    for (i <- 0 until 1000) {
+      x.set(i, ""+i)
+    }
+
+    x.get(265) shouldBe "265"
+    x.get(42) shouldBe "42"
+    x.get(999) shouldBe "999"
+  }
+
+  test("foreach") {
+    val x = new GrowingArray[String]
+    for (i <- 0 until 10) {
+      x.set(i, ""+i)
+    }
+
+    val builder = Seq.newBuilder[String]
+    x.foreach(str => builder += str)
+    builder shouldBe Seq("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+  }
+
+  test("hasNeverSeenData") {
+    val x = new GrowingArray[String]
+    x.hasNeverSeenData shouldBe true
+
+    x.set(0, "a")
+    x.hasNeverSeenData shouldBe false
+
+    x.set(0, null)
+    x.hasNeverSeenData shouldBe false
+  }
+
+  test("set on an large out-of-bounds index") {
+    val x = new GrowingArray[String]
+    x.set(1234, "a")
+    x.get(1234) shouldBe "a"
+  }
+}
