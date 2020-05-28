1732027b692cffb055e88bd1a54a81ba7497ecd5
MishaDemianenko <mikhaylo.demianenko@neotechnology.com>
2020-05-28 17:35:56 +0200

    Switch default record format for new databases to be aligned by default.
    Existing databases will continue to use the format that they use to have.
    Configured formats will take precedence as it was before. Only new database formats are affected.

--- community/record-storage-engine/src/main/java/org/neo4j/kernel/impl/store/format/RecordFormatSelector.java
+++ community/record-storage-engine/src/main/java/org/neo4j/kernel/impl/store/format/RecordFormatSelector.java
@@ -37,6 +37,7 @@ import org.neo4j.io.layout.DatabaseLayout;
 import org.neo4j.io.pagecache.PageCache;
 import org.neo4j.io.pagecache.tracing.PageCacheTracer;
 import org.neo4j.kernel.impl.store.MetaDataStore;
+import org.neo4j.kernel.impl.store.format.aligned.PageAlignedV4_1;
 import org.neo4j.kernel.impl.store.format.standard.MetaDataRecordFormat;
 import org.neo4j.kernel.impl.store.format.standard.Standard;
 import org.neo4j.kernel.impl.store.format.standard.StandardV3_4;
@@ -63,11 +64,12 @@ import static org.neo4j.kernel.impl.store.format.FormatFamily.isSameFamily;
 public class RecordFormatSelector
 {
     private static final String STORE_SELECTION_TAG = "storeSelection";
-    private static final RecordFormats DEFAULT_FORMAT = Standard.LATEST_RECORD_FORMATS;
+    private static final RecordFormats DEFAULT_FORMAT = PageAlignedV4_1.RECORD_FORMATS;
 
     private static final List<RecordFormats> KNOWN_FORMATS = asList(
             StandardV3_4.RECORD_FORMATS,
-            StandardV4_0.RECORD_FORMATS
+            StandardV4_0.RECORD_FORMATS,
+            PageAlignedV4_1.RECORD_FORMATS
     );
 
     private RecordFormatSelector()
