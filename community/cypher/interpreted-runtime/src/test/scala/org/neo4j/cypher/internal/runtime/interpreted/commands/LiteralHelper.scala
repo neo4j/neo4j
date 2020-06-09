df28e991a2cac9a6647157842ba26e504aa2affa
Pontus Melke <pontusmelke@gmail.com>
2020-06-09 08:32:23 +0200

    Minor test fixups

--- community/cypher/interpreted-runtime/src/test/scala/org/neo4j/cypher/internal/runtime/interpreted/commands/LiteralHelper.scala
+++ community/cypher/interpreted-runtime/src/test/scala/org/neo4j/cypher/internal/runtime/interpreted/commands/LiteralHelper.scala
@@ -25,7 +25,11 @@ import org.neo4j.values.virtual.MapValueBuilder
 import org.neo4j.values.virtual.VirtualValues
 
 object LiteralHelper {
-  def literal(any: Any): Literal = Literal(ValueUtils.of(any))
+  def literal(any: Any): Literal = any match {
+    case seq: Seq[_] => literal(seq)
+    case map: Map[_, _] => literal(map.asInstanceOf[Map[String,_]])
+    case _ => Literal (ValueUtils.of (any) )
+  }
   def literal(anys: Seq[_]): Literal = Literal(VirtualValues.list(anys.map(ValueUtils.of):_*))
   def literal(map: Map[String, _]): Literal = {
     val mapBuilder = new MapValueBuilder()
