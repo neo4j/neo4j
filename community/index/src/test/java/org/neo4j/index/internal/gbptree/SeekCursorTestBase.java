bf7b8c012923c5f8b3c281e6d6c717180ebfc860
Anton Persson <anton.persson@neotechnology.com>
2020-06-12 13:17:20 +0200

    Remove @Disabled from SeekCursorTestBase.shouldCatchupRootWhenNodeHasTooNewGenerationWhileTraversingDownTree
    This was originally added because CI would hang on this test.
    Let's see if that has been fixed.

--- community/index/src/test/java/org/neo4j/index/internal/gbptree/SeekCursorTestBase.java
+++ community/index/src/test/java/org/neo4j/index/internal/gbptree/SeekCursorTestBase.java
@@ -21,7 +21,6 @@ package org.neo4j.index.internal.gbptree;
 
 import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.ResourceLock;
@@ -1960,7 +1959,6 @@ abstract class SeekCursorTestBase<KEY, VALUE>
     }
 
     @Test
-    @Disabled
     void shouldCatchupRootWhenNodeHasTooNewGenerationWhileTraversingDownTree() throws Exception
     {
         // given
