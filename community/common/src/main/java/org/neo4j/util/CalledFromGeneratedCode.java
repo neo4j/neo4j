735ef3c9ec4112d3d623b0130483faab0579863a
Pontus Melke <pontusmelke@gmail.com>
2020-08-03 11:25:23 +0200

    Handle procedure call from fused code

--- community/common/src/main/java/org/neo4j/util/CalledFromGeneratedCode.java
+++ community/common/src/main/java/org/neo4j/util/CalledFromGeneratedCode.java
@@ -27,7 +27,7 @@ import java.lang.annotation.Target;
 /**
  * Use this annotation to signal that a method that would otherwise appear unused is actually called from generated code.
  */
-@Target( ElementType.METHOD )
+@Target( {ElementType.METHOD, ElementType.FIELD} )
 @Retention( RetentionPolicy.SOURCE )
 public @interface CalledFromGeneratedCode
 {
