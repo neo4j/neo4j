1b05a4c9d102ce739f6d99401913c588ccd660c3
gjmwoods <42248895+gjmwoods@users.noreply.github.com>
2020-08-14 15:25:57 +0100

    Fixes consistently failing RejectTransportEncryptionIT due to the latest Zulu JDK 8 patch appearing to change the exception message which the test asserted on. This change makes the assertion more flexible. ()

--- community/community-it/bolt-it/src/test/java/org/neo4j/bolt/v1/transport/integration/RejectTransportEncryptionIT.java
+++ community/community-it/bolt-it/src/test/java/org/neo4j/bolt/v1/transport/integration/RejectTransportEncryptionIT.java
@@ -19,6 +19,7 @@
  */
 package org.neo4j.bolt.v1.transport.integration;
 
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -38,8 +39,8 @@ import org.neo4j.function.Factory;
 import org.neo4j.kernel.configuration.BoltConnector;
 
 import static java.util.Arrays.asList;
-import static org.apache.commons.lang3.JavaVersion.JAVA_9;
-import static org.apache.commons.lang3.SystemUtils.isJavaVersionAtLeast;
+import static org.hamcrest.core.AnyOf.anyOf;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.neo4j.bolt.v1.transport.integration.Neo4jWithSocket.DEFAULT_CONNECTOR_KEY;
 import static org.neo4j.kernel.configuration.BoltConnector.EncryptionLevel.DISABLED;
 
@@ -54,13 +55,13 @@ public class RejectTransportEncryptionIT
                 settings.put( new BoltConnector( DEFAULT_CONNECTOR_KEY ).encryption_level.name(), DISABLED.name() );
             } );
     @Rule
-    public ExpectedException exception = ExpectedException.none();
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Parameterized.Parameter( 0 )
     public Factory<TransportConnection> cf;
 
     @Parameterized.Parameter( 1 )
-    public Exception expected;
+    public Matcher<String> exceptionMessageMatcher;
 
     private TransportConnection client;
     private TransportTestUtil util;
@@ -71,13 +72,12 @@ public class RejectTransportEncryptionIT
         return asList(
                 new Object[]{
                         (Factory<TransportConnection>) SecureWebSocketConnection::new,
-                        new IOException( "Failed to connect to the server within 10 seconds" )
+                        containsString( "Failed to connect to the server within 10 seconds" )
                 },
                 new Object[]{
-                        (Factory<TransportConnection>) SecureSocketConnection::new, new IOException(
-                        isJavaVersionAtLeast( JAVA_9 ) ? "Remote host terminated the handshake"
-                                                       : "Remote host closed connection during handshake" )
-
+                        (Factory<TransportConnection>) SecureSocketConnection::new,
+                        anyOf( containsString( "Remote host terminated the handshake" ),
+                               containsString( "Remote host closed connection during handshake" ) )
                 } );
     }
 
@@ -100,8 +100,8 @@ public class RejectTransportEncryptionIT
     @Test
     public void shouldRejectConnectionAfterHandshake() throws Throwable
     {
-        exception.expect( expected.getClass() );
-        exception.expectMessage( expected.getMessage() );
+        expectedException.expect( IOException.class );
+        expectedException.expectMessage( exceptionMessageMatcher );
         client.connect( server.lookupDefaultConnector() ).send( util.acceptedVersions( 1, 0, 0, 0 ) );
     }
 }
