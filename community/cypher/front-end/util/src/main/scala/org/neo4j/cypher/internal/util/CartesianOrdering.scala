989cbd46c7d4711b751abbc453dbbef8d6f708f9
Satia Herfert <satiaherfert@gmx.de>
2020-09-08 10:53:13 +0200

    Order CartesianProducts by Cost and Cardinality instead of only Cost.

---
+++ community/cypher/front-end/util/src/main/scala/org/neo4j/cypher/internal/util/CartesianOrdering.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.cypher.internal.util
+
+/**
+ * This can be used to determine in which order to align plans with Cartesian Products.
+ * The math is coincidentally the same as in [[PredicateOrdering]].
+ */
+object CartesianOrdering extends Ordering[(Cost, Cardinality)] {
+  override def compare(side0: (Cost, Cardinality), side1: (Cost, Cardinality)): Int = {
+    val side0Xside1 = costFor(side0, side1)
+    val side1Xside0 = costFor(side1, side0)
+    if (side0Xside1 > side1Xside0) {
+      1
+    } else if (side0Xside1 < side1Xside0) {
+      -1
+    } else {
+      0
+    }
+  }
+
+  /**
+   * The cost for CartesianProduct(side0, side1)
+   */
+  private def costFor(side0: (Cost, Cardinality), side1: (Cost, Cardinality)): Cost = (side0, side1) match {
+    case ((cost0, Cardinality(card0)), (cost1, _)) =>
+      // side0 needs to be executed once. side1 is executed card0 times.
+      cost0 + cost1 * card0
+  }
+}
