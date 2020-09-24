43f6dbb4381d63a0b57b3c0976e44259b0d134a8
linneaandersson <Linneamaria10@gmail.com>
2020-09-24 11:26:09 +0200

    Improve memory estimation for aggregation function percentile

--- community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/aggregation/PercentileFunction.scala
+++ community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/pipes/aggregation/PercentileFunction.scala
@@ -19,6 +19,8 @@
  */
 package org.neo4j.cypher.internal.runtime.interpreted.pipes.aggregation
 
+import org.neo4j.collection.trackable.HeapTrackingArrayList
+import org.neo4j.collection.trackable.HeapTrackingCollections
 import org.neo4j.cypher.internal.runtime.ReadableRow
 import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.Expression
 import org.neo4j.cypher.internal.runtime.interpreted.commands.expressions.NumericHelper
@@ -26,14 +28,16 @@ import org.neo4j.cypher.internal.runtime.interpreted.pipes.QueryState
 import org.neo4j.exceptions.InvalidArgumentException
 import org.neo4j.memory.MemoryTracker
 import org.neo4j.values.AnyValue
+import org.neo4j.values.storable.NumberValue
 import org.neo4j.values.storable.Values
 
 abstract class PercentileFunction(val value: Expression, val percentile: Expression, memoryTracker: MemoryTracker) extends AggregationFunction
                                                                                      with NumericExpressionOnly {
 
-  protected var temp: Vector[AnyValue] = Vector[AnyValue]() // TODO: Use heap tracking collection
+  protected var temp: HeapTrackingArrayList[NumberValue] = HeapTrackingCollections.newArrayList[NumberValue](memoryTracker)
   protected var count: Int = 0
   protected var perc: Double = 0
+  protected var estimatedNumberValue: Long = -1
 
   override def apply(data: ReadableRow, state: QueryState) {
     actOnNumber(value(data, state), number => {
@@ -44,8 +48,11 @@ abstract class PercentileFunction(val value: Expression, val percentile: Express
             s"Invalid input '$perc' is not a valid argument, must be a number in the range 0.0 to 1.0")
       }
       count += 1
-      temp = temp :+ number
-      memoryTracker.allocateHeap(number.estimatedHeapUsage())
+      temp.add(number)
+      if(estimatedNumberValue == -1) {
+        estimatedNumberValue = number.estimatedHeapUsage();
+      }
+      memoryTracker.allocateHeap(estimatedNumberValue)
     })
   }
 }
@@ -56,20 +63,24 @@ class PercentileContFunction(value: Expression, percentile: Expression, memoryTr
   def name = "PERCENTILE_CONT"
 
   override def result(state: QueryState): AnyValue = {
-    temp = temp.sortBy((num: AnyValue) => NumericHelper.asDouble(num).doubleValue())
+    temp.sort((o1: NumberValue, o2: NumberValue) => java.lang.Double.compare(o1.doubleValue(), o2.doubleValue()))
 
-    if (perc == 1.0 || count == 1) {
-      temp.last
+    val result = if (perc == 1.0 || count == 1) {
+      temp.get(0)
     } else if (count > 1) {
       val floatIdx = perc * (count - 1)
       val floor = floatIdx.toInt
       val ceil = math.ceil(floatIdx).toInt
-      if (ceil == floor || floor == count - 1) temp(floor)
-      else Values.doubleValue(NumericHelper.asDouble(temp(floor)).doubleValue() * (ceil - floatIdx) +
-        NumericHelper.asDouble(temp(ceil)).doubleValue() * (floatIdx - floor))
+      if (ceil == floor || floor == count - 1) temp.get(floor)
+      else Values.doubleValue(NumericHelper.asDouble(temp.get(floor)).doubleValue() * (ceil - floatIdx) +
+        NumericHelper.asDouble(temp.get(ceil)).doubleValue() * (floatIdx - floor))
     } else {
       Values.NO_VALUE
     }
+
+    memoryTracker.releaseHeap(count*estimatedNumberValue)
+    temp.close()
+    result
   }
 }
 
@@ -79,18 +90,22 @@ class PercentileDiscFunction(value: Expression, percentile: Expression, memoryTr
   def name = "PERCENTILE_DISC"
 
   override def result(state: QueryState): AnyValue = {
-    temp = temp.sortBy((num: AnyValue) => NumericHelper.asDouble(num).doubleValue())
+    temp.sort((o1: NumberValue, o2: NumberValue) => java.lang.Double.compare(o1.doubleValue(), o2.doubleValue()))
 
-    if (perc == 1.0 || count == 1) {
-      temp.last
+    val result = if (perc == 1.0 || count == 1) {
+      temp.get(0);
     } else if (count > 1) {
       val floatIdx = perc * count
       var idx = floatIdx.toInt
       idx = if (floatIdx != idx || idx == 0) idx
       else idx - 1
-      temp(idx)
+      temp.get(idx)
     } else {
       Values.NO_VALUE
     }
+
+    memoryTracker.releaseHeap(count*estimatedNumberValue)
+    temp.close()
+    result
   }
 }
