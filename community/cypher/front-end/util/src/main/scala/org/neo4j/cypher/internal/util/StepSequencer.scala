ebabcdaaf5da1227e5d055800cd78af82fa86212
Satia Herfert <satiaherfert@gmx.de>
2020-10-02 11:35:31 +0200

    Add a new StepSequencer
    This StepSequencer is able to order any steps,
    as opposed to just RewriterSteps.
    It does not just apply the given order, it orders
    steps automatically given their pre-, post- and
    disabled conditions using an extension of Khan's
    algorithm for topological sorting.

---
+++ community/cypher/front-end/util/src/main/scala/org/neo4j/cypher/internal/util/StepSequencer.scala
@@ -0,0 +1,318 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.cypher.internal.util
+
+import org.neo4j.cypher.internal.util.StepSequencer.Condition
+import org.neo4j.cypher.internal.util.StepSequencer.MutableDirectedGraph
+import org.neo4j.cypher.internal.util.StepSequencer.Step
+import org.neo4j.cypher.internal.util.StepSequencer.StepAccumulator
+
+import scala.collection.immutable.Set
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
+/**
+ * This class allows to order steps with dependencies and combine them given a [[StepAccumulator]]
+ *
+ * @param stepAccumulator
+ * @tparam S
+ * @tparam ACC
+ */
+case class StepSequencer[S <: Step, ACC](stepAccumulator: StepAccumulator[S, ACC]) {
+  /**
+   * Order a set of steps and combine them using the classes stepAccumulator.
+   *
+   * The order guarantees that each step is run at least once,
+   * and that all post-conditions of any steps are given at the end of the step sequence.
+   * This means that some steps can appear more than once in the step sequence.
+   */
+  def orderSteps(steps: Set[S]): ACC = {
+    // For each post-condition, find the step that introduces it
+    val introducingSteps: Map[Condition, S] = {
+      val is = for {
+        step <- steps.toSeq
+        _ = { if (step.postConditions.isEmpty) throw new IllegalArgumentException(s"Step $step has no post-conditions. That is not allowed.") }
+        postCondition <- step.postConditions
+      } yield postCondition -> step
+
+      if (is.map(_._1).distinct != is.map(_._1)) {
+        throw new IllegalArgumentException("It is not allowed for multiple steps to have the same post-conditions.")
+      }
+
+      is.toMap
+    }
+
+    // To implement a safety check for circular disabling steps, we create a graph
+    // where each step points at the steps it invalidates.
+    // We then try a topological sort of that graph, which will fail if there are circles.
+    val graphWithDisabling = new MutableDirectedGraph[S]
+    steps.foreach(graphWithDisabling.add)
+    steps.foreach { step =>
+      // (a)-->(b) means a invalidates b's work
+      step.invalidatedConditions.foreach { condition => graphWithDisabling.connect(step, introducingSteps(condition)) }
+    }
+    StepSequencer.topologicalSort(graphWithDisabling, None, (_: S, _: mutable.Set[S]) => ())
+
+
+    // Assemble a dependency graph for the actual ordering.
+    val graph = new MutableDirectedGraph[S]
+    steps.foreach(graph.add)
+    steps.foreach { step =>
+      // (a)-->(b) means a needs to happen before b
+      step.preConditions.foreach { condition => graph.connect(introducingSteps(condition), step) }
+    }
+
+    // Sort steps topologically
+    val sortedSteps = StepSequencer.sort(graph, introducingSteps, steps.toSeq)
+
+    // Put steps together
+    sortedSteps.foldLeft(stepAccumulator.empty)(stepAccumulator.addNext)
+  }
+}
+
+object StepSequencer {
+
+  trait Condition
+
+  trait Step {
+    /**
+     * @return the conditions that needs to be met before this step can be allowed to run.
+     */
+    def preConditions: Set[Condition]
+
+    /**
+     * @return the conditions that are guaranteed to be met after this step has run.
+     *         Must not be empty, and must not contain any elements that are postConditions of other steps.
+     */
+    def postConditions: Set[Condition]
+
+    /**
+     * @return the conditions that this step invalidates as a side-effect of its work.
+     */
+    def invalidatedConditions: Set[Condition]
+  }
+
+  /**
+   * This is used to accumulate the final sequence of steps with a foldLeft.
+   * @tparam S the Step type
+   * @tparam ACC the accumulator type
+   */
+  trait StepAccumulator[S <: Step, ACC] {
+    def empty: ACC
+    def addNext(acc: ACC, step: S): ACC
+  }
+
+  private case class AdjacencyList[S](outgoing: mutable.Set[S], incoming: mutable.Set[S])
+
+  private object MutableDirectedGraph {
+    def copyOf[S](other: MutableDirectedGraph[S]): MutableDirectedGraph[S] = {
+      val res = new MutableDirectedGraph[S]
+      other.allNodes.foreach(res.add)
+
+      other.allNodes.foreach { node =>
+        other.outgoing(node).foreach { neighbor => res.connect(node, neighbor) }
+      }
+
+      res
+    }
+  }
+
+  /**
+   * A mutable directed graph implemented with AdjacencyLists.
+   */
+  private class MutableDirectedGraph[S] {
+    private val elems = mutable.Map.empty[S, AdjacencyList[S]]
+
+    /**
+     * Add a node to the graph.
+     */
+    def add(elem: S): Unit = elems += elem -> AdjacencyList(mutable.Set.empty[S], mutable.Set.empty[S])
+
+    /**
+     * Add an edge to the graph. Both `from` and `to` must have been added to the graph previously.
+     */
+    def connect(from: S, to: S): Unit = {
+      elems(from).outgoing += to
+      elems(to).incoming += from
+    }
+
+    /**
+     * Remove an edge from the graph.
+     */
+    def disconnect(from: S, to: S): Unit = {
+      elems(from).outgoing -= to
+      elems(to).incoming -= from
+    }
+
+    /**
+     * @return all nodes in the graph
+     */
+    def allNodes: Set[S] = elems.keySet.toSet
+
+    /**
+     * @param from the origin node
+     * @return all neighbors of `from` going via outgoing edges.
+     */
+    def outgoing(from: S): Set[S] = elems(from).outgoing.toSet
+
+    /**
+     * @param to the target node
+     * @return all neighbors of `to` going via incoming edges.
+     */
+    def incoming(to: S): Set[S] = elems(to).incoming.toSet
+  }
+
+  /**
+   * Instead of picking a random Step when the topological Sort offers multiple options,
+   * we define this heuristic that will lead to better results given that steps can undo work of other steps.
+   *
+   * The heuristic is to order steps that invalidate many conditions first and
+   * steps that have their conditions invalidated often last.
+   * These two criteria are weighted equally.
+   */
+  private def heuristicStepOrdering[S <: Step](numberOfTimesEachStepIsinvalidated: Map[S, Int], allSteps: Seq[S]): Ordering[S] = {
+    // Putting the steps in a random order will help us discover dependencies we didn't know about.
+    // If a query is suddenly failing because the order of steps changed, it is likely that there is a dependency we didn't capture.
+    val allStepsInRandomOrder = Random.shuffle(allSteps)
+    (x, y) => {
+      val diffInvalidating = y.invalidatedConditions.size - x.invalidatedConditions.size
+      val diffInvalidated = numberOfTimesEachStepIsinvalidated(x) - numberOfTimesEachStepIsinvalidated(y)
+
+      val diff = diffInvalidating + diffInvalidated
+
+      if (diff != 0) {
+        diff
+      } else {
+        // We need to define a total ordering for SortedSets
+        allStepsInRandomOrder.indexOf(x) - allStepsInRandomOrder.indexOf(y)
+      }
+    }
+  }
+
+  /**
+   * Implements topological sort using Kahn's algorithm.
+   * In addition, whenever a step is added that undoes work of other steps,
+   * these steps are re-inserted to be run again.
+   *
+   * @param graph            the dependency graph.
+   * @param introducingSteps Map from condition to the step that introduces it.
+   * @param allSteps         all steps
+   */
+  private def sort[S <: Step](graph: MutableDirectedGraph[S], introducingSteps: Map[Condition, S], allSteps: Seq[S]): Seq[S] = {
+    val allConditions = introducingSteps.keySet
+
+    val numberOfTimesEachStepIsInvalidated = allSteps
+      .flatMap(_.invalidatedConditions.map(introducingSteps))
+      .groupBy(identity)
+      .mapValues(_.size)
+      .withDefaultValue(0)
+    val order = heuristicStepOrdering(numberOfTimesEachStepIsInvalidated, allSteps)
+
+    // We need to be able to look at the original state, so we make a copy in the beginning
+    val workingGraph = MutableDirectedGraph.copyOf(graph)
+    // During the algorithm keep track of all conditions currently enabled
+    val currentConditions = mutable.Set.empty[Condition]
+
+    def dealWithInvalidatedConditions(nextStep: S, startPoints: mutable.Set[S]): Unit = {
+      currentConditions ++= nextStep.postConditions
+      currentConditions --= nextStep.invalidatedConditions
+      if (nextStep.invalidatedConditions.nonEmpty) {
+        // We need to reinsert parts of the graph for the work that n is undoing.
+        val cannotStartFrom = mutable.Set.empty[S]
+        val stepsThatHaveTheirWorkUndone = nextStep.invalidatedConditions.map(introducingSteps)
+        for (r <- stepsThatHaveTheirWorkUndone) {
+          // Go through the original outgoing edges of r and restore them
+          graph.outgoing(r)
+            .filterNot(step => step.postConditions.subsetOf(currentConditions)) // All things which either haven't run or need to re-run
+            .foreach { rDep =>
+              workingGraph.connect(r, rDep)
+              // make sure to remove all dependencies of r from startPoints, since they now have incoming edges again
+              cannotStartFrom += rDep
+            }
+        }
+        // re-insert rs into the working graph for re-processing
+        startPoints ++= stepsThatHaveTheirWorkUndone
+        startPoints --= cannotStartFrom
+      }
+    }
+
+    val result = topologicalSort(workingGraph, Some(order), dealWithInvalidatedConditions)
+
+    if (AssertionRunner.isAssertionsEnabled) {
+      if (allSteps.exists(!result.contains(_))) {
+        throw new IllegalStateException(s"The step sequence $result did not include all steps from $allSteps.")
+      }
+      if (!currentConditions.subsetOf(allConditions)) {
+        throw new IllegalStateException(s"The step sequence $result did not lead to a state where all conditions $allConditions are met. " +
+          s"Only meeting $currentConditions.")
+      }
+    }
+
+    result
+  }
+
+  /**
+   * Implements topological sort using Kahn's algorithm.
+   *
+   * @param graph          the graph
+   * @param order          optionally define an order to choose between multiple starting points.
+   * @param nextStepChosen a callback to extend the algorithm. Called every time a next step was chosen.
+   *                       The arguments to the callback are the next step which was just chosen and the current startPoints.
+   *                       The callback is allowed to modify the startPoints.
+   * @return a topological sort of the graph.
+   */
+  private def topologicalSort[S](graph: MutableDirectedGraph[S],
+                                 order: Option[Ordering[S]],
+                                 nextStepChosen: (S, mutable.Set[S]) => Unit): Seq[S] = {
+    // Empty list that will contain the sorted elements
+    val result = new ArrayBuffer[S]()
+
+    // In the beginning we can start with all nodes with no incoming edge.
+    val startPoints = {
+      val nonEmpty = graph.allNodes.filter(graph.incoming(_).isEmpty)
+      order match {
+        case None => nonEmpty.to[mutable.Set]
+        case Some(order) =>
+          implicit val implicitOrder: Ordering[S] = order
+          nonEmpty.to[mutable.SortedSet]
+      }
+    }
+
+    while (startPoints.nonEmpty) {
+      val n = startPoints.head
+      startPoints.remove(n)
+      result += n
+
+      for (m <- graph.outgoing(n)) {
+        graph.disconnect(n, m)
+        if (graph.incoming(m).isEmpty) {
+          startPoints += m
+        }
+      }
+
+      nextStepChosen(n, startPoints)
+    }
+
+    // If any edges remain, there was a cycle
+    if (graph.allNodes.map(graph.outgoing).exists(_.nonEmpty)) {
+      throw new IllegalArgumentException("There was a cycle in the graph.")
+    }
+
+    result
+  }
+}
---
+++ community/cypher/front-end/util/src/test/scala/org/neo4j/cypher/internal/util/StepSequencerTest.scala
@@ -0,0 +1,296 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.cypher.internal.util
+
+import org.neo4j.cypher.internal.util.StepSequencer.Condition
+import org.neo4j.cypher.internal.util.StepSequencer.Step
+import org.neo4j.cypher.internal.util.StepSequencer.StepAccumulator
+import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
+
+import scala.collection.immutable.Set
+
+//noinspection ZeroIndexToHead
+class StepSequencerTest extends CypherFunSuite {
+
+  private case object condA extends Condition
+  private case object condB extends Condition
+  private case object condC extends Condition
+  private case object condD extends Condition
+  private case object condE extends Condition
+  private case object condF extends Condition
+  private case object condG extends Condition
+
+  // No case class since we want reference equality
+  class TestStep(name: String,
+                 override val preConditions: Set[Condition],
+                 override val postConditions: Set[Condition],
+                 override val invalidatedConditions: Set[Condition]) extends Step {
+    override def toString: String = s"TestStep($name)"
+  }
+
+  private val stepAccumulator = new StepAccumulator[Step, Seq[Step]] {
+    override def empty: Seq[Step] = Seq.empty
+    override def addNext(acc: Seq[Step], step: Step): Seq[Step] = acc :+ step
+  }
+
+  private val sequencer = StepSequencer(stepAccumulator)
+
+  test("fails if a step depends on itself") {
+    val steps = Seq(
+      new TestStep("0", Set(condA), Set(condA), Set()),
+    )
+    an[IllegalArgumentException] should be thrownBy sequencer.orderSteps(steps.toSet)
+  }
+
+  test("fails if there are circular dependencies") {
+    val steps = Seq(
+      new TestStep("0", Set(condC), Set(condA), Set()),
+      new TestStep("1", Set(condA), Set(condB), Set()),
+      new TestStep("2", Set(condB), Set(condC), Set()),
+    )
+    an[IllegalArgumentException] should be thrownBy sequencer.orderSteps(steps.toSet)
+  }
+
+  test("fails if a step does not have any post-conditions") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(), Set()),
+    )
+    an[IllegalArgumentException] should be thrownBy sequencer.orderSteps(steps.toSet)
+  }
+
+  test("fails if multiple steps have the same post-conditions") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(), Set(condA), Set()),
+    )
+    an[IllegalArgumentException] should be thrownBy sequencer.orderSteps(steps.toSet)
+  }
+
+  test("fails if a step undoes its own work") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set(condA)),
+    )
+    an[IllegalArgumentException] should be thrownBy sequencer.orderSteps(steps.toSet)
+  }
+
+  test("fails if there is circular invalidation") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set(condB)),
+      new TestStep("1", Set(), Set(condB), Set(condC)),
+      new TestStep("2", Set(), Set(condC), Set(condA)),
+    )
+    an[IllegalArgumentException] should be thrownBy sequencer.orderSteps(steps.toSet)
+  }
+
+  test("orders steps correctly if they have strong ordering requirements") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(condA), Set(condB), Set()),
+      new TestStep("2", Set(condB), Set(condC), Set()),
+    )
+    sequencer.orderSteps(steps.toSet) should equal(steps)
+  }
+
+  test("if only some steps have dependencies, order those correctly") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(), Set(condB), Set()),
+      new TestStep("2", Set(), Set(condC), Set()),
+      new TestStep("3", Set(condA), Set(condD), Set()),
+      new TestStep("4", Set(condA), Set(condE), Set()),
+      new TestStep("5", Set(condB), Set(condF), Set()),
+      new TestStep("6", Set(condB, condA), Set(condG), Set()),
+    )
+
+    val orderedSteps = sequencer.orderSteps(steps.toSet)
+    withClue(orderedSteps) {
+      orderedSteps.indexOfOrFail(steps(3)) should be > orderedSteps.indexOfOrFail(steps(0))
+      orderedSteps.indexOfOrFail(steps(4)) should be > orderedSteps.indexOfOrFail(steps(0))
+      orderedSteps.indexOfOrFail(steps(5)) should be > orderedSteps.indexOfOrFail(steps(1))
+      orderedSteps.indexOfOrFail(steps(6)) should be > orderedSteps.indexOfOrFail(steps(0))
+      orderedSteps.indexOfOrFail(steps(6)) should be > orderedSteps.indexOfOrFail(steps(1))
+    }
+  }
+
+  test("duplicates single step if needed because conditions get invalidated") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(condA), Set(condB), Set()),
+      new TestStep("2", Set(condB), Set(condC), Set(condB)),
+    )
+    sequencer.orderSteps(steps.toSet) should equal(Seq(
+      steps(0),
+      steps(1),
+      steps(2),
+      steps(1),
+    ))
+  }
+
+  test("orders independent steps correctly if conditions get invalidated") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(condA), Set(condB), Set(condA)),
+      new TestStep("2", Set(condA), Set(condC), Set()),
+    )
+    sequencer.orderSteps(steps.toSet) should (equal(Seq(
+      steps(0),
+      steps(2),
+      steps(1),
+      steps(0),
+    )) or equal(Seq(
+      steps(0),
+      steps(1),
+      steps(0),
+      steps(2),
+    )))
+  }
+
+  test("duplicates multiple steps if needed because conditions get invalidated") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(condA), Set(condB), Set()),
+      new TestStep("2", Set(condB), Set(condC), Set(condA, condB)),
+    )
+    sequencer.orderSteps(steps.toSet) should equal(Seq(
+      steps(0),
+      steps(1),
+      steps(2),
+      steps(0),
+      steps(1),
+    ))
+  }
+
+  test("duplicates steps multiple times if needed because conditions get invalidated") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(condA), Set(condB), Set(condA)),
+      new TestStep("2", Set(condA), Set(condC), Set(condA)),
+    )
+    sequencer.orderSteps(steps.toSet) should (equal(Seq(
+      steps(0),
+      steps(1),
+      steps(0),
+      steps(2),
+      steps(0),
+    )) or equal(Seq(
+      steps(0),
+      steps(2),
+      steps(0),
+      steps(1),
+      steps(0),
+    )))
+  }
+
+  test("very invalidating steps") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(condA), Set(condB), Set(condA)),
+      new TestStep("2", Set(condA, condB), Set(condC), Set(condA, condB)),
+    )
+    sequencer.orderSteps(steps.toSet) should equal(Seq(
+      steps(0),
+      steps(1),
+      steps(0),
+      steps(2),
+      steps(0),
+      steps(1),
+      steps(0),
+    ))
+  }
+
+  test("should order invalidating step first if it has no dependencies") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(), Set(condB), Set()),
+      new TestStep("2", Set(), Set(condE), Set(condA, condB, condC, condD)),
+      new TestStep("3", Set(), Set(condC), Set()),
+      new TestStep("4", Set(), Set(condD), Set()),
+      new TestStep("5", Set(condA, condB, condC, condD), Set(condF), Set()),
+    )
+
+    val orderedSteps = sequencer.orderSteps(steps.toSet)
+    withClue(orderedSteps) {
+      orderedSteps.size should equal(steps.size)
+      orderedSteps(0) should equal(steps(2))
+      orderedSteps(5) should equal(steps(5))
+    }
+  }
+
+  test("should order often invalidated step last if it has no dependencies") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set(condE)),
+      new TestStep("1", Set(), Set(condB), Set(condE)),
+      new TestStep("2", Set(), Set(condE), Set(condF, condG)),
+      new TestStep("3", Set(), Set(condC), Set(condE)),
+      new TestStep("4", Set(), Set(condD), Set(condE)),
+      new TestStep("5", Set(condA, condB, condC, condD, condE), Set(condF, condG), Set()),
+    )
+
+    val orderedSteps = sequencer.orderSteps(steps.toSet)
+    withClue(orderedSteps) {
+      orderedSteps.size should equal(steps.size)
+      orderedSteps(4) should equal(steps(2))
+      orderedSteps(5) should equal(steps(5))
+    }
+  }
+
+  test("should order often invalidated step last if it has no dependencies 2") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA, condB, condC), Set()),
+      new TestStep("1", Set(), Set(condD), Set(condA)),
+      new TestStep("2", Set(), Set(condE), Set(condA)),
+      new TestStep("3", Set(), Set(condF), Set(condA, condB)),
+    )
+
+    val orderedSteps = sequencer.orderSteps(steps.toSet)
+    withClue(orderedSteps) {
+      orderedSteps.size should equal(steps.size)
+      orderedSteps(3) should equal(steps(0))
+    }
+  }
+
+  test("should find order where step that introduces frequently invalidated condition is not repeated too often") {
+    val steps = Seq(
+      new TestStep("0", Set(), Set(condA), Set()),
+      new TestStep("1", Set(condA), Set(condB), Set()),
+      new TestStep("2", Set(condA), Set(condC), Set(condB)),
+      new TestStep("3", Set(condA), Set(condD), Set(condB)),
+      new TestStep("4", Set(condB, condC, condD), Set(condE), Set()),
+    )
+    sequencer.orderSteps(steps.toSet) should (equal(Seq(
+      steps(0),
+      steps(2),
+      steps(3),
+      steps(1),
+      steps(4),
+    )) or equal(Seq(
+      steps(0),
+      steps(3),
+      steps(2),
+      steps(1),
+      steps(4),
+    )))
+  }
+
+  implicit class Indexer[S](s: Seq[S]) {
+    def indexOfOrFail(elem: S): Int = {
+      val i = s.indexOf(elem)
+      if (i == -1) throw new IllegalArgumentException(s"$elem not in $s")
+      i
+    }
+  }
+}
