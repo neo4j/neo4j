61d43bc75d6c9c3139d098a472acd0da8d9b464e
emmaholmbergohlsson <emma.holmberg.ohlsson@neo4j.com>
2020-10-21 15:23:12 +0200

    Improve description of user log settings
    There are some dependencies between the user log settings that aren't obvious - now documented.

--- community/kernel/src/main/java/org/neo4j/graphdb/factory/GraphDatabaseSettings.java
+++ community/kernel/src/main/java/org/neo4j/graphdb/factory/GraphDatabaseSettings.java
@@ -475,7 +475,8 @@ public class GraphDatabaseSettings implements LoadableConfig
                  "procedures will be loaded if they are placed in this directory." )
     public static final Setting<File> plugin_dir = pathSetting( "dbms.directories.plugins", "plugins" );
 
-    @Description( "Threshold for rotation of the user log. If set to 0 log rotation is disabled." )
+    @Description( "Threshold for rotation of the user log. If set to 0 log rotation is disabled. " +
+                  "Note that if dbms.logs.user.stdout_enabled is enabled this setting will be ignored." )
     public static final Setting<Long> store_user_log_rotation_threshold =
             buildSetting( "dbms.logs.user.rotation.size", BYTES, "0" ).constraint( range( 0L, Long.MAX_VALUE ) ).build();
 
@@ -512,7 +513,8 @@ public class GraphDatabaseSettings implements LoadableConfig
     public static final Setting<Duration> counts_store_rotation_timeout =
             setting( "unsupported.dbms.counts_store_rotation_timeout", DURATION, "10m" );
 
-    @Description( "Minimum time interval after last rotation of the user log before it may be rotated again." )
+    @Description( "Minimum time interval after last rotation of the user log before it may be rotated again. " +
+                  "Note that if dbms.logs.user.stdout_enabled is enabled this setting will be ignored." )
     public static final Setting<Duration> store_user_log_rotation_delay =
             setting( "dbms.logs.user.rotation.delay", DURATION, "300s" );
 
@@ -520,7 +522,7 @@ public class GraphDatabaseSettings implements LoadableConfig
     public static final Setting<Duration> store_internal_log_rotation_delay =
             setting( "dbms.logs.debug.rotation.delay", DURATION, "300s" );
 
-    @Description( "Maximum number of history files for the user log." )
+    @Description( "Maximum number of history files for the user log. Note that if dbms.logs.user.stdout_enabled is enabled this setting will be ignored." )
     public static final Setting<Integer> store_user_log_max_archives =
             buildSetting( "dbms.logs.user.rotation.keep_number", INTEGER, "7" ).constraint( min( 1 ) ).build();
 
@@ -876,7 +878,7 @@ public class GraphDatabaseSettings implements LoadableConfig
             logs -> new File( logs, "query.log" ),
             PATH );
 
-    @Description( "Path to the user log file." )
+    @Description( "Path to the user log file. Note that if dbms.logs.user.stdout_enabled is enabled this setting will be ignored." )
     public static final Setting<File> store_user_log_path =
             derivedSetting( "dbms.logs.user.path", logs_directory, logs -> new File( logs, "neo4j.log" ), PATH );
 
