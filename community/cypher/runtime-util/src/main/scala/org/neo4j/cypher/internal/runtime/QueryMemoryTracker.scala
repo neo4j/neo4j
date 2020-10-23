3d176360dd7fd6a31d9a0f8a55b8e19a51954cb1
Henrik Nyman <henrik.nyman@neotechnology.com>
2020-10-23 11:00:35 +0200

    Clean up QueryMemoryTracker API
    The old way to allocate / deallocate is no longer used, so can be removed

--- community/cypher/runtime-util/src/main/scala/org/neo4j/cypher/internal/runtime/QueryMemoryTracker.scala
+++ community/cypher/runtime-util/src/main/scala/org/neo4j/cypher/internal/runtime/QueryMemoryTracker.scala
@@ -24,66 +24,10 @@ import org.neo4j.cypher.internal.runtime.BoundedQueryMemoryTracker.OperatorMemor
 import org.neo4j.cypher.internal.runtime.MemoryTrackingController.MemoryTrackerDecorator
 import org.neo4j.cypher.result.OperatorProfile
 import org.neo4j.memory.EmptyMemoryTracker
-import org.neo4j.memory.Measurable
 import org.neo4j.memory.MemoryTracker
 import org.neo4j.memory.OptionalMemoryTracker
-import org.neo4j.values.AnyValue
 
 trait QueryMemoryTracker {
-
-  /**
-   * Returns true if memory tracking is enabled, false otherwise
-   */
-  def isEnabled: Boolean
-
-  /**
-   * Record allocation of bytes
-   *
-   * @param bytes number of allocated bytes
-   */
-  def allocated(bytes: Long, operatorId: Int): Unit
-
-  /**
-   * Record allocation of value
-   *
-   * @param value that value which was allocated
-   */
-  def allocated(value: AnyValue, operatorId: Int) : Unit
-
-  /**
-   * Record allocation of instance with heap usage estimation
-   *
-   * @param instance the allocated instance
-   */
-  def allocated(instance: Measurable, operatorId: Int): Unit
-
-  /**
-   * Record de-allocation of bytes
-   *
-   * @param bytes number of de-allocated bytes
-   */
-  def deallocated(bytes: Long, operatorId: Int): Unit
-
-  /**
-   * Record de-allocation of value
-   *
-   * @param value that value which was de-allocated
-   */
-  def deallocated(value: AnyValue, operatorId: Int): Unit
-
-  /**
-   * Record de-allocation of instance with heap usage estimation
-   *
-   * @param instance the de-allocated instance
-   */
-  def deallocated(instance: Measurable, operatorId: Int): Unit
-
-  /**
-   * Returns an Iterator that, given the memory config settings, might throw an exception if the
-   * memory used by the query grows too large.
-   */
-  def memoryTrackingIterator[T<: CypherRow](input: Iterator[T], operatorId: Int): Iterator[T]
-
   /**
    * Get the total allocated memory of this query, in bytes.
    *
@@ -120,21 +64,6 @@ object QueryMemoryTracker {
 }
 
 case object NoOpQueryMemoryTracker extends QueryMemoryTracker {
-  override val isEnabled: Boolean = false
-
-  override def memoryTrackingIterator[T](input: Iterator[T], operatorId: Int): Iterator[T] = input
-
-  override def allocated(value: AnyValue, operatorId: Int): Unit = {}
-
-  override def allocated(bytes: Long, operatorId: Int): Unit = {}
-
-  override def allocated(instance: Measurable, operatorId: Int): Unit = {}
-
-  override def deallocated(bytes: Long, operatorId: Int): Unit = {}
-
-  override def deallocated(value: AnyValue, operatorId: Int): Unit = {}
-
-  override def deallocated(instance: Measurable, operatorId: Int): Unit = {}
 
   override def totalAllocatedMemory: Long = OptionalMemoryTracker.ALLOCATIONS_NOT_TRACKED
 
@@ -145,7 +74,7 @@ case object NoOpQueryMemoryTracker extends QueryMemoryTracker {
 
 object BoundedQueryMemoryTracker {
   def apply(transactionMemoryTracker: MemoryTracker): BoundedQueryMemoryTracker = {
-    new BoundedQueryMemoryTracker(transactionMemoryTracker, new MemoryTrackerPerOperator(transactionMemoryTracker))
+    new BoundedQueryMemoryTracker(transactionMemoryTracker)
   }
 
   class OperatorMemoryTracker(queryMemoryTracker: MemoryTracker) extends HighWaterScopedMemoryTracker(queryMemoryTracker)
@@ -154,33 +83,16 @@ object BoundedQueryMemoryTracker {
 
 }
 
-class BoundedQueryMemoryTracker(transactionMemoryTracker: MemoryTracker, memoryTrackerPerOperator: MemoryTrackerPerOperator) extends HighWaterScopedMemoryTracker(transactionMemoryTracker)
-                                                                                                                             with QueryMemoryTracker {
-  override val isEnabled: Boolean = true
-
-  private val newTracker = () => new OperatorMemoryTracker(this)
-
-  def allocated(bytes: Long, operatorId: Int): Unit = {
-    memoryTrackerPerOperator.computeIfAbsent(operatorId, newTracker).allocateHeap(bytes)
-  }
-
-  override def allocated(value: AnyValue, operatorId: Int): Unit = allocated(value.estimatedHeapUsage(), operatorId)
-
-  override def allocated(instance: Measurable, operatorId: Int): Unit = allocated(instance.estimatedHeapUsage, operatorId)
-
-  override def deallocated(bytes: Long, operatorId: Int): Unit = {
-    memoryTrackerPerOperator.computeIfAbsent(operatorId, newTracker).releaseHeap(bytes)
-  }
-
-  override def deallocated(value: AnyValue, operatorId: Int): Unit = deallocated(value.estimatedHeapUsage(), operatorId)
-
-  override def deallocated(instance: Measurable, operatorId: Int): Unit = deallocated(instance.estimatedHeapUsage, operatorId)
+class BoundedQueryMemoryTracker(transactionMemoryTracker: MemoryTracker) extends HighWaterScopedMemoryTracker(transactionMemoryTracker)
+                                                                            with QueryMemoryTracker {
+  private[this] val memoryTrackerPerOperator: MemoryTrackerPerOperator = new MemoryTrackerPerOperator(this)
+  private[this] val newTracker = () => new OperatorMemoryTracker(this)
 
   override def totalAllocatedMemory: Long = heapHighWaterMark()
 
   override def maxMemoryOfOperator(operatorId: Int): Long = {
     if (memoryTrackerPerOperator.isDefinedAt(operatorId)) {
-      memoryTrackerPerOperator.get(operatorId).heapHighWaterMark()
+      memoryTrackerPerOperator.get(operatorId).heapHighWaterMark
     } else {
       OptionalMemoryTracker.ALLOCATIONS_NOT_TRACKED
     }
@@ -188,21 +100,6 @@ class BoundedQueryMemoryTracker(transactionMemoryTracker: MemoryTracker, memoryT
 
   override def memoryTrackerForOperator(operatorId: Int): MemoryTracker = {
     memoryTrackerPerOperator.computeIfAbsent(operatorId, newTracker)
-  }
-
-  override def memoryTrackingIterator[T <: Measurable](input: Iterator[T], operatorId: Int): Iterator[T] = new MemoryTrackingIterator[T](input, operatorId)
-
-  private class MemoryTrackingIterator[T <: Measurable](input: Iterator[T], operatorId: Int) extends Iterator[T] {
-    private val operatorMemoryTracker = memoryTrackerForOperator(operatorId)
-
-    override def hasNext: Boolean = input.hasNext
-
-    override def next(): T = {
-      val t = input.next()
-      val rowHeapUsage = t.estimatedHeapUsage
-      operatorMemoryTracker.allocateHeap(rowHeapUsage)
-      t
-    }
   }
 }
 
--- community/cypher/runtime-util/src/test/scala/org/neo4j/cypher/internal/runtime/BoundedQueryMemoryTrackerTest.scala
+++ community/cypher/runtime-util/src/test/scala/org/neo4j/cypher/internal/runtime/BoundedQueryMemoryTrackerTest.scala
@@ -35,102 +35,94 @@ class BoundedQueryMemoryTrackerTest extends CypherFunSuite {
 
   case class TestMemoryTracker(maxBytes: Long = Long.MaxValue) extends LocalMemoryTracker(MemoryPools.NO_TRACKING, maxBytes, 0, null)
 
+  private val sizeOfGrowingArray: Long = shallowSizeOfObjectArray(GrowingArray.DEFAULT_SIZE) + GrowingArray.SHALLOW_SIZE
+
   test("Tracks overall memory high water mark with bytes") {
     // Given
     val tracker = BoundedQueryMemoryTracker(TestMemoryTracker())
+    val tracker0 = tracker.memoryTrackerForOperator(0)
+
     // When
-    tracker.allocated(10, 0)
-    tracker.allocated(5, 0)
-    tracker.deallocated(6, 0)
-    tracker.allocated(4, 0)
+    tracker0.allocateHeap(10)
+    tracker0.allocateHeap(5)
+    tracker0.releaseHeap(6)
+    tracker0.allocateHeap(4)
+
     // Then
-    tracker.totalAllocatedMemory should be(15L)
+    tracker.totalAllocatedMemory should be(15L + sizeOfGrowingArray)
     tracker.maxMemoryOfOperator(0) should be(15L)
+    tracker0.heapHighWaterMark() should be(15L)
   }
 
   test("Tracks overall memory high water mark with AnyValues") {
     // Given
     val tracker = BoundedQueryMemoryTracker(TestMemoryTracker())
+    val tracker0 = tracker.memoryTrackerForOperator(0)
+
     val v1 = Values.intValue(5)
     val v2 = Values.booleanValue(true)
     val v3 = Values.stringValue("foo")
     val v4 = Values.booleanValue(false)
+
     // When
-    tracker.allocated(v1, 0)
-    tracker.allocated(v2, 0)
-    tracker.deallocated(v3, 0)
-    tracker.allocated(v4, 0)
+    tracker0.allocateHeap(v1.estimatedHeapUsage)
+    tracker0.allocateHeap(v2.estimatedHeapUsage)
+    tracker0.releaseHeap(v3.estimatedHeapUsage)
+    tracker0.allocateHeap(v4.estimatedHeapUsage)
+
     // Then
     val expected = Math.max(v1.estimatedHeapUsage() + v2.estimatedHeapUsage(), v1.estimatedHeapUsage() + v2.estimatedHeapUsage() - v3.estimatedHeapUsage() + v4.estimatedHeapUsage())
-    tracker.totalAllocatedMemory should be(expected)
+    tracker.totalAllocatedMemory should be(expected + sizeOfGrowingArray)
     tracker.maxMemoryOfOperator(0) should be(expected)
-  }
-
-  test("Tracks overall memory high water mark with WithHeapUsageEstimation") {
-    // Given
-    val tracker = BoundedQueryMemoryTracker(TestMemoryTracker())
-    // When
-    tracker.allocated(IMem(10), 0)
-    tracker.allocated(IMem(5), 0)
-    tracker.deallocated(IMem(6), 0)
-    tracker.allocated(IMem(4), 0)
-    // Then
-    tracker.totalAllocatedMemory should be(15L)
-    tracker.maxMemoryOfOperator(0) should be(15L)
-  }
-
-  test("Iterator tracks memory") {
-    // Given
-    val tracker = BoundedQueryMemoryTracker(TestMemoryTracker())
-    val iterator = tracker.memoryTrackingIterator(Iterator(IMem(1), IMem(6), IMem(2), IMem(100)), 0)
-    // When
-    iterator.next() // 1
-    tracker.allocated(10, 0) // 11
-    iterator.next() // 17
-    tracker.deallocated(3, 0) // 14
-    iterator.next() // 16
-    // Then
-    tracker.totalAllocatedMemory should be(17L)
-    tracker.maxMemoryOfOperator(0) should be(17L)
+    tracker0.heapHighWaterMark() should be(expected)
   }
 
   test("Throws exception if memory exceeds threshold") {
     // Given
-    val sizeOfGrowingArray = shallowSizeOfObjectArray(GrowingArray.DEFAULT_SIZE) + GrowingArray.SHALLOW_SIZE
     val tracker = BoundedQueryMemoryTracker(TestMemoryTracker(20 + sizeOfGrowingArray))
+
     // When
-    tracker.allocated(10, 0)
-    tracker.allocated(5, 0)
-    tracker.deallocated(6, 0)
-    tracker.allocated(9, 0)
+    tracker.allocateHeap(10)
+    tracker.allocateHeap(5)
+    tracker.releaseHeap(6)
+    tracker.allocateHeap(9)
+
     // Then
-    a[MemoryLimitExceededException] should be thrownBy tracker.allocated(3, 0)
+    a[MemoryLimitExceededException] should be thrownBy tracker.allocateHeap(3)
   }
 
   test("Tracks individual memory per operator plus overall") {
     // Given
     val tracker = BoundedQueryMemoryTracker(TestMemoryTracker())
-    val iterator0 = tracker.memoryTrackingIterator(Iterator(IMem(1), IMem(6), IMem(8)), 0)
-    val iterator1 = tracker.memoryTrackingIterator(Iterator(IMem(2), IMem(4), IMem(7)), 1)
-    val iterator2 = tracker.memoryTrackingIterator(Iterator(IMem(3), IMem(13), IMem(2)), 2)
+    val tracker0 = tracker.memoryTrackerForOperator(0)
+    val tracker1 = tracker.memoryTrackerForOperator(1)
+    val tracker2 = tracker.memoryTrackerForOperator(2)
+    val iterator0 = Iterator(IMem(1), IMem(6), IMem(8))
+    val iterator1 = Iterator(IMem(2), IMem(4), IMem(7))
+    val iterator2 = Iterator(IMem(3), IMem(13), IMem(2))
+
     // When
-    iterator0.next() // [1, 0, 0] / 1
-    tracker.allocated(10, 2) // [1, 0, 10] / 11
-    iterator1.next() // [1, 2, 10] / 13
-    iterator0.next() // [7, 2, 10] / 19
-    tracker.deallocated(5, 0) // [2, 2, 10] / 14
-    iterator2.next() // [2, 2, 13] / 17
-    iterator2.next() // [2, 2, 26] / 30
-    iterator2.next() // [2, 2, 28] / 32
-    tracker.deallocated(21, 2) // [2, 2, 7] / 11
-    iterator0.next() // [10, 2, 7] / 19
-    iterator1.next() // [10, 6, 7] / 23
-    tracker.deallocated(2, 1) // [10, 4, 7] / 21
-    iterator1.next() // [10, 11, 7] / 28
+    tracker0.allocateHeap(iterator0.next().estimatedHeapUsage) // [1, 0, 0] / 1
+    tracker2.allocateHeap(10) // [1, 0, 10] / 11
+    tracker1.allocateHeap(iterator1.next().estimatedHeapUsage) // [1, 2, 10] / 13
+    tracker0.allocateHeap(iterator0.next().estimatedHeapUsage) // [7, 2, 10] / 19
+    tracker0.releaseHeap(5) // [2, 2, 10] / 14
+    tracker2.allocateHeap(iterator2.next().estimatedHeapUsage) // [2, 2, 13] / 17
+    tracker2.allocateHeap(iterator2.next().estimatedHeapUsage) // [2, 2, 26] / 30
+    tracker2.allocateHeap(iterator2.next().estimatedHeapUsage) // [2, 2, 28] / 32
+    tracker2.releaseHeap(21) // [2, 2, 7] / 11
+    tracker0.allocateHeap(iterator0.next().estimatedHeapUsage) // [10, 2, 7] / 19
+    tracker1.allocateHeap(iterator1.next().estimatedHeapUsage) // [10, 6, 7] / 23
+    tracker1.releaseHeap(2) // [10, 4, 7] / 21
+    tracker1.allocateHeap(iterator1.next().estimatedHeapUsage) // [10, 11, 7] / 28
+
     // Then
-    tracker.totalAllocatedMemory should be(32L)
+    tracker.totalAllocatedMemory should be(32L + sizeOfGrowingArray)
     tracker.maxMemoryOfOperator(0) should be(10L)
     tracker.maxMemoryOfOperator(1) should be(11L)
     tracker.maxMemoryOfOperator(2) should be(28L)
+    tracker0.heapHighWaterMark() should be (10L)
+    tracker1.heapHighWaterMark() should be (11L)
+    tracker2.heapHighWaterMark() should be (28L)
   }
 }
