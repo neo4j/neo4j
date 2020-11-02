fe749fdf8c7c8a34eaad1ad275641ae56d1bbb63
Mattias Finné <mattias@neotechnology.com>
2020-11-02 13:06:55 +0100

    Fixes issues with iterating all entries in Lucene index
    There were two issues, both triggered with the same added test:
    
    - LeafReader#getLiveDocs() can rightfully return null, meaning
      that the leaf has no deletions.
    - Comparison with maxDoc was off-by-one since reader.maxDoc()
      returns one greater than max (i.e. high ID).

--- community/lucene-index/src/test/java/org/neo4j/kernel/api/impl/schema/LuceneIndexAccessorIT.java
+++ community/lucene-index/src/test/java/org/neo4j/kernel/api/impl/schema/LuceneIndexAccessorIT.java
@@ -48,6 +48,7 @@ import org.neo4j.test.extension.Inject;
 import org.neo4j.test.extension.testdirectory.TestDirectoryExtension;
 import org.neo4j.test.rule.TestDirectory;
 import org.neo4j.values.storable.TextValue;
+import org.neo4j.values.storable.Value;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -66,7 +67,6 @@ public class LuceneIndexAccessorIT
     private TestDirectory directory;
 
     private final LifeSupport life = new LifeSupport();
-    private final IndexDescriptor indexDescriptor = IndexPrototype.forSchema( SchemaDescriptor.forLabel( 1, 2 ) ).withName( "TestIndex" ).materialise( 99 );
     private LuceneIndexProvider indexProvider;
     private IndexSamplingConfig samplingConfig;
 
@@ -94,11 +94,12 @@ public class LuceneIndexAccessorIT
         int nodes = 100;
         MutableLongSet expectedNodes = LongSets.mutable.empty();
         TokenNameLookup tokenNameLookup = mock( TokenNameLookup.class );
-        populateWithInitialNodes( nodes, expectedNodes, tokenNameLookup );
+        IndexDescriptor indexDescriptor = IndexPrototype.forSchema( SchemaDescriptor.forLabel( 1, 2 ) ).withName( "TestIndex" ).materialise( 99 );
+        populateWithInitialNodes( indexDescriptor, nodes, expectedNodes, tokenNameLookup );
         try ( IndexAccessor accessor = indexProvider.getOnlineAccessor( indexDescriptor, samplingConfig, tokenNameLookup ) )
         {
             // when
-            removeSomeNodes( nodes / 2, accessor, expectedNodes );
+            removeSomeNodes( indexDescriptor, nodes / 2, accessor, expectedNodes );
 
             // then
             try ( BoundedIterable<Long> reader = accessor.newAllEntriesReader() )
@@ -110,32 +111,78 @@ public class LuceneIndexAccessorIT
         }
     }
 
-    private void removeSomeNodes( int nodes, IndexAccessor accessor, MutableLongSet expectedNodes ) throws IndexEntryConflictException
+    @Test
+    void shouldIterateAllDocumentsEvenWhenContainingDeletionsInOnlySomeLeaves() throws Exception
+    {
+        // given
+        int nodes = 300_000;
+        MutableLongSet expectedNodes = LongSets.mutable.empty();
+        TokenNameLookup tokenNameLookup = mock( TokenNameLookup.class );
+        IndexDescriptor indexDescriptor = IndexPrototype.forSchema( SchemaDescriptor.forLabel( 1, 2, 3, 4, 5 ) ).withName( "TestIndex" ).materialise( 99 );
+        populateWithInitialNodes( indexDescriptor, nodes, expectedNodes, tokenNameLookup );
+        try ( IndexAccessor accessor = indexProvider.getOnlineAccessor( indexDescriptor, samplingConfig, tokenNameLookup ) )
+        {
+            // when
+            removeSomeNodes( indexDescriptor, 2, accessor, expectedNodes );
+
+            // then
+            try ( BoundedIterable<Long> reader = accessor.newAllEntriesReader() )
+            {
+                MutableLongSet readIds = LongSets.mutable.empty();
+                reader.forEach( readIds::add );
+                assertEquals( expectedNodes, readIds );
+            }
+        }
+    }
+
+    private void removeSomeNodes( IndexDescriptor indexDescriptor, int nodes, IndexAccessor accessor, MutableLongSet expectedNodes )
+            throws IndexEntryConflictException
     {
         try ( IndexUpdater updater = accessor.newUpdater( IndexUpdateMode.ONLINE ) )
         {
             for ( long id = 0; id < nodes; id++ )
             {
-                updater.process( IndexEntryUpdate.remove( id, indexDescriptor, value( id ) ) );
+                updater.process( IndexEntryUpdate.remove( id, indexDescriptor, values( indexDescriptor, id ) ) );
                 expectedNodes.remove( id );
             }
         }
     }
 
-    private void populateWithInitialNodes( int nodes, MutableLongSet expectedNodes, TokenNameLookup tokenNameLookup ) throws IndexEntryConflictException
+    private void populateWithInitialNodes( IndexDescriptor indexDescriptor, int nodes, MutableLongSet expectedNodes, TokenNameLookup tokenNameLookup )
+            throws IndexEntryConflictException
     {
         IndexPopulator populator =
                 indexProvider.getPopulator( indexDescriptor, samplingConfig, ByteBufferFactory.heapBufferFactory( (int) kibiBytes( 100 ) ), tokenNameLookup );
         Collection<IndexEntryUpdate<IndexDescriptor>> initialData = new ArrayList<>();
+        populator.create();
         for ( long id = 0; id < nodes; id++ )
         {
-            initialData.add( add( id, indexDescriptor, value( id ) ) );
+            Value[] values = values( indexDescriptor, id );
+            initialData.add( add( id, indexDescriptor, values ) );
             expectedNodes.add( id );
+            if ( initialData.size() >= 100 )
+            {
+                populator.add( initialData );
+                initialData.clear();
+            }
         }
-        populator.create();
-        populator.add( initialData );
+        if ( !initialData.isEmpty() )
+        {
+            populator.add( initialData );
+        }
         populator.scanCompleted( nullInstance );
         populator.close( true );
+    }
+
+    private Value[] values( IndexDescriptor properties, long id )
+    {
+        int numProperties = properties.schema().getPropertyIds().length;
+        Value[] values = new Value[numProperties];
+        for ( int i = 0; i < numProperties; i++ )
+        {
+            values[i] = value( id * numProperties + i );
+        }
+        return values;
     }
 
     private TextValue value( long id )
