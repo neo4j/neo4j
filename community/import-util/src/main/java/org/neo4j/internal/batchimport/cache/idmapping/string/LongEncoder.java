381b6332ec17da4063e75254fd7ebe0aaa25dbb2
Mattias Finné <mattias@neotechnology.com>
2020-11-03 09:14:43 +0100

    Explicitly fails on trying to encode too large ID in importer
    A couple of higher-order bits of the long values when using INTEGER
    id type in the importer are reserved for internal use, but the
    LongEncoder didn't check that the incoming ID values were not using
    any of those bits. This would instead produce unexpected ordering
    failures later on in the sorting process.

--- community/import-util/src/main/java/org/neo4j/internal/batchimport/cache/idmapping/string/LongEncoder.java
+++ community/import-util/src/main/java/org/neo4j/internal/batchimport/cache/idmapping/string/LongEncoder.java
@@ -21,16 +21,22 @@ package org.neo4j.internal.batchimport.cache.idmapping.string;
 
 import static java.lang.Math.log10;
 import static java.lang.Math.max;
+import static org.neo4j.util.Preconditions.checkArgument;
 
 /**
  * {@link Encoder} that assumes that the entered strings can be parsed to {@link Long} directly.
+ * The 8 higher-order bits are reserved for internal use and therefore the IDs cannot use the whole range of the long.
  */
 public class LongEncoder implements Encoder
 {
+    private static final long ID_BITS = 0x00FFFFFF_FFFFFFFFL;
+    private static final long RESERVED_BITS = ~ID_BITS;
+
     @Override
     public long encode( Object value )
     {
         long longValue = ((Number)value).longValue();
+        checkArgument( (longValue & RESERVED_BITS) == 0, "Invalid integer ID %d, it must be %d <= id <= 0", longValue, ID_BITS );
         long length = numberOfDigits( longValue );
         length = length << 57;
         long returnVal = length | longValue;
