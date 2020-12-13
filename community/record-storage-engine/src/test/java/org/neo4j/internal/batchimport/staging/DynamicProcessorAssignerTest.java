79df09eb9be168da24b470cd4f6cf861753a1686
Mattias Finné <mattias@neotechnology.com>
2020-12-14 00:16:37 +0100

    Improvements to importer processor assignment
    There was this overly optimistic calculation of amount of active CPUs
    in DynamicProcessorAssigner where processing steps that had much lower
    average processing time than the slowest would be counted with a factor
    less than one. This would in some cases lead to an over-assignment
    of threads which could lead to OS scheduling problems. This could lead
    to a slowdown of the import as a whole.
    
    Also stops unnecessary removing threads from "too fast" steps if
    there are still threads that are unassigned, this to make things
    a fair amount more stable so that thread counts doesn't jump up and
    down a whole lot once they have been initially assigned.
    Now instead moves thread from one step to another if bottlenecks
    are discovered.

--- community/record-storage-engine/src/test/java/org/neo4j/internal/batchimport/staging/DynamicProcessorAssignerTest.java
+++ community/record-storage-engine/src/test/java/org/neo4j/internal/batchimport/staging/DynamicProcessorAssignerTest.java
@@ -27,15 +27,12 @@ import org.neo4j.internal.batchimport.Configuration;
 import org.neo4j.internal.batchimport.stats.Keys;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
 import static org.neo4j.internal.batchimport.staging.ControlledStep.stepWithStats;
 
 class DynamicProcessorAssignerTest
 {
     @Test
-    void shouldAssignAdditionalCPUToBottleNeckStep()
+    void shouldAssignProcessorsToSlowestStep()
     {
         // GIVEN
         Configuration config = config( 10, 5 );
@@ -51,23 +48,21 @@ class DynamicProcessorAssignerTest
         assigner.check( execution );
 
         // THEN
-        assertEquals( 5, slowStep.processors( 0 ) );
+        assertEquals( 4, slowStep.processors( 0 ) );
         assertEquals( 1, fastStep.processors( 0 ) );
     }
 
     @Test
-    void shouldRemoveCPUsFromWayTooFastStep()
+    void shouldMoveProcessorFromOverlyAssignedStep()
     {
         // GIVEN
-        Configuration config = config( 10, 3 );
+        Configuration config = config( 10, 5 );
         // available processors = 2 is enough because it will see the fast step as only using 20% of a processor
         // and it rounds down. So there's room for assigning one more.
         DynamicProcessorAssigner assigner = new DynamicProcessorAssigner( config );
 
-        ControlledStep<?> slowStep = spy( stepWithStats( "slow", 1, Keys.avg_processing_time, 6L, Keys.done_batches, 10L )
-                .setProcessors( 2 ) );
-        ControlledStep<?> fastStep = spy( stepWithStats( "fast", 0, Keys.avg_processing_time, 2L, Keys.done_batches, 10L )
-                .setProcessors( 2 ) );
+        ControlledStep<?> slowStep = stepWithStats( "slow", 0, Keys.avg_processing_time, 6L, Keys.done_batches, 10L ).setProcessors( 2 );
+        ControlledStep<?> fastStep = stepWithStats( "fast", 0, Keys.avg_processing_time, 2L, Keys.done_batches, 10L ).setProcessors( 3 );
 
         StageExecution execution = executionOf( config, slowStep, fastStep );
         assigner.start( execution );
@@ -75,30 +70,30 @@ class DynamicProcessorAssignerTest
         // WHEN checking
         assigner.check( execution );
 
-        // THEN one processor should be removed from the fast step
-        verify( fastStep ).processors( -1 );
+        // THEN one processor should have been moved from the fast step to the slower step
+        assertEquals( 2, fastStep.processors( 0 ) );
+        assertEquals( 3, slowStep.processors( 0 ) );
     }
 
     @Test
-    void shouldRemoveCPUsButNotSoThatTheFastStepBecomesBottleneck()
+    void shouldNotMoveProcessorFromFastStepSoThatItBecomesBottleneck()
     {
         // GIVEN
-        Configuration config = config( 10, 3 );
+        Configuration config = config( 10, 4 );
         DynamicProcessorAssigner assigner = new DynamicProcessorAssigner( config );
 
-        ControlledStep<?> slowStep = spy( stepWithStats( "slow", 1, Keys.avg_processing_time, 10L, Keys.done_batches, 10L ) );
-        ControlledStep<?> fastStep = spy( stepWithStats( "fast", 0, Keys.avg_processing_time, 7L, Keys.done_batches, 10L )
-                .setProcessors( 3 ) );
+        ControlledStep<?> slowStep = stepWithStats( "slow", 1, Keys.avg_processing_time, 10L, Keys.done_batches, 10L );
+        ControlledStep<?> fastStep = stepWithStats( "fast", 0, Keys.avg_processing_time, 7L, Keys.done_batches, 10L ).setProcessors( 3 );
 
         StageExecution execution = executionOf( config, slowStep, fastStep );
         assigner.start( execution );
 
-        // WHEN checking the first time
+        // WHEN checking
         assigner.check( execution );
 
-        // THEN one processor should be removed from the fast step
-        verify( fastStep, never() ).processors( 1 );
-        verify( fastStep, never() ).processors( -1 );
+        // THEN
+        assertEquals( 3, fastStep.processors( 0 ) );
+        assertEquals( 1, slowStep.processors( 0 ) );
     }
 
     @Test
@@ -123,20 +118,17 @@ class DynamicProcessorAssignerTest
     }
 
     @Test
-    void shouldRemoveCPUsFromTooFastStepEvenIfThereIsAWayFaster()
+    void shouldMoveProcessorFromNotOnlyFastestStep()
     {
         // The point is that not only the fastest step is subject to have processors removed,
-        // it's the relationship between all pairs of steps. This is important since the DPA has got
-        // a max permit count of processors to assign, so reclaiming unnecessary assignments can
-        // have those be assigned to a more appropriate step instead, where it will benefit the Stage more.
+        // it's the relationship between all pairs of steps.
 
         // GIVEN
-        Configuration config = config( 10, 3 );
+        Configuration config = config( 10, 5 );
         DynamicProcessorAssigner assigner = new DynamicProcessorAssigner( config );
         Step<?> wayFastest = stepWithStats( "wayFastest", 0, Keys.avg_processing_time, 50L, Keys.done_batches, 20L );
-        Step<?> fast = spy( stepWithStats( "fast", 0, Keys.avg_processing_time, 100L, Keys.done_batches, 20L )
-                .setProcessors( 3 ) );
-        Step<?> slow = stepWithStats( "slow", 1, Keys.avg_processing_time, 220L, Keys.done_batches, 20L );
+        Step<?> fast = stepWithStats( "fast", 0, Keys.avg_processing_time, 100L, Keys.done_batches, 20L ).setProcessors( 3 );
+        Step<?> slow = stepWithStats( "slow", 2, Keys.avg_processing_time, 220L, Keys.done_batches, 20L );
         StageExecution execution = executionOf( config, slow, wayFastest, fast );
         assigner.start( execution );
 
@@ -144,34 +136,29 @@ class DynamicProcessorAssignerTest
         assigner.check( execution );
 
         // THEN
-        verify( fast ).processors( -1 );
+        assertEquals( 2, fast.processors( 0 ) );
+        assertEquals( 2, slow.processors( 0 ) );
     }
 
     @Test
-    void shouldRemoveCPUsFromTooFastStepEvenIfNotAllPermitsAreUsed()
+    void shouldNotMoveProcessorFromOverlyAssignedStepIfRemainingPermits()
     {
         // GIVEN
-        Configuration config = config( 10, 20 );
+        Configuration config = config( 10, 10 );
         DynamicProcessorAssigner assigner = new DynamicProcessorAssigner( config );
-        Step<?> wayFastest = spy( stepWithStats( "wayFastest", 0,
-                Keys.avg_processing_time, 50L,
-                Keys.done_batches, 20L )
-                .setProcessors( 5 ) );
-        Step<?> fast = spy( stepWithStats( "fast", 0,
-                Keys.avg_processing_time, 100L,
-                Keys.done_batches, 20L )
-                .setProcessors( 3 ) );
-        Step<?> slow = stepWithStats( "slow", 1,
-                Keys.avg_processing_time, 220L,
-                Keys.done_batches, 20L );
+        Step<?> wayFastest = stepWithStats( "wayFastest", 0, Keys.avg_processing_time, 50L, Keys.done_batches, 20L );
+        Step<?> fast = stepWithStats( "fast", 0, Keys.avg_processing_time, 100L, Keys.done_batches, 20L ).setProcessors( 3 );
+        Step<?> slow = stepWithStats( "slow", 2, Keys.avg_processing_time, 220L, Keys.done_batches, 20L );
         StageExecution execution = executionOf( config, slow, wayFastest, fast );
         assigner.start( execution );
 
         // WHEN
         assigner.check( execution );
 
-        // THEN
-        verify( wayFastest ).processors( -1 );
+        // THEN no processor have been removed from the fast step
+        assertEquals( 3, fast.processors( 0 ) );
+        // although there were some to assign so slow step got one
+        assertEquals( 2, slow.processors( 0 ) );
     }
 
     private Configuration config( final int movingAverage, int processors )
