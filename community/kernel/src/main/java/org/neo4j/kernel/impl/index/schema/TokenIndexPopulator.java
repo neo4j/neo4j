064372874f56378e4e3f7c4f32a4ae520903b9ac
Emma Holmberg Ohlsson <emma.holmberg.ohlsson@neo4j.com>
2021-01-04 14:26:39 +0100

    Index populator for token indexes

---
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/index/schema/TokenIndexPopulator.java
@@ -0,0 +1,415 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel.impl.index.schema;
+
+import org.eclipse.collections.api.set.ImmutableSet;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.function.Consumer;
+
+import org.neo4j.annotations.documented.ReporterFactory;
+import org.neo4j.common.EntityType;
+import org.neo4j.configuration.Config;
+import org.neo4j.configuration.GraphDatabaseInternalSettings;
+import org.neo4j.index.internal.gbptree.GBPTree;
+import org.neo4j.index.internal.gbptree.GBPTreeConsistencyCheckVisitor;
+import org.neo4j.index.internal.gbptree.RecoveryCleanupWorkCollector;
+import org.neo4j.io.IOUtils;
+import org.neo4j.io.fs.FileSystemAbstraction;
+import org.neo4j.io.layout.DatabaseLayout;
+import org.neo4j.io.pagecache.IOLimiter;
+import org.neo4j.io.pagecache.PageCache;
+import org.neo4j.io.pagecache.PageCursor;
+import org.neo4j.io.pagecache.tracing.PageCacheTracer;
+import org.neo4j.io.pagecache.tracing.cursor.PageCursorTracer;
+import org.neo4j.kernel.api.exceptions.index.IndexEntryConflictException;
+import org.neo4j.kernel.api.index.IndexPopulator;
+import org.neo4j.kernel.api.index.IndexSample;
+import org.neo4j.kernel.api.index.IndexUpdater;
+import org.neo4j.monitoring.Monitors;
+import org.neo4j.storageengine.api.IndexEntryUpdate;
+import org.neo4j.storageengine.api.NodePropertyAccessor;
+import org.neo4j.util.Preconditions;
+
+import static org.eclipse.collections.impl.factory.Sets.immutable;
+import static org.neo4j.index.internal.gbptree.GBPTree.NO_HEADER_READER;
+import static org.neo4j.index.internal.gbptree.GBPTree.NO_HEADER_WRITER;
+
+public class TokenIndexPopulator implements IndexPopulator, ConsistencyCheckable
+{
+    /**
+     * Written in header to indicate native token index is clean
+     *
+     * NOTE that this is not the same byte as the other indexes use,
+     * to be able to handle the switch from old scan stores without rebuilding.
+     */
+    private static final byte CLEAN = (byte) 0x00;
+    static final byte ONLINE = CLEAN;
+
+    /**
+     * Written in header to indicate native token index is/needs rebuilding
+     *
+     * NOTE that this is not the same byte as the other indexes use,
+     * to be able to handle the switch from old scan stores without rebuilding.
+     */
+    private static final byte NEEDS_REBUILDING = (byte) 0x01;
+    static final byte POPULATING = NEEDS_REBUILDING;
+
+    /**
+     * Written in header to indicate native token index failed to population.
+     *
+     * NOTE that this is not the same byte as the other indexes use,
+     * to be able to handle the switch from old scan stores without rebuilding.
+     */
+    static final byte FAILED = (byte) 0x02;
+
+    /**
+     * The type of entity this token index is backing.
+     */
+    private final EntityType entityType;
+
+    /**
+     * Whether or not this token index is read-only.
+     */
+    private final boolean readOnly;
+
+    /**
+     * Monitoring internal events.
+     */
+    private final TokenScanStore.Monitor monitor;
+
+    /**
+     * Monitors used to pass down monitor to underlying {@link GBPTree}
+     */
+    private final Monitors monitors;
+
+    /**
+     * {@link PageCache} to {@link PageCache#map(Path, int, ImmutableSet)}
+     * store file backing this token scan store. Passed to {@link GBPTree}.
+     */
+    private final PageCache pageCache;
+
+    /**
+     * IndexFiles wrapping the store file {@link PageCache#map(Path, int, ImmutableSet)}.
+     */
+    private final IndexFiles indexFiles;
+
+    /**
+     * {@link FileSystemAbstraction} the backing file lives on.
+     */
+    private final FileSystemAbstraction fs;
+
+    /**
+     * Layout of the database.
+     */
+    private final DatabaseLayout directoryStructure;
+    private final Config config;
+    private final PageCacheTracer cacheTracer;
+
+    /**
+     * The actual index which backs this token index.
+     */
+    private GBPTree<TokenScanKey,TokenScanValue> index;
+
+    /**
+     * The single instance of {@link NativeTokenScanWriter} used for updates.
+     */
+    private NativeTokenIndexUpdater singleUpdater;
+
+    /**
+     * Monitor for all writes going into this token index.
+     */
+    private NativeTokenScanWriter.WriteMonitor writeMonitor;
+
+    /**
+     * Name of the store that will be used when describing work related to this store.
+     */
+    private final String tokenStoreName;
+
+    /**
+     * Write rebuilding bit to header.
+     */
+    private static final Consumer<PageCursor> needsRebuildingWriter =
+            pageCursor -> pageCursor.putByte( POPULATING );
+
+    private byte[] failureBytes;
+    private boolean dropped;
+    private boolean closed;
+
+    TokenIndexPopulator( PageCache pageCache, DatabaseLayout directoryStructure, IndexFiles indexFiles, FileSystemAbstraction fs,
+            boolean readOnly, Config config, Monitors monitors, EntityType entityType, PageCacheTracer cacheTracer, String tokenStoreName )
+    {
+        this.pageCache = pageCache;
+        this.indexFiles = indexFiles;
+        this.fs = fs;
+        this.directoryStructure = directoryStructure;
+        this.config = config;
+        this.cacheTracer = cacheTracer;
+        this.readOnly = readOnly;
+        this.monitors = monitors;
+        String monitorTag =
+                entityType == EntityType.NODE ? TokenScanStore.LABEL_SCAN_STORE_MONITOR_TAG : TokenScanStore.RELATIONSHIP_TYPE_SCAN_STORE_MONITOR_TAG;
+        this.monitor = monitors.newMonitor( TokenScanStore.Monitor.class, monitorTag );
+        this.entityType = entityType;
+        this.tokenStoreName = tokenStoreName;
+    }
+
+    private void instantiateTree( RecoveryCleanupWorkCollector recoveryCleanupWorkCollector, Consumer<PageCursor> headerWriter )
+    {
+        monitors.addMonitorListener( treeMonitor() );
+        GBPTree.Monitor monitor = monitors.newMonitor( GBPTree.Monitor.class );
+
+        index = new GBPTree<>( pageCache, indexFiles.getStoreFile(), new TokenScanLayout(), monitor, NO_HEADER_READER,
+                headerWriter, recoveryCleanupWorkCollector, readOnly, cacheTracer, immutable.empty(), tokenStoreName );
+    }
+
+    private GBPTree.Monitor treeMonitor()
+    {
+        return new TokenIndexTreeMonitor();
+    }
+
+    @Override
+    public synchronized void create()
+    {
+        assertNotDropped();
+        assertNotClosed();
+
+        indexFiles.clear();
+        instantiateTree( RecoveryCleanupWorkCollector.immediate(), needsRebuildingWriter );
+
+        writeMonitor = config.get( GraphDatabaseInternalSettings.token_scan_write_log_enabled )
+                       ? new TokenScanWriteMonitor( fs, directoryStructure, entityType, config )
+                       : NativeTokenScanWriter.EMPTY;
+        singleUpdater = new NativeTokenIndexUpdater( 1_000, writeMonitor );
+    }
+
+    @Override
+    public synchronized void drop()
+    {
+        try
+        {
+            if ( index != null )
+            {
+                index.setDeleteOnClose( true );
+            }
+            closeTree();
+            indexFiles.clear();
+        }
+        finally
+        {
+            dropped = true;
+            closed = true;
+        }
+    }
+
+    private void closeTree()
+    {
+        IOUtils.closeAllUnchecked( index );
+        index = null;
+        if ( writeMonitor != null )
+        {
+            writeMonitor.close();
+        }
+    }
+
+    @Override
+    public void add( Collection<? extends IndexEntryUpdate<?>> updates, PageCursorTracer cursorTracer ) throws IndexEntryConflictException
+    {
+        try ( NativeTokenIndexUpdater updater = singleUpdater.initialize( index.writer( cursorTracer ) ) )
+        {
+            for ( IndexEntryUpdate<?> update : updates )
+            {
+                updater.process( update );
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new UncheckedIOException( e );
+        }
+    }
+
+    @Override
+    public void verifyDeferredConstraints( NodePropertyAccessor nodePropertyAccessor )
+    {
+        // No-op, token indexes don't have any uniqueness constraints.
+    }
+
+    @Override
+    public IndexUpdater newPopulatingUpdater( NodePropertyAccessor accessor, PageCursorTracer cursorTracer )
+    {
+        try
+        {
+            return singleUpdater.initialize( index.writer( cursorTracer ) );
+        }
+        catch ( IOException e )
+        {
+            throw new UncheckedIOException( e );
+        }
+    }
+
+    @Override
+    public synchronized void close( boolean populationCompletedSuccessfully, PageCursorTracer cursorTracer )
+    {
+        if ( populationCompletedSuccessfully && failureBytes != null )
+        {
+            throw new IllegalStateException( "Can't mark index as online after it has been marked as failure" );
+        }
+
+        try
+        {
+            assertNotDropped();
+            if ( populationCompletedSuccessfully )
+            {
+                // Successful and completed population
+                assertPopulatorOpen();
+                flushTreeAndMarkAs( ONLINE, cursorTracer );
+            }
+            else if ( failureBytes != null )
+            {
+                // Failed population
+                ensureTreeInstantiated();
+                markTreeAsFailed( cursorTracer );
+            }
+            // else cancelled population. Here we simply close the tree w/o checkpointing it and it will look like POPULATING state on next open
+        }
+        finally
+        {
+            closeTree();
+            closed = true;
+        }
+    }
+
+    private void flushTreeAndMarkAs( byte state, PageCursorTracer cursorTracer )
+    {
+        index.checkpoint( IOLimiter.UNLIMITED, pageCursor -> pageCursor.putByte( state ), cursorTracer );
+    }
+
+    private void markTreeAsFailed( PageCursorTracer cursorTracer )
+    {
+        Preconditions.checkState( failureBytes != null, "markAsFailed hasn't been called, populator not actually failed?" );
+        index.checkpoint( IOLimiter.UNLIMITED, new FailureHeaderWriter( failureBytes, FAILED ), cursorTracer );
+    }
+
+    @Override
+    public void markAsFailed( String failure )
+    {
+        failureBytes = failure.getBytes( StandardCharsets.UTF_8 );
+    }
+
+    @Override
+    public void includeSample( IndexEntryUpdate<?> update )
+    {
+        // We don't do sampling for token indexes since that information is available in other ways.
+    }
+
+    @Override
+    public IndexSample sample( PageCursorTracer cursorTracer )
+    {
+        throw new UnsupportedOperationException( "Token indexes does not support index sampling" );
+    }
+
+    @Override
+    public boolean consistencyCheck( ReporterFactory reporterFactory, PageCursorTracer cursorTracer )
+    {
+        //noinspection unchecked
+        return consistencyCheck( reporterFactory.getClass( GBPTreeConsistencyCheckVisitor.class ), cursorTracer );
+    }
+
+    private boolean consistencyCheck( GBPTreeConsistencyCheckVisitor<TokenScanKey> visitor, PageCursorTracer cursorTracer )
+    {
+        try
+        {
+            return index.consistencyCheck( visitor, cursorTracer );
+        }
+        catch ( IOException e )
+        {
+            throw new UncheckedIOException( e );
+        }
+    }
+
+    private void assertNotDropped()
+    {
+        if ( dropped )
+        {
+            throw new IllegalStateException( "Populator has already been dropped." );
+        }
+    }
+
+    private void assertNotClosed()
+    {
+        if ( closed )
+        {
+            throw new IllegalStateException( "Populator has already been closed." );
+        }
+    }
+
+    private void ensureTreeInstantiated()
+    {
+        if ( index == null )
+        {
+            instantiateTree( RecoveryCleanupWorkCollector.ignore(), NO_HEADER_WRITER );
+        }
+    }
+
+    private void assertPopulatorOpen()
+    {
+        if ( index == null )
+        {
+            throw new IllegalStateException( "Populator has already been closed." );
+        }
+    }
+
+    private class TokenIndexTreeMonitor extends GBPTree.Monitor.Adaptor
+    {
+        @Override
+        public void cleanupRegistered()
+        {
+            monitor.recoveryCleanupRegistered();
+        }
+
+        @Override
+        public void cleanupStarted()
+        {
+            monitor.recoveryCleanupStarted();
+        }
+
+        @Override
+        public void cleanupFinished( long numberOfPagesVisited, long numberOfTreeNodes, long numberOfCleanedCrashPointers, long durationMillis )
+        {
+            monitor.recoveryCleanupFinished( numberOfPagesVisited, numberOfTreeNodes, numberOfCleanedCrashPointers, durationMillis );
+        }
+
+        @Override
+        public void cleanupClosed()
+        {
+            monitor.recoveryCleanupClosed();
+        }
+
+        @Override
+        public void cleanupFailed( Throwable throwable )
+        {
+            monitor.recoveryCleanupFailed( throwable );
+        }
+    }
+}
---
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/index/schema/TokenIndexPopulatorTests.java
@@ -0,0 +1,319 @@
+/*
+ * Copyright (c) 2002-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel.impl.index.schema;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.eclipse.collections.api.map.primitive.MutableLongObjectMap;
+import org.eclipse.collections.impl.factory.primitive.LongObjectMaps;
+import org.eclipse.collections.impl.list.mutable.primitive.LongArrayList;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.neo4j.common.EntityType;
+import org.neo4j.configuration.Config;
+import org.neo4j.index.internal.gbptree.GBPTree;
+import org.neo4j.index.internal.gbptree.Layout;
+import org.neo4j.index.internal.gbptree.Seeker;
+import org.neo4j.internal.schema.IndexDescriptor;
+import org.neo4j.io.fs.FileSystemAbstraction;
+import org.neo4j.io.layout.DatabaseFile;
+import org.neo4j.io.layout.DatabaseLayout;
+import org.neo4j.io.pagecache.PageCache;
+import org.neo4j.io.pagecache.tracing.PageCacheTracer;
+import org.neo4j.kernel.api.exceptions.index.IndexEntryConflictException;
+import org.neo4j.kernel.api.index.IndexUpdater;
+import org.neo4j.monitoring.Monitors;
+import org.neo4j.storageengine.api.IndexEntryUpdate;
+import org.neo4j.storageengine.api.NodePropertyAccessor;
+import org.neo4j.storageengine.api.TokenIndexEntryUpdate;
+import org.neo4j.test.rule.TestDirectory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.neo4j.collection.PrimitiveLongCollections.EMPTY_LONG_ARRAY;
+import static org.neo4j.io.pagecache.tracing.cursor.PageCursorTracer.NULL;
+import static org.neo4j.kernel.impl.api.index.PhaseTracker.nullInstance;
+
+class TokenIndexPopulatorTests extends IndexPopulatorTests<TokenScanKey,TokenScanValue,Layout<TokenScanKey, TokenScanValue>>
+{
+    @Override
+    IndexFiles createIndexFiles( FileSystemAbstraction fs, TestDirectory directory, IndexDescriptor indexDescriptor )
+    {
+        return new IndexFiles.SingleFile( fs, directory.homePath().resolve( DatabaseFile.LABEL_SCAN_STORE.getName() ) );
+    }
+
+    @Override
+    IndexDescriptor indexDescriptor()
+    {
+        //Not used for token indexes yet
+        return null;
+    }
+
+    @Override
+    Layout<TokenScanKey,TokenScanValue> createLayout()
+    {
+        return new TokenScanLayout();
+    }
+
+    @Override
+    byte failureByte()
+    {
+        return TokenIndexPopulator.FAILED;
+    }
+
+    @Override
+    byte populatingByte()
+    {
+        return TokenIndexPopulator.POPULATING;
+    }
+
+    @Override
+    byte onlineByte()
+    {
+        return TokenIndexPopulator.ONLINE;
+    }
+
+    @Override
+    TokenIndexPopulator createPopulator( PageCache pageCache )
+    {
+        return new TokenIndexPopulator( pageCache, DatabaseLayout.ofFlat( directory.homePath() ), indexFiles, fs, false, Config.defaults(),
+                new Monitors(), EntityType.NODE, PageCacheTracer.NULL, "Label Scan Store" );
+    }
+
+    @Test
+    void addShouldApplyAllUpdatesOnce() throws Exception
+    {
+        // Give
+        MutableLongObjectMap<long[]> entityTokens = LongObjectMaps.mutable.empty();
+
+        populator.create();
+
+        List<TokenIndexEntryUpdate<?>> updates = generateSomeRandomUpdates( entityTokens );
+        // Add updates to populator
+        populator.add( updates, NULL );
+
+        populator.scanCompleted( nullInstance, populationWorkScheduler, NULL );
+        populator.close( true, NULL );
+
+        verifyUpdates( entityTokens );
+    }
+
+    @Test
+    void updaterShouldApplyUpdates() throws Exception
+    {
+        // Give
+        MutableLongObjectMap<long[]> entityTokens = LongObjectMaps.mutable.empty();
+
+        populator.create();
+
+        List<TokenIndexEntryUpdate<?>> updates = generateSomeRandomUpdates( entityTokens );
+
+        try ( IndexUpdater updater = populator.newPopulatingUpdater( null_property_accessor, NULL ) )
+        {
+            for ( TokenIndexEntryUpdate<?> update : updates )
+            {
+                updater.process( update );
+            }
+        }
+
+        // then
+        populator.scanCompleted( nullInstance, populationWorkScheduler, NULL );
+        populator.close( true, NULL );
+        verifyUpdates( entityTokens );
+    }
+
+    @Test
+    void updaterMustThrowIfProcessAfterClose() throws Exception
+    {
+        // given
+        populator.create();
+        IndexUpdater updater = populator.newPopulatingUpdater( null_property_accessor, NULL );
+
+        // when
+        updater.close();
+
+        IllegalStateException e = assertThrows( IllegalStateException.class,
+                () -> updater.process( IndexEntryUpdate.change( random.nextInt(), null, EMPTY_LONG_ARRAY, generateRandomTokens() ) ) );
+        assertThat( e ).hasMessageContaining( "Updater has been closed" );
+        populator.close( true, NULL );
+    }
+
+    @Test
+    void shouldHandleInterleavedRandomizedUpdates() throws IndexEntryConflictException, IOException
+    {
+        // Give
+        int numberOfEntities = 1_000;
+        long currentScanId = 0;
+        MutableLongObjectMap<long[]> entityTokens = LongObjectMaps.mutable.empty();
+
+        populator.create();
+
+        while ( currentScanId < numberOfEntities )
+        {
+            // Collect a batch of max 100 updates from scan
+            List<TokenIndexEntryUpdate<?>> updates = new ArrayList<>();
+            for ( int i = 0; i < 100 && currentScanId < numberOfEntities; i++ )
+            {
+                generateRandomUpdate( currentScanId, entityTokens, updates );
+
+                // Advance scan
+                currentScanId++;
+            }
+            // Add updates to populator
+            populator.add( updates, NULL );
+
+            // Interleave external updates in id range lower than currentScanId
+            try ( IndexUpdater updater = populator.newPopulatingUpdater( NodePropertyAccessor.EMPTY, NULL ) )
+            {
+                for ( int i = 0; i < 100; i++ )
+                {
+                    long entityId = random.nextLong( currentScanId );
+                    // Current tokens for the entity in the tree
+                    long[] beforeTokens = entityTokens.get( entityId );
+                    if ( beforeTokens == null )
+                    {
+                        beforeTokens = EMPTY_LONG_ARRAY;
+                    }
+                    long[] afterTokens = generateRandomTokens();
+                    entityTokens.put( entityId, Arrays.copyOf( afterTokens, afterTokens.length ) );
+                    updater.process( IndexEntryUpdate.change( entityId, null, beforeTokens, afterTokens ) );
+                }
+            }
+        }
+
+        populator.scanCompleted( nullInstance, populationWorkScheduler, NULL );
+        populator.close( true, NULL );
+
+        verifyUpdates( entityTokens );
+    }
+
+    private void generateRandomUpdate( long entityId, MutableLongObjectMap<long[]> trackingState, List<TokenIndexEntryUpdate<?>> updates )
+    {
+        long[] addTokens = generateRandomTokens();
+        if ( addTokens.length != 0 )
+        {
+            TokenIndexEntryUpdate<?> update = IndexEntryUpdate.change( entityId, null, EMPTY_LONG_ARRAY, addTokens );
+            updates.add( update );
+
+            // Add update to tracking structure
+            trackingState.put( entityId, Arrays.copyOf( addTokens, addTokens.length ) );
+        }
+    }
+
+    private List<TokenIndexEntryUpdate<?>> generateSomeRandomUpdates( MutableLongObjectMap<long[]> entityTokens )
+    {
+        long currentScanId = 0;
+        List<TokenIndexEntryUpdate<?>> updates = new ArrayList<>();
+        for ( int i = 0; i < 10; i++ )
+        {
+            generateRandomUpdate( currentScanId, entityTokens, updates );
+
+            // Advance scan
+            currentScanId++;
+        }
+        return updates;
+    }
+
+    /**
+     * Generate array of random tokens.
+     * Generated array is empty with a certain probability.
+     * Generated array contains specific tokens with different probability to get varying distribution - some bitset
+     * should be quite full, while others should be quite empty and more likely to become empty with later updates.
+     */
+    private long[] generateRandomTokens()
+    {
+        long[] allTokens = new long[]{1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L};
+        double[] allTokensRatio = new double[]{0.9, 0.8, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.01, 0.001};
+        double emptyRatio = 0.1;
+
+        if ( random.nextDouble() < emptyRatio )
+        {
+            return EMPTY_LONG_ARRAY;
+        }
+        else
+        {
+            LongArrayList longArrayList = new LongArrayList();
+
+            for ( int i = 0; i < allTokens.length; i++ )
+            {
+                if ( random.nextDouble() < allTokensRatio[i] )
+                {
+                    longArrayList.add( allTokens[i] );
+                }
+            }
+            return longArrayList.toArray();
+        }
+    }
+
+    /**
+     * Compares the state of the tree with the expected values.
+     * @param expected Mapping from entity id to expected entity tokens.
+     */
+    private void verifyUpdates( MutableLongObjectMap<long[]> expected ) throws IOException
+    {
+        // Verify that everything in the tree is expected to exist.
+        try ( GBPTree<TokenScanKey,TokenScanValue> tree = getTree();
+              Seeker<TokenScanKey,TokenScanValue> scan = scan( tree ) )
+        {
+            while ( scan.next() )
+            {
+                TokenScanKey key = scan.key();
+                TokenScanValue value = scan.value();
+                long entityIdBase = key.idRange * TokenScanValue.RANGE_SIZE;
+
+                for ( int i = 0; i < TokenScanValue.RANGE_SIZE; i++ )
+                {
+                    long mask = 1L << i;
+                    long posInBits = value.bits & mask;
+                    if ( posInBits != 0 )
+                    {
+                        long entity = entityIdBase + i;
+                        long[] tokens = expected.remove( entity );
+                        assertThat( tokens ).withFailMessage( "Entity " + entity + " contained unexpected token " + key.tokenId + " in tree" )
+                                .contains( key.tokenId );
+
+                        // Put back the rest of the tokens that we haven't verified yet
+                        if ( tokens.length != 1 )
+                        {
+                            expected.put( entity, ArrayUtils.removeElement( tokens, key.tokenId ) );
+                        }
+                    }
+                }
+            }
+        }
+
+        // Verify that nothing expected was missing from the tree
+        expected.forEachKeyValue( ( entityId, tokenIds ) ->
+                assertThat( tokenIds ).withFailMessage( "Tokens " + Arrays.toString( tokenIds ) + " not found in tree for entity " + entityId ).isEmpty() );
+    }
+
+    private Seeker<TokenScanKey,TokenScanValue> scan( GBPTree<TokenScanKey,TokenScanValue> tree ) throws IOException
+    {
+        TokenScanKey lowest = layout.newKey();
+        layout.initializeAsLowest( lowest );
+        TokenScanKey highest = layout.newKey();
+        layout.initializeAsHighest( highest );
+        return tree.seek( lowest, highest, NULL );
+    }
+}
