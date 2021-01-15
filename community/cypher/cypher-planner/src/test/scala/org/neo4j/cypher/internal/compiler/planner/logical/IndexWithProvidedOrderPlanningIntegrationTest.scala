529af64f42118ae9cb4450f422f0f2392a91fd94
Satia Herfert <satiaherfert@gmx.de>
2021-01-15 13:18:41 +0100

    NodeLeftOuterHashJoin cannot provide DESC order.

--- community/cypher/cypher-planner/src/test/scala/org/neo4j/cypher/internal/compiler/planner/logical/IndexWithProvidedOrderPlanningIntegrationTest.scala
+++ community/cypher/cypher-planner/src/test/scala/org/neo4j/cypher/internal/compiler/planner/logical/IndexWithProvidedOrderPlanningIntegrationTest.scala
@@ -623,6 +623,8 @@ abstract class IndexWithProvidedOrderPlanningIntegrationTest(queryGraphSolverSet
     }
 
     test(s"$cypherToken-$orderCapability: Order by index backed property in a plan with an outer join") {
+      // Left outer hash join can only maintain ASC order
+      assume(sortOrder == Ascending)
       val plan = new given {
         indexOn("A", "prop").providesOrder(orderCapability)
         cardinality = mapCardinality {
