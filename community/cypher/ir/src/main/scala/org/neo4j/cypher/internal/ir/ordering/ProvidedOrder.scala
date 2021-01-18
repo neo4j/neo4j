1497ff4416011d498d384071df0b9c30cf33cdc5
Satia Herfert <satiaherfert@gmx.de>
2021-01-18 13:59:38 +0100

    Fix bug when RHS of NodeLeftOuterHashJoin is ordered by a join column.
    We did not catch all cases where that happened to remove the ProvidedOrder.

--- community/cypher/ir/src/main/scala/org/neo4j/cypher/internal/ir/ordering/ProvidedOrder.scala
+++ community/cypher/ir/src/main/scala/org/neo4j/cypher/internal/ir/ordering/ProvidedOrder.scala
@@ -139,7 +139,7 @@ case class NonEmptyProvidedOrder(allColumns: NonEmptyList[ColumnOrder], theOrder
   override def upToExcluding(args: Set[String]): ProvidedOrder = {
     val (_, trimmed) = columns.foldLeft((false,Seq.empty[ColumnOrder])) {
       case (acc, _) if acc._1 => acc
-      case (acc, col) if args.contains(col.expression.asCanonicalStringVal) => (true, acc._2)
+      case (acc, col) if args.intersect(col.dependencies.map(_.name)).nonEmpty => (true, acc._2)
       case (acc, col) => (acc._1, acc._2 :+ col)
     }
     if (trimmed.isEmpty) {
