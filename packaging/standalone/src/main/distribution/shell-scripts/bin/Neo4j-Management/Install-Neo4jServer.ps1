4bdf58c6a826ece7b276ff03fa026e8ac3590a5d
AndyHeap-NeoTech <andrew.heap@neotechnology.com>
2021-01-28 09:30:06 +0000

    Removal of -ComputerName '.' from powershell scripts. ()
    This is to ensure compatibility with PowerShell 7 whilst also not changing the script behaviours in earlier versions.

--- packaging/standalone/src/main/distribution/shell-scripts/bin/Neo4j-Management/Install-Neo4jServer.ps1
+++ packaging/standalone/src/main/distribution/shell-scripts/bin/Neo4j-Management/Install-Neo4jServer.ps1
@@ -56,7 +56,7 @@ function Install-Neo4jServer
   process
   {
     $ServiceName = Get-Neo4jWindowsServiceName -Neo4jServer $Neo4jServer -ErrorAction Stop
-    $Found = Get-Service -Name $ServiceName -ComputerName '.' -ErrorAction 'SilentlyContinue'
+    $Found = Get-Service -Name $ServiceName -ErrorAction 'SilentlyContinue'
     if (-not $Found)
     {
       $prunsrv = Get-Neo4jPrunsrv -Neo4jServer $Neo4jServer -ForServerInstall
