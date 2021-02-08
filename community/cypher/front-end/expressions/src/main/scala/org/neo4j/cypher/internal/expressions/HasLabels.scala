6348c0144dcfdcf77ff4a21b3bee60146b807e83
Love Leifland <love.leifland@neotechnology.com>
2021-02-08 13:46:34 +0100

    Support has labels or types in piplined runtime

--- community/cypher/front-end/expressions/src/main/scala/org/neo4j/cypher/internal/expressions/HasLabels.scala
+++ community/cypher/front-end/expressions/src/main/scala/org/neo4j/cypher/internal/expressions/HasLabels.scala
@@ -18,16 +18,25 @@ package org.neo4j.cypher.internal.expressions
 
 import org.neo4j.cypher.internal.util.InputPosition
 
+/*
+ * Checks if expression has all labels
+ */
 case class HasLabels(expression: Expression, labels: Seq[LabelName])(val position: InputPosition) extends BooleanExpression {
 
   override def asCanonicalStringVal = s"${expression.asCanonicalStringVal}${labels.map(_.asCanonicalStringVal).mkString(":", ":", "")}"
 }
 
+/*
+ * Checks if expression has all labels OR all types
+ */
 case class HasLabelsOrTypes(expression: Expression, labelsOrTypes: Seq[LabelOrRelTypeName])(val position: InputPosition) extends BooleanExpression {
 
   override def asCanonicalStringVal = s"${expression.asCanonicalStringVal}${labelsOrTypes.map(_.asCanonicalStringVal).mkString(":", ":", "")}"
 }
 
+/*
+ * Checks if expression has all types
+ */
 case class HasTypes(expression: Expression, types: Seq[RelTypeName])(val position: InputPosition) extends BooleanExpression {
 
   override def asCanonicalStringVal = s"${expression.asCanonicalStringVal}${types.map(_.asCanonicalStringVal).mkString(":", ":", "")}"
