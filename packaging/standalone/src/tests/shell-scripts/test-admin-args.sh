0781efd68d64ac451825a854a20cf6d152a48b05
Anton Klarén <anton.klaren@neotechnology.com>
2021-03-30 08:38:08 +0200

    Admin commands stop using the 'Xms' heap setting
    Server config is used as one source of heap settings for admin commands.
    
    That leads to ridiculous memory demands especially by simple admin commands like getting store info.
    
    This change is backward compatible-ish. The only slightly backward-incompatible thing is that if an admin command requires a lot of memory and the machine does not have it, the command will not fail at the start but some time later, which might be slightly annoying for long-running commands like backup or consistency checking.
    
    'Xmx' is still taken from the config in order to be backward compatible.

--- packaging/standalone/src/tests/shell-scripts/test-admin-args.sh
+++ packaging/standalone/src/tests/shell-scripts/test-admin-args.sh
@@ -36,21 +36,25 @@ test_expect_success "should let higher heap size env var override memory setting
   clear_config &&
   set_config 'dbms.memory.heap.max_size' '222m' neo4j.conf &&
   HEAP_SIZE=666m neo4j-home/bin/neo4j-admin backup &&
-  test_expect_java_arg '-Xmx666m' &&
-  test_expect_java_arg '-Xms666m'
+  test_expect_java_arg '-Xmx666m'
 "
 
 test_expect_success "should let lower heap size env var override memory setting when both given" "
   clear_config &&
   set_config 'dbms.memory.heap.max_size' '777m' neo4j.conf &&
   HEAP_SIZE=222m neo4j-home/bin/neo4j-admin backup &&
-  test_expect_java_arg '-Xmx222m' &&
-  test_expect_java_arg '-Xms222m'
+  test_expect_java_arg '-Xmx222m'
 "
 
 test_expect_success "should pass parallel collector option" "
   neo4j-home/bin/neo4j-admin &&
   test_expect_java_arg '-XX:+UseParallelGC'
+"
+
+test_expect_success "should not set -Xms" "
+  HEAP_SIZE=222m neo4j-home/bin/neo4j-admin &&
+  test_expect_java_arg '-Xmx222m' &&
+  test_expect_no_java_arg '-Xms222m'
 "
 
 test_done
