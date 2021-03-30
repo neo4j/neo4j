57fa5375e78fc0ade6db990c3f9b813f5e13aa1f
Anton Klarén <anton.klaren@neotechnology.com>
2021-03-30 08:38:08 +0200

    Admin commands stop using the 'Xms' heap setting
    Server config is used as one source of heap settings for admin commands.
    
    That leads to ridiculous memory demands especially by simple admin commands like getting store info.
    
    This change is backward compatible-ish. The only slightly backward-incompatible thing is that if an admin command requires a lot of memory and the machine does not have it, the command will not fail at the start but some time later, which might be slightly annoying for long-running commands like backup or consistency checking.
    
    'Xmx' is still taken from the config in order to be backward compatible.

--- packaging/standalone/src/tests/shell-scripts/test-admin-args.sh
+++ packaging/standalone/src/tests/shell-scripts/test-admin-args.sh
@@ -30,8 +30,7 @@ test_expect_success "should let higher heap size env var override memory setting
   set_config 'dbms.memory.heap.initial_size' '111m' neo4j.conf &&
   set_config 'dbms.memory.heap.max_size' '222m' neo4j.conf &&
   HEAP_SIZE=666m neo4j-home/bin/neo4j-admin backup &&
-  test_expect_java_arg '-Xmx666m' &&
-  test_expect_java_arg '-Xms666m'
+  test_expect_java_arg '-Xmx666m'
 "
 
 test_expect_success "should let lower heap size env var override memory setting when both given" "
@@ -39,13 +38,18 @@ test_expect_success "should let lower heap size env var override memory setting 
   set_config 'dbms.memory.heap.initial_size' '666m' neo4j.conf &&
   set_config 'dbms.memory.heap.max_size' '777m' neo4j.conf &&
   HEAP_SIZE=222m neo4j-home/bin/neo4j-admin backup &&
-  test_expect_java_arg '-Xmx222m' &&
-  test_expect_java_arg '-Xms222m'
+  test_expect_java_arg '-Xmx222m'
 "
 
 test_expect_success "should pass parallel collector option" "
   neo4j-home/bin/neo4j-admin &&
   test_expect_java_arg '-XX:+UseParallelGC'
+"
+
+test_expect_success "should not set -Xms" "
+  HEAP_SIZE=222m neo4j-home/bin/neo4j-admin &&
+  test_expect_java_arg '-Xmx222m' &&
+  test_expect_no_java_arg '-Xms222m'
 "
 
 test_done
