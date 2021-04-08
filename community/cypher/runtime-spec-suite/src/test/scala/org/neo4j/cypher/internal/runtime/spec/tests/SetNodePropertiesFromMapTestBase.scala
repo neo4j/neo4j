3c6c5e07b6baf62281e1946cc818128149734d7c
Pontus Melke <pontusmelke@gmail.com>
2021-04-08 20:05:15 +0200

    fix test that was order dependent

--- community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/SetNodePropertiesFromMapTestBase.scala
+++ community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/SetNodePropertiesFromMapTestBase.scala
@@ -181,9 +181,9 @@ abstract class SetNodePropertiesFromMapTestBase[CONTEXT <: RuntimeContext](
     // then
     val runtimeResult: RecordingRuntimeResult = execute(logicalQuery, runtime)
     consume(runtimeResult)
-    val properties = tx.getAllPropertyKeys.asScala.toList
+    val properties = tx.getAllPropertyKeys.asScala.toList.sorted
     runtimeResult should beColumns("p1", "p2", "p3").withSingleRow(null, 0, 1).withStatistics(propertiesSet = 3)
-    properties shouldBe Seq("prop", "propCopy", "newProp")
+    properties shouldBe Seq("newProp", "prop", "propCopy")
   }
 
 
