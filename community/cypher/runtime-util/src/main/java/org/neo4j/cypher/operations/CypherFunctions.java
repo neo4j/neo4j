138cc635ff5292260ba163a525a8a4daeffa905a
Love Kristofer Leifland <love.leifland@neotechnology.com>
2021-04-12 13:58:52 +0200

    Support arbitrary fused IN expressions ()
    Support arbitrary fused IN expressions

--- community/cypher/runtime-util/src/main/java/org/neo4j/cypher/operations/CypherFunctions.java
+++ community/cypher/runtime-util/src/main/java/org/neo4j/cypher/operations/CypherFunctions.java
@@ -36,6 +36,7 @@ import org.neo4j.internal.kernel.api.PropertyCursor;
 import org.neo4j.internal.kernel.api.RelationshipScanCursor;
 import org.neo4j.util.CalledFromGeneratedCode;
 import org.neo4j.values.AnyValue;
+import org.neo4j.values.Equality;
 import org.neo4j.values.SequenceValue;
 import org.neo4j.values.storable.ArrayValue;
 import org.neo4j.values.storable.BooleanValue;
@@ -1477,6 +1478,45 @@ public final class CypherFunctions
         {
             return VirtualValues.list( collection );
         }
+    }
+
+    @CalledFromGeneratedCode
+    public static AnyValue in( AnyValue findMe, AnyValue lookIn )
+    {
+        if ( lookIn == NO_VALUE )
+        {
+            return NO_VALUE;
+        }
+
+        var iterator = asList( lookIn ).iterator();
+
+        if ( !iterator.hasNext() )
+        {
+            return BooleanValue.FALSE;
+        }
+
+        if ( findMe == NO_VALUE )
+        {
+            return NO_VALUE;
+        }
+
+        var undefinedEquality = false;
+        while ( iterator.hasNext() )
+        {
+            var nextValue = iterator.next();
+            var equality = nextValue.ternaryEquals( findMe );
+
+            if ( equality == Equality.TRUE )
+            {
+                return BooleanValue.TRUE;
+            }
+            else if ( equality == Equality.UNDEFINED && !undefinedEquality )
+            {
+                undefinedEquality = true;
+            }
+        }
+
+        return undefinedEquality ? NO_VALUE : BooleanValue.FALSE;
     }
 
     @CalledFromGeneratedCode
