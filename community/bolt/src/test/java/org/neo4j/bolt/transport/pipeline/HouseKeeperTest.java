e01a9607166fe3b3cc11690f350dc0aa62ac7188
Johannes Donath <johannesdonath@gmail.com>
2021-04-30 09:25:54 +0200

    Corrected a potential NPE within the HouseKeeper handler when an exception message is set to null. ()

--- community/bolt/src/test/java/org/neo4j/bolt/transport/pipeline/HouseKeeperTest.java
+++ community/bolt/src/test/java/org/neo4j/bolt/transport/pipeline/HouseKeeperTest.java
@@ -29,6 +29,7 @@ import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.timeout.ReadTimeoutException;
 import io.netty.util.concurrent.EventExecutor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -212,6 +213,26 @@ public class HouseKeeperTest
         // Then
         assertThat( logProvider ).forClass( HouseKeeper.class ).forLevel( WARN ).containsMessageWithArguments(
                 "Fatal error occurred when handling a client connection, " + "remote peer unexpectedly closed connection: %s", channel );
+    }
+
+    @Test
+    void shouldHandleExceptionsWithNullMessages()
+    {
+        // Given
+        AssertableLogProvider logProvider = new AssertableLogProvider();
+        HouseKeeper keeper = new HouseKeeper( null, logProvider.getLog( HouseKeeper.class ) );
+        Channel channel = mock( Channel.class );
+        when( channel.toString() ).thenReturn( "[some channel info]" );
+        ChannelHandlerContext ctx = mock( ChannelHandlerContext.class );
+        when( ctx.channel() ).thenReturn( channel );
+        when( ctx.executor() ).thenReturn( mock( EventExecutor.class ) );
+
+        // When
+        keeper.exceptionCaught( ctx, ReadTimeoutException.INSTANCE );
+
+        // Then
+        assertThat( logProvider ).forClass( HouseKeeper.class ).forLevel( ERROR ).containsMessageWithException(
+                "Fatal error occurred when handling a client connection: " + ctx.channel(), ReadTimeoutException.INSTANCE );
     }
 
     private static Bootstrap newBootstrap( HouseKeeper houseKeeper )
