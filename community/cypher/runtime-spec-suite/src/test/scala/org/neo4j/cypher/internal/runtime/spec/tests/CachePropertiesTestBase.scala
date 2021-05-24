e413e293736821882304b7118831825cfb8e36b6
Pontus Melke <pontusmelke@gmail.com>
2021-05-24 13:50:58 +0200

    Fix property caching in fused node index seeks
    After a continuation we failed to cache properties for exact node
    index seeks.

--- community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/CachePropertiesTestBase.scala
+++ community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/CachePropertiesTestBase.scala
@@ -21,6 +21,7 @@ package org.neo4j.cypher.internal.runtime.spec.tests
 
 import org.neo4j.cypher.internal.CypherRuntime
 import org.neo4j.cypher.internal.RuntimeContext
+import org.neo4j.cypher.internal.logical.plans.GetValue
 import org.neo4j.cypher.internal.runtime.spec.Edition
 import org.neo4j.cypher.internal.runtime.spec.LogicalQueryBuilder
 import org.neo4j.cypher.internal.runtime.spec.RuntimeTestSuite
@@ -31,22 +32,117 @@ abstract class CachePropertiesTestBase[CONTEXT <: RuntimeContext](
                                                                    sizeHint: Int
                                                                  ) extends RuntimeTestSuite[CONTEXT](edition, runtime) {
 
-  test("should not explode on cached properties") {
-    // given
-    val nodes = given { nodePropertyGraph(sizeHint, { case i => Map("p" -> i)}) }
+//  test("should not explode on cached properties") {
+//    // given
+//    val nodes = given { nodePropertyGraph(sizeHint, { case i => Map("p" -> i)}) }
+//
+//    // when
+//    val logicalQuery = new LogicalQueryBuilder(this)
+//      .produceResults("n")
+//      .filter("cache[n.p] < 20 == 0")
+//      .cacheProperties("cache[n.p]")
+//      .allNodeScan("n")
+//      .build()
+//
+//    val runtimeResult = execute(logicalQuery, runtime)
+//
+//    // then
+//    val expected = nodes.take(20).map(n => Array(n))
+//    runtimeResult should beColumns("n").withRows(expected)
+//  }
+
+  test("node index exact seek should cache properties") {
+    given {
+      nodeIndex("A", "prop")
+      nodePropertyGraph(sizeHint, { case i => Map("prop" -> i % 3) }, "A")
+    }
 
     // when
     val logicalQuery = new LogicalQueryBuilder(this)
-      .produceResults("n")
-      .filter("cache[n.p] < 20 == 0")
-      .cacheProperties("cache[n.p]")
-      .allNodeScan("n")
+      .produceResults("a")
+      .union()
+      .|.filter("cache[a.prop] IN [1, 2]")
+      .|.nodeIndexOperator("a:A(prop = 2)", getValue = _ => GetValue)
+      .nodeByLabelScan("a", "A")
       .build()
 
-    val runtimeResult = execute(logicalQuery, runtime)
+    val runtimeResult = profile(logicalQuery, runtime)
+    consume(runtimeResult)
 
     // then
-    val expected = nodes.take(20).map(n => Array(n))
-    runtimeResult should beColumns("n").withRows(expected)
+    val queryProfile = runtimeResult.runtimeResult.queryProfile()
+    queryProfile.operatorProfile(2).dbHits() shouldBe 0 // filter
+  }
+
+
+  test("node index range seek should cache properties") {
+    given {
+      nodeIndex("A", "prop")
+      nodePropertyGraph(sizeHint, { case i => Map("prop" -> i % 3) }, "A")
+    }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("a")
+      .union()
+      .|.filter("cache[a.prop] IN [1, 2]")
+      .|.nodeIndexOperator("a:A(prop >= 2)", getValue = _ => GetValue)
+      .nodeByLabelScan("a", "A")
+      .build()
+
+    val runtimeResult = profile(logicalQuery, runtime)
+    consume(runtimeResult)
+
+    // then
+    val queryProfile = runtimeResult.runtimeResult.queryProfile()
+    queryProfile.operatorProfile(2).dbHits() shouldBe 0 // filter
+  }
+
+  test("node asserting multi index exact seek should cache properties") {
+    given {
+      nodeIndex("A", "prop")
+      nodePropertyGraph(sizeHint, { case i => Map("prop" -> i) }, "A")
+    }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("a")
+      .union()
+      .|.filter("cache[a.prop] IN [1, 2]")
+      .|.assertSameNode("a")
+      .|.|.nodeIndexOperator("a:A(prop = 2)", getValue = _ => GetValue)
+      .|.nodeIndexOperator("a:A(prop = 2)", getValue = _ => GetValue)
+      .nodeByLabelScan("a", "A")
+      .build()
+
+    val runtimeResult = profile(logicalQuery, runtime)
+    consume(runtimeResult)
+
+    // then
+    val queryProfile = runtimeResult.runtimeResult.queryProfile()
+    queryProfile.operatorProfile(2).dbHits() shouldBe 0 // filter
+  }
+
+  test("many node index exact seek should cache properties") {
+    given {
+      nodeIndex("A", "prop")
+      nodePropertyGraph(sizeHint, { case i => Map("prop" -> i % 3) }, "A")
+    }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("a")
+      .union()
+      .|.filter("cache[a.prop] IN [1, 2]")
+      .|.nodeIndexOperator("a:A(prop = 2 OR 3)", getValue = _ => GetValue)
+      .nodeByLabelScan("a", "A")
+      .build()
+
+    val runtimeResult = profile(logicalQuery, runtime)
+    consume(runtimeResult)
+
+    // then
+    val queryProfile = runtimeResult.runtimeResult.queryProfile()
+    queryProfile.operatorProfile(2).dbHits() shouldBe 0 // filter
   }
 }
