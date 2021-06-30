f8a9986fbc719ba94010b4900d0e8926890d0e9a
emmaholmbergohlsson <emma.holmberg.ohlsson@neo4j.com>
2021-06-30 14:01:53 +0200

    Introduce RangeLayout
    A new layout using the RangeKey

---
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/index/schema/RangeLayout.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel.impl.index.schema;
+
+import org.neo4j.index.internal.gbptree.Layout;
+import org.neo4j.io.pagecache.PageCursor;
+
+class RangeLayout extends IndexLayout<RangeKey,NativeIndexValue>
+{
+    private final int numberOfSlots;
+
+    RangeLayout( int numberOfSlots )
+    {
+        super( false, Layout.namedIdentifier( "RL", numberOfSlots ), 0, 1 );
+        this.numberOfSlots = numberOfSlots;
+    }
+
+    @Override
+    public RangeKey newKey()
+    {
+        return numberOfSlots == 1
+               // An optimized version which has the GenericKeyState built-in w/o indirection
+               ? new RangeKey()
+               // A version which has an indirection to GenericKeyState[]
+               : new CompositeRangeKey( numberOfSlots );
+    }
+
+    @Override
+    public RangeKey copyKey( RangeKey key, RangeKey into )
+    {
+        into.copyFrom( key );
+        return into;
+    }
+
+    @Override
+    public int keySize( RangeKey key )
+    {
+        return key.size();
+    }
+
+    @Override
+    public void writeKey( PageCursor cursor, RangeKey key )
+    {
+        key.put( cursor );
+    }
+
+    @Override
+    public void readKey( PageCursor cursor, RangeKey into, int keySize )
+    {
+        into.get( cursor, keySize );
+    }
+
+    @Override
+    public void minimalSplitter( RangeKey left, RangeKey right, RangeKey into )
+    {
+        right.minimalSplitter( left, right, into );
+    }
+
+    @Override
+    public void initializeAsLowest( RangeKey key )
+    {
+        key.initValuesAsLowest();
+    }
+
+    @Override
+    public void initializeAsHighest( RangeKey key )
+    {
+        key.initValuesAsHighest();
+    }
+}
---
+++ community/kernel/src/test/java/org/neo4j/kernel/impl/index/schema/RangeLayoutTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel.impl.index.schema;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class RangeLayoutTest
+{
+    @Test
+    void shouldHaveUniqueIdentifierForDifferentNumberOfSlots()
+    {
+        Map<Long,Integer> layouts = new HashMap<>();
+        for ( int i = 0; i < 100; i++ )
+        {
+            final RangeLayout rangeLayout = new RangeLayout( i );
+            final Integer previous = layouts.put( rangeLayout.identifier(), i );
+            assertNull( previous,
+                    String.format(
+                            "Expected identifier to be unique for layout with different number of slots, but two had the same identifier, " +
+                                    "firstSlotCount=%s, secondSlotCount=%s.", previous, i ) );
+        }
+    }
+}
