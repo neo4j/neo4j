f3e1e23396d0bddd0cf4e79174cead0be2b6ebe0
Satia Herfert <satiaherfert@gmx.de>
2021-07-14 15:00:04 +0200

    Move Graph counts to their own module

---
+++ community/cypher/graph-counts/src/main/scala/org/neo4j/cypher/graphCounts/GraphCountsJson.scala
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.graphCounts
+
+import org.json4s.CustomSerializer
+import org.json4s.DefaultFormats
+import org.json4s.FileInput
+import org.json4s.Formats
+import org.json4s.JArray
+import org.json4s.JString
+import org.json4s.StringInput
+import org.json4s.native.JsonMethods
+import org.neo4j.internal.schema.IndexType
+
+import java.io.File
+
+object GraphCountsJson {
+
+  /**
+   * Given a JSON file obtained from the data collector, e.g. via
+   * `curl -H accept:application/json -H content-type:application/json -d '{"statements":[{"statement":"CALL db.stats.retrieve(\"GRAPH COUNTS\")"}]}' http://_________:7474/db/neo4j/tx/commit > graphCounts.json`,
+   * return a 1-1 representation of the
+   * JSON structure in Scala case classes.
+   *
+   * @param file the file with the JSON contents
+   * @return the scala representation
+   */
+  def parseAsGraphCountData(file: File): GraphCountData = {
+    implicit val formats: Formats = DefaultFormats + RowSerializer
+    JsonMethods.parse(FileInput(file)).extract[GraphCountData]
+  }
+
+  def parseAsGraphCountsJson(file: File): DbStatsRetrieveGraphCountsJSON = {
+    implicit val formats: Formats = DefaultFormats + RowSerializer
+    JsonMethods.parse(FileInput(file)).extract[DbStatsRetrieveGraphCountsJSON]
+  }
+
+  def parse(str: String): DbStatsRetrieveGraphCountsJSON = {
+    implicit val formats: Formats = DefaultFormats + RowSerializer
+    JsonMethods.parse(StringInput(str)).extract[DbStatsRetrieveGraphCountsJSON]
+  }
+}
+
+/*
+ * The below classes represent the JSON structure from data collector output.
+ * If you need to inspect other parts of the JSON that are not represented yet, you have to edit
+ * these case classes to support deserialization of these parts.
+ */
+
+// These classes represent the general result format from calling a REST endpoint
+case class DbStatsRetrieveGraphCountsJSON(errors: Seq[String], results: Seq[Result])
+case class Result(columns: Seq[String], data: Seq[Data])
+case class Data(row: Row)
+
+// These classes represent the particular format for `db.stats.retrieve("GRAPH COUNTS")`
+case class Row(section: String, data: GraphCountData)
+case class GraphCountData(constraints: Seq[Constraint],
+                          indexes: Seq[Index],
+                          nodes: Seq[NodeCount],
+                          relationships: Seq[RelationshipCount])
+
+case class Constraint(label: Option[String],
+                      relationshipType: Option[String],
+                      properties: Seq[String],
+                      `type`: String)
+
+case class Index(labels: Option[Seq[String]],
+                 relationshipTypes: Option[Seq[String]],
+                 indexType: IndexType,
+                 properties: Seq[String],
+                 totalSize: Long,
+                 estimatedUniqueSize: Long,
+                 updatesSinceEstimation: Long)
+
+case class NodeCount(count: Long,
+                     label: Option[String])
+
+case class RelationshipCount(count: Long,
+                             relationshipType: Option[String],
+                             startLabel: Option[String],
+                             endLabel: Option[String])
+
+/**
+ * This custom serializer is needed because the format of Row is such that offsets in an array
+ * map to particular columns.
+ */
+case object RowSerializer extends CustomSerializer[Row](format => (
+  {
+    case JArray(arr) =>
+      implicit val formats: Formats = DefaultFormats + IndexTypeSerializer
+      Row(arr.head.extract[String], arr.last.extract[GraphCountData])
+  },
+  {
+    case _:Row => throw new UnsupportedOperationException("Serialization of GraphCounts is not supported.")
+  }))
+
+case object IndexTypeSerializer extends CustomSerializer[IndexType](format => (
+  {
+    case JString(stringValue) => IndexType.valueOf(stringValue)
+  },
+  {
+    case indexType: IndexType => JString(indexType.name())
+  }
+))
---
+++ community/cypher/graph-counts/src/test/scala/org/neo4j/cypher/graphCounts/GraphCountsJsonTest.scala
@@ -0,0 +1,225 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.graphCounts
+
+import org.json4s.DefaultFormats
+import org.json4s.Formats
+import org.json4s.StringInput
+import org.json4s.native.JsonMethods
+import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
+import org.neo4j.internal.schema.IndexType
+
+class GraphCountsJsonTest extends CypherFunSuite {
+
+  implicit val formats: Formats = DefaultFormats + RowSerializer
+
+  test("Constraint") {
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "label": "DeprecatedRelyingParty",
+        |    "properties": [
+        |      "relyingPartyId"
+        |    ],
+        |    "type": "Uniqueness constraint"
+        |}
+      """.stripMargin)).extract[Constraint] should be(
+      Constraint(
+        Some("DeprecatedRelyingParty"),
+        None,
+        List("relyingPartyId"),
+        "Uniqueness constraint"
+      )
+    )
+  }
+
+  test("Relationship Existence Constraint") {
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "relationshipType": "Foo",
+        |    "properties": [
+        |      "relyingPartyId"
+        |    ],
+        |    "type": "Existence constraint"
+        |}
+      """.stripMargin)).extract[Constraint] should be(
+      Constraint(
+        None,
+        Some("Foo"),
+        List("relyingPartyId"),
+        "Existence constraint"
+      )
+    )
+  }
+
+  test("index for label") {
+    implicit val formats: Formats = DefaultFormats + IndexTypeSerializer
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "estimatedUniqueSize": 2,
+        |    "labels": [
+        |        "Person"
+        |    ],
+        |    "properties": [
+        |        "uuid"
+        |    ],
+        |    "totalSize": 2,
+        |    "updatesSinceEstimation": 0,
+        |    "indexType": "BTREE"
+        |}
+      """.stripMargin)).extract[Index] should be(
+      Index(Some(Seq("Person")), None, IndexType.BTREE, Seq("uuid"), 2, 2, 0)
+    )
+  }
+
+  test("index for relationship type") {
+    implicit val formats: Formats = DefaultFormats + IndexTypeSerializer
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "estimatedUniqueSize": 2,
+        |    "relationshipTypes": [
+        |        "REL"
+        |    ],
+        |    "properties": [
+        |        "prop"
+        |    ],
+        |    "totalSize": 2,
+        |    "updatesSinceEstimation": 0,
+        |    "indexType": "BTREE"
+        |}
+      """.stripMargin)).extract[Index] should be(
+      Index(None, Some(Seq("REL")), IndexType.BTREE, Seq("prop"), 2, 2, 0)
+    )
+  }
+
+  test("NodeLCount with Label") {
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "count": 2,
+        |    "label": "Person"
+        |}
+      """.stripMargin)).extract[NodeCount] should be(
+      NodeCount(2, Some("Person"))
+    )
+  }
+
+  test("NodeCount") {
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "count": 2,
+        |}
+      """.stripMargin)).extract[NodeCount] should be(
+      NodeCount(2, None)
+    )
+  }
+
+  test("RelationshipCount") {
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "count": 5,
+        |}
+      """.stripMargin)).extract[RelationshipCount] should be(
+      RelationshipCount(5, None, None, None)
+    )
+  }
+  test("RelationshipCount with type") {
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "count": 5,
+        |    "relationshipType": "HAS_SSL_CERTIFICATE",
+        |}
+      """.stripMargin)).extract[RelationshipCount] should be(
+      RelationshipCount(5, Some("HAS_SSL_CERTIFICATE"), None, None)
+    )
+  }
+  test("RelationshipCount with type and startLabel") {
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "count": 5,
+        |    "relationshipType": "HAS_SSL_CERTIFICATE",
+        |    "startLabel": "RelyingParty"
+        |}
+      """.stripMargin)).extract[RelationshipCount] should be(
+      RelationshipCount(5, Some("HAS_SSL_CERTIFICATE"), Some("RelyingParty"), None)
+    )
+  }
+  test("RelationshipCount with type and endLabel") {
+    JsonMethods.parse(StringInput(
+      """
+        |{
+        |    "count": 5,
+        |    "relationshipType": "HAS_SSL_CERTIFICATE",
+        |    "endLabel": "RelyingParty"
+        |}
+      """.stripMargin)).extract[RelationshipCount] should be(
+      RelationshipCount(5, Some("HAS_SSL_CERTIFICATE"), None,  Some("RelyingParty"))
+    )
+  }
+
+  test("GraphCountData") {
+    implicit val formats: Formats = DefaultFormats + IndexTypeSerializer
+    JsonMethods.parse(StringInput(
+      """
+        |{"constraints": [{
+        |    "label": "SSLCertificate",
+        |    "properties": [
+        |        "serialNumber"
+        |    ],
+        |    "type": "Uniqueness constraint"
+        |}],
+        |"indexes": [{
+        |    "estimatedUniqueSize": 4,
+        |    "labels": [
+        |        "SSLCertificate"
+        |    ],
+        |    "properties": [
+        |        "serialNumber"
+        |    ],
+        |    "totalSize": 4,
+        |    "updatesSinceEstimation": 0,
+        |    "indexType": "BTREE"
+        |}],
+        |"nodes": [{
+        |    "count": 1,
+        |    "label": "VettingProvider"
+        |}],
+        |"relationships": [{
+        |    "count": 1,
+        |    "relationshipType": "HAS_GEOLOCATION",
+        |    "startLabel": "Address"
+        |}]}
+      """.stripMargin)).extract[GraphCountData] should be(
+      GraphCountData(
+        Seq(Constraint(Some("SSLCertificate"), None, Seq("serialNumber"), "Uniqueness constraint")),
+        Seq(Index(Some(Seq("SSLCertificate")), None, IndexType.BTREE, Seq("serialNumber"), 4, 4, 0)),
+        Seq(NodeCount(1, Some("VettingProvider"))),
+        Seq(RelationshipCount(1, Some("HAS_GEOLOCATION"), Some("Address"), None))
+      )
+    )
+  }
+}
