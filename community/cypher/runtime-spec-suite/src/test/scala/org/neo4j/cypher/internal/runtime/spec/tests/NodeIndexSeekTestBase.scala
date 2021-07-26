b1b1fa6ff73c7ef2ac31a9f5d90b25ecc6746de9
Pontus Melke <pontusmelke@gmail.com>
2021-07-26 08:21:19 +0200

    Fix test failure.
    Move test so that it is not run in parallel

--- community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/NodeIndexSeekTestBase.scala
+++ community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/NodeIndexSeekTestBase.scala
@@ -1448,6 +1448,11 @@ trait EnterpriseNodeIndexSeekTestBase[CONTEXT <: RuntimeContext] {
     } yield honey(10)
     runtimeResult should beColumns("x").withRows(singleColumn(expected))
   }
+}
+
+// Supported by slotted, pipelined, parallel (not compiled though because of composite index)
+trait SerialEnterpriseNodeIndexSeekTestBase[CONTEXT <: RuntimeContext] {
+  self: NodeIndexSeekTestBase[CONTEXT] =>
 
   test("should handle null in exact unique multiple seek") {
     given {
