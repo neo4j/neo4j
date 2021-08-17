5b6136a6408b6be75bae85e70a8ac1b628fffa7d
Pontus Melke <pontusmelke@gmail.com>
2021-08-17 11:06:04 +0200

    Code review: More tests and test fixups

---
+++ community/cypher/front-end/ast/src/test/scala/org/neo4j/cypher/internal/ast/semantics/StartsWithTest.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.cypher.internal.ast.semantics
+
+import org.neo4j.cypher.internal.expressions
+import org.neo4j.cypher.internal.util.DummyPosition
+import org.neo4j.cypher.internal.util.symbols.CTAny
+import org.neo4j.cypher.internal.util.symbols.CTBoolean
+import org.neo4j.cypher.internal.util.symbols.CTString
+
+class StartsWithTest extends InfixExpressionTestBase(expressions.StartsWith(_, _)(DummyPosition(0))) {
+  test("should combine strings and possible strings") {
+    testValidTypes(CTString, CTString)(CTBoolean)
+    testValidTypes(CTAny, CTString)(CTBoolean)
+    testValidTypes(CTString, CTAny)(CTBoolean)
+    testValidTypes(CTAny, CTAny)(CTBoolean)
+  }
+}
