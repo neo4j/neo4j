5652940f40eb3080c01c7a8ee8d8f5b047a3e24d
Pontus Melke <pontusmelke@gmail.com>
2021-08-20 09:14:41 +0200

    Wrong result in pipelined runtime
    Whenever we evaluated an expression containing cached properties in the leaf of
    the RHS of an apply and if that leaf operator didn't advance the cursor we got
    stale cached properties leading to the wrong result. For example
    
    ```
    MATCH (a:A)
    OPTIONAL MATCH (b:B) WHERE a.id = b.id
    WITH a.id AS id, b
    RETURN b
    ```
    
    if there is an index on :B(id).
    
    changelog: Fix wrong result in PIPELINED runtime for some cases of OPTIONAL MATCH

--- community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/CachePropertiesTestBase.scala
+++ community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/CachePropertiesTestBase.scala
@@ -301,4 +301,26 @@ abstract class CachePropertiesTestBase[CONTEXT <: RuntimeContext](
     val queryProfile = runtimeResult.runtimeResult.queryProfile()
     queryProfile.operatorProfile(2).dbHits() shouldBe 0 // filter
   }
+
+  test("handle cached properties in node index seek on the RHS of an apply") {
+    // given
+    val b = given {
+      nodeIndex("B", "id")
+      nodePropertyGraph(
+        sizeHint, { case i => Map("id" -> i)}, "A")
+      nodePropertyGraph(1, {case _ => Map("id" -> 1)}, "B").head
+    }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("b")
+      .apply()
+      .|.nodeIndexOperator("b:B(id = ???)", paramExpr = Some(cachedNodeProp("a", "id")), argumentIds = Set("b"))
+      .nodeByLabelScan("a", "A")
+      .build()
+    val runtimeResult = execute(logicalQuery, runtime)
+
+    // then
+    runtimeResult should beColumns("b").withSingleRow(b)
+  }
 }
