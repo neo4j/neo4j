0e79eff6de05a1e77fed3230297cb0c11fb33252
emmaholmbergohlsson <emma.holmberg.ohlsson@neo4j.com>
2021-08-26 15:35:12 +0200

    Experimental checker consistency check composite fusion indexes correctly
    Fixes a bug where composite fusion indexes (that has values since
    they are only using a btree underneath) would use an index reader
    during consistency check that never returned any values.
    This would make experimental consistency checker report inconsistencies for
    all entries in composite fusion indexes.
    
    Co-authored-by: Petr Janouch <petr.janouch@neotechnology.com>

---
+++ community/community-it/consistency-it/src/test/java/org/neo4j/consistency/checking/full/FullCheckCompositeFusionIndex.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.consistency.checking.full;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
+
+import org.neo4j.configuration.Config;
+import org.neo4j.configuration.GraphDatabaseInternalSettings;
+import org.neo4j.configuration.GraphDatabaseSettings;
+import org.neo4j.consistency.ConsistencyCheckService;
+import org.neo4j.dbms.api.DatabaseManagementService;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.internal.helpers.progress.ProgressMonitorFactory;
+import org.neo4j.io.layout.Neo4jLayout;
+import org.neo4j.logging.NullLogProvider;
+import org.neo4j.test.TestDatabaseManagementServiceBuilder;
+import org.neo4j.test.extension.Inject;
+import org.neo4j.test.extension.testdirectory.TestDirectoryExtension;
+import org.neo4j.test.rule.TestDirectory;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.neo4j.consistency.checking.full.ConsistencyFlags.DEFAULT;
+
+@TestDirectoryExtension
+public class FullCheckCompositeFusionIndex
+{
+    @Inject
+    TestDirectory dir;
+
+    // There was a bug for consistency checking of composite fusion indexes.
+    // For a composite fusion index, it is just a btree which means we can get the values.
+    // But a reader that always returned null as values were used, which meant that all entries in the index were reported as inconsistent.
+    @Test
+    void checkCompositeFusionIndexCorrectly() throws ConsistencyCheckIncompleteException
+    {
+        Label myLabel = Label.label( "myLabel" );
+        DatabaseManagementService managementService = new TestDatabaseManagementServiceBuilder()
+                .setDatabaseRootDirectory( dir.homeDir() )
+                .build();
+        GraphDatabaseService neo4j = managementService.database( "neo4j" );
+
+        try ( Transaction tx = neo4j.beginTx() )
+        {
+            tx.execute( "CALL db.createIndex( 'fusionIndex', ['myLabel'], ['prop1', 'prop2'], 'lucene+native-3.0' )" ).close();
+            tx.commit();
+        }
+        try ( Transaction tx = neo4j.beginTx() )
+        {
+            tx.schema().awaitIndexesOnline( 5, TimeUnit.MINUTES );
+            Node node = tx.createNode( myLabel );
+            node.setProperty( "prop1", "hej" );
+            node.setProperty( "prop2", "hi" );
+            tx.commit();
+        }
+
+        managementService.shutdown();
+
+        ConsistencyCheckService.Result result = runFullConsistencyCheck( dir.homeDir().toPath() );
+        assertTrue( result.isSuccessful() );
+    }
+
+    private ConsistencyCheckService.Result runFullConsistencyCheck( Path path )
+            throws ConsistencyCheckIncompleteException
+    {
+        var config = Config.newBuilder().set( GraphDatabaseSettings.neo4j_home, path )
+                .set( GraphDatabaseInternalSettings.experimental_consistency_checker, true ).build();
+
+        ConsistencyCheckService checkService = new ConsistencyCheckService();
+        return checkService.runFullConsistencyCheck( Neo4jLayout.of( config ).databaseLayout( "neo4j" ),
+                config, ProgressMonitorFactory.NONE, NullLogProvider.getInstance(), false, DEFAULT );
+    }
+}
--- community/kernel/src/main/java/org/neo4j/kernel/impl/index/schema/fusion/FusionIndexAccessor.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/index/schema/fusion/FusionIndexAccessor.java
@@ -36,6 +36,7 @@ import org.neo4j.kernel.api.exceptions.index.IndexEntryConflictException;
 import org.neo4j.kernel.api.index.IndexAccessor;
 import org.neo4j.kernel.api.index.IndexConfigProvider;
 import org.neo4j.kernel.api.index.IndexReader;
+import org.neo4j.kernel.api.index.IndexEntriesReader;
 import org.neo4j.kernel.api.index.IndexUpdater;
 import org.neo4j.kernel.impl.api.index.IndexUpdateMode;
 import org.neo4j.kernel.impl.index.schema.IndexFiles;
@@ -134,6 +135,19 @@ class FusionIndexAccessor extends FusionIndexBase<IndexAccessor> implements Inde
                 return Iterables.concat( entries ).iterator();
             }
         };
+    }
+
+    @Override
+    public IndexEntriesReader[] newAllIndexEntriesReader( int partitions, PageCursorTracer cursorTracer )
+    {
+        if ( descriptor.schema().getPropertyIds().length == 1 )
+        {
+            return IndexAccessor.super.newAllIndexEntriesReader( partitions, cursorTracer );
+        }
+        // For composite fusion index it is just a plain btree index being used.
+        // So we get the accessor for that slot and use its reader.
+        IndexAccessor accessor = instanceSelector.select( IndexSlot.GENERIC );
+        return accessor.newAllIndexEntriesReader( partitions, cursorTracer );
     }
 
     @Override
