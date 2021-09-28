630ac387ba408c77ad9206884b87d9b6d0eb037d
MishaDemianenko <12656787+mishademianenko@users.noreply.github.com>
2021-09-28 09:47:19 +0200

    Review comment: high quality styling, different map

--- community/monitoring/src/main/java/org/neo4j/monitoring/Monitors.java
+++ community/monitoring/src/main/java/org/neo4j/monitoring/Monitors.java
@@ -23,20 +23,16 @@ import org.apache.commons.lang3.ClassUtils;
 import org.eclipse.collections.api.bag.MutableBag;
 import org.eclipse.collections.impl.bag.mutable.MultiReaderHashBag;
 
-import java.lang.invoke.LambdaMetafactory;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 import org.neo4j.annotations.api.IgnoreApiCheck;
@@ -177,7 +173,7 @@ public class Monitors
     {
         private final Object monitorListener;
         private final MethodHandles.Lookup lookup;
-        private final Map<Method,MethodHandle> methods = new IdentityHashMap<>();
+        private final Map<Method,MethodHandle> methods = new ConcurrentHashMap<>();
 
         UntaggedMonitorListenerInvocationHandler( Object monitorListener )
         {
@@ -201,11 +197,11 @@ public class Monitors
         @Override
         public void invoke( Object proxy, Method method, Object[] args, String... tags ) throws Throwable
         {
-            methods.computeIfAbsent( method, method1 ->
+            methods.computeIfAbsent( method, absentMethod ->
             {
                 try
                 {
-                    return lookup.unreflect( method1 ).bindTo( monitorListener );
+                    return lookup.unreflect( absentMethod ).bindTo( monitorListener );
                 }
                 catch ( IllegalAccessException e )
                 {
