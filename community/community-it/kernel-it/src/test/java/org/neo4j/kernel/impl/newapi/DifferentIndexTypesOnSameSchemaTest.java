0f3ba85b7c3ff5bfdccf820ecf00d29753795621
Petr Janouch <petr.janouch@neotechnology.com>
2021-10-01 16:20:04 +0200

    A test for testing a situation when different index types exist on the same schema

---
+++ community/community-it/kernel-it/src/test/java/org/neo4j/kernel/impl/newapi/DifferentIndexTypesOnSameSchemaTest.java
@@ -0,0 +1,327 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel.impl.newapi;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.neo4j.configuration.GraphDatabaseInternalSettings;
+import org.neo4j.exceptions.KernelException;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.schema.IndexType;
+import org.neo4j.internal.kernel.api.IndexReadSession;
+import org.neo4j.internal.kernel.api.NodeValueIndexCursor;
+import org.neo4j.internal.kernel.api.RelationshipValueIndexCursor;
+import org.neo4j.kernel.api.KernelTransaction;
+import org.neo4j.test.TestDatabaseManagementServiceBuilder;
+import org.neo4j.values.storable.CoordinateReferenceSystem;
+import org.neo4j.values.storable.Value;
+import org.neo4j.values.storable.Values;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.neo4j.internal.kernel.api.IndexQueryConstraints.unconstrained;
+import static org.neo4j.internal.kernel.api.PropertyIndexQuery.exact;
+import static org.neo4j.io.pagecache.context.CursorContext.NULL;
+
+class DifferentIndexTypesOnSameSchemaTest extends KernelAPIWriteTestBase<KernelAPIWriteTestSupport>
+{
+
+    private static final String TOKEN = "TestToken";
+    private static final String PROPERTY = "prop";
+
+    @Override
+    public KernelAPIWriteTestSupport newTestSupport()
+    {
+        return new WriteTestSupport()
+        {
+
+            @Override
+            protected TestDatabaseManagementServiceBuilder configure( TestDatabaseManagementServiceBuilder builder )
+            {
+                builder.setConfig( GraphDatabaseInternalSettings.range_indexes_enabled, true );
+                builder.setConfig( GraphDatabaseInternalSettings.text_indexes_enabled, true );
+                builder.setConfig( GraphDatabaseInternalSettings.point_indexes_enabled, true );
+                return builder;
+            }
+        };
+    }
+
+    @EnumSource( EntityType.class )
+    @ParameterizedTest
+    void testNumberValueFromIndex( EntityType entityType ) throws KernelException
+    {
+        entityType.crateIndexes( graphDb );
+
+        Value value = Values.longValue( 1234 );
+        long entityId;
+        try ( KernelTransaction tx = beginTransaction() )
+        {
+            entityId = entityType.createEntity( tx, value );
+            tx.commit();
+        }
+
+        try ( KernelTransaction tx = beginTransaction() )
+        {
+            for ( IndexType indexType : List.of( IndexType.BTREE, IndexType.RANGE ) )
+            {
+                assertThat( entityType.getEntitiesByPropertyValue( tx, indexType, value ) ).containsExactly( entityId );
+            }
+
+            tx.commit();
+        }
+    }
+
+    @EnumSource( EntityType.class )
+    @ParameterizedTest
+    void testNumberValueFromTxState( EntityType entityType ) throws KernelException
+    {
+        entityType.crateIndexes( graphDb );
+
+        Value value = Values.longValue( 1234 );
+        try ( KernelTransaction tx = beginTransaction() )
+        {
+            long entityId = entityType.createEntity( tx, value );
+            for ( IndexType indexType : List.of( IndexType.BTREE, IndexType.RANGE ) )
+            {
+                assertThat( entityType.getEntitiesByPropertyValue( tx, indexType, value ) ).containsExactly( entityId );
+            }
+
+            tx.commit();
+        }
+    }
+
+    @EnumSource( EntityType.class )
+    @ParameterizedTest
+    void testTextValueFromIndex( EntityType entityType ) throws KernelException
+    {
+        entityType.crateIndexes( graphDb );
+
+        Value value = Values.stringValue( "Some string" );
+        long entityId;
+        try ( KernelTransaction tx = beginTransaction() )
+        {
+            entityId = entityType.createEntity( tx, value );
+            tx.commit();
+        }
+
+        try ( KernelTransaction tx = beginTransaction() )
+        {
+            for ( IndexType indexType : List.of( IndexType.BTREE, IndexType.RANGE, IndexType.TEXT ) )
+            {
+                assertThat( entityType.getEntitiesByPropertyValue( tx, indexType, value ) ).containsExactly( entityId );
+            }
+
+            tx.commit();
+        }
+    }
+
+    @EnumSource( EntityType.class )
+    @ParameterizedTest
+    void testTextValueFromTxState( EntityType entityType ) throws KernelException
+    {
+        entityType.crateIndexes( graphDb );
+
+        Value value = Values.stringValue( "Some string" );
+        try ( KernelTransaction tx = beginTransaction() )
+        {
+            long entityId = entityType.createEntity( tx, value );
+            for ( IndexType indexType : List.of( IndexType.BTREE, IndexType.RANGE, IndexType.TEXT ) )
+            {
+                assertThat( entityType.getEntitiesByPropertyValue( tx, indexType, value ) ).containsExactly( entityId );
+            }
+
+            tx.commit();
+        }
+    }
+
+    @EnumSource( EntityType.class )
+    @ParameterizedTest
+    void testPointValueFromIndex( EntityType entityType ) throws KernelException
+    {
+        entityType.crateIndexes( graphDb );
+
+        Value value = Values.pointValue( CoordinateReferenceSystem.Cartesian, 1, 2 );
+        long entityId;
+        try ( KernelTransaction tx = beginTransaction() )
+        {
+            entityId = entityType.createEntity( tx, value );
+            tx.commit();
+        }
+
+        try ( KernelTransaction tx = beginTransaction() )
+        {
+            for ( IndexType indexType : List.of( IndexType.BTREE, IndexType.RANGE, IndexType.POINT ) )
+            {
+                assertThat( entityType.getEntitiesByPropertyValue( tx, indexType, value ) ).containsExactly( entityId );
+            }
+
+            tx.commit();
+        }
+    }
+
+    @EnumSource( EntityType.class )
+    @ParameterizedTest
+    void testPointValueFromTxState( EntityType entityType ) throws KernelException
+    {
+        entityType.crateIndexes( graphDb );
+
+        Value value = Values.pointValue( CoordinateReferenceSystem.Cartesian, 1, 2 );
+        try ( KernelTransaction tx = beginTransaction() )
+        {
+            long entityId = entityType.createEntity( tx, value );
+            for ( IndexType indexType : List.of( IndexType.BTREE, IndexType.RANGE, IndexType.POINT ) )
+            {
+                assertThat( entityType.getEntitiesByPropertyValue( tx, indexType, value ) ).containsExactly( entityId );
+            }
+
+            tx.commit();
+        }
+    }
+
+    private static String nameForType( IndexType indexType )
+    {
+        return indexType + "-IDX";
+    }
+
+    private enum EntityType
+    {
+        NODE
+                {
+                    @Override
+                    void crateIndexes( GraphDatabaseService db )
+                    {
+                        try ( Transaction tx = db.beginTx() )
+                        {
+                            List.of( IndexType.BTREE, IndexType.RANGE, IndexType.TEXT, IndexType.POINT )
+                                .forEach( indexType ->
+                                        tx.schema().indexFor( Label.label( TOKEN ) ).on( PROPERTY )
+                                          .withName( nameForType( indexType ) )
+                                          .withIndexType( indexType ).create() );
+                            tx.commit();
+                        }
+                        try ( Transaction tx = db.beginTx() )
+                        {
+                            tx.schema().awaitIndexesOnline( 2, TimeUnit.MINUTES );
+                            tx.commit();
+                        }
+                    }
+
+                    @Override
+                    List<Long> getEntitiesByPropertyValue( KernelTransaction tx, IndexType indexType, Value value ) throws KernelException
+                    {
+                        IndexReadSession indexSession = tx.dataRead().indexReadSession( tx.schemaRead().indexGetForName( nameForType( indexType ) ) );
+                        // let's check that the obtained session is actually from the intended index
+                        assertEquals( nameForType( indexType ), indexSession.reference().getName() );
+
+                        int property = tx.token().propertyKey( PROPERTY );
+                        try ( NodeValueIndexCursor cursor = tx.cursors().allocateNodeValueIndexCursor( NULL, tx.memoryTracker() ) )
+                        {
+                            tx.dataRead().nodeIndexSeek( tx.queryContext(), indexSession, cursor, unconstrained(), exact( property, value ) );
+
+                            List<Long> hits = new ArrayList<>();
+                            while ( cursor.next() )
+                            {
+                                hits.add( cursor.nodeReference() );
+                            }
+
+                            return hits;
+                        }
+                    }
+
+                    @Override
+                    long createEntity( KernelTransaction tx, Value value ) throws KernelException
+                    {
+                        long nodeId = tx.dataWrite().nodeCreate();
+                        tx.dataWrite().nodeAddLabel( nodeId, tx.token().nodeLabel( TOKEN ) );
+                        tx.dataWrite().nodeSetProperty( nodeId, tx.token().propertyKey( PROPERTY ), value );
+                        return nodeId;
+                    }
+                },
+
+        RELATIONSHIP
+                {
+                    @Override
+                    void crateIndexes( GraphDatabaseService db )
+                    {
+                        try ( Transaction tx = db.beginTx() )
+                        {
+                            List.of( IndexType.BTREE, IndexType.RANGE, IndexType.TEXT, IndexType.POINT )
+                                .forEach( indexType ->
+                                        tx.schema().indexFor( RelationshipType.withName( TOKEN ) ).on( PROPERTY )
+                                          .withName( nameForType( indexType ) )
+                                          .withIndexType( indexType ).create() );
+                            tx.commit();
+                        }
+                        try ( Transaction tx = db.beginTx() )
+                        {
+                            tx.schema().awaitIndexesOnline( 2, TimeUnit.MINUTES );
+                            tx.commit();
+                        }
+                    }
+
+                    @Override
+                    List<Long> getEntitiesByPropertyValue( KernelTransaction tx, IndexType indexType, Value value ) throws KernelException
+                    {
+                        IndexReadSession indexSession = tx.dataRead().indexReadSession( tx.schemaRead().indexGetForName( nameForType( indexType ) ) );
+                        // let's check that the obtained session is actually from the intended index
+                        assertEquals( nameForType( indexType ), indexSession.reference().getName() );
+
+                        int property = tx.token().propertyKey( PROPERTY );
+                        try ( RelationshipValueIndexCursor cursor = tx.cursors().allocateRelationshipValueIndexCursor( NULL, tx.memoryTracker() ) )
+                        {
+                            tx.dataRead().relationshipIndexSeek( tx.queryContext(), indexSession, cursor, unconstrained(), exact( property, value ) );
+
+                            List<Long> hits = new ArrayList<>();
+                            while ( cursor.next() )
+                            {
+                                hits.add( cursor.relationshipReference() );
+                            }
+
+                            return hits;
+                        }
+                    }
+
+                    @Override
+                    long createEntity( KernelTransaction tx, Value value ) throws KernelException
+                    {
+                        long node1 = tx.dataWrite().nodeCreate();
+                        long node2 = tx.dataWrite().nodeCreate();
+                        long relationshipId = tx.dataWrite().relationshipCreate( node1, tx.token().relationshipType( TOKEN ), node2 );
+                        tx.dataWrite().relationshipSetProperty( relationshipId, tx.token().propertyKey( PROPERTY ), value );
+                        return relationshipId;
+                    }
+                };
+
+        abstract void crateIndexes( GraphDatabaseService db );
+
+        abstract List<Long> getEntitiesByPropertyValue( KernelTransaction tx, IndexType indexType, Value value )
+                throws KernelException;
+
+        abstract long createEntity( KernelTransaction tx, Value value ) throws KernelException;
+    }
+}
