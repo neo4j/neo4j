bfaf521f34cbf39150e8b5891f875e3072e01338
Valdemar Roxling <valdemar.roxling@neotechnology.com>
2021-11-09 09:55:30 +0100

    Allow bootloader to pass expand commands

--- community/neo4j/src/test/java/org/neo4j/server/startup/Neo4jCommandTest.java
+++ community/neo4j/src/test/java/org/neo4j/server/startup/Neo4jCommandTest.java
@@ -259,6 +259,7 @@ class Neo4jCommandTest
             }
             assertThat( execute( "start", "--expand-commands" ) ).isEqualTo( EXIT_CODE_OK );
             assertThat( out.toString() ).contains( "--expand-commands" );
+            assertThat( execute( "stop", "--expand-commands" ) ).isEqualTo( EXIT_CODE_OK );
         }
 
         @Test
