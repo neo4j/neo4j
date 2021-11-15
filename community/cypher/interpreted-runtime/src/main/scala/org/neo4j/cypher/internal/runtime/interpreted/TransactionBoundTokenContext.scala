bbb4a769b35de6751f4356943e3d861d6f41bbec
Arne Fischereit <79841228+arnefischereit@users.noreply.github.com>
2021-11-15 18:15:04 +0100

    Improved error message when a property could not be found ()

--- community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/TransactionBoundTokenContext.scala
+++ community/cypher/interpreted-runtime/src/main/scala/org/neo4j/cypher/internal/runtime/interpreted/TransactionBoundTokenContext.scala
@@ -35,8 +35,8 @@ abstract class TransactionBoundTokenContext(transaction: => KernelTransaction) e
 
   def getPropertyKeyId(propertyKeyName: String): Int = {
     val propertyId: Int = transaction.tokenRead().propertyKey(propertyKeyName)
-    if (propertyId ==TokenRead.NO_TOKEN)
-      throw new PropertyKeyNotFoundException("No such property.", null)
+    if (propertyId == TokenRead.NO_TOKEN)
+      throw new PropertyKeyNotFoundException(propertyKeyName, null)
     propertyId
   }
 
