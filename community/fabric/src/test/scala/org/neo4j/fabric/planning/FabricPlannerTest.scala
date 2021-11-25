fa36b087e530ffb7ec5e079c3575508a6360fd0b
Sergey Zaporozhtsev <zimmre@gmail.com>
2021-11-25 11:02:25 +0000

    Make bolt fabric database manager respect dynamically set transaction timeout () ()
    Make BoltFabricDatabaseService respect dynamic setting of dbms.transaction.timeout

--- community/fabric/src/test/scala/org/neo4j/fabric/planning/FabricPlannerTest.scala
+++ community/fabric/src/test/scala/org/neo4j/fabric/planning/FabricPlannerTest.scala
@@ -85,7 +85,7 @@ class FabricPlannerTest
     with FragmentTestUtils
     with AstConstructionTestSupport {
 
-  private def makeConfig(fabricDbName: String) = new FabricConfig(Duration.ZERO, new FabricConfig.DataStream(0, 0, 0, 0), false, true ) {
+  private def makeConfig(fabricDbName: String) = new FabricConfig( ()=>Duration.ZERO, new FabricConfig.DataStream(0, 0, 0, 0), false, true ) {
 
     override def getFabricDatabaseName: Optional[NormalizedDatabaseName] = Optional.of(new NormalizedDatabaseName(fabricDbName))
   }
