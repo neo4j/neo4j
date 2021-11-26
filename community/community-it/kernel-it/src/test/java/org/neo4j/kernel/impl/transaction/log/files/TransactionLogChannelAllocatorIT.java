839ccc98ad7139dcde98b9de2692e15c6da8962e
MishaDemianenko <12656787+mishademianenko@users.noreply.github.com>
2021-11-27 00:20:18 +0100

    Adapt test to the fact that we do not throw exception anymore

--- community/community-it/kernel-it/src/test/java/org/neo4j/kernel/impl/transaction/log/files/TransactionLogChannelAllocatorIT.java
+++ community/community-it/kernel-it/src/test/java/org/neo4j/kernel/impl/transaction/log/files/TransactionLogChannelAllocatorIT.java
@@ -34,6 +34,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.neo4j.configuration.Config;
+import org.neo4j.configuration.GraphDatabaseSettings;
 import org.neo4j.internal.nativeimpl.LinuxNativeAccess;
 import org.neo4j.internal.nativeimpl.NativeAccess;
 import org.neo4j.internal.nativeimpl.NativeAccessProvider;
@@ -62,6 +63,7 @@ import org.neo4j.test.extension.testdirectory.TestDirectoryExtension;
 import org.neo4j.test.utils.TestDirectory;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.neo4j.configuration.GraphDatabaseInternalSettings.dynamic_read_only_failover;
@@ -145,12 +147,11 @@ class TransactionLogChannelAllocatorIT
         var logFileContext = createLogFileContext( getUnavailableBytes(), new PreallocationFailingChannelNativeAccess() );
         var nativeChannelAccessor = new LogFileChannelNativeAccessor( fileSystem, logFileContext );
         var unreasonableAllocator = new TransactionLogChannelAllocator( logFileContext, fileHelper, new LogHeaderCache( 10 ), nativeChannelAccessor );
-        var exception = assertThrows( RuntimeException.class, () -> unreasonableAllocator.createLogChannel( 10, () -> 1L ) );
-        assertThat( exception ).hasRootCauseInstanceOf( ReadOnlyDbException.class )
-                .hasRootCauseMessage( "This Neo4j instance is read only for the database neo4j" );
+        assertDoesNotThrow( () -> unreasonableAllocator.createLogChannel( 10, () -> 1L ) );
 
         assertThat( logProvider.serialize() ).containsSequence( "Warning! System is running out of disk space. " +
                 "Failed to preallocate log file since disk does not have enough space left. Please provision more space to avoid that." );
+        assertThat( config.get( GraphDatabaseSettings.read_only_databases ) ).contains( DEFAULT_DATABASE_NAME );
     }
 
     @Test
