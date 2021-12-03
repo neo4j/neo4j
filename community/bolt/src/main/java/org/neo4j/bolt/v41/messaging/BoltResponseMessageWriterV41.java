f8f8f66157ed505c114263529c77d143c68b33dd
Johannes Donath <297893+dotStart@users.noreply.github.com>
2021-12-03 14:10:34 +0100

    Fixed an issue which occurs when keep-alives are dispatched on closed connections ()
    * Fixed an issue which causes an exception to be thrown when a keep-alive occurs after a connection has already been closed.
    
    * Fixed a formatting issue within the error message which is generated when a NOOP cannot be written.

--- community/bolt/src/main/java/org/neo4j/bolt/v41/messaging/BoltResponseMessageWriterV41.java
+++ community/bolt/src/main/java/org/neo4j/bolt/v41/messaging/BoltResponseMessageWriterV41.java
@@ -43,6 +43,7 @@ public class BoltResponseMessageWriterV41 implements BoltResponseMessageWriter
     private final MessageWriterTimer timer;
     private boolean inRecord;
     private boolean shouldFlushAfterRecord;
+    private boolean closed;
 
     public BoltResponseMessageWriterV41( PackProvider packerProvider, PackOutput output, LogService logService,
             SystemNanoClock clock, Duration keepAliveInterval )
@@ -74,11 +75,11 @@ public class BoltResponseMessageWriterV41 implements BoltResponseMessageWriter
     public void keepAlive() throws IOException
     {
         // Double-check locking. The timeout variable inside timer is volatile.
-        if ( timer.isTimedOut() )
+        if ( !this.closed && timer.isTimedOut() )
         {
             synchronized ( this )
             {
-                if ( timer.isTimedOut() )
+                if ( !this.closed && timer.isTimedOut() )
                 {
                     if ( inRecord )
                     {
@@ -128,6 +129,7 @@ public class BoltResponseMessageWriterV41 implements BoltResponseMessageWriter
     @Override
     public synchronized void close() throws IOException
     {
+        this.closed = true;
         delegator.close();
     }
 
@@ -154,7 +156,7 @@ public class BoltResponseMessageWriterV41 implements BoltResponseMessageWriter
         }
         catch ( Throwable e )
         {
-            delegator.log().error( "Failed to write NOOP because: %s", e );
+            delegator.log().error( "Failed to write NOOP", e );
             throw e;
         }
     }
