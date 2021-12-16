2b35cda2c2af011e6c0bea77f2bd2b05900b7703
MishaDemianenko <12656787+mishademianenko@users.noreply.github.com>
2021-12-16 18:39:18 +0100

    Remove unused UTF8 helper methods

--- community/common/src/main/java/org/neo4j/string/UTF8.java
+++ community/common/src/main/java/org/neo4j/string/UTF8.java
@@ -19,62 +19,26 @@
  */
 package org.neo4j.string;
 
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Utilities for working with UTF8 encoding and decoding.
  */
 public final class UTF8
 {
-    public static final int MINIMUM_SERIALISED_LENGTH_BYTES = Integer.BYTES;
-
     public static byte[] encode( String string )
     {
-        return string.getBytes( StandardCharsets.UTF_8 );
+        return string.getBytes( UTF_8 );
     }
 
     public static String decode( byte[] bytes )
     {
-        return new String( bytes, StandardCharsets.UTF_8 );
+        return new String( bytes, UTF_8 );
     }
 
     public static String decode( byte[] bytes, int offset, int length )
     {
-        return new String( bytes, offset, length, StandardCharsets.UTF_8 );
-    }
-
-    public static String getDecodedStringFrom( ByteBuffer source )
-    {
-        // Currently only one key is supported although the data format supports multiple
-        int count = source.getInt();
-        int remaining = source.remaining();
-        if ( count > remaining )
-        {
-            throw badStringFormatException( count, remaining );
-        }
-        byte[] data = new byte[count];
-        source.get( data );
-        return UTF8.decode( data );
-    }
-
-    private static IllegalArgumentException badStringFormatException( int count, int remaining )
-    {
-        return new IllegalArgumentException(
-                "Bad string format; claims string is " + count + " bytes long, " +
-                "but only " + remaining + " bytes remain in buffer" );
-    }
-
-    public static void putEncodedStringInto( String text, ByteBuffer target )
-    {
-        byte[] data = encode( text );
-        target.putInt( data.length );
-        target.put( data );
-    }
-
-    public static int computeRequiredByteBufferSize( String text )
-    {
-        return encode( text ).length + 4;
+        return new String( bytes, offset, length, UTF_8 );
     }
 
     private UTF8()
