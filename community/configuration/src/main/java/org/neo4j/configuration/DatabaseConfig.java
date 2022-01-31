d4373ab3ab9ac690fdb62a610ecfa12c5adaccae
Mattias Finné <mattias@neotechnology.com>
2022-01-31 12:25:55 +0100

    Removes DatabaseConfig listeners on shutdown
    Instead of in stop. This fixes a problem where listeners
    added during init stage would not be removed if database start
    would fail, causing listeners to remain in the global config.

--- community/configuration/src/main/java/org/neo4j/configuration/DatabaseConfig.java
+++ community/configuration/src/main/java/org/neo4j/configuration/DatabaseConfig.java
@@ -172,17 +172,20 @@ public class DatabaseConfig extends Config implements Lifecycle
     @Override
     public void init()
     {
-
     }
 
     @Override
     public void start()
     {
-
     }
 
     @Override
     public void stop() throws Exception
+    {
+    }
+
+    @Override
+    public void shutdown()
     {
         for ( var settingListeners : registeredListeners.entrySet() )
         {
@@ -194,12 +197,6 @@ public class DatabaseConfig extends Config implements Lifecycle
             }
         }
         registeredListeners = new ConcurrentHashMap<>();
-    }
-
-    @Override
-    public void shutdown()
-    {
-
     }
 
     Config getGlobalConfig()
