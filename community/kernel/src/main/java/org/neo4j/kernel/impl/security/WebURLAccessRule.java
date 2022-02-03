943654ee5f551232293e10bfafad4f6f579f6832
Nacho Cordón <ncordon@users.noreply.github.com>
2022-02-03 09:17:34 +0000

    Follows redirect url to resolve blocked LOAD CSV urls ()

--- community/kernel/src/main/java/org/neo4j/kernel/impl/security/WebURLAccessRule.java
+++ community/kernel/src/main/java/org/neo4j/kernel/impl/security/WebURLAccessRule.java
@@ -21,9 +21,10 @@ package org.neo4j.kernel.impl.security;
 
 import inet.ipaddr.IPAddressString;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
-import java.net.UnknownHostException;
 import java.util.List;
 
 import org.neo4j.configuration.GraphDatabaseInternalSettings;
@@ -31,8 +32,64 @@ import org.neo4j.graphdb.config.Configuration;
 import org.neo4j.graphdb.security.URLAccessRule;
 import org.neo4j.graphdb.security.URLAccessValidationError;
 
-class WebURLAccessRule implements URLAccessRule
+public class WebURLAccessRule implements URLAccessRule
 {
+    public static void checkNotBlocked( URL url, List<IPAddressString> blockedIpRanges ) throws Exception
+    {
+        InetAddress inetAddress = InetAddress.getByName( url.getHost() );
+
+        for ( var blockedIpRange : blockedIpRanges )
+        {
+            if ( blockedIpRange.contains( new IPAddressString( inetAddress.getHostAddress() ) ) )
+            {
+                throw new URLAccessValidationError( "access to " + inetAddress + " is blocked via the configuration property "
+                                                    + GraphDatabaseInternalSettings.cypher_ip_blocklist.name() );
+            }
+        }
+    }
+
+    private static URL checkUrlIncludingHoops( URL url, List<IPAddressString> blockedIpRanges ) throws Exception
+    {
+        URL result = url;
+        boolean isRedirect;
+
+        do
+        {
+            // We need to validate each intermediate url if there are redirects.
+            // Otherwise, we could have situations like an internal ip, e.g. 10.0.0.1
+            // is banned in the config, but it redirects to another different internal ip
+            // and we would still have a security hole
+            checkNotBlocked( result, blockedIpRanges );
+            HttpURLConnection con = (HttpURLConnection) result.openConnection();
+            con.setInstanceFollowRedirects( false );
+            con.connect();
+            con.getInputStream();
+            isRedirect = con.getResponseCode() >= 300 && con.getResponseCode() < 400;
+
+            if ( isRedirect )
+            {
+                String location = con.getHeaderField( "Location" );
+
+                if ( location == null )
+                {
+                    throw new IOException( "URL responded with a redirect but the location header was null" );
+                }
+
+                // If the path is relative, we need to adjust it with respect to the original url
+                if ( location.startsWith( "/" ) )
+                {
+                    location = result.getProtocol() + "://" + result.getAuthority() + location;
+                }
+                result = new URL( location );
+            }
+
+            con.disconnect();
+        }
+        while ( isRedirect );
+
+        return result;
+    }
+
     @Override
     public URL validate( Configuration config, URL url ) throws URLAccessValidationError
     {
@@ -42,18 +99,9 @@ class WebURLAccessRule implements URLAccessRule
         {
             try
             {
-                InetAddress inetAddress = InetAddress.getByName( url.getHost() );
-
-                for ( var blockedIpRange : blockedIpRanges )
-                {
-                    if ( blockedIpRange.contains( new IPAddressString( inetAddress.getHostAddress() ) ) )
-                    {
-                        throw new URLAccessValidationError( "access to " + inetAddress + " is blocked via the configuration property "
-                                                            + GraphDatabaseInternalSettings.cypher_ip_blocklist.name() );
-                    }
-                }
+                checkUrlIncludingHoops( url, blockedIpRanges );
             }
-            catch ( UnknownHostException e )
+            catch ( Exception e )
             {
                 throw new URLAccessValidationError( "Unable to verify access to " + host + ". Cause: " + e.getMessage() );
             }
