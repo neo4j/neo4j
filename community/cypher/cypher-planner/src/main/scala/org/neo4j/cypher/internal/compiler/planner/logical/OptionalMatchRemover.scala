b7acbca45514e0850e3f6777e38a3d3ba5e19ba9
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2022-02-22 10:53:48 +0100

    Add cancellation to rewriters in easy cases
    - Where we have a context already
    
    easy bottomUp
    
    more easy

--- community/cypher/cypher-planner/src/main/scala/org/neo4j/cypher/internal/compiler/planner/logical/OptionalMatchRemover.scala
+++ community/cypher/cypher-planner/src/main/scala/org/neo4j/cypher/internal/compiler/planner/logical/OptionalMatchRemover.scala
@@ -69,24 +69,26 @@ case object UnnecessaryOptionalMatchesRemoved extends StepSequencer.Condition
  */
 case object OptionalMatchRemover extends PlannerQueryRewriter with StepSequencer.Step with PlanPipelineTransformerFactory {
 
-  override def instance(from: LogicalPlanState, context: PlannerContext): Rewriter = topDown(Rewriter.lift {
-    case RegularSinglePlannerQuery(graph, interestingOrder, proj@AggregatingQueryProjection(distinctExpressions, aggregations, _, _), tail, queryInput)
-      if validAggregations(aggregations) =>
-      val projectionDeps: Iterable[LogicalVariable] = (distinctExpressions.values ++ aggregations.values).flatMap(_.dependencies)
-      rewrite(projectionDeps, graph, interestingOrder, proj, tail, queryInput, from.anonymousVariableNameGenerator)
+  override def instance(from: LogicalPlanState, context: PlannerContext): Rewriter = topDown(
+    rewriter = Rewriter.lift {
+      case RegularSinglePlannerQuery(graph, interestingOrder, proj@AggregatingQueryProjection(distinctExpressions, aggregations, _, _), tail, queryInput)
+        if validAggregations(aggregations) =>
+        val projectionDeps: Iterable[LogicalVariable] = (distinctExpressions.values ++ aggregations.values).flatMap(_.dependencies)
+        rewrite(projectionDeps, graph, interestingOrder, proj, tail, queryInput, from.anonymousVariableNameGenerator)
 
-    case RegularSinglePlannerQuery(graph, interestingOrder, proj@DistinctQueryProjection(distinctExpressions, _, _), tail, queryInput) =>
-      val projectionDeps: Iterable[LogicalVariable] = distinctExpressions.values.flatMap(_.dependencies)
-      rewrite(projectionDeps, graph, interestingOrder, proj, tail, queryInput, from.anonymousVariableNameGenerator)
+      case RegularSinglePlannerQuery(graph, interestingOrder, proj@DistinctQueryProjection(distinctExpressions, _, _), tail, queryInput) =>
+        val projectionDeps: Iterable[LogicalVariable] = distinctExpressions.values.flatMap(_.dependencies)
+        rewrite(projectionDeps, graph, interestingOrder, proj, tail, queryInput, from.anonymousVariableNameGenerator)
 
-    // Remove OPTIONAL MATCH if preceding MATCH solves the exact same query graph e.g.:
-    // OPTIONAL MATCH (n)
-    // MATCH (n)           -> QueryGraph {Nodes: ['n'], Arguments: ['n']}
-    // OPTIONAL MATCH (n)  -> QueryGraph {Nodes: ['n'], Arguments: ['n']}
-    case RegularSinglePlannerQuery(qg: QueryGraph, io, h, t, qi) if qg.optionalMatches.exists(om => qg.connectedComponents.contains(om)) =>
-      val newQg = qg.copy(optionalMatches = qg.optionalMatches.filterNot(om => qg.connectedComponents.contains(om)))
-      RegularSinglePlannerQuery(queryGraph = newQg, io, h, t, qi)
-  })
+      // Remove OPTIONAL MATCH if preceding MATCH solves the exact same query graph e.g.:
+      // OPTIONAL MATCH (n)
+      // MATCH (n)           -> QueryGraph {Nodes: ['n'], Arguments: ['n']}
+      // OPTIONAL MATCH (n)  -> QueryGraph {Nodes: ['n'], Arguments: ['n']}
+      case RegularSinglePlannerQuery(qg: QueryGraph, io, h, t, qi) if qg.optionalMatches.exists(om => qg.connectedComponents.contains(om)) =>
+        val newQg = qg.copy(optionalMatches = qg.optionalMatches.filterNot(om => qg.connectedComponents.contains(om)))
+        RegularSinglePlannerQuery(queryGraph = newQg, io, h, t, qi)
+    },
+    cancellation = context.cancellationChecker)
 
   private def rewrite(projectionDeps: Iterable[LogicalVariable],
                       graph: QueryGraph, interestingOrder: InterestingOrder,
--- community/cypher/front-end/frontend/src/main/scala/org/neo4j/cypher/internal/frontend/phases/isolateAggregation.scala
+++ community/cypher/front-end/frontend/src/main/scala/org/neo4j/cypher/internal/frontend/phases/isolateAggregation.scala
@@ -60,7 +60,7 @@ import org.neo4j.cypher.internal.util.topDown
  */
 case object isolateAggregation extends StatementRewriter with StepSequencer.Step with PlanPipelineTransformerFactory {
 
-  override def instance(from: BaseState, context: BaseContext): Rewriter = bottomUp(rewriter(from), _.isInstanceOf[Expression])
+  override def instance(from: BaseState, context: BaseContext): Rewriter = bottomUp(rewriter(from), _.isInstanceOf[Expression], context.cancellationChecker)
 
   private def rewriter(from: BaseState) = Rewriter.lift {
     case q@SingleQuery(clauses) =>
