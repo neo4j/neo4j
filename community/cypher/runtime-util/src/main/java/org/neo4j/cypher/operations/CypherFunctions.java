7e778685571afa00ad50bf447da350923317f9a5
Pontus Melke <pontusmelke@gmail.com>
2022-03-14 15:24:58 +0100

    toIntegerList should support property lists
    changelog: Use lists that are stored in properties as input to toIntegerList

--- community/cypher/runtime-util/src/main/java/org/neo4j/cypher/operations/CypherFunctions.java
+++ community/cypher/runtime-util/src/main/java/org/neo4j/cypher/operations/CypherFunctions.java
@@ -44,6 +44,8 @@ import org.neo4j.values.storable.BooleanValue;
 import org.neo4j.values.storable.CoordinateReferenceSystem;
 import org.neo4j.values.storable.DoubleValue;
 import org.neo4j.values.storable.DurationValue;
+import org.neo4j.values.storable.FloatingPointArray;
+import org.neo4j.values.storable.IntegralArray;
 import org.neo4j.values.storable.IntegralValue;
 import org.neo4j.values.storable.LongValue;
 import org.neo4j.values.storable.NumberValue;
@@ -1441,17 +1443,25 @@ public final class CypherFunctions
         }
     }
 
-    public static AnyValue toIntegerList( AnyValue in )
+    public static ListValue toIntegerList( AnyValue in )
     {
         assert in != NO_VALUE : "NO_VALUE checks need to happen outside this call";
-        if ( !(in instanceof ListValue lv) )
+        if ( in instanceof IntegralArray array )
         {
-            throw new CypherTypeException( String.format("Invalid input for function 'toIntegerList()': Expected a List, got: %s",in) );
+            return VirtualValues.fromArray( array );
         }
-
-        return Arrays.stream( lv.asArray() )
-                     .map( entry -> entry == NO_VALUE ? NO_VALUE : toIntegerOrNull( entry ) )
-                     .collect( ListValueBuilder.collector() );
+        else if ( in instanceof FloatingPointArray array )
+        {
+            return toIntegerList( array );
+        }
+        else if ( in instanceof SequenceValue sequenc )
+        {
+            return toIntegerList( sequenc );
+        }
+        else
+        {
+            throw new CypherTypeException( String.format( "Invalid input for function 'toIntegerList()': Expected a List, got: %s", in ) );
+        }
     }
 
     public static TextValue toString( AnyValue in )
@@ -1896,5 +1906,25 @@ public final class CypherFunctions
         return new CypherTypeException(
                 format( "Expected a string value for `%s`, but got: %s.",
                         method, mode ) );
+    }
+
+    private static ListValue toIntegerList( FloatingPointArray array )
+    {
+        var converted = ListValueBuilder.newListBuilder( array.length() );
+        for ( int i = 0; i < array.length(); i++ )
+        {
+            converted.add( longValue( (long) array.doubleValue( i ) ) );
+        }
+        return converted.build();
+    }
+
+    private static ListValue toIntegerList( SequenceValue sequenceValue )
+    {
+        var converted = ListValueBuilder.newListBuilder();
+        for ( AnyValue value : sequenceValue )
+        {
+            converted.add( value != NO_VALUE ? toIntegerOrNull( value ) : NO_VALUE );
+        }
+        return converted.build();
     }
 }
