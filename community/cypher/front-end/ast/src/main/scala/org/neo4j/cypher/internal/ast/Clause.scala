opencypher/front-end

Add a cancellation argument to tree folds

ast/src/main/scala/org/opencypher/v9_0/ast/Clause.scala
@@ -416,7 +416,7 @@ case class Match(
 
   private def getPropertyPredicates(variable: String): Seq[String] = {
     where.map(w => collectPropertiesInPredicates(variable, w.expression)).getOrElse(Seq.empty[String]) ++
-      pattern.treeFold(Seq.empty[String]) {
+      pattern.folder.treeFold(Seq.empty[String]) {
         case NodePattern(Some(Variable(id)), _, _, properties, predicate) if variable == id =>
           acc => SkipChildren(acc ++ collectPropertiesInPropertyMap(properties) ++ predicate.map(collectPropertiesInPredicates(variable, _)).getOrElse(Seq.empty[String]))
         case RelationshipPattern(Some(Variable(id)), _, _, properties, predicate, _, _) if variable == id =>
@@ -430,7 +430,7 @@ case class Match(
       case _ => Seq.empty[String]
     }
 
-  private def collectPropertiesInPredicates(variable: String, whereExpression: Expression): Seq[String] = whereExpression.treeFold(Seq.empty[String]) {
+  private def collectPropertiesInPredicates(variable: String, whereExpression: Expression): Seq[String] = whereExpression.folder.treeFold(Seq.empty[String]) {
       case Equals(Property(Variable(id), PropertyKeyName(name)), other) if id == variable && applicable(other) =>
         acc => SkipChildren(acc :+ name)
       case Equals(other, Property(Variable(id), PropertyKeyName(name))) if id == variable && applicable(other) =>
@@ -485,19 +485,19 @@ case class Match(
     getLabelAndRelTypePredicates(variable).contains(labelOrRelType)
 
   private def getLabelAndRelTypePredicates(variable: String): Seq[String] = {
-    val inlinedLabels = pattern.fold(Seq.empty[String]) {
+    val inlinedLabels = pattern.folder.fold(Seq.empty[String]) {
       case NodePattern(Some(Variable(id)), nodeLabels, _, _, _) if variable == id =>
         list => list ++ nodeLabels.map(_.name)
     }
-    val inlinedRelTypes = pattern.fold(Seq.empty[String]) {
+    val inlinedRelTypes = pattern.folder.fold(Seq.empty[String]) {
       case RelationshipPattern(Some(Variable(id)), types, _, _, _, _, _) if variable == id =>
         list => list ++ types.map(_.name)
     }
 
-    val labelExpressionLabels: Seq[String] = pattern.fold(Seq.empty[String]) {
+    val labelExpressionLabels: Seq[String] = pattern.folder.fold(Seq.empty[String]) {
       case NodePattern(Some(Variable(id)), _, Some(labelExpression), _, _) if variable == id =>
         list => list ++
-          labelExpression.treeFold(Seq.empty[String]) {
+          labelExpression.folder.treeFold(Seq.empty[String]) {
             case l: LabelExpression.Label =>
               acc => SkipChildren(acc :+ l.label.name)
             case _: LabelExpression.Conjunction | _: LabelExpression.Disjunction =>
@@ -508,7 +508,7 @@ case class Match(
     }
 
     val (predicateLabels, predicateRelTypes) = where match {
-      case Some(innerWhere) => innerWhere.treeFold((Seq.empty[String], Seq.empty[String])) {
+      case Some(innerWhere) => innerWhere.folder.treeFold((Seq.empty[String], Seq.empty[String])) {
         case HasLabels(Variable(id), predicateLabels) if id == variable => {
           case (ls, rs) => SkipChildren((ls ++ predicateLabels.map(_.name), rs))
         }
@@ -530,7 +530,7 @@ case class Match(
     allLabels ++ allRelTypes
   }
 
-  def allExportedVariables: Set[LogicalVariable] = pattern.patternParts.findAllByClass[LogicalVariable].toSet
+  def allExportedVariables: Set[LogicalVariable] = pattern.patternParts.folder.findAllByClass[LogicalVariable].toSet
 }
 
 sealed trait CommandClause extends Clause with SemanticAnalysisTooling {
@@ -1173,7 +1173,7 @@ object SubqueryCall {
 
   def isTransactionalSubquery(clause: SubqueryCall): Boolean = clause.inTransactionsParameters.isDefined
 
-  def findTransactionalSubquery(node: ASTNode) : Option[SubqueryCall] = node.treeFind[SubqueryCall] { case s if isTransactionalSubquery(s) => true }
+  def findTransactionalSubquery(node: ASTNode) : Option[SubqueryCall] = node.folder.treeFind[SubqueryCall] { case s if isTransactionalSubquery(s) => true }
 }
 
 case class SubqueryCall(part: QueryPart, inTransactionsParameters: Option[SubqueryCall.InTransactionsParameters])(val position: InputPosition) extends HorizonClause with SemanticAnalysisTooling {

frontend/src/main/scala/org/opencypher/v9_0/frontend/phases/ObfuscationMetadataCollection.scala
@@ -21,6 +21,7 @@ import org.opencypher.v9_0.expressions.SensitiveAutoParameter
 import org.opencypher.v9_0.expressions.SensitiveLiteral
 import org.opencypher.v9_0.expressions.SensitiveParameter
 import org.opencypher.v9_0.frontend.phases.CompilationPhaseTracer.CompilationPhase.METADATA_COLLECTION
+import org.opencypher.v9_0.util.Foldable.FoldableAny
 import org.opencypher.v9_0.util.Foldable.SkipChildren
 import org.opencypher.v9_0.util.LiteralOffset
 import org.opencypher.v9_0.util.ObfuscationMetadata
@@ -38,7 +39,7 @@ case object ObfuscationMetadataCollection extends Phase[BaseContext, BaseState, 
   override def process(from: BaseState, context: BaseContext): BaseState = {
     val extractedParamNames = from.maybeExtractedParams.map(_.keys.toSet).getOrElse(Set.empty)
     val preParserOffset = from.startPosition.map(_.offset).getOrElse(0)
-    val parameters = from.statement().findAllByClass[Parameter]
+    val parameters = from.statement().folder.findAllByClass[Parameter]
 
     val offsets = collectSensitiveLiteralOffsets(from.statement(), extractedParamNames, preParserOffset)
     val sensitiveParams = collectSensitiveParameterNames(parameters, extractedParamNames)
@@ -47,7 +48,7 @@ case object ObfuscationMetadataCollection extends Phase[BaseContext, BaseState, 
   }
 
   private def collectSensitiveLiteralOffsets(statement: Statement, extractedParamNames: Set[String], preParserOffset: Int): Vector[LiteralOffset] =
-    statement.treeFold(Vector.empty[LiteralOffset]) {
+    statement.folder.treeFold(Vector.empty[LiteralOffset]) {
       case literal: SensitiveLiteral =>
         acc => SkipChildren(acc :+ LiteralOffset(preParserOffset + literal.position.offset, literal.literalLength))
       case p: SensitiveAutoParameter if extractedParamNames.contains(p.name) =>
@@ -56,5 +57,5 @@ case object ObfuscationMetadataCollection extends Phase[BaseContext, BaseState, 
     }.distinct.sortBy(_.start)
 
   private def collectSensitiveParameterNames(queryParams: Seq[Parameter], extractedParamNames: Set[String]): Set[String] =
-    queryParams.findAllByClass[SensitiveParameter].map(_.name).toSet -- extractedParamNames
+    queryParams.folder.findAllByClass[SensitiveParameter].map(_.name).toSet -- extractedParamNames
 }

frontend/src/test/scala/org/opencypher/v9_0/frontend/phases/NamespacerTest.scala
@@ -182,7 +182,7 @@ class NamespacerTest extends CypherFunSuite with AstConstructionTestSupport with
 
     val statement = prepareFrom(query, rewriterPhaseUnderTest).statement()
 
-    val outerScope = statement.treeFold(Set.empty[LogicalVariable]) {
+    val outerScope = statement.folder.treeFold(Set.empty[LogicalVariable]) {
       case expr: ExistsSubClause =>
         acc => TraverseChildren(acc ++ expr.outerScope)
     }
@@ -198,7 +198,7 @@ class NamespacerTest extends CypherFunSuite with AstConstructionTestSupport with
     val query = s"UNWIND [1,2,3] AS ${names(0)} WITH ${names(0)} + 1 AS x MATCH (${names(0)})-[${names(1)}]-(${names(2)}) RETURN 1 AS foo"
     val statement = prepareFrom(query, rewriterPhaseUnderTest).statement()
 
-    statement.findAllByClass[Variable].map(_.name).foreach {
+    statement.folder.findAllByClass[Variable].map(_.name).foreach {
       case "x" | "foo" => // OK
       case v if AnonymousVariableNameGenerator.notNamed(v) => // OK
       case v => fail(s"$v was not an anonymous variable")

rewriting/src/main/scala/org/opencypher/v9_0/rewriting/rewriters/moveWithPastMatch.scala
@@ -78,7 +78,7 @@ case object moveWithPastMatch extends Rewriter with StepSequencer.Step with ASTR
     def usesVariableFromWith(w: With): Boolean = {
       // We can be sure all return items are aliased at this point
       clauses.exists {
-        m => m.findAllByClass[LogicalVariable].exists(w.returnItems.items.flatMap(_.alias).contains)
+        m => m.folder.findAllByClass[LogicalVariable].exists(w.returnItems.items.flatMap(_.alias).contains)
       }
     }
     def allExportedVariablesAsReturnItems: Seq[AliasedReturnItem] = clauses.flatMap(_.allExportedVariables.map(v => AliasedReturnItem(v))).distinct

rewriting/src/test/scala/org/opencypher/v9_0/rewriting/ParameterTypeValueReplacementTest.scala
@@ -59,12 +59,12 @@ class ParameterTypeValueReplacementTest extends CypherFunSuite {
     val nameGenerator = new AnonymousVariableNameGenerator
     val original: Statement = JavaCCParser.parse(originalQuery, exceptionFactory, nameGenerator)
 
-    original.findAllByClass[Parameter].size should equal(parameterTypes.size) // make sure we use all given parameters in the query
+    original.folder.findAllByClass[Parameter].size should equal(parameterTypes.size) // make sure we use all given parameters in the query
 
     val rewriter = parameterValueTypeReplacement(parameterTypes)
-    val result = original.rewrite(rewriter).asInstanceOf[Statement]
+    val result = original.endoRewrite(rewriter)
 
-    val rewrittenParameters: Seq[Parameter] = result.findAllByClass[Parameter]
+    val rewrittenParameters: Seq[Parameter] = result.folder.findAllByClass[Parameter]
     val rewrittenParameterTypes = rewrittenParameters.map(p => p.name -> parameterTypes.getOrElse(p.name, fail("something went wrong"))).toMap
     rewrittenParameterTypes should equal(parameterTypes)
   }

neo4j/neo4j

community/cypher/front-end/ast/src/main/scala/org/neo4j/cypher/internal/ast/Clause.scala
@@ -357,7 +357,7 @@ case class Match(
 
   private[ast] def containsPropertyPredicates(variable: String, propertiesInHint: Seq[PropertyKeyName]): Boolean = {
     val propertiesInPredicates: Seq[String] = (where match {
-      case Some(w) => w.treeFold(Seq.empty[String]) {
+      case Some(w) => w.folder.treeFold(Seq.empty[String]) {
         case Equals(Property(Variable(id), PropertyKeyName(name)), other) if id == variable && applicable(other) =>
           acc => SkipChildren(acc :+ name)
         case Equals(other, Property(Variable(id), PropertyKeyName(name))) if id == variable && applicable(other) =>
@@ -395,7 +395,7 @@ case class Match(
           acc => SkipChildren(acc)
       }
       case None => Seq.empty
-    }) ++ pattern.treeFold(Seq.empty[String]) {
+    }) ++ pattern.folder.treeFold(Seq.empty[String]) {
       case NodePattern(Some(Variable(id)), _, Some(MapExpression(prop))) if variable == id =>
         acc => SkipChildren(acc ++ prop.map(_._1.name))
       case RelationshipPattern(Some(Variable(id)), _, _, Some(MapExpression(prop)), _, _) if variable == id =>
@@ -419,16 +419,16 @@ case class Match(
   }
 
   private[ast] def containsLabelOrRelTypePredicate(variable: String, labelOrRelType: String): Boolean = {
-    val inlinedLabels = pattern.fold(Seq.empty[String]) {
+    val inlinedLabels = pattern.folder.fold(Seq.empty[String]) {
       case NodePattern(Some(Variable(id)), nodeLabels, _) if variable == id =>
         list => list ++ nodeLabels.map(_.name)
     }
-    val inlinedRelTypes = pattern.fold(Seq.empty[String]) {
+    val inlinedRelTypes = pattern.folder.fold(Seq.empty[String]) {
       case RelationshipPattern(Some(Variable(id)), types, _, _, _, _) if variable == id =>
         list => list ++ types.map(_.name)
     }
     val (predicateLabels, predicateRelTypes) = where match {
-      case Some(innerWhere) => innerWhere.treeFold((Seq.empty[String], Seq.empty[String])) {
+      case Some(innerWhere) => innerWhere.folder.treeFold((Seq.empty[String], Seq.empty[String])) {
         case HasLabels(Variable(id), predicateLabels) if id == variable => {
           case (ls, rs) => SkipChildren((ls ++ predicateLabels.map(_.name), rs))
         }
@@ -450,7 +450,7 @@ case class Match(
     allLabels.contains(labelOrRelType) || allRelTypes.contains(labelOrRelType)
   }
 
-  def allExportedVariables: Set[LogicalVariable] = pattern.patternParts.findByAllClass[LogicalVariable].toSet
+  def allExportedVariables: Set[LogicalVariable] = pattern.patternParts.folder.findByAllClass[LogicalVariable].toSet
 }
 
 sealed trait CommandClause extends Clause with SemanticAnalysisTooling {

community/cypher/front-end/frontend/src/main/scala/org/neo4j/cypher/internal/frontend/phases/ObfuscationMetadataCollection.scala
@@ -22,6 +22,7 @@ import org.neo4j.cypher.internal.expressions.SensitiveAutoParameter
 import org.neo4j.cypher.internal.expressions.SensitiveLiteral
 import org.neo4j.cypher.internal.expressions.SensitiveParameter
 import org.neo4j.cypher.internal.frontend.phases.CompilationPhaseTracer.CompilationPhase.METADATA_COLLECTION
+import org.neo4j.cypher.internal.util.Foldable.FoldableAny
 import org.neo4j.cypher.internal.util.Foldable.SkipChildren
 import org.neo4j.cypher.internal.util.LiteralOffset
 import org.neo4j.cypher.internal.util.ObfuscationMetadata
@@ -39,7 +40,7 @@ case object ObfuscationMetadataCollection extends Phase[BaseContext, BaseState, 
   override def process(from: BaseState, context: BaseContext): BaseState = {
     val extractedParamNames = from.maybeExtractedParams.map(_.keys.toSet).getOrElse(Set.empty)
     val preParserOffset = from.startPosition.map(_.offset).getOrElse(0)
-    val parameters = from.statement().findByAllClass[Parameter]
+    val parameters = from.statement().folder.findByAllClass[Parameter]
 
     val offsets = collectSensitiveLiteralOffsets(from.statement(), extractedParamNames, preParserOffset)
     val sensitiveParams = collectSensitiveParameterNames(parameters, extractedParamNames)
@@ -48,7 +49,7 @@ case object ObfuscationMetadataCollection extends Phase[BaseContext, BaseState, 
   }
 
   private def collectSensitiveLiteralOffsets(statement: Statement, extractedParamNames: Set[String], preParserOffset: Int): Vector[LiteralOffset] =
-    statement.treeFold(Vector.empty[LiteralOffset]) {
+    statement.folder.treeFold(Vector.empty[LiteralOffset]) {
       case literal: SensitiveLiteral =>
         acc => SkipChildren(acc :+ LiteralOffset(preParserOffset + literal.position.offset, literal.literalLength))
       case p: SensitiveAutoParameter if extractedParamNames.contains(p.name) =>
@@ -57,5 +58,5 @@ case object ObfuscationMetadataCollection extends Phase[BaseContext, BaseState, 
     }.distinct.sortBy(_.start)
 
   private def collectSensitiveParameterNames(queryParams: Seq[Parameter], extractedParamNames: Set[String]): Set[String] =
-    queryParams.findByAllClass[SensitiveParameter].map(_.name).toSet -- extractedParamNames
+    queryParams.folder.findByAllClass[SensitiveParameter].map(_.name).toSet -- extractedParamNames
 }

community/cypher/front-end/frontend/src/test/scala/org/neo4j/cypher/internal/frontend/phases/NamespacerTest.scala
@@ -183,7 +183,7 @@ class NamespacerTest extends CypherFunSuite with AstConstructionTestSupport with
 
     val statement = prepareFrom(query, rewriterPhaseUnderTest).statement()
 
-    val outerScope = statement.treeFold(Set.empty[Variable]) {
+    val outerScope = statement.folder.treeFold(Set.empty[Variable]) {
       case expr: ExistsSubClause =>
         acc => TraverseChildren(acc ++ expr.outerScope)
     }

community/cypher/front-end/rewriting/src/main/scala/org/neo4j/cypher/internal/rewriting/rewriters/moveWithPastMatch.scala
@@ -79,7 +79,7 @@ case object moveWithPastMatch extends Rewriter with StepSequencer.Step with ASTR
     def usesVariableFromWith(w: With): Boolean = {
       // We can be sure all return items are aliased at this point
       clauses.exists {
-        m => m.findByAllClass[LogicalVariable].exists(w.returnItems.items.flatMap(_.alias).contains)
+        m => m.folder.findByAllClass[LogicalVariable].exists(w.returnItems.items.flatMap(_.alias).contains)
       }
     }
     def allExportedVariablesAsReturnItems: Seq[AliasedReturnItem] = clauses.flatMap(_.allExportedVariables.map(v => AliasedReturnItem(v))).distinct

community/cypher/front-end/rewriting/src/test/scala/org/neo4j/cypher/internal/rewriting/ParameterTypeValueReplacementTest.scala
@@ -59,12 +59,12 @@ class ParameterTypeValueReplacementTest extends CypherFunSuite {
     val exceptionFactory = OpenCypherExceptionFactory(None)
     val original: Statement = parser.parse(originalQuery, exceptionFactory) // Do not use the old parameter syntax here
 
-    original.findByAllClass[Parameter].size should equal(parameterTypes.size) // make sure we use all given parameters in the query
+    original.folder.findByAllClass[Parameter].size should equal(parameterTypes.size) // make sure we use all given parameters in the query
 
     val rewriter = parameterValueTypeReplacement(parameterTypes)
-    val result = original.rewrite(rewriter).asInstanceOf[Statement]
+    val result = original.endoRewrite(rewriter)
 
-    val rewrittenParameters: Seq[Parameter] = result.findByAllClass[Parameter]
+    val rewrittenParameters: Seq[Parameter] = result.folder.findByAllClass[Parameter]
     val rewrittenParameterTypes = rewrittenParameters.map(p => p.name -> parameterTypes.getOrElse(p.name, fail("something went wrong"))).toMap
     rewrittenParameterTypes should equal(parameterTypes)
   }