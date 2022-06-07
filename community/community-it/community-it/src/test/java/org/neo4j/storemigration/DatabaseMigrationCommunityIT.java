e0ae3ea31dc9c44c8a03660dcad08e907e4ab066
Anton Persson <anton.persson@neotechnology.com>
2022-06-07 16:57:37 +0200

    Test migration in community
    Introduce DatabaseMigrationCommunityIT and add 4.4 zipped stores for community
    Add empty community 44 zipped stores
    Added 4.3 and 4.2 stores for migration testing
    Test system db migration in community

---
+++ community/community-it/community-it/src/test/java/org/neo4j/storemigration/DatabaseMigrationCommunityIT.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.storemigration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.neo4j.dbms.database.TopologyGraphDbmsModel.DATABASE_LABEL;
+import static org.neo4j.dbms.database.TopologyGraphDbmsModel.DATABASE_NAME_LABEL;
+import static org.neo4j.dbms.database.TopologyGraphDbmsModel.NAME_PROPERTY;
+import static org.neo4j.graphdb.schema.IndexType.LOOKUP;
+import static org.neo4j.server.security.systemgraph.versions.KnownCommunitySecurityComponentVersion.USER_LABEL;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.neo4j.consistency.checking.full.ConsistencyCheckIncompleteException;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.schema.ConstraintDefinition;
+import org.neo4j.graphdb.schema.IndexDefinition;
+import org.neo4j.internal.helpers.collection.Iterables;
+import org.neo4j.kernel.ZippedStore;
+import org.neo4j.kernel.ZippedStoreCommunity;
+import org.neo4j.kernel.impl.store.format.RecordFormats;
+import org.neo4j.kernel.impl.store.format.aligned.PageAligned;
+import org.neo4j.kernel.impl.store.format.standard.Standard;
+import org.neo4j.test.TestDatabaseManagementServiceBuilder;
+
+public class DatabaseMigrationCommunityIT extends DatabaseMigrationITBase {
+    private static final Map<String, RecordFormats> expectedFormat = Map.of(
+            Standard.LATEST_NAME, Standard.LATEST_RECORD_FORMATS,
+            PageAligned.LATEST_NAME, PageAligned.LATEST_RECORD_FORMATS);
+
+    public static Stream<Arguments> migrations() {
+        List<Arguments> permutations = new ArrayList<>();
+
+        // Stores with special node label index
+        permutations.add(Arguments.arguments(ZippedStoreCommunity.AF430_V42_INJECTED_NLI, "aligned"));
+        permutations.add(Arguments.arguments(ZippedStoreCommunity.AF430_V43D4_PERSISTED_NLI, "aligned"));
+        // 4.3 stores
+        permutations.add(Arguments.arguments(ZippedStoreCommunity.SF430_V43D4_ALL_NO_BTREE, "standard"));
+        permutations.add(Arguments.arguments(ZippedStoreCommunity.SF430_V43D4_ALL_NO_BTREE, "aligned"));
+        permutations.add(Arguments.arguments(ZippedStoreCommunity.AF430_V43D4_ALL_NO_BTREE, "aligned"));
+        // 4.4 stores
+        permutations.add(Arguments.arguments(ZippedStoreCommunity.SF430_V44_ALL, "standard"));
+        permutations.add(Arguments.arguments(ZippedStoreCommunity.SF430_V44_ALL, "aligned"));
+        permutations.add(Arguments.arguments(ZippedStoreCommunity.AF430_V44_ALL, "aligned"));
+
+        return permutations.stream();
+    }
+
+    public static Stream<SystemDbMigration> systemDbMigrations() {
+        List<SystemDbMigration> permutations = new ArrayList<>();
+
+        // Stores with different system dbs
+        permutations.add(new SystemDbMigration(ZippedStoreCommunity.AF430_V42_INJECTED_NLI, false));
+        permutations.add(new SystemDbMigration(ZippedStoreCommunity.AF430_V43D4_PERSISTED_NLI, false));
+        permutations.add(new SystemDbMigration(ZippedStoreCommunity.AF430_V43D4_ALL_NO_BTREE, true));
+        permutations.add(new SystemDbMigration(ZippedStoreCommunity.AF430_V44_ALL, true));
+
+        return permutations.stream();
+    }
+
+    @ParameterizedTest
+    @MethodSource("migrations")
+    void shouldMigrateDatabase(ZippedStore zippedStore, String toRecordFormat)
+            throws IOException, ConsistencyCheckIncompleteException {
+        doShouldMigrateDatabase(zippedStore, toRecordFormat);
+    }
+
+    @ParameterizedTest
+    @MethodSource("systemDbMigrations")
+    void shouldMigrateSystemDatabase(SystemDbMigration systemDbMigration)
+            throws IOException, ConsistencyCheckIncompleteException {
+        doShouldMigrateSystemDatabase(systemDbMigration);
+    }
+
+    @Override
+    protected TestDatabaseManagementServiceBuilder newDbmsBuilder(Path homeDir) {
+        return new TestDatabaseManagementServiceBuilder(homeDir);
+    }
+
+    @Override
+    protected RecordFormats expectedFormat(String toRecordFormat) {
+        return expectedFormat.get(toRecordFormat);
+    }
+
+    @Override
+    protected void verifySystemDbSchema(GraphDatabaseService system, SystemDbMigration systemDbMigration) {
+        try (Transaction tx = system.beginTx()) {
+            List<ConstraintDefinition> constraints =
+                    Iterables.asList(tx.schema().getConstraints());
+            verifyHasUniqueConstraint(constraints, DATABASE_NAME_LABEL, NAME_PROPERTY);
+            verifyHasUniqueConstraint(constraints, DATABASE_LABEL, NAME_PROPERTY);
+            verifyHasUniqueConstraint(constraints, USER_LABEL, "id");
+            verifyHasUniqueConstraint(constraints, USER_LABEL, "name");
+            assertThat(constraints).hasSize(4);
+
+            List<IndexDefinition> indexes = Iterables.asList(tx.schema().getIndexes());
+            verifyHasIndex(indexes, DATABASE_NAME_LABEL, NAME_PROPERTY);
+            verifyHasIndex(indexes, DATABASE_LABEL, NAME_PROPERTY);
+            verifyHasIndex(indexes, USER_LABEL, "id");
+            verifyHasIndex(indexes, USER_LABEL, "name");
+
+            assertThat(indexes).anySatisfy(indexDefinition -> {
+                assertThat(indexDefinition.getIndexType()).isEqualTo(LOOKUP);
+                assertThat(indexDefinition.isNodeIndex()).isEqualTo(true);
+            });
+
+            if (systemDbMigration.hasRelationshipTypeIndex()) {
+                assertThat(indexes).anySatisfy(indexDefinition -> {
+                    assertThat(indexDefinition.getIndexType()).isEqualTo(LOOKUP);
+                    assertThat(indexDefinition.isRelationshipIndex()).isEqualTo(true);
+                });
+                assertThat(indexes).hasSize(6);
+            } else {
+                assertThat(indexes).hasSize(5);
+            }
+            tx.commit();
+        }
+    }
+}
---
+++ community/community-it/it-test-support/src/main/java/org/neo4j/kernel/ZippedStoreCommunity.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.kernel;
+
+import static java.util.Objects.requireNonNull;
+import static org.neo4j.common.Edition.COMMUNITY;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import org.neo4j.test.Unzip;
+
+public enum ZippedStoreCommunity implements ZippedStore {
+    // Stores with special node label index
+    AF430_V42_INJECTED_NLI(
+            "AF4.3.0_V4.2_empty_community_injected_nli.zip",
+            new DbStatistics("AF4.3.0", KernelVersion.V4_2, 1, COMMUNITY, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)),
+    AF430_V43D4_PERSISTED_NLI(
+            "AF4.3.0_V4.3.D4_empty_community_persisted_nli.zip",
+            new DbStatistics("AF4.3.0", KernelVersion.V4_3_D4, 2, COMMUNITY, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1)),
+    // 4.3 stores
+    SF430_V43D4_ALL_NO_BTREE(
+            "SF4.3.0_V4.3.D4_all-no-btree_community.zip",
+            new DbStatistics(
+                    "SF4.3.0",
+                    KernelVersion.V4_3_D4,
+                    11,
+                    COMMUNITY,
+                    2587,
+                    2576,
+                    11,
+                    2586,
+                    351,
+                    3726,
+                    3725,
+                    11946,
+                    10,
+                    2,
+                    0,
+                    0,
+                    0,
+                    10)),
+    AF430_V43D4_ALL_NO_BTREE(
+            "AF4.3.0_V4.3.D4_all-no-btree_community.zip",
+            new DbStatistics(
+                    "AF4.3.0",
+                    KernelVersion.V4_3_D4,
+                    11,
+                    COMMUNITY,
+                    2586,
+                    2574,
+                    12,
+                    2585,
+                    349,
+                    3740,
+                    3739,
+                    11941,
+                    10,
+                    2,
+                    0,
+                    0,
+                    0,
+                    10)),
+    // 4.4 stores
+    SF430_V44_EMPTY(
+            "SF4.3.0_V4.4_empty_community.zip",
+            new DbStatistics("SF4.3.0", KernelVersion.V4_4, 4, COMMUNITY, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)),
+    AF430_V44_EMPTY(
+            "AF4.3.0_V4.4_empty_community.zip",
+            new DbStatistics("AF4.3.0", KernelVersion.V4_4, 4, COMMUNITY, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)),
+    SF430_V44_ALL(
+            "SF4.3.0_V4.4_all_community.zip",
+            new DbStatistics(
+                    "SF4.3.0",
+                    KernelVersion.V4_4,
+                    23,
+                    COMMUNITY,
+                    2585,
+                    2573,
+                    12,
+                    2584,
+                    366,
+                    3738,
+                    3737,
+                    11872,
+                    36,
+                    2,
+                    12,
+                    8,
+                    4,
+                    36)),
+    AF430_V44_ALL(
+            "AF4.3.0_V4.4_all_community.zip",
+            new DbStatistics(
+                    "AF4.3.0",
+                    KernelVersion.V4_4,
+                    23,
+                    COMMUNITY,
+                    2549,
+                    2537,
+                    12,
+                    2548,
+                    337,
+                    3636,
+                    3635,
+                    11676,
+                    36,
+                    2,
+                    12,
+                    8,
+                    4,
+                    36));
+
+    private final String resourceName;
+    private final DbStatistics statistics;
+
+    ZippedStoreCommunity(String resourceName, DbStatistics statistics) {
+        this.resourceName = resourceName;
+        this.statistics = statistics;
+    }
+
+    @Override
+    public Path pathToZip() throws URISyntaxException {
+        var sourceUrl = requireNonNull(ZippedStoreCommunity.class.getResource(resourceName));
+        var sourceUri = sourceUrl.toURI();
+        return Path.of(sourceUri);
+    }
+
+    @Override
+    public void unzip(Path targetDirectory) throws IOException {
+        Unzip.unzip(ZippedStoreCommunity.class, resourceName, targetDirectory);
+    }
+
+    @Override
+    public DbStatistics statistics() {
+        return statistics;
+    }
+}
