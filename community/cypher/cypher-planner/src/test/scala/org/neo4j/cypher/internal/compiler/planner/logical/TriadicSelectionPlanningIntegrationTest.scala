e16320b64d1ae2ee1ac8e2cce30129267171037d
Satia Herfert <satiaherfert@gmx.de>
2022-06-14 14:53:00 +0200

    Rewrite TriadicSelectionFinderTest as an Integration test.

---
+++ community/cypher/cypher-planner/src/test/scala/org/neo4j/cypher/internal/compiler/planner/logical/TriadicSelectionPlanningIntegrationTest.scala
@@ -0,0 +1,312 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.compiler.planner.logical
+
+import org.neo4j.cypher.internal.compiler.planner.LogicalPlanningIntegrationTestSupport
+import org.neo4j.cypher.internal.logical.plans.TriadicSelection
+import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
+
+class TriadicSelectionPlanningIntegrationTest extends CypherFunSuite with LogicalPlanningIntegrationTestSupport {
+
+  private val planner = plannerBuilder()
+    .setAllNodesCardinality(1000)
+    .setAllRelationshipsCardinality(100)
+    .setLabelCardinality("X", 10)
+    .setLabelCardinality("Y", 100)
+    .setLabelCardinality("Z", 100)
+    .setRelationshipCardinality("(:X)-[]->()", 10)
+    .setRelationshipCardinality("(:Y)-[]->()", 10)
+    .setRelationshipCardinality("(:Z)-[]->()", 10)
+    .setRelationshipCardinality("()-[:A]->()", 10)
+    .setRelationshipCardinality("(:X)-[:A]->()", 10)
+    .setRelationshipCardinality("()-[:A]->(:X)", 10)
+    .setRelationshipCardinality("()-[:B]->()", 10)
+    .setRelationshipCardinality("(:X)-[]->(:Y)", 10)
+    .setRelationshipCardinality("(:Y)-[]->(:Y)", 10)
+    .setRelationshipCardinality("(:Y)-[]->(:Z)", 10)
+    .setRelationshipCardinality("(:X)-[]->(:Z)", 10)
+    .setRelationshipCardinality("(:Z)-[]->(:Z)", 10)
+    .setRelationshipCardinality("()-[]->(:Y)", 10)
+    .setRelationshipCardinality("()-[]->(:Z)", 10)
+    .build()
+
+  // Negative Predicate Expression
+
+  test("MATCH (a:X)-->(b)-->(c) WHERE NOT (a)-->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b)-[r2]->(c) WHERE NOT (a)-->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .triadicSelection(positivePredicate = false, "a", "b", "c")
+      .|.filter("not r1 = r2")
+      .|.expandAll("(b)-[r2]->(c)")
+      .|.argument("b", "r1")
+      .expandAll("(a)-[r1]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  test("MATCH (a:X)-[:A]->(b)-[:B]->(c) WHERE NOT (a)-->(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[:A]->(b)-[:B]->(c) WHERE NOT (a)-->(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-[:A]->(b)-[:A]->(c) WHERE NOT (a)<-[:A]-(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[:A]->(b)-[:A]->(c) WHERE NOT (a)<-[:A]-(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-[:A]->(b)-[:A]->(c) WHERE NOT (a:X)-[:A]->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1:A]->(b)-[r2:A]->(c) WHERE NOT (a)-[:A]->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .triadicSelection(positivePredicate = false, "a", "b", "c")
+      .|.filter("not r1 = r2")
+      .|.expandAll("(b)-[r2:A]->(c)")
+      .|.argument("b", "r1")
+      .expandAll("(a)-[r1:A]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  test("MATCH (a:X)-[:A]->(b)-[:B]->(c) WHERE NOT (a:X)-[:A]->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1:A]->(b)-[r2:B]->(c) WHERE NOT (a)-[:A]->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .triadicSelection(positivePredicate = false, "a", "b", "c")
+      .|.expandAll("(b)-[r2:B]->(c)")
+      .|.argument("b", "r1")
+      .expandAll("(a)-[r1:A]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  test("MATCH (a:X)-[:A]->(b)<-[:B]-(c) WHERE NOT (a:X)-[:A]->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1:A]->(b)<-[r2:B]-(c) WHERE NOT (a)-[:A]->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .triadicSelection(positivePredicate = false, "a", "b", "c")
+      .|.expandAll("(b)<-[r2:B]-(c)")
+      .|.argument("b", "r1")
+      .expandAll("(a)-[r1:A]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  // Positive Predicate Expression
+
+  test("MATCH (a:X)-->(b)-[:A]->(c) WHERE (a:X)-[:A]->(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b)-[r2:A]->(c) WHERE (a)-[:A]->(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-->(b)-->(c) WHERE (a)-->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b)-[r2]->(c) WHERE (a)-->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .triadicSelection(positivePredicate = true, "a", "b", "c")
+      .|.filter("not r1 = r2")
+      .|.expandAll("(b)-[r2]->(c)")
+      .|.argument("b", "r1")
+      .expandAll("(a)-[r1]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  test("MATCH (a:X)-[:A]->(b)-[:B]->(c) WHERE (a)-->(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[r1:A]->(b)-[r2:B]->(c) WHERE (a)-->(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-[:A]->(b)-[:A]->(c) WHERE (a)<-[:A]-(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[r1:A]->(b)-[r2:A]->(c) WHERE (a)<-[:A]-(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-[:A]->(b)-[:A]->(c) WHERE (a:X)-[:A]->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1:A]->(b)-[r2:A]->(c) WHERE (a)-[:A]->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .triadicSelection(positivePredicate = true, "a", "b", "c")
+      .|.filter("not r1 = r2")
+      .|.expandAll("(b)-[r2:A]->(c)")
+      .|.argument("b", "r1")
+      .expandAll("(a)-[r1:A]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  test("MATCH (a:X)-[:A]->(b)-[:B]->(c) WHERE (a:X)-[:A]->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1:A]->(b)-[r2:B]->(c) WHERE (a)-[:A]->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .triadicSelection(positivePredicate = true, "a", "b", "c")
+      .|.expandAll("(b)-[r2:B]->(c)")
+      .|.argument("b", "r1")
+      .expandAll("(a)-[r1:A]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  test("MATCH (a:X)-[:A]->(b)<-[:B]-(c) WHERE (a:X)-[:A]->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1:A]->(b)<-[r2:B]-(c) WHERE (a)-[:A]->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .triadicSelection(positivePredicate = true, "a", "b", "c")
+      .|.expandAll("(b)<-[r2:B]-(c)")
+      .|.argument("b", "r1")
+      .expandAll("(a)-[r1:A]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  // Negative Predicate Expression and matching labels
+
+  test("MATCH (a:X)-->(b:Y)-->(c:Y) WHERE NOT (a)-->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b:Y)-[r2]->(c:Y) WHERE NOT (a)-->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .filter("not r1 = r2", "c:Y")
+      .triadicSelection(positivePredicate = false, "a", "b", "c")
+      .|.expandAll("(b)-[r2]->(c)")
+      .|.argument("b", "r1")
+      .filter("b:Y")
+      .expandAll("(a)-[r1]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  test("MATCH (a:X)-->(b:Y)-->(c:Z) WHERE NOT (a)-->(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b:Y)-[r2]->(c:Z) WHERE NOT (a)-->(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-->(b:Y)-->(c) WHERE NOT (a)-->(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b:Y)-[r2]->(c) WHERE NOT (a)-->(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-->(b)-->(c:Z) WHERE NOT (a)-->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b)-[r2]->(c:Z) WHERE NOT (a)-->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .filter("c:Z", "not r1 = r2")
+      .triadicSelection(positivePredicate = false, "a", "b", "c")
+      .|.expandAll("(b)-[r2]->(c)")
+      .|.argument("b", "r1")
+      .expandAll("(a)-[r1]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  test("MATCH (a:X)-->(b:Y:Z)-->(c:Z) WHERE NOT (a)-->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b:Y:Z)-[r2]->(c:Z) WHERE NOT (a)-->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .filter("c:Z", "not r1 = r2")
+      .triadicSelection(positivePredicate = false, "a", "b", "c")
+      .|.expandAll("(b)-[r2]->(c)")
+      .|.argument("b", "r1")
+      .filter("b:Y", "b:Z")
+      .expandAll("(a)-[r1]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  // Positive Predicate Expression and matching labels
+
+  test("MATCH (a:X)-->(b:Y)-->(c:Y) WHERE (a)-->(c)") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b:Y)-[r2]->(c:Y) WHERE (a)-->(c) RETURN 1").stripProduceResults
+
+    plan should equal(planner.subPlanBuilder()
+      .projection("1 AS 1")
+      .filter("not r1 = r2", "c:Y")
+      .triadicSelection(positivePredicate = true, "a", "b", "c")
+      .|.expandAll("(b)-[r2]->(c)")
+      .|.argument("b", "r1")
+      .filter("b:Y")
+      .expandAll("(a)-[r1]->(b)")
+      .nodeByLabelScan("a", "X")
+      .build())
+  }
+
+  test("MATCH (a:X)-->(b:Y)-->(c:Z) WHERE (a)-->(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b:Y)-[r2]->(c:Z) WHERE (a)-->(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-->(b:Y)-->(c) WHERE (a)-->(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b:Y)-[r2]->(c) WHERE (a)-->(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-->(b)-->(c:Z) WHERE (a)-->(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b)-[r2]->(c:Z) WHERE (a)-->(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+
+  test("MATCH (a:X)-->(b:Y:Z)-->(c:Z) WHERE (a)-->(c) passes through") {
+    val plan = planner.plan("MATCH (a:X)-[r1]->(b:Y:Z)-[r2]->(c:Z) WHERE (a)-->(c) RETURN 1").stripProduceResults
+
+    plan.folder.treeExists {
+      case _: TriadicSelection => true
+    } should be(false)
+  }
+}
