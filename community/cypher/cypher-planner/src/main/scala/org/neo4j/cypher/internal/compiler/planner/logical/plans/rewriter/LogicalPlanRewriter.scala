04fd8033799167e3076e28e9e874b81eea28078c
Satia Herfert <satiaherfert@gmx.de>
2022-06-29 15:11:49 +0200

    Remove unionScanRewriter

--- community/cypher/cypher-planner/src/main/scala/org/neo4j/cypher/internal/compiler/planner/logical/plans/rewriter/LogicalPlanRewriter.scala
+++ community/cypher/cypher-planner/src/main/scala/org/neo4j/cypher/internal/compiler/planner/logical/plans/rewriter/LogicalPlanRewriter.scala
@@ -82,7 +82,6 @@ case object PlanRewriter extends LogicalPlanRewriter with StepSequencer.Step wit
           otherAttributes.withAlso(effectiveCardinalities, solveds, providedOrders)
         ),
         combineHasLabels,
-        unionScanRewriter(solveds, otherAttributes.withAlso(cardinalities, effectiveCardinalities, providedOrders)),
         truncateDatabaseDeeagerizer
       )
     )
