914dabc63c0c65787b913ea55048eec53e8ac888
Matthew Parnell <matthew.parnell@neo4j.com>
2022-06-30 10:55:06 +0100

    Add somewhat 'standard' *nix exit codes
    Other than 0 for success and non-zero for error, there is no formal
    standard for exit codes.
    
    It's common for 1 to mean "general" error, but anything specific is
    usually left to the developer.
    
    sysexits is the closest to something, somewhat standard, and was
    introducted into BSD, and used by many *nix tools to indicate a few
    more specific errors, other than 1.
    
    This adds a class that wraps the constants from sysexits.h; though it
    would be ideal to be an enum, their use in annotations prevents this.

---
+++ community/command-line/src/main/java/org/neo4j/cli/ExitCode.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cli;
+
+/**
+ * System Exit Codes
+ * Based off of sysexits.h
+ */
+public class ExitCode {
+    // cannot be enum due to usage in annotations
+
+    public static final int OK = 0; // successful termination
+    public static final int FAIL = 1; // generic failure
+
+    public static final int USAGE = 64; // command line usage error
+    public static final int DATAERR = 65; // data format error
+    public static final int NOINPUT = 66; // cannot open input
+    public static final int NOUSER = 67; // addressee unknown
+    public static final int NOHOST = 68; // host name unknown
+    public static final int UNAVAILABLE = 69; // service unavailable
+    public static final int SOFTWARE = 70; // internal software error
+    public static final int OSERR = 71; // system error (e.g., can't fork)
+    public static final int OSFILE = 72; // critical OS file missing
+    public static final int CANTCREAT = 73; // can't create (user) output file
+    public static final int IOERR = 74; // input/output error
+    public static final int TEMPFAIL = 75; // temp failure; user is invited to retry
+    public static final int PROTOCOL = 76; // remote error in protocol
+    public static final int NOPERM = 77; // permission denied
+    public static final int CONFIG = 78; // configuration error
+
+    private ExitCode() {}
+}
