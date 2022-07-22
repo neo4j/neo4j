5b5416d0757c8a6d998dde4fc986ecbf693d4260
tselmegbaasan <tselmeg.baasan@neo4j.com>
2022-07-22 17:31:12 +0200

    Create default database resolver before setting up the procedure

--- community/neo4j/src/main/java/org/neo4j/graphdb/facade/DatabaseManagementServiceFactory.java
+++ community/neo4j/src/main/java/org/neo4j/graphdb/facade/DatabaseManagementServiceFactory.java
@@ -147,6 +147,8 @@ public class DatabaseManagementServiceFactory
 
         edition.bootstrapFabricServices();
 
+        edition.createDefaultDatabaseResolver( globalModule );
+        globalDependencies.satisfyDependency( edition.getDefaultDatabaseResolver() );
         setupProcedures( globalModule, edition, databaseManager );
 
         edition.registerSystemGraphComponents( globalModule.getSystemGraphComponents(), globalModule );
@@ -154,9 +156,6 @@ public class DatabaseManagementServiceFactory
 
         var dbmsRuntimeSystemGraphComponent = new DbmsRuntimeSystemGraphComponent( globalModule.getGlobalConfig() );
         globalModule.getSystemGraphComponents().register( dbmsRuntimeSystemGraphComponent );
-
-        edition.createDefaultDatabaseResolver( globalModule );
-        globalDependencies.satisfyDependency( edition.getDefaultDatabaseResolver() );
 
         edition.createSecurityModule( globalModule );
         SecurityProvider securityProvider = edition.getSecurityProvider();
