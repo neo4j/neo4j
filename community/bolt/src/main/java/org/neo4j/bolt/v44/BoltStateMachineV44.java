e03364be05881eeb5d19772dc206689108536879
Johannes Donath <johannes.donath@neo4j.com>
2022-09-08 17:14:05 +0200

    Fixed a bug which causes ROUTE to return FAILURE while the connection is in failed state. ()

--- community/bolt/src/main/java/org/neo4j/bolt/v44/BoltStateMachineV44.java
+++ community/bolt/src/main/java/org/neo4j/bolt/v44/BoltStateMachineV44.java
@@ -28,8 +28,8 @@ import org.neo4j.bolt.runtime.statemachine.impl.AbstractBoltStateMachine;
 import org.neo4j.bolt.transaction.TransactionManager;
 import org.neo4j.bolt.v3.runtime.InterruptedState;
 import org.neo4j.bolt.v4.runtime.AutoCommitState;
-import org.neo4j.bolt.v4.runtime.FailedState;
 import org.neo4j.bolt.v41.runtime.ConnectedState;
+import org.neo4j.bolt.v43.runtime.FailedState;
 import org.neo4j.bolt.v44.runtime.InTransactionState;
 import org.neo4j.bolt.v44.runtime.ReadyState;
 import org.neo4j.kernel.database.DefaultDatabaseResolver;
@@ -62,7 +62,7 @@ public class BoltStateMachineV44 extends AbstractBoltStateMachine
         var connected = new ConnectedState( connectionHints ); //v4.1
         var autoCommitState = new AutoCommitState(); // v4
         var inTransaction = new InTransactionState(); // v4.4
-        var failed = new FailedState(); // v4
+        var failed = new FailedState(); // v4.3
         var ready = new ReadyState( new ProcedureRoutingTableGetter() ); // v4.4
         var interrupted = new InterruptedState(); // v3
 
