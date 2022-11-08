eda203f5b991e271a0d84ccf3e8940a4aabbfce9
Georgiy Kargapolov <inmost-light@users.noreply.github.com>
2022-11-08 16:57:09 +0100

    Add QueryProjectionTest

---
+++ community/cypher/ir/src/test/scala/org/neo4j/cypher/internal/ir/QueryProjectionTest.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.cypher.internal.ir
+import org.neo4j.cypher.internal.ast.AstConstructionTestSupport
+import org.neo4j.cypher.internal.ast.semantics.SemanticTable
+import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
+
+class QueryProjectionTest extends CypherFunSuite with AstConstructionTestSupport {
+  test("should not mutate QueryProjection.empty state") {
+    val qp = QueryProjection.empty
+
+    qp.allQueryGraphs.foreach(_.allKnownUnstableNodeLabels(SemanticTable()))
+    qp.allQueryGraphs.foreach(_.allKnownUnstableNodeLabels.cacheSize shouldBe 1)
+
+    QueryProjection.empty.allQueryGraphs.foreach(_.allKnownUnstableNodeLabels.cacheSize shouldBe 0)
+  }
+}
