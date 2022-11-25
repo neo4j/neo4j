60d64ee679aa1a4c13110615e1579547e77d02e7
Pontus Melke <pontusmelke@gmail.com>
2022-11-25 14:50:31 +0100

    Code review: More tests

--- community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/IntersectionLabelScanTestBase.scala
+++ community/cypher/runtime-spec-suite/src/test/scala/org/neo4j/cypher/internal/runtime/spec/tests/IntersectionLabelScanTestBase.scala
@@ -27,6 +27,7 @@ import org.neo4j.cypher.internal.logical.plans.IndexOrderNone
 import org.neo4j.cypher.internal.runtime.spec.Edition
 import org.neo4j.cypher.internal.runtime.spec.LogicalQueryBuilder
 import org.neo4j.cypher.internal.runtime.spec.RuntimeTestSuite
+import org.neo4j.graphdb.Label
 
 abstract class IntersectionLabelScanTestBase[CONTEXT <: RuntimeContext](
   edition: Edition[CONTEXT],
@@ -166,8 +167,8 @@ abstract class IntersectionLabelScanTestBase[CONTEXT <: RuntimeContext](
   test("scan on the RHS of apply") {
     // given
     val (abNodes, cdNodes) = given {
-      val abNodes = nodeGraph(10, "A", "B")
-      val cdNodes = nodeGraph(10, "C", "D")
+      val abNodes = nodeGraph(sizeHint / 2, "A", "B")
+      val cdNodes = nodeGraph(sizeHint / 2, "C", "D")
       (abNodes, cdNodes)
     }
 
@@ -186,5 +187,129 @@ abstract class IntersectionLabelScanTestBase[CONTEXT <: RuntimeContext](
     // then
     val expected = for { ab <- abNodes; cd <- cdNodes } yield Array(ab, cd)
     runtimeResult should beColumns("x", "y").withRows(expected)
+  }
+
+  test("scan on the RHS of union") {
+    // given
+    val (abNodes, cdNodes) = given {
+      val abNodes = nodeGraph(sizeHint, "A", "B")
+      val cdNodes = nodeGraph(sizeHint, "C", "D")
+      (abNodes, cdNodes)
+    }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("x")
+      .union()
+      .|.filter("true")
+      .|.intersectionNodeByLabelsScan("x", Seq("C", "D"), IndexOrderNone, "x")
+      .filter("true")
+      .intersectionNodeByLabelsScan("x", Seq("A", "B"), IndexOrderNone)
+      .build()
+
+    val runtimeResult = execute(logicalQuery, runtime)
+
+    // then
+    runtimeResult should beColumns("x").withRows(singleColumn(abNodes ++ cdNodes))
+  }
+
+  test("scan on the RHS of cartesian product") {
+    // given
+    val (abNodes, cdNodes) = given {
+      val abNodes = nodeGraph(sizeHint / 2, "A", "B")
+      val cdNodes = nodeGraph(sizeHint / 2, "C", "D")
+      (abNodes, cdNodes)
+    }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("x", "y")
+      .cartesianProduct()
+      .|.filter("true")
+      .|.intersectionNodeByLabelsScan("y", Seq("C", "D"), IndexOrderNone, "x")
+      .filter("true")
+      .intersectionNodeByLabelsScan("x", Seq("A", "B"), IndexOrderNone)
+      .build()
+
+    val runtimeResult = execute(logicalQuery, runtime)
+
+    // then
+    val expected = for { ab <- abNodes; cd <- cdNodes } yield Array(ab, cd)
+    runtimeResult should beColumns("x", "y").withRows(expected)
+  }
+
+  test("join two scans") {
+    // given
+    val expected = given {
+      val abNodes = nodeGraph(sizeHint, "A", "B")
+      nodeGraph(sizeHint, "B", "C")
+      abNodes.zipWithIndex.foreach {
+        case (node, i) if i % 2 == 0 => node.addLabel(Label.label("C"))
+        case _                       => // do nothing
+      }
+      abNodes.filter(_.hasLabel(Label.label("C")))
+    }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("x")
+      .nodeHashJoin("x")
+      .|.filter("true")
+      .|.intersectionNodeByLabelsScan("x", Seq("B", "C"), IndexOrderNone, "x")
+      .filter("true")
+      .intersectionNodeByLabelsScan("x", Seq("A", "B"), IndexOrderNone)
+      .build()
+
+    val runtimeResult = execute(logicalQuery, runtime)
+
+    // then
+    runtimeResult should beColumns("x").withRows(singleColumn(expected))
+  }
+
+  test("scan + aggregation") {
+    given {
+      nodeGraph(sizeHint, "A")
+      nodeGraph(sizeHint, "A", "B")
+      nodeGraph(sizeHint, "A", "B", "C")
+    }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("c")
+      .aggregation(Seq.empty, Seq("count(*) AS c"))
+      .filter("x:A")
+      .intersectionNodeByLabelsScan("x", Seq("A", "B", "C"), IndexOrderNone)
+      .build()
+
+    val runtimeResult = execute(logicalQuery, runtime)
+
+    // then
+    runtimeResult should beColumns("c").withSingleRow(sizeHint)
+  }
+
+  test("scan on the RHS of apply with limit") {
+    // given
+    val (abNodes, cdNodes) = given {
+      val abNodes = nodeGraph(sizeHint, "A", "B")
+      val cdNodes = nodeGraph(sizeHint, "C", "D")
+      (abNodes, cdNodes)
+    }
+
+    // when
+    val logicalQuery = new LogicalQueryBuilder(this)
+      .produceResults("x", "y")
+      .apply()
+      .|.limit(10)
+      .|.filter("true")
+      .|.intersectionNodeByLabelsScan("y", Seq("C", "D"), IndexOrderNone, "x")
+      .limit(10)
+      .filter("true")
+      .intersectionNodeByLabelsScan("x", Seq("A", "B"), IndexOrderNone)
+      .build()
+
+    val runtimeResult = execute(logicalQuery, runtime)
+
+    // then
+    runtimeResult should beColumns("x", "y").withRows(rowCount(10 * 10))
   }
 }
