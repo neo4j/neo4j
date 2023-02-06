5846ab90773c32a099c13864fc711431d348651f
ConorNeo <121866402+ConorNeo@users.noreply.github.com>
2023-02-06 11:43:46 +0000

    Added Extra test for routing table for 2 different users ()

--- community/community-it/bolt-it/src/test/java/org/neo4j/bolt/RoutingTableIT.java
+++ community/community-it/bolt-it/src/test/java/org/neo4j/bolt/RoutingTableIT.java
@@ -34,6 +34,7 @@ import org.neo4j.bolt.test.annotation.test.ProtocolTest;
 import org.neo4j.bolt.test.annotation.wire.selector.ExcludeWire;
 import org.neo4j.bolt.test.annotation.wire.selector.Version;
 import org.neo4j.bolt.test.util.ServerUtil;
+import org.neo4j.bolt.testing.assertions.BoltConnectionAssertions;
 import org.neo4j.bolt.testing.client.TransportConnection;
 import org.neo4j.bolt.testing.messages.BoltWire;
 import org.neo4j.bolt.transport.Neo4jWithSocket;
@@ -101,6 +102,46 @@ public class RoutingTableIT {
                 .hasEntrySatisfying("rt", rt -> Assertions.assertThat(rt)
                         .asInstanceOf(InstanceOfAssertFactories.map(String.class, Object.class))
                         .satisfies(RoutingTableIT::assertRoutingTableHasCorrectShape)));
+    }
+
+    @ProtocolTest
+    @ExcludeWire({@Version(major = 4), @Version(major = 5, minor = 0)})
+    void shouldReturnTheSameRoutingForTwoDifferentUsers(BoltWire wire, @Authenticated TransportConnection connection)
+            throws IOException {
+        // Send the routing message and assure it is as intended
+        connection.send(wire.route(null, null, "neo4j"));
+        BoltConnectionAssertions.assertThat(connection).receivesSuccess(metadata -> {
+            Assertions.assertThat(metadata.containsKey("rt")).isTrue();
+            Assertions.assertThat(metadata.get("rt"))
+                    .isInstanceOf(Map.class)
+                    .satisfies(rt -> assertRoutingTableHasCorrectShape((Map<?, ?>) rt));
+        });
+
+        // add a new user
+        connection.send(wire.run("CREATE USER neo4j2 SET PASSWORD 'neo4jneo4jneo4j' CHANGE NOT REQUIRED"));
+        connection.send(wire.pull());
+        BoltConnectionAssertions.assertThat(connection).receivesSuccess(2);
+
+        // Logoff
+        connection.send(wire.logoff());
+        BoltConnectionAssertions.assertThat(connection).receivesSuccess();
+
+        // Logback on as a different user
+        connection.send(wire.logon(Map.of(
+                "scheme", "basic",
+                "principal", "neo4j2",
+                "credentials", "neo4jneo4jneo4j")));
+        BoltConnectionAssertions.assertThat(connection).receivesSuccess();
+
+        // Send another route and ensure that it is also correct and the same.
+        connection.send(wire.route(null, null, "neo4j"));
+
+        BoltConnectionAssertions.assertThat(connection).receivesSuccess(metadata -> {
+            Assertions.assertThat(metadata.containsKey("rt")).isTrue();
+            Assertions.assertThat(metadata.get("rt"))
+                    .isInstanceOf(Map.class)
+                    .satisfies(rt -> assertRoutingTableHasCorrectShape((Map<?, ?>) rt));
+        });
     }
 
     @ProtocolTest
