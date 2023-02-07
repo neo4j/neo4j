231b275b18085289862ee8901a6d5e6fb711dd8c
Satia Herfert <satiaherfert@gmx.de>
2023-02-07 10:27:52 +0100

    Formatting

--- community/cypher/cypher-logical-plans/src/main/scala/org/neo4j/cypher/internal/logical/plans/NestedPlanExpression.scala
+++ community/cypher/cypher-logical-plans/src/main/scala/org/neo4j/cypher/internal/logical/plans/NestedPlanExpression.scala
@@ -49,7 +49,7 @@ case class NestedPlanGetByNameExpression(
   override val solvedExpressionAsString: String
 )(val position: InputPosition) extends NestedPlanExpression
 
-abstract sealed class NestedPlanExpression extends Expression with SemanticCheckableExpression {
+sealed abstract class NestedPlanExpression extends Expression with SemanticCheckableExpression {
 
   def plan: LogicalPlan
 
