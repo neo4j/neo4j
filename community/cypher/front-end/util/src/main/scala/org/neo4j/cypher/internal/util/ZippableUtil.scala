85cb950eef51087418b4b820fc1d37bfa534fc42
Arne Fischereit <arne.fischereit@neo4j.com>
2023-02-13 09:38:12 +0100

    ZippableUtil introducing zipLeft and zipRight
    Co-authored-by: Satia Herfert <satiaherfert@gmx.de>

---
+++ community/cypher/front-end/util/src/main/scala/org/neo4j/cypher/internal/util/ZippableUtil.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.cypher.internal.util
+
+import scala.collection.BuildFrom
+
+object ZippableUtil {
+
+  implicit class Zippable[F[x] <: Iterable[x], A](left: F[A]) {
+
+    /**
+     * zip the two given sequences but fill right if it is too small.
+     */
+    def zipLeft[B](right: Iterable[B], fill: B)(implicit bf: BuildFrom[F[A], (A, B), F[(A, B)]]): F[(A, B)] = {
+      val right2 = right ++ Seq.fill(left.size - right.size)(fill)
+      val zipped = left.zip(right2)
+
+      bf.fromSpecific(left)(zipped)
+    }
+
+    /**
+     * zip the two given sequences but fill left if it is too small.
+     */
+    def zipRight[B](right: Iterable[B], fill: A)(implicit bf: BuildFrom[F[A], (A, B), F[(A, B)]]): F[(A, B)] = {
+      val left2 = left ++ Seq.fill(right.size - left.size)(fill)
+      val zipped = left2.zip(right)
+
+      bf.fromSpecific(left)(zipped)
+    }
+  }
+}
---
+++ community/cypher/front-end/util/src/test/scala/org/neo4j/cypher/internal/util/ZippableTest.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.cypher.internal.util
+
+import org.neo4j.cypher.internal.util.ZippableUtil.Zippable
+import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
+
+class ZippableTest extends CypherFunSuite {
+
+  test("should behave like zip/zipAll depending on which list is longer") {
+    for {
+      leftLength <- 1 to 10
+      left <- (1 to 10).combinations(leftLength)
+      rightLength <- 1 to 10
+      right <- (1 to 10).combinations(rightLength)
+    } {
+      if (left.length <= right.length) {
+        left.zipLeft(right, 0) should equal(left.zip(right))
+        left.zipRight(right, 0) should equal(left.zipAll(right, 0, 1))
+      } else {
+        left.zipLeft(right, 0) should equal(left.zipAll(right, 1, 0))
+        left.zipRight(right, 0) should equal(left.zip(right))
+      }
+    }
+  }
+}
