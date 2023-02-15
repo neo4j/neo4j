2cb6175dca821a236c6683ab0b78df6cb171b3df
Balazs Lendvai <balazs.lendvai@neotechnology.com>
2023-02-15 15:01:03 +0100

    Revert "Don't generate deterministic raft member ids"
    This reverts commit 0dac57c2074ae8b9dbc37d601e67d22fb38d5eb8.
    Upgrade is not possible, there needs to be a further investigation to this.

--- community/community-it/it-test-support/src/main/java/org/neo4j/dbms/systemgraph/BaseTopologyGraphDbmsModelIT.java
+++ community/community-it/it-test-support/src/main/java/org/neo4j/dbms/systemgraph/BaseTopologyGraphDbmsModelIT.java
@@ -86,7 +86,6 @@ import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -165,16 +164,6 @@ public abstract class BaseTopologyGraphDbmsModelIT {
             TopologyGraphDbmsModel.HostedOnMode mode,
             boolean bootstrapper,
             boolean wasHostedOn) {
-        connect(databaseId, serverId, mode, bootstrapper, wasHostedOn, UUID::randomUUID);
-    }
-
-    protected void connect(
-            NamedDatabaseId databaseId,
-            ServerId serverId,
-            TopologyGraphDbmsModel.HostedOnMode mode,
-            boolean bootstrapper,
-            boolean wasHostedOn,
-            Supplier<UUID> uuid) {
         try (var tx = db.beginTx()) {
             var database = findDatabase(databaseId, tx);
             var instance = findInstance(serverId, tx);
@@ -185,7 +174,7 @@ public abstract class BaseTopologyGraphDbmsModelIT {
             }
             if (mode == HostedOnMode.RAFT) {
                 relationship.setProperty(
-                        HOSTED_ON_RAFT_MEMBER_ID_PROPERTY, uuid.get().toString());
+                        HOSTED_ON_RAFT_MEMBER_ID_PROPERTY, UUID.randomUUID().toString());
             }
             tx.commit();
         }
