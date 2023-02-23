b5adade0921f9f74da9e7c8e481a5559270eed35
Anton Klarén <anton.klaren@neotechnology.com>
2023-02-23 11:51:36 +0100

    Check for old java versions using a multi version jar

---
+++ community/bootcheck/src/main/java/org/neo4j/server/startup/Neo4jBoot.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.server.startup;
+
+public class Neo4jBoot
+{
+
+    /**
+     * IMPORTANT NOTE! The only purpose of this class is just to forward to the actual bootloader. Its part of a multi-version jar to be able to provide a
+     * useful error message when used on an old and unsupported version of java
+     */
+    public static void main( String[] args )
+    {
+        Neo4jCommand.main( args );
+    }
+}
