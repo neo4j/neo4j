66ce65c850a8fcc2e1360caf73c58b5ef1600a57
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2023-04-19 11:49:37 +0200

    Adapt ServerPoliciesPlugin for use in server-side routing
    Return a rich result containing the internal bolt addresses and server ids.

---
+++ community/dbms/src/main/java/org/neo4j/dbms/routing/RichRoutingResult.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.dbms.routing;
+
+import java.util.List;
+import org.neo4j.configuration.helpers.SocketAddress;
+
+/**
+ * Extends {@link RoutingResult} with information needed by server-side routing
+ */
+public class RichRoutingResult extends RoutingResult {
+
+    private final List<RichRoutingServerAddress> routeServerAddresses;
+    private final List<RichRoutingServerAddress> writeServerAddresses;
+    private final List<RichRoutingServerAddress> readServerAddresses;
+
+    public RichRoutingResult(
+            List<RichRoutingServerAddress> routeServerAddresses,
+            List<RichRoutingServerAddress> writeServerAddresses,
+            List<RichRoutingServerAddress> readServerAddresses,
+            long timeToLiveMillis) {
+        super(
+                externalBoltAddresses(routeServerAddresses),
+                externalBoltAddresses(writeServerAddresses),
+                externalBoltAddresses(readServerAddresses),
+                timeToLiveMillis);
+        this.routeServerAddresses = routeServerAddresses;
+        this.writeServerAddresses = writeServerAddresses;
+        this.readServerAddresses = readServerAddresses;
+    }
+
+    public List<RichRoutingServerAddress> routeServerAddresses() {
+        return routeServerAddresses;
+    }
+
+    public List<RichRoutingServerAddress> writeServerAddresses() {
+        return writeServerAddresses;
+    }
+
+    public List<RichRoutingServerAddress> readServerAddresses() {
+        return readServerAddresses;
+    }
+
+    private static List<SocketAddress> externalBoltAddresses(List<RichRoutingServerAddress> serverAddresses) {
+        return serverAddresses.stream()
+                .flatMap(sa -> sa.externalBoltAddress().stream())
+                .toList();
+    }
+}
