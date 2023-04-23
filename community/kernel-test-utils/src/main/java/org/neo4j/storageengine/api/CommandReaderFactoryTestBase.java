626c65d0c45e78cf6bb3e41a0843d5f0516680c2
Matthew Parnell <matthew.parnell@neo4j.com>
2023-04-23 22:56:18 +0100

    test: CommandReaderFactory is providing a CommandReader that handles requested KernelVersion

---
+++ community/kernel-test-utils/src/main/java/org/neo4j/storageengine/api/CommandReaderFactoryTestBase.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.storageengine.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.neo4j.kernel.KernelVersion;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public abstract class CommandReaderFactoryTestBase<FACTORY extends CommandReaderFactory> {
+
+    private final FACTORY factory;
+
+    protected CommandReaderFactoryTestBase(FACTORY factory) {
+        this.factory = factory;
+    }
+
+    protected abstract Iterable<KernelVersion> supported();
+
+    protected abstract Iterable<KernelVersion> unsupported();
+
+    @ParameterizedTest
+    @MethodSource("unsupported")
+    void shouldThrowOnSupportedVersions(KernelVersion kernelVersion) {
+        assertThatThrownBy(() -> factory.get(kernelVersion), "acquiring reader for unsupported kernel version")
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContainingAll("Serialization is not supported", kernelVersion.name());
+    }
+
+    @ParameterizedTest
+    @MethodSource("supported")
+    void shouldProviderReaderWithCorrespondingKernelVersion(KernelVersion kernelVersion) {
+        assertReaderHandlesKernelVersions(kernelVersion);
+    }
+
+    @Test
+    void shouldProviderReaderForGloriousFuture() {
+        assertReaderHandlesKernelVersions(KernelVersion.GLORIOUS_FUTURE);
+    }
+
+    private void assertReaderHandlesKernelVersions(KernelVersion kernelVersion) {
+        assertThat(factory.get(kernelVersion).kernelVersion())
+                .as("reader should handle %s", kernelVersion)
+                .isEqualTo(kernelVersion);
+    }
+}
---
+++ community/record-storage-engine/src/test/java/org/neo4j/internal/recordstorage/RecordStorageCommandReaderFactoryTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.internal.recordstorage;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.neo4j.kernel.KernelVersion;
+import org.neo4j.storageengine.api.CommandReaderFactoryTestBase;
+
+final class RecordStorageCommandReaderFactoryTest
+        extends CommandReaderFactoryTestBase<RecordStorageCommandReaderFactory> {
+    private static final Set<KernelVersion> SUPPORTED;
+    private static final Set<KernelVersion> UNSUPPORTED;
+
+    static {
+        final var supported = KernelVersion.VERSIONS.stream()
+                .filter(v -> v.isAtLeast(KernelVersion.EARLIEST))
+                .collect(Collectors.toCollection(() -> EnumSet.noneOf(KernelVersion.class)));
+        UNSUPPORTED = Collections.unmodifiableSet(EnumSet.complementOf(supported));
+        supported.remove(KernelVersion.GLORIOUS_FUTURE);
+        SUPPORTED = Collections.unmodifiableSet(supported);
+    }
+
+    private RecordStorageCommandReaderFactoryTest() {
+        super(RecordStorageCommandReaderFactory.INSTANCE);
+    }
+
+    @Override
+    protected Iterable<KernelVersion> supported() {
+        return SUPPORTED;
+    }
+
+    @Override
+    protected Iterable<KernelVersion> unsupported() {
+        return UNSUPPORTED;
+    }
+}
