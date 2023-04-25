2fe3a552048838ee9b9e7a452dd71991cf061657
Love Kristofer Leifland <kristofer.leifland@gmail.com>
2023-04-25 09:16:02 +0200

    Revert "Fix test"
    This reverts commit 3dea180c6d93a0a716234c26fb54b3e9b6a5cf82.

--- community/cypher-shell/cypher-shell/src/test/java/org/neo4j/shell/UserMessagesHandlerTest.java
+++ community/cypher-shell/cypher-shell/src/test/java/org/neo4j/shell/UserMessagesHandlerTest.java
@@ -25,11 +25,8 @@ import static org.mockito.Mockito.when;
 import static org.neo4j.shell.test.Util.testConnectionConfig;
 
 import java.util.Optional;
-
-import org.fusesource.jansi.Ansi;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.neo4j.shell.printer.AnsiFormattedText;
 
 class UserMessagesHandlerTest {
     private Connector connector;
@@ -41,7 +38,6 @@ class UserMessagesHandlerTest {
         when(connector.getProtocolVersion()).thenReturn("3.0");
         final var connectionConfig = testConnectionConfig("bolt://some.place.com:99");
         when(connector.connectionConfig()).thenReturn(connectionConfig);
-        Ansi.setEnabled(true);
     }
 
     @Test
