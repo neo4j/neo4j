6e513c1b0e9a45f0ef83dd4091dd41c41a546c02
gjmwoods <42248895+gjmwoods@users.noreply.github.com>
2023-05-11 09:57:31 +0100

    Increase timeout on ResetLockedNodeIT ()

--- community/community-it/bolt-it/src/test/java/org/neo4j/bolt/ResetLockedNodeIT.java
+++ community/community-it/bolt-it/src/test/java/org/neo4j/bolt/ResetLockedNodeIT.java
@@ -39,7 +39,7 @@ import org.neo4j.values.virtual.MapValueBuilder;
 @BoltTestExtension
 public class ResetLockedNodeIT {
 
-    @Timeout(15)
+    @Timeout(30)
     @TransportTest
     void shouldErrorWhenResettingAConnectionWaitingOnALock(
             BoltWire wire,
