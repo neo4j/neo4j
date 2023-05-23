d420f847b2f4efbc007d3c24e10b4663edb4f575
gjmwoods <42248895+gjmwoods@users.noreply.github.com>
2023-05-23 16:13:32 +0100

    Add IT for Strict-Transport-Security ()

--- community/community-it/community-it/src/test/java/org/neo4j/server/HttpsAccessIT.java
+++ community/community-it/community-it/src/test/java/org/neo4j/server/HttpsAccessIT.java
@@ -19,6 +19,7 @@
  */
 package org.neo4j.server;
 
+import org.eclipse.jetty.http.HttpHeader;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,6 +31,7 @@ import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
 
 import org.neo4j.internal.helpers.HostnamePort;
+import org.neo4j.server.configuration.ServerSettings;
 import org.neo4j.server.helpers.CommunityWebContainerBuilder;
 import org.neo4j.server.helpers.TestWebContainer;
 import org.neo4j.test.PortUtils;
@@ -99,6 +101,20 @@ class HttpsAccessIT extends ExclusiveWebContainerTestBase
         shouldExposeCorrectSchemeInDiscoveryService( "https" );
     }
 
+    @Test
+    void shouldIncludeStrictTransportHeaderIfConfigured() throws Exception
+    {
+        var transportHeader = "max-age=31536000";
+        startServer( false, true, transportHeader );
+        shouldInstallConnector( "https" );
+
+        var response = GET( testWebContainer.getBaseUri().toString() );
+
+        assertThat( response.status() ).isEqualTo( 200 );
+        assertThat( response.header( HttpHeader.STRICT_TRANSPORT_SECURITY.lowerCaseName() ) )
+                .isEqualTo( transportHeader );
+    }
+
     private void shouldInstallConnector( String scheme )
     {
         var uri = testWebContainer.getBaseUri();
@@ -118,10 +134,15 @@ class HttpsAccessIT extends ExclusiveWebContainerTestBase
 
     private void startServer() throws Exception
     {
-        startServer( true, true );
+        startServer( true, true, null );
     }
 
     private void startServer( boolean httpEnabled, boolean httpsEnabled ) throws Exception
+    {
+        startServer( httpEnabled, httpsEnabled, null );
+    }
+
+    private void startServer( boolean httpEnabled, boolean httpsEnabled, String strictTransportHeader ) throws Exception
     {
         CommunityWebContainerBuilder serverBuilder = serverOnRandomPorts().usingDataDir( testDirectory.directory( methodName ).toAbsolutePath().toString() );
         if ( !httpEnabled )
@@ -131,6 +152,10 @@ class HttpsAccessIT extends ExclusiveWebContainerTestBase
         if ( httpsEnabled )
         {
             serverBuilder.withHttpsEnabled();
+        }
+        if ( strictTransportHeader != null )
+        {
+            serverBuilder.withProperty( ServerSettings.http_strict_transport_security.name(), strictTransportHeader );
         }
 
         testWebContainer = serverBuilder.build();
