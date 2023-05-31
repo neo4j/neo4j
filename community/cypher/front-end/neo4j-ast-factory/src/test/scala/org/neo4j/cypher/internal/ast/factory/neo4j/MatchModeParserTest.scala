fe2bf0c3b36b45e6265ae408043a9bd188fbd3fd
Georgiy Kargapolov <inmost-light@users.noreply.github.com>
2023-05-31 15:46:34 +0200

    Fix javacc warnings for MATCH modes

--- community/cypher/front-end/neo4j-ast-factory/src/test/scala/org/neo4j/cypher/internal/ast/factory/neo4j/MatchModeParserTest.scala
+++ community/cypher/front-end/neo4j-ast-factory/src/test/scala/org/neo4j/cypher/internal/ast/factory/neo4j/MatchModeParserTest.scala
@@ -21,9 +21,12 @@ package org.neo4j.cypher.internal.ast.factory.neo4j
 
 import org.neo4j.cypher.internal.ast.AstConstructionTestSupport
 import org.neo4j.cypher.internal.ast.Clause
+import org.neo4j.cypher.internal.ast.Match
 import org.neo4j.cypher.internal.expressions.MatchMode
+import org.neo4j.cypher.internal.expressions.NamedPatternPart
 import org.neo4j.cypher.internal.expressions.Pattern
 import org.neo4j.cypher.internal.expressions.PatternPart
+import org.neo4j.cypher.internal.expressions.PatternPartWithSelector
 import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
 
 class MatchModeParserTest extends CypherFunSuite with JavaccParserAstTestBase[Clause]
@@ -171,7 +174,87 @@ class MatchModeParserTest extends CypherFunSuite with JavaccParserAstTestBase[Cl
     }
   }
 
+  test("MATCH REPEATABLE ELEMENTS path = (a)-[r]->(b)") {
+    gives {
+      Match(
+        optional = false,
+        matchMode = MatchMode.RepeatableElements()(pos),
+        hints = List(),
+        where = None,
+        pattern = Pattern(Seq(NamedPatternPart(
+          varFor("path"),
+          PatternPartWithSelector(
+            relationshipChain(nodePat(Some("a")), relPat(Some("r")), nodePat(Some("b"))),
+            allPathsSelector()
+          )
+        )(pos)))(pos)
+      )(pos)
+    }
+  }
+
+  test("MATCH REPEATABLE = (a)-[r]->(b)") {
+    gives {
+      Match(
+        optional = false,
+        matchMode = MatchMode.default(pos),
+        hints = List(),
+        where = None,
+        pattern = Pattern(Seq(NamedPatternPart(
+          varFor("REPEATABLE"),
+          PatternPartWithSelector(
+            relationshipChain(nodePat(Some("a")), relPat(Some("r")), nodePat(Some("b"))),
+            allPathsSelector()
+          )
+        )(pos)))(pos)
+      )(pos)
+    }
+  }
+
+  test("MATCH DIFFERENT RELATIONSHIPS path = (x)-[p]->(y)") {
+    gives {
+      Match(
+        optional = false,
+        matchMode = MatchMode.DifferentRelationships()(pos),
+        hints = List(),
+        where = None,
+        pattern = Pattern(Seq(NamedPatternPart(
+          varFor("path"),
+          PatternPartWithSelector(
+            relationshipChain(nodePat(Some("x")), relPat(Some("p")), nodePat(Some("y"))),
+            allPathsSelector()
+          )
+        )(pos)))(pos)
+      )(pos)
+    }
+  }
+
+  test("MATCH DIFFERENT = (x)-[p]->(y)") {
+    gives {
+      Match(
+        optional = false,
+        matchMode = MatchMode.default(pos),
+        hints = List(),
+        where = None,
+        pattern = Pattern(Seq(NamedPatternPart(
+          varFor("DIFFERENT"),
+          PatternPartWithSelector(
+            relationshipChain(nodePat(Some("x")), relPat(Some("p")), nodePat(Some("y"))),
+            allPathsSelector()
+          )
+        )(pos)))(pos)
+      )(pos)
+    }
+  }
+
   test("MATCH () WHERE COLLECT {REPEATABLE ELEMENTS (n)-->(m) RETURN *}") {
+    failsToParse
+  }
+
+  test("MATCH REPEATABLE ELEMENT BINDINGS = ()-->()") {
+    failsToParse
+  }
+
+  test("MATCH DIFFERENT RELATIONSHIP BINDINGS = ()-->()") {
     failsToParse
   }
 }
