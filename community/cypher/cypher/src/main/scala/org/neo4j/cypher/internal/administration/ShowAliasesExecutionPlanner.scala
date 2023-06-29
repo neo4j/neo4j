f9d4a541fdb5cfd2bed5a35ea3113821f5de6e04
Mark Dixon <1756429+mnd999@users.noreply.github.com>
2023-06-29 08:45:51 +0100

    Filter databases in SHOW ALIASES ()
    The databases column in the SHOW ALIASES result set should be filtered in line with a users database visibility. Previously it was not. This change is easiest to accomplish by driving the result table from the DatabaseReferenceRepository instead of querying the system graph directly.

--- community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/administration/ShowAliasesExecutionPlanner.scala
+++ community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/administration/ShowAliasesExecutionPlanner.scala
@@ -23,9 +23,14 @@ import org.neo4j.cypher.internal.AdministrationCommandRuntime.DatabaseNameFields
 import org.neo4j.cypher.internal.AdministrationCommandRuntime.IdentityConverter
 import org.neo4j.cypher.internal.AdministrationCommandRuntime.checkNamespaceExists
 import org.neo4j.cypher.internal.AdministrationCommandRuntime.getDatabaseNameFields
+import org.neo4j.cypher.internal.AdministrationCommandRuntime.internalKey
 import org.neo4j.cypher.internal.AdministrationShowCommandUtils
 import org.neo4j.cypher.internal.ExecutionEngine
 import org.neo4j.cypher.internal.ExecutionPlan
+import org.neo4j.cypher.internal.administration.ShowAliasesExecutionPlanner.Alias
+import org.neo4j.cypher.internal.administration.ShowAliasesExecutionPlanner.LOCAL
+import org.neo4j.cypher.internal.administration.ShowAliasesExecutionPlanner.REMOTE
+import org.neo4j.cypher.internal.administration.ShowAliasesExecutionPlanner.aliasTargetParameter
 import org.neo4j.cypher.internal.ast.DatabaseName
 import org.neo4j.cypher.internal.ast.NamespacedName
 import org.neo4j.cypher.internal.ast.ParameterName
@@ -35,25 +40,34 @@ import org.neo4j.cypher.internal.procs.ParameterTransformer
 import org.neo4j.cypher.internal.procs.SystemCommandExecutionPlan
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.ALIAS_PROPERTIES
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.CONNECTS_WITH
-import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.DATABASE
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.DATABASE_NAME
+import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.DEFAULT_NAMESPACE
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.DISPLAY_NAME_PROPERTY
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.DRIVER_SETTINGS
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.NAMESPACE_PROPERTY
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.NAME_PROPERTY
-import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.PRIMARY_PROPERTY
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.PROPERTIES
-import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.REMOTE_DATABASE
-import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.TARGETS
-import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.TARGET_NAME_PROPERTY
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.URL_PROPERTY
 import org.neo4j.dbms.systemgraph.TopologyGraphDbmsModel.USERNAME_PROPERTY
+import org.neo4j.graphdb.Transaction
 import org.neo4j.internal.kernel.api.security.SecurityAuthorizationHandler
+import org.neo4j.internal.kernel.api.security.SecurityContext
+import org.neo4j.kernel.database.DatabaseReference
+import org.neo4j.kernel.database.DatabaseReferenceImpl
+import org.neo4j.kernel.database.DatabaseReferenceRepository
+import org.neo4j.values.AnyValue
+import org.neo4j.values.storable.Values
+import org.neo4j.values.virtual.MapValue
 import org.neo4j.values.virtual.VirtualValues
+
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+import scala.jdk.CollectionConverters.SeqHasAsJava
+import scala.jdk.OptionConverters.RichOptional
 
 case class ShowAliasesExecutionPlanner(
   normalExecutionEngine: ExecutionEngine,
-  securityAuthorizationHandler: SecurityAuthorizationHandler
+  securityAuthorizationHandler: SecurityAuthorizationHandler,
+  referenceResolver: DatabaseReferenceRepository
 ) {
 
   def planShowAliasesForDatabase(
@@ -70,19 +84,16 @@ case class ShowAliasesExecutionPlanner(
     val (aliasNameFields, aliasPropertyFilter) = filterAliasByName(aliasName)
 
     val query =
-      s"""MATCH (alias:$DATABASE_NAME)
-         |$aliasPropertyFilter
-         |OPTIONAL MATCH (alias)-[:$TARGETS]->(localDatabase:$DATABASE)<-[:$TARGETS]-(localPrimary:$DATABASE_NAME {$PRIMARY_PROPERTY: true})
-         |OPTIONAL MATCH (alias)-[:$CONNECTS_WITH]->(driverSettings:$DRIVER_SETTINGS)
-         |OPTIONAL MATCH (alias)-[:$PROPERTIES]->(properties:$ALIAS_PROPERTIES)
+      s"""UNWIND $$$aliasTargetParameter AS alias
+         |WITH alias $aliasPropertyFilter
+         |MATCH (aliasNode:$DATABASE_NAME{$NAME_PROPERTY: alias.name, $NAMESPACE_PROPERTY: alias.namespace})
+         |OPTIONAL MATCH (aliasNode)-[:$CONNECTS_WITH]->(driverSettings:$DRIVER_SETTINGS)
+         |OPTIONAL MATCH (aliasNode)-[:$PROPERTIES]->(properties:$ALIAS_PROPERTIES)
          |WITH alias.$DISPLAY_NAME_PROPERTY as name,
-         |coalesce(localPrimary.$NAME_PROPERTY, alias.$TARGET_NAME_PROPERTY) as database,
-         |CASE
-         | WHEN "$REMOTE_DATABASE" in labels(alias) THEN "remote"
-         | ELSE "local"
-         |END AS location,
-         |alias.$URL_PROPERTY as url,
-         |alias.$USERNAME_PROPERTY as user
+         |alias.database as database,
+         |alias.location as location,
+         |aliasNode.$URL_PROPERTY as url,
+         |aliasNode.$USERNAME_PROPERTY as user
          |$verboseColumns
          |$returnStatement
          |""".stripMargin
@@ -94,7 +105,9 @@ case class ShowAliasesExecutionPlanner(
       aliasNameFields.map(anf => VirtualValues.map(anf.keys, anf.values)).getOrElse(VirtualValues.EMPTY_MAP),
       source = sourcePlan,
       parameterTransformer =
-        ParameterTransformer().convert(aliasNameFields.map(_.nameConverter).getOrElse(IdentityConverter))
+        ParameterTransformer(generateVisibleAliases).convert(
+          aliasNameFields.map(_.nameConverter).getOrElse(IdentityConverter)
+        )
           .validate(aliasNameFields.map(checkNamespaceExists).getOrElse((_, p) => (p, Set.empty)))
     )
   }
@@ -109,18 +122,59 @@ case class ShowAliasesExecutionPlanner(
     // If it is a parameter, it could be either something that should be escaped, or an alias in a composite (or both)
     def filter(anf: DatabaseNameFields) = aliasName match {
       case Some(NamespacedName(_, _)) =>
-        s"""AND alias.$NAME_PROPERTY = $$`${anf.nameKey}` AND alias.$NAMESPACE_PROPERTY = $$`${anf.namespaceKey}`"""
-      case Some(ParameterName(_)) => s"AND alias.$DISPLAY_NAME_PROPERTY = $$`${anf.displayNameKey}`"
-      case None                   => s""
+        s"""WHERE alias.$NAME_PROPERTY = $$`${anf.nameKey}` AND alias.$NAMESPACE_PROPERTY = $$`${anf.namespaceKey}`"""
+      case Some(ParameterName(_)) => s"WHERE alias.$DISPLAY_NAME_PROPERTY = $$`${anf.displayNameKey}`"
+      case None                   => ""
     }
     val aliasPropertyFilter = aliasNameFields
-      .map(anf =>
-        s"""
-           | WHERE alias.$PRIMARY_PROPERTY = false ${filter(anf)}
-           |""".stripMargin
-      )
-      .getOrElse(s"WHERE alias.$PRIMARY_PROPERTY = false")
+      .map(anf => filter(anf)).getOrElse("")
     (aliasNameFields, aliasPropertyFilter)
   }
 
+  private def generateVisibleAliases(tx: Transaction, sc: SecurityContext): MapValue = {
+
+    def referencesToAlias(alias: DatabaseReference): Iterable[Alias] = alias match {
+      case a: DatabaseReferenceImpl.External => Seq(Alias(
+          a.alias().name(),
+          a.namespace().toScala.map(_.name()).getOrElse(DEFAULT_NAMESPACE),
+          Some(a.targetAlias().name()),
+          REMOTE
+        ))
+      case c: DatabaseReferenceImpl.Composite => c.constituents().asScala.flatMap(referencesToAlias)
+      case a: DatabaseReferenceImpl.Internal if !a.isPrimary =>
+        val primary = referenceResolver.getByAlias(a.databaseId().name()).toScala.collect {
+          case ref if sc.databaseAccessMode().canSeeDatabase(ref) => ref.alias().name()
+        }
+        Seq(Alias(a.alias().name(), a.namespace().toScala.map(_.name()).getOrElse(DEFAULT_NAMESPACE), primary, LOCAL))
+      case _ => Seq.empty
+    }
+
+    val namedDatabasesAndAliases: List[AnyValue] = referenceResolver.getAllDatabaseReferences.asScala
+      .flatMap(a => referencesToAlias(a).map(_.asMapValue)).toList
+    VirtualValues.map(
+      Array(aliasTargetParameter),
+      Array(VirtualValues.fromList(namedDatabasesAndAliases.asJava))
+    )
+  }
+}
+
+object ShowAliasesExecutionPlanner {
+  private val aliasTargetParameter = internalKey("aliasTargets")
+  private val LOCAL = "local"
+  private val REMOTE = "remote"
+
+  private case class Alias(name: String, namespace: String, database: Option[String], location: String) {
+    private val displayName = if (namespace == DEFAULT_NAMESPACE) name else s"$namespace.$name"
+
+    def asMapValue: MapValue = VirtualValues.map(
+      Array("name", "namespace", "database", "displayName", "location"),
+      Array(
+        Values.stringValue(name),
+        Values.stringValue(namespace),
+        database.map(Values.stringValue).getOrElse(Values.NO_VALUE),
+        Values.stringValue(displayName),
+        Values.stringValue(location)
+      )
+    )
+  }
 }
