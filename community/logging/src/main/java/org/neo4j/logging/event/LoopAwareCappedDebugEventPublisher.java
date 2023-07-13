dfa8acc1f8cef5d478720aaeec6ad32bf30c803f
Nick Giles <100630647+nick-giles-neo@users.noreply.github.com>
2023-07-13 10:28:43 +0100

    Add a loop aware logger for Raft binding ()
    Raft binding runs in a loop, and sometimes retries the same loop repeatedly on a short interval, which can spam the logs.
    
    The loop aware publisher will ignore loops where all the messages are the same as the last loop, but if any of the messages in a loop are different, it will log all the messages. This will affect their timestamp, and the order relative to log messages from other loggers in that case, but will preserve the order of messages within the logger.

---
+++ community/logging/src/main/java/org/neo4j/logging/event/LoopAwareCappedDebugEventPublisher.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.logging.event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * For the first loop, everything is logged.
+ * For the next loop, if all the log events are the same, nothing is logged.
+ * If any log event is different, all of them are logged, including the ones in the past.
+ * That loop is now the new baseline for future loops.
+ * If we reach the maximum number of loops without any change, publish an event that says so, publish that loop of
+ * events, and reset the counter.
+ *
+ * <b>N.B.</b> If you use this class without calling {@link #loopComplete()} it will continue to accumulate events
+ * and consume more memory, potentially leading to running out of heap. So consider the amount of memory used by the
+ * events of each loop when deciding to use this class.
+ */
+public class LoopAwareCappedDebugEventPublisher implements LoopAwareDebugEventPublisher {
+
+    private final DebugEventPublisher delegate;
+    private final int maximumNumberOfLoopsWithoutPassingOnEvent;
+
+    private List<Event> lastLoggedLoop = null;
+    private List<Event> currentLoop = new ArrayList<>();
+    private int loopsWithNoChange = 0;
+
+    public LoopAwareCappedDebugEventPublisher(
+            DebugEventPublisher delegate, int maximumNumberOfLoopsWithoutPassingOnEvent) {
+        this.delegate = delegate;
+        this.maximumNumberOfLoopsWithoutPassingOnEvent = maximumNumberOfLoopsWithoutPassingOnEvent;
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    private synchronized void addEvent(Event event) {
+        currentLoop.add(event);
+        if (lastLoggedLoop == null) {
+            event.apply(delegate);
+        } else if (lastLoggedLoop.size() >= currentLoop.size()
+                && lastLoggedLoop.get(currentLoop.size() - 1).equals(event)) {
+            // Same so far
+        } else {
+            // Different now
+            lastLoggedLoop = null;
+            logWholeLoop();
+        }
+    }
+
+    @Override
+    public synchronized void loopComplete() {
+        if (lastLoggedLoop == null) {
+            // First loop, or found a different message part way through the loop
+            lastLoggedLoop = currentLoop;
+        } else if (!lastLoggedLoop.equals(currentLoop)) {
+            // Loops had different content at the end (extra or missing events)
+            logWholeLoop();
+            lastLoggedLoop = currentLoop;
+        } else {
+            // Loop was the same
+            if (loopsWithNoChange >= maximumNumberOfLoopsWithoutPassingOnEvent) {
+                logWholeLoop();
+            } else {
+                loopsWithNoChange++;
+            }
+        }
+        currentLoop = new ArrayList<>();
+    }
+
+    private void logWholeLoop() {
+        if (loopsWithNoChange > 0) {
+            delegate.publish(
+                    Type.Info,
+                    String.format(
+                            "There were %d repetitions of the same loop of events which were not logged",
+                            loopsWithNoChange));
+            loopsWithNoChange = 0;
+        }
+        currentLoop.forEach(storedLogEvent -> storedLogEvent.apply(delegate));
+    }
+
+    @Override
+    public void publish(Type type, String message, Parameters parameters) {
+        addEvent(new Event(type, message, parameters));
+    }
+
+    @Override
+    public void publish(Type type, String message) {
+        addEvent(new Event(type, message, null));
+    }
+
+    private record Event(Type type, String message, Parameters parameters) {
+        void apply(DebugEventPublisher publisher) {
+            if (parameters == null) {
+                publisher.publish(type, message);
+            } else {
+                publisher.publish(type, message, parameters);
+            }
+        }
+    }
+}
---
+++ community/logging/src/test/java/org/neo4j/logging/event/LoopAwareCappedDebugEventPublisherTest.java
@@ -0,0 +1,246 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.logging.event;
+
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+class LoopAwareCappedDebugEventPublisherTest {
+
+    private static final int MAXIMUM_LOG_INTERVAL = 5;
+    private static final String MESSAGE_1 = "Some message";
+    private static final String MESSAGE_2 = "Some other message";
+    private static final String MESSAGE_3 = "A third message!";
+    private static final String MESSAGE_4 = "This is too many messages";
+
+    private final DebugEventPublisher mockPublisher = mock(DebugEventPublisher.class);
+
+    private final LoopAwareCappedDebugEventPublisher publisher =
+            new LoopAwareCappedDebugEventPublisher(mockPublisher, MAXIMUM_LOG_INTERVAL);
+
+    @ParameterizedTest
+    @EnumSource(Type.class)
+    void whenSingleEvent_thenShouldLog(Type type) {
+        publisher.publish(type, MESSAGE_1);
+        var parameters = Parameters.of("key1", "value1");
+        publisher.publish(type, MESSAGE_2, parameters);
+
+        var inOrder = inOrder(mockPublisher);
+        inOrder.verify(mockPublisher).publish(type, MESSAGE_1);
+        inOrder.verify(mockPublisher).publish(type, MESSAGE_2, parameters);
+    }
+
+    @Test
+    void whenEventRepeatsWithinLoop_thenShouldLog() {
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+        publisher.publish(Type.Info, MESSAGE_2);
+
+        var inOrder = inOrder(mockPublisher);
+        inOrder.verify(mockPublisher).publish(Type.Begin, MESSAGE_1);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        inOrder.verify(mockPublisher).publish(Type.Finish, MESSAGE_3);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    void whenWholeLoopIsTheSame_thenShouldNotLog() {
+        // Given
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+        publisher.loopComplete();
+        reset(mockPublisher);
+
+        // When
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+
+        publisher.loopComplete();
+
+        // Then
+        verifyNoMoreInteractions(mockPublisher);
+    }
+
+    @Test
+    void whenLoopHasDifferentLogEventAtTheEnd_thenShouldLog() {
+        // Given
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+        publisher.loopComplete();
+        reset(mockPublisher);
+
+        // When
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Warn, MESSAGE_3); // Different type
+
+        publisher.loopComplete();
+
+        // Then
+        var inOrder = inOrder(mockPublisher);
+        inOrder.verify(mockPublisher).publish(Type.Begin, MESSAGE_1);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        inOrder.verify(mockPublisher).publish(Type.Warn, MESSAGE_3);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    void whenLoopHasDifferentLogEventAtTheStart_thenShouldLog() {
+        // Given
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+        publisher.loopComplete();
+
+        // When
+        publisher.publish(Type.Warn, MESSAGE_4);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+
+        // Then
+        var inOrder = inOrder(mockPublisher);
+        inOrder.verify(mockPublisher).publish(Type.Begin, MESSAGE_1);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        inOrder.verify(mockPublisher).publish(Type.Finish, MESSAGE_3);
+        inOrder.verify(mockPublisher).publish(Type.Warn, MESSAGE_4);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        inOrder.verify(mockPublisher).publish(Type.Finish, MESSAGE_3);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    void whenLoopHasExtraLogEvent_thenShouldLog() {
+        // Given
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+        publisher.loopComplete();
+
+        // When
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+        publisher.publish(Type.Warn, MESSAGE_4);
+
+        // Then
+        var inOrder = inOrder(mockPublisher);
+        inOrder.verify(mockPublisher).publish(Type.Begin, MESSAGE_1);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        inOrder.verify(mockPublisher).publish(Type.Finish, MESSAGE_3);
+        inOrder.verify(mockPublisher).publish(Type.Begin, MESSAGE_1);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        inOrder.verify(mockPublisher).publish(Type.Finish, MESSAGE_3);
+        inOrder.verify(mockPublisher).publish(Type.Warn, MESSAGE_4);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    void whenLoopHasMissingLogEvent_thenShouldLog() {
+        // Given
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+        publisher.loopComplete();
+
+        // When
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+
+        // Then
+        var inOrder = inOrder(mockPublisher);
+        inOrder.verify(mockPublisher).publish(Type.Begin, MESSAGE_1);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        inOrder.verify(mockPublisher).publish(Type.Finish, MESSAGE_3);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        inOrder.verify(mockPublisher).publish(Type.Finish, MESSAGE_3);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    void whenLoopHasEventWithSameMessageButDifferentParameters_thenShouldLog() {
+        // Given
+        var parameters1 = Parameters.of("foo", "bar");
+        var parameters2 = Parameters.of("foo", "baz");
+        publisher.publish(Type.Info, MESSAGE_3, parameters1);
+        publisher.loopComplete();
+        reset(mockPublisher);
+
+        // When
+        publisher.publish(Type.Info, MESSAGE_3, parameters2);
+
+        // Then
+        verify(mockPublisher).publish(Type.Info, MESSAGE_3, parameters2);
+    }
+
+    @Test
+    void whenLoopHasEventWithSameMessageButDifferentLevels_thenShouldLog() {
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.loopComplete();
+        publisher.publish(Type.Info, MESSAGE_1);
+        publisher.loopComplete();
+        publisher.publish(Type.Warn, MESSAGE_1);
+
+        var inOrder = inOrder(mockPublisher);
+        inOrder.verify(mockPublisher).publish(Type.Begin, MESSAGE_1);
+        inOrder.verify(mockPublisher).publish(Type.Info, MESSAGE_1);
+        inOrder.verify(mockPublisher).publish(Type.Warn, MESSAGE_1);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    void whenMoreRepeatsThanTheMaximumInterval_thenShouldLog() {
+        // Given
+        publisher.publish(Type.Begin, MESSAGE_1);
+        publisher.publish(Type.Info, MESSAGE_2);
+        publisher.publish(Type.Finish, MESSAGE_3);
+        publisher.loopComplete();
+        reset(mockPublisher);
+
+        // When
+        IntStream.range(0, MAXIMUM_LOG_INTERVAL + 1).forEach(counter -> {
+            publisher.publish(Type.Begin, MESSAGE_1);
+            publisher.publish(Type.Info, MESSAGE_2);
+            publisher.publish(Type.Finish, MESSAGE_3);
+            publisher.loopComplete();
+        });
+
+        // Then
+        verify(mockPublisher).publish(eq(Type.Info), contains(MAXIMUM_LOG_INTERVAL + " repetitions"));
+        verify(mockPublisher).publish(Type.Begin, MESSAGE_1);
+        verify(mockPublisher).publish(Type.Info, MESSAGE_2);
+        verify(mockPublisher).publish(Type.Finish, MESSAGE_3);
+        verifyNoMoreInteractions(mockPublisher);
+    }
+}
