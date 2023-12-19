b6237ca4e31706b1efbd0f874d265117b84c12d5
Anton Klaren <anton.klaren@neotechnology.com>
2023-12-19 13:53:22 +0100

    Update licenses

--- enterprise/ha/src/test/java/jmx/HaBeanIT.java
+++ enterprise/ha/src/test/java/jmx/HaBeanIT.java
@@ -4,21 +4,13 @@
  *
  * This file is part of Neo4j Enterprise Edition. The included source
  * code can be redistributed and/or modified under the terms of the
- * GNU AFFERO GENERAL PUBLIC LICENSE Version 3
- * (http://www.fsf.org/licensing/licenses/agpl-3.0.html) with the
- * Commons Clause, as found in the associated LICENSE.txt file.
+ * Neo4j Sweden Software License, as found in the associated LICENSE.txt
+ * file.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * Neo4j object code can be licensed independently from the source
- * under separate terms from the AGPL. Inquiries can be directed to:
- * licensing@neo4j.com
- *
- * More information is also available at:
- * https://neo4j.com/licensing/
+ * Neo4j Sweden Software License for more details.
  */
 package jmx;
 
