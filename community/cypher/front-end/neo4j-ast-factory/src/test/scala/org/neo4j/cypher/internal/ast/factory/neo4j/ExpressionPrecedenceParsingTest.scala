453577af5dc16929425c3a1645b2debdd6251ba0
Love Leifland <love.leifland@neotechnology.com>
2024-01-28 21:46:30 +0100

    Fix merge conflicts

--- community/cypher/front-end/neo4j-ast-factory/src/test/scala/org/neo4j/cypher/internal/ast/factory/neo4j/ExpressionPrecedenceParsingTest.scala
+++ community/cypher/front-end/neo4j-ast-factory/src/test/scala/org/neo4j/cypher/internal/ast/factory/neo4j/ExpressionPrecedenceParsingTest.scala
@@ -53,12 +53,15 @@ class ExpressionPrecedenceParsingTest extends AstParsingTestBase with LegacyAstP
 
   test("precedence 12 vs 11") {
     // (1 XOR 2) OR (3 XOR 4)
-    parsing("1 XOR 2 OR 3 XOR 4") shouldGive or(xor(literalInt(1), literalInt(2)), xor(literalInt(3), literalInt(4)))
+    parsing[Expression]("1 XOR 2 OR 3 XOR 4") shouldGive or(
+      xor(literalInt(1), literalInt(2)),
+      xor(literalInt(3), literalInt(4))
+    )
   }
 
   test("precedence 11 vs 10") {
     // true XOR (true AND false) XOR false
-    parsing("true XOR true AND false XOR false") shouldGive xor(
+    parsing[Expression]("true XOR true AND false XOR false") shouldGive xor(
       xor(trueLiteral, and(trueLiteral, falseLiteral)),
       falseLiteral
     )
@@ -66,12 +69,15 @@ class ExpressionPrecedenceParsingTest extends AstParsingTestBase with LegacyAstP
 
   test("precedence 10 vs 9") {
     // (NOT 1) AND (NOT 3) AND 5
-    parsing("NOT 1 AND NOT 3 AND 5") shouldGive and(and(not(literalInt(1)), not(literalInt(3))), literalInt(5))
+    parsing[Expression]("NOT 1 AND NOT 3 AND 5") shouldGive and(
+      and(not(literalInt(1)), not(literalInt(3))),
+      literalInt(5)
+    )
   }
 
   test("precedence 9 vs 8") {
     // NOT (1 < 2 = 3 <= (NOT 4) <> 5 >= 6 > 7)
-    parsing("NOT 1 < 2 = 3 <= (NOT 4) <> 5 >= 6 > 7") shouldGive
+    parsing[Expression]("NOT 1 < 2 = 3 <= (NOT 4) <> 5 >= 6 > 7") shouldGive
       not(ands(
         lessThan(literalInt(1), literalInt(2)),
         eq(literalInt(2), literalInt(3)),
@@ -83,14 +89,14 @@ class ExpressionPrecedenceParsingTest extends AstParsingTestBase with LegacyAstP
   }
 
   test("precedence 9 vs 8 - negative") {
-    failsToParse("1 = NOT 2")
+    failsToParse[Expression]("1 = NOT 2")
   }
 
   test("precedence 8 vs 7") {
     // ('string' STARTS WITH 's') = ('string' =~ 's?') > ('string' ENDS WITH 's') < ('string' IS NULL)
     // >= ('string' CONTAINS 's') <> ('string' IS NOT NULL) <= ('string' IN list) = (y IS TYPED BOOLEAN)
     // = (1 IS NOT TYPED BOOLEAN) = ('string' IS NORMALIZED) = ('string' IS NOT NORMALIZED)
-    parsing(
+    parsing[Expression](
       "'string' STARTS WITH 's' = 'string' =~ 's?' > 'string' ENDS WITH 's' < 'string' IS NULL >= 'string' " +
         "CONTAINS 's' <> 'string' IS NOT NULL <= 'string' IN list = y IS TYPED BOOLEAN = 1 IS NOT TYPED BOOLEAN" +
         " = 'string' IS NORMALIZED = 'string' IS NOT NORMALIZED"
@@ -140,76 +146,76 @@ class ExpressionPrecedenceParsingTest extends AstParsingTestBase with LegacyAstP
   }
 
   test("precedence 7 - negative") {
-    failsToParse("'parse' ENDS WITH 'se' CONTAINS 'e'")
-    failsToParse("'ab' STARTS WITH 'a' IS NOT TYPED BOOLEAN")
-    failsToParse("RETURN [1] IS :: LIST<INT> IS :: BOOLEAN")
-    failsToParse("RETURN 'string' IS :: STRING IS NORMALIZED")
+    failsToParse[Expression]("'parse' ENDS WITH 'se' CONTAINS 'e'")
+    failsToParse[Expression]("'ab' STARTS WITH 'a' IS NOT TYPED BOOLEAN")
+    failsToParse[Expression]("RETURN [1] IS :: LIST<INT> IS :: BOOLEAN")
+    failsToParse[Expression]("RETURN 'string' IS :: STRING IS NORMALIZED")
   }
 
   test("precedence 7 vs 6") {
     // ('string' + 'thing') STARTS WITH ('s' + 't')
-    parsing("'string' + 'thing' STARTS WITH 's' + 't'") shouldGive
+    parsing[Expression]("'string' + 'thing' STARTS WITH 's' + 't'") shouldGive
       startsWith(
         add(literalString("string"), literalString("thing")),
         add(literalString("s"), literalString("t"))
       )
 
     // ('string' + 'thing') CONTAINS ('ring' - 'ing')
-    parsing("'string' + 'thing' CONTAINS 'ring' - 'ing'") shouldGive
+    parsing[Expression]("'string' + 'thing' CONTAINS 'ring' - 'ing'") shouldGive
       contains(
         add(literalString("string"), literalString("thing")),
         subtract(literalString("ring"), literalString("ing"))
       )
 
     // ('string' - 'ing') ENDS WITH ('s' + 't')
-    parsing("'string' - 'ing' ENDS WITH 's' + 't'") shouldGive
+    parsing[Expression]("'string' - 'ing' ENDS WITH 's' + 't'") shouldGive
       endsWith(
         subtract(literalString("string"), literalString("ing")),
         add(literalString("s"), literalString("t"))
       )
 
     // ('string' - 'ing') =~ ('s?' - 's')
-    parsing("'string' - 'ing' =~ 's?' - 's'") shouldGive
+    parsing[Expression]("'string' - 'ing' =~ 's?' - 's'") shouldGive
       regex(
         subtract(literalString("string"), literalString("ing")),
         subtract(literalString("s?"), literalString("s"))
       )
 
     // ('string' - 'ing') IS NORMALIZED
-    parsing("'string' - 'ing' IS NORMALIZED") shouldGive
+    parsing[Expression]("'string' - 'ing' IS NORMALIZED") shouldGive
       isNormalized(
         subtract(literalString("string"), literalString("ing")),
         NFCNormalForm
       )
 
     // (2 + 3) IN [(2 - 1)]
-    parsing("2 + 3 IN [2 - 1]") shouldGive
+    parsing[Expression]("2 + 3 IN [2 - 1]") shouldGive
       in(add(literalInt(2), literalInt(3)), listOf(subtract(literalInt(2), literalInt(1))))
 
     // (1 + 2) IS NOT NULL
-    parsing("1 + 2 IS NOT NULL") shouldGive isNotNull(add(literalInt(1), literalInt(2)))
+    parsing[Expression]("1 + 2 IS NOT NULL") shouldGive isNotNull(add(literalInt(1), literalInt(2)))
 
     // (1 - 2) IS NULL
-    parsing("1 - 2 IS NULL") shouldGive isNull(subtract(literalInt(1), literalInt(2)))
+    parsing[Expression]("1 - 2 IS NULL") shouldGive isNull(subtract(literalInt(1), literalInt(2)))
 
     //  ([true] + n.p) :: STRING
-    parsing(" [true] + n.p :: STRING") shouldGive
+    parsing[Expression](" [true] + n.p :: STRING") shouldGive
       isTyped(add(listOf(trueLiteral), prop("n", "p")), StringType(isNullable = true)(pos))
 
     // (3 - 4) IS NOT TYPED BOOLEAN
-    parsing("3 - 4 IS NOT :: BOOLEAN") shouldGive
+    parsing[Expression]("3 - 4 IS NOT :: BOOLEAN") shouldGive
       isNotTyped(subtract(literalInt(3), literalInt(4)), BooleanType(isNullable = true)(pos))
   }
 
   test("precedence 6 - left-associativity") {
     // ((1 + 2) - 3) + 4
-    parsing("1 + 2 - 3 + 4") shouldGive
+    parsing[Expression]("1 + 2 - 3 + 4") shouldGive
       add(subtract(add(literalInt(1), literalInt(2)), literalInt(3)), literalInt(4))
   }
 
   test("precedence 6 vs 5") {
     // 1 + (2 / 3 * 4) - (5 % 6)
-    parsing("1 + 2 / 3 * 4 - 5 % 6") shouldGive
+    parsing[Expression]("1 + 2 / 3 * 4 - 5 % 6") shouldGive
       subtract(
         add(literalInt(1), multiply(divide(literalInt(2), literalInt(3)), literalInt(4))),
         modulo(literalInt(5), literalInt(6))
@@ -218,15 +224,15 @@ class ExpressionPrecedenceParsingTest extends AstParsingTestBase with LegacyAstP
 
   test("precedence 5 - left-associativity") {
     // (2 / 3) * 4
-    parsing("2 / 3 * 4") shouldGive multiply(divide(literalInt(2), literalInt(3)), literalInt(4))
+    parsing[Expression]("2 / 3 * 4") shouldGive multiply(divide(literalInt(2), literalInt(3)), literalInt(4))
 
     // (5 % 4) % 2
-    parsing("5 % 4 % 2") shouldGive modulo(modulo(literalInt(5), literalInt(4)), literalInt(2))
+    parsing[Expression]("5 % 4 % 2") shouldGive modulo(modulo(literalInt(5), literalInt(4)), literalInt(2))
   }
 
   test("precedence 5 vs 4") {
     // 1 * (2^3) / 4
-    parsing("1 * 2 ^ 3 / 4") shouldGive divide(
+    parsing[Expression]("1 * 2 ^ 3 / 4") shouldGive divide(
       multiply(literalInt(1), pow(literalInt(2), literalInt(3))),
       literalInt(4)
     )
@@ -234,24 +240,24 @@ class ExpressionPrecedenceParsingTest extends AstParsingTestBase with LegacyAstP
 
   test("precedence 4 - left-associativity") {
     // (4 ^3) ^ 2
-    parsing("4 ^ 3 ^ 2") shouldGive pow(pow(literalInt(4), literalInt(3)), literalInt(2))
+    parsing[Expression]("4 ^ 3 ^ 2") shouldGive pow(pow(literalInt(4), literalInt(3)), literalInt(2))
   }
 
   test("precedence 4 vs 3") {
     // (+1) ^ (-2)
-    parsing("+1^-2") shouldGive pow(unaryAdd(literalInt(1)), literalInt(-2))
+    parsing[Expression]("+1^-2") shouldGive pow(unaryAdd(literalInt(1)), literalInt(-2))
   }
 
   test("precedence 3 vs 2") {
     // -(list[+(expr:Label)])
-    parsing("-list[+expr:Label]") shouldGive
+    parsing[Expression]("-list[+expr:Label]") shouldGive
       unarySubtract(containerIndex(
         varFor("list"),
         unaryAdd(labelExpressionPredicate(varFor("expr"), labelOrRelTypeLeaf("Label")))
       ))
 
     // +(list[-(x.y)..+(5)])
-    parsing("+list[-x.y..+5]") shouldGive
+    parsing[Expression]("+list[-x.y..+5]") shouldGive
       unaryAdd(ListSlice(
         varFor("list"),
         Some(unarySubtract(prop(varFor("x"), "y"))),
@@ -262,22 +268,24 @@ class ExpressionPrecedenceParsingTest extends AstParsingTestBase with LegacyAstP
   test("precedence 2 vs 1") {
 
     // ($list)[(single(x IN y WHERE ('a' + 'b').prop))]
-    parsing("$list[single(x IN y WHERE ('a' + 'b').prop)]") shouldGive
+    parsing[Expression]("$list[single(x IN y WHERE ('a' + 'b').prop)]") shouldGive
       containerIndex(
         parameter("list", CTAny),
         singleInList(varFor("x"), varFor("y"), prop(add(literalString("a"), literalString("b")), "prop"))
       )
 
     // (all(x IN y WHERE null)):Label
-    parsing("all(x IN y WHERE null):Label") shouldGive
+    parsing[Expression]("all(x IN y WHERE null):Label") shouldGive
       labelExpressionPredicate(allInList(varFor("x"), varFor("y"), nullLiteral), labelOrRelTypeLeaf("Label"))
 
     // (none(x IN y WHERE true)).prop
-    parsing("none(x IN y WHERE false).prop") shouldGive
+    parsing[Expression]("none(x IN y WHERE false).prop") shouldGive
       prop(noneInList(varFor("x"), varFor("y"), falseLiteral), "prop")
 
     // (COLLECT {RETURN 42})[(any(x IN y WHERE (size('str')).prop))..(reduce(x=true, y IN list | x AND y))]
-    parsing("COLLECT {RETURN 42}[any(x IN y WHERE size('str').prop)..reduce(x=true, y IN list | x AND y)]") shouldGive
+    parsing[Expression](
+      "COLLECT {RETURN 42}[any(x IN y WHERE size('str').prop)..reduce(x=true, y IN list | x AND y)]"
+    ) shouldGive
       ListSlice(
         CollectExpression(
           singleQuery(
@@ -289,7 +297,7 @@ class ExpressionPrecedenceParsingTest extends AstParsingTestBase with LegacyAstP
       )(pos)
 
     // [(x IN (EXISTS {RETURN 42}) WHERE (x{.*}) = (COUNT {RETURN 42}))][([(n)-->() | n])]
-    parsing("[x IN EXISTS {RETURN 42} WHERE x{.*} = COUNT {RETURN 42}][[(n)-->() | n]]") shouldGive
+    parsing[Expression]("[x IN EXISTS {RETURN 42} WHERE x{.*} = COUNT {RETURN 42}][[(n)-->() | n]]") shouldGive
       containerIndex(
         listComprehension(
           varFor("x"),
@@ -314,7 +322,7 @@ class ExpressionPrecedenceParsingTest extends AstParsingTestBase with LegacyAstP
       )
 
     // (shortestPath((a)-->(b)))[(CASE x WHEN true THEN 1 ELSE 2 END)]
-    parsing("shortestPath((a)-->(b))[CASE x WHEN true THEN 1 ELSE 2 END]") shouldGive
+    parsing[Expression]("shortestPath((a)-->(b))[CASE x WHEN true THEN 1 ELSE 2 END]") shouldGive
       containerIndex(
         ShortestPathExpression(ShortestPathsPatternPart(
           relationshipChain(nodePat(Some("a")), relPat(), nodePat(Some("b"))),
--- community/cypher/front-end/neo4j-ast-factory/src/test/scala/org/neo4j/cypher/internal/ast/factory/neo4j/NormalizedPredicateParserTest.scala
+++ community/cypher/front-end/neo4j-ast-factory/src/test/scala/org/neo4j/cypher/internal/ast/factory/neo4j/NormalizedPredicateParserTest.scala
@@ -16,38 +16,31 @@
  */
 package org.neo4j.cypher.internal.ast.factory.neo4j
 
-import org.neo4j.cypher.internal.ast.AstConstructionTestSupport
-import org.neo4j.cypher.internal.cst.factory.neo4j.AntlrRule
-import org.neo4j.cypher.internal.cst.factory.neo4j.Cst
+import org.neo4j.cypher.internal.ast.factory.neo4j.test.util.AstParsingTestBase
+import org.neo4j.cypher.internal.ast.factory.neo4j.test.util.LegacyAstParsingTestSupport
 import org.neo4j.cypher.internal.expressions.Expression
 import org.neo4j.cypher.internal.expressions.NFCNormalForm
 import org.neo4j.cypher.internal.expressions.NFDNormalForm
 import org.neo4j.cypher.internal.expressions.NFKCNormalForm
 import org.neo4j.cypher.internal.expressions.NFKDNormalForm
-import org.neo4j.cypher.internal.parser.CypherParser.ExpressionContext
-import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
 
-class NormalizedPredicateParserTest extends CypherFunSuite with ParserSyntaxTreeBase[Cst.Expression, Expression]
-    with AstConstructionTestSupport {
-
-  implicit val javaccRule: JavaccRule[Expression] = JavaccRule.Expression
-  implicit val antlrRule: AntlrRule[ExpressionContext] = AntlrRule.Expression
+class NormalizedPredicateParserTest extends AstParsingTestBase with LegacyAstParsingTestSupport {
 
   test("'string' IS NORMALIZED") {
-    gives {
+    gives[Expression] {
       isNormalized(literalString("string"), NFCNormalForm)
     }
   }
 
   test("'string' IS NOT NORMALIZED") {
-    gives {
+    gives[Expression] {
       isNotNormalized(literalString("string"), NFCNormalForm)
     }
   }
 
   Seq(NFCNormalForm, NFDNormalForm, NFKCNormalForm, NFKDNormalForm).foreach { normalForm =>
     test(s"'string' IS ${normalForm.description} NORMALIZED") {
-      gives {
+      gives[Expression] {
         isNormalized(literalString("string"), normalForm)
       }
     }
@@ -55,14 +48,14 @@ class NormalizedPredicateParserTest extends CypherFunSuite with ParserSyntaxTree
 
   Seq(NFCNormalForm, NFDNormalForm, NFKCNormalForm, NFKDNormalForm).foreach { normalForm =>
     test(s"'string' IS NOT ${normalForm.description} NORMALIZED") {
-      gives {
+      gives[Expression] {
         isNotNormalized(literalString("string"), normalForm)
       }
     }
   }
 
   test("'hello ' + 'world'  IS NORMALIZED") {
-    gives {
+    gives[Expression] {
       isNormalized(add(literalString("hello "), literalString("world")), NFCNormalForm)
     }
   }
