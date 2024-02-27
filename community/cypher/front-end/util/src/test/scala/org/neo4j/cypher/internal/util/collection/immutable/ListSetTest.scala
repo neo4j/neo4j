e2777128908944ec791ca878630866379ec8d878
Satia Herfert <satiaherfert@gmx.de>
2024-02-27 15:34:37 +0100

    Add more tests for ListSet

--- community/cypher/front-end/util/src/test/scala/org/neo4j/cypher/internal/util/collection/immutable/ListSetTest.scala
+++ community/cypher/front-end/util/src/test/scala/org/neo4j/cypher/internal/util/collection/immutable/ListSetTest.scala
@@ -38,4 +38,128 @@ class ListSetTest extends CypherFunSuite {
     val ls = scala.collection.immutable.ListSet.from(seq)
     ListSet.from(ls).toSeq should equal(seq)
   }
+
+  test("Can build an empty ListSet") {
+    val seq = Seq()
+    ListSet.from(seq) should be(empty)
+    ListSet() should be(empty)
+    ListSet().iterator should be(empty)
+  }
+
+  // Tests below are adapted from https://github.com/scala/scala/blob/2.13.x/test/junit/scala/collection/immutable/ListSetTest.scala
+
+  test("t7445") {
+    val s = ListSet(1, 2, 3, 4, 5)
+    s.tail should equal(ListSet(2, 3, 4, 5))
+  }
+
+  test("hasCorrectBuilder") {
+    val m = ListSet("a", "b", "c", "b", "d")
+    m.toList should equal(List("a", "b", "c", "d"))
+  }
+
+  test("delete without stackoverflow") {
+    val s = ListSet(1 to 50000: _*)
+    try s - 25000
+    catch {
+      case _: StackOverflowError => fail("A stack overflow occurred")
+    }
+  }
+
+  test("hasCorrectHeadTailLastInit") {
+    val m = ListSet(1, 2, 3)
+    m.head should equal(1)
+    m.tail should equal(ListSet(2, 3))
+    m.last should equal(3)
+    m.init should equal(ListSet(1, 2))
+  }
+
+  test("hasCorrectAddRemove") {
+    val m = ListSet(1, 2, 3)
+    m + 4 should equal(ListSet(1, 2, 3, 4))
+    m + 2 should equal(ListSet(1, 2, 3))
+    m - 1 should equal(ListSet(2, 3))
+    m - 2 should equal(ListSet(1, 3))
+    m - 4 should equal(ListSet(1, 2, 3))
+  }
+
+  test("hasCorrectIterator") {
+    val s = ListSet(1, 2, 3, 5, 4)
+    s.iterator.toList should equal(List(1, 2, 3, 5, 4))
+  }
+
+  test("hasCorrectOrderAfterPlusPlus") {
+    val foo = ListSet(1)
+    var bar = foo ++ ListSet()
+    bar.iterator.toList should equal(List(1))
+
+    bar = foo ++ ListSet(1)
+    bar.iterator.toList should equal(List(1))
+
+    bar = foo ++ ListSet(2)
+    bar.iterator.toList should equal(List(1, 2))
+
+    bar = foo ++ ListSet(1, 2)
+    bar.iterator.toList should equal(List(1, 2))
+
+    bar = foo ++ ListSet(1, 2, 3)
+    bar.iterator.toList should equal(List(1, 2, 3))
+
+    bar = foo ++ ListSet(1, 2, 3, 4)
+    bar.iterator.toList should equal(List(1, 2, 3, 4))
+
+    bar = foo ++ ListSet(1, 2, 3, 4, 5)
+    bar.iterator.toList should equal(List(1, 2, 3, 4, 5))
+
+    bar = foo ++ ListSet(1, 2, 3, 4, 5, 6)
+    bar.iterator.toList should equal(List(1, 2, 3, 4, 5, 6))
+
+    foo ++ foo should equal(foo)
+    foo ++ ListSet.empty should equal(foo)
+    foo ++ Nil should equal(foo)
+  }
+
+  test("t12316 ++ is correctly ordered") {
+    // was: ListSet(1, 2, 3, 42, 43, 44, 29, 28, 27, 12, 11, 10)
+    (ListSet(1, 2, 3, 42, 43, 44) ++ ListSet(10, 11, 12, 42, 43, 44, 27, 28, 29)).iterator.toList should equal(List(1,
+      2, 3, 42, 43, 44, 10, 11, 12, 27, 28, 29))
+  }
+
+  test("smallPlusPlus1") {
+    def check(l1: ListSet[Int], l2: ListSet[Int]) = {
+      val expected = l1.iterator.toList ++ l2.iterator.filterNot(l1).toList
+      val actual = (l1 ++ l2).iterator.toList
+      actual should equal(expected)
+    }
+
+    for (
+      start0 <- 0 until 6;
+      end0 <- start0 until 6;
+      start1 <- 0 until 6;
+      end1 <- start1 until 6
+    ) {
+      val ls0 = ListSet((start0 until end0): _*)
+      val ls1 = ListSet((start1 until end1): _*)
+      check(ls0, ls1)
+    }
+  }
+
+  test("smallPlusPlusAfter") {
+    def check(l1: ListSet[Int], l2: ListSet[Int]) = {
+      val expected = l1.iterator.toList ++ l2.iterator.filterNot(l1).toList
+      val actual = (l1 ++ l2).iterator.toList
+      actual should equal(expected)
+    }
+
+    for (
+      start0 <- 0 until 9;
+      end0 <- start0 until 9;
+      start1 <- 10 until 19;
+      end1 <- start1 until 19
+    ) {
+      val ls0 = ListSet((start0 until end0): _*)
+      val ls1 = ListSet((start1 until end1): _*)
+      check(ls0, ls1)
+    }
+  }
 }
