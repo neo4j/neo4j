2e6ca123c1da375712be3ef17c114a17d7a6cd70
gjmwoods <42248895+gjmwoods@users.noreply.github.com>
2024-05-01 14:58:38 +0100

    Update the static pages Content-Security-Policy ()

--- community/server/src/main/java/org/neo4j/server/configuration/ServerSettings.java
+++ community/server/src/main/java/org/neo4j/server/configuration/ServerSettings.java
@@ -134,10 +134,10 @@ public class ServerSettings implements SettingsDeclaration {
     public static final Setting<String> http_static_content_security_policy = newBuilder(
                     "dbms.security.http_static_content_security_policy_header",
                     STRING,
-                    "default-src 'self'; script-src 'self' cdn.segment.com canny.io; img-src 'self';"
-                            + " style-src 'self' fonts.googleapis.com 'unsafe-inline'; font-src 'self' "
-                            + "fonts.gstatic.com; base-uri 'none'; object-src 'none'; frame-ancestors 'none';"
-                            + " connect-src 'self' api.canny.io api.segment.io wss")
+                    "default-src 'self'; script-src 'self' cdn.segment.com canny.io; img-src 'self' data:; "
+                            + "style-src 'self' fonts.googleapis.com 'unsafe-inline'; font-src 'self' "
+                            + "fonts.gstatic.com; base-uri 'none'; object-src 'none'; frame-ancestors 'none'; "
+                            + "connect-src 'self' api.canny.io api.segment.io ws: wss: http: https:")
             .build();
 
     @Description("Defines an allowlist of http paths where Neo4j authentication is not required.")
