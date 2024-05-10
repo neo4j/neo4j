bd327b23f0efa6b0c1fa7a4a8efef11ca242b5d1
Bruno Buss <bruno.buss@neo4j.com>
2024-05-10 17:26:34 +0100

    Add InternalKernelRuntimeException
    This is an exception type to represent internal kernel runtime
    exception. The goal is to have one type that can be handled separated
    from other RuntimeExceptions or together with KernelException, but that
    are not checked and does not require a status nor user message like
    KernelException.

---
+++ community/kernel-api/src/main/java/org/neo4j/internal/kernel/api/exceptions/InternalKernelRuntimeException.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.neo4j.internal.kernel.api.exceptions;
+
+/**
+ * An exception type used for internal runtime exceptions throw by Kernel and that
+ * have to be handled separated from other runtime exceptions.
+ */
+public final class InternalKernelRuntimeException extends RuntimeException {
+    public InternalKernelRuntimeException(Throwable cause, String message) {
+        super(message, cause);
+    }
+}
