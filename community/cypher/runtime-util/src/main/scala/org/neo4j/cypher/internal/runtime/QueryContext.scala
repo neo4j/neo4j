93834031912c78b2bc613175ab290a2274507f2c
Pontus Melke <pontusmelke@gmail.com>
2024-06-17 11:38:36 +0200

    Revert "Optimize call in transaction entity transformer"
    This reverts commit 771fada97b5e14ae14a91bdcc5e328ec0e8cab9b.

--- community/cypher/runtime-util/src/main/scala/org/neo4j/cypher/internal/runtime/QueryContext.scala
+++ community/cypher/runtime-util/src/main/scala/org/neo4j/cypher/internal/runtime/QueryContext.scala
@@ -938,12 +938,10 @@ class RelationshipValueHit(val inner: RelationshipValueIndexCursor, val values: 
 }
 
 trait EntityTransformer {
-  def needsRebinding(value: AnyValue): Boolean
   def rebindEntityWrappingValue(value: AnyValue): AnyValue
 }
 
 class NoopEntityTransformer extends EntityTransformer {
-  override def needsRebinding(value: AnyValue): Boolean = false
   override def rebindEntityWrappingValue(value: AnyValue): AnyValue = value
 }
 
