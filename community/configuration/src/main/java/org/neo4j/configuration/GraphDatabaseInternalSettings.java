126eb8c0606c51f17bd0eff9c8478093c6e5aaea
Sören Reichardt <soren.reichardt@neo4j.com>
2024-08-19 11:45:47 +0200

    Rename procedure
    Co-authored-by: Mats Rydberg <mats@neotechnology.com>

--- community/configuration/src/main/java/org/neo4j/configuration/GraphDatabaseInternalSettings.java
+++ community/configuration/src/main/java/org/neo4j/configuration/GraphDatabaseInternalSettings.java
@@ -1381,7 +1381,7 @@ public class GraphDatabaseInternalSettings implements SettingsDeclaration {
                             "gds.arrow.project.*",
                             "gds.arrow.write",
                             "gds.arrow.write.*",
-                            "gds.protocol.versions",
+                            "gds.session.dbms.protocol.version",
                             "internal.*",
                             "localdatetime.*",
                             "localtime.*",
