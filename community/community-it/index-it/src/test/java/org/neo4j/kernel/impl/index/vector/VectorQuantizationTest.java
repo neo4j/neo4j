deff9057464b2946c9ba51f9480ac6298c620fd2
Matthew Parnell <matthew.parnell@neo4j.com>
2026-05-11 14:32:52 +0100

    Skip vector quatization type test on SPD ()
    Currently `double`s are not supported in the block serialization
    of schema commands, which SPD relies upon.
    
    A new serialization requires a new kernel version, currently there
    is<br>no need for a kernel version specifically for this unreleased
    feature.
    
    We will need a new kernel version when releasing the feature;
    however, there are other issues surrounding that need to be solved
    beforehand.
    
    If an new kernel version is created for other reasons, we can
    biggy-back on that as well.

--- community/community-it/index-it/src/test/java/org/neo4j/kernel/impl/index/vector/VectorQuantizationTest.java
+++ community/community-it/index-it/src/test/java/org/neo4j/kernel/impl/index/vector/VectorQuantizationTest.java
@@ -35,7 +35,10 @@ import org.neo4j.kernel.KernelVersion;
 import org.neo4j.kernel.api.impl.schema.vector.VectorQuantizationType;
 import org.neo4j.test.TestDatabaseManagementServiceBuilder;
 import org.neo4j.test.extension.ExtensionCallback;
+import org.neo4j.test.extension.SkipOnSpd;
+import org.neo4j.test.extension.SkipOnSpd.Note;
 
+@SkipOnSpd(reason = "doubles are currently not supported via the block serialization SPD uses", notes = Note.temporary)
 public class VectorQuantizationTest extends VectorSSFTestBase {
 
     public static final int DIMENSION = 8;
