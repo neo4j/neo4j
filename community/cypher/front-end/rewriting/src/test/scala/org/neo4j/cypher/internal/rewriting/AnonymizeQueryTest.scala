7b07ccd574fb8a5883e9defe572e4e6dd407acac
Therese Magnusson <therese.magnusson@neo4j.com>
2026-06-04 10:06:06 +0200

    Add administration command anonymize query and literal obfuscation tests ()
    Also includes some additional tests for composable show and terminate,
    index, constraint, and graph type commands.

--- community/cypher/front-end/rewriting/src/test/scala/org/neo4j/cypher/internal/rewriting/AnonymizeQueryTest.scala
+++ community/cypher/front-end/rewriting/src/test/scala/org/neo4j/cypher/internal/rewriting/AnonymizeQueryTest.scala
@@ -17,7 +17,19 @@
 package org.neo4j.cypher.internal.rewriting
 
 import org.neo4j.cypher.internal.CypherVersion
+import org.neo4j.cypher.internal.ast.CreateUser
+import org.neo4j.cypher.internal.ast.ShowAliases
+import org.neo4j.cypher.internal.ast.ShowAuthRules
+import org.neo4j.cypher.internal.ast.ShowColumn
 import org.neo4j.cypher.internal.ast.ShowConstraintsClause
+import org.neo4j.cypher.internal.ast.ShowCurrentGraphTypeClause
+import org.neo4j.cypher.internal.ast.ShowCurrentUser
+import org.neo4j.cypher.internal.ast.ShowPrivilegeCommands
+import org.neo4j.cypher.internal.ast.ShowPrivileges
+import org.neo4j.cypher.internal.ast.ShowRoles
+import org.neo4j.cypher.internal.ast.ShowServers
+import org.neo4j.cypher.internal.ast.ShowSupportedPrivilegeCommand
+import org.neo4j.cypher.internal.ast.ShowUsers
 import org.neo4j.cypher.internal.expressions.Expression
 import org.neo4j.cypher.internal.rewriting.rewriters.Anonymizer
 import org.neo4j.cypher.internal.rewriting.rewriters.anonymizeQuery
@@ -308,7 +320,7 @@ class AnonymizeQueryTest extends AnonymizerTestBase {
       "SHOW INDEXES WHERE Xname = 'string[my_index]'"
     )
     assertRewrite(
-      // Default columns differ between Cypher versions so only run one version
+      // Default columns differ between Cypher versions so run them separately
       CypherVersion.Cypher25,
       "SHOW CONSTRAINTS YIELD name ORDER BY name WHERE name = $param",
       "SHOW CONSTRAINTS YIELD name AS Xname ORDER BY Xname WHERE Xname = $Xparam",
@@ -319,6 +331,42 @@ class AnonymizeQueryTest extends AnonymizerTestBase {
             val updatedWithClause = s.yieldWith.map(w => {
               val updatedReturnItems =
                 w.returnItems.copy(defaultOrderOnColumns = Some(List("name")))(w.returnItems.position)
+              w.copy(returnItems = updatedReturnItems)(w.position)
+            })
+            s.copy(yieldWith = updatedWithClause)(s.position)
+        }))
+      }
+    )
+    assertRewrite(
+      // Default columns differ between Cypher versions so run them separately
+      CypherVersion.Cypher5,
+      "SHOW CONSTRAINTS YIELD name ORDER BY name WHERE name = $param",
+      "SHOW CONSTRAINTS YIELD name AS Xname ORDER BY Xname WHERE Xname = $Xparam",
+      additionalExpectedAstUpdates = expectedStatement => {
+        expectedStatement.endoRewrite(bottomUp(Rewriter.lift {
+          case s: ShowConstraintsClause =>
+            // Update WITH clause to 'keep' correct default order
+            val updatedWithClause = s.yieldWith.map(w => {
+              val updatedReturnItems =
+                w.returnItems.copy(defaultOrderOnColumns = Some(List("name")))(w.returnItems.position)
+              w.copy(returnItems = updatedReturnItems)(w.position)
+            })
+            s.copy(yieldWith = updatedWithClause)(s.position)
+        }))
+      }
+    )
+    assertRewrite(
+      // Command is only available in Cypher 25
+      CypherVersion.Cypher25,
+      "SHOW CURRENT GRAPH TYPE AS GRAPH YIELD nodes WHERE nodes <> []",
+      "SHOW CURRENT GRAPH TYPE AS GRAPH YIELD nodes AS Xnodes WHERE Xnodes <> []",
+      additionalExpectedAstUpdates = expectedStatement => {
+        expectedStatement.endoRewrite(bottomUp(Rewriter.lift {
+          case s: ShowCurrentGraphTypeClause =>
+            // Update WITH clause to 'keep' correct default order
+            val updatedWithClause = s.yieldWith.map(w => {
+              val updatedReturnItems =
+                w.returnItems.copy(defaultOrderOnColumns = Some(List("nodes")))(w.returnItems.position)
               w.copy(returnItems = updatedReturnItems)(w.position)
             })
             s.copy(yieldWith = updatedWithClause)(s.position)
@@ -357,5 +405,315 @@ class AnonymizeQueryTest extends AnonymizerTestBase {
       "TERMINATE TRANSACTIONS 'txId', 'txId2'",
       "TERMINATE TRANSACTIONS 'string[txId]', 'string[txId2]'"
     )
+    assertRewrite(
+      // Default columns differ between Cypher versions so run them separately
+      CypherVersion.Cypher25,
+      "SHOW DATABASE foo",
+      "SHOW DATABASE foo"
+    )
+    assertRewrite(
+      // Default columns differ between Cypher versions so run them separately
+      CypherVersion.Cypher5,
+      "SHOW DATABASE foo",
+      "SHOW DATABASE foo"
+    )
+  }
+
+  test("administration user commands") {
+    assertRewrite(
+      "SHOW CURRENT USER YIELD home",
+      "SHOW CURRENT USER YIELD Xhome",
+      additionalExpectedAstUpdates = _ match {
+        case s: ShowCurrentUser => s.copy(defaultColumnSet = rewriteAdminShowDefaultColumnSet(s.defaultColumnSet))
+        case s                  => s
+      }
+    )
+    assertRewrite(
+      "SHOW USERS WHERE user IN ['user1', $userParam]",
+      "SHOW USERS WHERE Xuser IN ['string[user1]', $XuserParam]",
+      additionalExpectedAstUpdates = _ match {
+        case s: ShowUsers => s.copy(defaultColumnSet = rewriteAdminShowDefaultColumnSet(s.defaultColumnSet))
+        case s            => s
+      }
+    )
+    assertRewrite(
+      // The password is printed as '******' but needs to be the same string for the comparison
+      "CREATE USER foo SET PASSWORD 'secret-secret' SET AUTH 'provider' { SET ID 'id' }",
+      "CREATE USER `string[foo]` SET PASSWORD 'secret-secret' SET AUTH 'provider' { SET ID 'string[id]' }"
+    )
+    assertRewrite(
+      "ALTER USER foo SET HOME DATABASE neo4j",
+      "ALTER USER `string[foo]` SET HOME DATABASE neo4j"
+    )
+    assertRewrite(
+      // Command is only available in Cypher 25
+      CypherVersion.Cypher25,
+      "ALTER USERS foo, bar SET TAG 'Tag'",
+      "ALTER USERS `string[foo]`, `string[bar]` SET TAG 'string[Tag]'"
+    )
+    assertRewrite(
+      // The passwords are printed as '******' but needs to be the same string for the comparison
+      "ALTER CURRENT USER SET PASSWORD FROM 'old' TO 'new'",
+      "ALTER CURRENT USER SET PASSWORD FROM 'old' TO 'new'"
+    )
+    assertRewrite(
+      "RENAME USER foo TO bar",
+      "RENAME USER `string[foo]` TO `string[bar]`"
+    )
+    assertRewrite(
+      "DROP USER foo",
+      "DROP USER `string[foo]`"
+    )
+  }
+
+  test("administration role commands") {
+    assertRewrite(
+      "SHOW ROLES YIELD role, immutable WHERE role = 'wanted' RETURN immutable",
+      "SHOW ROLES YIELD Xrole, Ximmutable WHERE Xrole = 'string[wanted]' RETURN Ximmutable",
+      additionalExpectedAstUpdates = _ match {
+        case s: ShowRoles => s.copy(defaultColumnSet = rewriteAdminShowDefaultColumnSet(s.defaultColumnSet))
+        case s            => s
+      }
+    )
+    assertRewrite(
+      "CREATE ROLE `foo` AS COPY OF `bar`",
+      "CREATE ROLE `string[foo]` AS COPY OF `string[bar]`"
+    )
+    assertRewrite(
+      "RENAME ROLE foo TO bar",
+      "RENAME ROLE `string[foo]` TO `string[bar]`"
+    )
+    assertRewrite(
+      "DROP ROLE foo",
+      "DROP ROLE `string[foo]`"
+    )
+    assertRewrite(
+      "GRANT ROLE foo TO bar",
+      "GRANT ROLE `string[foo]` TO `string[bar]`"
+    )
+    assertRewrite(
+      // Command is only available in Cypher 25
+      CypherVersion.Cypher25,
+      "GRANT ROLE foo TO AUTH RULE bar",
+      "GRANT ROLE `string[foo]` TO AUTH RULE `string[bar]`"
+    )
+    assertRewrite(
+      "REVOKE ROLE foo FROM bar",
+      "REVOKE ROLE `string[foo]` FROM `string[bar]`"
+    )
+    assertRewrite(
+      // Command is only available in Cypher 25
+      CypherVersion.Cypher25,
+      "REVOKE ROLE foo FROM AUTH RULE bar",
+      "REVOKE ROLE `string[foo]` FROM AUTH RULE `string[bar]`"
+    )
+  }
+
+  test("administration auth rule commands") {
+    // These commands are only available in Cypher 25
+
+    assertRewrite(
+      CypherVersion.Cypher25,
+      "SHOW AUTH RULES YIELD name",
+      "SHOW AUTH RULES YIELD Xname",
+      additionalExpectedAstUpdates = _ match {
+        case s: ShowAuthRules => s.copy(defaultColumnSet = rewriteAdminShowDefaultColumnSet(s.defaultColumnSet))
+        case s                => s
+      }
+    )
+    assertRewrite(
+      CypherVersion.Cypher25,
+      "CREATE AUTH RULE $param SET CONDITION foo = my.func('value', 42)",
+      "CREATE AUTH RULE $Xparam SET CONDITION Xfoo = my.func('string[value]', 42)"
+    )
+    assertRewrite(
+      CypherVersion.Cypher25,
+      "RENAME AUTH RULE foo TO bar",
+      "RENAME AUTH RULE `string[foo]` TO `string[bar]`"
+    )
+    assertRewrite(
+      CypherVersion.Cypher25,
+      "DROP AUTH RULE foo",
+      "DROP AUTH RULE `string[foo]`"
+    )
+    assertRewrite(
+      CypherVersion.Cypher25,
+      "ALTER AUTH RULE foo SET CONDITION true",
+      "ALTER AUTH RULE `string[foo]` SET CONDITION true"
+    )
+  }
+
+  test("administration privilege commands") {
+    assertRewrite(
+      "SHOW ROLE role PRIVILEGES",
+      "SHOW ROLE `string[role]` PRIVILEGES",
+      additionalExpectedAstUpdates = _ match {
+        case s: ShowPrivileges => s.copy(defaultColumnSet = rewriteAdminShowDefaultColumnSet(s.defaultColumnSet))
+        case s                 => s
+      }
+    )
+    assertRewrite(
+      "SHOW USER user PRIVILEGES AS COMMANDS WHERE command CONTAINS 'GRANT'",
+      "SHOW USER `string[user]` PRIVILEGES AS COMMANDS WHERE Xcommand CONTAINS 'string[GRANT]'",
+      additionalExpectedAstUpdates = _ match {
+        case s: ShowPrivilegeCommands =>
+          s.copy(defaultColumnSet = rewriteAdminShowDefaultColumnSet(s.defaultColumnSet))
+        case s => s
+      }
+    )
+    assertRewrite(
+      "SHOW SUPPORTED PRIVILEGES YIELD action, description ORDER BY action SKIP 1 LIMIT 2",
+      "SHOW SUPPORTED PRIVILEGES YIELD Xaction, Xdescription ORDER BY Xaction SKIP 1 LIMIT 2",
+      additionalExpectedAstUpdates = _ match {
+        case s: ShowSupportedPrivilegeCommand =>
+          s.copy(defaultColumnSet = rewriteAdminShowDefaultColumnSet(s.defaultColumnSet))
+        case s => s
+      }
+    )
+    assertRewrite(
+      "GRANT READ {foo} ON GRAPH bar NODES Baz TO bao",
+      "GRANT READ {foo} ON GRAPH bar NODES Baz TO `string[bao]`"
+    )
+    assertRewrite(
+      "GRANT TRAVERSE ON GRAPH bar FOR (n:Baz {foo: 'available'}) TO bao",
+      "GRANT TRAVERSE ON GRAPH bar FOR (Xn:Baz {Xfoo: 'string[available]'}) TO `string[bao]`"
+    )
+    assertRewrite(
+      "DENY EXECUTE PROCEDURE db.* ON DBMS TO bao",
+      "DENY EXECUTE PROCEDURE db.* ON DBMS TO `string[bao]`"
+    )
+    assertRewrite(
+      "REVOKE TRANSACTION (user) ON DATABASE bar FROM bao",
+      "REVOKE TRANSACTION (`string[user]`) ON DATABASE bar FROM `string[bao]`"
+    )
+    assertRewrite(
+      "GRANT LOAD ON CIDR 'cidr' TO bao",
+      "GRANT LOAD ON CIDR 'cidr' TO `string[bao]`"
+    )
+  }
+
+  test("administration server commands") {
+    assertRewrite(
+      "SHOW SERVERS YIELD * WHERE serverId <> name",
+      "SHOW SERVERS YIELD * WHERE XserverId <> Xname",
+      additionalExpectedAstUpdates = _ match {
+        case s: ShowServers => s.copy(
+            defaultColumns = s.defaultColumns.copy(columns = rewriteAdminShowDefaultColumnSet(s.defaultColumnSet))
+          )
+        case s => s
+      }
+    )
+    assertRewrite(
+      "ENABLE SERVER 'foo' OPTIONS {key: 'value'}",
+      "ENABLE SERVER 'foo' OPTIONS {key: 'string[value]'}"
+    )
+    assertRewrite(
+      "ALTER SERVER $foo SET OPTIONS {key1: 'value', key2: $param}",
+      "ALTER SERVER $Xfoo SET OPTIONS {key1: 'string[value]', key2: $Xparam}"
+    )
+    assertRewrite(
+      "RENAME SERVER 'foo' TO 'bar'",
+      "RENAME SERVER 'foo' TO 'bar'"
+    )
+    assertRewrite(
+      "DROP SERVER 'foo'",
+      "DROP SERVER 'foo'"
+    )
+    assertRewrite(
+      "DEALLOCATE DATABASES FROM SERVERS 'foo', $bar",
+      "DEALLOCATE DATABASES FROM SERVERS 'foo', $Xbar"
+    )
+    assertRewrite(
+      "DRYRUN REALLOCATE DATABASES",
+      "DRYRUN REALLOCATE DATABASES"
+    )
+  }
+
+  test("administration database commands") {
+    // SHOW DATABASES are part of 'show and terminate commands'
+
+    assertRewrite(
+      "CREATE DATABASE foo TOPOLOGY 1 PRIMARY OPTIONS {key: 'value'} WAIT 10",
+      "CREATE DATABASE foo TOPOLOGY 1 PRIMARY OPTIONS {key: 'string[value]'} WAIT 10"
+    )
+    assertRewrite(
+      // Command is only available in Cypher 25
+      CypherVersion.Cypher25,
+      "CREATE DATABASE foo GRAPH SHARD { TOPOLOGY 2 PRIMARIES } PROPERTY SHARDS { COUNT 3 TOPOLOGY 2 REPLICA }",
+      "CREATE DATABASE foo GRAPH SHARD { TOPOLOGY 2 PRIMARIES } PROPERTY SHARDS { COUNT 3 TOPOLOGY 2 REPLICA }"
+    )
+    assertRewrite(
+      "CREATE COMPOSITE DATABASE foo DEFAULT LANGUAGE CYPHER 25 OPTIONS $param",
+      "CREATE COMPOSITE DATABASE foo DEFAULT LANGUAGE CYPHER 25 OPTIONS $Xparam"
+    )
+    assertRewrite(
+      "ALTER DATABASE $foo REMOVE OPTION bar WAIT 3",
+      "ALTER DATABASE $Xfoo REMOVE OPTION bar WAIT 3"
+    )
+    assertRewrite(
+      "ALTER DATABASE $foo SET OPTION bar 1+'xx' SET TOPOLOGY 1 SECONDARY SET DEFAULT LANGUAGE CYPHER 5",
+      "ALTER DATABASE $Xfoo SET OPTION bar 1+'string[xx]' SET TOPOLOGY 1 SECONDARY SET DEFAULT LANGUAGE CYPHER 5"
+    )
+    assertRewrite(
+      // Command is only available in Cypher 25
+      CypherVersion.Cypher25,
+      "ALTER DATABASE foo SET TOPOLOGY 3 REPLICAS SET GRAPH SHARD { SET TOPOLOGY 1 PRIMARY } SET PROPERTY SHARDS { SET TOPOLOGY 3 REPLICAS }",
+      "ALTER DATABASE foo SET TOPOLOGY 3 REPLICAS SET GRAPH SHARD { SET TOPOLOGY 1 PRIMARY } SET PROPERTY SHARDS { SET TOPOLOGY 3 REPLICAS }"
+    )
+    assertRewrite(
+      "DROP DATABASE foo",
+      "DROP DATABASE foo"
+    )
+    assertRewrite(
+      "START DATABASE foo WAIT 5 SECONDS",
+      "START DATABASE foo WAIT 5 SECONDS"
+    )
+    assertRewrite(
+      "STOP DATABASE foo",
+      "STOP DATABASE foo"
+    )
+  }
+
+  test("administration alias commands") {
+    assertRewrite(
+      "SHOW ALIAS foo FOR DATABASES YIELD name, database, location",
+      "SHOW ALIAS foo FOR DATABASES YIELD Xname, Xdatabase, Xlocation",
+      additionalExpectedAstUpdates = _ match {
+        case s: ShowAliases => s.copy(
+            defaultColumns = s.defaultColumns.copy(columns = rewriteAdminShowDefaultColumnSet(s.defaultColumnSet))
+          )
+        case s => s
+      }
+    )
+    assertRewrite(
+      "CREATE ALIAS foo FOR DATABASE bar PROPERTIES {key: 'value'}",
+      "CREATE ALIAS foo FOR DATABASE bar PROPERTIES {key: 'string[value]'}"
+    )
+    assertRewrite(
+      "CREATE ALIAS foo FOR DATABASE bar AT 'url' USER user PASSWORD 'secret' DRIVER {key: 'value'} DEFAULT LANGUAGE CYPHER 25",
+      "CREATE ALIAS foo FOR DATABASE bar AT 'url' USER `string[user]` PASSWORD 'secret' DRIVER {key: 'string[value]'} DEFAULT LANGUAGE CYPHER 25"
+    )
+    assertRewrite(
+      "ALTER ALIAS $foo SET DATABASE TARGET bar",
+      "ALTER ALIAS $Xfoo SET DATABASE TARGET bar"
+    )
+    assertRewrite(
+      "ALTER ALIAS $foo SET DATABASE TARGET bar AT $url USER user PASSWORD $pw",
+      "ALTER ALIAS $Xfoo SET DATABASE TARGET bar AT $Xurl USER `string[user]` PASSWORD $Xpw"
+    )
+    assertRewrite(
+      "ALTER ALIAS foo SET DATABASE DRIVER $param1 PROPERTIES $param2 DEFAULT LANGUAGE CYPHER 5",
+      "ALTER ALIAS foo SET DATABASE DRIVER $Xparam1 PROPERTIES $Xparam2 DEFAULT LANGUAGE CYPHER 5"
+    )
+    assertRewrite(
+      "DROP ALIAS foo FOR DATABASE",
+      "DROP ALIAS foo FOR DATABASE"
+    )
+  }
+
+  // Updates default columns for administration show commands to match anonymized names,
+  // the anonymization only affect the variables, not the string names
+  private def rewriteAdminShowDefaultColumnSet(defaultColumnSet: List[ShowColumn]): List[ShowColumn] = {
+    defaultColumnSet.map(c => c.copy(variable = c.variable.renameId("X" + c.variable.name)))
   }
 }
