36f6affacd93155ed432fd64b5d3feafd7f51c0a
Wilco <162103508+WilcoNeo@users.noreply.github.com>
2026-07-10 11:20:35 +0200

    PLAN-3414 - Add PlanLogJsonLayout ()
    This PR adds a separate JsonLayout for the planner logs, instead of
    reusing the one for the query logs.
    
    Include a UTC timestamp to each log line.
    Include the log level to each log line.
    The remaining element of each log line are given as a `Map<String,
    Object>`, which is handled by the map resolver. Flatten merges the
    fields with the parent, instead of creating a new JSON object from it
    and attaching it to the key `includeFullMap`.

--- community/logging/src/main/java/org/neo4j/logging/log4j/LogConfig.java
+++ community/logging/src/main/java/org/neo4j/logging/log4j/LogConfig.java
@@ -60,7 +60,7 @@ public final class LogConfig {
     public static final String HTTP_LOG = "http.log";
 
     public static final String QUERY_LOG_JSON_TEMPLATE = "classpath:org/neo4j/logging/QueryLogJsonLayout.json";
-    public static final String PLAN_LOG_JSON_TEMPLATE = "classpath:org/neo4j/logging/QueryLogJsonLayout.json";
+    public static final String PLAN_LOG_JSON_TEMPLATE = "classpath:org/neo4j/logging/PlanLogJsonLayout.json";
     // Please note: These 2 files seem unused in the monorepo, but they're used in Aura.
     // Do not simply remove these files. The constants here remain as a reminder, even if they
     // are unused.
