9a4621ff2f7874a027992b502b59f5b0f468df16
Emelie Bondesson <57797772+EmelieBondesson@users.noreply.github.com>
2026-07-10 08:07:41 +0200

    Remove split tests for UploadCommand that don't work ()
    Remove tests added with split dumps for `UploadCommand` used to upload
    artifact to aura. The tests only upload the first part and are green but
    that is wrong.
    
    Since it doesn't work right now and when we fix that command we will
    have to rewrite them anyway it's better to not have them and it looking
    like split files work
    
    Part of KRNL-1705

--- community/push-to-cloud-test/src/test/java/org/neo4j/export/UploadCommandTest.java
+++ community/push-to-cloud-test/src/test/java/org/neo4j/export/UploadCommandTest.java
@@ -188,30 +188,6 @@ class UploadCommandTest {
         verifyGCPPresignedEndpoints();
     }
 
-    @Test
-    void happyPathGCPSplitUploadCommandTest() {
-        String authResponse = "token";
-        createAuraHappyPathStubs(authResponse);
-        createGCPHappyPathWireMockStubs(authResponse);
-        AuraConsole testConsole = new AuraConsole(MOCK_BASE_URL, "sausage");
-        wireMockServer.stubFor(get(urlMatching(".*?/import/status$"))
-                .withHeader("Authorization", equalTo("Bearer " + authResponse))
-                .willReturn(firstSuccessfulDatabaseRunningResponse())
-                .inScenario("test")
-                .whenScenarioStateIs(Scenario.STARTED)
-                .willSetStateTo(STATUS_POLLING_PASSED_FIRST_CALL));
-
-        AuraURLFactory auraURLFactory = mock(AuraURLFactory.class);
-        when(auraURLFactory.buildConsoleURI(any(), anyBoolean())).thenReturn(testConsole);
-        UploadCommand command = buildUploadCommand(auraURLFactory);
-        String[] args = getNormalRuntimeArgs(SPLIT_DBNAME);
-
-        assertThatCode(() -> new CommandLine(command).execute(args)).doesNotThrowAnyException();
-
-        verifyCommonConsoleUrls();
-        verifyGCPPresignedEndpoints();
-    }
-
     private String[] getNormalRuntimeArgs() {
         return getNormalRuntimeArgs(DBNAME);
     }
@@ -286,43 +262,6 @@ class UploadCommandTest {
                 PushToCloudCLI.fakeCLI("username", "password", false));
 
         String[] args = getNormalRuntimeArgs();
-        assertThatCode(() -> new CommandLine(command).execute(args)).doesNotThrowAnyException();
-
-        verifyCommonConsoleUrls();
-
-        verify(putRequestedFor(urlEqualTo("/signed1")));
-        verify(putRequestedFor(urlEqualTo("/signed2")));
-        verify(putRequestedFor(urlEqualTo("/signed3")));
-    }
-
-    @Test
-    void happyPathAWSSplitUploadCommandTest() {
-        String authResponse = "token";
-        createAuraHappyPathStubs(authResponse);
-
-        wireMockServer.stubFor(
-                initiateUploadTargetRequest(authResponse).willReturn(successfulInitiateUploadTargetAWSResponse()));
-        wireMockServer.stubFor(uploadRequest().willReturn(aResponse().withStatus(HTTP_OK)));
-        AuraConsole testConsole = new AuraConsole(MOCK_BASE_URL, "sausage");
-        wireMockServer.stubFor(get(urlMatching(".*?/import/status$"))
-                .withHeader("Authorization", equalTo("Bearer " + authResponse))
-                .willReturn(firstSuccessfulDatabaseRunningResponse())
-                .inScenario("test")
-                .whenScenarioStateIs(Scenario.STARTED)
-                .willSetStateTo(STATUS_POLLING_PASSED_FIRST_CALL));
-        wireMockServer.stubFor(getMultiPartStatusRequest().willReturn(successfulMultiPartStatusResponse()));
-
-        AuraURLFactory auraURLFactory = mock(AuraURLFactory.class);
-        when(auraURLFactory.buildConsoleURI(any(), anyBoolean())).thenReturn(testConsole);
-        AuraClient.AuraClientBuilder auraClientBuilder = new AuraClient.AuraClientBuilder(ctx);
-        UploadCommand command = new UploadCommand(
-                ctx,
-                auraClientBuilder,
-                auraURLFactory,
-                new FakeAWSUploadURLFactory(),
-                PushToCloudCLI.fakeCLI("username", "password", false));
-
-        String[] args = getNormalRuntimeArgs(SPLIT_DBNAME);
         assertThatCode(() -> new CommandLine(command).execute(args)).doesNotThrowAnyException();
 
         verifyCommonConsoleUrls();
