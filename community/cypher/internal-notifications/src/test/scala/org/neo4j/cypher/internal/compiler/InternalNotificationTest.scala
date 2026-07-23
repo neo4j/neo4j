510952bc4be92ad234eaa854a9efd6c1d5e69264
Georgiy Kargapolov <1788612+inmost-light@users.noreply.github.com>
2026-07-23 10:31:20 +0200

    Move InternalNotificationTest to neo4j-internal-notifications ()

--- community/cypher/internal-notifications/src/test/scala/org/neo4j/cypher/internal/compiler/InternalNotificationTest.scala
+++ community/cypher/internal-notifications/src/test/scala/org/neo4j/cypher/internal/compiler/InternalNotificationTest.scala
@@ -21,6 +21,7 @@ package org.neo4j.cypher.internal.compiler
 
 import org.neo4j.cypher.internal.notification.InternalNotification
 import org.neo4j.cypher.internal.notification.InternalNotifications
+import org.neo4j.cypher.internal.util.test_helpers.CypherFunSuite
 import org.neo4j.cypher.internal.util.test_helpers.DiffPrinter
 import org.reflections.Reflections
 import org.reflections.scanners.Scanners
@@ -28,7 +29,7 @@ import org.reflections.util.ConfigurationBuilder
 
 import scala.jdk.CollectionConverters.CollectionHasAsScala
 
-class InternalNotificationTest extends CypherPlannerTestSuite {
+class InternalNotificationTest extends CypherFunSuite {
 
   private val reflections = new Reflections(new ConfigurationBuilder()
     .forPackages("org.neo4j")
